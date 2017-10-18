2e30e78399a0f2a3f8adaaf62c58a9075c74d7ff
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-18 12:13:54 +0100

    expose h and preact render

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//@flow
+import { render } from 'preact';
+
+export { render };
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,43 @@
 //@flow
 const path = require('path');
 
-module.exports = {
-    entry: './src/h.jsx',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'h.js',
+module.exports = [
+    {
+        entry: {
+            h: './src/h.jsx',
+        },
+        output: {
+            path: __dirname,
+            filename: '[name].js',
+            library: 'h',
+            libraryTarget: 'commonjs2',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+            ],
+        },
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
-};
+    {
+        entry: {
+            index: './src/index.js',
+        },
+        output: {
+            path: __dirname,
+            filename: '[name].js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+            ],
+        },
+    }
+];
