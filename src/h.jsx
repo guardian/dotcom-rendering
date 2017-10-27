63d368c568a5081918ea7c8bc6e17bce202bc50a
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-27 13:46:14 +0100

    simplify comment

--- src/h.jsx
+++ src/h.jsx
@@ -1,8 +1,7 @@
 // @flow
 /* eslint-disable camelcase */
 
-// bespoke wrapper around preact's `h` that passes any CSS data
-// on `attributes.style` to styletron, then hands off to `preact#h`
+// bespoke wrapper around preact's `h`
 
 import { h as preact_h } from 'preact';
 import { css } from 'emotion';
