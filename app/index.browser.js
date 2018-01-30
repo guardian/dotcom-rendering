9d623ccbb80a46c97ed4b73a55765659f2935049
Alex Sanders <a@snd.rs>
2018-01-30 18:43:57 +0000

    overkill

--- app/index.browser.js
+++ app/index.browser.js
@@ -1,12 +1,8 @@
 // @flow
 
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import App from './app';
 
 __webpack_public_path__ = '/assets/javascript/';
 
-const render = () => {
-    ReactDOM.hydrate(<App />, document.getElementById('app'));
-};
-
-render();
+hydrate(<App />, document.getElementById('app'));
