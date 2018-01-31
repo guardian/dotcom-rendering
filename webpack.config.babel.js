21b17dadd6c48dca0e8c60ff827318573a8bfc40
Alex Sanders <a@snd.rs>
2018-01-31 10:47:34 +0000

    set envs

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -16,27 +16,54 @@ const moduleConfig = {
     ],
 };
 
-const resolveConfig = {
-    extensions: ['.js', '.jsx'],
+const stats = {
+    assets: false,
+    cached: false,
+    cachedAssets: false,
+    children: false,
+    chunks: false,
+    chunkModules: false,
+    chunkOrigins: false,
+    depth: false,
+    entrypoints: false,
+    exclude: [],
+    hash: false,
+    modules: false,
+    moduleTrace: false,
+    performance: false,
+    providedExports: false,
+    publicPath: false,
+    reasons: false,
+    source: false,
+    version: false,
+
+    // show these...
+    colors: true,
+    errors: true,
+    errorDetails: true,
+    timings: true,
+    warnings: true,
 };
 
 module.exports = {
     entry: {
         app: './app/index.browser.js',
+        'app.server': './app/index.server.js',
     },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js',
     },
     module: moduleConfig,
-    resolve: resolveConfig,
     devServer: {
         publicPath: '/assets/javascript/',
         port: 3000,
         overlay: true,
-        noInfo: true,
+        // noInfo: true,
+        stats,
         before(app) {
             app.get('/', (req, res) => {
+                // make sure each reload is a fresh rendering
                 Object.keys(require.cache).forEach(
                     key => delete require.cache[key],
                 );
@@ -46,6 +73,7 @@ module.exports = {
 
                 // eslint-disable-next-line global-require
                 const document = require('./app/document/html').default;
+
                 res.send(document({ html, css }));
             });
         },
