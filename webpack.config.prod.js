78014ca0407cdc6975693196cc2d85ffc6ae7d90
Alex Sanders <a@snd.rs>
2018-01-31 14:00:20 +0000

    optimse manually

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,22 +2,60 @@
 /* eslint-disable no-console */
 import path from 'path';
 import { readdirSync, readFileSync } from 'fs';
+
+import webpack from 'webpack';
 import WebpackOnBuildPlugin from 'on-build-webpack';
 import filesizegzip from 'filesizegzip';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 module.exports = ({ dist }: { dist: string }) => ({
     browser: {
         devtool: 'source-map',
         plugins: [
             new WebpackOnBuildPlugin(() => {
-                readdirSync(path.join(__dirname, 'dist')).forEach(file => {
-                    console.log(
-                        `${file} ${filesizegzip(
-                            readFileSync(path.join(dist, file), 'utf8'),
-                            true,
-                        )}`,
-                    );
-                });
+                readdirSync(path.join(__dirname, 'dist'))
+                    .filter(
+                        file =>
+                            !file.endsWith('.map') && file.includes('browser'),
+                    )
+                    .forEach(file => {
+                        console.log(
+                            `${file} ${filesizegzip(
+                                readFileSync(path.join(dist, file), 'utf8'),
+                                true,
+                            )}`,
+                        );
+                    });
+            }),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production'),
+            }),
+            new webpack.optimize.ModuleConcatenationPlugin(),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                sourceMap: true,
+                parallel: true,
+            }),
+            new BundleAnalyzerPlugin({
+                defaultSizes: 'gzip',
+                reportFilename: path.join(dist, 'ui.bundle.browser.stats.html'),
+                analyzerMode: 'static',
+                openAnalyzer: false,
+            }),
+        ],
+    },
+    server: {
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production'),
+            }),
+            new webpack.optimize.ModuleConcatenationPlugin(),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                sourceMap: true,
+                mangle: false,
+                beautify: true,
+                comments: true,
             }),
         ],
     },
