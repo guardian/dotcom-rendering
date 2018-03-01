3b0371876ba0683be6506fc2cd662206b2662713
Alex Sanders <a@snd.rs>
2018-03-01 17:06:55 +0000

    arrays of fonts

--- src/pasteup/fonts.js
+++ src/pasteup/fonts.js
@@ -1,10 +1,20 @@
 // @flow
 
-export const textSans =
-    '"Guardian Text Sans Web","Helvetica Neue","Helvetica","Arial","Lucida Grande","sans-serif"';
+export const textSans = [
+    'Guardian Text Sans Web',
+    'Helvetica Neue',
+    'Helvetica',
+    'Arial',
+    'Lucida Grande',
+    'sans-serif',
+].join(',');
 
-export const egyptian = '"Guardian Egyptian Web", "Georgia", "serif"';
+export const egyptian = ['Guardian Egyptian Web', 'Georgia', 'serif'].join(',');
 
-export const textEgyptian = '"Guardian Text Egyptian Web", "Georgia", "serif"';
+export const textEgyptian = [
+    'Guardian Text Egyptian Web',
+    'Georgia',
+    'serif',
+].join(',');
 
-export const headline = '"GH Guardian Headline", "Georgia", "serif"';
+export const headline = ['GH Guardian Headline', 'Georgia', 'serif'].join(',');
