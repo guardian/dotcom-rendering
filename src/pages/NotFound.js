352b34de716491d4fc87d4768796aea54f271b05
Matthew Walls <matthew.walls@guardian.co.uk>
2018-03-08 14:36:26 +0000

    Remove unused variables

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,6 @@
 // @flow
 
-import { Subscribe } from 'unstated';
-
 import NotFound from 'components/NotFound';
-import App from 'lib/AppContainer';
 
 const NotFoundPage = () => (
     <NotFound/>
