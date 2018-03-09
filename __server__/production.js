5df147451d73ea628ba986412c4535aef4971dce
Alex Sanders <a@snd.rs>
2018-03-09 23:20:17 +0000

    make prod 😍 dev

--- __server__/production.js
+++ __server__/production.js
@@ -1,5 +1,6 @@
 // @flow
 
+const path = require('path');
 const express = require('express');
 // eslint-disable-next-line import/no-unresolved
 const pagesMiddleware = require('../dist/app.server.js').default;
@@ -7,12 +8,18 @@ const fetch = require('node-fetch');
 
 const app = express();
 
+app.use('/static', express.static(path.join(__dirname, '..', 'src', 'static')));
+app.use(
+    '/assets/javascript',
+    express.static(path.join(__dirname, '..', 'dist')),
+);
+
 app.use('/pages/:page', [
     async (req, res, next) => {
         // TODO: get config from request
         const { html, ...config } = await fetch(
             `${req.query.url ||
-                'https://www.theguardian.com/world/2013/jun/09/edward-snowden-nsa-whistleblower-surveillance'}.json`,
+                'https://www.theguardian.com/world/2013/jun/09/edward-snowden-nsa-whistleblower-surveillance'}.json?guui`,
         ).then(article => article.json());
         req.body = config;
         next();
