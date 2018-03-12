9d7c8c32052ba20e50198386971d711d24e02092
Alex Sanders <a@snd.rs>
2018-03-12 09:42:40 +0000

    don’t compose on each call

--- __tools__/clean-capi/index.js
+++ __tools__/clean-capi/index.js
@@ -6,15 +6,18 @@ import { string as curly } from 'curlyquotes';
 import minify from './minify';
 import bigBullets from './big-bullets';
 
+const headline = compose(minify, curly);
+const standfirst = compose(minify, bigBullets);
+const main = minify;
+const body = minify;
+
 export default ({ contentFields, config }) => ({
     // here we create our own object of CAPI content on the 'content' key
     content: {
-        headline: compose(minify, curly)(config.page.headline),
-        standfirst: compose(minify, bigBullets)(
-            contentFields.fields.standfirst,
-        ),
-        main: minify(contentFields.fields.main),
-        body: minify(
+        headline: headline(config.page.headline),
+        standfirst: standfirst(contentFields.fields.standfirst),
+        main: main(contentFields.fields.main),
+        body: body(
             contentFields.fields.blocks.body
                 .map(block => block.bodyHtml)
                 .filter(Boolean)
