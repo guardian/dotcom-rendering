b29a134547f2e47a2cdfa579419dbf734481c3ca
Alex Sanders <a@snd.rs>
2018-03-21 11:22:15 +0000

    make site message a component

--- src/components/Epic.js
+++ src/components/Epic.js
@@ -1,25 +1,12 @@
 // @flow
 import styled from 'preact-emotion';
-import { transparentize } from 'polished';
 import pallete from 'pasteup/palette';
 import { headline } from 'pasteup/fonts';
 
-import CloseButton from 'components/buttons/Close';
+import SiteMessage from 'components/SiteMessage';
 
 const yellow = pallete.yellow.medium;
 const black = pallete.neutral[2];
-
-const Overlay = styled('div')({
-    backgroundColor: transparentize(0.03, yellow),
-    width: ' 100%',
-    position: 'fixed',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 20,
-    display: 'flex',
-    justifyContent: 'space-between',
-});
 
 const Content = styled('div')({
     fontFamily: headline,
@@ -33,9 +20,8 @@ const Content = styled('div')({
 
 export default function Epic({ children }) {
     return (
-        <Overlay>
+        <SiteMessage foregroundColor={black} backgroundColor={yellow}>
             <Content>{children}</Content>
-            <CloseButton foregroundColor={black} backgroundColor={yellow} />
-        </Overlay>
+        </SiteMessage>
     );
 }
