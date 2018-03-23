db72a7804b3d5f3b09ea860fedf27359ee035bd3
Simon Adcock <simon.adcock@guardian.co.uk>
2018-03-23 14:47:52 +0000

    make fix

--- src/__lib__/asset-path.js
+++ src/__lib__/asset-path.js
@@ -9,16 +9,17 @@ try {
     // do nothing
 }
 
-const prefix = (path : string, name: string): string => {
+const prefix = (path: string, name: string): string => {
     if (process.env.NODE_ENV === 'production') {
         return `//assets.guim.co.uk/guui/${path}/${name}`;
     }
     return `/${path}/${name}`;
 };
 
-type filetype = 'javascript' | 'css';
+type FileType = 'javascript' | 'css';
 
-export const hashedPath = (filetype : filetype, name : string) => prefix(`assets/${filetype}`, assetHash[name] || name);
+export const hashedPath = (fileType: FileType, name: string) =>
+    prefix(`assets/${fileType}`, assetHash[name] || name);
 
-export const staticPath = (filetype : filetype, name : string) => prefix(`static/${filetype}`, assetHash[name] || name);
-
+export const staticPath = (fileType: FileType, name: string) =>
+    prefix(`static/${fileType}`, assetHash[name] || name);
