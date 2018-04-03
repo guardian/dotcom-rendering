167c023f39a0bf019b7484a1d1aabebf8ea24fa4
Simon Adcock <simon.adcock@guardian.co.uk>
2018-04-03 16:06:35 +0100

    most viewed poc

---
+++ src/components/MostViewed/index.js
@@ -0,0 +1,35 @@
+// @flow
+import { Component } from 'preact';
+import styled from 'preact-emotion';
+
+const MostViewed = styled('div')({});
+
+MostViewed.displayName = 'MostViewed';
+
+export default class MostViewedComp extends Component {
+    constructor() {
+        super();
+        this.setState({
+            html: 'Placeholder text',
+        });
+    }
+
+    componentDidMount() {
+        fetch('https://api.nextgen.guardianapps.co.uk/most-read-geo.json')
+            .then(resp => resp.json())
+            .then(({ html }) => {
+                this.setState({
+                    html,
+                });
+            });
+    }
+
+    render() {
+        return (
+            <MostViewed>
+                <h1>Most Viewed</h1>
+                <div dangerouslySetInnerHTML={{ __html: this.state.html }} />
+            </MostViewed>
+        );
+    }
+}
