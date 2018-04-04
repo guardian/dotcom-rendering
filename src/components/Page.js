126c0e172fa3ac7ba3a5258b28bc3c896e873107
Simon Adcock <simon.adcock@guardian.co.uk>
2018-04-04 14:43:59 +0100

    move header and epic to article

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,7 +1,5 @@
 // @flow
 import styled from 'preact-emotion';
-import Header from 'components/Header';
-import Epic from 'components/Epic';
 
 import {
     mobileLandscape,
@@ -34,24 +32,5 @@ const PageWrapper = styled('div')({
 });
 
 export default function Page({ children }) {
-    return (
-        <PageWrapper>
-            <Header />
-            {children}
-            <Epic>
-                <strong>
-                    Unlike many news organisations, we haven’t put up a paywall
-                    – we want to keep our journalism as open as we can.
-                </strong>{' '}
-                The Guardian’s independent, investigative journalism takes a lot
-                of time, money and hard work to produce. But the revenue we get
-                from advertising is falling, so we increasingly need our readers
-                to fund us. If everyone who reads our reporting, who likes it,
-                helps fund it, our future would be much more secure.{' '}
-                <strong>
-                    Support The Guardian for just 17p a day or £5 a month.
-                </strong>
-            </Epic>
-        </PageWrapper>
-    );
+    return <PageWrapper>{children}</PageWrapper>;
 }
