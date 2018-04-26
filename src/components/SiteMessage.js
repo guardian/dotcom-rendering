c2eae94c26251b709f7de2d9b52d8b2ed4c2472a
Alex Sanders <a@snd.rs>
2018-04-26 17:17:11 +0100

    rejig

--- src/components/SiteMessage.js
+++ src/components/SiteMessage.js
@@ -6,32 +6,47 @@ import { transparentize } from 'polished';
 import pallete from 'pasteup/palette';
 import CloseButton from 'components/buttons/Close';
 
-const Overlay = styled('div')(({ foregroundColor, backgroundColor, fade }) => ({
-    backgroundColor: transparentize(0.03, backgroundColor),
-    color: foregroundColor,
-    fill: foregroundColor,
-    width: ' 100%',
-    position: 'fixed',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 20,
-    display: 'flex',
-    justifyContent: 'space-between',
-    transform: fade ? 'translateY(100%)' : 'translateY(0)',
-    transition: 'transform .1s ease-out',
-}));
+const duration = 100;
+
+const Overlay = styled('div')(
+    ({ foregroundColor, backgroundColor, close }) => ({
+        backgroundColor: transparentize(0.03, backgroundColor),
+        color: foregroundColor,
+        fill: foregroundColor,
+        width: ' 100%',
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        padding: 20,
+        display: 'flex',
+        justifyContent: 'space-between',
+        transform: close ? 'translateY(100%)' : 'translateY(0)',
+        transition: `transform ${duration}ms ease-out`,
+    }),
+);
 
 export default class SiteMessage extends Component {
+    static defaultProps = {
+        foregroundColor: 'white',
+        backgroundColor: pallete.neutral[1],
+    };
+
     constructor(props) {
         super(props);
 
-        this.state = { closed: false, fade: false };
+        this.state = { closed: false, close: false };
     }
 
     close = () => {
-        this.setState({ fade: true });
-        setTimeout(() => this.setState({ closed: true }), 300);
+        this.setState({ close: true });
+        setTimeout(
+            () =>
+                window.requestAnimationFrame(() =>
+                    this.setState({ closed: true }),
+                ),
+            duration,
+        );
     };
 
     render() {
@@ -41,7 +56,7 @@ export default class SiteMessage extends Component {
                 <Overlay
                     foregroundColor={this.props.foregroundColor}
                     backgroundColor={this.props.backgroundColor}
-                    fade={this.state.fade}
+                    close={this.state.close}
                     {...this.props}
                 >
                     {this.props.children}
@@ -55,7 +70,3 @@ export default class SiteMessage extends Component {
         );
     }
 }
-SiteMessage.defaultProps = {
-    foregroundColor: 'white',
-    backgroundColor: pallete.neutral[1],
-};
