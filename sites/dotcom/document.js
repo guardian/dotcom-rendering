37992a6f1f1fa56657238827772fa2c76f53423f
GHaberis <george.haberis@theguardian.com>
2018-05-30 11:37:26 +0100

    fix validation

--- sites/dotcom/document.js
+++ sites/dotcom/document.js
@@ -22,7 +22,9 @@ export default ({ Page, data: { body, ...data } }: Props) => {
         'javascript',
         `${cleanedData.site}.${cleanedData.page.toLowerCase()}.js`,
     ];
-    const { html, css, ids: cssIDs } = renderToString(<App data={{ ...cleanedData }} Page={Page} />);
+    const { html, css, ids: cssIDs } = renderToString(
+        <App data={{ ...cleanedData }} Page={Page} />,
+    );
     /**
      * To save sending CAPI data twice (in the HEAD and BODY)
      * we replace any keys present in the CapiComponent keyRegister
