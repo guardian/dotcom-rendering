00420e6323a68be55050146685127d846aec877a
Nicolas Long <nicolaslong@gmail.com>
2018-07-24 15:51:49 +0100

    Update footer links

--- sites/frontend/components/Footer/index.js
+++ sites/frontend/components/Footer/index.js
@@ -14,94 +14,98 @@ type Link = {
 const footerLinks: Array<Array<Link>> = [
     [
         {
-            title: 'jobs',
-            url: '/',
+            title: 'Jobs',
+            url: 'https://jobs.theguardian.com/?INTCMP=NGW_FOOTER_UK_GU_JOBS',
         },
         {
-            title: 'dating',
-            url: '/',
+            title: 'Dating',
+            url:
+                'https://soulmates.theguardian.com/?INTCMP=NGW_FOOTER_UK_GU_SOULMATES',
         },
         {
-            title: 'make a contribution',
-            url: '/',
+            title: 'Make a contribution',
+            url:
+                'https://support.theguardian.com/contribute?INTCMP=footer_support_contribute&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_FOOTER%22%2C%22componentId%22%3A%22footer_support_contribute%22%2C%22referrerPageviewId%22%3A%22jjztcoahsd3lnjj62kb8%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fwww.theguardian.com%2Fbooks%2F2018%2Fjul%2F24%2Fman-booker-prize-2018-longlist-nick-drnaso-sabrina-ondaatje-graphic-novel%22%7D',
         },
         {
-            title: 'subscribe',
-            url: '/',
+            title: 'Subscribe',
+            url:
+                'https://support.theguardian.com/subscribe?INTCMP=footer_support_subscribe&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_FOOTER%22%2C%22componentId%22%3A%22footer_support_subscribe%22%2C%22referrerPageviewId%22%3A%22jjztcoahsd3lnjj62kb8%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fwww.theguardian.com%2Fbooks%2F2018%2Fjul%2F24%2Fman-booker-prize-2018-longlist-nick-drnaso-sabrina-ondaatje-graphic-novel%22%7D',
         },
         {
-            title: 'guardian labs',
-            url: '/',
-        },
-    ],
-    [
-        {
-            title: 'about us',
-            url: '/',
-        },
-        {
-            title: 'work for us',
-            url: '/',
-        },
-        {
-            title: 'advertise with us',
-            url: '/',
-        },
-        {
-            title: 'contact us',
-            url: '/',
-        },
-        {
-            title: 'help',
-            url: '/',
+            title: 'Guardian Labs',
+            url: '/guardian-labs',
         },
     ],
     [
         {
-            title: 'terms & conditions',
-            url: '/',
+            title: 'About us',
+            url: '/about',
         },
         {
-            title: 'privacy policy',
-            url: '/',
+            title: 'Work for us',
+            url: 'https://workforus.theguardian.com/locations/london',
         },
         {
-            title: 'cookie policy',
-            url: '/',
+            title: 'Advertise with us',
+            url: 'https://advertising.theguardian.com/',
         },
         {
-            title: 'securedrop',
-            url: '/',
+            title: 'Contact us',
+            url: '/help/contact-us',
         },
         {
-            title: 'digital newspaper archive',
-            url: '/',
-        },
-        {
-            title: 'complaints and corrections',
-            url: '/',
+            title: 'Help',
+            url: '/help',
         },
     ],
     [
         {
-            title: 'all topics',
-            url: '/',
+            title: 'Terms & conditions',
+            url: '/help/terms-of-service',
         },
         {
-            title: 'all contributors',
-            url: '/',
+            title: 'Privacy policy',
+            url: '/info/privacy',
         },
         {
-            title: 'modern slavery act',
-            url: '/',
+            title: 'Cookie policy',
+            url: '/info/cookies',
         },
         {
-            title: 'facebook',
-            url: '/',
+            title: 'Securedrop',
+            url: 'https://securedrop.theguardian.com/',
         },
         {
-            title: 'twitter',
-            url: '/',
+            title: 'Digital newspaper archive',
+            url: 'https://theguardian.newspapers.com/',
+        },
+        {
+            title: 'Complaints and corrections',
+            url: '/info/complaints-and-corrections',
+        },
+    ],
+    [
+        {
+            title: 'All topics',
+            url: '/index/subjects/a',
+        },
+        {
+            title: 'All contributors',
+            url: '/index/contributors',
+        },
+        {
+            title: 'Modern Slavery Act',
+            url:
+                '/info/2016/jul/27/modern-slavery-and-our-supply-chains?INTCMP=NGW_FOOTER_UK_GU_MODERN_SLAVERY_ACT',
+        },
+        {
+            title: 'Facebook',
+            url: 'https://www.facebook.com/theguardian',
+        },
+        {
+            title: 'Twitter',
+            url: 'https://twitter.com/guardian',
         },
     ],
 ];
