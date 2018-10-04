448874bba31a980ff90dd99820c45560e14a7e44
GHaberis <george.haberis@theguardian.com>
2018-10-04 17:53:42 +0100

    pull SubMetaLinksList out of ArticleBody

--- frontend/components/ArticleBody.tsx
+++ frontend/components/ArticleBody.tsx
@@ -7,6 +7,7 @@ import dateformat from 'dateformat';
 import { sans, serif } from '@guardian/pasteup/fonts';
 import { ShareCount } from './ShareCount';
 import { SharingIcons } from './ShareIcons';
+import { SubMetaLinksList } from './SubMetaLinksList';
 
 // tslint:disable:react-no-dangerous-html
 
@@ -311,67 +312,6 @@ const subMetaLabel = css`
     color: ${palette.neutral[60]};
 `;
 
-const subMetaLinksList = css`
-    list-style: none;
-`;
-
-const subMetaSectionLinksList = css`
-    line-height: 24px;
-`;
-
-const subMetaKeywordLinksList = css`
-    padding-bottom: 12px;
-    margin-bottom: 6px;
-    border-bottom: 1px solid ${palette.neutral[86]};
-`;
-
-const subMetaLinksListItem = css`
-    margin-right: 5px;
-    display: inline-block;
-    a {
-        position: relative;
-        display: block;
-        padding-right: 5px;
-        text-decoration: none;
-    }
-    a::after {
-        content: '/';
-        position: absolute;
-        pointer-events: none;
-        top: 0;
-        right: -3px;
-        color: ${palette.neutral[86]};
-    }
-    a:hover {
-        text-decoration: underline;
-    }
-`;
-
-const subMetaLink = css`
-    text-decoration: none;
-    :hover {
-        text-decoration: underline;
-    }
-`;
-
-const subMetaSectionLink = css`
-    font-size: 16px;
-    line-height: 20px;
-    font-family: ${serif.body};
-`;
-
-const subMetaKeywordLink = css`
-    font-size: 14px;
-    line-height: 22px;
-    font-family: ${sans.body};
-`;
-
-const hideSlash = css`
-    a::after {
-        content: '';
-    }
-`;
-
 const subMetaSharingIcons = css`
     :after {
         content: '';
@@ -379,44 +319,6 @@ const subMetaSharingIcons = css`
         clear: left;
     }
 `;
-
-const SubMetaLinksList: React.SFC<{
-    links: SimpleLinkType[];
-    isSectionLinkList: boolean;
-    pillar: Pillar;
-}> = ({ links, isSectionLinkList, pillar }) => (
-    <ul
-        className={cx(subMetaLinksList, [
-            isSectionLinkList
-                ? subMetaSectionLinksList
-                : subMetaKeywordLinksList,
-        ])}
-    >
-        {links.map((link, i) => (
-            <li
-                className={cx(
-                    subMetaLinksListItem,
-                    [
-                        isSectionLinkList
-                            ? subMetaSectionLink
-                            : subMetaKeywordLink,
-                    ],
-                    {
-                        [hideSlash]: i === links.length - 1,
-                    },
-                )}
-                key={link.url}
-            >
-                <a
-                    className={cx(subMetaLink, pillarColours[pillar])}
-                    href={link.url}
-                >
-                    {link.title}
-                </a>
-            </li>
-        ))}
-    </ul>
-);
 
 const ArticleBody: React.SFC<{
     CAPI: CAPIType;
