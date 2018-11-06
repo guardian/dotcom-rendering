d03822682718d8c64818ab5493d70ed6586e6f25
GHaberis <george.haberis@theguardian.com>
2018-11-06 12:29:42 +0000

    Update async tests

--- frontend/components/ShareCount.test.tsx
+++ frontend/components/ShareCount.test.tsx
@@ -3,14 +3,12 @@ import { shallow } from 'enzyme';
 import { ShareCount } from './ShareCount';
 import { CAPI } from '../../test/fixtures/CAPI';
 
-const fetchShareCount: (shareCount: number) => Promise<{}> = shareCount =>
-    new Promise(resolve => {
-        resolve({
-            ok: true,
-            json: () => ({
-                share_count: shareCount,
-            }),
-        });
+const fetchResult: (shareCount: number) => Promise<{}> = shareCount =>
+    Promise.resolve({
+        ok: true,
+        json: () => ({
+            share_count: shareCount,
+        }),
     });
 
 describe('ShareCount', () => {
@@ -29,8 +27,8 @@ describe('ShareCount', () => {
     });
 
     describe('snapshots', () => {
-        it('It should not render if state.shareCount is falsy', () => {
-            const fakeFetch = fetchShareCount(0);
+        it('It should not render if state.shareCount is falsy', async () => {
+            const fakeFetch = fetchResult(0);
 
             globalAny.fetch = jest.fn().mockImplementation(() => fakeFetch);
 
@@ -38,17 +36,14 @@ describe('ShareCount', () => {
                 <ShareCount config={config} CAPI={CAPI} />,
             );
 
-            return fakeFetch
-                .then(() => {
-                    component.update();
-                })
-                .then(() => {
-                    expect(component).toMatchSnapshot();
-                });
+            await fakeFetch;
+            await component.update();
+
+            expect(component).toMatchSnapshot();
         });
 
-        it('It should render if state.shareCount is truthy', () => {
-            const fakeFetch = fetchShareCount(100);
+        it('It should render if state.shareCount is truthy', async () => {
+            const fakeFetch = fetchResult(100);
 
             globalAny.fetch = jest.fn().mockImplementation(() => fakeFetch);
 
@@ -56,17 +51,14 @@ describe('ShareCount', () => {
                 <ShareCount config={config} CAPI={CAPI} />,
             );
 
-            return fakeFetch
-                .then(() => {
-                    component.update();
-                })
-                .then(() => {
-                    expect(component).toMatchSnapshot();
-                });
+            await fakeFetch;
+            await component.update();
+
+            expect(component).toMatchSnapshot();
         });
 
-        it('It should format long shareCount correctly', () => {
-            const fakeFetch = fetchShareCount(25000);
+        it('It should format long shareCount correctly', async () => {
+            const fakeFetch = fetchResult(25000);
 
             globalAny.fetch = jest.fn().mockImplementation(() => fakeFetch);
 
@@ -74,68 +66,56 @@ describe('ShareCount', () => {
                 <ShareCount config={config} CAPI={CAPI} />,
             );
 
-            return fakeFetch
-                .then(() => {
-                    component.update();
-                })
-                .then(() => {
-                    expect(component).toMatchSnapshot();
-                });
+            await fakeFetch;
+            await component.update();
+
+            expect(component).toMatchSnapshot();
         });
     });
 
-    it('It should render null if state.shareCount is falsy', () => {
-        const fakeFetch = fetchShareCount(0);
+    it('It should render null if state.shareCount is falsy', async () => {
+        const fakeFetch = fetchResult(0);
 
         globalAny.fetch = jest.fn().mockImplementation(() => fakeFetch);
 
         const component = shallow(<ShareCount config={config} CAPI={CAPI} />);
 
-        return fakeFetch
-            .then(() => {
-                component.update();
-            })
-            .then(() => {
-                expect(component.type()).toBeNull();
-                expect(component.state('shareCount')).toBe(0);
-            });
+        await fakeFetch;
+        await component.update();
+
+        expect(component.type()).toBeNull();
+        expect(component.state('shareCount')).toBe(0);
     });
 
-    it('It should render if state.shareCount is truthy', () => {
-        const fakeFetch = fetchShareCount(100);
+    it('It should render if state.shareCount is truthy', async () => {
+        const fakeFetch = fetchResult(100);
 
         globalAny.fetch = jest.fn().mockImplementation(() => fakeFetch);
 
         const component = shallow(<ShareCount config={config} CAPI={CAPI} />);
 
-        return fakeFetch
-            .then(() => {
-                component.update();
-            })
-            .then(() => {
-                expect(component.type()).not.toBeNull();
-                expect(component.state('shareCount')).toBe(100);
-                expect(component.find('.js-countFull').text()).toBe('100');
-                expect(component.find('.js-countShort').text()).toBe('100');
-            });
+        await fakeFetch;
+        await component.update();
+
+        expect(component.type()).not.toBeNull();
+        expect(component.state('shareCount')).toBe(100);
+        expect(component.find('.js-countFull').text()).toBe('100');
+        expect(component.find('.js-countShort').text()).toBe('100');
     });
 
-    it('It should format long shareCount correctly', () => {
-        const fakeFetch = fetchShareCount(25000);
+    it('It should format long shareCount correctly', async () => {
+        const fakeFetch = fetchResult(25000);
 
         globalAny.fetch = jest.fn().mockImplementation(() => fakeFetch);
 
         const component = shallow(<ShareCount config={config} CAPI={CAPI} />);
 
-        return fakeFetch
-            .then(() => {
-                component.update();
-            })
-            .then(() => {
-                expect(component.type()).not.toBeNull();
-                expect(component.state('shareCount')).toBe(25000);
-                expect(component.find('.js-countFull').text()).toBe('25,000');
-                expect(component.find('.js-countShort').text()).toBe('25k');
-            });
+        await fakeFetch;
+        await component.update();
+
+        expect(component.type()).not.toBeNull();
+        expect(component.state('shareCount')).toBe(25000);
+        expect(component.find('.js-countFull').text()).toBe('25,000');
+        expect(component.find('.js-countShort').text()).toBe('25k');
     });
 });
