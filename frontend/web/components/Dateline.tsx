6b5533a4717da0e6102f7790523d014091031cf2
Pascal Honoré <pascal@alseyn.net>
2018-11-08 12:48:42 +0000

    Refactoring

--- frontend/web/components/Dateline.tsx
+++ frontend/web/components/Dateline.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import { css } from 'react-emotion';
+import { palette } from '@guardian/pasteup/palette';
+import { sans } from '@guardian/pasteup/fonts';
+
+const captionFont = css`
+    font-size: 12px;
+    line-height: 16px;
+    font-family: ${sans.body};
+    color: ${palette.neutral[46]};
+`;
+
+const dateline = css`
+    ${captionFont};
+
+    padding-top: 2px;
+    margin-bottom: 6px;
+`;
+
 const Dateline: React.SFC<{
     dateDisplay: string;
-}> = ({ dateDisplay }) => <>{dateDisplay}</>;
+}> = ({ dateDisplay }) => (
+    <>
+        <div className={dateline}>{dateDisplay}</div>
+    </>
+);
 
 export default Dateline;
