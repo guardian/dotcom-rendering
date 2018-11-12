3945508ff2ec9956175bd0d294f4ab85730c0cd0
GHaberis <george.haberis@theguardian.com>
2018-11-12 10:41:33 +0000

    move window.guardian declaration into the head

--- frontend/web/htmlTemplate.ts
+++ frontend/web/htmlTemplate.ts
@@ -47,6 +47,15 @@ export default ({
                     .join('\n')}
                 <style>${fontsCSS}${resetCSS}${css}</style>
                 <script>
+                window.guardian = ${sanitiseDomRefs(
+                    JSON.stringify({
+                        app: {
+                            data,
+                            cssIDs,
+                        },
+                        config: {},
+                    }),
+                )};
                 // this is a global that's called at the bottom of the pf.io response,
                 // once the polyfills have run. This may be useful for debugging.
                 // mainly to support browsers that don't support async=false or defer
@@ -80,15 +89,6 @@ export default ({
             <body>
                 <div id="app">${html}</div>
                 <script>
-                window.guardian = ${sanitiseDomRefs(
-                    JSON.stringify({
-                        app: {
-                            data,
-                            cssIDs,
-                        },
-                        config: {},
-                    }),
-                )};
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
