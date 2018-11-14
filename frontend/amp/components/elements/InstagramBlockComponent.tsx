793d7a127857efeb9de5d9e38b13ac61472a856e
Alex Ware <alex.ware@guardian.co.uk>
2018-11-14 17:12:37 +0000

    lazy test

--- frontend/amp/components/elements/InstagramBlockComponent.tsx
+++ frontend/amp/components/elements/InstagramBlockComponent.tsx
@@ -17,8 +17,21 @@ export const InstagramBlockComponent: React.SFC<{
             width="1" // Magic numbers, we don't track aspect ratio and instagrams brand is somewhat square based
             height="1" // And if you remove them it'll fill the whole screen
             data-shortcode={shortcode}
-            data-captioned={element.hasCaption}
+            data-captioned={false}
             layout="responsive"
         />
     );
 };
+/*
+About width and height.
+
+The documentation says that these numbers account only for the image.
+
+This doesn't seem correct, from observation, it appears these numbers represent
+the total initial space made available for the element.
+
+This space is used intially for the image, and then for the load in of the ig chrome.
+
+The ig chrome is about 200px tall
+
+*/
