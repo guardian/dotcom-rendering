04f86421d4300c0d07fa9899d00a6ae2cd55cf38
Nicolas Long <nicolaslong@gmail.com>
2018-11-19 17:14:14 +0000

    Update footer with new styling
    * new link structure
    * 'back to top'
    * various style changes

--- frontend/amp/components/Footer.tsx
+++ frontend/amp/components/Footer.tsx
@@ -3,16 +3,18 @@ import { css } from 'react-emotion';
 import { sans } from '@guardian/pasteup/fonts';
 import { palette } from '@guardian/pasteup/palette';
 import InnerContainer from './InnerContainer';
-import { footerLinks, Link } from '@frontend/lib/footer-links';
+import { Link, footerLinksNew } from '@frontend/lib/footer-links';
 
 const footer = css`
     background-color: ${palette.brand.blue};
     color: ${palette.neutral[86]};
     font-family: ${sans.body};
     font-size: 16px;
+    margin-top: 20px;
 `;
 
 const footerInner = css`
+    position: relative;
     padding-bottom: 6px;
 `;
 
@@ -31,39 +33,84 @@ const footerList = css`
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
+    position: relative;
+    padding-top: 12px;
+
+    :before {
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: calc(50% - 10px);
+        width: 1px;
+        display: block;
+        background-color: rgba(255, 255, 255, 0.3);
+    }
 
     ul {
-        width: 50%;
-        border-left: 1px solid ${palette.neutral[46]};
-
-        :nth-child(odd) {
-            border-left: 0px;
-            padding-left: 0px;
-        }
-
-        :nth-child(3) {
-            padding-top: 8px;
-        }
-
-        :nth-child(4) {
-            padding-top: 8px;
-        }
-
-        :nth-child(1) {
-            border-left: 0px;
-            padding-left: 0px;
-        }
-
-        padding: 0 0 0 10px;
-        margin-top: 12px;
+        margin-right: 10px;
+        width: calc(50% - 10px);
+        margin-top: 0;
     }
+`;
+
+const copyrightContainer = css`
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 20px;
+    padding-bottom: 18px;
+    border-top: 1px solid rgba(255, 255, 255, 0.3);
+    margin-top: 12px;
+    position: relative;
 `;
 
 const copyright = css`
     font-size: 12px;
-    padding: 6px 0 18px;
-    border-top: 1px solid ${palette.neutral[46]};
-    margin-top: 12px;
+`;
+
+const iconContainer = css`
+    position: relative;
+    float: right;
+    margin-top: -6px;
+    border-radius: 100%;
+    background-color: ${palette.neutral[100]};
+    cursor: pointer;
+    height: 42px;
+    min-width: 42px;
+`;
+
+const icon = css`
+    :before {
+        position: absolute;
+        top: 6px;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        border: 2px solid ${palette.brand.blue};
+        border-bottom: 0;
+        border-right: 0;
+        content: '';
+        height: 12px;
+        width: 12px;
+        transform: rotate(45deg);
+    }
+`;
+
+const backToTopLink = css`
+    position: absolute;
+    background-color: ${palette.brand.blue};
+    color: ${palette.neutral[100]};
+    font-weight: 700;
+    top: -14px;
+    right: 20px;
+    padding: 0 5px;
+`;
+
+const backToTopText = css`
+    display: inline-block;
+    padding-right: 0.3125rem;
+    padding-top: 3px;
 `;
 
 const FooterLinks: React.SFC<{
@@ -89,11 +136,19 @@ const Footer: React.SFC = () => (
     <footer className={footer}>
         <InnerContainer>
             <div className={footerInner}>
-                <FooterLinks links={footerLinks} />
-                <div className={copyright}>
-                    © 2018 Guardian News and Media Limited or its affiliated
-                    companies. All rights reserved.
-                </div>
+                <FooterLinks links={footerLinksNew} />
+            </div>
+        </InnerContainer>
+        <InnerContainer className={copyrightContainer}>
+            <a className={backToTopLink} href="#top">
+                <span className={backToTopText}>Back to top</span>
+                <span className={iconContainer}>
+                    <i className={icon} />
+                </span>
+            </a>
+            <div className={copyright}>
+                © 2018 Guardian News and Media Limited or its affiliated
+                companies. All rights reserved.
             </div>
         </InnerContainer>
     </footer>
--- frontend/lib/footer-links.ts
+++ frontend/lib/footer-links.ts
@@ -101,3 +101,95 @@ export const footerLinks: Link[][] = [
         },
     ],
 ];
+
+export const footerLinksNew: Link[][] = [
+    [
+        {
+            title: 'About us',
+            url: '/about',
+        },
+        {
+            title: 'Contact us',
+            url: '/help/contact-us',
+        },
+        {
+            title: 'Complaints and corrections',
+            url: '/info/complaints-and-corrections',
+        },
+        {
+            title: 'Securedrop',
+            url: 'https://securedrop.theguardian.com/',
+        },
+        {
+            title: 'Work for us',
+            url: 'https://workforus.theguardian.com/locations/london',
+        },
+        {
+            title: 'Privacy policy',
+            url: '/info/privacy',
+        },
+        {
+            title: 'Cookie policy',
+            url: '/info/cookies',
+        },
+        {
+            title: 'Terms & conditions',
+            url: '/help/terms-of-service',
+        },
+        {
+            title: 'Help',
+            url: '/help',
+        },
+    ],
+    [
+        {
+            title: 'All topics',
+            url: '/index/subjects/a',
+        },
+        {
+            title: 'All writers',
+            url: '/index/contributors',
+        },
+        {
+            title: 'Modern Slavery Act',
+            url:
+                '/info/2016/jul/27/modern-slavery-and-our-supply-chains?INTCMP=NGW_FOOTER_UK_GU_MODERN_SLAVERY_ACT',
+        },
+        {
+            title: 'Digital newspaper archive',
+            url: 'https://theguardian.newspapers.com/',
+        },
+        {
+            title: 'Facebook',
+            url: 'https://www.facebook.com/theguardian',
+        },
+        {
+            title: 'Twitter',
+            url: 'https://twitter.com/guardian',
+        },
+    ],
+    [
+        {
+            title: 'Advertise with us',
+            url: 'https://advertising.theguardian.com/',
+        },
+        {
+            title: 'Search jobs',
+            url: 'https://jobs.theguardian.com/?INTCMP=NGW_FOOTER_UK_GU_JOBS',
+        },
+        {
+            title: 'Dating',
+            url:
+                'https://soulmates.theguardian.com/?INTCMP=NGW_FOOTER_UK_GU_SOULMATES',
+        },
+        {
+            title: 'Patrons',
+            url: 'https://patrons.theguardian.com/?INTCMP=footer_patrons',
+        },
+        {
+            title: 'Discount Codes',
+            url:
+                'https://discountcode.theguardian.com/uk?INTCMP=guardian_footer',
+        },
+    ],
+];
