a9d0c9b5b34cb021fb2f4c73a3c5180886e5597e
Nicolas Long <nicolaslong@gmail.com>
2018-11-28 11:12:51 +0000

    Support mustache in gap-list

--- packages/gap/gap-list/gap-list.ts
+++ packages/gap/gap-list/gap-list.ts
@@ -1,3 +1,5 @@
+import Mustache from 'mustache';
+
 /**
  * Loads JSON content from a CORS endpoint and renders it through a provided
  * template.
@@ -6,13 +8,16 @@
 const GapList: Extension = {
     do: async (el: Element): Promise<void> => {
         const src = el.attributes.getNamedItem('data-src');
-
         if (src === null) return;
+
+        const tpl = el.querySelector('template');
+        if (tpl === null) return;
+
         const res = await fetch(src.value);
         const json = await res.json();
+        const html = Mustache.render(tpl.innerHTML, json);
 
-        el.innerHTML = `<div>${JSON.stringify(json)}</div>`;
-        return;
+        el.innerHTML = html;
     },
 };
 
