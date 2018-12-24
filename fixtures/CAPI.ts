c009c36263fc0d2f60cae4a9ded8af4f7b5da2c2
Simon Adcock <simon.adcock@guardian.co.uk>
2018-12-24 15:24:03 +0000

    improve types

--- fixtures/CAPI.ts
+++ fixtures/CAPI.ts
@@ -89,6 +89,230 @@ export const CAPI: CAPIType = {
         '<figure data-media-id="c0c1beb4b6c6889b095fbb4aef36009222420d65" class="element element-image"><img class="gu-image" height="600" width="1000" alt="The Palace Theatre, London, showing Harry Potter and the Cursed Child" src="https://media.guim.co.uk/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/1000.jpg"><figcaption><span class="element-image__caption">Tickets for the West End show Harry Potter and the Cursed Child at the Palace Theatre, London, are among those targeted by the secondary ticketing industry. </span><span class="element-image__credit">Photograph: Kirsty Wigglesworth/AP</span></figcaption></figure>',
     body:
         '<p>Touts who use computer software to <a href="https://www.theguardian.com/business/2017/jan/23/ticket-touts-beat-adele-take-that-hamilton">harvest concert tickets in bulk</a> and resell them at vast mark-ups face unlimited fines as part of a crackdown on highly profitable resale sites such as Viagogo, StubHub and GetMeIn.</p> <p>The Department of Culture, Media and Sport will announce a package of measures to curb the growing power of the so-called “secondary ticketing” industry, which now regularly offers tickets at huge mark-ups, even before they are available to the general public.</p> <p>Proposals will include a new criminal offence for the use of “bots” – <a href="https://www.theguardian.com/money/2016/may/21/ticket-touts-powerful-software-assist-widely-available">software that helps touts bypass limits</a> on the number of tickets one person can buy.</p> <p>National Trading Standards will also be handed a ringfenced pot of money to fund efforts to stop fans being ripped off or shut out of the most in-demand events.</p> <p>The government is taking action following outrage that face-value tickets to see <a href="https://www.theguardian.com/music/2016/dec/02/adele-tickets-appear-online-for-9000-despite-singers-efforts-to-stop-touts">artists such as Adele</a> and Ed Sheeran are selling out in minutes, only to appear for thousands of pounds on resale websites such as <a href="https://www.theguardian.com/business/2016/sep/23/what-we-know-about-ticket-reseller-viagogo">Viagogo</a>, StubHub and GetMeIn moments later.</p> <p>These websites make money by allowing touts, as well as genuine fans, to resell tickets in return for a cut of anything up to 25% of the sale price.</p> <p>As well as criminalising bots, ministers at DCMS will accept in full the recommendations of a <a href="https://www.theguardian.com/money/2016/may/26/ticket-touts-review-licensing-enforcement">review by Professor Michael Waterson</a>, who <a href="https://www.gov.uk/government/publications/consumer-protection-measures-applying-to-ticket-resale-waterson-review">published proposals</a> to tackle rogue ticket traders last year.</p> <p>These include demanding that ticket firms to step up their own efforts to prevent the use of bots and to report any attacks on their systems by touts trying to harvest tickets.</p> <p>While primary ticket firms such as Ticketmaster say they are doing their utmost to stop bot users, the company also owns secondary sites, such as GetMeIn and Seatwave, which have <a href="https://www.theguardian.com/money/2016/jul/16/bands-fans-declare-war-online-ticket-touts">close relationships with major touts</a> and take a cut of their profits.</p> <p>Ministers will also propose stronger enforcement of consumer rights laws, amid concern that tickets are being sold with no information about the seat location or the name of the seller, in contravention of the Consumer Rights Act 2015.</p> <p>Efforts to step up scrutiny of firms’ adherence to consumer laws is also aimed at sites that sell tickets whose terms and conditions specifically ban resale, meaning fans are turned away at the door.</p> <p>Labour MP Sharon Hodgson, a long-time campaigner for ticket reform, welcomed measures she said would address a “broken and parasitic market”. </p> <p>“These measures will ensure that fans are protected, but there still remains work to do to make sure that these measures are enforced properly so touts do not circumvent them as this is going to very soon be the law of the land.”</p> <p>The DCMS proposals will be informed by an <a href="https://www.theguardian.com/business/2016/dec/19/ticket-resale-websites-run-risk-of-fines-cma-launches-investigation-competition-and-markets-authority">ongoing review by the Competition and Market Authority</a> into secondary ticketing firms’ compliance with the law.</p> <p>Ticket resale sites will face even harsher measures if they do not prove that they are taking sufficient steps to address the power of touts, the Guardian understands.</p> <p>Theresa May <a href="https://www.theguardian.com/money/2017/mar/01/viagogo-ticket-rip-off-parliament-theresa-may-promises-crackdown">promised to take action on ticket resale</a> at Prime Minister’s Questions after Nigel Adams MP urged her to help “ensure genuine fans are not fleeced by <a href="https://www.theguardian.com/money/2016/may/15/shady-world-of-the-ticket-touts">ticket touts and rogues</a>”.</p> <p>Increased scrutiny of secondary ticketing follows revelations in the Guardian about the actions of well-known ticket resale sites and the <a href="https://www.theguardian.com/money/2016/may/15/shady-world-of-the-ticket-touts">touts who have used them to build multi-million pound businesses</a> by harvesting tickets in bulk.</p> <p>Viagogo, which is based in Switzerland but has a large office in Cannon Street, London, was recently <a href="https://www.theguardian.com/money/2017/feb/17/viagogo-condemned-ed-sheeran-cancer-benefit-concert-tickets-teenage-cancer-trust">accused of “moral repugnance”</a> for seeking to profit from the resale of tickets for an Ed Sheeran gig at the Royal Albert Hall in aid of the Teenage Cancer Trust.</p> <p>Ticketmaster, which owns resale sites GetMeIn and Seatwave, has also come under the spotlight after it emerged that a <a href="https://www.theguardian.com/business/2017/feb/22/convicted-fraudsters-sell-ed-sheeran-tickets-through-ticketmaster">man previously convicted of a £2m ticket fraud</a> was using the sites to sell thousands of pounds’ worth of tickets.<br></p> <p>The identities of some of the UK’s most powerful touts were revealed last year after a whistleblower passed the Guardian information revealing the vast rewards on offer to those with a grip on access to the UK’s most popular events.</p> <p>Many have since <a href="https://www.theguardian.com/business/2017/jan/23/ticket-touts-investigations-competition-hmrc">rebranded their companies</a> amid mounting outrage about their business models.</p> <p>While music concerts have proved the most lucrative targets for touts, high-profile theatre productions such as <a href="https://www.theguardian.com/stage/2017/jan/16/hamilton-west-end-tickets-found-on-resale-sites-viagogo-despite-anti-tout-measures">critically acclaimed hip-hop musical Hamilton</a>, as well as <a href="https://www.theguardian.com/stage/2016/aug/13/harry-potter-cursed-child-ticket-resale-prices">Harry Potter and the Cursed Child</a>, have also been targeted.</p>',
+    mainMediaElements: [
+        {
+            media: {
+                allImages: [
+                    {
+                        index: 0,
+                        fields: {
+                            height: '1200',
+                            width: '2000',
+                        },
+                        mediaType: 'Image',
+                        mimeType: 'image/jpeg',
+                        url:
+                            'https://media.guim.co.uk/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/2000.jpg',
+                    },
+                    {
+                        index: 1,
+                        fields: {
+                            height: '600',
+                            width: '1000',
+                        },
+                        mediaType: 'Image',
+                        mimeType: 'image/jpeg',
+                        url:
+                            'https://media.guim.co.uk/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/1000.jpg',
+                    },
+                    {
+                        index: 2,
+                        fields: {
+                            height: '300',
+                            width: '500',
+                        },
+                        mediaType: 'Image',
+                        mimeType: 'image/jpeg',
+                        url:
+                            'https://media.guim.co.uk/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/500.jpg',
+                    },
+                    {
+                        index: 3,
+                        fields: {
+                            height: '84',
+                            width: '140',
+                        },
+                        mediaType: 'Image',
+                        mimeType: 'image/jpeg',
+                        url:
+                            'https://media.guim.co.uk/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/140.jpg',
+                    },
+                    {
+                        index: 4,
+                        fields: {
+                            height: '3009',
+                            width: '5015',
+                        },
+                        mediaType: 'Image',
+                        mimeType: 'image/jpeg',
+                        url:
+                            'https://media.guim.co.uk/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/5015.jpg',
+                    },
+                    {
+                        index: 5,
+                        fields: {
+                            isMaster: 'true',
+                            height: '3009',
+                            width: '5015',
+                        },
+                        mediaType: 'Image',
+                        mimeType: 'image/jpeg',
+                        url:
+                            'http://media.guim.co.uk/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg',
+                    },
+                ],
+            },
+            data: {
+                copyright:
+                    'Copyright 2016 The Associated Press. All rights reserved. This material may not be published, broadcast, rewritten or redistribu',
+                alt:
+                    'The Palace Theatre, London, showing Harry Potter and the Cursed Child',
+                caption:
+                    'Tickets for the West End show Harry Potter and the Cursed Child at the Palace Theatre, London, are among those targeted by the secondary ticketing industry. ',
+                credit: 'Photograph: Kirsty Wigglesworth/AP',
+            },
+            displayCredit: true,
+            role: 'inline',
+            imageSources: [
+                {
+                    weighting: 'inline',
+                    srcSet: [
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=620&quality=85&auto=format&fit=max&s=1bcb12104405d365e4176d25f9bff704',
+                            width: 620,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=605&quality=85&auto=format&fit=max&s=2c0e6978b4b165ca97f03f8a816529a8',
+                            width: 605,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=445&quality=85&auto=format&fit=max&s=516527f4ac7bdcab733dbcb0f13c25bd',
+                            width: 445,
+                        },
+                    ],
+                },
+                {
+                    weighting: 'thumbnail',
+                    srcSet: [
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=140&quality=85&auto=format&fit=max&s=47fa9641d01f7b562c1407c999bd9da0',
+                            width: 140,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=120&quality=85&auto=format&fit=max&s=0aec1c40ad08585352196ba9cbbe712e',
+                            width: 120,
+                        },
+                    ],
+                },
+                {
+                    weighting: 'supporting',
+                    srcSet: [
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=380&quality=85&auto=format&fit=max&s=6bf99684d34d7e640b42e77de94d2369',
+                            width: 380,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=300&quality=85&auto=format&fit=max&s=4e8c8811000fc54bea72dedd69ab13d6',
+                            width: 300,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=620&quality=85&auto=format&fit=max&s=1bcb12104405d365e4176d25f9bff704',
+                            width: 620,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=605&quality=85&auto=format&fit=max&s=2c0e6978b4b165ca97f03f8a816529a8',
+                            width: 605,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=445&quality=85&auto=format&fit=max&s=516527f4ac7bdcab733dbcb0f13c25bd',
+                            width: 445,
+                        },
+                    ],
+                },
+                {
+                    weighting: 'showcase',
+                    srcSet: [
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=860&quality=85&auto=format&fit=max&s=16f69557a812fa0f97f386567be954d6',
+                            width: 860,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=780&quality=85&auto=format&fit=max&s=ce247f6cf701cb7c91cc6243a0b52bbe',
+                            width: 780,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=620&quality=85&auto=format&fit=max&s=1bcb12104405d365e4176d25f9bff704',
+                            width: 620,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=605&quality=85&auto=format&fit=max&s=2c0e6978b4b165ca97f03f8a816529a8',
+                            width: 605,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=445&quality=85&auto=format&fit=max&s=516527f4ac7bdcab733dbcb0f13c25bd',
+                            width: 445,
+                        },
+                    ],
+                },
+                {
+                    weighting: 'halfwidth',
+                    srcSet: [
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=620&quality=85&auto=format&fit=max&s=1bcb12104405d365e4176d25f9bff704',
+                            width: 620,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=605&quality=85&auto=format&fit=max&s=2c0e6978b4b165ca97f03f8a816529a8',
+                            width: 605,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=445&quality=85&auto=format&fit=max&s=516527f4ac7bdcab733dbcb0f13c25bd',
+                            width: 445,
+                        },
+                    ],
+                },
+                {
+                    weighting: 'immersive',
+                    srcSet: [
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=620&quality=85&auto=format&fit=max&s=1bcb12104405d365e4176d25f9bff704',
+                            width: 620,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=605&quality=85&auto=format&fit=max&s=2c0e6978b4b165ca97f03f8a816529a8',
+                            width: 605,
+                        },
+                        {
+                            src:
+                                'https://i.guim.co.uk/img/media/c0c1beb4b6c6889b095fbb4aef36009222420d65/0_184_5015_3009/master/5015.jpg?width=445&quality=85&auto=format&fit=max&s=516527f4ac7bdcab733dbcb0f13c25bd',
+                            width: 445,
+                        },
+                    ],
+                },
+            ],
+            _type: 'model.dotcomrendering.pageElements.ImageBlockElement',
+        },
+    ],
     editionLongForm: 'uk edition',
     author: {
         byline: 'Rob Davies',
