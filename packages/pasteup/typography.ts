477e2afbb5e42e1d67d1c4e50eb3bf9b8080c5e5
Pascal Honoré <pascal@alseyn.net>
2018-12-28 18:32:05 +0000

    Introduce typography.ts

---
+++ packages/pasteup/typography.ts
@@ -0,0 +1,48 @@
+export const serif = {
+    headline: ['GH Guardian Headline', 'Georgia', 'serif'].join(','),
+    body: ['GuardianTextEgyptian', 'Georgia', 'serif'].join(','),
+};
+
+export const sans = {
+    body: [
+        'GuardianTextSans',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'Lucida Grande',
+        'sans-serif',
+    ].join(','),
+};
+
+const fontScaleMapping: any = {
+    headline: {
+        1: { fontSize: 14, lineHeight: 18 },
+        2: { fontSize: 16, lineHeight: 20 },
+        3: { fontSize: 20, lineHeight: 24 },
+        4: { fontSize: 24, lineHeight: 28 },
+        5: { fontSize: 28, lineHeight: 32 },
+        6: { fontSize: 32, lineHeight: 36 },
+        7: { fontSize: 36, lineHeight: 40 },
+        8: { fontSize: 40, lineHeight: 44 },
+        9: { fontSize: 44, lineHeight: 48 },
+    },
+    body: {
+        1: { fontSize: 14, lineHeight: 20 },
+        2: { fontSize: 16, lineHeight: 24 },
+        3: { fontSize: 18, lineHeight: 28 },
+    },
+    textSans: {
+        1: { fontSize: 12, lineHeight: 16 },
+        2: { fontSize: 13, lineHeight: 18 },
+        3: { fontSize: 14, lineHeight: 20 },
+        4: { fontSize: 14, lineHeight: 22 },
+        5: { fontSize: 16, lineHeight: 22 },
+        6: { fontSize: 18, lineHeight: 18 },
+    },
+};
+
+// (font-size: 14, line-height: 18)
+export const fontScale = (typex: string, level: number): string =>
+    `
+font-size: ${fontScaleMapping[typex][level].fontSize}, 
+line-height: ${fontScaleMapping[typex][level].lineHeight}`;
