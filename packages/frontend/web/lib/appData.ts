20630458bcf64fdfa55f41c5abdbcd7317b44cf3
GHaberis <george.haberis@theguardian.com>
2019-01-03 10:47:31 +0000

    add tests for appData

--- packages/frontend/web/lib/appData.ts
+++ packages/frontend/web/lib/appData.ts
@@ -2,7 +2,7 @@
 // because this is the one that gets imported to all other modules
 export const appData = window.guardian.app.data;
 
-// allows you to safely get items from config using a query of
+// get allows you to safely get items from window.guardian.app.data using a query of
 // dot or bracket notation, with optional default fallback
 export const get = (path: string = '', defaultValue?: any): any => {
     const value = path
@@ -15,4 +15,23 @@ export const get = (path: string = '', defaultValue?: any): any => {
     }
 
     return defaultValue;
+};
+
+// these should be used for testing purposes only
+export const testHelpers = {
+    // set allows you to safely set items in window.guardian.app.data
+    set(path: string, value: any): void {
+        const pathSegments: string[] = path.split('.');
+        const last: string | undefined = pathSegments.pop();
+        if (!last) {
+            return;
+        }
+        pathSegments.reduce((obj, subpath) => {
+            if (typeof obj[subpath] === 'object') {
+                return obj[subpath];
+            }
+            obj[subpath] = {};
+            return obj[subpath];
+        }, appData)[last] = value;
+    },
 };
--- scripts/jest/setup.ts
+++ scripts/jest/setup.ts
@@ -2,3 +2,15 @@
 import 'jest-dom/extend-expect';
 // this is basically: afterEach(cleanup)
 import 'react-testing-library/cleanup-after-each';
+
+// Stub global Guardian object
+window.guardian = {
+    app: {
+        data: {},
+        cssIDs: [],
+    },
+    polyfilled: true,
+    onPolyfilled: () => {
+        return undefined;
+    },
+};
