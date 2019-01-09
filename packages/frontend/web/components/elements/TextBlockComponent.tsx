a430aa645a16ff8dff859ab65ac9b986ef6e05ad
Simon Adcock <simon.adcock@guardian.co.uk>
2019-01-09 15:21:03 +0000

    use correct body font

--- packages/frontend/web/components/elements/TextBlockComponent.tsx
+++ packages/frontend/web/components/elements/TextBlockComponent.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { css } from 'emotion';
+import { body } from '@guardian/pasteup/typography';
 // tslint:disable:react-no-dangerous-html
+
+const para = css`
+    p {
+        margin-bottom: 16px;
+        ${body(2)};
+    }
+`;
 
 export const TextBlockComponent: React.SFC<{ html: string }> = ({ html }) => (
     <span
+        className={para}
         dangerouslySetInnerHTML={{
             __html: html,
         }}
