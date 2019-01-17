8d15528b00a30c2e3f2508b05c8249f1d5f0f126
Nicolas Long <nicolaslong@gmail.com>
2019-01-17 17:13:36 +0000

    Reinstate validation test (currently failing)

--- packages/frontend/amp/server/document.test.tsx
+++ packages/frontend/amp/server/document.test.tsx
@@ -1,7 +1,11 @@
 import { document } from './document';
 import validator from 'amphtml-validator';
 import React from 'react';
-// import { Article } from './pages/Article';
+import { data } from '@root/fixtures/article';
+import { Article } from '@frontend/amp/pages/Article';
+import { extract as extractNAV } from '@frontend/model/extract-nav';
+import { extract as extractConfig } from '@frontend/model/extract-config';
+import { extract as extractModel } from '@frontend/model/extract-capi';
 
 test('rejects invalid AMP doc (to test validator)', async () => {
     const v = await validator.getInstance();
@@ -11,8 +15,20 @@ test('rejects invalid AMP doc (to test validator)', async () => {
     expect(result.errors.length > 0).toBe(true);
 });
 
-// test('produces valid AMP doc', async () => {
-//     const v = await validator.getInstance();
-//     const result = v.validateString(document({ body: <Article  /> }));
-//     expect(result.errors.length).toBe(0);
-// });
+test('produces valid AMP doc', async () => {
+    const v = await validator.getInstance();
+    const config = extractConfig(data);
+    const nav = extractNAV(data);
+    const model = extractModel(data);
+
+    const body = <Article nav={nav} articleData={model} config={config} />;
+    const result = v.validateString(
+        document({ body, title: 'foo', scripts: [] }),
+    );
+
+    if (result.errors.length > 0) {
+        console.log(result.errors);
+    }
+
+    expect(result.errors.length).toBe(0);
+});
