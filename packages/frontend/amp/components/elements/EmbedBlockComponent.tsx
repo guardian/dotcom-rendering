25ab57c7ebb5d42ed6a6c68810fba5c13d755773
Alex Ware <alex.ware@guardian.co.uk>
2019-01-23 17:18:19 +0000

    actually add component

---
+++ packages/frontend/amp/components/elements/EmbedBlockComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const EmbedBlockComponent: React.SFC<{
+    element: EmbedBlockElement;
+}> = ({ element }) => {
+    if (element.isMandatory) {
+        throw new Error(
+            'This page cannot be rendered due to incompatible content that is marked as mandatory.',
+        );
+    }
+    return null;
+};
