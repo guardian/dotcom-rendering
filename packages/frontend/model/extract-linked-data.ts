681fa2326f76e52ddf5f097829576781d15cdef0
GHaberis <george.haberis@theguardian.com>
2019-01-24 15:17:44 +0000

    reinstate temporary fallback fix

--- packages/frontend/model/extract-linked-data.ts
+++ packages/frontend/model/extract-linked-data.ts
@@ -1,5 +1,5 @@
 import { getObject } from '@frontend/model/validators';
 
 export const extract = (data: {}): object => {
-    return getObject(data, 'config.page.linkedData');
+    return getObject(data, 'config.page.linkedData', {});
 };
