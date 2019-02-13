f0971fb753168bf87fe1cc0c707012d49945b01f
Nicolas Long <nicolaslong@gmail.com>
2019-02-13 14:44:03 +0000

    Add affiliate link notice

---
+++ packages/frontend/amp/components/elements/DisclaimerBlockComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { css } from 'emotion';
+import { pillarPalette } from '@frontend/lib/pillars';
+import { textSans } from '@guardian/pasteup/typography';
+
+const style = (pillar: Pillar) => css`
+    ${textSans(2)};
+    line-height: 24px;
+
+    a {
+        color: ${pillarPalette[pillar].dark};
+    }
+`;
+
+export const DisclaimerBlockComponent: React.FC<{
+    html: string;
+    pillar: Pillar;
+}> = ({ html, pillar }) => (
+    // tslint:disable-next-line:react-no-dangerous-html
+    <span
+        className={style(pillar)}
+        dangerouslySetInnerHTML={{
+            __html: html,
+        }}
+    />
+);
