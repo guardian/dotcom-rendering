315a8e07241487f3bdc62e606cf2b8283b220d7a
Laura <lawr@lauragonzalez.cc>
2019-02-19 15:22:27 +0000

    allow for color composition

--- packages/pasteup/palette.ts
+++ packages/pasteup/palette.ts
@@ -25,74 +25,103 @@ export interface OtherColours {
         97: colour;
         100: colour;
     };
+    state: { success: colour; error: colour };
     specialReport: { dark: colour };
     labs: { dark: colour; main: colour };
     green: { dark: colour; main: colour };
     brand: { dark: colour; main: colour; pastel: colour };
 }
 
+const news: PillarColours = {
+    dark: '#ab0613',
+    main: '#c70000',
+    bright: '#ff4e36',
+    pastel: '#ffbac8',
+    faded: '#fff4f2',
+};
+
+const opinion: PillarColours = {
+    dark: '#bd5318',
+    main: '#e05e00',
+    bright: '#ff7f0f',
+    pastel: '#f9b376',
+    faded: '#fef9f5',
+};
+
+const sport: PillarColours = {
+    dark: '#005689',
+    main: '#0084c6',
+    bright: '#00b2ff',
+    pastel: '#90dcff',
+    faded: '#f1f8fc',
+};
+
+const culture: PillarColours = {
+    dark: '#6b5840',
+    main: '#a1845c',
+    bright: '#eacca0',
+    pastel: '#e7d4b9',
+    faded: '#fbf6ef',
+};
+
+const lifestyle: PillarColours = {
+    dark: '#7d0068',
+    main: '#bb3b80',
+    bright: '#ffabdb',
+    pastel: '#fec8d3',
+    faded: '#feeef7',
+};
+
+const green = {
+    dark: '#185E36',
+    main: '#22874D',
+};
+
+const brand = {
+    dark: '#041f4a',
+    main: '#052962',
+    pastel: '#506991',
+};
+
+const state = {
+    success: green.main,
+    error: news.main,
+};
+
+const highlight = {
+    main: '#ffe500',
+    dark: '#ffbb50',
+};
+
+const neutral = {
+    7: '#121212',
+    20: '#333333',
+    46: '#767676',
+    60: '#999999',
+    86: '#dcdcdc',
+    93: '#ededed',
+    97: '#f6f6f6',
+    100: '#ffffff',
+};
+
+const specialReport = { dark: '#3f464a' };
+
+const labs = {
+    dark: '#65a897',
+    main: '#69d1ca',
+};
+
 export const palette: AllPillarColours & OtherColours = {
-    news: {
-        dark: '#ab0613',
-        main: '#c70000',
-        bright: '#ff4e36',
-        pastel: '#ffbac8',
-        faded: '#fff4f2',
-    },
-    opinion: {
-        dark: '#bd5318',
-        main: '#e05e00',
-        bright: '#ff7f0f',
-        pastel: '#f9b376',
-        faded: '#fef9f5',
-    },
-    sport: {
-        dark: '#005689',
-        main: '#0084c6',
-        bright: '#00b2ff',
-        pastel: '#90dcff',
-        faded: '#f1f8fc',
-    },
-    culture: {
-        dark: '#6b5840',
-        main: '#a1845c',
-        bright: '#eacca0',
-        pastel: '#e7d4b9',
-        faded: '#fbf6ef',
-    },
-    lifestyle: {
-        dark: '#7d0068',
-        main: '#bb3b80',
-        bright: '#ffabdb',
-        pastel: '#fec8d3',
-        faded: '#feeef7',
-    },
-    highlight: {
-        main: '#ffe500',
-        dark: '#ffbb50',
-    },
-    neutral: {
-        7: '#121212',
-        20: '#333333',
-        46: '#767676',
-        60: '#999999',
-        86: '#dcdcdc',
-        93: '#ededed',
-        97: '#f6f6f6',
-        100: '#ffffff',
-    },
-    specialReport: { dark: '#3f464a' },
-    labs: {
-        dark: '#65a897',
-        main: '#69d1ca',
-    },
-    green: {
-        dark: '#185E36',
-        main: '#22874D',
-    },
-    brand: {
-        dark: '#041f4a',
-        main: '#052962',
-        pastel: '#506991',
-    },
+    news,
+    opinion,
+    sport,
+    culture,
+    lifestyle,
+    highlight,
+    neutral,
+    specialReport,
+    labs,
+    green,
+    brand,
+    state,
 };
