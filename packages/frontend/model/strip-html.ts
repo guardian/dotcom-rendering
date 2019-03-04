996cf3339662563fda6796fc278357391c0d752e
Nicolas Long <nicolaslong@gmail.com>
2019-03-04 11:09:17 +0000

    Strip HTML from trailtext
    It's unclear from the CAPI model if trailtext can contain markup
    but we currently strip HTML tags in Frontend so it seems prudent
    to do it here as well.
    
    I also expect stripHTML to be a useful function generally.

---
+++ packages/frontend/model/strip-html.ts
@@ -0,0 +1,8 @@
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const { window } = new JSDOM('');
+const DOMPurify = createDOMPurify(window);
+
+export const stripHTML = (s: string): string =>
+    DOMPurify.sanitize(s, { ALLOWED_TAGS: [] });
