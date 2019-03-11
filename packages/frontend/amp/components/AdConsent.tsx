4842651c27005f3e171d9010823282e9b768d047
Jerome Eteve <jerome.eteve@guardian.co.uk>
2019-03-11 17:36:16 +0000

    Added styling to consent banner

--- packages/frontend/amp/components/AdConsent.tsx
+++ packages/frontend/amp/components/AdConsent.tsx
@@ -1,5 +1,61 @@
 import React from 'react';
 import { AMPJSON } from './AMPJSON';
+import { css, cx } from 'emotion';
+
+const consentUIStyle = css`
+    font-family: 'Guardian Text Sans Web', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+    font-size: 1rem;
+    line-height: 1.25rem;
+    color: #f6f6f6;
+    background-color: rgb(51, 51, 51);
+
+    max-width: 37.5rem;
+    margin: 0 auto;
+    overflow-x: hidden;
+`;
+
+const containerDivStyle = css`
+    margin: 1em;
+`;
+
+const h2Style = css`
+    margin-bottom: 1em;
+    font-size: 1.5em;
+`;
+
+const aStyle = css`
+    color: #f6f6f6;
+    &:hover {
+        text-decoration: 0;
+    }
+`;
+
+const actionsStyle = css`
+    text-align: right;
+`;
+
+const buttonStyle = css`
+    font: inherit;
+    border: 0;
+    background: transparent;
+    color: inherit;
+    display: block;
+    width: 100%;
+    margin-top: 1em;
+`;
+
+const acceptStyle = css`
+    border-radius: 20px;
+    border: .0625rem solid rgba(255, 255, 255, .3);
+    background: #ffe500;
+    color: #121212;
+    padding: 5px 20px;
+    font-weight: bold;
+`;
+
+const rejectStyle = css`
+    text-decoration: underline;
+`;
 
 export const AdConsent: React.FC<{}> = ({}) => {
     return (
@@ -15,7 +71,11 @@ export const AdConsent: React.FC<{}> = ({}) => {
                     }}
                 />
             </amp-geo>
-            <amp-consent layout="nodisplay" id="the-adconsent-element">
+            <amp-consent
+                layout="nodisplay"
+                id="the-adconsent-element"
+                style={{ background: 'none' }}
+            >
                 <AMPJSON
                     o={{
                         consents: {
@@ -35,11 +95,9 @@ export const AdConsent: React.FC<{}> = ({}) => {
                         },
                     }}
                 />
-                <div
-                    id="adconsent-ui"
-                    className="with-uk-frame consent-ui main-body"
-                >
-                    <div className="adconsent-message">
+                <div id="adconsent-ui" className={consentUIStyle}>
+                    <div className={containerDivStyle}>
+                        <h2 className={h2Style}>Your pricacy</h2>
                         <p>
                             We use cookies to improve your experience on our
                             personalised advertising.
@@ -47,14 +105,14 @@ export const AdConsent: React.FC<{}> = ({}) => {
                         <p>
                             To find out more, read our{' '}
                             <a
-                                className="u-underline"
+                                className={aStyle}
                                 href="https://www.theguardian.com/help/privacy-policy"
                             >
                                 privacy policy
                             </a>
-                            and{' '}
+                            {' '}and{' '}
                             <a
-                                className="u-underline"
+                                className={aStyle}
                                 href="https://www.theguardian.com/info/cookies"
                             >
                                 cookie policy
@@ -62,17 +120,17 @@ export const AdConsent: React.FC<{}> = ({}) => {
                             .
                         </p>
                     </div>
-                    <div className="adconsent-actions">
+                    <div className={cx(actionsStyle, containerDivStyle)}>
                         <button
                             on="tap:the-adconsent-element.accept"
-                            className="adconsent-accept"
+                            className={cx(buttonStyle,acceptStyle, 'blabla')}
                             role="button"
                         >
                             I'm OK with that
                         </button>
                         <button
                             on="tap:the-adconsent-element.reject"
-                            className="u-underline adconsent-reject"
+                            className={cx(buttonStyle,rejectStyle)}
                             role="button"
                         >
                             I do not want to see personalised ads
