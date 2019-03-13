e6ca7197b949e93d060b310acba4c07e015d45b3
Simon Adcock <simon.adcock@guardian.co.uk>
2019-03-13 16:21:22 +0000

    Refactor commercialProperties as map of targeting params
    A refactor of commercial properities passed to AMP ads. Previously
    this was an array of params, and we had to filter the array for the
    correct edition.
    
    This change restructures commercial properties as a map of edition
    to targeting params, making it much easier to select the correct
    params for the current edition. It simplifies our code and makes
    it more efficient.
    
    This corresponds to guardian/frontend#21208. We can remove the original
    commercialProperties after this change is merged.
    
    This provides the groundwork for paid content theming, as Branding is
    easier to retrieve from the new editionalised map.

--- packages/frontend/amp/lib/ad-json.test.ts
+++ packages/frontend/amp/lib/ad-json.test.ts
@@ -3,33 +3,33 @@ import { adJson, stringify } from './ad-json';
 const paramSet: AdTargetParam[] = [
     {
         name: 'su',
-        values: ['4', '5', '1', '2', '3'],
+        value: ['4', '5', '1', '2', '3'],
     },
     {
         name: 'url',
-        values: [
+        value: [
             '/business/2019/feb/07/no-deal-brexit-uk-exporters-risk-being-locked-out-of-world-harbours',
         ],
     },
     {
         name: 'tn',
-        values: ['news'],
+        value: ['news'],
     },
     {
         name: 'ct',
-        values: ['article'],
+        value: ['article'],
     },
     {
         name: 'p',
-        values: ['ng'],
+        value: ['ng'],
     },
     {
         name: 'co',
-        values: ['richard-partington'],
+        value: ['richard-partington'],
     },
     {
         name: 'k',
-        values: [
+        value: [
             'asia-pacific',
             'politics',
             'business',
@@ -50,22 +50,17 @@ const paramSet: AdTargetParam[] = [
     },
     {
         name: 'edition',
-        values: ['au'],
+        value: ['au'],
     },
     {
         name: 'sh',
-        values: ['https://gu.com/p/akj3n'],
+        value: ['https://gu.com/p/akj3n'],
     },
 ];
 
 describe('ampadslots', () => {
     it('should set platform to amp', () => {
-        const edition = 'UK';
-        const targetings: EditionAdTargeting = {
-            paramSet,
-            edition: 'UK',
-        };
-        const res = adJson(edition, [targetings]);
+        const res = adJson(paramSet);
         const p = res.targeting.find(param => param.name === 'p');
         if (p === undefined) {
             return fail();
@@ -75,12 +70,7 @@ describe('ampadslots', () => {
     });
 
     it('should set rendering platform to dotcom-rendering', () => {
-        const edition = 'UK';
-        const targetings: EditionAdTargeting = {
-            paramSet,
-            edition,
-        };
-        const res = adJson(edition, [targetings]);
+        const res = adJson(paramSet);
         const renderingPlatform = res.targeting.find(
             param => param.name === 'rp',
         );
@@ -91,12 +81,7 @@ describe('ampadslots', () => {
     });
 
     it('should set values to a comma-separated string', () => {
-        const edition = 'UK';
-        const targetings: EditionAdTargeting = {
-            paramSet,
-            edition: 'UK',
-        };
-        const res = adJson(edition, [targetings]);
+        const res = adJson(paramSet);
         const p = res.targeting.find(param => param.name === 'su');
         if (p === undefined) {
             return fail();
@@ -107,12 +92,7 @@ describe('ampadslots', () => {
 
 describe('stringify', () => {
     it('should generate the correct string value from an AdJson', () => {
-        const edition = 'AU';
-        const targetings: EditionAdTargeting = {
-            paramSet,
-            edition,
-        };
-        const res = adJson(edition, [targetings]);
+        const res = adJson(paramSet);
         const outputJson = {
             targeting: {
                 su: '4,5,1,2,3',
