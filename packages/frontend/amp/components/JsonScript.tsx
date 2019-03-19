96a74a1c3aa206dbe1620c229dcc24ded29b4809
Francesca Hammond <francesca.hammond@guardian.co.uk>
2019-03-19 15:30:19 +0000

    Fix tslint error

--- packages/frontend/amp/components/JsonScript.tsx
+++ packages/frontend/amp/components/JsonScript.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const JsonScript: React.FC<{ o: any }> = ({ o }) => {
     const JSONString: string = JSON.stringify(o);
     return (
+        // tslint:disable-next-line:react-no-dangerous-html
         <script
             type="application/json"
-            // tslint:disable-next-line:react-no-dangerous-html
             dangerouslySetInnerHTML={{ __html: JSONString }}
         />
     );
