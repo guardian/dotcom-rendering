bc8b2ffd56fc72be37a02f72d3b5d4c1dc8ff7e5
Adem Gaygusuz <adem.gaygusuz.freelance@guardian.co.uk>
2019-03-26 12:34:56 +0000

    Render list of structured data as separate script tags instead of list List broke amp-subscriptions-google

--- packages/frontend/amp/server/document.tsx
+++ packages/frontend/amp/server/document.tsx
@@ -16,6 +16,10 @@ interface Metadata {
     canonicalURL: string;
 }
 
+interface LinkedDataEntity {
+    '@type': string;
+}
+
 export const document = ({
     linkedData,
     title,
@@ -23,7 +27,7 @@ export const document = ({
     scripts,
     metadata,
 }: {
-    linkedData: object[];
+    linkedData: LinkedDataEntity[];
     title: string;
     body: React.ReactElement<any>;
     scripts: string[];
@@ -53,10 +57,13 @@ export const document = ({
     <link rel="canonical" href="${metadata.canonicalURL}" />
     <meta name="viewport" content="width=device-width,minimum-scale=1">
     <link rel="icon" href="https://static.guim.co.uk/images/${favicon}">
-
-    <script type="application/ld+json">
-        ${JSON.stringify(linkedData)}
-    </script>
+    
+    ${linkedData.map(
+        ld => `
+<script type="application/ld+json" data-schema="${
+            ld['@type']
+        }">${JSON.stringify(ld)}</script>`,
+    )}
 
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
     <script async src="https://cdn.ampproject.org/v0.js"></script>
