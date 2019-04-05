90b6c044f403ece63e16905035ab0567c143c9e4
Laura buns <lawr@lauragonzalez.cc>
2019-04-05 16:16:26 +0100

    wip

---
+++ packages/design/src/components/details.js
@@ -0,0 +1,21 @@
+import React, { useRef, useEffect, useState } from "react"
+
+const Details = ({ children, initialOpenState }) => {
+  const deets = useRef()
+  const [open, setOpen] = useState(initialOpenState)
+  useEffect(() => {
+    if (deets.current) {
+      deets.current.addEventListener("toggle", ev => {
+        setOpen(ev.currentTarget.open)
+      })
+    }
+  }, [deets])
+
+  return (
+    <details ref={deets} open={open}>
+      {children}
+    </details>
+  )
+}
+
+export { Details }
