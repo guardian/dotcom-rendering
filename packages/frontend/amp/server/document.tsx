62e81894463a2080da7d4f8fce3357e347a953ee
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-05 11:22:13 +0100

    Use reduce instead of map to avoid rouge comment

--- packages/frontend/amp/server/document.tsx
+++ packages/frontend/amp/server/document.tsx
@@ -53,10 +53,12 @@ export const document = ({
     <link rel="canonical" href="${metadata.canonicalURL}" />
     <meta name="viewport" content="width=device-width,minimum-scale=1">
     <link rel="icon" href="https://static.guim.co.uk/images/${favicon}">
-    
-    ${linkedData.map(
-        ld => `
-<script type="application/ld+json">${JSON.stringify(ld)}</script>`
+
+
+    ${linkedData.reduce(
+        (prev, ld) => `${prev}
+<script type="application/ld+json">${JSON.stringify(ld)}</script>`,
+        '',
     )}
 
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
