868ff54d2b30ecd0fa95f8f9326590609d3bdbc8
Laura buns <lawr@lauragonzalez.cc>
2019-04-05 16:16:26 +0100

    wip

--- packages/design/src/components/list.js
+++ packages/design/src/components/list.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import styles from "./list.module.css"
 
-const List = ({ children }) => <ul class={styles.base}>{children}</ul>
+const List = ({ children }) => <ul className={styles.base}>{children}</ul>
 
 const ListItem = ({ children }) => <li>{children}</li>
-const HappyListItem = ({ children }) => <li class={styles.happy}>{children}</li>
-const SadListItem = ({ children }) => <li class={styles.sad}>{children}</li>
+const HappyListItem = ({ children }) => (
+  <li className={styles.happy}>{children}</li>
+)
+const SadListItem = ({ children }) => <li className={styles.sad}>{children}</li>
 
 export { List, ListItem, HappyListItem, SadListItem }
