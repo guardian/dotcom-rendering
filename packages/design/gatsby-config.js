c4c9fe6d67a60c02a6545af33a0ea021388fa154
Laura buns <lawr@lauragonzalez.cc>
2019-04-08 15:54:38 +0100

    fix

--- packages/design/gatsby-config.js
+++ packages/design/gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  siteMetadata: {
-    title: `Title from siteMetadata`,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
+    siteMetadata: {
+        title: `Title from siteMetadata`,
     },
-    `gatsby-mdx`,
-    `gatsby-plugin-typescript`,
-  ],
-}
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        },
+        `gatsby-mdx`,
+        `gatsby-plugin-typescript`,
+    ],
+};
