aef9db59f44647cf10e6adf5665314bae2d5c563
Laura buns <lawr@lauragonzalez.cc>
2019-04-08 11:01:02 +0100

    wip

--- packages/design/src/components/figure.js
+++ packages/design/src/components/figure.js
@@ -1,11 +1,26 @@
-import React from "react"
-
+import React, { useState } from "react"
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live"
 import styles from "./figure.module.css"
+import scope from "../helpers/mdx-scope"
 
 const Figure = ({ children, appearance }) => {
+  const [open, setOpen] = useState(false)
   return (
     <figure data-appearance={appearance} className={styles.base}>
-      {children}
+      <LiveProvider scope={scope} code={children}>
+        <div className={styles.preview}>
+          <LivePreview />
+          <LiveError />
+          <button className={styles.btn} onClick={() => setOpen(open => !open)}>
+            {open ? "close" : "show code"}
+          </button>
+        </div>
+        {open && (
+          <div className={styles.code}>
+            <LiveEditor />
+          </div>
+        )}
+      </LiveProvider>
     </figure>
   )
 }
---
+++ packages/design/src/helpers/mdx-scope.js
@@ -0,0 +1,8 @@
+import * as figures from "../components/figure"
+import * as lists from "../components/list"
+import * as asides from "../components/aside"
+import * as guui from "@guardian/guui/index.ts"
+
+const scope = { ...asides, ...lists, ...figures, ...guui }
+
+export default scope
