3d27f8865febbdff34b250e2bfbd50797c0c755c
Laura buns <lawr@lauragonzalez.cc>
2019-04-08 15:40:43 +0100

    fix u

--- packages/design/gatsby-node.js
+++ packages/design/gatsby-node.js
@@ -4,7 +4,7 @@ const path = require(`path`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `Mdx`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({ node, getNode, basePath: `content` })
     createNodeField({
       node,
       name: `slug`,
@@ -13,39 +13,40 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-exports.createPages = ({ graphql, actions: { createPage } }) =>
-  new Promise((resolve, reject) => {
-    resolve(
-      graphql(`
-        {
-          allMdx {
-            edges {
-              node {
-                id
-                fields {
-                  slug
-                }
-              }
+exports.createPages = ({ graphql, actions: { createPage } }) => {
+  return graphql(`
+    {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
             }
           }
         }
-      `).then(result => {
-        if (result.errors) {
-          reject(result.errors)
-        }
-        // We'll call `createPage` for each result
-        result.data.allMdx.edges.forEach(({ node }) => {
-          createPage({
-            // This is the slug we created before
-            // (or `node.frontmatter.slug`)
-            path: node.fields.slug,
-            // This component will wrap our MDX content
-            component: path.resolve(`./src/templates/page.js`),
-            // We can use the values in this context in
-            // our page layout component
-            context: { id: node.id },
-          })
+      }
+    }
+  `)
+    .then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
+      // We'll call `createPage` for each result
+      result.data.allMdx.edges.forEach(({ node }) => {
+        createPage({
+          // This is the slug we created before
+          // (or `node.frontmatter.slug`)
+          path: node.fields.slug,
+          // This component will wrap our MDX content
+          component: path.resolve(`./src/templates/page.js`),
+          // We can use the values in this context in
+          // our page layout component
+          context: { id: node.id },
         })
       })
-    )
-  })
+    })
+    .catch(err => {
+      console.error(err)
+    })
+}
