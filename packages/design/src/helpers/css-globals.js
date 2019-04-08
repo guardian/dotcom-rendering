b9f9942ed38289046ef1fd983d72fb89f10bf652
Laura buns <lawr@lauragonzalez.cc>
2019-04-08 15:40:43 +0100

    fix u

---
+++ packages/design/src/helpers/css-globals.js
@@ -0,0 +1,35 @@
+import { palette } from "@guardian/pasteup/palette"
+import { injectGlobal } from "emotion"
+
+/*
+Given the pasteup palette,
+this function flattens all colours into a
+child free map where the keys are named as
+parent-child.
+*/
+const flatten = (obj, prefixes = []) =>
+  Object.entries(obj).reduce(
+    (prev, [key, val]) =>
+      typeof val === "string"
+        ? {
+            ...prev,
+            [[...prefixes, key].join("-")]: val,
+          }
+        : {
+            ...prev,
+            ...flatten(val, [...prefixes, key]),
+          },
+    {}
+  )
+
+const paletteAsMap = () => flatten(palette)
+
+const inject = () => {
+  injectGlobal`
+  :root {
+    ${Object.entries(paletteAsMap()).map(([key, val]) => `--${key}:${val};`)}
+  }
+  `
+}
+
+export default inject
--- packages/design/src/pages/index.js
+++ packages/design/src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Layout } from "../components/layout"
 import logo from "./tierz.svg"
 import { css } from "emotion"
-
+import "../templates/page"
 export default () => (
   <Layout>
     <div
