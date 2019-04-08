ea7818b9150eb49b1e9aa00a9f31c2759cd7f6ab
Laura buns <lawr@lauragonzalez.cc>
2019-04-08 16:09:09 +0100

    fix

--- packages/design/src/components/figure.js
+++ packages/design/src/components/figure.js
@@ -1,48 +1,51 @@
-import React, { useState } from "react"
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live"
-import styles from "./figure.module.css"
-import scope from "../helpers/mdx-scope"
+import React, { useState } from 'react';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import styles from './figure.module.css';
+import { scope } from '../helpers/mdx-scope';
 
 const Figure = ({ children, appearance }) => {
-  const [open, setOpen] = useState(false)
-  return (
-    <figure data-appearance={appearance} className={styles.base}>
-      <LiveProvider scope={scope} code={children}>
-        <div className={styles.preview}>
-          <LivePreview />
-          <LiveError />
-          <button className={styles.btn} onClick={() => setOpen(open => !open)}>
-            {open ? "close" : "show code"}
-          </button>
-        </div>
-        {open && (
-          <div className={styles.code}>
-            <LiveEditor />
-          </div>
-        )}
-      </LiveProvider>
-    </figure>
-  )
-}
+    const [open, setOpen] = useState(false);
+    return (
+        <figure data-appearance={appearance} className={styles.base}>
+            <LiveProvider scope={scope} code={children}>
+                <div className={styles.preview}>
+                    <LivePreview />
+                    <LiveError />
+                    <button
+                        className={styles.btn}
+                        onClick={() => setOpen(current => !current)}
+                    >
+                        {open ? 'close' : 'show code'}
+                    </button>
+                </div>
+                {open && (
+                    <div className={styles.code}>
+                        <LiveEditor />
+                    </div>
+                )}
+            </LiveProvider>
+        </figure>
+    );
+};
 
 const FigureTable = ({ children }) => (
-  <table data-type="preview">
-    <tbody>{children}</tbody>
-  </table>
-)
+    <table data-type="preview">
+        <tbody>{children}</tbody>
+    </table>
+);
 
 const FigureRow = ({ name, usage, ...props }) => {
-  return (
-    <tr>
-      <td>
-        <Figure {...props} />
-      </td>
-      <td>
-        <strong>{name}</strong>
-        <p>{usage}</p>
-      </td>
-    </tr>
-  )
-}
+    return (
+        <tr>
+            <td>
+                <Figure {...props} />
+            </td>
+            <td>
+                <strong>{name}</strong>
+                <p>{usage}</p>
+            </td>
+        </tr>
+    );
+};
 
-export { Figure, FigureRow, FigureTable }
+export { Figure, FigureRow, FigureTable };
