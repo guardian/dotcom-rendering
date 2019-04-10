36b6d55500f854d9209b3ef5af2644d6560c91a4
Simon Adcock <simonadcock2@gmail.com>
2019-04-10 15:58:15 +0100

    add byline story

--- packages/guui/components/Byline/Byline.stories.tsx
+++ packages/guui/components/Byline/Byline.stories.tsx
@@ -3,31 +3,33 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, radios } from '@storybook/addon-knobs';
 import { Byline } from './Byline';
 import { palette } from '@guardian/pasteup/palette';
+import { css } from 'emotion';
 
 const stories = storiesOf('User Interface', module).addDecorator(withKnobs);
 
-const options: { [s: string]: string[] } = {
-    Dark: [palette.neutral[97], palette.neutral[20]],
-    Light: [palette.neutral[20], palette.neutral[97]],
+const options: { [s: string]: string } = {
+    News: palette.news.main,
+    Opinion: palette.opinion.main,
 };
 
-const radioOptions: { [s: string]: string } = { Dark: 'Dark', Light: 'Light' };
+const radioOptions: { [s: string]: string } = { News: 'News', Opinion: 'Opinion' };
 
 stories.add('Close', () => {
-    //const value = radios('Variants', radioOptions, 'Dark');
+    const color = radios('Variants', radioOptions, 'News');
     const author: AuthorType = {
         byline: 'CP Scott',
-        twitterHandle: '@cpscott',
+        twitterHandle: 'cpscott',
         email: 'cpscott@theguardian.com',
     };
-
+    const contributorTags: TagType[] = [{
+        id: 'profile/cpscott',
+        title: 'CP Scott',
+        type: 'Contributor',
+    }];
+    const className = css`
+        color: ${options[color]};
+    `;
     return (
-        <Byline
-            bylineText={author}
-            contributorTags={[]}
-            pillar="news"
-            // foregroundColour={options[value][0]}
-            // backgroundColour={options[value][1]}
-        />
+        <Byline author={author} tags={contributorTags} className={className} />
     );
 });
