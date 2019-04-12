bd1ec362ea4d545a51c110e169115d725e2d97c7
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-12 12:10:20 +0100

    Add background parameters to close button

--- packages/design/.storybook/addons.js
+++ packages/design/.storybook/addons.js
@@ -4,3 +4,4 @@ import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-storysource/register';
 import '@storybook/addon-viewport/register';
+import '@storybook/addon-backgrounds/register';
