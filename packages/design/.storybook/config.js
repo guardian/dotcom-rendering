3fa40ffbed015152eb6170852d88acbde573fc5a
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-12 11:13:28 +0100

    Update options:

--- packages/design/.storybook/config.js
+++ packages/design/.storybook/config.js
@@ -15,27 +15,11 @@ const reqDC = require.context(
 );
 
 const viewportOptions = {
-    /**
-     * name to display in the dropdown
-     * @type {String}
-     */
     name: 'Responsive',
-
-    /**
-     * Inline styles to be applied to the story (iframe).
-     * styles is an object whose key is the camelCased version of the style name, and whose
-     * value is the style’s value, usually a string
-     * @type {Object}
-     */
     styles: {
         width: '100%',
         height: '100%',
     },
-
-    /**
-     * type of the device (e.g. desktop, mobile, or tablet)
-     * @type {String}
-     */
     type: 'desktop',
 };
 
