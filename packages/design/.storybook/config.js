c6deebc3e5499c647bff95c58b265f4bdff910c4
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-12 10:39:55 +0100

    Add a11y knob

--- packages/design/.storybook/config.js
+++ packages/design/.storybook/config.js
@@ -1,20 +1,24 @@
-import { configure } from "@storybook/react"
+import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+
 // automatically import all files ending in *.stories.tsx
 const reqGuui = require.context(
-  "@guardian/guui/components/",
-  true,
-  /\.stories\.tsx$/
-)
+    '@guardian/guui/components/',
+    true,
+    /\.stories\.tsx$/,
+);
 
 const reqDC = require.context(
-  "@guardian/frontend-rendering/web/components/",
-  true,
-  /\.stories\.tsx$/
-)
+    '@guardian/frontend-rendering/web/components/',
+    true,
+    /\.stories\.tsx$/,
+);
 
 function loadStories() {
-  reqGuui.keys().forEach(reqGuui)
-  reqDC.keys().forEach(reqDC)
+    reqGuui.keys().forEach(reqGuui);
+    reqDC.keys().forEach(reqDC);
 }
 
-configure(loadStories, module)
+addDecorator(withA11y);
+
+configure(loadStories, module);
