790074525029c028020463c90358b428c49c3d27
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-12 11:08:14 +0100

    Add viewports addon to storybook

--- packages/design/.storybook/addons.js
+++ packages/design/.storybook/addons.js
@@ -3,3 +3,4 @@ import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-storysource/register';
+import '@storybook/addon-viewport/register';
--- packages/design/.storybook/config.js
+++ packages/design/.storybook/config.js
@@ -1,4 +1,4 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 
 // automatically import all files ending in *.stories.tsx
@@ -14,11 +14,37 @@ const reqDC = require.context(
     /\.stories\.tsx$/,
 );
 
+const viewportOptions = {
+    /**
+     * name to display in the dropdown
+     * @type {String}
+     */
+    name: 'Responsive',
+
+    /**
+     * Inline styles to be applied to the story (iframe).
+     * styles is an object whose key is the camelCased version of the style name, and whose
+     * value is the style’s value, usually a string
+     * @type {Object}
+     */
+    styles: {
+        width: '100%',
+        height: '100%',
+    },
+
+    /**
+     * type of the device (e.g. desktop, mobile, or tablet)
+     * @type {String}
+     */
+    type: 'desktop',
+};
+
 function loadStories() {
     reqGuui.keys().forEach(reqGuui);
     reqDC.keys().forEach(reqDC);
 }
 
 addDecorator(withA11y);
+addParameters({ viewport: viewportOptions });
 
 configure(loadStories, module);
