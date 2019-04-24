3b9ce4fcfd7d7584cd27c05cfa40f7223d45527f
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-24 16:01:39 +0100

    Remove BlockElement from video elements

--- packages/frontend/amp/components/elements/VideoVimeo.tsx
+++ packages/frontend/amp/components/elements/VideoVimeo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const VideoVimeo: React.FC<{
-    element: VideoVimeoBlockElement;
+    element: VideoVimeo;
 }> = ({ element }) => {
     const vimeoId = element.url.split('/').pop();
     return (
--- packages/frontend/amp/components/elements/VideoYoutube.tsx
+++ packages/frontend/amp/components/elements/VideoYoutube.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const VideoYoutube: React.FC<{
-    element: VideoYoutubeBlockElement;
+    element: VideoYoutube;
 }> = ({ element }) => {
     const youtubeId = element.url.split('v=').pop();
     return (
