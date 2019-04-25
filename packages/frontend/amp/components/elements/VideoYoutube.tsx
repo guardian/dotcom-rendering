1191782ff48d3064cfd9877ec5db1e92cb66a962
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-25 08:39:09 +0100

    Use getIdFromUrl validator in vimeo and youtube embeds

--- packages/frontend/amp/components/elements/VideoYoutube.tsx
+++ packages/frontend/amp/components/elements/VideoYoutube.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { getIdFromUrl } from '@frontend/model/validators';
 
 export const VideoYoutube: React.FC<{
     element: VideoYoutube;
 }> = ({ element }) => {
-    const youtubeId = element.url.split('v=').pop();
+    const youtubeId = getIdFromUrl(
+        element.url,
+        '^[a-zA-Z0-9_-]{11}$', // Alpha numeric, underscores and hyphens, exactly 11 numbers long
+        false,
+        'v',
+    );
     return (
         <amp-youtube
             data-videoid={youtubeId}
