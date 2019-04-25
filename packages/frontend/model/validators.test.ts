bec3136f6807cbeaf9aa06945263590c35652e6c
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-25 08:38:54 +0100

    Add validator and tests for popping IDs from URLs

--- packages/frontend/model/validators.test.ts
+++ packages/frontend/model/validators.test.ts
@@ -5,6 +5,7 @@ import {
     getBoolean,
     getArray,
     getObject,
+    getIdFromUrl,
 } from './validators';
 
 describe('validators', () => {
@@ -256,6 +257,94 @@ describe('validators', () => {
 
             expect(() => {
                 getObject(data, 'config.prop');
+            }).toThrow();
+        });
+    });
+
+    describe('getIdFromUrl', () => {
+        it('Returns matching ID for YouTube formats', () => {
+            const youtubeRegEx = '^[a-zA-Z0-9_-]{11}$';
+
+            const formats = [
+                {
+                    url: 'http://www.youtube.com/ytscreeningroom?v=NRHEIGHTx8I',
+                    id: 'NRHEIGHTx8I',
+                },
+                {
+                    url: 'http://www.youtube.com/ytscreeningroom?v=NRH_IGHTx8I',
+                    id: 'NRH_IGHTx8I',
+                },
+                {
+                    url: 'http://www.youtube.com/ytscreeningroom?v=NRH-IGHTx8I',
+                    id: 'NRH-IGHTx8I',
+                },
+            ];
+
+            formats.map(_ => {
+                expect(getIdFromUrl(_.url, youtubeRegEx, false, 'v')).toBe(
+                    _.id,
+                );
+            });
+        });
+
+        it('Returns matching ID for Vimeo formats', () => {
+            const vimeoRegEx = '(\\d+)($|\\/)';
+
+            const formats = [
+                {
+                    url: 'https://vimeo.com/channels/staffpicks/332085955',
+                    id: '332085955',
+                },
+                {
+                    url:
+                        'https://vimeo.com/channels/another/staffpicks/332085955',
+                    id: '332085955',
+                },
+                {
+                    url: 'https://vimeo.com/channels/staffpicks/123456',
+                    id: '123456',
+                },
+            ];
+
+            formats.map(_ => {
+                expect(getIdFromUrl(_.url, vimeoRegEx, true)).toBe(_.id);
+            });
+        });
+
+        it('Throws an error if it cannot find an ID', () => {
+            expect(() => {
+                getIdFromUrl('https://theguardian.com', '', false, 'v');
+            }).toThrow();
+
+            expect(() => {
+                getIdFromUrl('https://theguardian.com?p=test', '', false, 'v');
+            }).toThrow();
+
+            expect(() => {
+                getIdFromUrl('https://theguardian.com/test', '', false, 'p');
+            }).toThrow();
+        });
+
+        it('Throws an error if it ID is in incorrect format', () => {
+            expect(() => {
+                expect(
+                    getIdFromUrl(
+                        'https://theguardian.com/test',
+                        'nottest',
+                        true,
+                    ),
+                ).toBe('test');
+            }).toThrow();
+
+            expect(() => {
+                expect(
+                    getIdFromUrl(
+                        'https://theguardian.com?p=test',
+                        'nottest',
+                        false,
+                        'p',
+                    ),
+                ).toBe('test');
             }).toThrow();
         });
     });
--- packages/frontend/model/validators.ts
+++ packages/frontend/model/validators.ts
@@ -1,4 +1,5 @@
 import get from 'lodash.get';
+import { parse, URLSearchParams } from 'url';
 
 export const getString = (
     obj: object,
@@ -124,4 +125,36 @@ export const getObject = (
             obj,
         )}'`,
     );
+};
+
+export const getIdFromUrl = (
+    urlString: string,
+    regexFormat: string,
+    inPath?: boolean,
+    queryParam?: string,
+) => {
+    const logErr = (actual: string, message: string) => {
+        throw new Error(`The URL ${urlString} returned ${actual}. ${message}`);
+    };
+
+    const url = parse(urlString);
+
+    const id =
+        (inPath && url.pathname && url.pathname.split('/').pop()) ||
+        (queryParam &&
+            url.query &&
+            new URLSearchParams(url.query).get(queryParam)) ||
+        logErr(
+            'an undefined ID',
+            'Could not get ID from pathname or searchParams.',
+        );
+
+    if (!new RegExp(regexFormat).test(id)) {
+        return logErr(
+            id,
+            `Popped value didn't match regexFormat ${regexFormat}`,
+        );
+    }
+
+    return id;
 };
