d758aec2d3dbf12baabc7b1cdd744b040e065ce1
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-26 17:27:00 +0100

    Add caption type (plain text or html to story)

--- packages/guui/components/Caption/Caption.stories.tsx
+++ packages/guui/components/Caption/Caption.stories.tsx
@@ -12,14 +12,27 @@ const radioOptions = pillarNames.reduce(
     {},
 );
 
+const plainCaption = 'This is a caption';
+const captionTypes = {
+    'Plain text': plainCaption,
+    HTML:
+        '<a href="https://www.theguardian.com">This is a caption with HTML</a>',
+};
+
 stories.add('Caption', () => {
     const pillarValue = radios('Pillars', radioOptions, 'news');
+    const captionValue = radios(
+        'Caption Types',
+        captionTypes,
+        captionTypes['Plain text'],
+    );
     const padding = boolean('Pad the caption', false);
     return (
         <Caption
-            captionText={'This is a caption'}
+            captionText={captionValue}
             pillar={pillarValue}
             padCaption={padding}
+            dirtyHtml={captionValue !== plainCaption}
         >
             <img src="https://placekitten.com/420/320?image=2" />
         </Caption>
