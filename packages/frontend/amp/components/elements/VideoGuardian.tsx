f07824957547dac952fb66b2cf067e9f38a3b2dc
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-26 13:52:46 +0100

    Reimplement captions in videos

--- packages/frontend/amp/components/elements/VideoGuardian.tsx
+++ packages/frontend/amp/components/elements/VideoGuardian.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import { Caption } from '@guardian/guui/components/Caption/Caption';
 
 export const VideoGuardian: React.FC<{
     element: VideoGuardian;
-}> = ({ element }) => {
+    pillar: Pillar;
+}> = ({ element, pillar }) => {
     return (
-        <amp-video controls={''} width="16" height="9" layout="responsive">
-            <div fallback={''}>
-                Sorry, your browser is unable to play this video.
-                <br />
-                Please <a href="http://whatbrowser.org/">upgrade</a> to a modern
-                browser and try again.
-            </div>
-            {element.assets.map(
-                encoding =>
-                    encoding.mimeType.includes('video') && (
-                        <source src={encoding.url} type={encoding.mimeType} />
-                    ),
-            )}
-        </amp-video>
+        <Caption captionText={element.caption} pillar={pillar}>
+            <amp-video controls={''} width="16" height="9" layout="responsive">
+                <div fallback={''}>
+                    Sorry, your browser is unable to play this video.
+                    <br />
+                    Please <a href="http://whatbrowser.org/">upgrade</a> to a
+                    modern browser and try again.
+                </div>
+                {element.assets.map(
+                    encoding =>
+                        encoding.mimeType.includes('video') && (
+                            <source
+                                src={encoding.url}
+                                type={encoding.mimeType}
+                            />
+                        ),
+                )}
+            </amp-video>
+        </Caption>
     );
 };
