d18816c9f3def8f1c00a7b866c54e372491b5ab1
google3278500_student@qwiklabs.net <google3278500_student@qwiklabs.net@qwiklabs.net>
2019-05-09 15:43:20 +0100

    Introduce separate body components for liveblog and article
    The assumption here is that we don't need to split higher up the
    stack (i.e. routing/article) for now, but that it does make sense
    to have separate body components.
    
    At the moment liveblogs have, I think, sufficient body differences
    (key events, multiple blocks, pagination, and different top-meta)
    to warrant this.

---
+++ packages/frontend/amp/components/BodyLiveblog.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { InnerContainer } from '@frontend/amp/components/InnerContainer';
+import { Elements } from '@frontend/amp/components/lib/Elements';
+import { css } from 'emotion';
+import { ArticleModel } from '@frontend/amp/pages/Article';
+import { TopMetaNews } from '@frontend/amp/components/TopMetaNews';
+import { TopMetaOpinion } from '@frontend/amp/components/TopMetaOpinion';
+import { TopMetaPaidContent } from '@frontend/amp/components/TopMetaPaidContent';
+import { SubMeta } from '@frontend/amp/components/SubMeta';
+import { getToneType, StyledTone } from '@frontend/amp/lib/tag-utils';
+import { pillarPalette } from '@frontend/lib/pillars';
+import { palette } from '@guardian/pasteup/palette';
+
+const body = (pillar: Pillar, tone: StyledTone) => {
+    const bgColorMap = {
+        'default-tone': palette.neutral[100],
+        'tone/comment': palette.opinion.faded,
+        'tone/advertisement-features': palette.neutral[100],
+    };
+    return css`
+        background-color: ${bgColorMap[tone]};
+        ${bulletStyle(pillar)}
+    `;
+};
+
+const bulletStyle = (pillar: Pillar) => css`
+    .bullet {
+        color: transparent;
+        font-size: 1px;
+    }
+
+    .bullet:before {
+        display: inline-block;
+        content: '';
+        border-radius: 6px;
+        height: 12px;
+        width: 12px;
+        margin-right: 2px;
+        background-color: ${pillarPalette[pillar].main};
+        margin-left: 0px;
+    }
+`;
+
+export const Body: React.FC<{
+    pillar: Pillar;
+    data: ArticleModel;
+    config: ConfigType;
+}> = ({ pillar, data, config }) => {
+    const tone = getToneType(data.tags);
+    const topMeta = {
+        'default-tone': <TopMetaNews articleData={data} />,
+        'tone/comment': <TopMetaOpinion articleData={data} />,
+        'tone/advertisement-features': (
+            <TopMetaPaidContent articleData={data} />
+        ),
+    };
+
+    return (
+        <InnerContainer className={body(pillar, tone)}>
+            {topMeta[tone]}
+            <Elements
+                pillar={pillar}
+                elements={data.blocks[0] || []}
+                // stuff for ads
+                edition={data.editionId}
+                section={data.sectionName}
+                contentType={data.contentType}
+                switches={config.switches}
+                commercialProperties={data.commercialProperties}
+                isImmersive={data.isImmersive}
+            />
+            <SubMeta
+                sections={data.subMetaSectionLinks}
+                keywords={data.subMetaKeywordLinks}
+                pillar={pillar}
+                sharingURLs={data.sharingUrls}
+                pageID={data.pageId}
+                isCommentable={data.isCommentable}
+                guardianBaseURL={data.guardianBaseURL}
+            />
+        </InnerContainer>
+    );
+};
