52c443a3c304caa0c9ae6285289634fe9e6a89b5
vanessa <32312712+vlbee@users.noreply.github.com>
2019-05-09 16:23:57 +0100

    Add neutral theme to PillarColours

--- packages/pasteup/palette.ts
+++ packages/pasteup/palette.ts
@@ -1,10 +1,15 @@
 export type colour = string;
+
 export interface PillarColours {
     dark: colour;
     main: colour;
     bright: colour;
     pastel: colour;
     faded: colour;
+    neutral: NeutralTheme;
+}
+export interface NeutralTheme {
+    border: colour;
 }
 export interface AllPillarColours {
     news: PillarColours;
@@ -56,46 +61,6 @@ const red = {
     main: '#c70000',
 };
 
-const news: PillarColours = {
-    dark: red.dark,
-    main: red.main,
-    bright: '#ff4e36',
-    pastel: '#ffbac8',
-    faded: '#fff4f2',
-};
-
-const opinion: PillarColours = {
-    dark: '#bd5318',
-    main: '#e05e00',
-    bright: '#ff7f0f',
-    pastel: '#f9b376',
-    faded: '#fef9f5',
-};
-
-const sport: PillarColours = {
-    dark: '#005689',
-    main: '#0084c6',
-    bright: '#00b2ff',
-    pastel: '#90dcff',
-    faded: '#f1f8fc',
-};
-
-const culture: PillarColours = {
-    dark: '#6b5840',
-    main: '#a1845c',
-    bright: '#eacca0',
-    pastel: '#e7d4b9',
-    faded: '#fbf6ef',
-};
-
-const lifestyle: PillarColours = {
-    dark: '#7d0068',
-    main: '#bb3b80',
-    bright: '#ffabdb',
-    pastel: '#fec8d3',
-    faded: '#feeef7',
-};
-
 const brand = {
     dark: '#041f4a',
     main: '#052962',
@@ -124,12 +89,66 @@ const neutral = {
     100: '#ffffff',
 };
 
+const lightTheme: NeutralTheme = {
+    border: neutral[86],
+};
+
+const darkTheme: NeutralTheme = {
+    border: neutral[60],
+};
+
+const news: PillarColours = {
+    dark: red.dark,
+    main: red.main,
+    bright: '#ff4e36',
+    pastel: '#ffbac8',
+    faded: '#fff4f2',
+    neutral: lightTheme,
+};
+
+const opinion: PillarColours = {
+    dark: '#bd5318',
+    main: '#e05e00',
+    bright: '#ff7f0f',
+    pastel: '#f9b376',
+    faded: '#fef9f5',
+    neutral: lightTheme,
+};
+
+const sport: PillarColours = {
+    dark: '#005689',
+    main: '#0084c6',
+    bright: '#00b2ff',
+    pastel: '#90dcff',
+    faded: '#f1f8fc',
+    neutral: lightTheme,
+};
+
+const culture: PillarColours = {
+    dark: '#6b5840',
+    main: '#a1845c',
+    bright: '#eacca0',
+    pastel: '#e7d4b9',
+    faded: '#fbf6ef',
+    neutral: lightTheme,
+};
+
+const lifestyle: PillarColours = {
+    dark: '#7d0068',
+    main: '#bb3b80',
+    bright: '#ffabdb',
+    pastel: '#fec8d3',
+    faded: '#feeef7',
+    neutral: lightTheme,
+};
+
 const labs: PillarColours = {
     dark: neutral[7],
     main: neutral[7],
     bright: '#69d1ca', // bright teal
     pastel: '', // TODO
     faded: '#65a897', // dark teal
+    neutral: darkTheme,
 };
 
 const specialReport = { dark: '#3f464a' };
