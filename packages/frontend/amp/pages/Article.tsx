77d3736a7c928121d5a4e795f3428387bf80813b
google3278500_student@qwiklabs.net <google3278500_student@qwiklabs.net@qwiklabs.net>
2019-05-09 15:43:20 +0100

    Introduce separate body components for liveblog and article
    The assumption here is that we don't need to split higher up the
    stack (i.e. routing/article) for now, but that it does make sense
    to have separate body components.
    
    At the moment liveblogs have, I think, sufficient body differences
    (key events, multiple blocks, pagination, and different top-meta)
    to warrant this.

--- packages/frontend/amp/pages/Article.tsx
+++ packages/frontend/amp/pages/Article.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Footer } from '@frontend/amp/components/Footer';
 import { Container } from '@frontend/amp/components/Container';
-import { Body } from '@frontend/amp/components/Body';
+import { Body as BodyArticle } from '@frontend/amp/components/BodyArticle';
+import { Body as BodyLiveblog } from '@frontend/amp/components/BodyLiveblog';
 import { Header } from '@frontend/amp/components/Header';
 import { palette } from '@guardian/pasteup/palette';
 import { Onward } from '@frontend/amp/components/Onward';
@@ -50,6 +51,23 @@ export interface ArticleModel {
     starRating?: number;
 }
 
+const Body: React.SFC<{
+    data: ArticleModel;
+    config: ConfigType;
+}> = ({ data, config }) => {
+    // TODO check if there is a better way to determine if liveblog
+    const isLiveBlog =
+        data.tags.find(tag => tag.id === 'tone/minutebyminute') !== undefined;
+
+    if (isLiveBlog) {
+        return (
+            <BodyLiveblog pillar={data.pillar} data={data} config={config} />
+        );
+    }
+
+    return <BodyArticle pillar={data.pillar} data={data} config={config} />;
+};
+
 export const Article: React.FC<{
     nav: NavType;
     articleData: ArticleModel;
@@ -69,11 +87,7 @@ export const Article: React.FC<{
                     config={config}
                     guardianBaseURL={articleData.guardianBaseURL}
                 />
-                <Body
-                    pillar={articleData.pillar}
-                    data={articleData}
-                    config={config}
-                />
+                <Body data={articleData} config={config} />
                 <Onward
                     shouldHideAds={articleData.shouldHideAds}
                     pageID={articleData.pageId}
