3f5e1ffba03ec5f3811aaecd1cd6b69ffa75ba35
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-05-14 17:10:15 +0100

    Improved tracking iframe location

--- packages/frontend/amp/components/AdUserSync.tsx
+++ packages/frontend/amp/components/AdUserSync.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css } from 'emotion';
 
 const preBidSrc =
     'https://cdn.jsdelivr.net/npm/prebid-universal-creative@latest/dist/load-cookie.html';
@@ -6,13 +7,19 @@ const preBidSrc =
 const preBidImg =
     'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
 
+const iframeStyle = css`
+    position: fixed;
+    top: -1px;
+`;
+
 export const AdUserSync: React.FC<{}> = () => {
     return (
         <amp-iframe
+            class={iframeStyle}
             data-block-on-consent="_till_accepted"
-            width="1"
             title="User Sync"
             height="1"
+            width="1"
             sandbox="allow-scripts"
             frameborder="0"
             src={preBidSrc}
