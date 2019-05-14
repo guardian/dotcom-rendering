bbbf1ef8231ee71ffb28ab169e1da8c1da4d9ba5
Philip McMahon <philip.mcmahon@guardian.co.uk>
2019-05-14 17:01:08 +0100

    Add json log appender.

--- packages/frontend/app/logging.ts
+++ packages/frontend/app/logging.ts
@@ -1,13 +1,41 @@
 import path from 'path';
-import { configure, getLogger } from 'log4js';
+import { configure, getLogger, addLayout } from 'log4js';
 
 const logLocation =
     process.env.NODE_ENV === 'production'
         ? '/var/log/dotcom-rendering/dotcom-rendering.log'
         : `${path.resolve('logs')}/dotcom-rendering.log`;
 
+const logFields = (logEvent: any): any => {
+    return {
+        stack: 'frontend',
+        app: 'dotcom-rendering',
+        stage: 'CODE',
+        '@timestamp': logEvent.startTime,
+        '@version': 1,
+        level: logEvent.level.levelStr,
+        level_value: logEvent.level.level,
+        message: logEvent.data.join(','),
+    };
+};
+
+addLayout('json', config => {
+    return logEvent => {
+        return JSON.stringify(logFields(logEvent));
+    };
+});
+
 configure({
-    appenders: { fileAppender: { type: 'file', filename: logLocation } },
+    appenders: {
+        fileAppender: {
+            type: 'file',
+            filename: logLocation,
+            maxLogSize: 10000,
+            backups: 5,
+            compress: true,
+            layout: { type: 'json', separator: ',' },
+        },
+    },
     categories: { default: { appenders: ['fileAppender'], level: 'info' } },
 });
 
