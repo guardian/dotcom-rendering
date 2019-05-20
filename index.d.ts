2998f92131fde3e391f0755212f53f713067e8d3
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-05-20 17:11:34 +0100

    Some lint fixes

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare global {
     /*~ Here, declare things that go in the global namespace, or augment
      *~ existing declarations in the global namespace
      */
-    interface Window { 
+    interface Window {
         guardian: {
             app: {
                 data: any;
