c42d4e8f2b34fef7ecb08e845f88f60fba429cf1
Nicolas Long <nicolas.long@theguardian.com>
2019-06-04 14:36:55 +0100

    Add pagination styling

--- packages/frontend/amp/components/BodyLiveblog.tsx
+++ packages/frontend/amp/components/BodyLiveblog.tsx
@@ -10,6 +10,10 @@ import { KeyEvents } from '@frontend/amp/components/KeyEvents';
 import { headline, textSans } from '@guardian/pasteup/typography';
 import { Blocks } from '@frontend/amp/components/Blocks';
 import RefreshIcon from '@guardian/pasteup/icons/refresh.svg';
+import ChevronRightSingle from '@guardian/pasteup/icons/chevron-right-single.svg';
+import ChevronRightDouble from '@guardian/pasteup/icons/chevron-right-double.svg';
+import ChevronLeftSingle from '@guardian/pasteup/icons/chevron-left-single.svg';
+import ChevronLeftDouble from '@guardian/pasteup/icons/chevron-left-double.svg';
 
 // TODO check if liveblog background colours are more complex - like regular
 // article is
@@ -65,16 +69,69 @@ const updateButtonStyle = css`
     }
 `;
 
+// TODO create link style css function and pass in isActive, isMarginRight to
+// improve things
+const paginationStyle = css`
+    ${textSans(1)};
+    font-weight: bold;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const paginationLinkStyle = (isActive: boolean, isMarginRight: boolean) => css`
+    width: 2.25rem;
+    border-radius: 100%;
+
+    position: relative;
+    color: ${palette.neutral[7]};
+    border: 1px solid ${palette.neutral[86]};
+    height: 36px;
+    line-height: 38px;
+    display: inline-block;
+
+    margin-right: ${isMarginRight ? '5px' : '0px'};
+
+    span {
+        fill: ${palette.neutral[100]};
+
+        svg {
+            position: absolute;
+            top: 10px;
+            left: 9px;
+            width: 16px;
+            height: 16px;
+            fill: ${isActive ? palette.neutral[46] : palette.neutral[86]};
+        }
+    }
+`;
+
 const Pagination: React.SFC<{
     pagination?: Pagination;
     guardianURL: string;
 }> = ({ pagination, guardianURL }) => {
-    const link = (url: string, suffix?: string): JSX.Element => {
-        if (!suffix) {
-            return <></>;
-        }
+    const link = (
+        url: string,
+        icon: JSX.Element,
+        suffix?: string,
+        hasRightMargin: boolean = false,
+    ): JSX.Element => {
+        const styles = paginationLinkStyle(
+            suffix !== undefined,
+            hasRightMargin,
+        );
 
-        return <a href={url + suffix}>{suffix}</a>;
+        const attrs = {
+            className: styles,
+            href: suffix ? url + suffix : undefined,
+        };
+
+        return (
+            <a {...attrs}>
+                <span>{icon}</span>
+            </a>
+        );
     };
 
     if (!pagination) {
@@ -82,14 +139,30 @@ const Pagination: React.SFC<{
     }
 
     return (
-        <div>
-            {link(guardianURL, pagination.newest)}
-            {link(guardianURL, pagination.newer)}
-            <div>
-                Page {pagination.currentPage} of {pagination.totalPages}
-            </div>
-            {link(guardianURL, pagination.older)}
-            {link(guardianURL, pagination.oldest)}
+        <div className={paginationStyle}>
+            <span>
+                {link(
+                    guardianURL,
+                    <ChevronLeftDouble />,
+                    pagination.newest,
+                    true,
+                )}
+                {link(guardianURL, <ChevronLeftSingle />, pagination.newer)}
+            </span>
+
+            <span>
+                {pagination.currentPage} of {pagination.totalPages}
+            </span>
+
+            <span>
+                {link(
+                    guardianURL,
+                    <ChevronRightSingle />,
+                    pagination.older,
+                    true,
+                )}
+                {link(guardianURL, <ChevronRightDouble />, pagination.oldest)}
+            </span>
         </div>
     );
 };
