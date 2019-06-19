9f3e3682c14e6639336c13924414928359596770
Nicolas Long <nicolas.long@theguardian.com>
2019-06-19 17:38:19 +0100

    Set default severity to error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-    "parser": '@typescript-eslint/parser',
-    "plugins": [
-        "@typescript-eslint/tslint"
-    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint/tslint'],
     parserOptions: {
         ecmaVersion: 6,
-        project: "./tsconfig.json",
-        sourceType: "module"
+        project: './tsconfig.json',
+        sourceType: 'module',
     },
-    'rules': {
-        "@typescript-eslint/tslint/config": [1, {
-            "lintFile": "./tslint.json", // path to tslint.json of your project
-        }],
-    }
-}
+    rules: {
+        '@typescript-eslint/tslint/config': [
+            'error',
+            {
+                lintFile: './tslint.json', // path to tslint.json of your project
+            },
+        ],
+    },
+};
