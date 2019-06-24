244d849863e520430a918dd20e84707c92557c4b
vanessa <32312712+vlbee@users.noreply.github.com>
2019-06-24 14:21:54 +0100

    Add JsonSchema samples test in frontend/model

---
+++ packages/frontend/model/schema/schemaSamples.test.ts
@@ -0,0 +1,88 @@
+const glob = require('glob');
+const fs = require('fs');
+
+// JSON Schema validation layer
+import { validateRequestData } from '../validate';
+// NEW extraction functions without validation
+import { extract as extractCAPI } from '@frontend/model/extract-capi';
+import { extract as extractConfig } from '@frontend/model/extract-config';
+import { extract as extractLinkedData } from '@frontend/model/extract-linked-data';
+import { extract as extractGA } from '@frontend/model/extract-ga';
+
+//  OLD data cleaner and extraction functions
+import { extract as oldExtractCAPI } from '@frontend/model/old-extract/extract-capi';
+import { extract as oldExtractConfig } from '@frontend/model/old-extract/extract-config';
+import { extract as oldExtractLinkedData } from '@frontend/model/old-extract/extract-linked-data';
+import { extract as oldExtractGA } from '@frontend/model/old-extract/extract-ga';
+
+const validateNew = (data: any) => {
+    let validatedBody;
+    try {
+        validatedBody = validateRequestData(data, '/AMPArticle');
+    } catch (err) {
+        console.log("validation error", data.content.pageId)
+        return err
+    }
+
+    const CAPI = extractCAPI(validatedBody);
+    const linkedData = extractLinkedData(validatedBody);
+    const config = extractConfig(validatedBody);
+    const ga = extractGA(validatedBody); // WEB ONLY
+
+    return {
+        CAPI,
+        linkedData,
+        config,
+        ga,
+    };
+};
+
+const validateOld = (data: any) => {
+    const CAPI = oldExtractCAPI(data);
+    const linkedData = oldExtractLinkedData(data);
+    const config = oldExtractConfig(data);
+    const ga = oldExtractGA(data);  // WEB ONLY
+
+    return {
+        CAPI,
+        linkedData,
+        config,
+        ga,
+    };
+};
+
+
+const runTest = (samples: string[]) => {
+    let count = 0
+    const rootIndex = __dirname.split('/').indexOf('dotcom-rendering')
+    const root = __dirname.split('/').slice(0, rootIndex + 1).join('/')
+
+    const results = samples.map((sample: string) => {
+        count = count + 1
+        const path = `${root}/${sample}`
+        const sampleJson = JSON.parse(fs.readFileSync(path, 'utf-8'))
+        const name = sample.split('/').slice(3)
+        try {
+            const newRes = validateNew(sampleJson);
+            const oldRes = validateOld(sampleJson);
+            return [count, name, { new: newRes, old: oldRes, error: null }]
+        } catch (error) {
+            return [count, name, { new: null, old: null, error: error }]
+        }
+    })
+    return results
+}
+
+describe('JSON SCHEMA validation layer sample comparison test', () => {
+
+    // NOTE: Run scripts/validation/sampleCollection.js to collect json samples for testing
+    const files = glob.sync('scripts/validation/samples/*.json', {})
+    const results = runTest(files)
+
+    it.each(results)(`test %i of ${files.length}, %s`, (count, name, sample) => {
+        expect(sample.error).toEqual(null)
+        expect(sample.new).toMatchObject(sample.old);
+    })
+})
+
+
