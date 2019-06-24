05d8f29168022993c5d81e96e31799e4f1043641
vanessa <32312712+vlbee@users.noreply.github.com>
2019-06-24 14:08:10 +0100

    Move old data extraction functions

---
+++ packages/frontend/model/old-extract/extract-config.ts
@@ -0,0 +1,12 @@
+
+import { getNonEmptyString, getString, getObject } from '../validators';
+
+export const extract = (data: {}): ConfigType => ({
+    ajaxUrl: getNonEmptyString(data, 'site.ajaxUrl'),
+    sentryPublicApiKey: getString(data, 'site.sentryPublicApiKey', ''),
+    sentryHost: getString(data, 'site.sentryHost', ''),
+    isDev: process.env.NODE_ENV === 'development',
+    switches: getObject(data, 'site.switches', {}),
+    dfpAccountId: getObject(data, 'site.dfpAccountId', ''), // TODO check and fix
+    commercialUrl: getNonEmptyString(data, 'site.commercialUrl'),
+});
---
+++ packages/frontend/model/old-extract/extract-ga.ts
@@ -0,0 +1,43 @@
+export interface GADataType {
+    pillar: string;
+    webTitle: string;
+    section: string;
+    contentType: string;
+    commissioningDesks: string;
+    contentId: string;
+    authorIds: string;
+    keywordIds: string;
+    toneIds: string;
+    seriesId: string;
+    isHosted: string;
+    edition: string;
+    beaconUrl: string;
+}
+
+// All GA fields should  fall back to default values -
+import { getString, getBoolean } from '../validators';
+import { findPillar } from '../find-pillar';
+
+// we should not bring down the website if a trackable field is missing!
+export const extract = (data: any): GADataType => {
+    const edition = getString(data, 'page.edition', '').toLowerCase();
+
+    return {
+        webTitle: getString(data, 'page.webTitle', ''),
+        pillar: findPillar(getString(data, 'page.pillar', '')) || 'news',
+        section: getString(data, 'page.section', ''),
+        contentType: getString(data, 'page.contentType', '')
+            .toLowerCase()
+            .split(' ')
+            .join(''),
+        commissioningDesks: getString(data, 'page.tags.commissioningDesks', ''),
+        contentId: getString(data, 'page.contentId', ''),
+        authorIds: getString(data, 'page.tags.authorIds', ''),
+        keywordIds: getString(data, 'page.tags.keywordIds', ''),
+        toneIds: getString(data, 'page.tags.toneIds', ''),
+        seriesId: getString(data, 'page.seriesId', ''),
+        isHosted: getBoolean(data, 'page.meta.isHosted', false).toString(),
+        edition: edition === 'int' ? 'international' : edition,
+        beaconUrl: getString(data, 'site.beaconUrl', ''),
+    };
+};
