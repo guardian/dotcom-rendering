cba66f7d79dbb1e9b6277c25a3b067e33eb96267
vanessa <32312712+vlbee@users.noreply.github.com>
2019-06-24 14:10:30 +0100

    Refactor validateRequestData

--- packages/frontend/model/validate.test.ts
+++ packages/frontend/model/validate.test.ts
@@ -1,18 +1,114 @@
 import { validateRequestData, ValidationError } from './validate';
-import { data } from '@root/fixtures/article';
+import { data as articleData } from '@root/fixtures/article';
+import cloneDeep from 'lodash.clonedeep';
 
-describe('JSON Schema request data validation', () => {
-    it('returns true if data is valid', () => {
-        expect(validateRequestData(data, '/AMPArticle')).toBe(true);
+let testData: any;
+
+beforeEach(() => {
+    testData = cloneDeep(articleData);
+});
+
+describe('JSON Schema validation of request body', () => {
+    it('returns data if data is valid', () => {
+        expect(validateRequestData(articleData, '/AMPArticle')).toEqual(
+            articleData,
+        );
     });
 
-    it('throws validation Error if data is invalid', () => {
-        const testData = {
-            ...data,
-            page: { content: { headline: 1 } },
-        };
+    it('throws Validation Error if required data is invalid type', () => {
+        testData.page.content.headline = 1;
         expect(() => {
             validateRequestData(testData, '/AMPArticle');
         }).toThrowError(ValidationError);
+    });
+
+    it('throws Validation Error if non-required data is invalid type', () => {
+        testData.page.content.standfirst = 1;
+        expect(() => {
+            validateRequestData(testData, '/AMPArticle');
+        }).toThrowError(ValidationError);
+    });
+
+    it('throws Validation Error if required data is missing/undefined', () => {
+        testData.page.contentType = undefined;
+        expect(() => {
+            validateRequestData(testData, '/AMPArticle');
+        }).toThrowError(ValidationError);
+    });
+
+    it('throws Validation Error if required data is null', () => {
+        testData.page.webURL = null;
+        expect(() => {
+            validateRequestData(testData, '/AMPArticle');
+        }).toThrowError(ValidationError);
+    });
+
+    it('throws Validation Error if required non-empty string is empty string', () => {
+        testData.page.webURL = '';
+        expect(() => {
+            validateRequestData(testData, '/AMPArticle');
+        }).toThrowError(ValidationError);
+    });
+
+    it('returns data with default values if non-required data is null', () => {
+        testData.site.sentryHost = null;
+        const validatedDataWithDefaults = {
+            ...testData,
+            site: { ...testData.site, sentryHost: '' },
+        };
+        expect(validateRequestData(testData, '/AMPArticle')).toEqual(
+            validatedDataWithDefaults,
+        );
+    });
+
+    it('returns data with default values if non-required string data is empty string', () => {
+        testData.site.sentryHost = '';
+        const validatedDataWithDefaults = {
+            ...testData,
+            site: { ...testData.site, sentryHost: '' },
+        };
+        expect(validateRequestData(testData, '/AMPArticle')).toEqual(
+            validatedDataWithDefaults,
+        );
+    });
+
+    it('returns data with default values if non-required non-string data is empty string', () => {
+        testData.site.switches = '';
+        const validatedDataWithDefaults = {
+            ...testData,
+            site: { ...testData.site, switches: {} },
+        };
+        expect(validateRequestData(testData, '/AMPArticle')).toEqual(
+            validatedDataWithDefaults,
+        );
+    });
+
+    it('returns data with default values if non-required string data is undefined', () => {
+        testData.site.sentryHost = undefined;
+        const validatedDataWithDefaults = {
+            ...testData,
+            site: { ...testData.site, sentryHost: '' },
+        };
+        expect(validateRequestData(testData, '/AMPArticle')).toEqual(
+            validatedDataWithDefaults,
+        );
+    });
+
+    it('returns data with default values if non-required array data is undefined', () => {
+        testData.page.meta.linkedData = undefined;
+
+        const validatedDataWithDefaults = {
+            ...testData,
+            page: {
+                ...testData.page,
+                meta: {
+                    ...testData.page.meta,
+                    linkedData: [],
+                },
+            },
+        };
+        expect(validateRequestData(testData, '/AMPArticle')).toEqual(
+            validatedDataWithDefaults,
+        );
     });
 });
--- packages/frontend/model/validate.ts
+++ packages/frontend/model/validate.ts
@@ -1,4 +1,5 @@
 import Ajv from 'ajv';
+import { schema } from './schema/articleV2.json.schema';
 
 export class ValidationError extends Error {
     constructor(message: string) {
@@ -9,36 +10,23 @@ export class ValidationError extends Error {
 
 // enpoint can be used to reference matching schema
 export const validateRequestData = (data: any, endpoint: string) => {
-    const schema = {
-        properties: {
-            page: {
-                type: 'object',
-                properties: {
-                    content: {
-                        type: 'object',
-                        properties: {
-                            headline: { type: 'string' },
-                        },
-                    },
-                },
-            },
-        },
-    };
-
-    const options = {
+    const options: Ajv.Options = {
         verbose: true,
         allErrors: true,
+        useDefaults: 'empty', // modifies data in place // TODO add 'empty' to ajv type def
     };
-
     const ajv = new Ajv(options);
     const isValid = ajv.validate(schema, data);
 
     if (!isValid) {
         throw new ValidationError(
             `Could not validate ${endpoint} request for ${
-                data.page.pageId
+            data.page.pageId
             }.\n ${JSON.stringify(ajv.errors, null, 2)}`,
         );
     }
-    return isValid;
+
+    // console.log(`Data validated against Article Schema version: ${data.version}`)
+    // Includes fallback values from schema defaults for undefined properties
+    return data;
 };
