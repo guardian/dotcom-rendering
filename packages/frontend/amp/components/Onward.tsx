2b9d8f59598b56a4ba152ede04155c8fd82fcabe
Pascal Honoré <pascal@alseyn.net>
2019-07-04 10:11:16 +0100

    Correct Outbrain compliance logic

--- packages/frontend/amp/components/Onward.tsx
+++ packages/frontend/amp/components/Onward.tsx
@@ -147,10 +147,10 @@ export const Onward: React.FC<{
     );
 
     // Outbrain is compliant if it appears in the top 2 containers
-    const outbrainIsCompliant = storyPackage.concat(series).length > 1;
+    const outbrainIsNotCompliant = storyPackage.concat(series).length > 1;
     const outbrain = shouldHideAds
         ? []
-        : [outbrainContainer(webURL, outbrainIsCompliant)];
+        : [outbrainContainer(webURL, !outbrainIsNotCompliant)];
 
     // Note, if order changes, you may need to recalculate outbrain compliance
     const containers = storyPackage.concat(
