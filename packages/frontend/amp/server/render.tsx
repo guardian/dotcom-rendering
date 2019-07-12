a2e49581c098590b8349b5bfbef89b93b6610cd0
Pascal Honoré <pascal@alseyn.net>
2019-07-12 11:55:03 +0100

    Set the correct neilsenAPIID value

--- packages/frontend/amp/server/render.tsx
+++ packages/frontend/amp/server/render.tsx
@@ -7,6 +7,7 @@ import { extract as extractCAPI } from '@frontend/model/extract-capi';
 import { extract as extractNAV } from '@frontend/model/extract-nav';
 import { AnalyticsModel } from '@frontend/amp/components/Analytics';
 import { validateAsCAPIType as validateV2 } from '@frontend/modelV2/validate';
+import { findBySubsection } from '@frontend/model/article-sections';
 
 export const render = (
     { body, path }: express.Request,
@@ -24,16 +25,18 @@ export const render = (
 
         const scripts = [...extractScripts(elements, CAPI.mainMediaElements)];
 
+        const sectionName = CAPI.sectionName || '';
+
         const analytics: AnalyticsModel = {
             gaTracker: 'UA-78705427-1',
             title: CAPI.headline,
             fbPixelaccount: '279880532344561',
             comscoreID: '6035250',
-            section: CAPI.sectionName || '',
+            section: sectionName,
             contentType: CAPI.contentType,
             id: CAPI.pageId,
             beacon: `${CAPI.beaconURL}/count/pv.gif`,
-            neilsenAPIID: '', // TODO: find the correct value for CAPI.nielsenAPIID,
+            neilsenAPIID: findBySubsection(sectionName).apiID,
             domain: 'amp.theguardian.com',
         };
 
