e237aaa3a1572984e70871d52934f2b4d99f6989
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-07-26 17:07:51 +0100

    Add guard against window access

--- packages/frontend/web/browser/ophan.ts
+++ packages/frontend/web/browser/ophan.ts
@@ -1,3 +1,11 @@
 export const sendOphanPlatformRecord = () => {
-    window.guardian.ophan.record({ platformVariant: 'dotcom-rendering' });
+    if (
+        window.guardian &&
+        window.guardian.ophan &&
+        window.guardian.ophan.record
+    ) {
+        window.guardian.ophan.record({ platformVariant: 'dotcom-rendering' });
+    } else {
+        throw new Error("window.guardian.ophan.record doesn't exist");
+    }
 };
