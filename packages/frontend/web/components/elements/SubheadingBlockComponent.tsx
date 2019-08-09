260a7020d12c0e68a90ae6c17d6526ac5b7094b4
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-08-09 16:01:12 +0100

    Remove unneeded css

--- packages/frontend/web/components/elements/SubheadingBlockComponent.tsx
+++ packages/frontend/web/components/elements/SubheadingBlockComponent.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import { css } from 'emotion';
-import { headline } from '@guardian/pasteup/typography';
 // tslint:disable:react-no-dangerous-html
-
-const heading = css`
-    h2 {
-        ${headline(3)};
-    }
-`;
 
 export const SubheadingBlockComponent: React.FC<{ html: string }> = ({
     html,
@@ -17,7 +9,6 @@ export const SubheadingBlockComponent: React.FC<{ html: string }> = ({
     // There's no way to do `dangerouslySetInnerHTML` on Fragments though, so we may have
     // to try to think of a workaround so that our ads will work properly.
     <div
-        className={heading}
         dangerouslySetInnerHTML={{
             __html: html,
         }}
