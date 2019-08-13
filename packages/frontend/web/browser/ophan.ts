966cc00ecf8d36dc413b081194e155646921c6f0
Nicolas Long <nicolas.long@theguardian.com>
2019-08-13 16:35:24 +0100

    Record (Ophan) server-side experiment variants

--- packages/frontend/web/browser/ophan.ts
+++ packages/frontend/web/browser/ophan.ts
@@ -1,3 +1,12 @@
+interface ABTestRecord {
+    variantName: string;
+    complete: string | boolean;
+}
+
+interface ABTestPayload {
+    abTestRegister: { [key: string]: ABTestRecord };
+}
+
 export const sendOphanPlatformRecord = () => {
     if (
         window.guardian &&
@@ -5,7 +14,27 @@ export const sendOphanPlatformRecord = () => {
         window.guardian.ophan.record
     ) {
         window.guardian.ophan.record({ platformVariant: 'dotcom-rendering' });
+
+        // Record server-side AB test variants (i.e. control or variant)
+        if (window.guardian.config.tests) {
+            const tests = window.guardian.config.tests;
+            window.guardian.ophan.record(abTestPayload(tests));
+        }
     } else {
         throw new Error("window.guardian.ophan.record doesn't exist");
     }
+};
+
+export const abTestPayload = (tests: {
+    [key: string]: string;
+}): ABTestPayload => {
+    const records: { [key: string]: ABTestRecord } = {};
+    Object.keys(tests).forEach(testName => {
+        records[`ab${testName}`] = {
+            variantName: tests[testName],
+            complete: false,
+        };
+    });
+
+    return { abTestRegister: records };
 };
