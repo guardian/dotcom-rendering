64d89d11dd0ca218a142b9e33db3ef197755dced
Rob Phillips <robert.phillips@guardian.co.uk>
2019-08-30 11:49:06 +0100

    adding script from Twitter to render tweet

--- packages/frontend/web/components/elements/TweetBlockComponent.tsx
+++ packages/frontend/web/components/elements/TweetBlockComponent.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import { css } from 'emotion';
-import { body } from '@guardian/pasteup/typography';
-
-// const makeFallback = (html: string): string | null => {
-//     const { window } = new JSDOM(html);
-//     const blockquotes = window.document.getElementsByTagName('blockquote');
-
-//     if (blockquotes.length !== 1) {
-//         return null;
-//     }
-
-//     const q = blockquotes[0];
-//     return q.innerHTML;
-// };
-
-export const tweetStyle = (pillar: Pillar) => css`
-    p {
-        ${body(3)};
-    }
-`;
 
 // tslint:disable:react-no-dangerous-html
 export const TweetBlockComponent: React.FC<{
     element: TweetBlockElement;
     pillar: Pillar;
 }> = ({ element, pillar }) => {
-    return <div dangerouslySetInnerHTML={{ __html: element.html }} />;
+    // return <div dangerouslySetInnerHTML={{ __html: element.html }} />;
+    return (
+        <div>
+            <div dangerouslySetInnerHTML={{ __html: element.html }} />
+            <script
+                async={true}
+                src="https://platform.twitter.com/widgets.js"
+            />
+        </div>
+    );
 };
