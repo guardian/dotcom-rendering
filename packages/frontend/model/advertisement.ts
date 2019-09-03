989e5894bfff82dfd1494565a6694ad8c18b3a72
Pascal Honoré <pascal@alseyn.net>
2019-09-03 11:59:15 +0100

    Activate Ad display on local

--- packages/frontend/model/advertisement.ts
+++ packages/frontend/model/advertisement.ts
@@ -3,7 +3,10 @@ import { AdSlotParameters } from '@frontend/web/components/AdSlot';
 // We are using this function to control the activation of the commercial features
 // Currently it reports that the user has opted in to a 0% AB test.
 export const shouldDisplayAdvertisements = (config: ConfigType) => {
-    return config.abTests.dotcomRenderingAdvertisementsVariant === 'variant';
+    return (
+        config.stage === 'DEV' ||
+        config.abTests.dotcomRenderingAdvertisementsVariant === 'variant'
+    );
 };
 
 export const adSlotParameters = () => {
