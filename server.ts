c060ec726a1ddffd95e2cc92c7ee357a205890d6
Jamie Webb <jamie.webb@guardian.co.uk>
2019-09-06 15:50:38 +0100

    Move out html generation

--- server.ts
+++ server.ts
@@ -17,7 +17,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use("/public", express.static(path.resolve(__dirname, 'public')));
 app.use(compression());
 
-const capiEndpoint = (articleId, key) => `https://content.guardianapis.com/${articleId}?format=json&api-key=${key}&show-elements=all&show-atoms=all&show-rights=all&show-fields=all&show-tags=all&show-blocks=all&show-references=all`;
+// TODO: request less data from capi
+const capiEndpoint = (articleId, key) => `https://content.guardianapis.com/${articleId}?format=json&api-key=${key}&show-elements=all&show-atoms=all&show-fields=all&show-tags=all`;
 
 app.get('/*', (req, res) => {
     try {
@@ -30,32 +31,40 @@ app.get('/*', (req, res) => {
             const articleId = req.params[0] || 'world/2019/sep/03/hong-kong-protests-carrie-lam-denies-she-considered-resigning';
 
             getConfigValue<string>("capi.key")
-              .then(key => fetch(capiEndpoint(articleId, key)))
+              .then(key => fetch(capiEndpoint(articleId, key), {}))
               .then(resp => resp.json())
-              .then(capi => {
-                const { type, fields, elements } = capi.response.content;
-                const mainAssets = elements.filter(elem => elem.relation === 'main' && elem.type === 'image')[0]['assets'];
-                if (type !== 'article') return res.send(`${type} type is not yet supported`);
-                const articleProps = {
-                  ...fields,
-                  ...capi.response.content,
-                  feature: false,
-                  mainAssets
-                };
-                const body = renderToString(React.createElement(Article, articleProps));
-                return res.send(
-                  data.replace(
-                    '<div id="root"></div>',
-                    `<div id="root">${body}</div>`
-                  )
-                )
-              })
-              .catch(error => res.send(error))
+              .then(capi => res.send(generateArticleHtml(capi, data)))
+              .catch(error => res.send(`<pre>${error}</pre>`))
           })
     } catch (e) {
         res.status(500).send(`<pre>${e.stack}</pre>`);
     }
 });
+
+const generateArticleHtml = (capi, data): string => {
+    const { type, fields, elements } = capi.response.content;
+
+    if (type !== 'article') return `${type} type is not yet supported`;
+    if (fields.displayHint === 'immersive') return `immersive displayHint is not yet supported`;
+    if ('starRating' in fields) return `Reviews not yet supported`;
+
+    const mainImages = elements.filter(elem => elem.relation === 'main' && elem.type === 'image');
+    const mainAssets = mainImages.length ? mainImages[0]['assets'] : null;
+
+    const articleProps = {
+      ...fields,
+      ...capi.response.content,
+      feature: false,
+      mainAssets
+    };
+
+    const body = renderToString(React.createElement(Article, articleProps));
+
+    return data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${body}</div>`
+      )
+}
 
 app.use((err: any, req: any, res: any, next: any) => {
     res.status(500).send(`<pre>${err.stack}</pre>`);
