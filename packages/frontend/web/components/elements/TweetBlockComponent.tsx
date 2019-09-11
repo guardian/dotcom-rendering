744b5ef481c9d5ea914c877ed1ddb67c056b6036
Rob Phillips <robert.phillips@guardian.co.uk>
2019-09-11 21:21:08 +0100

    Adding NoJS rendering of tweet

--- packages/frontend/web/components/elements/TweetBlockComponent.tsx
+++ packages/frontend/web/components/elements/TweetBlockComponent.tsx
@@ -1,4 +1,26 @@
 import React from 'react';
+import { css } from 'emotion';
+import { palette } from '@guardian/pasteup/palette';
+import { body } from '@guardian/pasteup/typography';
+const noJSStyling = css`
+    .twitter-tweet :not(.twitter-tweet-rendered) {
+        /* // We're going to style this like the upgraded embed */
+        border: 1px solid ${palette.neutral[86]};
+        border-radius: 4px;
+        padding: 20px;
+        /* End tweet embed styles  */
+
+        display: inline-block;
+        margin: 0;
+        margin-bottom: $gs-baseline;
+        width: 100%;
+        ${body(1)};
+    }
+
+    .twitter-tweet p {
+        padding-bottom: 10px;
+    }
+`;
 
 // tslint:disable:react-no-dangerous-html
 export const TweetBlockComponent: React.FC<{
@@ -6,7 +28,10 @@ export const TweetBlockComponent: React.FC<{
 }> = ({ element }) => {
     return (
         <div>
-            <div dangerouslySetInnerHTML={{ __html: element.html }} />
+            <div
+                className={noJSStyling}
+                dangerouslySetInnerHTML={{ __html: element.html }}
+            />
             <script
                 async={true}
                 src="https://platform.twitter.com/widgets.js"
