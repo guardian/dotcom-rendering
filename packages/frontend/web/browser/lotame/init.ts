f05eca7342a78ea748736b726927c841e60d0b80
Pascal Honoré <pascal@alseyn.net>
2019-09-11 11:52:06 +0100

    Refactoring for code climate

--- packages/frontend/web/browser/lotame/init.ts
+++ packages/frontend/web/browser/lotame/init.ts
@@ -1,18 +1,17 @@
 import { startup } from '@frontend/web/browser/startup';
 
+const shouldServeLotame = (window: Window) => {
+    const geo = window.localStorage.getItem('gu.geolocation');
+    if (geo === null) {
+        return false;
+    }
+    return !['US', 'CA', 'AU', 'NZ'].includes(JSON.parse(geo).value);
+};
+
 const init = (): Promise<void> => {
     try {
         ((document, window) => {
-            function shouldServeLotame() {
-                const geo = window.localStorage.getItem('gu.geolocation');
-                if (geo === null) {
-                    return false;
-                }
-                return !['US', 'CA', 'AU', 'NZ'].includes(
-                    JSON.parse(geo).value,
-                );
-            }
-            if (shouldServeLotame()) {
+            if (shouldServeLotame(window)) {
                 const script = document.createElement('script');
                 script.src = 'https://tags.crwdcntrl.net/c/12666/cc.js';
                 document.body.appendChild(script);
