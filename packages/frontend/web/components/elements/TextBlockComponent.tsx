4cac148160c25a94800c295f7ba225a4ee0fea0f
Nicolas Long <nicolaslong@gmail.com>
2019-09-18 16:16:37 +0100

    Avoid wrapper span for text block elements
    This results in cleaner markup and avoids having to modify
    commercial.js which expects p's at the top-level rather
    than span elements.

--- packages/frontend/web/components/elements/TextBlockComponent.tsx
+++ packages/frontend/web/components/elements/TextBlockComponent.tsx
@@ -4,17 +4,47 @@ import { body } from '@guardian/pasteup/typography';
 // tslint:disable:react-no-dangerous-html
 
 const para = css`
+    margin-bottom: 16px;
+    ${body(2)};
+`;
+
+const innerPara = css`
     p {
-        margin-bottom: 16px;
-        ${body(2)};
+        ${para}
     }
 `;
 
-export const TextBlockComponent: React.FC<{ html: string }> = ({ html }) => (
-    <span
-        className={para}
-        dangerouslySetInnerHTML={{
-            __html: html,
-        }}
-    />
-);
+export const TextBlockComponent: React.FC<{ html: string }> = ({ html }) => {
+    const prefix = '<p>';
+    const suffix = '</p>';
+
+    // Ideally we want to want to avoid an unnecessary 'span' wrapper,
+    // which also will cause issues with ads (spacefinder rules). React
+    // requires a wrapping element for dangerouslySetInnerHTML so we
+    // strip the original p markup and rewrap. The fallback case is
+    // added for safety but should never happen. If it does happen
+    // though it will cause issues with commercial JS which expects
+    // paras to be top-level.
+    if (html.startsWith(prefix) && html.endsWith(suffix)) {
+        return (
+            <p
+                className={para}
+                dangerouslySetInnerHTML={{
+                    __html: html.slice(
+                        prefix.length,
+                        html.length - suffix.length,
+                    ),
+                }}
+            />
+        );
+    }
+
+    return (
+        <span
+            className={innerPara}
+            dangerouslySetInnerHTML={{
+                __html: html,
+            }}
+        />
+    );
+};
