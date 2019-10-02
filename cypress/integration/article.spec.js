36081a381c37a166bb1e239e8d4a094878ff7804
Oliver Lloyd <email@oliverlloyd.com>
2019-10-02 15:33:26 +0100

    Install Percy

--- cypress/integration/article.spec.js
+++ cypress/integration/article.spec.js
@@ -3,5 +3,7 @@ describe('Basic article', function() {
         cy.visit(
             'http://localhost:3030/Article?url=https://www.theguardian.com/football/2019/jun/04/juventus-interested-paul-pogba-manchester-united',
         );
+
+        cy.percySnapshot();
     });
 });
