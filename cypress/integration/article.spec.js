6f3cd24b616922f25c34238ccdad7e1e15b75d55
Oliver Lloyd <email@oliverlloyd.com>
2019-10-02 17:14:26 +0100

    Changed describe text

--- cypress/integration/article.spec.js
+++ cypress/integration/article.spec.js
@@ -1,4 +1,4 @@
-describe('Basic article', function() {
+describe('Page rendering', function() {
     it('should load a basic article', function() {
         cy.visit(
             'http://localhost:3030/Article?url=https://www.theguardian.com/football/2019/jun/04/juventus-interested-paul-pogba-manchester-united',
