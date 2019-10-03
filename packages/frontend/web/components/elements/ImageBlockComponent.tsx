18e9e29b2cce2ddfbaf084854fefbe244b483011
Oliver Lloyd <email@oliverlloyd.com>
2019-10-03 10:09:09 +0100

    Extracted css

--- packages/frontend/web/components/elements/ImageBlockComponent.tsx
+++ packages/frontend/web/components/elements/ImageBlockComponent.tsx
@@ -61,89 +61,98 @@ const getFallback: (imageSources: ImageSource[]) => string = imageSources => {
     return bestFor(300, inlineSrcSets).src;
 };
 
+const imageCss = {
+    inline: css`
+        margin-top: 16px;
+        margin-bottom: 12px;
+    `,
+
+    supporting: css`
+        ${tablet} {
+            position: relative;
+            float: left;
+            width: 300px;
+            margin-top: 6px;
+            margin-bottom: 12px;
+            margin-right: 20px;
+            line-height: 0;
+        }
+        ${leftCol} {
+            margin-left: -160px;
+        }
+        ${wide} {
+            width: 380px;
+            margin-left: -240px;
+        }
+    `,
+
+    // TODO: immersive is pending review of different article types
+    immersive: css``,
+
+    // TODO: showcase is only a partial implementation as sometimes showcase images
+    //       appear differently based on where they appear in the article
+    //       See top image for: https://www.theguardian.com/stage/2019/oct/01/hammed-animashaun-nick-hytner-dream-master-harold-and-the-boys-national-theatre-london
+    showcase: css`
+        position: relative;
+        margin-top: 16px;
+        margin-bottom: 12px;
+        ${leftCol} {
+            position: relative;
+            margin-bottom: 16px;
+            margin-left: -160px;
+        }
+        ${wide} {
+            margin-left: -240px;
+        }
+    `,
+
+    thumbnail: css`
+        float: left;
+        clear: left;
+        margin-bottom: 0;
+        width: 120px;
+        margin-right: 20px;
+        margin-top: 6px;
+        ${tablet} {
+            margin-right: 20px;
+        }
+        ${wide} {
+            margin-left: -160px;
+        }
+        ${wide} {
+            margin-left: -240px;
+        }
+        ${leftCol} {
+            margin-left: -160px;
+        }
+        ${leftCol} {
+            position: relative;
+        }
+        ${tablet} {
+            width: 140px;
+        }
+    `,
+
+    // TODO:
+    'half-width': css``,
+};
+
 const decidePosition = (role: RoleType) => {
     switch (role) {
-        case 'immersive':
-            return css``; // TODO pending review of different article types
-            break;
-        case 'supporting':
-            return css`
-                ${tablet} {
-                    position: relative;
-                    float: left;
-                    width: 300px;
-                    margin-top: 6px;
-                    margin-bottom: 12px;
-                    margin-right: 20px;
-                    line-height: 0;
-                }
-                ${leftCol} {
-                    margin-left: -160px;
-                }
-                ${wide} {
-                    width: 380px;
-                    margin-left: -240px;
-                }
-            `;
-            break;
-        case 'showcase':
-            // TODO: This is only a partial implementation as sometimes showcase images
-            //       appear differently based on where they appear in the article
-            //       See top image for: https://www.theguardian.com/stage/2019/oct/01/hammed-animashaun-nick-hytner-dream-master-harold-and-the-boys-national-theatre-london
-            return css`
-                position: relative;
-                margin-top: 16px;
-                margin-bottom: 12px;
-                ${leftCol} {
-                    position: relative;
-                    margin-bottom: 16px;
-                    margin-left: -160px;
-                }
-                ${wide} {
-                    margin-left: -240px;
-                }
-            `;
-            break;
         case 'inline':
-            return css`
-                margin-top: 16px;
-                margin-bottom: 12px;
-            `;
-            break;
+            return imageCss.inline;
+        case 'supporting':
+            return imageCss.supporting;
+        case 'immersive':
+            return imageCss.immersive;
+        case 'showcase':
+            return imageCss.showcase;
         case 'thumbnail':
-            return css`
-                float: left;
-                clear: left;
-                margin-bottom: 0;
-                width: 120px;
-                margin-right: 20px;
-                margin-top: 6px;
-
-                ${tablet} {
-                    margin-right: 20px;
-                }
-                ${wide} {
-                    margin-left: -160px;
-                }
-                ${wide} {
-                    margin-left: -240px;
-                }
-                ${leftCol} {
-                    margin-left: -160px;
-                }
-                ${leftCol} {
-                    position: relative;
-                }
-                ${tablet} {
-                    width: 140px;
-                }
-            `;
-            break;
+            return imageCss.thumbnail;
         case 'half-width':
-            return css``; // TODO
-            break;
+            return imageCss['half-width'];
         default:
-            return css``;
+            return imageCss.inline;
     }
 };
 
