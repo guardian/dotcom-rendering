a987f6a4cd241696d2c38bffcb4abb45ef3d1948
Oliver Lloyd <email@oliverlloyd.com>
2019-10-08 16:44:29 +0100

    Refactored to use designTypes.ts. Includes Interview example usage

---
+++ packages/frontend/web/pages/Interview.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { MostViewed } from '@frontend/web/components/MostViewed';
+import { Header } from '@frontend/web/components/Header/Header';
+import { Footer } from '@frontend/web/components/Footer';
+import { Content } from '@frontend/web/components/Content';
+import { SubNav } from '@frontend/web/components/Header/Nav/SubNav/SubNav';
+import { CookieBanner } from '@frontend/web/components/CookieBanner';
+
+export const Interview: React.FC<{
+    data: ArticleProps;
+}> = ({ data }) => (
+    <div>
+        <Header
+            nav={data.NAV}
+            pillar={data.CAPI.pillar}
+            edition={data.CAPI.editionId}
+            config={data.config}
+            isAdFreeUser={data.CAPI.isAdFreeUser}
+            shouldHideAds={data.CAPI.shouldHideAds}
+        />
+
+        <Content CAPI={data.CAPI} config={data.config} />
+
+        <MostViewed sectionName={data.CAPI.sectionName} config={data.config} />
+
+        <SubNav
+            subnav={data.NAV.subNavSections}
+            pillar={data.CAPI.pillar}
+            currentNavLink={data.NAV.currentNavLink}
+        />
+
+        <Footer
+            nav={data.NAV}
+            edition={data.CAPI.editionId}
+            pageFooter={data.CAPI.pageFooter}
+            pillar={data.CAPI.pillar}
+            pillars={data.NAV.pillars}
+        />
+
+        <CookieBanner />
+    </div>
+);
--- packages/frontend/web/server/DecidePage.tsx
+++ packages/frontend/web/server/DecidePage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Article } from '../pages/Article';
+import { Interview } from '../pages/Interview';
+import { designTypeDefault } from '@frontend/lib/designTypes';
 
 type Props = {
     designType: DesignType;
@@ -7,40 +9,11 @@ type Props = {
 };
 
 export const DecidePage = ({ designType, data }: Props) => {
-    switch (designType) {
-        case 'Article':
-            return <Article data={data} />;
-        case 'Immersive':
-            return <Article data={data} />;
-        case 'Media':
-            return <Article data={data} />;
-        case 'Review':
-            return <Article data={data} />;
-        case 'Analysis':
-            return <Article data={data} />;
-        case 'Comment':
-            return <Article data={data} />;
-        case 'Feature':
-            return <Article data={data} />;
-        case 'Live':
-            return <Article data={data} />;
-        case 'SpecialReport':
-            return <Article data={data} />;
-        case 'Recipe':
-            return <Article data={data} />;
-        case 'MatchReport':
-            return <Article data={data} />;
-        case 'Interview':
-            return <Article data={data} />;
-        case 'GuardianView':
-            return <Article data={data} />;
-        case 'GuardianLabs':
-            return <Article data={data} />;
-        case 'Quiz':
-            return <Article data={data} />;
-        case 'AdvertisementFeature':
-            return <Article data={data} />;
-        default:
-            return <Article data={data} />;
-    }
+    const designTypePage: DesignTypesObj = designTypeDefault(
+        <Article data={data} />,
+    );
+
+    designTypePage.Interview = <Interview data={data} />;
+
+    return designTypePage[designType];
 };
