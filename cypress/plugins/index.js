8328a36f3e9805edd8adeb9c6d1107c6e4a97c11
Oliver Lloyd <email@oliverlloyd.com>
2019-10-10 15:19:27 +0100

    Install Percy

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,8 +10,8 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+let percyHealthCheck = require('@percy/cypress/task');
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+    on('task', percyHealthCheck);
+};
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+import '@percy/cypress';
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
