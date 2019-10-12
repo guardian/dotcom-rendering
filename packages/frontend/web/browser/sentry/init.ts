b31df2f1cbb1441fa4cc8c175f82d3ccea9824d5
Oliver Lloyd <email@oliverlloyd.com>
2019-10-12 16:59:20 +0100

    Use beforeSend to filter events

--- packages/frontend/web/browser/sentry/init.ts
+++ packages/frontend/web/browser/sentry/init.ts
@@ -1,10 +1,18 @@
 import * as Sentry from '@sentry/browser';
 import { startup } from '@frontend/web/browser/startup';
 
+import { isAdBlockInUse } from './detectAdBlocker';
+
+let adBlockInUse = false; // Adblock checking is async so we assume adblock is off until we know it's not
+isAdBlockInUse().then(isInUse => (adBlockInUse = isInUse));
+
+const { editionLongForm, contentType } = window.guardian.app.data.CAPI;
+
 // Only send 10% of errors (this is set to 10% here rather than the normal 1% so we get good
 // feedback early on). TODO: This should be reduced to 1% once the site is fully deployed.
 const SAMPLE_RATE = 0.1;
 
+// Only send errors matching these regexes
 const whitelistUrls = [
     /webpack-internal/,
     /localhost/,
@@ -12,6 +20,7 @@ const whitelistUrls = [
     /ophan\.co\.uk/,
 ];
 
+// Ignore these errors
 const ignoreErrors = [
     // https://docs.sentry.io/platforms/javascript/#decluttering-sentry
     "Can't execute code from a freed script",
@@ -23,6 +32,11 @@ const ignoreErrors = [
 ];
 
 const init = () => {
+    const {
+        isDev,
+        switches: { enableSentryReporting },
+    } = window.guardian.app.data.config;
+
     return Promise.resolve().then(() => {
         try {
             Sentry.init({
@@ -33,6 +47,15 @@ const init = () => {
                 // release: 'release',
                 // environment: 'environment',
                 sampleRate: SAMPLE_RATE,
+                beforeSend(event) {
+                    // Skip sending events in certain situations
+                    const dontSend =
+                        adBlockInUse || isDev || !enableSentryReporting;
+                    if (dontSend) return null;
+                    return event;
+                },
+            });
+
             Sentry.configureScope(function(scope) {
                 scope.setTag('edition', editionLongForm);
                 scope.setTag('contentType', contentType);
