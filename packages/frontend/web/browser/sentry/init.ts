320b30d87ed69059a547b91a6eaba4298d03810e
Oliver Lloyd <email@oliverlloyd.com>
2019-10-12 12:59:17 +0100

    Created sentry init file

---
+++ packages/frontend/web/browser/sentry/init.ts
@@ -0,0 +1,47 @@
+import * as Sentry from '@sentry/browser';
+import { startup } from '@frontend/web/browser/startup';
+
+// Only send 10% of errors (this is set to 10% here rather than the normal 1% so we get good
+// feedback early on). TODO: This should be reduced to 1% once the site is fully deployed.
+const SAMPLE_RATE = 0.1;
+
+const whitelistUrls = [
+    /webpack-internal/,
+    /localhost/,
+    /assets\.guim\.co\.uk/,
+    /ophan\.co\.uk/,
+];
+
+const ignoreErrors = [
+    // https://docs.sentry.io/platforms/javascript/#decluttering-sentry
+    "Can't execute code from a freed script",
+    /InvalidStateError/gi,
+    /Fetch error:/gi,
+    'Network request failed',
+    'This video is no longer available.',
+    'UnknownError',
+];
+
+const init = () => {
+    return Promise.resolve().then(() => {
+        try {
+            Sentry.init({
+                ignoreErrors,
+                whitelistUrls,
+                dsn:
+                    'https://1937ab71c8804b2b8438178dfdd6468f@sentry.io/1377847',
+                // release: 'release',
+                // environment: 'environment',
+                sampleRate: SAMPLE_RATE,
+            });
+        } catch {
+            /**
+             * Sentry will have reported any unhandled promise
+             * rejections from this chain so return here.
+             */
+            return;
+        }
+    });
+};
+
+startup('sentry', null, init);
