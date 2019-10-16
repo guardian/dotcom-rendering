a74d32ae1321c5108f92ed6c1fac12b077f7e23c
Jamie Webb <jamie.webb@guardian.co.uk>
2019-10-16 10:42:38 +0100

    correct test case

--- src/server.test.ts
+++ src/server.test.ts
@@ -3,9 +3,9 @@ import { capiEndpoint } from './server';
 describe('server logic runs as expected', () => {
     it('generates capi endpoint url', () => {
         const articleId = 'cities/2019/sep/13/reclaimed-lakes-and-giant-airports-how-mexico-city-might-have-looked';
-        const key = 'FAKE_KEY';
+        const key = 'TEST_KEY';
         const capiUrl = capiEndpoint(articleId, key);
 
-        expect(capiUrl).toEqual('')
+        expect(capiUrl).toEqual('https://content.guardianapis.com/cities/2019/sep/13/reclaimed-lakes-and-giant-airports-how-mexico-city-might-have-looked?format=json&api-key=TEST_KEY&show-elements=all&show-atoms=all&show-fields=all&show-tags=all&show-blocks=all')
     });
 });
