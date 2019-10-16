02e4fb2594f70382f17c5ec8f6d66a681cbf5bbc
Jamie Webb <jamie.webb@guardian.co.uk>
2019-10-16 10:35:53 +0100

    more descriptive test case

--- src/server.test.ts
+++ src/server.test.ts
@@ -3,7 +3,7 @@ import { capiEndpoint } from './server';
 describe('server logic runs as expected', () => {
     it('generates capi endpoint url', () => {
         const articleId = 'cities/2019/sep/13/reclaimed-lakes-and-giant-airports-how-mexico-city-might-have-looked';
-        const key = 'XJSYDKHDIE78EJIKW';
+        const key = 'FAKE_KEY';
         const capiUrl = capiEndpoint(articleId, key);
 
         expect(capiUrl).toEqual('')
