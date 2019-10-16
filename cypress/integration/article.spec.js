c45847031d61dcccfbca629df24bc7dafa1a36aa
Oliver Lloyd <email@oliverlloyd.com>
2019-10-16 18:05:58 +0100

    Add additional articles + AMP support

--- cypress/integration/article.spec.js
+++ cypress/integration/article.spec.js
@@ -1,17 +1,31 @@
 import { getPolyfill } from '../lib/polyfill';
 import { visitOptions } from '../lib/config';
-import { articles } from '../lib/articles.js';
+import { articles, AMPArticles } from '../lib/articles.js';
 
 describe('Page rendering', function() {
     beforeEach(getPolyfill);
 
-    it('should load a basic article', function() {
-        articles.map(article => {
+    it('should load WEB articles', function() {
+        articles.map((article, index) => {
             cy.visit(
-                `http://localhost:3030/Article?url=${article.url}#noads`,
+                `http://localhost:3030/Article?url=${article.url}`,
                 visitOptions,
             );
-            cy.percySnapshot();
+            cy.percySnapshot(
+                `WEB-${article.pillar}-${article.designType}-${index}`,
+            );
+        });
+    });
+
+    it('should load AMP articles', function() {
+        AMPArticles.map((article, index) => {
+            cy.visit(
+                `http://localhost:3030/AMPArticle?url=${article.url}`,
+                visitOptions,
+            );
+            cy.percySnapshot(
+                `AMP-${article.pillar}-${article.designType}-${index}`,
+            );
         });
     });
 });
