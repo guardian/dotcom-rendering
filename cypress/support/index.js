d49c5e5522d24468a6f0f46d574d5fb47d4996df
Oliver Lloyd <email@oliverlloyd.com>
2019-10-17 16:35:04 +0100

    Add e2e tests and split them out into separate folders and cmds

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,25 +18,3 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-beforeEach(() => {
-    cy.server();
-    // Mock share count
-    cy.route({
-        method: 'GET',
-        url: '/sharecount/**',
-        response: 'fixture:shareCount.json',
-    });
-    // Mock most-read
-    cy.route({
-        method: 'GET',
-        url: '/most-read/**',
-        response: 'fixture:mostRead.json',
-    });
-    // Mock most-read
-    cy.route({
-        method: 'GET',
-        url: '/embed/card/**',
-        response: 'fixture:richLink.json',
-    });
-});
