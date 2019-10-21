c13255d3d26a2c0f45683d0338a8fb1e5d19514e
Jamie Webb <jamie.webb@guardian.co.uk>
2019-10-21 10:53:08 +0100

    render interactive blocks

--- src/components/news/ArticleBody.tsx
+++ src/components/news/ArticleBody.tsx
@@ -14,7 +14,8 @@ const ArticleBodyStyles = (pillarStyles: PillarStyles): SerializedStyles => css`
         margin: 8px 24px 8px 0;
     }
 
-    p {
+    p,
+    .interactive {
         ${sidePadding}
     }
 
