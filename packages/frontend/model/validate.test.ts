c3377a20fc1b26370425a46232f22e7979293cbb
Oliver Lloyd <email@oliverlloyd.com>
2019-10-24 12:01:42 +0100

    Add test to check we throw locally

--- packages/frontend/model/validate.test.ts
+++ packages/frontend/model/validate.test.ts
@@ -2,9 +2,28 @@ import { validateAsCAPIType } from './validate';
 import { CAPI } from '@root/fixtures/CAPI';
 
 describe('validate', () => {
-    it('does not throw an error on invalid data', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+        jest.resetModules(); // Clear cache
+        process.env = { ...OLD_ENV };
+        delete process.env.NODE_ENV;
+    });
+
+    afterEach(() => {
+        process.env = OLD_ENV;
+    });
+
+    it('does not throw an error in production on invalid data', () => {
+        process.env.NODE_ENV = 'production';
         const data = { foo: 'bar' };
         expect(() => validateAsCAPIType(data)).not.toThrowError(TypeError);
+    });
+
+    it('does throw an error in development on invalid data', () => {
+        process.env.NODE_ENV = 'development';
+        const data = { foo: 'bar' };
+        expect(() => validateAsCAPIType(data)).toThrowError(TypeError);
     });
 
     it('confirm valid data', () => {
