f3e07a3f2af81fbfe4d4d8e0ff127693e2ebc9a4
Oliver Lloyd <email@oliverlloyd.com>
2019-10-28 09:49:28 +0000

    Include AMP tests for `make cypress` command

--- cypress/integration/e2e/article.e2e.spec.js
+++ cypress/integration/e2e/article.e2e.spec.js
@@ -1,40 +1,57 @@
 import { getPolyfill } from '../../lib/polyfill';
 import { visitOptions } from '../../lib/config';
-import { articles } from '../../lib/articles.js';
+import { articles, AMPArticles } from '../../lib/articles.js';
 import { setupApiRoutes } from '../../lib/apiRoutes.js';
 
 describe('E2E Page rendering', function() {
     beforeEach(getPolyfill);
     beforeEach(setupApiRoutes);
 
-    articles.map((article, index) => {
-        const { url, pillar, designType } = article;
-        it(`It should load ${designType} articles under the ${pillar} pillar`, function() {
-            cy.visit(`Article?url=${url}`, visitOptions);
-            cy.contains('Sign in');
+    describe('for WEB', function() {
+        articles.map((article, index) => {
+            const { url, pillar, designType } = article;
 
-            cy.wait('@getMostRead').then(xhr => {
-                expect(xhr.response.body.length).to.be.at.least(1);
-                expect(xhr.status).to.be.equal(200);
+            it(`It should load ${designType} articles under the ${pillar} pillar`, function() {
+                cy.visit(`Article?url=${url}`, visitOptions);
+                cy.contains('Sign in');
 
-                cy.contains('Most popular');
-            });
-
-            cy.wait('@getShareCount').then(xhr => {
-                expect(xhr.status).to.be.equal(200);
-                expect(xhr.response.body).to.have.property('path');
-                expect(xhr.response.body).to.have.property('refreshStatus');
-                expect(xhr.response.body)
-                    .to.have.property('share_count')
-                    .that.is.a('number');
-            });
-
-            if (article.hasRichLinks) {
-                cy.wait('@getRichLinks').then(xhr => {
+                cy.wait('@getMostRead').then(xhr => {
+                    expect(xhr.response.body.length).to.be.at.least(1);
                     expect(xhr.status).to.be.equal(200);
-                    cy.contains('Read more');
+
+                    cy.contains('Most popular');
                 });
-            }
+
+                cy.wait('@getShareCount').then(xhr => {
+                    expect(xhr.status).to.be.equal(200);
+                    expect(xhr.response.body).to.have.property('path');
+                    expect(xhr.response.body).to.have.property('refreshStatus');
+                    expect(xhr.response.body)
+                        .to.have.property('share_count')
+                        .that.is.a('number');
+                });
+
+                if (article.hasRichLinks) {
+                    cy.wait('@getRichLinks').then(xhr => {
+                        expect(xhr.status).to.be.equal(200);
+                        cy.contains('Read more');
+                    });
+                }
+            });
+        });
+    });
+
+    describe('for AMP', function() {
+        AMPArticles.map((article, index) => {
+            const { url, pillar, designType } = article;
+
+            it(`It should load ${designType} articles under the ${pillar} pillar`, function() {
+                // Prevent the Privacy consent banner from obscuring snapshots
+                cy.setCookie('GU_TK', 'true');
+
+                cy.visit(`AMPArticle?url=${url}`, visitOptions);
+                cy.contains('Opinion');
+            });
         });
     });
 });
