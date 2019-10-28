bd3a9e8c7e838f3dcd4cedcc147516c194409755
Jamie Webb <jamie.webb@guardian.co.uk>
2019-10-28 12:24:12 +0000

    typesctipt and eslint warnings

--- src/ads.ts
+++ src/ads.ts
@@ -2,19 +2,20 @@ import React from 'react';
 
 const h = React.createElement;
 
-function insertAdPlaceholders(reactNodes: any) {
+function insertAdPlaceholders(reactNodes: React.ReactNode[]): React.ReactNode[] {
     // Insert ad placeholders after the following paragraph tags
     const adIndexes = [3, 9];
 
     const ad = h('div', { className: 'ad-placeholder' }, "this is an advert. buy now.");
 
-    const flattenedNodes = reactNodes
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const flattenedNodes: any = reactNodes
         .flat()
-        .map((node: any, index: any) => ({ ...node, index }));
+        .map((node: { type: string }, index: number) => ({ ...node, index }));
 
-    const paragraphs = flattenedNodes.filter((node: { type: string; }) => node.type == 'p');
+    const paragraphs = flattenedNodes.filter((node: { type: string }) => node.type === 'p');
     
-    paragraphs.forEach((p: { index: number; }, i: number) => {
+    paragraphs.forEach((p: { index: number }, i: number) => {
         adIndexes.forEach((adIndex, idx) => {
             if (i === adIndex) {
                 flattenedNodes.splice(p.index + idx, 0, ad);
