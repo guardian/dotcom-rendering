8e184fe1ddde8b6a3f8700a90d559d6d16947a73
Jamie Webb <jamie.webb@guardian.co.uk>
2019-11-07 11:51:34 +0000

    update ad placeholder margin

--- src/styles.ts
+++ src/styles.ts
@@ -302,8 +302,7 @@ export const adStyles = css`
         }
 
         ${until.phablet} {
-            margin-left: ${basePx(-1)};
-            margin-right: ${basePx(-1)};
+            margin: 1em ${basePx(-1)};
         }
 
         ${from.desktop} {
