776dcf0a2a13ee3fff60098265efdb735627af3b
Oliver Lloyd <email@oliverlloyd.com>
2019-11-13 16:26:56 +0000

    Enhance QuoteIcon to accept size prop

--- src/web/components/QuoteIcon.tsx
+++ src/web/components/QuoteIcon.tsx
@@ -10,12 +10,48 @@ const quoteStyles = (colour?: string) => css`
     fill: ${colour && colour};
 `;
 
-type Props = {
-    colour?: string;
+const sizeStyles = (size: 'tiny' | 'xxsmall' | 'xsmall') => {
+    switch (size) {
+        case 'tiny':
+            return css`
+                svg {
+                    height: 1rem;
+                    width: 0.5rem;
+                }
+            `;
+        case 'xxsmall':
+            return css`
+                margin-right: 0.25rem;
+                svg {
+                    height: 1.25rem;
+                    width: 0.675rem;
+                }
+            `;
+        case 'xsmall':
+            return css`
+                margin-right: 0.5rem;
+                svg {
+                    height: 1.5rem;
+                    width: 0.81rem;
+                }
+            `;
+        default:
+            return css`
+                svg {
+                    height: 1.25rem;
+                    width: 0.675rem;
+                }
+            `;
+    }
 };
 
-export const QuoteIcon = ({ colour }: Props) => (
-    <>
+type Props = {
+    colour?: string;
+    size?: 'tiny' | 'xxsmall' | 'xsmall';
+};
+
+export const QuoteIcon = ({ colour, size = 'xxsmall' }: Props) => (
+    <span className={sizeStyles(size)}>
         <svg
             width="70"
             height="49"
@@ -25,5 +61,5 @@ export const QuoteIcon = ({ colour }: Props) => (
         >
             <path d="M69.587.9c-1.842 15.556-3.89 31.316-4.708 48.1H37.043c3.07-16.784 8.391-32.544 17.602-48.1h14.942zM32.949.9c-2.047 15.556-4.094 31.316-4.912 48.1H.2C3.066 32.216 8.592 16.456 17.598.9h15.35z" />
         </svg>
-    </>
+    </span>
 );
