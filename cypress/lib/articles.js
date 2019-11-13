7840f0461439bc208abf99ee73f994e2a33b2979
Oliver Lloyd <email@oliverlloyd.com>
2019-11-13 10:20:06 +0000

    Fix failing test caused by merge

--- cypress/lib/articles.js
+++ cypress/lib/articles.js
@@ -18,15 +18,14 @@ export const articles = [
             'https://www.theguardian.com/environment/2018/dec/21/from-spectacular-orchids-to-towering-trees-2018s-top-new-plant-discoveries',
         pillar: 'news',
         designType: 'Feature',
-        hasRichLinks: true,
+        hasRichLinks: false,
     },
     {
         url:
-          'https://www.theguardian.com/politics/2019/nov/11/uk-parties-struggle-to-turn-whatsapp-into-political-weapon',
+            'https://www.theguardian.com/politics/2019/nov/11/uk-parties-struggle-to-turn-whatsapp-into-political-weapon',
         pillar: 'news',
         designType: 'Article',
         hasRichLinks: true,
-      
     },
     {
         url:
