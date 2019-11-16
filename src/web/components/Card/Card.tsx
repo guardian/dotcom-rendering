70a9518da72966decc74a84d70c02167fbf17c55
Oliver Lloyd <email@oliverlloyd.com>
2019-11-16 11:32:42 +0000

    Created CardListItem

--- src/web/components/Card/Card.tsx
+++ src/web/components/Card/Card.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { css } from 'emotion';
 
 import { palette } from '@guardian/src-foundations';
 
@@ -9,26 +8,10 @@ import { ImageRightLayout } from './ImageRightLayout';
 import { ImageWrapper } from './ImageWrapper';
 import { TopBar } from './TopBar';
 import { CardLink } from './CardLink';
+import { CardListItem } from './CardListItem';
 
 import { SmallHeadline } from '@frontend/web/components/SmallHeadline';
 import { ImageComponent } from '@frontend/web/components/elements/ImageComponent';
-
-const listStyles = css`
-    /* Here we ensure the card stretches to fill the containing space */
-    flex-basis: 100%;
-    /* display block here prevents that annoying bar at the bottom of the image problem
-    we might be able to remove this if this is already set in global css */
-    display: flex;
-
-    /* We absolutely position the 1 pixel top bar in
-       the card so this is required here */
-    position: relative;
-
-    /* Set spacing margins on the li element */
-    margin-left: 10px;
-    margin-right: 10px;
-    margin-bottom: 12px;
-`;
 
 const decideLayout = (image?: CardImageType) => {
     if (!image) {
@@ -70,7 +53,7 @@ export const Card = ({
 }: Props) => {
     const Layout = decideLayout(image);
     return (
-        <li className={listStyles}>
+        <CardListItem>
             <CardLink
                 linkTo={linkTo}
                 backgroundColour={palette.neutral[93]}
@@ -99,6 +82,6 @@ export const Card = ({
                     </Layout>
                 </TopBar>
             </CardLink>
-        </li>
+        </CardListItem>
     );
 };
