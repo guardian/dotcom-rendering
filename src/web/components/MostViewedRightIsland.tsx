0f472884d91228c64d61a4bb05570a8903e711be
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2019-11-21 14:30:28 +0000

    Rename class name

--- src/web/components/MostViewedRightIsland.tsx
+++ src/web/components/MostViewedRightIsland.tsx
@@ -3,12 +3,12 @@ import { css } from 'emotion';
 
 // Styling the data island root so it stetches to cover the full height available in the container.
 // Requires us to subtract the height of its sibbling in the container (StickyAd).
-const wrapperStyles = css`
+const stretchWrapperHeight = css`
     display: flex;
     flex-direction: column;
     height: calc(100% - 1059px);
 `;
 
 export const MostViewedRightIsland = () => (
-    <div data-island="most-viewed-right" className={wrapperStyles} />
+    <div data-island="most-viewed-right" className={stretchWrapperHeight} />
 );
