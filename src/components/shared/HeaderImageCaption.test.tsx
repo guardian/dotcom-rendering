c458f0a6a8e91edc3a8da33ed260c1076ebd1756
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-05 11:19:12 +0000

    use exported id in test

--- src/components/shared/HeaderImageCaption.test.tsx
+++ src/components/shared/HeaderImageCaption.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import HeaderImageCaption from 'components/shared/headerImageCaption';
+import HeaderImageCaption, { captionId } from 'components/shared/headerImageCaption';
 
 configure({ adapter: new Adapter() });
 
 describe('HeaderImageCaption component renders as expected', () => {
     it('Caption formatted correctly', () => {
         const headerImageCaption = shallow(<HeaderImageCaption caption="Here is a caption." credit="Photograph: cameraman"/>)
-        expect(headerImageCaption.find('#header-image-caption').text()).toBe("Here is a caption. Photograph: cameraman")
+        expect(headerImageCaption.find(`#${captionId}`).text()).toBe("Here is a caption. Photograph: cameraman")
     })
 });
