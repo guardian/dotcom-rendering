37f5c159d2241f7637c30ef69e3782390845aaf1
Oliver Lloyd <email@oliverlloyd.com>
2019-12-09 16:51:48 +0000

    Refactored LI props to make stretching explicit

--- src/web/components/Card/components/LI.tsx
+++ src/web/components/Card/components/LI.tsx
@@ -3,14 +3,11 @@ import { css, cx } from 'emotion';
 
 import { verticalDivider } from '../lib/verticalDivider';
 
-const liStyles = (percentage?: CardPercentageType) => css`
+const liStyles = css`
     /* This position relative is needed to contain the veritcal divider */
     position: relative;
 
-    /* margin-bottom: 12px; */
-
     display: flex;
-    ${percentage ? `flex-basis: ${percentage};` : `flex-grow: 1;`}
 `;
 
 const sidePaddingStyles = css`
@@ -23,9 +20,28 @@ const marginBottomStyles = css`
     margin-bottom: 10px;
 `;
 
+const decideSize = (percentage?: CardPercentageType, stretch?: boolean) => {
+    let sizeStyle;
+    if (percentage) {
+        sizeStyle = css`
+            flex-basis: ${percentage};
+        `;
+    } else if (stretch) {
+        sizeStyle = css`
+            flex-grow: 1;
+        `;
+    } else {
+        sizeStyle = css`
+            flex: 1;
+        `;
+    }
+    return sizeStyle;
+};
+
 type Props = {
     children: JSXElements;
-    percentage?: CardPercentageType; // Passed to flex-basis, defaults to flex grow
+    percentage?: CardPercentageType; // Used to give a particular LI more or less weight / space
+    stretch?: boolean; // When true, the card stretches based on content
     showDivider?: boolean; // If this LI wraps a card in a row this should be true
     padSides?: boolean; // If this LI directly wraps a card this should be true
     bottomMargin?: boolean; // True when wrapping a card in a column and not the last item
@@ -34,14 +50,19 @@ type Props = {
 export const LI = ({
     children,
     percentage,
+    stretch,
     showDivider,
     padSides = false,
     bottomMargin,
 }: Props) => {
+    // Decide sizing
+    const sizeStyles = decideSize(percentage, stretch);
+
     return (
         <li
             className={cx(
-                liStyles(percentage),
+                liStyles,
+                sizeStyles,
                 showDivider && verticalDivider,
                 padSides && sidePaddingStyles,
                 bottomMargin && marginBottomStyles,
