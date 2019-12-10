f6163b4c59b0b6f464de9131b962dcc3208d0cde
Oliver Lloyd <email@oliverlloyd.com>
2019-12-10 16:46:24 +0000

    Don't show vertical divider

--- src/web/components/Card/lib/verticalDivider.ts
+++ src/web/components/Card/lib/verticalDivider.ts
@@ -1,17 +1,20 @@
 import { css } from 'emotion';
 
+import { from } from '@guardian/src-foundations/mq';
 import { palette } from '@guardian/src-foundations';
 
 export const verticalDivider = css`
-    :before {
-        content: '';
-        display: block;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        width: 1px;
-        height: 100%;
-        border-left: 1px solid ${palette.neutral[86]};
+    ${from.tablet} {
+        :before {
+            content: '';
+            display: block;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            width: 1px;
+            height: 100%;
+            border-left: 1px solid ${palette.neutral[86]};
+        }
     }
 `;
