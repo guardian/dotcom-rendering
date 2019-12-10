acb2131e4c4a4f620a4683aa7e105aca7cb120ff
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-10 17:04:59 +0000

    add storybook to readme

--- src/components/news/news.stories.tsx
+++ src/components/news/news.stories.tsx
@@ -10,3 +10,32 @@ export const standard = (): JSX.Element => <ArticleHeadline
   analysis={false}
   pillarStyles={pillarColours[Pillar.news]}
 />
+
+export const feature = (): JSX.Element => <ArticleHeadline
+  headline="Feature"
+  feature={true}
+  analysis={false}
+  pillarStyles={pillarColours[Pillar.news]}
+/>
+
+export const sportsFeature = (): JSX.Element => <ArticleHeadline
+  headline="Feature"
+  feature={true}
+  analysis={false}
+  pillarStyles={pillarColours[Pillar.sport]}
+/>
+
+export const analysis = (): JSX.Element => <ArticleHeadline
+  headline="Analysis"
+  feature={false}
+  analysis={true}
+  pillarStyles={pillarColours[Pillar.news]}
+/>
+
+export const starRating = (): JSX.Element => <ArticleHeadline
+  headline="Headline"
+  feature={false}
+  analysis={false}
+  pillarStyles={pillarColours[Pillar.news]}
+  rating="5"
+/>
