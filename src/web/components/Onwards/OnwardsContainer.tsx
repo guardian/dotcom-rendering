1505aaf4ea932213051bb0b70dc30a7031af16a7
Oliver Lloyd <email@oliverlloyd.com>
2019-12-11 10:59:53 +0000

    Parity on padding

--- src/web/components/Onwards/OnwardsContainer.tsx
+++ src/web/components/Onwards/OnwardsContainer.tsx
@@ -14,10 +14,28 @@ export const OnwardsContainer = ({ children }: Props) => (
             flex-grow: 1;
             flex-direction: column;
 
-            margin-top: 10px;
-            margin-bottom: 50px;
-            ${from.tablet} {
+            margin-top: 6px;
+            ${from.leftCol} {
+                margin-top: 36px;
+            }
+
+            margin-bottom: 35px;
+
+            ${from.wide} {
                 margin-right: 70px;
+            }
+
+            margin-left: 0px;
+            margin-right: 0px;
+            ${from.tablet} {
+                /* Shrink the container to remove the leading and
+                   trailing side margins from the list of cards */
+                margin-left: -10px;
+                margin-right: -10px;
+            }
+            ${from.leftCol} {
+                margin-left: 0px;
+                margin-right: -10px;
             }
         `}
     >
