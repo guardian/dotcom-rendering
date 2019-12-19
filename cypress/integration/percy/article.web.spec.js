bef5d77a0af712aed801693eb477895a6688f7d9
Oliver Lloyd <email@oliverlloyd.com>
2019-12-19 09:53:15 +0000

    Remove edition query param
    Tempoary fix while we wait for Percy to handle this url format without breaking

--- cypress/integration/percy/article.web.spec.js
+++ cypress/integration/percy/article.web.spec.js
@@ -15,7 +15,7 @@ describe('For WEB', function() {
             // Prevent the Privacy consent banner from obscuring snapshots
             cy.setCookie('GU_TK', 'true');
             // Make the request, forcing the location to UK (for edition)
-            cy.visit(`Article?url=${url}?_edition=UK`, fetchPolyfill);
+            cy.visit(`Article?url=${url}`, fetchPolyfill);
             cy.percySnapshot(`WEB-${pillar}-${designType}-${index}`, {
                 widths: [600, 979, 1139, 1299, 1400],
             });
