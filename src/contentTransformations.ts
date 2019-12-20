6d53439ee7578edc4234437c25fee8c852714724
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-20 14:40:56 +0000

    use optional chaining in content transformations

--- src/contentTransformations.ts
+++ src/contentTransformations.ts
@@ -1,4 +1,4 @@
 export const transform = (html: string): string => html
-    .replace(/•/g, '<span class="bullet">•</span>')
-    .replace(new RegExp(/\<h2\>\* \* \*\<\/h2\>/, 'g'), '<hr class="section-rule"/>')
+    ?.replace(/•/g, '<span class="bullet">•</span>')
+    ?.replace(new RegExp(/\<h2\>\* \* \*\<\/h2\>/, 'g'), '<hr class="section-rule"/>')
 ;
