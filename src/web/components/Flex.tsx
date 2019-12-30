b6ef12338f059f2789875cf178a870f2850c6653
Rui Costa <ruicosta042@gmail.com>
2019-12-30 11:02:32 +0000

    fix: only set flex props if they are passed in

--- src/web/components/Flex.tsx
+++ src/web/components/Flex.tsx
@@ -1,35 +1,33 @@
 import React from 'react';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 
 type Props = {
     children: JSXElements;
-    direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
-    justify?:
-        | 'space-between'
-        | 'flex-end'
-        | 'flex-start'
-        | 'space-around'
-        | 'center';
-    wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
-    basis?: string;
 };
 
-export const Flex = ({
-    children,
+type FlexProps = {
+    direction?: 'row' | 'column';
+    justify?: 'space-between' | 'flex-start'; // Extend as required
+    wrap?: 'wrap';
+};
+
+const flexStyles = ({
     direction = 'row',
     justify = 'space-between',
-    wrap = 'nowrap',
-    basis = 'auto',
-}: Props) => (
-    <div
-        className={css`
+    wrap,
+}: FlexProps) =>
+    cx(
+        css`
             display: flex;
             flex-direction: ${direction};
             justify-content: ${justify};
-            flex-wrap: ${wrap};
-            flex-basis: ${basis};
-        `}
-    >
-        {children}
-    </div>
+        `,
+        typeof wrap !== `undefined` &&
+            css`
+                flex-wrap: ${wrap};
+            `,
+    );
+
+export const Flex = (props: Props & FlexProps) => (
+    <div className={flexStyles(props)}>{props.children}</div>
 );
