4e4ce0e0389656ef2f680305d1cf91374e113b01
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-31 17:32:04 +0000

    shared and liveblog stories

--- src/components/shared/Tags.test.tsx
+++ src/components/shared/Tags.test.tsx
@@ -2,51 +2,15 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Tags from 'components/shared/tags';
-import { Tag, TagType } from 'capiThriftModels';
 
 configure({ adapter: new Adapter() });
 
-describe('Keyline component renders as expected', () => {
-    const tagsProps: Tag[] = [{
-        id: "",
-        type: TagType.KEYWORD,
-        sectionId: "",
-        sectionName: "",
-        webTitle: "Tag title",
-        webUrl: "https://mapi.co.uk/tag",
-        apiUrl: "",
-        references: [{ id: "", type: "" }],
-        description: "",
-        bio: "",
-        bylineImageUrl: "",
-        bylineLargeImageUrl: "",
-        podcast: {
-            linkUrl: "",
-            copyright: "",
-            author: "",
-            subscriptionUrl: "",
-            explicit: false,
-            image: "",
-            categories: [],
-            podcastType: "",
-            googlePodcastsUrl: "",
-            spotifyUrl: ""
-        },
-        firstName: "",
-        lastName: "",
-        emailAddress: "",
-        twitterHandle: "",
-        activeSponsorships: [],
-        paidContentType: "",
-        paidContentCampaignColour: "",
-        rcsId: "",
-        r2ContributorId: "",
-        tagCategories: [""],
-        entityIds: [""],
-        campaignInformationType: "",
-        internalName: "",
-    }]
+const tagsProps = [{
+    webTitle: "Tag title",
+    webUrl: "https://mapi.co.uk/tag"
+}];
 
+describe('Keyline component renders as expected', () => {
     it('Renders link to tag', () => {
         const tags = shallow(<Tags tags={tagsProps} />)
         const link = tags.find('a')
