520fd868a2e3dc739a881f862ba11fe193a4e3e6
Oliver Lloyd <email@oliverlloyd.com>
2020-01-05 15:41:02 +0000

    Added Dropdown stories

---
+++ src/web/components/Dropdown.stories.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { css } from 'emotion';
+
+import { palette } from '@guardian/src-foundations';
+
+import { Dropdown } from '@frontend/web/components/Dropdown';
+
+const Header = ({ children }: { children: JSX.Element | JSX.Element[] }) => (
+    <div
+        className={css`
+            height: 300px;
+            width: 100%;
+            background-color: ${palette.brand.main};
+        `}
+    >
+        {children}
+    </div>
+);
+
+const Nav = ({ children }: { children: JSX.Element | JSX.Element[] }) => (
+    <div
+        className={css`
+            height: 20px;
+            position: absolute;
+            top: 10px;
+            display: block;
+            right: 258px;
+            width: 197px;
+            z-index: 1072;
+            transform: translateX(100%);
+            padding-top: 7px;
+        `}
+    >
+        {children}
+    </div>
+);
+
+const links = [
+    {
+        url: '/preference/edition/uk',
+        title: 'UK edition',
+        isActive: true,
+        dataLinkName: 'linkname-UK',
+    },
+    {
+        url: '/preference/edition/us',
+        title: 'US edition',
+        dataLinkName: 'linkname-US',
+    },
+    {
+        url: '/preference/edition/au',
+        title: 'Australian edition',
+        dataLinkName: 'linkname-AU',
+    },
+    {
+        url: '/preference/edition/int',
+        title: 'International edition',
+        dataLinkName: 'linkname-INT',
+    },
+];
+
+const linksWithNoneActive = [
+    {
+        ...links[0],
+        isActive: false,
+    },
+    { ...links[1] },
+    { ...links[2] },
+    { ...links[3] },
+];
+
+/* tslint:disable */
+export default {
+    component: Dropdown,
+    title: 'Components/Dropdown',
+};
+/* tslint:enable */
+
+export const DropdownActive = () => (
+    <Header>
+        <Nav>
+            <Dropdown
+                id="d1"
+                label="UK edition"
+                links={links}
+                dataLinkName="linkname1"
+            />
+        </Nav>
+    </Header>
+);
+DropdownActive.story = { name: 'Dropdown with first item active' };
+
+export const DropdownNoActive = () => (
+    <Header>
+        <Nav>
+            <Dropdown
+                id="d2"
+                label="UK edition"
+                links={linksWithNoneActive}
+                dataLinkName="linkname2"
+            />
+        </Nav>
+    </Header>
+);
+DropdownNoActive.story = { name: 'Dropdown with nothing active' };
