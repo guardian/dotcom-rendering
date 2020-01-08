a070e7fddc5dec3a9d4eb9874364f0afbece438d
Oliver Lloyd <email@oliverlloyd.com>
2020-01-08 09:51:39 +0000

    Removed all custom styles from Outbrain container

--- src/web/components/Outbrain.tsx
+++ src/web/components/Outbrain.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { css } from 'emotion';
-import { textSans, body, headline } from '@guardian/src-foundations/typography';
 
 interface OutbrainSelectors {
     widget: string;
@@ -43,41 +41,6 @@ const OutbrainWidget: React.FC<{}> = ({}) => {
     );
 };
 
-const outbrainContainer = css`
-    .js-outbrain-container {
-        padding-top: 20px;
-        padding-bottom: 20px;
-    }
-    .ob-widget {
-        div.ob-widget-header {
-            ${headline.xsmall()};
-            font-weight: 900;
-            span,
-            .ob_about_this_content a {
-                ${body.small({ fontWeight: 'regular' })};
-                font-size: 17px;
-
-                text-decoration: none;
-                /* stylelint-disable-next-line color-no-hex */
-                color: #00456e;
-            }
-        }
-        div.ob-widget-section {
-            margin-top: 0px;
-        }
-
-        span.ob-rec-text {
-            max-height: fit-content;
-            ${textSans.medium({ fontWeight: 'bold' })};
-            font-size: 16px;
-        }
-    }
-`;
-
 export const OutbrainContainer: React.FC<{}> = () => {
-    return (
-        <div className={outbrainContainer}>
-            <OutbrainWidget />
-        </div>
-    );
+    return <OutbrainWidget />;
 };
