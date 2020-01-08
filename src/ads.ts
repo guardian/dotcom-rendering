bbc931a199ebcb74e3afc47a905cbc78c9d6dca8
Jamie Webb <jamie.webb@guardian.co.uk>
2020-01-08 11:35:19 +0000

    fix react key warnings

--- src/ads.ts
+++ src/ads.ts
@@ -9,7 +9,7 @@ function insertAdPlaceholders(reactNodes: ReactNode[]): ReactNode[] {
 
     const className = numParas < 15 ? 'ad-placeholder short' : 'ad-placeholder';
 
-    const ad = h('aside', { className },
+    const ad = (key: number) => h('aside', { className, key },
         h('div', { className: 'ad-labels' },
             h('h1', null, 'Advertisement'),
             h('button', { className: 'ad-hide' },
@@ -19,7 +19,7 @@ function insertAdPlaceholders(reactNodes: ReactNode[]): ReactNode[] {
     );
 
     const insertAd = (para: number, nodes: ReactNode[]): ReactNode[] =>
-        adIndices.includes(para) ? [...nodes, ad] : nodes;
+        adIndices.includes(para) ? [...nodes, ad(++para)] : nodes;
 
     return flattenedNodes
         .reduce(([paraNum, prevNodes], node) => {
