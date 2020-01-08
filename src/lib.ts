7077ab5228761a5b9d12ae56ab44b3acea4ac915
Jamie Webb <jamie.webb@guardian.co.uk>
2020-01-08 12:26:25 +0000

    lint fixes

--- src/lib.ts
+++ src/lib.ts
@@ -3,7 +3,8 @@
 const compose = <A, B, C>(f: (_b: B) => C, g: (_a: A) => B) => (a: A): C => f(g(a));
 
 const hexToRGBA = (hex: string, alpha: number): string => {
-    const [r, g, b] = hex.match(/\w\w/g)?.map(x => parseInt(x, 16)) ?? [0, 0, 0];
+    const [r, g, b] = hex.match(/\w\w/g)
+      ?.map((x: string): number => parseInt(x, 16)) ?? [0, 0, 0];
     return `rgba(${r},${g},${b},${alpha})`;
   };
 
