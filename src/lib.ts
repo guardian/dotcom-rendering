b2a8c3149aa178937ff0919791c61ca0933ffdab
Jamie Webb <jamie.webb@guardian.co.uk>
2020-01-09 10:51:08 +0000

    replace rgba function with hex values with opacity

--- src/lib.ts
+++ src/lib.ts
@@ -2,15 +2,8 @@
 
 const compose = <A, B, C>(f: (_b: B) => C, g: (_a: A) => B) => (a: A): C => f(g(a));
 
-const hexToRGBA = (hex: string, alpha: number): string => {
-    const [r, g, b] = hex.match(/\w\w/g)
-      ?.map((x: string): number => parseInt(x, 16)) ?? [0, 0, 0];
-    return `rgba(${r},${g},${b},${alpha})`;
-  };
-
 // ----- Exports ----- //
 
 export {
     compose,
-    hexToRGBA,
 };
