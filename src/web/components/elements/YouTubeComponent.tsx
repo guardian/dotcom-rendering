37d1e8ad738a3d911b72be0bc109acefeec9431b
Oliver Lloyd <email@oliverlloyd.com>
2020-01-13 10:23:52 +0000

    Removed special margins for you tube videos

--- src/web/components/elements/YouTubeComponent.tsx
+++ src/web/components/elements/YouTubeComponent.tsx
@@ -1,61 +1,49 @@
 import React from 'react';
-import { css } from 'emotion';
 
 import { Caption } from '@root/src/web/components/Caption';
 import { YouTubeEmbed } from '@root/src/web/components/YouTubeEmbed';
 
 type Props = {
-    children: JSX.Element | JSX.Element[];
-};
-
-const Margins = ({ children }: Props) => (
-    <div
-        className={css`
-            margin-top: 16px;
-            margin-bottom: 12px;
-        `}
-    >
-        {children}
-    </div>
-);
-
-export const YouTubeComponent: React.FC<{
     element: YoutubeBlockElement;
     pillar: Pillar;
     hideCaption?: boolean;
     role: RoleType;
     adTargeting?: AdTargeting;
-}> = ({ element, pillar, hideCaption, role, adTargeting }) => {
+};
+
+export const YouTubeComponent = ({
+    element,
+    pillar,
+    hideCaption,
+    role,
+    adTargeting,
+}: Props) => {
     if (hideCaption) {
         return (
-            <Margins>
-                <YouTubeEmbed
-                    assetId={element.assetId}
-                    pillar={pillar}
-                    adTargeting={adTargeting}
-                    duration={element.duration}
-                    title={element.mediaTitle}
-                />
-            </Margins>
+            <YouTubeEmbed
+                assetId={element.assetId}
+                pillar={pillar}
+                adTargeting={adTargeting}
+                duration={element.duration}
+                title={element.mediaTitle}
+            />
         );
     }
     return (
-        <Margins>
-            <Caption
-                captionText={element.mediaTitle || ''}
+        <Caption
+            captionText={element.mediaTitle || ''}
+            pillar={pillar}
+            dirtyHtml={true}
+            displayCredit={false}
+            role={role}
+        >
+            <YouTubeEmbed
+                assetId={element.assetId}
                 pillar={pillar}
-                dirtyHtml={true}
-                displayCredit={false}
-                role={role}
-            >
-                <YouTubeEmbed
-                    assetId={element.assetId}
-                    pillar={pillar}
-                    adTargeting={adTargeting}
-                    duration={element.duration}
-                    title={element.mediaTitle}
-                />
-            </Caption>
-        </Margins>
+                adTargeting={adTargeting}
+                duration={element.duration}
+                title={element.mediaTitle}
+            />
+        </Caption>
     );
 };
