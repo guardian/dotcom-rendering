7050b92019bf42d452ad4c05e9aa13c6487ed426
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-01-13 10:42:11 +0000

    Use GB instead of US for page language

--- src/components/shared/page.tsx
+++ src/components/shared/page.tsx
@@ -130,7 +130,7 @@ function Page({ content, imageSalt }: Props): JSX.Element {
         : null
 
     return (
-        <html lang="en-US" css={PageStyles}>
+        <html lang="en-GB" css={PageStyles}>
             <head>
                 <title>{content.id}</title>
                 <meta charSet="UTF-8" />
