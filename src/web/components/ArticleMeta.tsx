ea9644e5b1db351395efc24c5d179f15f190574d
Gareth <gtrufitt@gmail.com>
2020-01-14 12:17:00 +0000

    Remove unused path Byline|

--- src/web/components/ArticleMeta.tsx
+++ src/web/components/ArticleMeta.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { css, cx } from 'emotion';
 import { palette } from '@guardian/src-foundations';
 import { between, until } from '@guardian/src-foundations/mq';
-
-import { Byline } from '@root/src/web/components/Byline';
 import { Contributor } from '@root/src/web/components/Contributor';
 import { GuardianLines } from '@root/src/web/components/GuardianLines';
 import { getSharingUrls } from '@root/src/lib/sharing-urls';
