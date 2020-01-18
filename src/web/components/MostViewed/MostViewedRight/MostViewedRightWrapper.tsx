b32d900e2944a09eaec633b54d566f3a59d3c101
Oliver Lloyd <email@oliverlloyd.com>
2020-01-18 16:07:42 +0000

    Refactored to handle slow to render article elements

--- src/web/components/MostViewed/MostViewedRight/MostViewedRightWrapper.tsx
+++ src/web/components/MostViewed/MostViewedRight/MostViewedRightWrapper.tsx
@@ -1,33 +1,49 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, RefObject } from 'react';
 import { css } from 'emotion';
 import { MostViewedRight } from './MostViewedRight';
+
+type Props = {
+    pillar: Pillar;
+    limitItems?: number;
+};
+
+// Minimum height needed to render MostViewedRight is its own outer height.
+const HEIGHT_REQUIRED = 482 + 24 + 24;
 
 const flexGrow = css`
     flex-grow: 1;
 `;
 
-interface Props {
-    pillar: Pillar;
-    limitItems?: number;
-}
-
 // Wrapping MostViewedRight so we can determine whether or not there's enough vertical space in the container to render it.
 export const MostViewedRightWrapper = ({ pillar, limitItems }: Props) => {
     const bodyRef = useRef<HTMLDivElement>(null);
-    const [availableHeight, setAvailableHeight] = useState(0);
+    const [heightIsAvailable, setHeightIsAvailable] = useState<boolean>(false);
+
+    const checkHeight = (ref: RefObject<HTMLDivElement>) => {
+        if (!heightIsAvailable) {
+            // Don't bother checking if height already available
+            if (ref.current) {
+                const { offsetHeight } = ref.current;
+                setHeightIsAvailable(offsetHeight > HEIGHT_REQUIRED);
+            }
+        }
+    };
 
     useEffect(() => {
-        if (bodyRef.current) {
-            const { offsetHeight } = bodyRef.current;
-            setAvailableHeight(offsetHeight);
-        }
-    }, [availableHeight]);
+        // Check if we have the available height
+        checkHeight(bodyRef);
 
-    // Minimum height needed to render MostViewedRight is its own outer height.
-    const heightRequired = 482 + 24 + 24;
+        // setTimeout here lets us put another check at the end of the
+        // event queue in case any in body elements still need to render
+        // which could push the page down giving us the space we need
+        setTimeout(() => {
+            checkHeight(bodyRef);
+        });
+    }, [heightIsAvailable]);
+
     return (
         <div ref={bodyRef} className={flexGrow}>
-            {availableHeight > heightRequired ? (
+            {heightIsAvailable ? (
                 <MostViewedRight pillar={pillar} limitItems={limitItems} />
             ) : null}
         </div>
