8aaf638bb2dea9c17cd21d537a4c99dd44fcf797
Oliver Lloyd <email@oliverlloyd.com>
2020-01-18 14:23:17 +0000

    Don't use sticky ads for showcase articles

--- src/web/layouts/ShowcaseLayout.tsx
+++ src/web/layouts/ShowcaseLayout.tsx
@@ -4,7 +4,7 @@ import { css } from 'emotion';
 import { palette } from '@guardian/src-foundations';
 
 import { Flex } from '@root/src/web/components/Flex';
-import { StickyAd } from '@root/src/web/components/StickyAd';
+import { namedAdSlotParameters } from '@root/src/model/advertisement';
 import { ArticleBody } from '@root/src/web/components/ArticleBody';
 import { RightColumn } from '@root/src/web/components/RightColumn';
 import { LeftColumn } from '@root/src/web/components/LeftColumn';
@@ -23,7 +23,7 @@ import { OutbrainContainer } from '@root/src/web/components/Outbrain';
 import { Section } from '@root/src/web/components/Section';
 import { Nav } from '@root/src/web/components/Nav/Nav';
 import { HeaderAdSlot } from '@root/src/web/components/HeaderAdSlot';
-import { MobileStickyContainer } from '@root/src/web/components/AdSlot';
+import { MobileStickyContainer, AdSlot } from '@root/src/web/components/AdSlot';
 
 import { buildAdTargeting } from '@root/src/lib/ad-targeting';
 
@@ -167,7 +167,10 @@ export const ShowcaseLayout = ({ CAPI, NAV }: Props) => {
                                 </main>
                             </div>
                             <RightColumn>
-                                <StickyAd />
+                                <AdSlot
+                                    asps={namedAdSlotParameters('right')}
+                                    className=""
+                                />
                                 {!isPaidContent ? (
                                     <MostViewedRightIsland />
                                 ) : (
