292f5c6aafd071d023dda90ccc68ce36dae7d8d5
Gareth <gtrufitt@gmail.com>
2020-01-20 12:47:02 +0000

    Add rewrappedComponent component

---
+++ src/web/components/elements/RewrappedComponent.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { css } from 'emotion';
+import { unescapeData } from '@root/src/lib/escapeData';
+import { unwrapHtml } from '@root/src/model/unwrapHtml';
+
+// Ideally we want to want to avoid an unnecessary 'span' wrapper,
+// which also will cause issues with ads (spacefinder rules). React
+// requires a wrapping element for dangerouslySetInnerHTML so we
+// strip the original element markup and rewrap. The fallback case is
+// added for safety but should never happen. If it does happen
+// though it will cause issues with commercial JS which expects
+// paras to be top-level.
+
+export const RewrappedComponent = ({
+    prefix,
+    suffix,
+    html,
+    elCss,
+    tagName,
+}: {
+    prefix: string;
+    suffix: string;
+    html: string;
+    elCss: string;
+    tagName: string;
+}): JSX.Element => {
+    const { willUnwrap: isUnwrapped, unwrappedHtml } = unwrapHtml(
+        prefix,
+        suffix,
+        html,
+    );
+
+    const element = isUnwrapped ? tagName : 'span';
+
+    // If we implement a span, we want to apply the CSS to the inner element
+    // to ensure we still style correctly
+    const innerElCss = css`
+        ${tagName} {
+            ${elCss}
+        }
+    `;
+
+    const style = isUnwrapped ? elCss : innerElCss;
+
+    // Create a react element from the tagName passed in OR
+    // default to <span> if we've not been able to unwrap based on prefix & suffix
+    const ElementComponent = React.createElement(`${element}`, {
+        className: style,
+        dangerouslySetInnerHTML: {
+            __html: unescapeData(unwrappedHtml),
+        },
+    });
+
+    return ElementComponent;
+};
