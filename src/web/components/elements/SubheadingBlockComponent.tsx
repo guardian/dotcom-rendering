c9c6dfa79076cf5117b41bc802de1de95de7d39a
Gareth <gtrufitt@gmail.com>
2020-01-20 16:05:12 +0000

    Use RewrappedComponent for subheading

--- src/web/components/elements/SubheadingBlockComponent.tsx
+++ src/web/components/elements/SubheadingBlockComponent.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { unescapeData } from '@root/src/lib/escapeData';
+import { unwrapHtml } from '@root/src/model/unwrapHtml';
+import { RewrappedComponent } from '@root/src/web/components/elements/RewrappedComponent';
 // tslint:disable:react-no-dangerous-html
 
 export const SubheadingBlockComponent: React.FC<{ html: string }> = ({
     html,
-}) => (
-    // TODO - wrapping headings in a div is really not very nice, similar in textBlockComponent
-    // wrapping paragraphs in spans.
-    // There's no way to do `dangerouslySetInnerHTML` on Fragments though, so we may have
-    // to try to think of a workaround so that our ads will work properly.
-    <div
-        dangerouslySetInnerHTML={{
-            __html: unescapeData(html),
-        }}
-    />
-);
+}) => {
+    const { willUnwrap: isUnwrapped, unwrappedHtml } = unwrapHtml(
+        '<h2>',
+        '</h2>',
+        html,
+    );
+
+    return (
+        <RewrappedComponent
+            isUnwrapped={isUnwrapped}
+            html={unwrappedHtml}
+            tagName="h2"
+        />
+    );
+};
