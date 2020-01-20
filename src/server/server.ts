e72156c9c2f1f95013028a82a44b059c6bc4d5a4
Jamie Webb <jamie.webb@guardian.co.uk>
2020-01-20 15:55:48 +0000

    post endpoint

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,6 +6,11 @@ import compression from 'compression';
 import { createElement as h } from 'react';
 import { renderToString } from 'react-dom/server';
 import fetch, { Response } from 'node-fetch';
+import bodyParser from 'body-parser';
+import {
+  BufferedTransport,
+  CompactProtocol
+} from '@creditkarma/thrift-server-core'
 
 import { Content } from 'mapiThriftModels/Content';
 import { getConfigValue } from 'server/ssmConfig';
@@ -58,13 +63,16 @@ const mapiError = async (mapiResponse: Response): Promise<string> =>
 
 async function serveArticlePost({ body }: Request, res: ExpressResponse): Promise<void> {
   try {
-    const support = checkSupport(body);
+    const transport = new BufferedTransport(body);
+    const protocol = new CompactProtocol(transport);
+    const content: Content = Content.read(protocol);
+
+    const support = checkSupport(content);
     const imageSalt = await getConfigValue<string>('apis.img.salt');
 
     if (support.kind === Support.Supported) {
-      const page = h(Page, { content: body, imageSalt });
+      const page = h(Page, { content, imageSalt });
       const html = renderToString(page);
-
       res.write('<!DOCTYPE html>');
       res.write(html);
       res.end();
@@ -146,7 +154,6 @@ async function queryMapi(req: Request, res: ExpressResponse): Promise<void> {
 
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
 app.use('/public', express.static(path.resolve(__dirname, '../public')));
 app.use('/assets', express.static(path.resolve(__dirname, '../dist/assets')));
 app.use(compression());
@@ -165,7 +172,7 @@ app.get('/*', async (req, res) => {
   }
 });
 
-app.post('/article', serveArticlePost);
+app.post('/article', bodyParser.raw(), serveArticlePost);
 
 const port = 3040;
 app.listen(port, () => console.log(`Server listening on port ${port}!\nWebpack dev server listening on port 8080!`));
