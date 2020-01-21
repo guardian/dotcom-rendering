174a6f5dabf60ce492cd69fda6d060f6ef107a24
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2020-01-21 13:44:51 +0000

    temp amp iframe for testing (permutive/prebid server)

--- src/amp/components/AdUserSync.tsx
+++ src/amp/components/AdUserSync.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { css } from 'emotion';
 
-const prebidSrc =
-    'https://cdn.jsdelivr.net/npm/prebid-universal-creative@latest/dist/load-cookie.html';
+// const prebidSrc =
+//     'https://cdn.jsdelivr.net/npm/prebid-universal-creative@latest/dist/load-cookie.html';
+
+const hostedIframeSrc =
+    'https://uploads.guim.co.uk/2020/01/21/amp-prebid-permutive.html';
+
+// s3 url https://s3-eu-west-1.amazonaws.com/uploads-origin.guim.co.uk/2020/01/21/amp-prebid-permutive.html
 
 const prebidImg =
     'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
@@ -20,9 +25,9 @@ export const AdUserSync: React.FC<{}> = () => {
             title="User Sync"
             height="1"
             width="1"
-            sandbox="allow-scripts"
+            sandbox="allow-scripts allow-same-origin"
             frameborder="0"
-            src={prebidSrc}
+            src={hostedIframeSrc}
         >
             <amp-img layout="fill" src={prebidImg} placeholder="" />
         </amp-iframe>
