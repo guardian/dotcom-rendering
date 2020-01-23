f0498df117b547e0e430b8b0fa86958c5d6221a3
Oliver Lloyd <email@oliverlloyd.com>
2020-01-23 16:45:18 +0000

    Make image optional in line with CAPI

--- index.d.ts
+++ index.d.ts
@@ -438,8 +438,8 @@ interface TrailType {
     url: string;
     headline: string;
     isLiveBlog: boolean;
-    image: string;
     webPublicationDate: string;
+    image?: string;
     avatarUrl?: string;
     mediaType?: MediaType;
     mediaDuration?: number;
