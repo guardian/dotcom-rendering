9165f551253f9c0feab226cb194888410ab2a9c8
Oliver Lloyd <email@oliverlloyd.com>
2020-01-27 22:27:14 +0000

    Pad headline

--- src/web/layouts/CommentLayout.tsx
+++ src/web/layouts/CommentLayout.tsx
@@ -15,6 +15,7 @@ import { MostViewedRightIsland } from '@root/src/web/components/MostViewedRightI
 import { SubMeta } from '@root/src/web/components/SubMeta';
 import { MainMedia } from '@root/src/web/components/MainMedia';
 import { ArticleHeadline } from '@root/src/web/components/ArticleHeadline';
+import { ArticleHeadlinePadding } from '@root/src/web/components/ArticleHeadlinePadding';
 import { ArticleStandfirst } from '@root/src/web/components/ArticleStandfirst';
 import { Header } from '@root/src/web/components/Header';
 import { Footer } from '@root/src/web/components/Footer';
@@ -234,14 +235,18 @@ export const CommentLayout = ({ CAPI, NAV }: Props) => {
                     </GridItem>
                     <GridItem area="headline">
                         <div className={maxWidth}>
-                            <ArticleHeadline
-                                headlineString={CAPI.headline}
+                            <ArticleHeadlinePadding
                                 designType={CAPI.designType}
-                                pillar={CAPI.pillar}
-                                webPublicationDate={CAPI.webPublicationDate}
-                                tags={CAPI.tags}
-                                byline={CAPI.author.byline}
-                            />
+                            >
+                                <ArticleHeadline
+                                    headlineString={CAPI.headline}
+                                    designType={CAPI.designType}
+                                    pillar={CAPI.pillar}
+                                    webPublicationDate={CAPI.webPublicationDate}
+                                    tags={CAPI.tags}
+                                    byline={CAPI.author.byline}
+                                />
+                            </ArticleHeadlinePadding>
                         </div>
                     </GridItem>
                     <GridItem area="lines">
