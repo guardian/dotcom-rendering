d37880251618498e865a0f433ddce703b9b16f88
Oliver Lloyd <email@oliverlloyd.com>
2020-01-29 10:52:05 +0000

    Extract formatCount to a lib

---
+++ src/web/components/lib/formatCount.ts
@@ -0,0 +1,14 @@
+import { integerCommas } from '@root/src/lib/formatters';
+
+export const formatCount = (count: number) => {
+    const countAsInteger = parseInt(count.toFixed(0), 10);
+    const displayCountLong = integerCommas(countAsInteger);
+    const displayCountShort =
+        countAsInteger > 10000
+            ? `${Math.round(countAsInteger / 1000)}k`
+            : countAsInteger.toString();
+    return {
+        short: displayCountShort,
+        long: displayCountLong,
+    };
+};
