496528c735b9541601b55c35b352ccef174c0a25
William <william.j.t.lacroix@gmail.com>
2020-01-31 11:29:29 +0000

    add rich link styling

--- src/web/components/elements/RichLinkComponent.tsx
+++ src/web/components/elements/RichLinkComponent.tsx
@@ -181,6 +181,11 @@ const paidForBranding = css`
     color: ${palette.neutral[46]};
 `;
 
+const starWrapper = css`
+    background-color: ${palette.brandYellow.main};
+    display: inline-block;
+`;
+
 const readMoreText: (contentType: string) => string = contentType => {
     switch (contentType) {
         case 'audio':
@@ -243,7 +248,12 @@ const RichLinkBody: React.FC<{ richLink: RichLink }> = ({ richLink }) => {
                         </div>
                     )}
                     {richLink.starRating && richLink.starRating > 0 && (
-                        <StarRating rating={richLink.starRating} size="small" />
+                        <div className={starWrapper}>
+                            <StarRating
+                                rating={richLink.starRating}
+                                size="medium"
+                            />
+                        </div>
                     )}
                     {isPaidContent && richLink.sponsorName && (
                         <div className={paidForBranding}>
