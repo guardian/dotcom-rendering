a6f567edc69844a2c1d95a926528052033c573cb
Gareth <gtrufitt@gmail.com>
2020-02-21 13:49:53 +0000

    Remove lodash.flow

--- src/web/lib/formatAttrString.ts
+++ src/web/lib/formatAttrString.ts
@@ -1,28 +1,14 @@
-import flow from 'lodash.flow';
+type strInOutFunc = (str: string) => string;
 
-export const formatAttrString = (input: string) => {
-    // Remove all non alphaNumeric
-    // Lowercase all
-    // Replace Spaces with Hyphens
+const removeAlphaNumeric: strInOutFunc = str =>
+    str.replace(new RegExp('[^0-9a-z ]', 'gi'), ';'); // We set it to semicolon so we can remove it at the end
+const lowercase: strInOutFunc = str => str.toLowerCase();
+const replaceSpacesWithHyphens: strInOutFunc = str =>
+    str.replace(new RegExp(' ', 'g'), '-'); // Replace the space
+const removeTempChars: strInOutFunc = str =>
+    str.replace(new RegExp('[;]', 'g'), ''); // Strip out those semicolons
 
-    type strInOutFunc = (str: string) => string;
-
-    const removeAlphaNumeric: strInOutFunc = str =>
-        str.replace(new RegExp('[^0-9a-z ]', 'gi'), ';'); // We set it to semicolon so we can remove it at the end
-    const lowercase: strInOutFunc = str => str.toLowerCase();
-    const replaceSpacesWithHyphens: strInOutFunc = str =>
-        str.replace(new RegExp(' ', 'g'), '-'); // Replace the space
-    const removeTempChars: strInOutFunc = str =>
-        str.replace(new RegExp('[;]', 'g'), ''); // Strip out those semicolons
-
-    // https://lodash.com/docs/4.17.15#flow
-    // Function composite
-    const output = flow([
-        removeAlphaNumeric,
-        lowercase,
-        replaceSpacesWithHyphens,
-        removeTempChars,
-    ]);
-
-    return output(input);
-};
+export const formatAttrString = (input: string) =>
+    removeTempChars(
+        replaceSpacesWithHyphens(lowercase(removeAlphaNumeric(input))),
+    );
