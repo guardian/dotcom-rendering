6e03ac93f9c95cbeac8d2bf36416a6eb8baa9ae8
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-02-27 16:46:37 +0000

    Switched immersives to universal standfirst

--- src/components/standfirst.tsx
+++ src/components/standfirst.tsx
@@ -1,12 +1,12 @@
 // ----- Imports ----- //
 
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, ReactNode } from 'react';
 import { css, SerializedStyles } from '@emotion/core';
 import { headline } from '@guardian/src-foundations/typography';
 import { background, neutral, text } from '@guardian/src-foundations/palette';
 import { from } from '@guardian/src-foundations/mq';
 
-import { Item, Design } from 'item';
+import { Item, Design, Display } from 'item';
 import { renderText } from 'renderer';
 import { spaceToRem, textPadding, darkModeCss as darkMode } from 'styles';
 import { PillarStyles, getPillarStyles } from 'pillar';
@@ -36,6 +36,10 @@ const styles = (pillarStyles: PillarStyles): SerializedStyles => css`
         margin: 0;
     }
 
+    address {
+        font-style: normal;
+    }
+
     ${darkStyles(pillarStyles)}
 `;
 
@@ -51,21 +55,61 @@ const thinHeadline = css`
     }
 `;
 
+const immersive = (pillarStyles: PillarStyles): SerializedStyles => css`
+    ${styles(pillarStyles)}
+    ${headline.xsmall({ fontWeight: 'light' })}
+    margin-top: ${spaceToRem(3)};
+
+    a {
+        box-shadow: inset 0 -0.1rem ${pillarStyles.kicker};
+        padding-bottom: 0.2rem;
+    }
+`;
+
 const getStyles = (item: Item): SerializedStyles => {
+    const pillarStyles = getPillarStyles(item.pillar);
+
+    if (item.display === Display.Immersive) {
+        return immersive(pillarStyles);
+    }
+
     switch (item.design) {
         case Design.Review:
         case Design.Feature:
         case Design.Comment:
-            return css(styles(getPillarStyles(item.pillar)), thinHeadline);
+            return css(styles(pillarStyles), thinHeadline);
 
         default:
-            return css(styles(getPillarStyles(item.pillar)), normalHeadline);
+            return css(styles(pillarStyles), normalHeadline);
     }
 }
 
+function content(standfirst: DocumentFragment, item: Item): ReactNode {
+    const rendered = renderText(standfirst, item.pillar);
+
+    // Immersives append the byline to the standfirst.
+    // Sometimes CAPI includes this within the standfirst HTML,
+    // sometimes we have to add it ourselves
+    const bylineInStandfirst = item.byline !== '' && standfirst.textContent?.includes(item.byline);
+
+    if (item.display === Display.Immersive && !bylineInStandfirst) {
+        return item.bylineHtml.fmap<ReactNode>(byline =>
+            <>
+                {rendered}
+                <address>
+                    <p>By {renderText(byline, item.pillar)}</p>
+                </address>
+            </>
+        ).withDefault(rendered);
+    }
+
+    return rendered;
+}
+
+
 const Standfirst: FC<Props> = ({ item }) =>
     item.standfirst.fmap<ReactElement | null>(standfirst =>
-        <div css={getStyles(item)}>{renderText(standfirst, item.pillar)}</div>,
+        <div css={getStyles(item)}>{content(standfirst, item)}</div>,
     ).withDefault(null);
 
 
