c2f995cfcfc26dbc8c69bc5ea1225fc584f6a473
Gareth <gtrufitt@gmail.com>
2020-03-02 07:24:02 +0000

    Split chunks

--- scripts/webpack/browser.js
+++ scripts/webpack/browser.js
@@ -2,9 +2,7 @@ const webpack = require('webpack');
 const AssetsManifest = require('webpack-assets-manifest');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const chalk = require('chalk');
-const {
-    siteName
-} = require('../frontend/config');
+const { siteName } = require('../frontend/config');
 
 const friendlyErrorsWebpackPlugin = () =>
     new FriendlyErrorsWebpackPlugin({
@@ -31,15 +29,14 @@ const generateName = isLegacyJS => {
 const manifestData = {};
 const legacyManifestData = {};
 
-const scriptPath = package => [
-    `./src/web/browser/${package}/init.ts`,
-    DEV &&
-    'webpack-hot-middleware/client?name=browser&overlayWarnings=true',
-].filter(Boolean);
+const scriptPath = package =>
+    [
+        `./src/web/browser/${package}/init.ts`,
+        DEV &&
+            'webpack-hot-middleware/client?name=browser&overlayWarnings=true',
+    ].filter(Boolean);
 
-module.exports = ({
-    isLegacyJS
-}) => ({
+module.exports = ({ isLegacyJS }) => ({
     entry: {
         sentry: scriptPath('sentry'),
         ga: scriptPath('ga'),
@@ -47,18 +44,41 @@ module.exports = ({
         react: scriptPath('react'),
         lotame: scriptPath('lotame'),
     },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 2000,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(
+                            /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
+                        )[1];
+
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
+                },
+            },
+        },
+    },
     output: {
         filename: generateName(isLegacyJS),
         chunkFilename: generateName(isLegacyJS),
     },
     plugins: [
         PROD &&
-        new AssetsManifest({
-            writeToDisk: true,
-            assets: isLegacyJS ? legacyManifestData : manifestData,
-            // Need to explicitly define output file names to avoid overwrites
-            output: isLegacyJS ? 'manifest.legacy.json' : 'manifest.json',
-        }),
+            new AssetsManifest({
+                writeToDisk: true,
+                assets: isLegacyJS ? legacyManifestData : manifestData,
+                // Need to explicitly define output file names to avoid overwrites
+                output: isLegacyJS ? 'manifest.legacy.json' : 'manifest.json',
+            }),
         DEV && new webpack.HotModuleReplacementPlugin(),
         DEV && new webpack.NamedModulesPlugin(),
         DEV && friendlyErrorsWebpackPlugin(),
@@ -66,32 +86,35 @@ module.exports = ({
         // [...].filter(Boolean) why it is used
     ].filter(Boolean),
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /(\.tsx)|(\.js)|(\.ts)$/,
                 exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-typescript',
-                            '@babel/preset-react',
-                            // @babel/preset-env is used for legacy browsers
-                            // @babel/preset-modules is used for modern browsers
-                            // this allows us to reduce bundle sizes
-                            isLegacyJS ?
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                        ie: '11',
-                                    },
-                                    modules: false,
-                                },
-                            ] :
-                            '@babel/preset-modules',
-                        ],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-typescript',
+                                '@babel/preset-react',
+                                // @babel/preset-env is used for legacy browsers
+                                // @babel/preset-modules is used for modern browsers
+                                // this allows us to reduce bundle sizes
+                                isLegacyJS
+                                    ? [
+                                          '@babel/preset-env',
+                                          {
+                                              targets: {
+                                                  ie: '11',
+                                              },
+                                              modules: false,
+                                          },
+                                      ]
+                                    : '@babel/preset-modules',
+                            ],
+                        },
                     },
-                }, ],
+                ],
             },
             {
                 test: /\.css$/,
