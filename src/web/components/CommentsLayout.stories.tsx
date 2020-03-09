9825ba56bb4bd606c3f1285adf5ae0aaa8fa8184
Oliver Lloyd <email@oliverlloyd.com>
2020-03-09 12:46:18 +0000

    The story now shows the full layout

--- src/web/components/CommentsLayout.stories.tsx
+++ src/web/components/CommentsLayout.stories.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { Section } from '@frontend/web/components/Section';
+import { RightColumn } from '@frontend/web/components/RightColumn';
+import { Flex } from '@frontend/web/components/Flex';
+
 import { CommentsLayout } from './CommentsLayout';
 
 /* tslint:disable */
@@ -9,5 +13,14 @@ export default {
 };
 /* tslint:enable */
 
-export const Default = () => <CommentsLayout commentCount={345} />;
+export const Default = () => (
+    <Section>
+        <Flex>
+            <CommentsLayout commentCount={345} isClosedForComments={false} />
+            <RightColumn>
+                <p>TODO: Comments ad slot goes here</p>
+            </RightColumn>
+        </Flex>
+    </Section>
+);
 Default.story = { name: 'default' };
