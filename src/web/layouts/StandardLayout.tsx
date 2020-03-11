52734bb7a668201aa3a5f91b41ecbdbdda2e91f9
William <william.j.t.lacroix@gmail.com>
2020-03-11 17:26:34 +0000

    change prop name subNavSections

--- src/web/layouts/StandardLayout.tsx
+++ src/web/layouts/StandardLayout.tsx
@@ -281,7 +281,7 @@ export const StandardLayout = ({ CAPI, NAV }: Props) => {
                             islandId="sub-nav-root"
                         >
                             <SubNav
-                                subnav={NAV.subNavSections}
+                                subNavSections={NAV.subNavSections}
                                 currentNavLink={NAV.currentNavLink}
                                 pillar={CAPI.pillar}
                             />
@@ -462,7 +462,7 @@ export const StandardLayout = ({ CAPI, NAV }: Props) => {
             {NAV.subNavSections && (
                 <Section padded={false} islandId="sub-nav-root">
                     <SubNav
-                        subnav={NAV.subNavSections}
+                        subNavSections={NAV.subNavSections}
                         pillar={CAPI.pillar}
                         currentNavLink={NAV.currentNavLink}
                     />
