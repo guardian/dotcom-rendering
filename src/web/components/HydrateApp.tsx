4447c19da7bce882c4a2beb58c699e6ee862a0e3
Oliver Lloyd <email@oliverlloyd.com>
2020-03-12 13:24:02 +0000

    Added CAPIBrowserType as part of merge

--- src/web/components/HydrateApp.tsx
+++ src/web/components/HydrateApp.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 
 import { App } from '@root/src/web/components/App';
 
-export const HydrateApp = ({ CAPI, NAV }: { CAPI: CAPIType; NAV: NavType }) => {
+type Props = {
+    CAPI: CAPIBrowserType;
+    NAV: NavType;
+};
+
+export const HydrateApp = ({ CAPI, NAV }: Props) => {
     ReactDOM.render(
         <App CAPI={CAPI} NAV={NAV} />,
         document.getElementById('react-root'),
