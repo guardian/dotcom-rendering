b764b9eeac5d1f57d30f2ef2d3a0241ed3a5ffd0
Francesca Hammond <francesca.hammond@guardian.co.uk>
2020-03-18 11:00:12 +0000

    Add ads and slideshow to galleries

--- src/client/article.ts
+++ src/client/article.ts
@@ -1,9 +1,7 @@
 // ----- Imports ----- //
 
-import { commercialClient, userClient, notificationsClient, galleryClient, acquisitionsClient } from 'native/nativeApi';
-import { AdSlot } from 'bridget-typescript/AdSlot'
+import { notificationsClient, acquisitionsClient } from 'native/nativeApi';
 import { Topic } from 'bridget-typescript/Topic';
-import { Image } from 'bridget-typescript/Image';
 import { IMaybeEpic as MaybeEpic } from 'bridget-typescript/MaybeEpic';
 import { formatDate } from 'date';
 import { logger } from "../logger";
@@ -11,6 +9,7 @@ import { createElement as h } from 'react';
 import setup from 'client/setup';
 import Epic from 'components/shared/epic';
 import ReactDOM from 'react-dom';
+import {ads, slideshow} from 'client/nativeCommunication';
 
 // ----- Run ----- //
 
@@ -22,64 +21,6 @@ declare global {
     interface Document {
         fonts: FontFaceSet;
     }
-}
-
-function getAdSlots(): AdSlot[] {
-    const advertSlots = document.getElementsByClassName('ad-slot');
-
-    if (!advertSlots) {
-        return [];
-    }
-
-    const scrollLeft = document.scrollingElement 
-        ? document.scrollingElement.scrollLeft : document.body.scrollLeft;
-    const scrollTop = document.scrollingElement 
-        ? document.scrollingElement.scrollTop : document.body.scrollTop;
-
-    return Array.from(advertSlots).map(adSlot => {
-        const slotPosition = adSlot.getBoundingClientRect();
-        return new AdSlot({
-            x: slotPosition.left + scrollLeft,
-            y: slotPosition.top + scrollTop,
-            width: slotPosition.width,
-            height: slotPosition.height
-        })
-    });
-}
-
-function insertAds(): void {
-    let adSlots = getAdSlots();
-    if (adSlots.length > 0) {
-        commercialClient.insertAdverts(adSlots);
-        const targetNode = document.querySelector('body') as Node;
-        const config = { attributes: true, childList: true, subtree: true };
-        const callback = function(): void {
-            const currentAdSlots = getAdSlots();
-            if (JSON.stringify(adSlots) !== JSON.stringify(currentAdSlots)) {
-                adSlots = currentAdSlots;
-                commercialClient.updateAdverts(currentAdSlots);
-            }
-        };
-
-        const observer = new MutationObserver(callback);
-        observer.observe(targetNode, config);
-
-        try {
-            document.fonts.ready.then(() => commercialClient.updateAdverts(getAdSlots()));
-        } catch (e) {
-            logger.error(`font loading API not supported: ${e}`)
-        }
-    }
-}
-
-function ads(): void {
-    userClient.isPremium().then(premiumUser => {
-        if (!premiumUser) {
-            Array.from(document.querySelectorAll('.ad-placeholder'))
-                 .forEach(placeholder => placeholder.classList.remove('hidden'))
-            insertAds();
-        }
-    })
 }
 
 function topicClick(e: Event): void {
@@ -127,28 +68,6 @@ function topics(): void {
             status.textContent = "Following";
         }
     })
-}
-
-function launchSlideshow(src: string | null): void {
-    const images = Array.from(document.querySelectorAll('.js-launch-slideshow'));
-    const imagesWithCaptions: Image[] = images.flatMap((image: Element) => {
-        const url = image.getAttribute('src');
-        const caption =  image.getAttribute('data-caption') ?? undefined;
-        const credit = image.getAttribute('data-credit') ?? undefined;
-        return url ? new Image({ url, caption, credit }) : [];
-    });
-    const clickedImageIndex = images.findIndex((image: Element) => image.getAttribute('src') === src);
-    if (imagesWithCaptions.length && clickedImageIndex >= 0) {
-        galleryClient.launchSlideshow(imagesWithCaptions, clickedImageIndex);
-    }
-}
-
-function slideshow(): void {
-    const images = document.querySelectorAll('.js-launch-slideshow');
-    Array.from(images)
-        .forEach((image: Element) => image.addEventListener('click', (e: Event) => {
-            launchSlideshow(image.getAttribute('src'));
-        }));
 }
 
 function formatDates(): void {
