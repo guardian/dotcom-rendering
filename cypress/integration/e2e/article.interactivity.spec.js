31a0301bef96a3c2b7b5c7019c4441ba088f070f
Oliver Lloyd <email@oliverlloyd.com>
2020-03-19 14:26:11 +0000

    Slow things down a bit to deal with CI failures

--- cypress/integration/e2e/article.interactivity.spec.js
+++ cypress/integration/e2e/article.interactivity.spec.js
@@ -57,12 +57,12 @@ describe('Interactivity', function() {
 
         it('should render the reader revenue links in the header and footer', function() {
             cy.visit(`/Article?url=${articleUrl}`);
-            cy.scrollTo('bottom', { duration: 100 });
+            cy.scrollTo('bottom', { duration: 1000 });
             cy.get('header')
                 .contains(READER_REVENUE_TITLE_TEXT)
                 .should('be.visible');
             cy.get('footer')
-                .contains(READER_REVENUE_TITLE_TEXT)
+                .contains(READER_REVENUE_TITLE_TEXT, { timeout: 10000 })
                 .should('be.visible');
         });
     });
