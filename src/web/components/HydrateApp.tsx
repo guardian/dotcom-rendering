14cf637affa56e180b1f33576c93ec9a4f093703
William Lacroix <william.lacroix.extern@joyn.de>
2020-03-20 10:45:00 +0000

    fix test

--- src/web/components/HydrateApp.tsx
+++ src/web/components/HydrateApp.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { loadableReady } from '@loadable/component'
 
 import { App } from '@root/src/web/components/App';
 
@@ -9,14 +8,9 @@ type Props = {
     NAV: NavType;
 };
 
-// https://loadable-components.com/docs/server-side-rendering/
-// Loadable components loads all your scripts asynchronously to ensure optimal performances.
-// All scripts are loaded in parallel, so you have to wait for them to be ready using loadableReady.
 export const HydrateApp = ({ CAPI, NAV }: Props) => {
-    loadableReady(() => {
-        ReactDOM.render(
-            <App CAPI={CAPI} NAV={NAV} />,
-            document.getElementById('react-root'),
-        );
-    })
+    ReactDOM.render(
+        <App CAPI={CAPI} NAV={NAV} />,
+        document.getElementById('react-root'),
+    );
 };
