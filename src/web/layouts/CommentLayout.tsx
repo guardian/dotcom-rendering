ca5ce4687d33197d00d26746a988cab862ffabac
Pascal Honoré <pascal@alseyn.net>
2020-04-03 12:34:53 +0100

    Introduce the 'comments' ad slot

--- src/web/layouts/CommentLayout.tsx
+++ src/web/layouts/CommentLayout.tsx
@@ -425,7 +425,9 @@ export const CommentLayout = ({ CAPI, NAV }: Props) => {
                             <Flex>
                                 <div id="comments-root" />
                                 <RightColumn>
-                                    {/* TODO: Comments ad slot goes here */}
+                                    <AdSlot
+                                        asps={namedAdSlotParameters('comments')}
+                                    />
                                 </RightColumn>
                             </Flex>
                         </Section>
