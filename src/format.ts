237fe272a13eea03307a535c304249cf1363d3d7
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-04-20 11:15:01 +0100

    Replaced format module with @guardian/types

--- src/format.ts
+++ src/format.ts
@@ -1,40 +1,6 @@
-// ----- Types ----- //
+// ----- Imports ----- //
 
-const enum Pillar {
-    News,
-    Opinion,
-    Sport,
-    Culture,
-    Lifestyle,
-}
-
-const enum Design {
-    Article,
-    Media,
-    Review,
-    Analysis,
-    Comment,
-    Feature,
-    Live,
-    Recipe,
-    MatchReport,
-    Interview,
-    GuardianView,
-    Quiz,
-    AdvertisementFeature,
-}
-
-const enum Display {
-    Standard,
-    Immersive,
-    Showcase,
-}
-
-interface Format {
-    pillar: Pillar;
-    design: Design;
-    display: Display;
-}
+import { Pillar, Design, Display, Format } from '@guardian/types/Format'
 
 
 // ----- Exports ----- //
