d37343ae3d6806ce1236dfc98732eef4698e9b09
Oliver Lloyd <email@oliverlloyd.com>
2020-04-27 10:06:09 +0100

    Removed unused route

--- cypress/integration/e2e/article.elements.spec.js
+++ cypress/integration/e2e/article.elements.spec.js
@@ -22,13 +22,6 @@ describe('Elements', function() {
         };
 
         it('should render the corona embed', function() {
-            // https://api.nextgen.guardianapps.co.uk/embed/atom/interactive/interactives%2F2020%2F03%2Fcovid-19-uk%2Fdefault#amp=1
-            cy.server();
-            cy.route({
-                method: 'GET',
-                url: '**/interactives/**/covid-19-uk/**',
-            }).as('getCoronaEmbed');
-
             cy.visit(
                 'AMPArticle?url=https://www.theguardian.com/world/2020/apr/24/new-mother-dies-of-coronavirus-six-days-after-giving-birth',
             );
