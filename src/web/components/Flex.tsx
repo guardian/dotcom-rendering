02bc9214e36c379bc11fa99a85b75429ef353a65
Joshua <joshuathomaslieberman@gmail.com>
2020-04-29 17:44:42 +0100

    stylelint instead of eslint

--- src/web/components/Flex.tsx
+++ src/web/components/Flex.tsx
@@ -12,18 +12,16 @@ export const Flex = ({
     direction = 'row',
     justify = 'space-between',
 }: Props) => (
-    /* eslint-disable property-no-vendor-prefix */
     <div
         className={css`
             display: flex;
             flex-direction: ${direction};
             justify-content: ${justify};
-
             /* Fixes IE 10/11 bug that collapses this container by default: */
+            /* stylelint-disable-next-line property-no-vendor-prefix */
             -ms-flex-positive: 1;
         `}
     >
         {children}
     </div>
-    /* eslint-enable property-no-vendor-prefix */
 );
