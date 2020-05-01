b581ecee62ba6a8eeeaef1b2c2378d080bde43af
Oliver Lloyd <email@oliverlloyd.com>
2020-05-01 13:07:08 +0100

    Really prevent the border from showing

--- src/web/components/Border.tsx
+++ src/web/components/Border.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { css } from 'emotion';
 
 import { border } from '@guardian/src-foundations/palette';
-import { until } from '@guardian/src-foundations/mq';
+import { from } from '@guardian/src-foundations/mq';
 
 export const Border = () => (
     <div
         className={css`
-            ${until.leftCol} {
-                visibility: collapse;
+            ${from.leftCol} {
+                border-left: 1px solid ${border.secondary};
+                height: 100%;
             }
-
-            border-left: 1px solid ${border.secondary};
-            height: 100%;
         `}
     />
 );
