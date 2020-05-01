7b14e31ca7c550af3a38bd3ae60d4ef166dc019a
Oliver Lloyd <email@oliverlloyd.com>
2020-05-01 11:08:20 +0100

    Don't display Border below leftCol

--- src/web/components/Border.tsx
+++ src/web/components/Border.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { css } from 'emotion';
 
 import { border } from '@guardian/src-foundations/palette';
+import { until } from '@guardian/src-foundations/mq';
 
 export const Border = () => (
     <div
         className={css`
+            ${until.leftCol} {
+                visibility: collapse;
+            }
+
             border-left: 1px solid ${border.secondary};
             height: 100%;
         `}
