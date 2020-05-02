0e15ad95dcdc9921b20f55efefba5adc2307a89e
Oliver Lloyd <email@oliverlloyd.com>
2020-05-02 14:58:09 +0100

    Added switch statement for coverage

--- src/model/enhance.ts
+++ src/model/enhance.ts
@@ -1,4 +1,37 @@
 const enhancedElement = (element: CAPIElement): CAPIElement => {
+    switch (element._type) {
+        case 'model.dotcomrendering.pageElements.TextBlockElement':
+        case 'model.dotcomrendering.pageElements.SubheadingBlockElement':
+        case 'model.dotcomrendering.pageElements.RichLinkBlockElement':
+        case 'model.dotcomrendering.pageElements.ImageBlockElement':
+        case 'model.dotcomrendering.pageElements.YoutubeBlockElement':
+        case 'model.dotcomrendering.pageElements.VideoYoutubeBlockElement':
+        case 'model.dotcomrendering.pageElements.VideoVimeoBlockElement':
+        case 'model.dotcomrendering.pageElements.VideoFacebookBlockElement':
+        case 'model.dotcomrendering.pageElements.GuVideoBlockElement':
+        case 'model.dotcomrendering.pageElements.InstagramBlockElement':
+        case 'model.dotcomrendering.pageElements.TweetBlockElement':
+        case 'model.dotcomrendering.pageElements.CommentBlockElement':
+        case 'model.dotcomrendering.pageElements.SoundcloudBlockElement':
+        case 'model.dotcomrendering.pageElements.EmbedBlockElement':
+        case 'model.dotcomrendering.pageElements.DisclaimerBlockElement':
+        case 'model.dotcomrendering.pageElements.PullquoteBlockElement':
+        case 'model.dotcomrendering.pageElements.BlockquoteBlockElement':
+        case 'model.dotcomrendering.pageElements.QABlockElement':
+        case 'model.dotcomrendering.pageElements.GuideBlockElement':
+        case 'model.dotcomrendering.pageElements.ProfileBlockElement':
+        case 'model.dotcomrendering.pageElements.TimelineBlockElement':
+        case 'model.dotcomrendering.pageElements.AtomEmbedMarkupBlockElement':
+        case 'model.dotcomrendering.pageElements.AtomEmbedUrlBlockElement':
+        case 'model.dotcomrendering.pageElements.MapBlockElement':
+        case 'model.dotcomrendering.pageElements.AudioAtomBlockElement':
+        case 'model.dotcomrendering.pageElements.ContentAtomBlockElement':
+        case 'model.dotcomrendering.pageElements.AudioBlockElement':
+        case 'model.dotcomrendering.pageElements.VideoBlockElement':
+        case 'model.dotcomrendering.pageElements.CodeBlockElement':
+        default:
+            return element;
+    }
     return element;
 };
 
