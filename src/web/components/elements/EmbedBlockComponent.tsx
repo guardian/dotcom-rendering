bf35bd3dc79fe1ad9953ffdd22021fb9d5970c95
Rob Phillips <robert.phillips@guardian.co.uk>
2020-05-04 13:26:15 +0100

    adding styles for alt text element

--- src/web/components/elements/EmbedBlockComponent.tsx
+++ src/web/components/elements/EmbedBlockComponent.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 import { unescapeData } from '@root/src/lib/escapeData';
 import { css } from 'emotion';
+import { textSans } from '@guardian/src-foundations/typography';
+import { text } from '@guardian/src-foundations/palette';
 
 type Props = {
     html: string;
     alt?: string;
 };
 
+const emailCaptionStyle = css`
+    ${textSans.xsmall()};
+    word-wrap: break-word;
+    color: ${text.supporting};
+`;
+
 const widthOverride = css`
     iframe {
         /* Some embeds can hijack the iframe and calculate an incorrect width, which pushes the body out */
         width: 100% !important;
     }
+    margin-bottom: 16px;
 `;
 
 export const EmbedBlockComponent = ({ html, alt }: Props) => {
@@ -20,7 +29,9 @@ export const EmbedBlockComponent = ({ html, alt }: Props) => {
     return (
         <div className={widthOverride}>
             <div dangerouslySetInnerHTML={{ __html: unescapeData(html) }} />
-            {isEmailEmbed && alt && <div>{alt}</div>}
+            {isEmailEmbed && alt && (
+                <div className={emailCaptionStyle}>{alt}</div>
+            )}
         </div>
     );
 };
