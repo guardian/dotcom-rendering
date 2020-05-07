8b14b4be89fec59fbf9f014242fa974ea6afcfb5
Oliver Lloyd <email@oliverlloyd.com>
2020-05-07 13:25:56 +0100

    Added styles for divider

--- src/web/components/elements/DividerBlockComponent.tsx
+++ src/web/components/elements/DividerBlockComponent.tsx
@@ -1,3 +1,18 @@
 import React from 'react';
+import { css } from 'emotion';
 
-export const DividerBlockComponent = () => <hr />;
+import { border } from '@guardian/src-foundations/palette';
+
+export const DividerBlockComponent = () => (
+    <hr
+        className={css`
+            width: 150px;
+            height: 1px;
+            border: 0;
+            margin-left: -10px;
+            margin-top: 48px;
+            margin-bottom: 3px;
+            background-color: ${border.secondary};
+        `}
+    />
+);
