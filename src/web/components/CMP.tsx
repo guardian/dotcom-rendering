f3ee686d21da42492f74fa8936803ac561c232f0
William Lacroix <william.lacroix.extern@joyn.de>
2020-05-11 16:34:59 +0100

    dynamic import consent-management-platform-utilities

--- src/web/components/CMP.tsx
+++ src/web/components/CMP.tsx
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import {
-    shouldShow,
-    setErrorHandler,
-} from '@guardian/consent-management-platform';
+
 import { ConsentManagementPlatform } from '@guardian/consent-management-platform/lib/ConsentManagementPlatform';
+import { initPerf } from '@root/src/web/browser/initPerf';
 
 export const CMP = () => {
     const [show, setShow] = useState(false);
 
     useEffect(() => {
-        if (shouldShow()) {
-            setShow(true);
+        const { start, end } = initPerf(
+            'consent-management-platform-utilities',
+        );
+        start();
+        import(
+            /* webpackChunkName: "consent-management-platform-utilities" */ '@guardian/consent-management-platform'
+        ).then(({ shouldShow, setErrorHandler }) => {
+            end();
+            if (shouldShow()) {
+                setShow(true);
 
-            // setErrorHandler takes function to be called on errors in the CMP UI
-            setErrorHandler((errMsg: string): void => {
-                const err = new Error(errMsg);
+                // setErrorHandler takes function to be called on errors in the CMP UI
+                setErrorHandler((errMsg: string): void => {
+                    const err = new Error(errMsg);
 
-                window.guardian.modules.sentry.reportError(err, 'cmp');
-            });
-        }
+                    window.guardian.modules.sentry.reportError(err, 'cmp');
+                });
+            }
+        });
     }, []);
 
     return (
