84fdb05dd30b3454f7d37ee937925d62907587d0
Joshua <joshuathomaslieberman@gmail.com>
2020-05-13 12:11:19 +0100

    removing more aws code

--- src/app/aws/metrics-baseline.ts
+++ src/app/aws/metrics-baseline.ts
@@ -1,44 +1,44 @@
-import os from 'os';
-import disk from 'diskusage';
-import { BytesMetric, collectAndSendAWSMetrics } from './aws-metrics';
+// import os from 'os';
+// import disk from 'diskusage';
+// import { BytesMetric, collectAndSendAWSMetrics } from './aws-metrics';
 
-const maxHeapMemory = BytesMetric('rendering', 'PROD', 'max-heap-memory');
-const freeDiskSpace = BytesMetric('rendering', 'PROD', 'free-disk-memory');
-const usedHeapMemory = BytesMetric('rendering', 'PROD', 'used-heap-memory');
-const freePhysicalMemory = BytesMetric(
-    'rendering',
-    'PROD',
-    'free-physical-memory',
-);
-const totalPhysicalMemory = BytesMetric(
-    'rendering',
-    'PROD',
-    'total-physical-memory',
-);
+// const maxHeapMemory = BytesMetric('rendering', 'PROD', 'max-heap-memory');
+// const freeDiskSpace = BytesMetric('rendering', 'PROD', 'free-disk-memory');
+// const usedHeapMemory = BytesMetric('rendering', 'PROD', 'used-heap-memory');
+// const freePhysicalMemory = BytesMetric(
+//     'rendering',
+//     'PROD',
+//     'free-physical-memory',
+// );
+// const totalPhysicalMemory = BytesMetric(
+//     'rendering',
+//     'PROD',
+//     'total-physical-memory',
+// );
 
 // transmits metrics to AWS
 
-collectAndSendAWSMetrics(
-    maxHeapMemory,
-    usedHeapMemory,
-    totalPhysicalMemory,
-    freePhysicalMemory,
-    freeDiskSpace,
-);
+// collectAndSendAWSMetrics(
+//     maxHeapMemory,
+//     usedHeapMemory,
+//     totalPhysicalMemory,
+//     freePhysicalMemory,
+//     freeDiskSpace,
+// );
 
 // records system metrics
 
-export const recordBaselineCloudWatchMetrics = () => {
-    disk.check('/', (err, diskinfo) => {
-        if (err) {
-            // eslint-disable-next-line no-console
-            console.error(err);
-        } else {
-            maxHeapMemory.record(process.memoryUsage().heapTotal);
-            usedHeapMemory.record(process.memoryUsage().heapUsed);
-            totalPhysicalMemory.record(os.totalmem());
-            freePhysicalMemory.record(os.freemem());
-            freeDiskSpace.record(diskinfo.free);
-        }
-    });
-};
+// export const recordBaselineCloudWatchMetrics = () => {
+//     disk.check('/', (err, diskinfo) => {
+//         if (err) {
+//             // eslint-disable-next-line no-console
+//             console.error(err);
+//         } else {
+//             maxHeapMemory.record(process.memoryUsage().heapTotal);
+//             usedHeapMemory.record(process.memoryUsage().heapUsed);
+//             totalPhysicalMemory.record(os.totalmem());
+//             freePhysicalMemory.record(os.freemem());
+//             freeDiskSpace.record(diskinfo.free);
+//         }
+//     });
+// };
