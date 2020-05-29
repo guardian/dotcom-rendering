95ec5ce4516fd65f96139da8405e458ac422177d
Oliver Lloyd <email@oliverlloyd.com>
2020-05-29 17:49:25 +0100

    Added Donut component

--- src/web/components/Donut.tsx
+++ src/web/components/Donut.tsx
@@ -1,63 +1,62 @@
 import React from 'react';
-// import { css } from 'emotion';
+import { css } from 'emotion';
 
-type Props = {};
+import { headline } from '@guardian/src-foundations/typography';
+import { text } from '@guardian/src-foundations/palette';
 
-export const Donut = ({  }: Props) => {
-    const data = [
-        {
-            value: 20,
-            label: 'Dat1',
-            color: 'red',
-        },
-        {
-            value: 80,
-            label: 'Dat2',
-            color: 'blue',
-        },
-    ];
+type Props = {
+    sections: Section[];
+    percentCutout?: number;
+    width?: number;
+    height?: number;
+};
 
-    const options: {
-        percentCutout: number;
-        unit: string;
-        showValues: boolean;
-        width: number;
-        height: number;
-    } = {
-        percentCutout: 35,
-        unit: '',
-        showValues: false,
-        width: 200,
-        height: 200,
-    };
+type Section = {
+    label: string;
+    value: number;
+    color: string;
+};
 
-    const w = options.width;
-    const h = options.height || w;
-    const radius = Math.min(h / 2, w / 2);
-    const cutoutRadius = radius * (options.percentCutout / 100);
+const unitStyles = css`
+    ${headline.medium({ fontWeight: 'bold' })}
+    text-anchor: middle;
+`;
 
-    const totalValue = data
-        .map(item => item.value)
+const valueStyles = css`
+    ${headline.small({ fontWeight: 'bold' })}
+    fill: ${text.ctaPrimary};
+    text-anchor: middle;
+`;
+
+const labelStyles = css`
+    ${headline.xxsmall()}
+    fill: ${text.ctaPrimary};
+    text-anchor: middle;
+`;
+
+export const Donut = ({
+    sections,
+    percentCutout = 35,
+    width = 300,
+    height = 300,
+}: Props) => {
+    const radius = Math.min(height / 2, width / 2);
+    const cutoutRadius = radius * (percentCutout / 100);
+
+    const totalValue = sections
+        .map(section => section.value)
         .reduce((prev, next) => prev + next);
 
     const halfPI = Math.PI / 2;
     const doublePI = Math.PI * 2;
-    const c = [w / 2, h / 2];
 
     const center = {
-        x: w / 2,
-        y: h / 2,
+        x: width / 2,
+        y: height / 2,
     };
 
-    // const $svg = $.create(
-    //     '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMinYMin" class="chart chart--doughnut"></svg>'
-    // ).attr({
-    //     width: w,
-    //     height: h,
-    //     viewbox: `0 0 ${[w, h].join(' ')}`,
-    // });
-
     // Segments
+    const segments: any[] = [];
     let segmentAngle;
     let endRadius;
     let arc;
@@ -65,16 +64,14 @@ export const Donut = ({  }: Props) => {
     let inner;
     let r;
     let a;
-    let d;
-    let $g;
-    let $t;
-    const startRadius = -halfPI;
+    let startRadius = -halfPI;
 
-    data.forEach(datum => {
-        segmentAngle = (datum.value / totalValue) * doublePI;
+    sections.forEach(section => {
+        segmentAngle = (section.value / totalValue) * doublePI;
+
         endRadius = startRadius + segmentAngle;
         arc = (endRadius - startRadius) % doublePI > Math.PI ? 1 : 0;
-        // TODO (jamesgorrie): functionalise
+
         outer = {
             start: {
                 x: center.x + Math.cos(startRadius) * radius,
@@ -98,91 +95,78 @@ export const Donut = ({  }: Props) => {
 
         r = (cutoutRadius + radius) / 2;
         a = (startRadius + endRadius) / 2;
-        /**
-         * M: Move pointer
-         * A: Outer arc
-         * L: Connect outer and inner arc
-         * A: Inner arc
-         * Z: Close path
-         */
-        d = [
-            'M',
-            outer.start.x,
-            outer.start.y,
-            'A',
-            radius,
-            radius,
-            0,
-            arc,
-            1,
-            outer.end.x,
-            outer.end.y,
-            'L',
-            inner.start.x,
-            inner.start.y,
-            'A',
-            cutoutRadius,
-            cutoutRadius,
-            0,
-            arc,
-            0,
-            inner.end.x,
-            inner.end.y,
-            'Z',
-        ];
-        // $g = svgEl('g')
-        //     .attr('class', 'chart__arc')
-        //     .append(
-        //         svgEl('path').attr({
-        //             d: d.join(' '),
-        //             fill: datum.color,
-        //         })
-        //     );
 
-        // labels
-        // $t = svgEl('text').attr('class', 'chart__label');
-        // if (options.showValues) {
-        //     $t.append(
-        //         svgEl('tspan')
-        //             .attr('class', 'chart__label-text')
-        //             .text(datum.label)
-        //             .attr({
-        //                 x: 0,
-        //                 dy: '0',
-        //             })
-        //     ).append(
-        //         svgEl('tspan')
-        //             .attr('class', 'chart__label-value')
-        //             .text(datum.value)
-        //             .attr({
-        //                 x: 0,
-        //                 dy: '.9em',
-        //             })
-        //     );
-        // } else {
-        //     $t.text(datum.label);
-        // }
-        // $t.attr({
-        //     transform: translate([
-        //         Math.cos(a) * r + center.x,
-        //         Math.sin(a) * r + center.y,
-        //     ]),
-        // }).appendTo($g);
+        segments.push({
+            /**
+             * M: Move pointer
+             * A: Outer arc
+             * L: Connect outer and inner arc
+             * A: Inner arc
+             * Z: Close path
+             */
+            d: [
+                'M',
+                outer.start.x,
+                outer.start.y,
+                'A',
+                radius,
+                radius,
+                0,
+                arc,
+                1,
+                outer.end.x,
+                outer.end.y,
+                'L',
+                inner.start.x,
+                inner.start.y,
+                'A',
+                cutoutRadius,
+                cutoutRadius,
+                0,
+                arc,
+                0,
+                inner.end.x,
+                inner.end.y,
+                'Z',
+            ].join(' '),
+            label: section.label,
+            value: section.value,
+            transform: `translate(${Math.cos(a) * r + center.x}, ${Math.sin(a) *
+                r +
+                center.y})`,
+            color: section.color,
+        });
 
-        // $g.appendTo($svg);
-        // startRadius += (datum.value / totalValue) * doublePI;
+        startRadius += (section.value / totalValue) * doublePI;
     });
 
-    // Unit of measurement
-    // return $svg.append(
-    //     svgEl('text')
-    //         .attr('class', 'chart__unit')
-    //         .text(options.unit)
-    //         .attr({
-    //             transform: translate(c),
-    //             dy: '0.4em',
-    //         })
-    // );
-
-    return <div>donut</div>;
+    return (
+        <svg
+            preserveAspectRatio="xMinYMin"
+            width={width}
+            height={height}
+            viewBox={`0 0 ${width} ${height}`}
+        >
+            {segments.map(segment => (
+                <g>
+                    <path d={segment.d} fill={segment.color} />
+                    <text transform={segment.transform}>
+                        <tspan className={labelStyles} x="0" dy="0">
+                            {segment.label}
+                        </tspan>
+                        <tspan className={valueStyles} x="0" dy=".9em">
+                            {segment.value}
+                        </tspan>
+                    </text>
+                </g>
+            ))}
+            <text
+                className={unitStyles}
+                transform={`translate(${center.x}, ${center.y})`}
+                dy="0.4em"
+            >
+                %
+            </text>
+        </svg>
+    );
 };
