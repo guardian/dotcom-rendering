4bc6446a05a339876600838d088e339661bca208
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-06-03 16:30:16 +0100

    Introduce Fixture Module For Item (#363)
    - New fixtures file to generate Item data structures
    - Updated headline, standfirst and starRating to use Item fixtures

--- src/components/starRating.stories.tsx
+++ src/components/starRating.stories.tsx
@@ -4,37 +4,10 @@ import React, { ReactElement } from 'react';
 import { withKnobs, radios } from '@storybook/addon-knobs';
 
 import StarRating from './starRating';
-import { Item } from 'item';
-import { Pillar, Design, Display } from 'format';
-import { None, Some } from 'types/option';
+import { article, review } from 'fixtures/item';
 
 
 // ----- Setup ----- //
-
-const item: Item = {
-    pillar: Pillar.News,
-    design: Design.Article,
-    display: Display.Standard,
-    body: [],
-    headline: 'Reclaimed lakes and giant airports: how Mexico City might have looked',
-    standfirst: new None(),
-    byline: '',
-    bylineHtml: new None(),
-    publishDate: new None(),
-    mainImage: new None(),
-    contributors: [],
-    series: new Some({
-        id: '',
-        type: 0,
-        webTitle: '',
-        webUrl: '',
-        apiUrl: '',
-        references: [],
-    }),
-    commentable: false,
-    tags: [],
-    shouldHideReaderRevenue: false,
-};
 
 const starRating: Record<number, number> = [0, 1, 2, 3, 4, 5];
 
@@ -43,13 +16,12 @@ const starRating: Record<number, number> = [0, 1, 2, 3, 4, 5];
 
 const Default = (): ReactElement =>
     <StarRating item={{
-        ...item,
-        design: Design.Review,
+        ...review,
         starRating: radios('Rating', starRating, 3),
     }} />
 
 const NotReview = (): ReactElement =>
-    <StarRating item={item} />
+    <StarRating item={article} />
 
 
 // ----- Exports ----- //
