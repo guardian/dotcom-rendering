151681403f92a102b03ab5dcca520c65c6659582
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2020-06-04 17:24:13 +0100

    Add bwid getCookieValue

--- src/web/server/htmlTemplate.ts
+++ src/web/server/htmlTemplate.ts
@@ -207,6 +207,31 @@ export const htmlTemplate = ({
                     };
                 </script>
 
+                <script>
+                    // Set the browserId from the bwid cookie on the ophan object created above
+                    // This will need to be replaced later with an async request to an endpoint
+                    (function (window, document) {
+
+                        function getCookieValue(name) {
+                            var nameEq = name + "=",
+                                cookies = document.cookie.split(';'),
+                                value = null;
+                            cookies.forEach(function (cookie) {
+                                while (cookie.charAt(0) === ' ') {
+                                    cookie = cookie.substring(1, cookie.length);
+                                }
+                                if (cookie.indexOf(nameEq) === 0) {
+                                    value = cookie.substring(nameEq.length, cookie.length);
+                                }
+                            });
+                            return value;
+                        }
+
+                        window.guardian.config.ophan.browserId = getCookieValue("bwid");
+
+                    })(window, document);
+                </script>
+
                 <script>${prepareCmpString}</script>
 
                 <noscript>
