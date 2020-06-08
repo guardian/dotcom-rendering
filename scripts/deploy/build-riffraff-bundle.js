b00781ff7f3312fdf303a2a018ea7ce60fdfaf3f
Nicolas Long <nicolas.long@theguardian.com>
2020-06-08 14:56:52 +0100

    Solve maxBuffer error

--- scripts/deploy/build-riffraff-bundle.js
+++ scripts/deploy/build-riffraff-bundle.js
@@ -71,6 +71,7 @@ const zipBundle = () => {
         ['--recurse-paths', 'rendering.zip', '.', '--exclude', '.git/**\\*'],
         {
             shell: true,
+            maxBuffer: 200000000, // increase if you get a maxBuffer exceeded error
         },
     ).then(() => {
         cpy(['rendering.zip'], path.resolve(target, 'rendering', 'dist'));
