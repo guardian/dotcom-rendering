ed3e61027d26bb2d22f6b96b8fa1dc841db4b3c8
Oliver Lloyd <email@oliverlloyd.com>
2020-06-09 13:55:45 +0100

    Remove this override now that the problem it was fixing is no longer there

--- src/web/components/elements/TweetBlockComponent.tsx
+++ src/web/components/elements/TweetBlockComponent.tsx
@@ -19,17 +19,6 @@ const noJSStyling = css`
         padding-bottom: 10px;
     }
 
-    /* Why are we using important here? Because this is the only way to override
-    the inline styles of the twitter-widget element and for some reason twitter
-    is adding display: block and width: 500px on this embed causing it to overflow.
-    I did think this might be caused by
-    https://stackoverflow.com/questions/36247140/why-dont-flex-items-shrink-past-content-size
-    but the solutions posted there did not help. Checking other sites with twitter embeds
-    like the BBC, we saw that this important override was being used as well. */
-    .twitter-tweet-rendered {
-        display: inline !important;
-    }
-
     a {
         /* stylelint-disable-next-line color-no-hex */
         color: #2b7bb9;
