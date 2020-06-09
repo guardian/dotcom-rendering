2514e092cbb04c64c1da24fa42d477a04b09d844
Oliver Lloyd <email@oliverlloyd.com>
2020-06-09 09:40:03 +0100

    Remove children before inserting content

--- src/web/components/Portal.tsx
+++ src/web/components/Portal.tsx
@@ -11,6 +11,11 @@ export const Portal = ({ root, children, richLinkIndex }: Props) => {
     const element = document.getElementById(rootId);
     if (!element) return null;
     window.performance.mark(`${rootId}-portal-start`);
+    // Remove any children. Why? Because we sometimes server render Placeholders in
+    // the root divs where the Portal is being inserted and we want to remove then
+    // when we load in the client side content
+    // See: https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    element.textContent = '';
     return ReactDOM.createPortal(children, element);
     window.performance.mark(`${rootId}-portal-end`);
     window.performance.measure(
