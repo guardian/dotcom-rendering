7ddd97d8dda3fea76923b14d1ec6dc934dbbc849
Jamie Webb <jamie.webb@guardian.co.uk>
2020-06-09 17:00:25 +0100

    disable increasing scale

--- src/server/page.tsx
+++ src/server/page.tsx
@@ -133,7 +133,7 @@ const Head: FC<HeadProps> = ({ webTitle }: HeadProps) =>
     <>
         <title>{webTitle}</title>
         <meta id="twitter-theme" name="twitter:widgets:theme" content="light" />
-        <meta name="viewport" content="initial-scale=1, maximum-scale=5" />
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
         <link rel="stylesheet" href="native://fontSize.css" />
     </>
 
