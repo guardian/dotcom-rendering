88721a5e60172fdbe05d9adf20d943710c4a761c
William Lacroix <william.lacroix.extern@joyn.de>
2020-06-09 16:19:18 +0100

    normalize callout block CAPI elements

---
+++ src/model/normalizeCallout.test.ts
@@ -0,0 +1,306 @@
+import { normalizeCallout } from './normalizeCallout';
+import { bodyJSON } from './exampleBodyJSON';
+
+const example = JSON.parse(bodyJSON);
+
+const preNormalizedCampaign: preNormalizedCampaignType = {
+    id: '14d1b1bc-8983-43fb-8f2e-8ca08a711944',
+    name: 'CALLOUT: early coronavirus events',
+    rules: [],
+    priority: 0,
+    activeFrom: 1588118400000,
+    displayOnSensitive: false,
+    fields: {
+        formId: 3860296,
+        callout: 'Were you infected at this time?',
+        _type: 'callout',
+        description:
+            '<p>If you attended one of these events and believe you may have been infected by coronavirus, we\'d like to hear from you. You can get in touch by filling in the form below, or by contacting us&nbsp;<a href="https://www.theguardian.com/info/2015/aug/12/whatsapp-sharing-stories-with-the-guardian">via WhatsApp</a>&nbsp;by&nbsp;<a href="https://api.whatsapp.com/send?phone=447867825056">clicking here&nbsp;</a>or adding the contact +44(0)7867825056. Only the Guardian can see your contributions and one of our journalists may contact you to discuss further. </p>',
+        formFields: [
+            {
+                text_size: 50,
+                name: 'name',
+                description: 'You can use your first name only',
+                hide_label: '0',
+                label: 'Name',
+                id: '91884870',
+                type: 'text',
+                required: '1',
+            },
+            {
+                name: 'share_your_experiences_here',
+                description: 'Please include as much detail as possible',
+                hide_label: '0',
+                label: 'Share your experiences here',
+                id: '91884874',
+                type: 'textarea',
+                required: '1',
+            },
+            {
+                name:
+                    'you_can_upload_a_photo_here_if_you_think_it_will_add_to_your_story',
+                hide_label: '0',
+                label:
+                    'You can upload a photo here if you think it will add to your story',
+                id: '91884877',
+                type: 'file',
+                required: '0',
+            },
+            {
+                name: 'can_we_publish_your_response',
+                options: [
+                    {
+                        label: 'Yes, entirely',
+                        value: 'Yes, entirely',
+                    },
+                    {
+                        label: 'Yes, but please keep me anonymous',
+                        value: 'Yes, but please keep me anonymous',
+                    },
+                    {
+                        label: 'Yes, but please contact me first',
+                        value: 'Yes, but please contact me first',
+                    },
+                    {
+                        label: 'No, this is information only',
+                        value: 'No, this is information only',
+                    },
+                ],
+                hide_label: '0',
+                label: 'Can we publish your response?',
+                id: '91884878',
+                type: 'radio',
+                required: '0',
+            },
+            {
+                name: 'can_we_publish_your_response',
+                options: [
+                    {
+                        label: 'Yes, entirely',
+                        value: 'Yes, entirely',
+                    },
+                    {
+                        label: 'Yes, but please keep me anonymous',
+                        value: 'Yes, but please keep me anonymous',
+                    },
+                    {
+                        label: 'Yes, but please contact me first',
+                        value: 'Yes, but please contact me first',
+                    },
+                    {
+                        label: 'No, this is information only',
+                        value: 'No, this is information only',
+                    },
+                ],
+                hide_label: '0',
+                label: 'Can we publish your response?',
+                id: '918848785',
+                type: 'checkbox',
+                required: '1',
+            },
+            {
+                name: 'do_you_have_anything_else_to_add',
+                hide_label: '0',
+                label: 'Do you have anything else to add?',
+                id: '91884881',
+                type: 'select',
+                options: [
+                    {
+                        label: 'Yes, entirely',
+                        value: 'Yes, entirely',
+                    },
+                    {
+                        label: 'Yes, but please keep me anonymous',
+                        value: 'Yes, but please keep me anonymous',
+                    },
+                    {
+                        label: 'Yes, but please contact me first',
+                        value: 'Yes, but please contact me first',
+                    },
+                    {
+                        label: 'No, this is information only',
+                        value: 'No, this is information only',
+                    },
+                ],
+                required: '0',
+            },
+        ],
+        tagName: 'callout-early-coronavirus-events',
+    },
+};
+
+const postNormalizedCampaign: CalloutBlockElement = {
+    _type: 'model.dotcomrendering.pageElements.CalloutBlockElement',
+    id: '14d1b1bc-8983-43fb-8f2e-8ca08a711944',
+    activeFrom: 1588118400000,
+    displayOnSensitive: false,
+    formId: 3860296,
+    title: 'Were you infected at this time?',
+    description:
+        '<p>If you attended one of these events and believe you may have been infected by coronavirus, we\'d like to hear from you. You can get in touch by filling in the form below, or by contacting us&nbsp;<a href="https://www.theguardian.com/info/2015/aug/12/whatsapp-sharing-stories-with-the-guardian">via WhatsApp</a>&nbsp;by&nbsp;<a href="https://api.whatsapp.com/send?phone=447867825056">clicking here&nbsp;</a>or adding the contact +44(0)7867825056. Only the Guardian can see your contributions and one of our journalists may contact you to discuss further. </p>',
+    tagName: 'callout-early-coronavirus-events',
+    formFields: [
+        {
+            textSize: 50,
+            name: 'name',
+            description: 'You can use your first name only',
+            hideLabel: false,
+            label: 'Name',
+            id: 91884870,
+            type: 'text',
+            required: true,
+        },
+        {
+            textSize: undefined,
+            name: 'share_your_experiences_here',
+            description: 'Please include as much detail as possible',
+            hideLabel: false,
+            label: 'Share your experiences here',
+            id: 91884874,
+            type: 'textarea',
+            required: true,
+        },
+        {
+            name:
+                'you_can_upload_a_photo_here_if_you_think_it_will_add_to_your_story',
+            hideLabel: false,
+            label:
+                'You can upload a photo here if you think it will add to your story',
+            id: 91884877,
+            type: 'file',
+            required: false,
+        },
+        {
+            name: 'can_we_publish_your_response',
+            options: [
+                {
+                    label: 'Yes, entirely',
+                    value: 'Yes, entirely',
+                },
+                {
+                    label: 'Yes, but please keep me anonymous',
+                    value: 'Yes, but please keep me anonymous',
+                },
+                {
+                    label: 'Yes, but please contact me first',
+                    value: 'Yes, but please contact me first',
+                },
+                {
+                    label: 'No, this is information only',
+                    value: 'No, this is information only',
+                },
+            ],
+            hideLabel: false,
+            label: 'Can we publish your response?',
+            id: 91884878,
+            type: 'radio',
+            required: false,
+        },
+        {
+            name: 'can_we_publish_your_response',
+            options: [
+                {
+                    label: 'Yes, entirely',
+                    value: 'Yes, entirely',
+                },
+                {
+                    label: 'Yes, but please keep me anonymous',
+                    value: 'Yes, but please keep me anonymous',
+                },
+                {
+                    label: 'Yes, but please contact me first',
+                    value: 'Yes, but please contact me first',
+                },
+                {
+                    label: 'No, this is information only',
+                    value: 'No, this is information only',
+                },
+            ],
+            hideLabel: false,
+            label: 'Can we publish your response?',
+            id: 918848785,
+            type: 'checkbox',
+            required: true,
+        },
+        {
+            name: 'do_you_have_anything_else_to_add',
+            hideLabel: false,
+            label: 'Do you have anything else to add?',
+            id: 91884881,
+            type: 'select',
+            options: [
+                {
+                    label: 'Yes, entirely',
+                    value: 'Yes, entirely',
+                },
+                {
+                    label: 'Yes, but please keep me anonymous',
+                    value: 'Yes, but please keep me anonymous',
+                },
+                {
+                    label: 'Yes, but please contact me first',
+                    value: 'Yes, but please contact me first',
+                },
+                {
+                    label: 'No, this is information only',
+                    value: 'No, this is information only',
+                },
+            ],
+            required: false,
+        },
+    ],
+};
+
+// We are assuming that there will only be 1 callout per page
+describe('Normalize callout campaigne data', () => {
+    it('convert relavant EmbedBlockElement to CalloutBlockElement', () => {
+        const input = {
+            ...example,
+            blocks: [
+                {
+                    elements: [
+                        {
+                            alt: 'Callout callout-early-coronavirus-events',
+                            _type:
+                                'model.dotcomrendering.pageElements.EmbedBlockElement',
+                            html:
+                                '<div data-callout-tagname="callout-early-coronavirus-events">          \n                               <h2>Callout</h2>\n                               <p>callout-early-coronavirus-events</p>\n                           </div>',
+                            isMandatory: false,
+                        },
+                        {
+                            _type:
+                                'model.dotcomrendering.pageElements.TextBlockElement',
+                            html: '<p>I should not be edited.</p>',
+                        },
+                    ],
+                },
+            ],
+            config: {
+                ...example.config,
+                campaigns: [{ name: 'should-not-edit' }, preNormalizedCampaign],
+            },
+        };
+
+        const expectedOutput = {
+            ...example,
+            blocks: [
+                {
+                    elements: [
+                        postNormalizedCampaign,
+                        {
+                            _type:
+                                'model.dotcomrendering.pageElements.TextBlockElement',
+                            html: '<p>I should not be edited.</p>',
+                        },
+                    ],
+                },
+            ],
+            config: {
+                ...example.config,
+                campaigns: [{ name: 'should-not-edit' }, preNormalizedCampaign],
+            },
+        };
+
+        expect(normalizeCallout(input)).toEqual(expectedOutput);
+    });
+});
---
+++ src/model/normalizeCallout.ts
@@ -0,0 +1,158 @@
+const formatCalloutObject = (
+    calloutObject: preNormalizedCampaignType,
+): CalloutBlockElement => ({
+    // this is a type suggestion for EmbedBlockElement that are acctually callout
+    _type: 'model.dotcomrendering.pageElements.CalloutBlockElement',
+    id: calloutObject.id,
+    activeFrom: calloutObject.activeFrom,
+    displayOnSensitive: calloutObject.displayOnSensitive,
+    formId: calloutObject.fields.formId,
+    title: calloutObject.fields.callout,
+    description: calloutObject.fields.description,
+    tagName: calloutObject.fields.tagName,
+    formFields: calloutObject.fields.formFields.map(
+        (field: preNormalizedCampaignFieldType): CampaignFieldType => {
+            switch (field.type) {
+                case 'text':
+                    return {
+                        id: Number(field.id),
+                        type: 'text',
+                        name: field.name,
+                        description: field.description,
+                        required: field.required === '1',
+                        textSize: field.text_size
+                            ? Number(field.text_size)
+                            : undefined,
+                        hideLabel: field.hide_label === '1',
+                        label: field.label,
+                    } as CampaignFieldText;
+                case 'textarea':
+                    return {
+                        id: Number(field.id),
+                        type: 'textarea',
+                        name: field.name,
+                        description: field.description,
+                        required: field.required === '1',
+                        textSize: field.text_size
+                            ? Number(field.text_size)
+                            : undefined,
+                        hideLabel: field.hide_label === '1',
+                        label: field.label,
+                    } as CampaignFieldTextArea;
+                case 'file':
+                    return {
+                        id: Number(field.id),
+                        type: 'file',
+                        name: field.name,
+                        required: field.required === '1',
+                        hideLabel: field.hide_label === '1',
+                        label: field.label,
+                    } as CampaignFieldFile;
+                case 'radio':
+                    return {
+                        id: Number(field.id),
+                        type: 'radio',
+                        name: field.name,
+                        required: field.required === '1',
+                        options: field.options.map(option => ({
+                            label: option.label,
+                            value: option.value,
+                        })),
+                        hideLabel: field.hide_label === '1',
+                        label: field.label,
+                    } as CampaignFieldRadio;
+                case 'checkbox':
+                    return {
+                        id: Number(field.id),
+                        type: 'checkbox',
+                        name: field.name,
+                        required: field.required === '1',
+                        options: field.options.map(option => ({
+                            label: option.label,
+                            value: option.value,
+                        })),
+                        hideLabel: field.hide_label === '1',
+                        label: field.label,
+                    } as CampaignFieldCheckbox;
+                case 'select':
+                    return {
+                        id: Number(field.id),
+                        type: 'select',
+                        name: field.name,
+                        required: field.required === '1',
+                        options: field.options.map(option => ({
+                            label: option.label,
+                            value: option.value,
+                        })),
+                        hideLabel: field.hide_label === '1',
+                        label: field.label,
+                    } as CampaignFieldSelect;
+            }
+        },
+    ),
+});
+
+// Convert callout campaigns from EmbedBlockElement to CalloutBlockElement
+export const normalizeCallout = (body: CAPIType): CAPIType => {
+    const calloutElements: preNormalizedCampaignType[] = body.config.campaigns
+        ? body.config.campaigns.filter(
+              ele => ele && ele.fields && ele.fields._type === 'callout',
+          )
+        : [];
+
+    if (calloutElements.length) {
+        return {
+            ...body,
+
+            // Need to convert EmbedBlockElement to relavant CalloutBlockElement
+            // This is made a little complex due to the fact that we are handling multiple arrys.
+            // Furthermore there is no clear way of checking if a EmbedBlockElement is a CalloutBlockElement
+            // currently `alt` and `html` in the EmbedBlockElement to search for a string mathcing the calloutElements tagName
+            blocks: body.blocks.map(block => ({
+                ...block,
+                elements: block.elements.map(block => {
+                    if (
+                        block._type ===
+                        'model.dotcomrendering.pageElements.EmbedBlockElement'
+                    ) {
+                        // need to determin if EmbedBlockElement is a callout
+                        const relatedCalloutElement = calloutElements.filter(
+                            ele => {
+                                // if `alt` is defined we should use that in favor of HTML
+                                // as doing such a search can be costly
+                                if (block.alt) {
+                                    return block.alt.search(ele.fields.tagName);
+                                } else {
+                                    return block.html.search(
+                                        ele.fields.tagName,
+                                    );
+                                }
+                            },
+                        );
+
+                        // TODO: throw warning if we have more than 1 match
+                        if (relatedCalloutElement.length > 1)
+                            console.warn(
+                                'More than one callout block match candidate, need to investigate and mitigate issue',
+                            );
+
+                        // if we find a match we should remove that EmbedBlockElement with a CalloutBlockElement
+                        if (relatedCalloutElement.length) {
+                            return formatCalloutObject(
+                                relatedCalloutElement[0],
+                            );
+                        } else {
+                            return block;
+                        }
+                    } else {
+                        return block;
+                    }
+                }),
+            })),
+
+            // TODO: we can potentually remove callout campaigns from configs?
+        };
+    } else {
+        return body;
+    }
+};
