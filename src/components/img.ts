e2b7b2b018c8753b8c786963c34f9dce417c1316
Jamie Webb <jamie.webb@guardian.co.uk>
2020-06-15 10:41:10 +0100

    remove background image on thumbnail images

--- src/components/img.ts
+++ src/components/img.ts
@@ -4,9 +4,10 @@ import { createElement as h, FC } from 'react';
 import { css, jsx as styledH, SerializedStyles } from '@emotion/core';
 import { neutral } from '@guardian/src-foundations/palette';
 
-import { Image } from 'image';
+import { Image, Role } from 'image';
 import { darkModeCss } from 'styles';
 import { Format, Design } from '@guardian/types/Format';
+import { Option } from 'types/option';
 
 
 // ----- Component ----- //
@@ -18,14 +19,19 @@ interface Props {
     format?: Format;
 }
 
-const styles = (format?: Format): SerializedStyles => css`
-    background-color: ${format?.design === Design.Media ? neutral[20] : neutral[97]};
-
-    ${darkModeCss`
+const styles = (role: Option<Role>, format?: Format): SerializedStyles => {
+    const darkModeStyles = darkModeCss`
         color: ${neutral[60]};
         background-color: ${neutral[20]};
-    `}
-`;
+    `
+
+    return role.fmap(imageRole => imageRole).withDefault(Role.HalfWidth) === Role.Thumbnail
+        ? darkModeStyles
+        : css`
+            background-color: ${format?.design === Design.Media ? neutral[20] : neutral[97]};
+            ${darkModeStyles}
+        `
+}
 
 const Img: FC<Props> = ({ image, sizes, className, format }) =>
     h('picture', null, [
@@ -42,7 +48,7 @@ const Img: FC<Props> = ({ image, sizes, className, format }) =>
             src: image.src,
             alt: image.alt.withDefault(''),
             className: image.launchSlideshow ? 'js-launch-slideshow' : '',
-            css: [styles(format), className],
+            css: [styles(image.role, format), className],
             'data-caption': image.nativeCaption.withDefault(''),
             'data-credit': image.credit.withDefault(''),
         }),
