f5e03aebbd81e9a8a70fc7ef757e83c5f90355db
William Lacroix <william.lacroix.extern@joyn.de>
2020-06-16 15:09:20 +0100

    remove local state from checkboxes

--- src/web/components/Callout/CheckboxSelect.tsx
+++ src/web/components/Callout/CheckboxSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { CheckboxGroup, Checkbox } from '@guardian/src-checkbox';
 import { FieldLabel } from './FieldLabel';
@@ -9,61 +9,48 @@ type Props = {
     setFormData: React.Dispatch<React.SetStateAction<{ [x: string]: any }>>;
 };
 
-// We encapsulate the Component's state here instead of directly sourcing it from
-// `formData` to avoid unnessesary rerenders, as this was causing selection animation
-// to be applied whenever formData was changing
-export const CheckboxSelect = ({ formField, formData, setFormData }: Props) => {
-    const [state, setState] = useState([]);
-    const updateState = (newState: []) => {
-        setState(newState);
-        setFormData({
-            ...formData,
-            [formField.id]: newState,
-        });
-    };
+export const CheckboxSelect = ({ formField, formData, setFormData }: Props) => (
+    <>
+        <FieldLabel formField={formField} />
+        <CheckboxGroup name={formField.name}>
+            {formField.options.map((option, index) => {
+                // data related to this field is mapped to `formData` using `formField.id`
+                // We cannot assume that the data exists, so we need to check if `formField.id` key exists in `formData`
+                const selectedCheckboxesArray =
+                    formField.id in formData ? formData[formField.id] : [];
 
-    return (
-        <>
-            <FieldLabel formField={formField} />
-            <CheckboxGroup name={formField.name}>
-                {formField.options.map((option, index) => {
-                    // data related to this field is mapped to `formData` using `formField.id`
-                    // We cannot assume that the data exists, so we need to check if `formField.id` key exists in `formData`
-                    const selectedCheckboxesArray =
-                        formField.id in formData ? formData[formField.id] : [];
+                const isCheckboxChecked = !!selectedCheckboxesArray.find(
+                    (ele: string) => ele === option.value,
+                );
 
-                    const isCheckboxChecked = !!selectedCheckboxesArray.find(
-                        (ele: string) => ele === option.value,
+                const filterOutCheckboxFromArray = () =>
+                    selectedCheckboxesArray.filter(
+                        (ele: string) => ele !== option.value,
                     );
 
-                    const filterOutCheckboxFromArray = () =>
-                        selectedCheckboxesArray.filter(
-                            (ele: string) => ele !== option.value,
-                        );
+                const addCheckboxToArray = () => [
+                    ...selectedCheckboxesArray,
+                    option.value,
+                ];
 
-                    const addCheckboxToArray = () => [
-                        ...selectedCheckboxesArray,
-                        option.value,
-                    ];
-
-                    return (
-                        <Checkbox
-                            data-testid={`form-field-${option.value}`}
-                            key={index}
-                            label={option.value}
-                            value={option.value}
-                            checked={isCheckboxChecked}
-                            onChange={() => {
-                                updateState(
-                                    isCheckboxChecked
-                                        ? filterOutCheckboxFromArray()
-                                        : addCheckboxToArray(),
-                                );
-                            }}
-                        />
-                    );
-                })}
-            </CheckboxGroup>
-        </>
-    );
-};
+                return (
+                    <Checkbox
+                        data-testid={`form-field-${option.value}`}
+                        key={index}
+                        label={option.label}
+                        value={option.value}
+                        checked={isCheckboxChecked}
+                        onChange={() => {
+                            setFormData({
+                                ...formData,
+                                [formField.id]: isCheckboxChecked
+                                    ? filterOutCheckboxFromArray()
+                                    : addCheckboxToArray(),
+                            });
+                        }}
+                    />
+                );
+            })}
+        </CheckboxGroup>
+    </>
+);
--- src/web/components/Callout/RadioSelect.tsx
+++ src/web/components/Callout/RadioSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { RadioGroup, Radio } from '@guardian/src-radio';
 import { FieldLabel } from './FieldLabel';
@@ -9,44 +9,35 @@ type fieldProp = {
     setFormData: React.Dispatch<React.SetStateAction<{ [x: string]: any }>>;
 };
 
-// We encapsulate the Component's state here instead of directly sourcing it from
-// `formData` to avoid unnessesary rerenders, as this was causing selection animation
-// to be applied whenever formData was changin
 export const RadioSelect = ({
     formField,
     formData,
     setFormData,
-}: fieldProp) => {
-    const [state, setState] = useState();
-    const updateState = (newState: string) => {
-        setState(newState);
-        setFormData({
-            ...formData,
-            [formField.id]: newState,
-        });
-    };
-
-    return (
-        <>
-            <FieldLabel formField={formField} />
-            <RadioGroup name={formField.name} orientation="horizontal">
-                {formField.options.map((option, index) => {
-                    const isRadioChecked =
-                        formField.id in formData &&
-                        formData[formField.id] === option.value;
-                    return (
-                        <Radio
-                            data-testid={`form-field-${option.value}`}
-                            key={index}
-                            label={option.value}
-                            value={option.value}
-                            name={`${formField.id}`}
-                            checked={!!isRadioChecked}
-                            onClick={() => updateState(option.value)}
-                        />
-                    );
-                })}
-            </RadioGroup>
-        </>
-    );
-};
+}: fieldProp) => (
+    <>
+        <FieldLabel formField={formField} />
+        <RadioGroup name={formField.name} orientation="horizontal">
+            {formField.options.map((option, index) => {
+                const isRadioChecked =
+                    formField.id in formData &&
+                    formData[formField.id] === option.value;
+                return (
+                    <Radio
+                        data-testid={`form-field-${option.value}`}
+                        key={index}
+                        label={option.label}
+                        value={option.value}
+                        name={`${formField.id}`}
+                        checked={!!isRadioChecked}
+                        onClick={() =>
+                            setFormData({
+                                ...formData,
+                                [formField.id]: option.value,
+                            })
+                        }
+                    />
+                );
+            })}
+        </RadioGroup>
+    </>
+);
