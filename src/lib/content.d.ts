19a92962a4a5a0e87f2cc8ffca9cb457144d1de6
Rob Phillips <robert.phillips@guardian.co.uk>
2020-06-18 11:18:41 +0100

    Adding proper validation

--- src/lib/content.d.ts
+++ src/lib/content.d.ts
@@ -226,7 +226,7 @@ interface VideoVimeoBlockElement {
     url: string;
     height: number;
     width: number;
-    caption: string;
+    caption?: string;
     credit: string;
     title: string;
     description: string;
