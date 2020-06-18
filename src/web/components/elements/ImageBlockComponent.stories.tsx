7bc2e6e1281b857092c8bef35892f16cba5c01b5
Oliver Lloyd <email@oliverlloyd.com>
2020-06-18 11:46:46 +0100

    We can now show a title as an overlay on images

--- src/web/components/elements/ImageBlockComponent.stories.tsx
+++ src/web/components/elements/ImageBlockComponent.stories.tsx
@@ -142,3 +142,87 @@ export const HideCaption = () => {
 HideCaption.story = {
     name: 'with hideCaption true',
 };
+
+export const InlineTitle = () => {
+    return (
+        <Container>
+            <ImageBlockComponent
+                display="standard"
+                designType="Article"
+                element={{ ...image, role: 'inline' }}
+                pillar="news"
+                title="This is the title text"
+                hideCaption={true}
+            />
+        </Container>
+    );
+};
+InlineTitle.story = {
+    name: 'with title and role inline',
+    parameters: {
+        viewport: { defaultViewport: 'desktop' },
+        chromatic: { viewports: [1300] },
+    },
+};
+
+export const InlineTitleMobile = () => {
+    return (
+        <Container>
+            <ImageBlockComponent
+                display="standard"
+                designType="Article"
+                element={{ ...image, role: 'inline' }}
+                pillar="news"
+                title="This is the title text"
+                hideCaption={true}
+            />
+        </Container>
+    );
+};
+InlineTitleMobile.story = {
+    name: 'with title and role inline on mobile',
+    parameters: {
+        viewport: { defaultViewport: 'mobileMedium' },
+        chromatic: { viewports: [375] },
+    },
+};
+
+export const ImmersiveTitle = () => {
+    return (
+        <Container>
+            <ImageBlockComponent
+                display="standard"
+                designType="Article"
+                element={{ ...image, role: 'immersive' }}
+                pillar="news"
+                title="This is the title text"
+                hideCaption={true}
+            />
+        </Container>
+    );
+};
+ImmersiveTitle.story = {
+    name: 'with title and role immersive',
+};
+
+export const ShowcaseTitle = () => {
+    return (
+        <Container>
+            <ImageBlockComponent
+                display="standard"
+                designType="Article"
+                element={{ ...image, role: 'showcase' }}
+                pillar="news"
+                title="This is the title text"
+                hideCaption={true}
+            />
+        </Container>
+    );
+};
+ShowcaseTitle.story = {
+    name: 'with title and role showcase',
+    parameters: {
+        viewport: { defaultViewport: 'desktop' },
+        chromatic: { viewports: [980] },
+    },
+};
