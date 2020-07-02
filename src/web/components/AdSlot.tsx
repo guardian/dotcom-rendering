05ed24ab97aa3a42816a9f058cec3909b6683834
Max Duval <max@mxdvl.com>
2020-07-02 14:12:38 +0100

    label styles should apply to scroll, too

--- src/web/components/AdSlot.tsx
+++ src/web/components/AdSlot.tsx
@@ -6,7 +6,8 @@ import { textSans } from '@guardian/src-foundations/typography';
 import { from } from '@guardian/src-foundations/mq';
 
 export const labelStyles = css`
-    .ad-slot__label {
+    .ad-slot__label,
+    .ad-slot__scroll {
         ${textSans.xsmall()};
         position: relative;
         height: 24px;
@@ -20,6 +21,12 @@ export const labelStyles = css`
 
     .ad-slot__close-button {
         display: none;
+    }
+
+    .ad-slot__scroll {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
     }
 `;
 const mobileStickyAdStyles = css`
@@ -104,7 +111,7 @@ export const makeClassNames = (
     optClassNames: string[],
 ): string => {
     const baseClassNames = ['js-ad-slot', 'ad-slot', `ad-slot--${name}`];
-    const adTypeClassNames = adTypes.map(adType => `ad-slot--${adType}`);
+    const adTypeClassNames = adTypes.map((adType) => `ad-slot--${adType}`);
     return baseClassNames.concat(adTypeClassNames, optClassNames).join(' ');
 };
 
