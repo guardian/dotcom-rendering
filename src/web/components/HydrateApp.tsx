adbd3d1cb712e073d9a609c74109ce6ef49f9d1d
Gareth <gtrufitt@gmail.com>
2020-07-14 15:52:14 +0100

    Add ab test framework to DCR

--- src/web/components/HydrateApp.tsx
+++ src/web/components/HydrateApp.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from '@root/src/web/components/App';
+import { ABProvider } from '@guardian/ab-react';
+import { tests } from '@frontend/web/experiments/ab-tests';
+import { getCookie } from '../browser/cookie';
 
 type Props = {
     CAPI: CAPIBrowserType;
@@ -9,8 +12,31 @@ type Props = {
 };
 
 export const HydrateApp = ({ CAPI, NAV }: Props) => {
+    const mvtId = Number(
+        (window.guardian.config.page.isDev && getCookie('GU_mvt_id_local')) || // Simplify localhost testing by creating a different mvt id
+            getCookie('GU_mvt_id'),
+    );
+    if (!mvtId) {
+        // 0 is default and falsy here
+        // eslint-disable-next-line no-console
+        console.log('There is no MVT ID set, see HydrateApp.tsx');
+    }
+
     ReactDOM.render(
-        <App CAPI={CAPI} NAV={NAV} />,
+        <ABProvider
+            arrayOfTestObjects={tests}
+            abTestSwitches={{
+                ...{ abAbTestTest: true }, // Test switch, used for Cypress integration test
+                ...CAPI.config.switches,
+            }}
+            pageIsSensitive={CAPI.config.isSensitive}
+            mvtMaxValue={1000000}
+            mvtId={mvtId}
+            ophanRecord={window.guardian.ophan.record}
+        >
+            <App CAPI={CAPI} NAV={NAV} />
+        </ABProvider>,
+
         document.getElementById('react-root'),
     );
 };
