6b57d80d8248314ff634a56b78bc14271085803e
Gareth <gtrufitt@gmail.com>
2020-07-15 11:58:37 +0100

    Add the sign-in-gate A/B Tests
    Co-Authored-By: Dominic Kendrick <dominickendrick@users.noreply.github.com>

--- src/web/experiments/ab-tests.ts
+++ src/web/experiments/ab-tests.ts
@@ -1,4 +1,12 @@
 import { ABTest } from '@guardian/ab-core';
 import { abTestTest } from '@frontend/web/experiments/tests/ab-test-test';
+import { signInGateCentesimus } from '@frontend/web/experiments/tests/sign-in-gate-centesimus';
+import { signInGatePatientia } from '@frontend/web/experiments/tests/sign-in-gate-patientia';
+import { signInGateVii } from '@frontend/web/experiments/tests/sign-in-gate-vii';
 
-export const tests: ABTest[] = [abTestTest];
+export const tests: ABTest[] = [
+    abTestTest,
+    signInGateCentesimus,
+    signInGatePatientia,
+    signInGateVii,
+];
