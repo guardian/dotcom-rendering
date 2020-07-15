350834ba5651a20c234f8ca0fc39ffe9b3eb3d9b
Gareth <gtrufitt@gmail.com>
2020-07-15 14:23:01 +0100

    Add Ophan Types
    Co-Authored-By: Dominic Kendrick <dominickendrick@users.noreply.github.com>

--- src/web/browser/ophan/ophan.ts
+++ src/web/browser/ophan/ophan.ts
@@ -1,17 +1,60 @@
-interface ABTestRecord {
+/**
+ * an individual A/B test, structured for Ophan
+ */
+export type OphanABEvent = {
     variantName: string;
     complete: string | boolean;
-}
+    campaignCodes?: Array<string>;
+};
 
-interface ABTestPayload {
-    abTestRegister: { [key: string]: ABTestRecord };
-}
+/**
+ * the actual payload we send to Ophan: an object of OphanABEvents with test IDs as keys
+ */
+export type OphanABPayload = {
+    abTestRegister: { [testId: string]: OphanABEvent };
+};
 
-export type OphanAction = 'INSERT' | 'VIEW';
+export type OphanProduct =
+    | 'CONTRIBUTION'
+    | 'RECURRING_CONTRIBUTION'
+    | 'MEMBERSHIP_SUPPORTER'
+    | 'MEMBERSHIP_PATRON'
+    | 'MEMBERSHIP_PARTNER'
+    | 'DIGITAL_SUBSCRIPTION'
+    | 'PRINT_SUBSCRIPTION';
+
+export type OphanAction =
+    | 'INSERT'
+    | 'VIEW'
+    | 'EXPAND'
+    | 'LIKE'
+    | 'DISLIKE'
+    | 'SUBSCRIBE'
+    | 'ANSWER'
+    | 'VOTE'
+    | 'CLICK';
 
 export type OphanComponentType =
+    | 'READERS_QUESTIONS_ATOM'
+    | 'QANDA_ATOM'
+    | 'PROFILE_ATOM'
+    | 'GUIDE_ATOM'
+    | 'TIMELINE_ATOM'
+    | 'NEWSLETTER_SUBSCRIPTION'
+    | 'SURVEYS_QUESTIONS'
     | 'ACQUISITIONS_EPIC'
-    | 'ACQUISITIONS_ENGAGEMENT_BANNER';
+    | 'ACQUISITIONS_ENGAGEMENT_BANNER'
+    | 'ACQUISITIONS_THANK_YOU_EPIC'
+    | 'ACQUISITIONS_HEADER'
+    | 'ACQUISITIONS_FOOTER'
+    | 'ACQUISITIONS_INTERACTIVE_SLICE'
+    | 'ACQUISITIONS_NUGGET'
+    | 'ACQUISITIONS_STANDFIRST'
+    | 'ACQUISITIONS_THRASHER'
+    | 'ACQUISITIONS_EDITORIAL_LINK'
+    | 'ACQUISITIONS_SUBSCRIPTIONS_BANNER'
+    | 'ACQUISITIONS_OTHER'
+    | 'SIGN_IN_GATE';
 
 export type OphanComponent = {
     componentType: OphanComponentType;
@@ -63,8 +106,8 @@ export const sendOphanContributionsComponentEvent = (
 
 export const abTestPayload = (tests: {
     [key: string]: string;
-}): ABTestPayload => {
-    const records: { [key: string]: ABTestRecord } = {};
+}): OphanABPayload => {
+    const records: { [key: string]: OphanABEvent } = {};
     Object.keys(tests).forEach((testName) => {
         records[`ab${testName}`] = {
             variantName: tests[testName],
