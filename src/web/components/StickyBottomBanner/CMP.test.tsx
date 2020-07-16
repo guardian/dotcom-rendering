0b48ac7f057b89cd1514eaa3fe6c122c2e3cfb8a
Max Duval <max@mxdvl.com>
2020-07-16 13:33:47 +0100

    remove obsolete CMP test

--- src/web/components/StickyBottomBanner/CMP.test.tsx
+++ src/web/components/StickyBottomBanner/CMP.test.tsx
@@ -1,43 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { waitFor } from '@testing-library/dom';
-import { ConsentManagementPlatform as ConsentManagementPlatform_ } from '@guardian/consent-management-platform/dist/ConsentManagementPlatform';
-
-import { CMP } from './CMP';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const ConsentManagementPlatform: any = ConsentManagementPlatform_;
+import { cmp } from '@guardian/consent-management-platform';
 
 jest.mock('@guardian/consent-management-platform', () => ({
-    setErrorHandler: jest.fn(),
+    cmp:{
+        showPrivacyManager: jest.fn(),
+    }:
 }));
 
-jest.mock(
-    '@guardian/consent-management-platform/dist/ConsentManagementPlatform',
-);
-
 describe('CMP', () => {
-    beforeAll(() => {
-        ConsentManagementPlatform.mockImplementation(() => ({
-            render: () => {
-                return <div />;
-            },
-        }));
-        // stub window.performace as it is not defined on Node
-        Object.defineProperty(window, 'performance', {
-            value: {
-                mark: () => {},
-                measure: () => {},
-                getEntriesByName: () => {},
-            },
-        });
-    });
-
-    it('It should render the CMP', async () => {
-        const { container } = render(<CMP />);
-
-        await waitFor(() =>
-            expect(container.firstChild).toBeInstanceOf(HTMLDivElement),
-        );
+    it('It should render the Sourcepoint CMP', async () => {
+        // TODO: implement test
+        cmp.showPrivacyManager();
+        expect(true).toBeTruthy();
     });
 });
