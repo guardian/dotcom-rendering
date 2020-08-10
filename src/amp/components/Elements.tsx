19f39302e1e7ba1c89703f6276963b59f6809531
Pascal Honoré <pascal@alseyn.net>
2020-08-10 14:54:21 +0100

    Remove unused import

--- src/amp/components/Elements.tsx
+++ src/amp/components/Elements.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AudioAtomBlockComponent } from '@root/src/amp/components/elements/AudioAtomBlockComponent';
+// import { AudioAtomBlockComponent } from '@root/src/amp/components/elements/AudioAtomBlockComponent';
 import { CommentBlockComponent } from '@root/src/amp/components/elements/CommentBlockComponent';
 import { ContentAtomBlockComponent } from '@root/src/amp/components/elements/ContentAtomBlockComponent';
 import { DisclaimerBlockComponent } from '@root/src/amp/components/elements/DisclaimerBlockComponent';
