c3051ad5287492942faa0c77175fda08a2731e7d
Pascal Honoré <pascal@alseyn.net>
2020-08-12 12:15:08 +0100

    Handle optionality of SpotifyBlockComponent's parameters

--- src/web/components/elements/SpotifyBlockComponent.tsx
+++ src/web/components/elements/SpotifyBlockComponent.tsx
@@ -6,24 +6,28 @@ export const SpotifyBlockComponent: React.FC<{
     title?: string;
     height?: number;
     width?: number;
-}> = ({ embedUrl, width, height, title }) => {
+}> = ({ embedUrl, title, width, height }) => {
     return (
-        <div
-            className={css`
-                iframe {
-                    width: 100%;
-                }
-                margin-bottom: 16px;
-            `}
-            data-cy="spotify-embed"
-        >
-            <iframe
-                src={embedUrl}
-                title={title}
-                height={height}
-                width={width}
-                allowFullScreen={true}
-            />
-        </div>
+        <>
+            {embedUrl && title && width && height && (
+                <div
+                    className={css`
+                        iframe {
+                            width: 100%;
+                        }
+                        margin-bottom: 16px;
+                    `}
+                    data-cy="spotify-embed"
+                >
+                    <iframe
+                        src={embedUrl}
+                        title={title}
+                        height={height}
+                        width={width}
+                        allowFullScreen={true}
+                    />
+                </div>
+            )}
+        </>
     );
 };
