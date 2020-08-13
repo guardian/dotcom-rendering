d7a1c249652e874d7c4642d022922ed29063d650
Gareth <gtrufitt@gmail.com>
2020-08-13 09:37:12 +0100

    Remove arrow functions to ensure scope

--- cypress/integration/e2e/sign-in-gate.spec.js
+++ cypress/integration/e2e/sign-in-gate.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable func-names */
 
-describe('Sign In Gate Tests', () => {
+describe('Sign In Gate Tests', function() {
     const setArticleCount = (n) => {
         // set article count for today to be n
         localStorage.setItem('gu.history.dailyArticleCount', JSON.stringify({ value: [{ day: Math.floor(Date.now() / 86400000), count: n }] }));
@@ -35,8 +35,8 @@ describe('Sign In Gate Tests', () => {
         scrollToGateForLazyLoading(roughPosition);
     }
 
-    describe('SignInGateMain', () => {
-        beforeEach(() => {
+    describe('SignInGateMain', function() {
+        beforeEach(function() {
             // sign in gate main runs from 0-900000 MVT IDs, so 500 forces user into test
             setMvtCookie('500');
 
@@ -44,13 +44,13 @@ describe('Sign In Gate Tests', () => {
             setArticleCount(3);
         })
 
-        it('should load the sign in gate', () => {
+        it('should load the sign in gate', function() {
             visitArticleAndScrollToGateForLazyLoad();
 
             cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
         });
 
-        it('should not load the sign in gate if the user has not read at least 3 article in a day', () => {
+        it('should not load the sign in gate if the user has not read at least 3 article in a day', function() {
             setArticleCount(1);
 
             visitArticleAndScrollToGateForLazyLoad();
@@ -58,7 +58,7 @@ describe('Sign In Gate Tests', () => {
             cy.get('[data-cy=sign-in-gate-main]').should('not.be.visible');
         });
 
-        it('should not load the sign in gate if the user is signed in', () => {
+        it('should not load the sign in gate if the user is signed in', function() {
             // use GU_U cookie to determine if user is signed in
             cy.setCookie('GU_U', 'MCwCFHbDHWevL_GqgH0CcbeDWp4N9kR5AhQ2lD3zMjjbKJAgC7FUDtc18Ac8BA', { log: true });
 
@@ -75,7 +75,7 @@ describe('Sign In Gate Tests', () => {
             cy.get('[data-cy=sign-in-gate-main]').should('not.be.visible');
         });
 
-        it('should not load the sign in gate if the user has already dismissed the gate', () => {
+        it('should not load the sign in gate if the user has already dismissed the gate', function() {
             localStorage.setItem('gu.prefs.sign-in-gate', '{"SignInGateMain-main-variant-1":"2020-07-22T08:25:05.567Z"}')
 
             visitArticleAndScrollToGateForLazyLoad();
@@ -83,13 +83,13 @@ describe('Sign In Gate Tests', () => {
             cy.get('[data-cy=sign-in-gate-main]').should('not.be.visible');
         });
 
-        it('should not load the sign in gate if the article is not a valid section (membership)', () => {
+        it('should not load the sign in gate if the article is not a valid section (membership)', function() {
             visitArticleAndScrollToGateForLazyLoad({ url: 'https://www.theguardian.com/membership/2018/nov/15/support-guardian-readers-future-journalism' })
 
             cy.get('[data-cy=sign-in-gate-main]').should('not.be.visible');
         });
 
-        it('should not load the sign in gate on a device with an ios9 user agent string', () => {
+        it('should not load the sign in gate on a device with an ios9 user agent string', function() {
             // can't use visitArticleAndScrollToGateForLazyLoad for this method as overriding user agent
             cy.visit('Article?url=https://www.theguardian.com/games/2018/aug/23/nier-automata-yoko-taro-interview', {
                 onBeforeLoad: win => {
@@ -103,7 +103,7 @@ describe('Sign In Gate Tests', () => {
             cy.get('[data-cy=sign-in-gate-main]').should('not.be.visible');
         });
 
-        it('should remove gate when the dismiss button is clicked', () => {
+        it('should remove gate when the dismiss button is clicked', function() {
             visitArticleAndScrollToGateForLazyLoad();
 
             cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -113,7 +113,7 @@ describe('Sign In Gate Tests', () => {
             cy.get('[data-cy=sign-in-gate-main]').should('not.be.visible');
         });
 
-        it('register button should contain profile.theguardian.com href', () => {
+        it('register button should contain profile.theguardian.com href', function() {
             visitArticleAndScrollToGateForLazyLoad();
 
             cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -122,7 +122,7 @@ describe('Sign In Gate Tests', () => {
                 .invoke('attr', 'href').should('contains', 'profile.theguardian.com');
         });
 
-        it('sign in link should contain profile.theguardian.com href', () => {
+        it('sign in link should contain profile.theguardian.com href', function() {
             visitArticleAndScrollToGateForLazyLoad();
 
             cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -132,7 +132,7 @@ describe('Sign In Gate Tests', () => {
                 .invoke('attr', 'href').should('contains', 'profile.theguardian.com');
         });
 
-        it('should show cmp ui when privacy settings link is clicked', () => {
+        it('should show cmp ui when privacy settings link is clicked', function() {
             visitArticleAndScrollToGateForLazyLoad();
 
             cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -143,8 +143,8 @@ describe('Sign In Gate Tests', () => {
         });
     });
 
-    describe('SignInGatePatientia', () => {
-        beforeEach(() => {
+    describe('SignInGatePatientia', function() {
+        beforeEach(function() {
             // sign in gate patientia runs from 999901-1000000 MVT IDs, so 999901 forces user into test variant
             setMvtCookie('999901');
 
@@ -152,13 +152,13 @@ describe('Sign In Gate Tests', () => {
             setArticleCount(3);
         });
 
-        it('should load the sign in gate', () => {
+        it('should load the sign in gate', function() {
             visitArticleAndScrollToGateForLazyLoad();
 
             cy.get('[data-cy=sign-in-gate-patientia]').should('be.visible');
         });
 
-        it('should remove gate when the dismiss button is clicked', () => {
+        it('should remove gate when the dismiss button is clicked', function() {
             visitArticleAndScrollToGateForLazyLoad();
 
             cy.get('[data-cy=sign-in-gate-patientia]').should('be.visible');
