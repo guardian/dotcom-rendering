28f32025a0107e056b5f5f8bc279e2523f71cad9
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2020-08-27 15:17:01 +0100

    Set cookie using puppeteer

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,7 @@ module.exports = {
       url: ['http://localhost:9000/Article?url=https://www.theguardian.com/commentisfree/2020/feb/08/hungary-now-for-the-new-right-what-venezuela-once-was-for-the-left#noads'],
       startServerCommand: 'NODE_ENV=production DISABLE_LOGGING_AND_METRICS=true node dist/frontend.server.js',
       numberOfRuns: '6',
+      puppeteerScript: './scripts/lighthouse/puppeteer-script.js',
     },
     upload: {
       target: 'temporary-public-storage',
