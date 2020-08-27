bed5d3645ebc6cf57ae7666ade18ac155c29548b
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2020-08-27 17:14:18 +0100

    Use url provided by the context param

--- scripts/lighthouse/puppeteer-script.js
+++ scripts/lighthouse/puppeteer-script.js
@@ -3,13 +3,12 @@
  * @param {{url: string, options: LHCI.CollectCommand.Options}} context
  */
 module.exports = async (browser, context) => {
-  const url = 'http://localhost:9000/Article?url=https://www.theguardian.com/commentisfree/2020/feb/08/hungary-now-for-the-new-right-what-venezuela-once-was-for-the-left#noads';
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(context.url);
   const cookie = {
           name: 'gu-cmp-disabled',
           value: 'true',
-          url: url,
+          url: context.url,
           'Max-Age': '31536000', //year
         };
 
