c2f6c0467c06193afeea6ffc972e96d01bbceb03
Demetrios Skamiotis <dskamiotis@gmail.com>
2020-08-27 11:54:06 +0100

    included pillar in props for dateline.stories

--- src/components/dateline.stories.tsx
+++ src/components/dateline.stories.tsx
@@ -5,12 +5,16 @@ import { date, withKnobs } from '@storybook/addon-knobs';
 
 import { some } from '@guardian/types/option';
 import Dateline from './dateline';
+import { Pillar } from '@guardian/types/Format';
 
+type DefaultProps = {
+    pillar: Pillar
+}
 
 // ----- Stories ----- //
 
-const Default: FC = () =>
-    <Dateline date={some(new Date(date('Publish Date', new Date('2019-12-17T03:24:00'))))} />
+const Default: FC<DefaultProps> = ({pillar}:DefaultProps) =>
+    <Dateline pillar={pillar} date={some(new Date(date('Publish Date', new Date('2019-12-17T03:24:00'))))} />
 
 
 // ----- Exports ----- //
