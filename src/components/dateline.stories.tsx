3d323c696c287dfd330c07605365f8e4dd182e07
Demetrios Skamiotis <dskamiotis@gmail.com>
2020-08-28 11:29:19 +0100

    Updated datline storybook

--- src/components/dateline.stories.tsx
+++ src/components/dateline.stories.tsx
@@ -5,15 +5,16 @@ import { date, withKnobs } from '@storybook/addon-knobs';
 import { some } from '@guardian/types/option';
 import Dateline from './dateline';
 import { Pillar } from '@guardian/types/Format';
+import { selectPillar } from 'storybookHelpers';
 
-type DefaultProps = {
-    pillar: Pillar
-}
 
 // ----- Stories ----- //
 
-const Default: FC<DefaultProps> = ({pillar}:DefaultProps) =>
-    <Dateline pillar={pillar} date={some(new Date(date('Publish Date', new Date('2019-12-17T03:24:00'))))} />
+const Default: FC = () =>
+    <Dateline 
+        pillar={selectPillar(Pillar.Opinion)} 
+        date={some(new Date(date('Publish Date', new Date('2019-12-17T03:24:00'))))} 
+    />
 
 
 // ----- Exports ----- //
