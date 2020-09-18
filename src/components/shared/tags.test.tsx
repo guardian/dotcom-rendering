6f21e8987f077ec2fc2de122dda0baa71d5ce3ad
Demetrios Skamiotis <dskamiotis@gmail.com>
2020-09-18 12:06:07 +0100

    migrate from enzyme to react test

--- src/components/shared/tags.test.tsx
+++ src/components/shared/tags.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { configure, shallow, ShallowWrapper } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import Tags from 'components/shared/tags';
-import { SerializedStyles } from '@emotion/core';
 import { Design, Display, Format, Pillar } from '@guardian/types/Format';
+import renderer from 'react-test-renderer';
+import { matchers } from 'jest-emotion';
 
-configure({ adapter: new Adapter() });
+expect.extend(matchers);
 
 const tagsProps = [{
     webTitle: "Tag title",
@@ -18,30 +17,25 @@ const mockFormat: Format = {
     display: Display.Standard,
 };
 
-const styles = (component: ShallowWrapper): string => Array.from(component.prop<SerializedStyles[]>('css'))
-    .flat()
-    .map(({ styles }: { styles: string }) => styles)
-    .reduce((a: string, b: string) => a + b, '')
-
-describe('Keyline component renders as expected', () => {
+describe('Tags component renders as expected', () => {
     it('Renders link to tag', () => {
-        const tags = shallow(<Tags tags={tagsProps} format={mockFormat}/>)
-        const link = tags.find('a')
-        expect(link.prop('href')).toBe('https://mapi.co.uk/tag')
+        const tags = renderer.create(<Tags tags={tagsProps} format={mockFormat}/>)
+        const link = tags.root.findByType("a")
+        expect(link.props.href).toBe('https://mapi.co.uk/tag')
     })
 
     it('Renders tag title', () => {
-        const tags = shallow(<Tags tags={tagsProps} format={mockFormat} />)
-        expect(tags.find('li').text()).toBe("Tag title")
+        const tags = renderer.create(<Tags tags={tagsProps} format={mockFormat} />)
+        expect(tags.root.findByType('a').children.includes("Tag title")).toBe(true)
     })
 
     it('Renders correct number of tags', () => {
-        const tags = shallow(<Tags tags={[...tagsProps, ...tagsProps]} format={mockFormat} />)
-        expect(tags.find('li').length).toBe(2)
+        const tags = renderer.create(<Tags tags={[...tagsProps, ...tagsProps]} format={mockFormat} />)
+        expect(tags.root.findAllByType('li').length).toBe(2)
     })
 
     it('Renders correct background color', () => {
-        const tags = shallow(<Tags tags={tagsProps} format={mockFormat}/>)
-        expect(styles(tags)).toContain("background-color: #DCDCDC;")
+        const tags = renderer.create(<Tags tags={tagsProps} format={mockFormat}/>)
+        expect(tags.toJSON()).toHaveStyleRule("background-color","#DCDCDC",{ target: 'li a' })
     })
 });
