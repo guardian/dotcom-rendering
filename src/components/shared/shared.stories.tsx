3380ccc422362de8527ef820fd79d551cc37d1f9
Demetrios Skamiotis <dskamiotis@gmail.com>
2020-09-24 10:08:54 +0100

    updated styles

---
+++ src/components/shared/shared.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tags from './tags';
+import { withKnobs } from "@storybook/addon-knobs";
+import { Format } from '@guardian/types/Format';
+export default { title: 'Shared', decorators: [withKnobs] };
+
+
+
+const tagsProps = [{
+    webTitle: "Tag title",
+    webUrl: "https://mapi.co.uk/tag"
+}];
+
+export const tags = (format: Format): JSX.Element =>
+
+<Tags tags={[...tagsProps, ...tagsProps, ...tagsProps]} format={format} />
