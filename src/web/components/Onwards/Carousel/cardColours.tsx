10b3a2f72afa4e781f7e2431e4427f3cd6164206
Nicolas Long <nicolas.long@theguardian.com>
2020-09-25 11:24:18 +0100

    Add kickers to carousel and standardise to 8 items for test

---
+++ src/web/components/Onwards/Carousel/cardColours.tsx
@@ -0,0 +1,70 @@
+import { css } from 'emotion';
+import { neutral, opinion } from '@guardian/src-foundations/palette';
+import { pillarPalette } from '@frontend/lib/pillars';
+
+export const headlineBackgroundColour = (
+    designType: DesignType,
+    pillar: Pillar,
+) => {
+    switch (designType) {
+        case 'GuardianView':
+        case 'Comment':
+            return css`
+                background-color: ${opinion[800]};
+            `;
+        case 'Media':
+            return css`
+                background-color: ${neutral[20]};
+            `;
+        case 'Live':
+            return css`
+                background-color: ${pillarPalette[pillar].dark};
+            `;
+        case 'Article':
+        case 'Review':
+        case 'PhotoEssay':
+        case 'SpecialReport':
+        case 'Recipe':
+        case 'MatchReport':
+        case 'GuardianLabs':
+        case 'Quiz':
+        case 'AdvertisementFeature':
+        case 'Feature':
+        case 'Analysis':
+        case 'Interview':
+        case 'Immersive':
+        default:
+            return css`
+                background-color: ${neutral[97]};
+            `;
+    }
+};
+
+const colourStyles = (colour: string) => css`
+    color: ${colour};
+`;
+
+export const headlineColour = (designType: DesignType, pillar: Pillar) => {
+    switch (designType) {
+        case 'Feature':
+        case 'Interview':
+            return colourStyles(pillarPalette[pillar].dark);
+        case 'Media':
+        case 'Live':
+            return colourStyles(neutral[97]);
+        case 'Analysis':
+        case 'PhotoEssay':
+        case 'Article':
+        case 'Review':
+        case 'SpecialReport':
+        case 'Recipe':
+        case 'MatchReport':
+        case 'GuardianView':
+        case 'GuardianLabs':
+        case 'Quiz':
+        case 'AdvertisementFeature':
+        case 'Comment':
+        case 'Immersive':
+        default:
+    }
+};
