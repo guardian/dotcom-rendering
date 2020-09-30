78c69da43e324cb0478d53445fcfd105256bc08e
Peter Colley <zvvvvt@gmail.com>
2020-09-30 14:30:45 +0100

    fix cypress test

--- cypress/integration/e2e/sign-in-gate.spec.js
+++ cypress/integration/e2e/sign-in-gate.spec.js
@@ -105,7 +105,10 @@ describe('Sign In Gate Tests', function () {
         it('should not load the sign in gate if the user has already dismissed the gate', function () {
             localStorage.setItem(
                 'gu.prefs.sign-in-gate',
-                '{"SignInGateMain-main-variant-2":"2020-07-22T08:25:05.567Z"}',
+                `{
+                    "SignInGateMain-main-variant-2": "2020-07-22T08:25:05.567Z",
+                    "gate-dismissed-count-SignInGateMain-main-variant-2": 6
+                }`,
             );
 
             visitArticleAndScrollToGateForLazyLoad();
