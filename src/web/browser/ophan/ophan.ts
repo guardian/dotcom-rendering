cd36f5f68f979b627b18b3c82f29fb9110de378b
Tom Wey <tom.wey@theguardian.com>
2020-09-30 11:51:47 +0100

    Use Ophan types from @guardian/types
    The types have been extracted from DCR in order to share among other
    TypeScript projects.

--- src/web/browser/ophan/ophan.ts
+++ src/web/browser/ophan/ophan.ts
@@ -1,89 +1,13 @@
-/**
- * an individual A/B test, structured for Ophan
- */
-export type OphanABEvent = {
-    variantName: string;
-    complete: string | boolean;
-    campaignCodes?: Array<string>;
-};
-
-/**
- * the actual payload we send to Ophan: an object of OphanABEvents with test IDs as keys
- */
-export type OphanABPayload = {
-    abTestRegister: { [testId: string]: OphanABEvent };
-};
-
-export type OphanProduct =
-    | 'CONTRIBUTION'
-    | 'RECURRING_CONTRIBUTION'
-    | 'MEMBERSHIP_SUPPORTER'
-    | 'MEMBERSHIP_PATRON'
-    | 'MEMBERSHIP_PARTNER'
-    | 'DIGITAL_SUBSCRIPTION'
-    | 'PRINT_SUBSCRIPTION';
-
-export type OphanAction =
-    | 'INSERT'
-    | 'VIEW'
-    | 'EXPAND'
-    | 'LIKE'
-    | 'DISLIKE'
-    | 'SUBSCRIBE'
-    | 'ANSWER'
-    | 'VOTE'
-    | 'CLICK';
-
-export type OphanComponentType =
-    | 'READERS_QUESTIONS_ATOM'
-    | 'QANDA_ATOM'
-    | 'PROFILE_ATOM'
-    | 'GUIDE_ATOM'
-    | 'TIMELINE_ATOM'
-    | 'NEWSLETTER_SUBSCRIPTION'
-    | 'SURVEYS_QUESTIONS'
-    | 'ACQUISITIONS_EPIC'
-    | 'ACQUISITIONS_ENGAGEMENT_BANNER'
-    | 'ACQUISITIONS_THANK_YOU_EPIC'
-    | 'ACQUISITIONS_HEADER'
-    | 'ACQUISITIONS_FOOTER'
-    | 'ACQUISITIONS_INTERACTIVE_SLICE'
-    | 'ACQUISITIONS_NUGGET'
-    | 'ACQUISITIONS_STANDFIRST'
-    | 'ACQUISITIONS_THRASHER'
-    | 'ACQUISITIONS_EDITORIAL_LINK'
-    | 'ACQUISITIONS_SUBSCRIPTIONS_BANNER'
-    | 'ACQUISITIONS_OTHER'
-    | 'SIGN_IN_GATE'
-    | 'RETENTION_ENGAGEMENT_BANNER';
-
-export type OphanComponent = {
-    componentType: OphanComponentType;
-    id?: string;
-    products?: Array<OphanProduct>;
-    campaignCode?: string;
-    labels?: Array<string>;
-};
-
-export type OphanComponentEvent = {
-    component: OphanComponent;
-    action: OphanAction;
-    value?: string;
-    id?: string;
-    abTest?: {
-        name: string;
-        variant: string;
-    };
-};
-
-export type TestMeta = {
-    abTestName: string;
-    abTestVariant: string;
-    campaignCode: string;
-    campaignId?: string;
-    componentType: OphanComponentType;
-    products?: OphanProduct[];
-};
+import {
+    OphanABEvent,
+    OphanABPayload,
+    OphanAction,
+    OphanComponent,
+    OphanComponentEvent,
+    OphanComponentType,
+    OphanProduct,
+    TestMeta,
+} from '@guardian/types/ophan';
 
 export const record = (event: {}): void => {
     if (
@@ -189,4 +113,15 @@ export const recordPerformance = () => {
     record({
         performance,
     });
+};
+
+export {
+    OphanABEvent,
+    OphanABPayload,
+    OphanAction,
+    OphanComponent,
+    OphanComponentEvent,
+    OphanComponentType,
+    OphanProduct,
+    TestMeta,
 };
