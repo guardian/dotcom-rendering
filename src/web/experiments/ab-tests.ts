a0c3bfc66ea90a59109f08b3d2df21ceb84baa19
vanessa <32312712+vlbee@users.noreply.github.com>
2020-10-07 16:07:07 +0100

    Add signInGatePageview AB test to experiments

--- src/web/experiments/ab-tests.ts
+++ src/web/experiments/ab-tests.ts
@@ -3,6 +3,7 @@ import { abTestTest } from '@frontend/web/experiments/tests/ab-test-test';
 import { signInGateMainVariant } from '@root/src/web/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from '@root/src/web/experiments/tests/sign-in-gate-main-control';
 import { signInGatePatientia } from '@frontend/web/experiments/tests/sign-in-gate-patientia';
+import { signInGatePageview } from '@frontend/web/experiments/tests/sign-in-gate-pageview';
 import { curatedContainerTest } from '@frontend/web/experiments/tests/curated-container-test';
 
 export const tests: ABTest[] = [
@@ -10,5 +11,6 @@ export const tests: ABTest[] = [
     signInGateMainVariant,
     signInGateMainControl,
     signInGatePatientia,
+    signInGatePageview,
     curatedContainerTest,
 ];
