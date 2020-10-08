9c7725579283e9534dac62384ea973f6ac5027f9
Gareth <gtrufitt@gmail.com>
2020-10-08 17:22:33 +0100

    Use placeholderUrl and set atom height based on comment in html

--- src/amp/components/elements/InteractiveAtomBlockComponent.tsx
+++ src/amp/components/elements/InteractiveAtomBlockComponent.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { css } from 'emotion';
 import { ShowMoreButton } from '@root/src/amp/components/ShowMoreButton';
 
-const styles = css`
+const styles = (height: number) => css`
+    height: ${height}px;
+    width: 100%;
     margin-top: 16px;
     margin-bottom: 12px;
 `;
@@ -16,20 +18,50 @@ const showMore = css`
     }
 `;
 
-export const InteractiveAtomBlockComponent: React.SFC<{ url: string }> = ({
-    url,
-}) => {
+// We look in the html field of the atom for a hint
+// for the height of the atom
+// We look for an html comment <!-- MobileHeight: 100 -->
+// and if we find it use that value for the CSS height of the atom
+// otherwise we use a default height
+const getHeight = (html?: string): number => {
+    const defaultInteractiveAtomHeight = 100;
+    if (html) {
+        const heightRegex = /<!-- MobileHeight: (.*) -->/;
+        const getHeightFromComment = heightRegex.exec(html);
+        if (
+            getHeightFromComment &&
+            typeof Number(getHeightFromComment[1]) === 'number'
+        ) {
+            return Number(getHeightFromComment[1]); // Returns [ '<!-- MobileHeight: 100 -->', '100', index: 4, input: 'test<!-- MobileHeight: 100 -->', groups: undefined ]
+        }
+    }
+
+    return defaultInteractiveAtomHeight;
+};
+
+export const InteractiveAtomBlockComponent: React.SFC<{
+    url: string;
+    html?: string;
+    placeholderUrl?: string;
+}> = ({ url, placeholderUrl, html }) => {
     return (
         <amp-iframe
-            class={styles}
+            class={styles(getHeight(html))}
             src={url}
             layout="responsive"
             sandbox="allow-scripts allow-same-origin allow-top-navigation-by-user-activation allow-popups"
             height="1"
-            width="8"
+            width="1"
             resizable=""
             data-cy="atom-embed-url"
         >
+            {placeholderUrl && (
+                <amp-img
+                    placeholder={true}
+                    layout="fill"
+                    src={placeholderUrl}
+                />
+            )}
             <div overflow="" className={showMore}>
                 <ShowMoreButton />
             </div>
