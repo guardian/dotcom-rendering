6ebcecce00948eb11252947fa0fd5c88907ea202
Gareth <gtrufitt@gmail.com>
2020-10-12 15:31:47 +0100

    Add more comment details to the interactive atom height stuff

--- src/amp/components/elements/InteractiveAtomBlockComponent.tsx
+++ src/amp/components/elements/InteractiveAtomBlockComponent.tsx
@@ -18,15 +18,37 @@ const showMore = css`
     }
 `;
 
-// We look in the html field of the atom for a hint
-// for the height of the atom
-// We look for an html comment <!-- MobileHeight: 100 -->
-// and if we find it use that value for the CSS height of the atom
-// otherwise we use a default height
+// We look in the html field of the atom for a hint for the height of the atom
+// We look for an html comment <!-- MobileHeight: 100 --> and if we find it use
+// that value for the CSS height of the atom otherwise we use a default height
+//
+// 150px default height is a fairly safe arbitrary number to avoid excessive
+// whitespace but give top-of-page atoms the chance to avoid resizing (or at
+// least show a good portion of the atom)
+
+const defaultInteractiveAtomHeight = 150;
+
+// Using the MobileHeight comment is ok (lets be very aware we're creating a
+// loosely-defined contract between interactive atoms and AMP here, that should
+// really be in CAPI)
+
+const heightRegex = /<!-- MobileHeight: (.*) -->/;
+
+//  The value of <!-- MobileHeight: value --> should be the height of an
+//  interactive atom at the widest mobile breakpoint to give a guesstimate to
+//  AMP for a reasonable height
+//
+// It should only be used on interactive atoms that are at the top of articles,
+// as it only matters when the bottom of the interactive atom is within the
+// viewport (otherwise AMP will auto-resize)
+//
+// Placeholder images must always exist for atoms that require using the
+// MobileHeight comment, otherwise the atom will not show on AMP
+//
+// Full page interactives should always have both
+
 const getHeight = (html?: string): number => {
-    const defaultInteractiveAtomHeight = 100;
     if (html) {
-        const heightRegex = /<!-- MobileHeight: (.*) -->/;
         const getHeightFromComment = heightRegex.exec(html);
         if (
             getHeightFromComment &&
