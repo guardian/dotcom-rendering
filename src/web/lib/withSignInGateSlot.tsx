c36aa4e5d4a440828e7d6463b46aeb2e58b39ba7
Mahesh Makani <maheshmakani@gmail.com>
2020-10-15 15:36:36 +0100

    change sign in gate slot to use p tag
    Co-Authored-By: Dominic Kendrick <dominickendrick@users.noreply.github.com>

--- src/web/lib/withSignInGateSlot.tsx
+++ src/web/lib/withSignInGateSlot.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // This placeholder span is used by the SignInGate component to insert the sign in gate into the appropriate location within body of an article,
 // if the SignInGateSelector determines a gate should be rendered.
 
-const SignInGateSlot = <span id="sign-in-gate" />;
+const SignInGateSlot = <p id="sign-in-gate" />;
 
 export const withSignInGateSlot = (
     articleElements: (JSX.Element | null | undefined)[],
