adf5f3f0d791a4e11f9c5fb7cd9928a5c142b0b0
George Haberis <george.haberis@guardian.co.uk>
2020-10-23 12:05:45 +0100

    remove switches from payload

--- src/web/components/StickyBottomBanner/ReaderRevenueBanner.tsx
+++ src/web/components/StickyBottomBanner/ReaderRevenueBanner.tsx
@@ -134,7 +134,6 @@ export const canShow = async ({
         alreadyVisitedCount,
         engagementBannerLastClosedAt,
         subscriptionBannerLastClosedAt,
-        switches,
         hasConsentedToArticleCounts,
     });
     const forcedVariant = getForcedVariant('banner');
