4e502fada815414bf4847402b667c0e216439857
William Lacroix <william.lacroix.extern@joyn.de>
2020-10-23 15:41:21 +0100

    use src-icons for alert icon

--- src/web/components/elements/YoutubeBlockComponent.tsx
+++ src/web/components/elements/YoutubeBlockComponent.tsx
@@ -3,33 +3,12 @@ import { css } from 'emotion';
 
 import { palette } from '@guardian/src-foundations';
 import { body } from '@guardian/src-foundations/typography';
+import { SvgAlertRound } from '@guardian/src-icons';
 
 import { Caption } from '@root/src/web/components/Caption';
 import { Display } from '@root/src/lib/display';
 import { YoutubeAtom } from '@guardian/atoms-rendering';
 import { asFormat } from '@root/src/lib/format';
-
-const expiredFallbackTextStyles = css`
-    position: absolute;
-    display: table-cell;
-    padding-top: 18px;
-    padding-bottom: 18px;
-    padding-left: 60px;
-    padding-right: 60px;
-    ${body.medium({ lineHeight: 'tight' })}
-    color: ${palette.neutral[100]};
-    background-color: ${palette.neutral[20]};
-    ::before {
-        content: '';
-        background-image: url(data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMzYgMzYiIHdpZHRoPSIzNiIgaGVpZ2h0PSIzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxjaXJjbGUgZmlsbD0iI2ZmZiIgY3g9IjE4IiBjeT0iMTgiIHI9IjE4Ii8+PHBhdGggZD0iTTE0LjcyNyA1LjcyN2wxLjYzNi0xLjYzNmgzLjI3M2wxLjYzNiAxLjYzNi0xLjYzNiAxNS41NDVoLTMuMjczTDE0LjcyNyA1LjcyN202LjU0NSAyMi4wOWMwLTEuOC0xLjQ3My0zLjI3My0zLjI3My0zLjI3M2EzLjI4MyAzLjI4MyAwIDAgMC0zLjI3MyAzLjI3M2MwIDEuOCAxLjQ3MyAzLjI3MyAzLjI3MyAzLjI3M3MzLjI3My0xLjQ3MyAzLjI3My0zLjI3MyIgZmlsbD0iIzMzMyIvPjwvZz48L3N2Zz4=);
-        width: 36px;
-        height: 36px;
-        position: absolute;
-        top: 50%;
-        left: 10px;
-        margin-top: -18px;
-    }
-`;
 
 type Props = {
     display: Display;
@@ -46,6 +25,40 @@ type Props = {
     title?: string;
     duration?: number; // in seconds
 };
+
+const expiredOverlayStyles = (overrideImage: string) => css`
+    height: 0px;
+    position: relative;
+    background-image: url(${overrideImage});
+    background-size: cover;
+    background-position: 49% 49%;
+    background-repeat: no-repeat;
+    padding-bottom: 56%;
+    color: ${palette.neutral[100]};
+    background-color: ${palette.neutral[20]};
+`;
+
+const expiredTextWrapperStyles = css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    padding-top: 12px;
+    padding-bottom: 12px;
+    padding-left: 5px;
+    padding-right: 60px;
+    color: ${palette.neutral[100]};
+    background-color: ${palette.neutral[20]};
+`;
+
+const expiredSVGWrapperStyles = css`
+    padding-right: 5px;
+    svg {
+        width: 50px;
+        height: 50px;
+        fill: ${palette.neutral[100]};
+    }
+`;
 
 export const YoutubeBlockComponent = ({
     display,
@@ -74,19 +87,22 @@ export const YoutubeBlockComponent = ({
                     margin-bottom: 16px;
                 `}
             >
-                <div
-                    className={css`
-                        position: relative;
-                        background-image: url(${element.overrideImage});
-                        background-size: cover;
-                        background-repeat: no-repeat;
-                        padding-bottom: 56%;
-                    `}
-                >
-                    <div className={expiredFallbackTextStyles}>
-                        This video has been removed. This could be because it
-                        launched early, our rights have expired, there was a
-                        legal issue, or for another reason.
+                <div className={expiredOverlayStyles(element.overrideImage)}>
+                    <div className={expiredTextWrapperStyles}>
+                        <div className={expiredSVGWrapperStyles}>
+                            <SvgAlertRound />
+                        </div>
+                        <p
+                            className={css`
+                                ${body.medium({
+                                    lineHeight: 'tight',
+                                })}
+                            `}
+                        >
+                            This video has been removed. This could be because
+                            it launched early, our rights have expired, there
+                            was a legal issue, or for another reason.
+                        </p>
                     </div>
                 </div>
                 {!hideCaption && (
