225fb37af447b88a04142222818dabd3496ded62
Demetrios Skamiotis <49187886+dskamiotis@users.noreply.github.com>
2020-10-23 12:27:42 +0100

    Update capi.ts

--- src/capi.ts
+++ src/capi.ts
@@ -135,55 +135,6 @@ const getThirdPartyEmbeds = (content: Content): ThirdPartyEmbeds => {
     return allThirdPartyEmbeds;
 }
 
-// const includesTweets = (content: Content): boolean => {
-//     const body = content?.blocks?.body;
-
-//     if (!body) {
-//         return false
-//     }
-
-//     return body
-//         .flatMap(block => block.elements.some(element => element.type === ElementType.TWEET))
-//         .some(Boolean)
-
-// }
-
-// const includesInstagram = (content: Content): boolean => {
-//     const body = content?.blocks?.body;
-
-//     if (!body) {
-//         return false
-//     }
-
-//     return body
-//         .flatMap(block => block.elements.some(element => element.type === ElementType.INSTAGRAM))
-//         .some(Boolean)
-// }
-
-// const includesYoutube = (content: Content): boolean => {
-//     const body = content?.blocks?.body;
-
-//     if (!body) {
-//         return false
-//     }
-
-//     return body
-//         .flatMap(block => block.elements.some(element => element.type === ElementType.VIDEO))
-//         .some(Boolean)
-// }
-
-// const includesSpotify = (content: Content): boolean => {
-//     const body = content?.blocks?.body;
-
-//     if (!body) {
-//         return false
-//     }
-
-//     return body
-//         .flatMap(block => block.elements.some(element => element.type === ElementType.AUDIO))
-//         .some(Boolean)
-// }
-
 const paidContentLogo = (tags: Tag[]): Option<Logo> => {
     const sponsorship = tags
         .find(tag => tag.type === TagType.PAID_CONTENT)?.activeSponsorships?.pop();
