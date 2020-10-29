aebdf243e8027ffeddc67914fa78150e71664b00
Josh Buckland <buck06191@gmail.com>
2020-10-29 16:25:48 +0000

    Add Newsletter merch unit AB test
    In order to test the impact of a newsletters merch unit across
    lighthouse segments we want to roll out a 50/50 AB test. The variant
    will then be targeted with the newsletters merch unit.

---
+++ src/web/experiments/tests/newsletter-merch-unit-test.ts
@@ -0,0 +1,26 @@
+import { ABTest } from '@guardian/ab-core';
+
+export const newsletterMerchUnit: ABTest = {
+    id: 'NewsletterMerchUnit',
+    start: '2020-11-02',
+    expiry: '2020-12-01',
+    author: 'Josh Buckland',
+    description:
+        'Show a newsletter advert in the merchandising unit to 50% of users',
+    audience: 1,
+    audienceOffset: 0.0,
+    successMeasure:
+        'We see increased engagement from users shown the Newsletters ad unit',
+    audienceCriteria: 'Website users only.',
+    dataLinkNames: 'n/a',
+    idealOutcome:
+        'Increase engagement for lighthouse segments 4 and 5 via newsletters',
+    showForSensitive: false,
+    canRun: () => true,
+    variants: [
+        {
+            id: 'variant',
+            test: (): void => {},
+        },
+    ],
+};
