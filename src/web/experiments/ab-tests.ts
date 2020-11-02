2191bf8afde0bf9a7d755c1e5574d9f0e8cf0a7a
Josh Buckland <buck06191@gmail.com>
2020-11-02 16:26:33 +0000

    correct A/B test import

--- src/web/experiments/ab-tests.ts
+++ src/web/experiments/ab-tests.ts
@@ -3,7 +3,7 @@ import { abTestTest } from '@frontend/web/experiments/tests/ab-test-test';
 import { signInGateMainVariant } from '@root/src/web/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from '@root/src/web/experiments/tests/sign-in-gate-main-control';
 import { curatedContainerTest } from '@frontend/web/experiments/tests/curated-container-test';
-import { newsletterMerchUnit } from './tests/newsletter-merch-unit-test';
+import { newsletterMerchUnit } from '@frontend/web/experiments/tests/newsletter-merch-unit-test';
 
 export const tests: ABTest[] = [
     abTestTest,
