8ccf3ebf8a23030ca4793f171d90da2c5eb6d2e0
William Lacroix <william.lacroix.extern@joyn.de>
2020-11-11 11:30:13 +0000

    delete banner test

--- cypress/integration/e2e/article.bannerPicker.e2e.spec.js
+++ cypress/integration/e2e/article.bannerPicker.e2e.spec.js
@@ -22,24 +22,4 @@ describe('Banner Picker Integration', function () {
             cmpIframe().contains("It's your choice");
         });
     });
-
-    // TODO: unable to get test working, need contributions to help understand why API call never fired
-    // eslint-disable-next-line mocha/no-skipped-tests
-    it.skip('makes a single request to the banner service', function () {
-        cy.visit(
-            `Article?url=https://www.theguardian.com/games/2018/aug/23/nier-automata-yoko-taro-interview`,
-        );
-
-        cy.window().then((win) => {
-            const fetchSpy = cy.spy(win, 'fetch');
-            fetchSpy
-                .withArgs(
-                    'https://contributions.guardianapis.com/banner',
-                    Cypress.sinon.match.any,
-                )
-                .as('readerRevenueBannerFetch');
-        });
-
-        cy.get('@readerRevenueBannerFetch').should('have.been.calledOnce');
-    });
 });
