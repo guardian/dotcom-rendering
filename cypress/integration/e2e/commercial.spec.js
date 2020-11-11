69db549eb5aa8f84d4447b8c10f95df3b4e7e18a
Oliver Lloyd <email@oliverlloyd.com>
2020-11-11 14:51:44 +0000

    Use disableCMP lib + prettier

--- cypress/integration/e2e/commercial.spec.js
+++ cypress/integration/e2e/commercial.spec.js
@@ -1,27 +1,25 @@
 import { getPolyfill } from '../../lib/polyfill';
 import { fetchPolyfill } from '../../lib/config';
+import { disableCMP } from '../../lib/disableCMP';
 
 describe('Commercial E2E tests', function () {
     before(getPolyfill);
 
     beforeEach(function () {
-        // Disable CMP
-        cy.setCookie('gu-cmp-disabled', 'true', {
-            log: true,
-        });
-
+        disableCMP();
     });
 
     describe('for WEB', function () {
         // eslint-disable-next-line mocha/no-setup-in-describe
         it(`It should check slots for a long article`, function () {
-                const longReadURL = "https://www.theguardian.com/environment/2020/oct/13/maverick-rewilders-endangered-species-extinction-conservation-uk-wildlife";
-                cy.log(`Opening A long read`);
-                cy.visit(`Article?url=${longReadURL}`, fetchPolyfill);
+            const longReadURL =
+                'https://www.theguardian.com/environment/2020/oct/13/maverick-rewilders-endangered-species-extinction-conservation-uk-wildlife';
+            cy.log(`Opening A long read`);
+            cy.visit(`Article?url=${longReadURL}`, fetchPolyfill);
 
-                cy.scrollTo('bottom', { duration: 4500 });
+            cy.scrollTo('bottom', { duration: 4500 });
 
-                cy.get('.js-ad-slot').should('have.length', 15)
-            });
+            cy.get('.js-ad-slot').should('have.length', 15);
+        });
     });
 });
