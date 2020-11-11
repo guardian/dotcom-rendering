c0e40ae3da420bfea67e749883eaf7fec22139d9
William Lacroix <william.lacroix.extern@joyn.de>
2020-11-11 13:26:27 +0000

    add conditional orientation

--- src/web/components/Callout/RadioSelect.tsx
+++ src/web/components/Callout/RadioSelect.tsx
@@ -25,7 +25,12 @@ export const RadioSelect = ({
         `}
     >
         <FieldLabel formField={formField} />
-        <RadioGroup name={formField.name} orientation="vertical">
+        <RadioGroup
+            name={formField.name}
+            orientation={
+                formField.options.length > 2 ? 'vertical' : 'horizontal'
+            }
+        >
             {formField.options.map((option, index) => {
                 const isRadioChecked =
                     formField.id in formData &&
