6f8316a928cb04dd5684877faf6894574e4335e1
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-11-12 14:36:15 +0000

    Splitting the test in 2 to have three variants, at 50%, 25% and 25%

--- src/web/experiments/ab-tests.ts
+++ src/web/experiments/ab-tests.ts
@@ -3,12 +3,13 @@ import { abTestTest } from '@frontend/web/experiments/tests/ab-test-test';
 import { signInGateMainVariant } from '@root/src/web/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from '@root/src/web/experiments/tests/sign-in-gate-main-control';
 import { curatedContainerTest } from '@frontend/web/experiments/tests/curated-container-test';
-import {newsletterMerchUnitLighthouse} from "@root/src/web/experiments/tests/newsletter-merch-unit-test";
+import {newsletterMerchUnitLighthouseControl, newsletterMerchUnitLighthouseVariants} from "@root/src/web/experiments/tests/newsletter-merch-unit-test";
 
 export const tests: ABTest[] = [
     abTestTest,
     signInGateMainVariant,
     signInGateMainControl,
     curatedContainerTest,
-    newsletterMerchUnitLighthouse,
+    newsletterMerchUnitLighthouseControl,
+    newsletterMerchUnitLighthouseVariants
 ];
--- src/web/experiments/tests/newsletter-merch-unit-test.ts
+++ src/web/experiments/tests/newsletter-merch-unit-test.ts
@@ -1,12 +1,12 @@
 import { ABTest } from '@guardian/ab-core';
 
-export const newsletterMerchUnitLighthouse: ABTest = {
-    id: 'NewsletterMerchUnitLighthouse',
+export const newsletterMerchUnitLighthouseControl: ABTest = {
+    id: 'NewsletterMerchUnitLighthouseControl',
     start: '2020-11-11',
     expiry: '2020-12-01',
     author: 'Josh Buckland & Alex Dufournet',
     description: 'Show a newsletter advert in the merchandising unit to 50% of users',
-    audience: 1.0,
+    audience: 0.5,
     audienceOffset: 0.0,
     successMeasure: 'We see increased engagement from users shown the Newsletters ad unit',
     audienceCriteria: 'Website users only.',
@@ -18,8 +18,29 @@ export const newsletterMerchUnitLighthouse: ABTest = {
             id: 'control',
             test: (): void => {},
         },
+    ],
+};
+
+export const newsletterMerchUnitLighthouseVariants: ABTest = {
+    id: 'NewsletterMerchUnitLighthouseVariants',
+    start: '2020-11-11',
+    expiry: '2020-12-01',
+    author: 'Josh Buckland & Alex Dufournet',
+    description: 'Show a newsletter advert in the merchandising unit to 50% of users',
+    audience: 0.5,
+    audienceOffset: 0.5,
+    successMeasure: 'We see increased engagement from users shown the Newsletters ad unit',
+    audienceCriteria: 'Website users only.',
+    idealOutcome: 'Investigate lighthouse segment engagement via newsletters',
+    showForSensitive: false,
+    canRun: () => true,
+    variants: [
         {
-            id: 'variant',
+            id: 'variant1',
+            test: (): void => {},
+        },
+        {
+            id: 'variant2',
             test: (): void => {},
         },
     ],
