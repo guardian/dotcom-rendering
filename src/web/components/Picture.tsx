a0b89a2e5b7f657118148d043ddcb68f0e2d70af
Oliver Lloyd <email@oliverlloyd.com>
2020-11-16 09:24:27 +0000

    Replace picture tag with an img tag

--- src/web/components/Picture.tsx
+++ src/web/components/Picture.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// IE 9 needs this workaround as per https://scottjehl.github.io/picturefill/
+import { css } from 'emotion';
 
 type Props = {
     imageSources: ImageSource[];
@@ -7,96 +7,81 @@ type Props = {
     alt: string;
     height: string;
     width: string;
+    isMainMedia?: boolean;
     isLazy?: boolean;
 };
 
-interface PictureSource {
-    width: number;
-    minWidth: number;
-    srcset: string;
-    hidpi: boolean;
-}
-
-const mq: (source: PictureSource) => string = (source) =>
-    source.hidpi
-        ? `(min-width: ${source.minWidth}px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: ${source.minWidth}px) and (min-resolution: 120dpi)`
-        : `(min-width: ${source.minWidth}px)`;
-
-const forSource: (source: PictureSource) => string = (source) =>
-    ` <source media="${mq(source)}" sizes="${source.width}px" srcset="${
-        source.srcset
-    }" />`;
-
-const selectScrSetItemForWidth = (
+const getClosestSetForWidth = (
     desiredWidth: number,
     inlineSrcSets: SrcSetItem[],
 ): SrcSetItem => {
+    // For a desired width, find the SrcSetItem which is the closest match
     const sorted = inlineSrcSets.sort((a, b) => b.width - a.width);
-
     return sorted.reduce((best, current) => {
         if (current.width < best.width && current.width >= desiredWidth) {
             return current;
         }
-
         return best;
     });
 };
 
-const getSrcSetsForWeighting = (
+const getFallbackSrc = (srcSets: SrcSetItem[]): string => {
+    // The assumption here is readers on devices that do not support srcset are likely to be on poor
+    // network connections so we're going to fallback to a small image
+    return getClosestSetForWidth(300, srcSets).src;
+};
+
+// const getSrcSetsForRole = (
+//     role: RoleType,
+//     imageSources: ImageSource[],
+// ): SrcSetItem[] | undefined => {
+//     return imageSources.find((source) => source.weighting === role)?.srcSet;
+// };
+
+const getSrcSetsForRole = (
+    role: RoleType,
     imageSources: ImageSource[],
-    forWeighting: RoleType,
-): SrcSetItem[] =>
-    imageSources.filter(
+): SrcSetItem[] => {
+    return imageSources.filter(
         ({ weighting }) =>
             // Use toLowerCase to handle cases where we have halfWidth comparing to halfwidth
-            weighting.toLowerCase() === forWeighting.toLowerCase(),
+            weighting.toLowerCase() === role.toLowerCase(),
     )[0].srcSet;
+};
 
-const makePictureSource = (
-    hidpi: boolean,
-    minWidth: number,
-    srcSet: SrcSetItem,
-): PictureSource => {
-    return {
-        hidpi,
-        minWidth,
-        width: srcSet.width,
-        srcset: `${srcSet.src} ${hidpi ? srcSet.width * 2 : srcSet.width}w`,
+const buildSourcesString = (srcSets: SrcSetItem[]): string => {
+    return srcSets.map((srcSet) => `${srcSet.src} ${srcSet.width}w`).join(',');
+};
+
+const buildSizesString = (role: RoleType, isMainMedia: boolean): string => {
+    const breakpoints = {
+        mobileMedium: 375,
+        mobileLandscape: 480,
+        phablet: 660,
+        tablet: 740,
+        desktop: 980,
+        leftCol: 1140,
+        wide: 1300,
     };
-};
 
-const makeSources = (
-    imageSources: ImageSource[],
-    role: RoleType,
-): PictureSource[] => {
-    const inlineSrcSets = getSrcSetsForWeighting(imageSources, role);
-    const sources: PictureSource[] = [];
-    inlineSrcSets
-        .map((item) => item.width)
-        .forEach((width) => {
-            sources.push(
-                makePictureSource(
-                    true,
-                    width,
-                    selectScrSetItemForWidth(width, inlineSrcSets),
-                ),
-            );
-            sources.push(
-                makePictureSource(
-                    false,
-                    width,
-                    selectScrSetItemForWidth(width, inlineSrcSets),
-                ),
-            );
-        });
-
-    return sources;
-};
-
-const getFallback: (imageSources: ImageSource[]) => string = (imageSources) => {
-    const inlineSrcSets = getSrcSetsForWeighting(imageSources, 'inline');
-
-    return selectScrSetItemForWidth(300, inlineSrcSets).src;
+    switch (role) {
+        case 'inline':
+            return `(min-width: ${breakpoints.phablet}px) 620px, 100vw`;
+        case 'halfWidth':
+            return `(min-width: ${breakpoints.phablet}px) 300px, 50vw`;
+        case 'thumbnail':
+            return '140px';
+        case 'immersive':
+            return isMainMedia
+                ? '100vw'
+                : `(min-width: ${breakpoints.wide}px) 1300px, 100vw`;
+        case 'supporting':
+            return `(min-width: ${breakpoints.wide}px) 380px, 300px`;
+        case 'showcase':
+            return isMainMedia
+                ? `(min-width: ${breakpoints.wide}px) 1020px, (min-width: ${breakpoints.leftCol}px) 940px, (min-width: ${breakpoints.tablet}px) 700px, (min-width: ${breakpoints.phablet}px) 660px, 100vw`
+                : `(min-width: ${breakpoints.wide}px) 860px, (min-width: ${breakpoints.leftCol}px) 780px, (min-width: ${breakpoints.phablet}px) 620px, 10vw`;
+    }
 };
 
 export const Picture = ({
@@ -105,23 +90,29 @@ export const Picture = ({
     alt,
     height,
     width,
+    isMainMedia = false,
     isLazy = true,
 }: Props) => {
-    const sources = makeSources(imageSources, role);
-    const src = getFallback(imageSources);
+    const srcSetForRole = getSrcSetsForRole(role, imageSources);
+    const src = getFallbackSrc(srcSetForRole);
+    const sources = buildSourcesString(srcSetForRole);
+    const sizes = buildSizesString(role, isMainMedia);
+
     return (
-        // https://stackoverflow.com/questions/10844205/html-5-strange-img-always-adds-3px-margin-at-bottom
-        // why did we put `style="vertical-align: middle;"` inside the img tag
-        <picture
-            dangerouslySetInnerHTML={{
-                __html: `<!--[if IE 9]><video style="display: none;"><![endif]-->${sources
-                    .map(forSource)
-                    .join(
-                        '',
-                    )}<!--[if IE 9]></video><![endif]--><img style="vertical-align: middle;" itemprop="contentUrl" alt="${alt}" src="${src}" height="${height}" width="${width}" ${
-                    isLazy ? 'loading="lazy"' : ''
-                } />`,
-            }}
+        <img
+            itemProp="contentUrl"
+            alt={alt}
+            src={src}
+            srcSet={sources}
+            sizes={sizes}
+            height={height}
+            width={width}
+            loading={isLazy ? 'lazy' : undefined}
+            // https://stackoverflow.com/questions/10844205/html-5-strange-img-always-adds-3px-margin-at-bottom
+            // why did we add the css `vertical-align: middle;` to the img tag
+            className={css`
+                vertical-align: middle;
+            `}
         />
     );
 };
