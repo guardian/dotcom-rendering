b0bb4229360680c8b6a2a2f7bf2dd62f102c76a4
Oliver Lloyd <email@oliverlloyd.com>
2020-11-23 09:20:25 +0000

    Added support for simple block quotes as well as quoted

--- src/web/components/elements/BlockquoteBlockComponent.tsx
+++ src/web/components/elements/BlockquoteBlockComponent.tsx
@@ -11,42 +11,80 @@ import { QuoteIcon } from '@root/src/web/components/QuoteIcon';
 type Props = {
     html: string;
     pillar: Pillar;
+    quoted?: boolean;
 };
 
-const Row = ({ children }: { children: React.ReactNode }) => (
-    <div
+const BlockquoteRow = ({ children }: { children: React.ReactNode }) => (
+    <blockquote
         className={css`
             display: flex;
             flex-direction: row;
         `}
     >
         {children}
-    </div>
+    </blockquote>
 );
+
+const baseBlockquoteStyles = css`
+    margin-bottom: 16px;
+    ${body.medium()};
+    font-style: italic;
+    color: ${neutral[46]};
+`;
+
+const simpleBlockquoteStyles = css`
+    ${baseBlockquoteStyles}
+    margin-top: 16px;
+    margin-right: 0;
+    margin-bottom: 16px;
+    margin-left: 33px;
+`;
 
 export const BlockquoteBlockComponent: React.FC<Props> = ({
     html,
     pillar,
+    quoted,
 }: Props) => {
-    const { willUnwrap: isUnwrapped, unwrappedHtml } = unwrapHtml({
-        fixes: [{ prefix: '<blockquote>', suffix: '</blockquote>' }],
+    const {
+        willUnwrap: isUnwrapped,
+        unwrappedHtml,
+        unwrappedElement,
+    } = unwrapHtml({
+        fixes: [
+            { prefix: '<p>', suffix: '</p>', unwrappedElement: 'p' },
+            {
+                prefix: '<blockquote>',
+                suffix: '</blockquote>',
+                unwrappedElement: 'blockquote',
+            },
+            {
+                prefix: '<blockquote class="quoted">',
+                suffix: '</blockquote>',
+                unwrappedElement: 'div',
+            },
+        ],
         html,
     });
 
+    if (quoted) {
+        return (
+            <BlockquoteRow>
+                <QuoteIcon colour={pillarPalette[pillar].main} size="large" />
+                <RewrappedComponent
+                    isUnwrapped={isUnwrapped}
+                    html={unwrappedHtml}
+                    elCss={baseBlockquoteStyles}
+                    tagName={unwrappedElement}
+                />
+            </BlockquoteRow>
+        );
+    }
     return (
-        <Row>
-            <QuoteIcon colour={pillarPalette[pillar].main} size="large" />
-            <RewrappedComponent
-                isUnwrapped={isUnwrapped}
-                html={unwrappedHtml}
-                elCss={css`
-                    margin-bottom: 16px;
-                    ${body.medium()};
-                    font-style: italic;
-                    color: ${neutral[46]};
-                `}
-                tagName="blockquote"
-            />
-        </Row>
+        <RewrappedComponent
+            isUnwrapped={isUnwrapped}
+            html={unwrappedHtml}
+            elCss={simpleBlockquoteStyles}
+            tagName={unwrappedElement}
+        />
     );
 };
