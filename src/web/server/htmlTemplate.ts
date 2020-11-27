9b26ce90bbcd25e366ee651b0b823b6f17701bec
Alex Sanders <a@snd.rs>
2020-11-27 01:12:30 +0000

    fetch all normal fonts, plus headline

--- src/web/server/htmlTemplate.ts
+++ src/web/server/htmlTemplate.ts
@@ -210,17 +210,13 @@ export const htmlTemplate = ({
                 window.autoFoft = {
                     isCritical: function (font) {
                         switch (font.family) {
-                            case 'GuardianTextEgyptian':
-                            case 'Guardian Text Egyptian Web':
-                            case 'GuardianTextSans':
-                            case 'Guardian Text Sans Web':
-                                return font.weight === 'normal' || font.weight === '400';
                             case 'GH Guardian Headline':
                             case 'Guardian Egyptian Web':
                                 return (font.weight === '500' || font.weight === '700') &&
                                     font.style === 'normal';
                             default:
-                                return false;
+                                return (font.weight === 'normal' || font.weight === '400') &&
+                                    font.style === 'normal';
                         }
                     }
                 }
