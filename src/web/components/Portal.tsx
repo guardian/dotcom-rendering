1858a4c96ba8ec0d43a77bb7c57bdd4af4ef6808
Oliver Lloyd <email@oliverlloyd.com>
2020-12-02 09:43:32 +0000

    Guard against performance not being defined on older browsers

--- src/web/components/Portal.tsx
+++ src/web/components/Portal.tsx
@@ -10,7 +10,7 @@ export const Portal = ({ root, children, richLinkIndex }: Props) => {
     const rootId = richLinkIndex ? `${root}-${richLinkIndex}` : root;
     const element = document.getElementById(rootId);
     if (!element) return null;
-    window.performance.mark(`${rootId}-portal-start`);
+    window.performance?.mark(`${rootId}-portal-start`);
     // First remove any placeholder. Why? Because we sometimes server side render Placeholders in
     // the root divs where the Portal is being inserted so the page is rendered with the placeholder
     // showing (to reduce jank). But ReactDOM.createPortal won't replace content so we need to
@@ -21,8 +21,8 @@ export const Portal = ({ root, children, richLinkIndex }: Props) => {
     if (placeholderElement) placeholderElement.remove();
 
     const result = ReactDOM.createPortal(children, element);
-    window.performance.mark(`${rootId}-portal-end`);
-    window.performance.measure(
+    window.performance?.mark(`${rootId}-portal-end`);
+    window.performance?.measure(
         `${rootId}-portal`,
         `${rootId}-portal-start`,
         `${rootId}-portal-end`,
