919ca589e489454712c54ff47550d0342cb9c0a9
Tom Forbes <tom.forbes@theguardian.com>
2020-12-04 09:05:51 +0000

    remove Aus header
    WIP
    
    header test
    
    oops
    
    countryCode
    
    format
    
    fix tests
    
    fix tests
    
    test result
    
    pass in countryCode
    
    fix story
    
    fix storybook
    
    use edition
    
    lint etc
    
    fix test
    
    reuse
    
    nbsp
    
    update all urls

---
+++ src/web/experiments/tests/global-eoy-header-test.ts
@@ -0,0 +1,31 @@
+import { ABTest } from '@guardian/ab-core';
+
+export const GlobalEoyHeaderTestName = 'GlobalEoyHeaderTest';
+export type GlobalEoyHeaderTestVariant = 'control' | 'variant' | 'notintest';
+
+const month = new Date().getMonth() + 1;    // js date month begins at 0
+
+export const globalEoyHeaderTest: ABTest = {
+    id: GlobalEoyHeaderTestName,
+    start: '2020-12-02',
+    expiry: '2021-02-01',
+    author: 'Tom Forbes',
+    description: 'Test reader revenue message in header',
+    audience: 1.0,
+    audienceOffset: 0,
+    successMeasure: 'AV',
+    idealOutcome: 'AV',
+    showForSensitive: false,
+    audienceCriteria: 'All',
+    canRun: () => month === 12 || month === 1,
+    variants: [
+        {
+            id: 'control',
+            test: (): void => {},
+        },
+        {
+            id: 'variant',
+            test: (): void => {},
+        },
+    ],
+};
