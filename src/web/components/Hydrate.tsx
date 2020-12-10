142b2d6f1e61b1ee9710f4a6c2a34540d5f19234
Oliver Lloyd <email@oliverlloyd.com>
2020-12-10 13:26:50 +0000

    Move performance mark into the hydrate callback to be more accurate

--- src/web/components/Hydrate.tsx
+++ src/web/components/Hydrate.tsx
@@ -11,12 +11,14 @@ export const Hydrate = ({ root, index, children }: Props) => {
     const element = document.getElementById(rootWithIndex);
     if (!element) return null;
     window.performance?.mark(`${rootWithIndex}-hydrate-start`);
-    ReactDOM.hydrate(children, element);
-    window.performance?.mark(`${rootWithIndex}-hydrate-end`);
-    window.performance?.measure(
-        `${rootWithIndex}-hydrate`,
-        `${rootWithIndex}-hydrate-start`,
-        `${rootWithIndex}-hydrate-end`,
-    );
+    ReactDOM.hydrate(children, element, () => {
+        console.log('Hydrated', root);
+        window.performance?.mark(`${rootWithIndex}-hydrate-end`);
+        window.performance?.measure(
+            `${rootWithIndex}-hydrate`,
+            `${rootWithIndex}-hydrate-start`,
+            `${rootWithIndex}-hydrate-end`,
+        );
+    });
     return null;
 };
