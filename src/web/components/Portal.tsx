252727062a436b574549cfc1daf1caf9a2fa139f
Oliver Lloyd <email@oliverlloyd.com>
2020-12-10 14:50:13 +0000

    Only create Portals once

--- src/web/components/Portal.tsx
+++ src/web/components/Portal.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import { initPerf } from '@root/src/web/browser/initPerf';
@@ -5,10 +6,23 @@ import { initPerf } from '@root/src/web/browser/initPerf';
 type Props = {
     root: IslandType;
     children: React.ReactNode;
+    dependencies?: unknown[];
     richLinkIndex?: number;
 };
 
-export const Portal = ({ root, children, richLinkIndex }: Props) => {
+const isReady = (dependencies: unknown[]): boolean => {
+    return dependencies.every((dep) => dep !== undefined);
+};
+
+export const Portal = ({
+    root,
+    children,
+    dependencies = [],
+    richLinkIndex,
+}: Props) => {
+    const [alreadyCreated, setAlreadyCreated] = useState(false);
+    if (alreadyCreated) return null;
+    if (!isReady(dependencies)) return null;
     const rootId = richLinkIndex ? `${root}-${richLinkIndex}` : root;
     const { start, end } = initPerf(`${rootId}-portal`);
     const element = document.getElementById(rootId);
@@ -22,8 +36,8 @@ export const Portal = ({ root, children, richLinkIndex }: Props) => {
         '[data-name="placeholder"]',
     );
     if (placeholderElement) placeholderElement.remove();
-
     const result = ReactDOM.createPortal(children, element);
     end();
+    setAlreadyCreated(true);
     return result;
 };
