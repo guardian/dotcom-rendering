5683c61fee30833a0fd9d6b31df21273e5ab312a
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2020-12-14 16:48:04 +0000

    border wip

--- src/components/editions/pullquote.tsx
+++ src/components/editions/pullquote.tsx
@@ -15,16 +15,20 @@ export const pullquoteWidth = '10.875rem';
 
 const styles = (format: Format): SerializedStyles => {
 	const { kicker, inverted } = getThemeStyles(format.theme);
-	return css`
+    return css`
 		color: ${kicker};
 		${darkModeCss`color: ${inverted};`}
+        border: 1px solid ${kicker};
 		border-top: 12px solid ${kicker};
-		${darkModeCss`border-top: ${inverted};`}
-		${headline.xxsmall({ fontWeight: 'medium' })}
+        ${darkModeCss`border: 1px solid ${inverted};`}
+		${darkModeCss`border-top: 12px solid ${inverted};`}
+        border-bottom: none;
+        ${darkModeCss`border-bottom: none;`}
         float: left;
 		clear: left;
+        padding: ${basePx(1)};
+        padding-bottom: ${basePx(3)};
 		margin: ${basePx(0, 2, 1, 0)};
-
 		width: ${pullquoteWidth};
 		${from.wide} {
 			margin-left: calc(-${pullquoteWidth} - ${basePx(2)} - ${basePx(3)});
@@ -37,7 +41,7 @@ const quoteStyles = (format: Format): SerializedStyles => {
 
 	return css`
 		margin: 0;
-
+        ${headline.xxsmall({ fontWeight: "regular" })}
 		svg {
 			margin-bottom: -0.6rem;
 			height: 2.3rem;
@@ -51,6 +55,7 @@ const quoteStyles = (format: Format): SerializedStyles => {
 
 const citeStyles = css`
 	font-style: normal;
+    ${headline.xxsmall({ fontWeight: "bold" })}
 `;
 
 type Props = {
