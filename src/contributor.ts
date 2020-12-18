a9fc8758734ee536913daa583866c91489c0dac1
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-12-18 14:11:40 +0000

    Migrated to image-rendering for image sources (#1014)
    - Updated image-rendering to v4.1.0
    - Removed code to generate image sources from `image.ts`
    - Updated imports to use new types and functions for image sources

--- src/contributor.ts
+++ src/contributor.ts
@@ -1,12 +1,11 @@
 // ----- Imports ----- //
 
 import type { Content } from '@guardian/content-api-models/v1/content';
-import { Role } from '@guardian/image-rendering';
+import { Dpr, Role, src, srcsetWithWidths } from '@guardian/image-rendering';
 import type { Option } from '@guardian/types';
 import { fromNullable, map, none } from '@guardian/types';
 import { articleContributors } from 'capi';
 import type { Image } from 'image';
-import { Dpr, src, srcsetWithWidths } from 'image';
 import { pipe2 } from 'lib';
 
 // ------ Types ----- //
--- src/image.test.ts
+++ src/image.test.ts
@@ -2,7 +2,7 @@
 
 import { JSDOM } from 'jsdom';
 
-import { Dpr, parseImage, Image, srcset } from 'image';
+import { parseImage, Image } from 'image';
 import { BlockElement } from '@guardian/content-api-models/v1/blockElement';
 import { ElementType } from '@guardian/content-api-models/v1/elementType';
 import { AssetType } from '@guardian/content-api-models/v1/assetType';
@@ -86,23 +86,5 @@ describe('image', () => {
 				withDefault(''),
 			),
 		).toBe('');
-	});
-
-	test('show lower quality when DPR is 2', () => {
-		const src = srcset(
-			'https://media.guim.co.uk/img/media/948ad0a2ebe6d931d8827ea89ac184986af76c1b/0_22_1313_788/master/1313.jpg',
-			'',
-			Dpr.Two,
-		);
-		expect(src).toContain('quality=45');
-	});
-
-	test('show higher quality when DPR is 1', () => {
-		const src = srcset(
-			'https://media.guim.co.uk/img/media/948ad0a2ebe6d931d8827ea89ac184986af76c1b/0_22_1313_788/master/1313.jpg',
-			'',
-			Dpr.One,
-		);
-		expect(src).toContain('quality=85');
 	});
 });
