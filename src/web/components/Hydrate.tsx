d5537585f97179144a9fa571b23b1c4953d32ad1
Oliver Lloyd <email@oliverlloyd.com>
2020-12-21 17:37:17 +0000

    Rename dependencies to waitFor

--- src/web/components/Hydrate.tsx
+++ src/web/components/Hydrate.tsx
@@ -5,24 +5,19 @@ import { initPerf } from '@root/src/web/browser/initPerf';
 
 type Props = {
     root: IslandType;
-    dependencies?: unknown[];
     children: JSX.Element;
     index?: number;
+    waitFor?: unknown[];
 };
 
 const isReady = (dependencies: unknown[]): boolean => {
     return dependencies.every((dep) => dep !== undefined);
 };
 
-export const Hydrate = ({
-    root,
-    index,
-    dependencies = [],
-    children,
-}: Props) => {
+export const Hydrate = ({ root, children, index, waitFor = [] }: Props) => {
     const [alreadyHydrated, setAlreadyHydrated] = useState(false);
     if (alreadyHydrated) return null;
-    if (!isReady(dependencies)) return null;
+    if (!isReady(waitFor)) return null;
     const rootId = index === 0 || index ? `${root}-${index}` : root;
     const { start, end } = initPerf(`${rootId}-hydrate`);
     const element = document.getElementById(rootId);
