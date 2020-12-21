c283b40b343819b4f4654a3a20d90fc27880aeb2
Oliver Lloyd <email@oliverlloyd.com>
2020-12-21 18:05:55 +0000

    Added jsdoc comments

--- src/web/components/HydrateOnce.tsx
+++ src/web/components/HydrateOnce.tsx
@@ -14,6 +14,13 @@ const isReady = (dependencies: unknown[]): boolean => {
     return dependencies.every((dep) => dep !== undefined);
 };
 
+/**
+ * Finds the element with the same id as `root` and hydrates it using `children`
+ * @param {String} root - The id of the element to hydrate
+ * @param children - The react elements passed to ReactDOM.hydrate()
+ * @param {number} index - Used when there are multiple elements the same (eg. RichLinks)
+ * @param {Array} waitFor - An array of variables that must be defined before the task is executed
+ * */
 export const HydrateOnce = ({ root, children, index, waitFor = [] }: Props) => {
     const [alreadyHydrated, setAlreadyHydrated] = useState(false);
     if (alreadyHydrated) return null;
