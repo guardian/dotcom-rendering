ca4efe228b14d4ed80a95c1b264dcb3f63d5f415
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2020-12-22 11:48:36 +0000

    colour

--- src/components/editions/series.tsx
+++ src/components/editions/series.tsx
@@ -1,22 +1,27 @@
 // ----- Imports ----- //
 
-import { css } from '@emotion/core';
+import { css, SerializedStyles } from '@emotion/core';
 import { remSpace } from '@guardian/src-foundations';
-import { news } from '@guardian/src-foundations/palette';
 import { titlepiece } from '@guardian/src-foundations/typography';
-import type { Item } from 'item';
+import { getFormat, Item } from 'item';
 import { maybeRender } from 'lib';
 import type { FC } from 'react';
+import { getThemeStyles } from 'themeStyles';
 import { kickerPicker } from './kickerPicker';
 
 // ----- Component ----- //
 
-const styles = css`
-	${titlepiece.small()}
-	color: ${news[400]};
-	font-size: 1.0625rem;
-	padding: ${remSpace[1]} 0 ${remSpace[2]};
-`;
+const styles = (item: Item): SerializedStyles => {
+	const format = getFormat(item);
+	const { kicker } = getThemeStyles(format.theme);
+
+	return css`
+		${titlepiece.small()}
+		color: ${kicker};
+		font-size: 1.0625rem;
+		padding: ${remSpace[1]} 0 ${remSpace[2]};
+	`;
+};
 
 interface Props {
 	item: Item;
@@ -24,7 +29,7 @@ interface Props {
 
 const Series: FC<Props> = ({ item }) =>
 	maybeRender(kickerPicker(item), (kicker) => (
-		<nav css={styles}>{kicker}</nav>
+		<nav css={styles(item)}>{kicker}</nav>
 	));
 
 // ----- Exports ----- //
