a9afecf7717372869a4be40c670652daaa6752b1
William Lacroix <william.j.t.lacroix@gmail.com>
2020-12-31 13:56:07 +0000

    add cypress test for embed youtube video

--- cypress/integration/e2e/atom.video.spec.js
+++ cypress/integration/e2e/atom.video.spec.js
@@ -1,7 +1,10 @@
 import { disableCMP } from '../../lib/disableCMP.js';
 
 const mainMediaVideo = 'https://www.theguardian.com/uk-news/2020/dec/04/edinburgh-hit-by-thundersnow-as-sonic-boom-wakes-residents'
-const playEvent = {"id":"gu-video-youtube-2b33a7b7-e639-4232-9ecd-0fb920fa8147","eventType":"video:content:play"}
+const mainMediaPlayEvent = {"id":"gu-video-youtube-2b33a7b7-e639-4232-9ecd-0fb920fa8147","eventType":"video:content:play"}
+
+const embedMediaVideo = 'https://www.theguardian.com/us-news/2020/oct/29/lordstown-ohio-trump-gm-plant-election'
+const embedPlayEvent = {"id":"gu-video-youtube-35f9a38f-c0c5-4bbd-9395-e338c9e97d30","eventType":"video:content:play"}
 
 describe('Video', function () {
     describe('Main media youtube video', function () {
@@ -21,7 +24,7 @@ describe('Video', function () {
 
         it('should dispatch play to server', function () {
             const listOfOphanVideoValues = []
-            // video: encodeURIComponent(JSON.stringify(playEvent))}
+
             cy.intercept({
                 url: /^http:\/\/ophan\.theguardian\.com\/img\/2/,
                 query: {
@@ -44,7 +47,51 @@ describe('Video', function () {
 
             // eslint-disable-next-line cypress/no-unnecessary-waiting
             cy.wait(2000).then(function(){
-                expect(listOfOphanVideoValues).to.deep.include(playEvent);
+                expect(listOfOphanVideoValues).to.deep.include(mainMediaPlayEvent);
+            })
+
+        })
+    })
+
+    describe('Embed youtube video', function () {
+        it('should render', function() {
+            cy.visit(`/Article?url=${embedMediaVideo}`);
+            cy.get(`#youtube-block-41`).scrollIntoView();
+            cy.get(`#youtube-block-41`).should('be.visible');
+        })
+
+        it('should hide overlay on click', function() {
+            cy.get(`#youtube-overlay-N9Cgy-ke5-s`).should('be.visible');
+            cy.get(`#youtube-overlay-N9Cgy-ke5-s`).click();
+            cy.get(`#youtube-overlay-N9Cgy-ke5-s`).should('not.be.visible');
+        })
+
+        it('should dispatch play to server', function () {
+            const listOfOphanVideoValues = []
+            cy.intercept({
+                url: /^http:\/\/ophan\.theguardian\.com\/img\/2/,
+                query: {
+                    viewId: /^\.*/,
+                    video: /^\.*/,
+                }
+            }, (req) => {
+                const url = new URL(req.url)
+                const videoValue = url.searchParams.get('video')
+                if (videoValue) listOfOphanVideoValues.push(JSON.parse(videoValue))
+            })
+
+            cy.visit(`/Article?url=${embedMediaVideo}`);
+            cy.get(`#youtube-block-41`).scrollIntoView();
+            cy.get(`#youtube-block-41`).should('be.visible');
+
+
+            cy.get(`#youtube-overlay-N9Cgy-ke5-s`).should('be.visible');
+            cy.get(`#youtube-overlay-N9Cgy-ke5-s`).click();
+            cy.get(`#youtube-overlay-N9Cgy-ke5-s`).should('not.be.visible');
+
+            // eslint-disable-next-line cypress/no-unnecessary-waiting
+            cy.wait(2000).then(function(){
+                expect(listOfOphanVideoValues).to.deep.include(embedPlayEvent);
             })
 
         })
