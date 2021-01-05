ae43151ccdea4fa3644b29e79d86f0fb0c33956d
William Lacroix <william.j.t.lacroix@gmail.com>
2021-01-05 13:42:56 +0000

    fix import fixture

--- src/web/layouts/Standard.stories.tsx
+++ src/web/layouts/Standard.stories.tsx
@@ -9,7 +9,7 @@ import { Analysis } from '@root/fixtures/articles/Analysis';
 import { Feature } from '@root/fixtures/articles/Feature';
 import { GuardianView } from '@root/fixtures/articles/GuardianView';
 import { Interview } from '@root/fixtures/articles/Interview';
-import { Quiz } from 'fixtures/articles/Quiz';
+import { Quiz } from '@root/fixtures/articles/Quiz';
 import { Recipe } from '@root/fixtures/articles/Recipe';
 import { Comment } from '@root/fixtures/articles/Comment';
 import { MatchReport } from '@root/fixtures/articles/MatchReport';
