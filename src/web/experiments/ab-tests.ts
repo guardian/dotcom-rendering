71efb65a427ef6927ab3d8dffd66af81a8cd4adb
Mark Addis <mark.addis@guardian.co.uk>
2021-01-05 16:40:54 +0000

    Deeply Read logic

--- src/web/experiments/ab-tests.ts
+++ src/web/experiments/ab-tests.ts
@@ -3,6 +3,7 @@ import { abTestTest } from '@frontend/web/experiments/tests/ab-test-test';
 import { signInGateMainVariant } from '@root/src/web/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from '@root/src/web/experiments/tests/sign-in-gate-main-control';
 import { curatedContainerTest2 } from '@frontend/web/experiments/tests/curated-container-test';
+import { deeplyReadTest } from '@root/src/web/experiments/tests/deeply-read-test';
 import {
 	newsletterMerchUnitLighthouseControl,
 	newsletterMerchUnitLighthouseVariants,
@@ -16,5 +17,6 @@ export const tests: ABTest[] = [
 	curatedContainerTest2,
 	newsletterMerchUnitLighthouseControl,
 	newsletterMerchUnitLighthouseVariants,
-	globalEoyHeaderTest,
+    globalEoyHeaderTest,
+    deeplyReadTest,
 ];
