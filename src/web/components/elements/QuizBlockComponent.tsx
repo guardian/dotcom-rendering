e2bd2167883e1b139a8d0724166b058918f86398
William Lacroix <william.j.t.lacroix@gmail.com>
2021-01-05 11:41:27 +0000

    remove bottom padding

--- src/web/components/elements/QuizBlockComponent.tsx
+++ src/web/components/elements/QuizBlockComponent.tsx
@@ -1,38 +1,31 @@
 import React from 'react';
-import { css } from 'emotion';
-
-import { space } from '@guardian/src-foundations';
 
 import {
-    PersonalityQuizAtom,
-    KnowledgeQuizAtom,
+	PersonalityQuizAtom,
+	KnowledgeQuizAtom,
 } from '@guardian/atoms-rendering';
 
 type Props = {
-    id: string;
-    questions: QuestionType[];
-    resultBuckets?: ResultBucketsType[];
-    quizType: 'personality' | 'knowledge';
+	id: string;
+	questions: QuestionType[];
+	resultBuckets?: ResultBucketsType[];
+	quizType: 'personality' | 'knowledge';
 };
 export const QuizBlockComponent = ({
-    id,
-    questions,
-    resultBuckets,
-    quizType,
+	id,
+	questions,
+	resultBuckets,
+	quizType,
 }: Props) => (
-    <div
-        className={css`
-            padding-bottom: ${space[2]}px;
-        `}
-    >
-        {quizType === 'personality' && resultBuckets ? (
-            <PersonalityQuizAtom
-                id={id}
-                questions={questions}
-                resultBuckets={resultBuckets}
-            />
-        ) : (
-            <KnowledgeQuizAtom id={id} questions={questions} />
-        )}
-    </div>
+	<>
+		{quizType === 'personality' && resultBuckets ? (
+			<PersonalityQuizAtom
+				id={id}
+				questions={questions}
+				resultBuckets={resultBuckets}
+			/>
+		) : (
+			<KnowledgeQuizAtom id={id} questions={questions} />
+		)}
+	</>
 );
