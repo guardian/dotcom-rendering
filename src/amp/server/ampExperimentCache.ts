09eec1cb3470fed5c85a156db600e3f898e8e2dd
Michael Jacobson <contact@michaeljacobson.co.uk>
2021-01-07 14:31:26 +0000

    replace GU_STAGE with NODE_ENV

--- src/amp/server/ampExperimentCache.ts
+++ src/amp/server/ampExperimentCache.ts
@@ -1,38 +1,38 @@
 import fetch from 'node-fetch';
 
 export type AmpExperiment = {
-	sticky?: boolean;
-	consentNotificationId?: string;
-	variants: {
-		[key: string]: number; // floating point number
-	};
+    sticky?: boolean;
+    consentNotificationId?: string;
+    variants: {
+        [key: string]: number; // floating point number
+    };
 };
 
 export type AmpExperiments = {
-	[key: string]: AmpExperiment;
+    [key: string]: AmpExperiment;
 };
 
 export const ampExperimentsDataUrl =
-	process.env.GU_ENV === 'PROD'
-		? 'https://contributions.guardianapis.com/amp/experiments_data'
-		: 'https://contributions.code.dev-guardianapis.com/amp/experiments_data';
+    process.env.NODE_ENV === 'production'
+        ? 'https://contributions.guardianapis.com/amp/experiments_data'
+        : 'https://contributions.code.dev-guardianapis.com/amp/experiments_data';
 
 let ampExperimentCache: AmpExperiments = {};
 export const getAmpExperimentCache = (): AmpExperiments => ampExperimentCache;
 
 const fetchExperimentsData = (): Promise<void> => {
-	return fetch(ampExperimentsDataUrl)
-		.then((rawResponse) => rawResponse.json())
-		.then((json) => {
-			ampExperimentCache = json;
-		});
+    return fetch(ampExperimentsDataUrl)
+        .then((rawResponse) => rawResponse.json())
+        .then((json) => {
+            ampExperimentCache = json;
+        });
 };
 
 const oneMinute = 60_000;
 const refreshExperimentsData = () => {
-	setTimeout(() => {
-		fetchExperimentsData().then(refreshExperimentsData);
-	}, oneMinute * 2);
+    setTimeout(() => {
+        fetchExperimentsData().then(refreshExperimentsData);
+    }, oneMinute * 2);
 };
 
 fetchExperimentsData().then(refreshExperimentsData);
