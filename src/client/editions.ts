25dcbafae5d9100ec6b43f7f53ccfd986f9b0df2
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2021-01-07 14:19:22 +0000

    clean up

--- src/client/editions.ts
+++ src/client/editions.ts
@@ -1,13 +1,8 @@
 import { createElement as h } from 'react';
 import ReactDOM from 'react-dom';
 import { ShareIcon } from 'components/editions/shareIcon';
-import {
-	initPingEditionsRendering,
-	pingEditionsNative,
-} from './editionsCommunication';
+import { initPingEditionsRendering } from './editionsCommunication';
 
 initPingEditionsRendering();
 
 ReactDOM.render(h(ShareIcon), document.querySelector('.js-share-button'));
-
-pingEditionsNative({ kind: 'platform-query' });
