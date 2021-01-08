4627c849326368c9dfd761611ab9b6dedc671f74
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2021-01-08 15:57:30 +0000

    disable user-scalable

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -15,7 +15,7 @@ const Meta: FC<Props> = ({ title, cspString }) => (
 		<title>{title}</title>
 		<meta id="twitter-theme" name="twitter:widgets:theme" content="light" />
 		<meta name="twitter:dnt" content="on" />
-		<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no"/>
 		<meta name="description" content={title} />
 		<meta httpEquiv="Content-Security-Policy" content={cspString} />
 	</>
