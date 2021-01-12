0d79268fab12e126d5167128e262ee0170a11b1e
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2021-01-12 09:32:29 +0000

    add caption

--- src/components/editions/headerImage.tsx
+++ src/components/editions/headerImage.tsx
@@ -2,6 +2,7 @@
 
 import { css } from '@emotion/core';
 import { remSpace } from '@guardian/src-foundations';
+import HeaderImageCaption, { captionId } from 'components/headerImageCaption';
 import Img from 'components/img';
 import { MainMediaKind } from 'headerMedia';
 import type { Item } from 'item';
@@ -11,7 +12,8 @@ import type { FC } from 'react';
 // ----- Component ----- //
 
 const styles = css`
-	margin: 0;
+	margin: 0 0 ${remSpace[2]} 0;
+	position: relative;
 `;
 
 const imgStyles = css`
@@ -26,13 +28,21 @@ interface Props {
 const HeaderImage: FC<Props> = ({ item }) =>
 	maybeRender(item.mainMedia, (media) => {
 		if (media.kind === MainMediaKind.Image) {
+			const {
+				image,
+				image: { nativeCaption, credit },
+			} = media;
 			return (
-				<figure css={styles}>
+				<figure css={styles} aria-labelledby={captionId}>
 					<Img
-						image={media.image}
+						image={image}
 						sizes={`calc(100vw - (${remSpace[4]} * 2)`}
 						format={item}
 						className={imgStyles}
+					/>
+					<HeaderImageCaption
+						caption={nativeCaption}
+						credit={credit}
 					/>
 				</figure>
 			);
