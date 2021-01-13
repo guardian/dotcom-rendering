ce5362ac3c9f7ebe58eb0f6bf047b7e51bbb66a9
Josh Buckland <josh.russell.buckland@gmail.com>
2021-01-13 11:25:31 +0000

    Prettyify the `fixtures` directory
    Prettify up the `fixtures/` directory. Makes it easier to add and read
    new fixtures.

--- fixtures/card.ts
+++ fixtures/card.ts
@@ -1,88 +1,88 @@
 export const richLinkCard = {
-    tags: [
-        {
-            id: 'music/eminem',
-            type: 'Keyword',
-            title: 'Eminem',
-        },
-        {
-            id: 'music/music',
-            type: 'Keyword',
-            title: 'Music',
-        },
-        {
-            id: 'music/hip-hop',
-            type: 'Keyword',
-            title: 'Hip-hop',
-        },
-        {
-            id: 'music/rap',
-            type: 'Keyword',
-            title: 'Rap',
-        },
-        {
-            id: 'culture/culture',
-            type: 'Keyword',
-            title: 'Culture',
-        },
-        {
-            id: 'us-news/donaldtrump',
-            type: 'Keyword',
-            title: 'Donald Trump',
-        },
-        {
-            id: 'us-news/trump-administration',
-            type: 'Keyword',
-            title: 'Trump administration',
-        },
-        {
-            id: 'us-news/secret-service',
-            type: 'Keyword',
-            title: 'Secret Service',
-        },
-        {
-            id: 'type/article',
-            type: 'Type',
-            title: 'Article',
-        },
-        {
-            id: 'tone/news',
-            type: 'Tone',
-            title: 'News',
-        },
-        {
-            id: 'profile/ben-beaumont-thomas',
-            type: 'Contributor',
-            title: 'Ben Beaumont-Thomas',
-        },
-        {
-            id: 'publication/theguardian',
-            type: 'Publication',
-            title: 'The Guardian',
-        },
-        {
-            id: 'theguardian/mainsection',
-            type: 'NewspaperBook',
-            title: 'Main section',
-        },
-        {
-            id: 'theguardian/mainsection/international',
-            type: 'NewspaperBookSection',
-            title: 'International',
-        },
-        {
-            id: 'tracking/commissioningdesk/uk-culture',
-            type: 'Tracking',
-            title: 'UK Culture',
-        },
-    ],
-    cardStyle: 'news',
-    thumbnailUrl:
-        'https://i.guim.co.uk/img/media/0847eccb8898d4e91499f8a68c4cfdb454f91382/101_177_3650_2191/master/3650.jpg?width=460&quality=85&auto=format&fit=max&s=ca06880ab92aee625f7b6f691bf3e8c5',
-    headline: 'Eminem attacks Donald Trump on surprise album Kamikaze',
-    contentType: 'Article',
-    contributorImage:
-        'https://i.guim.co.uk/img/uploads/2017/10/06/Ben-Beaumont-Thomas,-L.png?width=173&quality=85&auto=format&fit=max&s=e5b75fdef4eecc5eba2db1966e9b5d93',
-    url: '/music/2018/aug/31/eminem-donald-trump-surprise-album-kamikaze',
-    pillar: 'culture',
+	tags: [
+		{
+			id: 'music/eminem',
+			type: 'Keyword',
+			title: 'Eminem',
+		},
+		{
+			id: 'music/music',
+			type: 'Keyword',
+			title: 'Music',
+		},
+		{
+			id: 'music/hip-hop',
+			type: 'Keyword',
+			title: 'Hip-hop',
+		},
+		{
+			id: 'music/rap',
+			type: 'Keyword',
+			title: 'Rap',
+		},
+		{
+			id: 'culture/culture',
+			type: 'Keyword',
+			title: 'Culture',
+		},
+		{
+			id: 'us-news/donaldtrump',
+			type: 'Keyword',
+			title: 'Donald Trump',
+		},
+		{
+			id: 'us-news/trump-administration',
+			type: 'Keyword',
+			title: 'Trump administration',
+		},
+		{
+			id: 'us-news/secret-service',
+			type: 'Keyword',
+			title: 'Secret Service',
+		},
+		{
+			id: 'type/article',
+			type: 'Type',
+			title: 'Article',
+		},
+		{
+			id: 'tone/news',
+			type: 'Tone',
+			title: 'News',
+		},
+		{
+			id: 'profile/ben-beaumont-thomas',
+			type: 'Contributor',
+			title: 'Ben Beaumont-Thomas',
+		},
+		{
+			id: 'publication/theguardian',
+			type: 'Publication',
+			title: 'The Guardian',
+		},
+		{
+			id: 'theguardian/mainsection',
+			type: 'NewspaperBook',
+			title: 'Main section',
+		},
+		{
+			id: 'theguardian/mainsection/international',
+			type: 'NewspaperBookSection',
+			title: 'International',
+		},
+		{
+			id: 'tracking/commissioningdesk/uk-culture',
+			type: 'Tracking',
+			title: 'UK Culture',
+		},
+	],
+	cardStyle: 'news',
+	thumbnailUrl:
+		'https://i.guim.co.uk/img/media/0847eccb8898d4e91499f8a68c4cfdb454f91382/101_177_3650_2191/master/3650.jpg?width=460&quality=85&auto=format&fit=max&s=ca06880ab92aee625f7b6f691bf3e8c5',
+	headline: 'Eminem attacks Donald Trump on surprise album Kamikaze',
+	contentType: 'Article',
+	contributorImage:
+		'https://i.guim.co.uk/img/uploads/2017/10/06/Ben-Beaumont-Thomas,-L.png?width=173&quality=85&auto=format&fit=max&s=e5b75fdef4eecc5eba2db1966e9b5d93',
+	url: '/music/2018/aug/31/eminem-donald-trump-surprise-album-kamikaze',
+	pillar: 'culture',
 };
