215a6a15dff1ba7cc98e33579ce9803b769fe057
Joe Cowton <joe.cowton@guardian.co.uk>
2021-01-14 14:42:30 +0000

    feat(editions-article): add article showcase header component

--- src/components/editions/standfirst.tsx
+++ src/components/editions/standfirst.tsx
@@ -1,18 +1,38 @@
 // ----- Imports ----- //
 
 import { css } from '@emotion/core';
-import { remSpace } from '@guardian/src-foundations';
+import { remSpace, text } from '@guardian/src-foundations';
+import { from } from '@guardian/src-foundations/mq';
 import { body } from '@guardian/src-foundations/typography';
 import type { Item } from 'item';
 import { maybeRender } from 'lib';
 import type { FC } from 'react';
 import { renderStandfirstText } from 'renderer';
+import { wideContentWidth } from 'styles';
 
 // ----- Component ----- //
 
 const styles = css`
 	${body.medium({ lineHeight: 'tight' })}
-	margin: ${remSpace[4]} 0 0;
+	margin-bottom: ${remSpace[6]};
+	color: ${text.primary};
+
+	${from.wide} {
+		margin: 0 auto;
+	}
+
+	${from.phablet} {
+		width: ${wideContentWidth}px;
+	}
+
+	p,
+	ul {
+		margin: ${remSpace[2]} 0;
+	}
+
+	address {
+		font-style: normal;
+	}
 `;
 
 interface Props {
