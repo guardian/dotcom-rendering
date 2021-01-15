899c4396e518b86c7f95b89f3038684bb97a16ec
Joe Cowton <joe.cowton@guardian.co.uk>
2021-01-15 12:20:17 +0000

    feat(editions-article): add border to header components

--- src/components/editions/lines.tsx
+++ src/components/editions/lines.tsx
@@ -2,15 +2,21 @@
 
 import { css } from '@emotion/core';
 import { Lines } from '@guardian/src-ed-lines';
+import { border } from '@guardian/src-foundations';
 import { from } from '@guardian/src-foundations/mq';
 import type { FC } from 'react';
-import { wideContentWidth } from 'styles';
+import { editionsArticleWidth } from './styles';
 
 // ----- Component ----- //
 
 const styles = css`
 	${from.phablet} {
-		width: ${wideContentWidth}px;
+		width: ${editionsArticleWidth}rem;
+		border-right: 1px solid ${border.secondary};
+	}
+
+	${from.wide} {
+		margin: 0 auto;
 	}
 `;
 
--- src/components/editions/standfirst.tsx
+++ src/components/editions/standfirst.tsx
@@ -1,20 +1,20 @@
 // ----- Imports ----- //
 
 import { css } from '@emotion/core';
-import { remSpace, text } from '@guardian/src-foundations';
+import { border, remSpace, text } from '@guardian/src-foundations';
 import { from } from '@guardian/src-foundations/mq';
 import { body } from '@guardian/src-foundations/typography';
 import type { Item } from 'item';
 import { maybeRender } from 'lib';
 import type { FC } from 'react';
 import { renderStandfirstText } from 'renderer';
-import { wideContentWidth } from 'styles';
+import { editionsArticleWidth } from './styles';
 
 // ----- Component ----- //
 
 const styles = css`
 	${body.medium({ lineHeight: 'tight' })}
-	margin-bottom: ${remSpace[6]};
+	padding-bottom: ${remSpace[6]};
 	color: ${text.primary};
 
 	${from.wide} {
@@ -22,12 +22,14 @@ const styles = css`
 	}
 
 	${from.phablet} {
-		width: ${wideContentWidth}px;
+		border-right: 1px solid ${border.secondary};
+		width: ${editionsArticleWidth}rem;
 	}
 
 	p,
 	ul {
-		margin: ${remSpace[2]} 0;
+		padding: ${remSpace[2]} ${remSpace[1]} 0 0;
+		margin: 0;
 	}
 
 	address {
