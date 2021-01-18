35b8d4b413cf7b9e56c3a181f5fac6e7efaf855f
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-01-18 14:52:57 +0000

    Added getheadlineColour function to palette

--- src/web/lib/decidePalette.ts
+++ src/web/lib/decidePalette.ts
@@ -1,8 +1,34 @@
-export const decidePalette = (format : Format) => {
+import { Display } from '@guardian/types/Format';
+import { pillarPalette } from '@root/src/lib/pillars';
 
-    return {
-        headline: {
-            colour: "blue"
-        }
-    }
+type Format = { display: Display; designType: DesignType; pillar: CAPIPillar };
+
+const getHeadlineColour = (format: Format): string => {
+	switch (format.display) {
+		case Display.Immersive:
+			return 'white';
+		case Display.Showcase:
+		case Display.Standard: {
+			switch (format.designType) {
+				case 'Review':
+				case 'Recipe':
+				case 'Feature':
+					return pillarPalette[format.pillar].dark;
+				case 'Interview':
+					return 'white';
+				default:
+					return 'black';
+			}
+		}
+		default:
+			return 'black';
+	}
+};
+
+export const decidePalette = (format: Format) => {
+	return {
+		headline: {
+			colour: getHeadlineColour(format),
+		},
+	};
 };
