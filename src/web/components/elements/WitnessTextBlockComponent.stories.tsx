901af602effad6c266cf1bd514252670231a6dbd
William Lacroix <william.j.t.lacroix@gmail.com>
2021-01-22 15:26:02 +0000

    add text block component

---
+++ src/web/components/elements/WitnessTextBlockComponent.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { css } from 'emotion';
+
+import { WitnessTextBlockComponent } from './WitnessTextBlockComponent';
+
+export default {
+	component: WitnessTextBlockComponent,
+	title: 'Components/WitnessTextBlockComponent',
+};
+
+const witnessTypeData: WitnessTypeDataText = {
+	authorGuardianProfileUrl:
+		'https://profile.theguardian.com/user/id/12260883',
+	_type: 'model.dotcomrendering.pageElements.WitnessTypeDataText',
+	description:
+		'You could dial first and then push in ur 2p. I never had any change and needed a lift. My parents knew the routine, I&#39;d dial and you could get a second or two to shout down the line &quot;Pick me up&quot; Ah how I miss the days of a simple life where no one knew where I was and I could get away with anything as long as I was in the door by 10pm! ',
+	authorUsername: 'guardianUser12260883',
+	originalUrl:
+		'https://witness.theguardian.com/assignment/56a8ad12e4b0b3d3432b53c4/1903093',
+	source: 'Guardian Witness',
+	type: 'text',
+	title: 'Back in the 1970&#39;s',
+	url:
+		'https://witness.theguardian.com/assignment/56a8ad12e4b0b3d3432b53c4/1903093',
+	dateCreated: '2016-01-29T22:19:51Z',
+	apiUrl: 'https://n0ticeapis.com/2/report/1903093',
+	authorName: 'Louise Griffiths',
+	witnessEmbedType: 'text',
+	authorWitnessProfileUrl:
+		'https://witness.theguardian.com/user/guardianUser12260883',
+};
+
+export const WitnessTextBlockComponentDefault = () => (
+	<div
+		className={css`
+			margin: 15px;
+			width: 620px;
+		`}
+	>
+		<WitnessTextBlockComponent
+			title={witnessTypeData.title}
+			description={witnessTypeData.description}
+			authorUsername={witnessTypeData.authorUsername}
+			dateCreated={witnessTypeData.dateCreated}
+			pillar="sport"
+		/>
+	</div>
+);
---
+++ src/web/components/elements/WitnessTextBlockComponent.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { css } from 'emotion';
+
+import { space } from '@guardian/src-foundations';
+import { headline, body } from '@guardian/src-foundations/typography';
+
+import { pillarPalette } from '@root/src/lib/pillars';
+import { WitnessWrapper } from '@frontend/web/components/WitnessWrapper';
+
+type Props = {
+	title: string;
+	authorUsername: string;
+	dateCreated: string;
+	description: string;
+	pillar: CAPIPillar;
+};
+
+const titleStyles = (pillar: CAPIPillar) => css`
+	margin-bottom: ${space[2]}px;
+	color: ${pillarPalette[pillar].main};
+	${headline.xxxsmall()}
+`;
+
+export const WitnessTextBlockComponent = ({
+	title,
+	authorUsername,
+	dateCreated,
+	description,
+	pillar,
+}: Props) => (
+	<WitnessWrapper
+		authorName={authorUsername}
+		dateCreated={dateCreated}
+		pillar={pillar}
+	>
+		<>
+			<h3 className={titleStyles(pillar)} itemProp="name">
+				{title}
+			</h3>
+			<div itemProp="text">
+				<p
+					className={css`
+						${body.medium()}
+					`}
+				>
+					{description}
+				</p>
+			</div>
+		</>
+	</WitnessWrapper>
+);
