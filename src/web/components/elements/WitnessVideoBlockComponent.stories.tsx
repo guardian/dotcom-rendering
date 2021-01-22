005f319f42760be211bb7535dc2b5701af330a0a
William Lacroix <william.j.t.lacroix@gmail.com>
2021-01-22 15:25:43 +0000

    add video block component

---
+++ src/web/components/elements/WitnessVideoBlockComponent.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { css } from 'emotion';
+
+import { WitnessVideoBlockComponent } from './WitnessVideoBlockComponent';
+
+export default {
+	component: WitnessVideoBlockComponent,
+	title: 'Components/WitnessVideoBlockComponent',
+};
+
+const witnessTypeData: WitnessTypeDataVideo = {
+	authorGuardianProfileUrl:
+		'https://profile.theguardian.com/user/id/15282285',
+	_type: 'model.dotcomrendering.pageElements.WitnessTypeDataVideo',
+	description:
+		'Tell me if you&#39;ve heard this one before: A Dwarf, a thief, a fighter and a priest walk into a dungeon ... Warhammer Quest is the quintessential &#39;Old School&#39; dungeon-crawler.',
+	authorUsername: 'Gregg Lewis-Qualls',
+	originalUrl:
+		'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1683313',
+	source: 'Guardian Witness',
+	youtubeHtml: '',
+	type: 'video',
+	title: 'Warhammer Quest Timelapse',
+	url:
+		'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1683313',
+	dateCreated: '2015-08-27T13:32:32Z',
+	apiUrl: 'https://n0ticeapis.com/2/report/1683313',
+	youtubeDescription:
+		"Tell me if you've heard this one before: A Dwarf, a thief, a fighter and a priest walk into a dungeon ... Warhammer Quest is the quintessential 'Old School' dungeon-crawler. By Gregg Lewis-Qualls Board games! Are you a recent convert, or have you been playing them for many a moon?",
+	authorName: 'Gregg Lewis-Qualls',
+	youtubeUrl: 'http://www.youtube.com/watch?v=XRq83txBgxw',
+	width: 440,
+	youtubeSource: 'YouTube',
+	youtubeAuthorName: 'guardianwitness',
+	witnessEmbedType: 'video',
+	html: '',
+	authorWitnessProfileUrl:
+		'https://witness.theguardian.com/user/Gregg Lewis-Qualls',
+	height: 330,
+	youtubeTitle: 'Warhammer Quest Timelapse',
+};
+
+export const WitnessVideoBlockComponentDefault = () => (
+	<div
+		className={css`
+			margin: 15px;
+			width: 620px;
+		`}
+	>
+		<WitnessVideoBlockComponent
+			title={witnessTypeData.title}
+			description={witnessTypeData.description}
+			authorUsername={witnessTypeData.authorUsername}
+			width={witnessTypeData.width}
+			height={witnessTypeData.height}
+			youtubeUrl={witnessTypeData.youtubeUrl}
+			dateCreated={witnessTypeData.dateCreated}
+			pillar="sport"
+		/>
+	</div>
+);
---
+++ src/web/components/elements/WitnessVideoBlockComponent.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { css } from 'emotion';
+
+import { space } from '@guardian/src-foundations';
+import { headline, body } from '@guardian/src-foundations/typography';
+
+import { pillarPalette } from '@root/src/lib/pillars';
+import { WitnessWrapper } from '@frontend/web/components/WitnessWrapper';
+
+type Props = {
+	title: string;
+	description: string;
+	authorUsername: string;
+	width: number;
+	height: number;
+	youtubeUrl: string;
+	dateCreated: string;
+	pillar: CAPIPillar;
+};
+
+const captionStyles = css`
+	margin-top: ${space[3]}px;
+`;
+
+const titleStyles = (pillar: CAPIPillar) => css`
+	margin-bottom: ${space[2]}px;
+	color: ${pillarPalette[pillar].main};
+	${headline.xxxsmall()}
+`;
+
+export const WitnessVideoBlockComponent = ({
+	title,
+	description,
+	authorUsername,
+	width,
+	height,
+	youtubeUrl,
+	dateCreated,
+	pillar,
+}: Props) => {
+	const youtubeVideoId = youtubeUrl.split('?v=')[1];
+	const parsedURL = `https://www.youtube-nocookie.com/embed/${youtubeVideoId}?wmode=opaque&feature=oembed`;
+	return (
+		<WitnessWrapper
+			authorName={authorUsername}
+			dateCreated={dateCreated}
+			pillar={pillar}
+		>
+			<>
+				<iframe
+					className={css`
+						width: 100%;
+					`}
+					title={title}
+					width={width}
+					height={height}
+					src={parsedURL}
+				/>
+				<figcaption className={captionStyles}>
+					<h3 className={titleStyles(pillar)} itemProp="name">
+						{title}
+					</h3>
+					<div itemProp="description">
+						<p
+							className={css`
+								${body.medium()}
+							`}
+						>
+							{description}
+						</p>
+					</div>
+				</figcaption>
+			</>
+		</WitnessWrapper>
+	);
+};
