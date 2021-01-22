b74a5604d55a198766a266885acc1aca63c0fc45
William Lacroix <william.j.t.lacroix@gmail.com>
2021-01-22 15:26:45 +0000

    add image block component

---
+++ src/web/components/elements/WitnessImageBlockComponent.stories.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { css } from 'emotion';
+
+import { WitnessImageBlockComponent } from './WitnessImageBlockComponent';
+
+export default {
+	component: WitnessImageBlockComponent,
+	title: 'Components/WitnessImageBlockComponent',
+};
+
+const witnessTypeData: WitnessTypeDataImage = {
+	authorGuardianProfileUrl:
+		'https://profile.theguardian.com/user/id/14989770',
+	alt: 'Risk - Home Made Space expansion',
+	_type: 'model.dotcomrendering.pageElements.WitnessTypeDataImage',
+	authorUsername: 'Nick Ellis',
+	caption:
+		'Risk was always my favourite board game. I decided to send it into orbit with a home-made space expansion. ',
+	originalUrl:
+		'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969',
+	source: 'Guardian Witness',
+	type: 'image',
+	mediaId:
+		'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969',
+	title: 'Risk - Home Made Space expansion',
+	url:
+		'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969',
+	dateCreated: '2015-08-25T12:20:58Z',
+	apiUrl: 'https://n0ticeapis.com/2/report/1679969',
+	authorName: 'Nick Ellis',
+	witnessEmbedType: 'image',
+	html: '',
+	photographer: 'Nick Ellis',
+	authorWitnessProfileUrl: 'https://witness.theguardian.com/user/Nick Ellis',
+};
+
+export const WitnessImageBlockComponentDefault = () => (
+	<div
+		className={css`
+			margin: 15px;
+			width: 620px;
+		`}
+	>
+		<WitnessImageBlockComponent
+			caption={witnessTypeData.caption}
+			title={witnessTypeData.title}
+			authorName={witnessTypeData.authorName}
+			dateCreated={witnessTypeData.dateCreated}
+			alt={witnessTypeData.alt}
+			pillar="sport"
+		/>
+	</div>
+);
---
+++ src/web/components/elements/WitnessImageBlockComponent.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { css } from 'emotion';
+
+import { space } from '@guardian/src-foundations';
+import { headline, body } from '@guardian/src-foundations/typography';
+
+import { pillarPalette } from '@root/src/lib/pillars';
+import { WitnessWrapper } from '@frontend/web/components/WitnessWrapper';
+
+type Props = {
+	caption: string;
+	title: string;
+	authorName: string;
+	dateCreated: string;
+	alt: string;
+	pillar: CAPIPillar;
+};
+
+const captionStyles = css`
+	margin-top: ${space[3]}px;
+`;
+
+const titleStyles = (pillar: CAPIPillar) => css`
+	margin-bottom: ${space[2]}px;
+	color: ${pillarPalette[pillar].main};
+	${headline.xxxsmall()}
+`;
+
+export const WitnessImageBlockComponent = ({
+	caption,
+	title,
+	authorName,
+	dateCreated,
+	alt,
+	pillar,
+}: Props) => (
+	<WitnessWrapper
+		authorName={authorName}
+		dateCreated={dateCreated}
+		pillar={pillar}
+	>
+		<>
+			<img
+				className={css`
+					width: 100%;
+				`}
+				src="https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumoriginalaspectdouble.jpg"
+				alt={alt}
+				itemProp="contentURL"
+			/>
+			<figcaption className={captionStyles}>
+				<h3 className={titleStyles(pillar)} itemProp="name">
+					{title}
+				</h3>
+				<div itemProp="description">
+					<p
+						className={css`
+							${body.medium()}
+						`}
+					>
+						{caption}
+					</p>
+				</div>
+			</figcaption>
+		</>
+	</WitnessWrapper>
+);
