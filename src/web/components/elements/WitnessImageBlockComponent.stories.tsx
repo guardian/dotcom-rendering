df61a15026ea0b4fb2819936b352414875750d1d
William Lacroix <william.j.t.lacroix@gmail.com>
2021-01-25 13:07:53 +0000

    set props inline for storybook

--- src/web/components/elements/WitnessImageBlockComponent.stories.tsx
+++ src/web/components/elements/WitnessImageBlockComponent.stories.tsx
@@ -10,129 +10,96 @@ export default {
 	title: 'Components/WitnessImageBlockComponent',
 };
 
-const witnessBlockElement: WitnessTypeBlockElement = {
-	assets: [
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumlandscapecropdouble.jpg',
-			typeData: {
-				name: 'mediumlandscapecropdouble',
-			},
+const assets: WitnessAssetType[] = [
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumlandscapecropdouble.jpg',
+		typeData: {
+			name: 'mediumlandscapecropdouble',
 		},
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumlandscapecrop.jpg',
-			typeData: {
-				name: 'mediumlandscapecrop',
-			},
-		},
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumoriginalaspectdouble.jpg',
-			typeData: {
-				name: 'mediumoriginalaspectdouble',
-			},
-		},
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-small.jpg',
-			typeData: {
-				name: 'small',
-			},
-		},
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-medium.jpg',
-			typeData: {
-				name: 'medium',
-			},
-		},
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-large.jpg',
-			typeData: {
-				name: 'large',
-			},
-		},
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-extralarge.jpg',
-			typeData: {
-				name: 'extralarge',
-			},
-		},
-		{
-			type: 'Image',
-			mimeType: 'image/jpeg',
-			file:
-				'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumdouble.jpg',
-			typeData: {
-				name: 'mediumdouble',
-			},
-		},
-	],
-	isThirdPartyTracking: false,
-	_type: 'model.dotcomrendering.pageElements.WitnessBlockElement',
-	witnessTypeData: {
-		authorGuardianProfileUrl:
-			'https://profile.theguardian.com/user/id/14989770',
-		alt: 'Risk - Home Made Space expansion',
-		_type: 'model.dotcomrendering.pageElements.WitnessTypeDataImage',
-		authorUsername: 'Nick Ellis',
-		caption:
-			'Risk was always my favourite board game. I decided to send it into orbit with a home-made space expansion. ',
-		originalUrl:
-			'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969',
-		source: 'Guardian Witness',
-		type: 'image',
-		mediaId:
-			'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969',
-		title: 'Risk - Home Made Space expansion',
-		url:
-			'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969',
-		dateCreated: '2015-08-25T12:20:58Z',
-		apiUrl: 'https://n0ticeapis.com/2/report/1679969',
-		authorName: 'Nick Ellis',
-		witnessEmbedType: 'image',
-		// html: "<div class="element-witness--main"> <a href="https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969" itemprop="url"><img src="https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumoriginalaspectdouble.jpg" alt="Risk - Home Made Space expansion" itemprop="contentURL"/></a> <figcaption> <h3 class="element-witness--title" itemprop="name"><a href="https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1679969" itemprop="url">Risk - Home Made Space expansion</a></h3> <div itemprop="description"> <p>Risk was always my favourite board game. I decided to send it into orbit with a home-made space expansion.</p> </div> </figcaption> </div> <footer class="element-witness--footer"> <p class="element-witness--source">Sent via <a href="https://witness.theguardian.com/" class="element-witness--brand" itemprop="provider">GuardianWitness</a></p> <div class="element-witness--metadata"> <p class="element-witness--author" itemprop="author" itemscope itemtype="http://schema.org/Person"> By <a href="https://witness.theguardian.com/user/Nick Ellis" class="element-witness--author-name" itemprop="name">Nick Ellis</a> </p> <p class="element-witness--date"> <time itemprop="dateCreated" datetime="2015-08-25T12:20:58Z">25 August 2015, 13:20</time> </p> </div> </footer> ",
-		photographer: 'Nick Ellis',
-		authorWitnessProfileUrl:
-			'https://witness.theguardian.com/user/Nick Ellis',
 	},
-};
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumlandscapecrop.jpg',
+		typeData: {
+			name: 'mediumlandscapecrop',
+		},
+	},
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumoriginalaspectdouble.jpg',
+		typeData: {
+			name: 'mediumoriginalaspectdouble',
+		},
+	},
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-small.jpg',
+		typeData: {
+			name: 'small',
+		},
+	},
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-medium.jpg',
+		typeData: {
+			name: 'medium',
+		},
+	},
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-large.jpg',
+		typeData: {
+			name: 'large',
+		},
+	},
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-extralarge.jpg',
+		typeData: {
+			name: 'extralarge',
+		},
+	},
+	{
+		type: 'Image',
+		mimeType: 'image/jpeg',
+		file:
+			'https://gu-witness.s3.amazonaws.com/image/1440505256168e17c87ce879c40d3f575b6625340fb7c-mediumdouble.jpg',
+		typeData: {
+			name: 'mediumdouble',
+		},
+	},
+];
 
-export const WitnessImageBlockComponentDefault = () => {
-	const witnessTypeData = witnessBlockElement.witnessTypeData as WitnessTypeDataImage;
-	return (
-		<div
-			className={css`
-				margin: 15px;
-				width: 620px;
-			`}
-		>
-			<WitnessImageBlockComponent
-				assets={witnessBlockElement.assets}
-				caption={witnessTypeData.caption}
-				title={witnessTypeData.title}
-				authorName={witnessTypeData.authorName}
-				dateCreated={witnessTypeData.dateCreated}
-				alt={witnessTypeData.alt}
-				pillar={Pillar.Sport}
-			/>
-		</div>
-	);
-};
+export const WitnessImageBlockComponentDefault = () => (
+	<div
+		className={css`
+			margin: 15px;
+			width: 620px;
+		`}
+	>
+		<WitnessImageBlockComponent
+			assets={assets}
+			caption="Risk was always my favourite board game. I decided to send it into orbit with a home-made space expansion. "
+			title="Risk - Home Made Space expansion"
+			authorName="Nick Ellis"
+			dateCreated="2015-08-25T12:20:58Z"
+			alt="Risk - Home Made Space expansion"
+			pillar={Pillar.Sport}
+		/>
+	</div>
+);
--- src/web/components/elements/WitnessVideoBlockComponent.stories.tsx
+++ src/web/components/elements/WitnessVideoBlockComponent.stories.tsx
@@ -10,39 +10,6 @@ export default {
 	title: 'Components/WitnessVideoBlockComponent',
 };
 
-const witnessTypeData: WitnessTypeDataVideo = {
-	authorGuardianProfileUrl:
-		'https://profile.theguardian.com/user/id/15282285',
-	_type: 'model.dotcomrendering.pageElements.WitnessTypeDataVideo',
-	description:
-		'Tell me if you&#39;ve heard this one before: A Dwarf, a thief, a fighter and a priest walk into a dungeon ... Warhammer Quest is the quintessential &#39;Old School&#39; dungeon-crawler.',
-	authorUsername: 'Gregg Lewis-Qualls',
-	originalUrl:
-		'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1683313',
-	source: 'Guardian Witness',
-	youtubeHtml:
-		'<iframe width="440" height="330" src="https://www.youtube.com/embed/XRq83txBgxw?wmode=opaque&feature=oembed" frameborder="0" allowfullscreen></iframe>',
-	type: 'video',
-	title: 'Warhammer Quest Timelapse',
-	url:
-		'https://witness.theguardian.com/assignment/55db3248e4b0959e5f8de003/1683313',
-	dateCreated: '2015-08-27T13:32:32Z',
-	apiUrl: 'https://n0ticeapis.com/2/report/1683313',
-	youtubeDescription:
-		"Tell me if you've heard this one before: A Dwarf, a thief, a fighter and a priest walk into a dungeon ... Warhammer Quest is the quintessential 'Old School' dungeon-crawler. By Gregg Lewis-Qualls Board games! Are you a recent convert, or have you been playing them for many a moon?",
-	authorName: 'Gregg Lewis-Qualls',
-	youtubeUrl: 'http://www.youtube.com/watch?v=XRq83txBgxw',
-	width: 440,
-	youtubeSource: 'YouTube',
-	youtubeAuthorName: 'guardianwitness',
-	witnessEmbedType: 'video',
-	html: '',
-	authorWitnessProfileUrl:
-		'https://witness.theguardian.com/user/Gregg Lewis-Qualls',
-	height: 330,
-	youtubeTitle: 'Warhammer Quest Timelapse',
-};
-
 export const WitnessVideoBlockComponentDefault = () => (
 	<div
 		className={css`
@@ -51,11 +18,11 @@ export const WitnessVideoBlockComponentDefault = () => (
 		`}
 	>
 		<WitnessVideoBlockComponent
-			title={witnessTypeData.title}
-			description={witnessTypeData.description}
-			authorName={witnessTypeData.authorName}
-			youtubeHtml={witnessTypeData.youtubeHtml}
-			dateCreated={witnessTypeData.dateCreated}
+			title="Warhammer Quest Timelapse"
+			description="Tell me if you&#39;ve heard this one before: A Dwarf, a thief, a fighter and a priest walk into a dungeon ... Warhammer Quest is the quintessential &#39;Old School&#39; dungeon-crawler."
+			authorName="Gregg Lewis-Qualls"
+			youtubeHtml='<iframe width="440" height="330" src="https://www.youtube.com/embed/XRq83txBgxw?wmode=opaque&feature=oembed" frameborder="0" allowfullscreen></iframe>'
+			dateCreated="2015-08-27T13:32:32Z"
 			pillar={Pillar.Sport}
 		/>
 	</div>
