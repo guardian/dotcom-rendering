6555d0bd9b753fb35d7b46132b9580b735dfa850
Josh Buckland <josh.russell.buckland@gmail.com>
2021-01-27 17:14:22 +0000

    Add unix times for timeline events

--- src/lib/content.d.ts
+++ src/lib/content.d.ts
@@ -480,8 +480,10 @@ interface VideoAssets {
 interface TimelineEvent {
 	title: string;
 	date: string;
+	unixDate: number;
 	body?: string;
 	toDate?: string;
+	toUnixDate?: number;
 }
 
 interface Switches {
