24d50389e47451ce0238219393175e6a880be94c
Oliver Lloyd <email@oliverlloyd.com>
2021-01-29 13:59:03 +0000

    Refactor to inline logic

--- src/web/components/ShareIcons.tsx
+++ src/web/components/ShareIcons.tsx
@@ -3,9 +3,9 @@ import { css, cx } from 'emotion';
 
 import { border } from '@guardian/src-foundations/palette';
 import { from } from '@guardian/src-foundations/mq';
+import type { Theme } from '@guardian/types';
 
-import { pillarMap, pillarPalette } from '@root/src/lib/pillars';
-import { getSharingUrls } from '@frontend/lib/sharing-urls';
+import { pillarPalette } from '@root/src/lib/pillars';
 
 import TwitterIconPadded from '@frontend/static/icons/twitter-padded.svg';
 import FacebookIcon from '@frontend/static/icons/facebook.svg';
@@ -15,12 +15,7 @@ import PinterestIcon from '@frontend/static/icons/pinterest.svg';
 import WhatsAppIcon from '@frontend/static/icons/whatsapp.svg';
 import MessengerIcon from '@frontend/static/icons/messenger.svg';
 
-const pillarFill = pillarMap(
-	(pillar) =>
-		css`
-			fill: ${pillarPalette[pillar].main};
-		`,
-);
+import { Hide } from './Hide';
 
 const shareIconList = css`
 	float: left;
@@ -29,14 +24,14 @@ const shareIconList = css`
 	}
 `;
 
-const shareIconsListItem = css`
+const liStyles = css`
 	padding: 0 3px 6px 0;
 	float: left;
 	min-width: 32px;
 	cursor: pointer;
 `;
 
-const shareIcon = (colour: string) => css`
+const iconStyles = (pillar: Theme) => css`
 	border: 1px solid ${border.secondary};
 	white-space: nowrap;
 	overflow: hidden;
@@ -50,6 +45,7 @@ const shareIcon = (colour: string) => css`
 	vertical-align: middle;
 	position: relative;
 	box-sizing: content-box;
+	fill: ${pillarPalette[pillar].main};
 
 	svg {
 		height: 88%;
@@ -63,25 +59,11 @@ const shareIcon = (colour: string) => css`
 	}
 
 	:hover {
-		background-color: ${colour};
-		border-color: ${colour};
+		background-color: ${pillarPalette[pillar].main};
+		border-color: ${pillarPalette[pillar].main};
 		fill: white;
 	}
 `;
-
-const mobileOnlyShareIconsListItem = css`
-	${from.phablet} {
-		display: none;
-	}
-`;
-
-interface ShareListItemType {
-	id: SharePlatform;
-	Icon: React.ComponentType;
-	url: string;
-	userMessage: string;
-	mobileOnly: boolean;
-}
 
 export const ShareIcons: React.FC<{
 	pageId: string;
@@ -90,72 +72,119 @@ export const ShareIcons: React.FC<{
 	pillar: Theme;
 	className?: string;
 }> = ({ pageId, webTitle, displayIcons, pillar, className }) => {
-	const sharingUrls = getSharingUrls(pageId, webTitle);
-	const icons: { [K in SharePlatform]?: React.ComponentType } = {
-		facebook: FacebookIcon,
-		twitter: TwitterIconPadded,
-		email: EmailIcon,
-		linkedIn: LinkedInIcon,
-		pinterest: PinterestIcon,
-		whatsApp: WhatsAppIcon,
-		messenger: MessengerIcon,
-	};
-
-	const mobileOnlyIcons: SharePlatform[] = ['whatsApp', 'messenger'];
-
-	const shareList = displayIcons.reduce((list: ShareListItemType[], id) => {
-		const icon = icons[id];
-		const sharingUrl = sharingUrls[id];
-
-		if (icon && sharingUrl) {
-			const listItem: ShareListItemType = {
-				id,
-				Icon: icon,
-				mobileOnly: mobileOnlyIcons.includes(id),
-				...sharingUrl,
-			};
-			list.push(listItem);
-		}
-
-		return list;
-	}, []);
-
 	return (
 		<ul className={cx(shareIconList, [className])}>
-			{shareList.map((shareListItem) => {
-				const {
-					Icon,
-					id,
-					url,
-					userMessage,
-					mobileOnly,
-				} = shareListItem;
-
-				return (
-					<li
-						className={cx(shareIconsListItem, {
-							[mobileOnlyShareIconsListItem]: mobileOnly,
-						})}
-						key={`${id}Share`}
+			{displayIcons.includes('facebook') && (
+				<li className={liStyles} key="facebook">
+					<a
+						href={`https://www.facebook.com/dialog/share?app_id=202314643182694&href=https://www.theguardian.com/${pageId}&CMP=share_btn_fb`}
+						role="button"
+						aria-label="Share on Facebook"
+						target="_blank"
 					>
+						<span className={iconStyles(pillar)}>
+							<FacebookIcon />
+						</span>
+					</a>
+				</li>
+			)}
+
+			{displayIcons.includes('pinterest') && (
+				<li className={liStyles} key="pinterest">
+					<a
+						href={`http://www.pinterest.com/pin/find/?url=https://www.theguardian.com/${pageId}`}
+						role="button"
+						aria-label="Share on Pinterest"
+						target="_blank"
+					>
+						<span className={iconStyles(pillar)}>
+							<PinterestIcon />
+						</span>
+					</a>
+				</li>
+			)}
+
+			{displayIcons.includes('twitter') && (
+				<li className={liStyles} key="twitter">
+					<a
+						href={`https://twitter.com/intent/tweet?text=${webTitle.replace(
+							/Leave.EU/gi,
+							'Leave. EU',
+						)}&url=https://www.theguardian.com/&CMP=share_btn_tw`}
+						role="button"
+						aria-label="Share on Twitter"
+						target="_blank"
+					>
+						<span className={iconStyles(pillar)}>
+							<TwitterIconPadded />
+						</span>
+					</a>
+				</li>
+			)}
+
+			{displayIcons.includes('whatsApp') && (
+				<Hide when="above" breakpoint="phablet">
+					<li className={liStyles} key="whatsApp">
 						<a
-							href={url}
+							href={`whatsapp://send?text="${webTitle}" https://www.theguardian.com/${pageId}&CMP=share_btn_wa`}
 							role="button"
-							aria-label={userMessage}
+							aria-label="Share on WhatsApp"
 							target="_blank"
 						>
-							<span
-								className={cx(
-									shareIcon(pillarPalette[pillar].main),
-									pillarFill[pillar],
-								)}
-							>
-								<Icon />
+							<span className={iconStyles(pillar)}>
+								<WhatsAppIcon />
 							</span>
 						</a>
 					</li>
-				);
-			})}
+				</Hide>
+			)}
+
+			{displayIcons.includes('email') && (
+				<li className={liStyles} key="email">
+					<a
+						href={`mailto:?subject=${webTitle}&body=https://www.theguardian.com/${pageId}&CMP=share_btn_link`}
+						role="button"
+						aria-label="Share via Email"
+						target="_blank"
+					>
+						<span className={iconStyles(pillar)}>
+							<EmailIcon />
+						</span>
+					</a>
+				</li>
+			)}
+
+			{displayIcons.includes('linkedIn') && (
+				<li className={liStyles} key="linkedIn">
+					<a
+						href={`http://www.linkedin.com/shareArticle?title=${webTitle}&mini=true&url=https://www.theguardian.com/${pageId}`}
+						role="button"
+						aria-label="Share on LinkedIn"
+						target="_blank"
+					>
+						<span className={iconStyles(pillar)}>
+							<LinkedInIcon />
+						</span>
+					</a>
+				</li>
+			)}
+
+			{displayIcons.includes('messenger') && (
+				<Hide when="above" breakpoint="phablet">
+					<li className={liStyles} key="messenger">
+						<a
+							href={`fb-messenger://share?link=https://www.theguardian.com/${pageId}&app_id=180444840287&CMP=share_btn_me`}
+							role="button"
+							aria-label="Share on Messanger>"
+							target="_blank"
+						>
+							<span className={iconStyles(pillar)}>
+								<MessengerIcon />
+							</span>
+						</a>
+					</li>
+				</Hide>
+			)}
 		</ul>
 	);
 };
