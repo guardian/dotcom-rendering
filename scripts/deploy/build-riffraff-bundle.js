c6223352198ab32ddec1d936bfde4ee45c23a19f
Gareth <gtrufitt@gmail.com>
2021-02-03 10:50:33 +0000

    Add prettier fixes for scripts/

--- scripts/deploy/build-riffraff-bundle.js
+++ scripts/deploy/build-riffraff-bundle.js
@@ -29,76 +29,76 @@ const { siteName, root, dist, target } = require('../frontend/config');
 //         └── rendering.zip
 
 const copyCfn = () => {
-    log(' - copying cloudformation config');
-    return cpy([`cloudformation.yml`], path.resolve(target, `${siteName}-cfn`));
+	log(' - copying cloudformation config');
+	return cpy([`cloudformation.yml`], path.resolve(target, `${siteName}-cfn`));
 };
 
 const copyStatic = () => {
-    log(' - copying static');
-    return cpy(
-        ['**/*'],
-        path.resolve(target, `${siteName}-static`, 'static', siteName),
-        {
-            cwd: path.resolve(root, 'src/static'),
-            parents: true,
-            nodir: true,
-        },
-    );
+	log(' - copying static');
+	return cpy(
+		['**/*'],
+		path.resolve(target, `${siteName}-static`, 'static', siteName),
+		{
+			cwd: path.resolve(root, 'src/static'),
+			parents: true,
+			nodir: true,
+		},
+	);
 };
 
 const copyDist = () => {
-    log(' - copying dist');
-    return cpy(
-        ['**/*.!(html|json)'],
-        path.resolve(target, `${siteName}-static`, 'assets'),
-        {
-            cwd: path.resolve(dist),
-            parents: true,
-            nodir: true,
-        },
-    );
+	log(' - copying dist');
+	return cpy(
+		['**/*.!(html|json)'],
+		path.resolve(target, `${siteName}-static`, 'assets'),
+		{
+			cwd: path.resolve(dist),
+			parents: true,
+			nodir: true,
+		},
+	);
 };
 
 const copyRiffRaff = () => {
-    log(' - copying riffraff yaml');
-    return cpy(['riff-raff.yaml'], target, { cwd: __dirname });
+	log(' - copying riffraff yaml');
+	return cpy(['riff-raff.yaml'], target, { cwd: __dirname });
 };
 
 const zipBundle = () => {
-    log(' - zipping bundle');
-    return execa(
-        'zip',
-        ['--recurse-paths', 'rendering.zip', '.', '--exclude', '.git/**\\*'],
-        {
-            shell: true,
-            maxBuffer: 200000000, // increase if you get a maxBuffer exceeded error
-        },
-    ).then(() => {
-        cpy(['rendering.zip'], path.resolve(target, 'rendering', 'dist'));
-    });
+	log(' - zipping bundle');
+	return execa(
+		'zip',
+		['--recurse-paths', 'rendering.zip', '.', '--exclude', '.git/**\\*'],
+		{
+			shell: true,
+			maxBuffer: 200000000, // increase if you get a maxBuffer exceeded error
+		},
+	).then(() => {
+		cpy(['rendering.zip'], path.resolve(target, 'rendering', 'dist'));
+	});
 };
 
 const createBuildConfig = () => {
-    log(' - creating build.json');
-    const buildConfig = {
-        projectName: process.env.PROJECT || 'dotcom:rendering',
-        buildNumber: process.env.BUILD_NUMBER || '0',
-        startTime: process.env.BUILD_START_DATE || new Date().toISOString(),
-        revision: process.env.BUILD_VCS_NUMBER || 'unknown',
-        vcsURL: 'git@github.com:guardian/dotcom-rendering.git',
-        branch: process.env.BRANCH_NAME || 'unknown',
-    };
+	log(' - creating build.json');
+	const buildConfig = {
+		projectName: process.env.PROJECT || 'dotcom:rendering',
+		buildNumber: process.env.BUILD_NUMBER || '0',
+		startTime: process.env.BUILD_START_DATE || new Date().toISOString(),
+		revision: process.env.BUILD_VCS_NUMBER || 'unknown',
+		vcsURL: 'git@github.com:guardian/dotcom-rendering.git',
+		branch: process.env.BRANCH_NAME || 'unknown',
+	};
 
-    return writeFile(
-        path.resolve(target, 'build.json'),
-        JSON.stringify(buildConfig, null, 2),
-    );
+	return writeFile(
+		path.resolve(target, 'build.json'),
+		JSON.stringify(buildConfig, null, 2),
+	);
 };
 
 Promise.all([copyCfn(), copyStatic(), copyDist(), copyRiffRaff()])
-    .then(zipBundle)
-    .then(createBuildConfig)
-    .catch((err) => {
-        warn(err.stack);
-        process.exit(1);
-    });
+	.then(zipBundle)
+	.then(createBuildConfig)
+	.catch((err) => {
+		warn(err.stack);
+		process.exit(1);
+	});
