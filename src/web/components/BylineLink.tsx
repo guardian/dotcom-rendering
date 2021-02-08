fe68dce6aadc16e058c4711c8d64d82db7a4851d
Pascal Honoré <pascal@alseyn.net>
2021-02-08 12:38:11 +0000

    linting

--- src/web/components/BylineLink.tsx
+++ src/web/components/BylineLink.tsx
@@ -5,15 +5,17 @@ type Props = {
 	tags: TagType[];
 };
 
+const getContributorTags = (tags: TagType[]): TagType[] => {
+	return tags.filter((t) => t.type === 'Contributor');
+};
+
 // This crazy function aims to split bylines such as
 // 'Harry Potter in Hogwarts' to ['Harry Potter', 'in Hogwarts']
 // Or
 // 'Jane Doe and John Smith` to ['Jane Doe', ' and ', 'John Smith']
 // It does this so we can have separate links to both contributors
 const bylineAsTokens = (byline: string, tags: TagType[]): string[] => {
-	const contributorTags = tags
-		.filter((t) => t.type === 'Contributor')
-		.map((c) => c.title);
+	const contributorTags = getContributorTags(tags).map((c) => c.title);
 	// The contributor tag title should exist inside the byline for this regex to work
 	const regex = new RegExp(`(${contributorTags.join('|')})`);
 
@@ -22,7 +24,9 @@ const bylineAsTokens = (byline: string, tags: TagType[]): string[] => {
 
 export const BylineLink = ({ byline, tags }: Props) => {
 	const renderedTokens = bylineAsTokens(byline, tags).map((token, i) => {
-		const associatedTags = tags.filter((t) => t.title === token);
+		const associatedTags = getContributorTags(tags).filter(
+			(t) => t.title === token,
+		);
 		if (associatedTags.length > 0) {
 			return (
 				<ContributorLink
