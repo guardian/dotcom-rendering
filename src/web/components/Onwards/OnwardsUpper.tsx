31f940daa6d14e9d7c376add226f5b8cf7acc34d
Josh Buckland <josh.russell.buckland@gmail.com>
2021-02-09 14:31:39 +0000

    expose immersive design by default for now

--- src/web/components/Onwards/OnwardsUpper.tsx
+++ src/web/components/Onwards/OnwardsUpper.tsx
@@ -307,7 +307,7 @@ export const OnwardsUpper = ({
 						ophanComponentName="curated-content"
 						Container={Carousel}
 						pillar={pillar}
-						// isFullCardImage={true}
+						isFullCardImage={true}
 					/>
 				</Section>
 			)}
