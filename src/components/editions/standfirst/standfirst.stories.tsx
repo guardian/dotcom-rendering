8ae09b6c4f7e2c0ba203b42a3ba52c3a8d663246
Anna Beddow <anna.beddow@gmail.com>
2021-02-09 10:40:05 +0000

    Add stories and setup

--- src/components/editions/standfirst/standfirst.stories.tsx
+++ src/components/editions/standfirst/standfirst.stories.tsx
@@ -1,11 +1,25 @@
 // ----- Imports ----- //
 
-import { Pillar } from '@guardian/types';
+import { Display, Pillar, toOption} from '@guardian/types';
+import type { Option } from '@guardian/types';
 import { withKnobs } from '@storybook/addon-knobs';
-import { article } from 'fixtures/item';
+import { parse } from 'client/parser';
+import { article, analysis, comment, media } from 'fixtures/item';
+import { pipe2 } from 'lib';
 import type { ReactElement } from 'react';
 import { selectPillar } from 'storybookHelpers';
 import Standfirst from '.';
+
+// ----- Setup ----- //
+
+const parser = new DOMParser();
+const parseStandfirst = parse(parser);
+
+const standfirst: Option<DocumentFragment> = pipe2(
+	'<p>The Mexican capital was founded by Aztecs on an island in a vast lake. No wonder water flows through so many of its unbuilt projects</p>',
+	parseStandfirst,
+	toOption,
+);
 
 // ----- Stories ----- //
 
@@ -13,6 +27,49 @@ const Default = (): ReactElement => (
 	<Standfirst
 		item={{
 			...article,
+			standfirst,
+			theme: selectPillar(Pillar.News),
+		}}
+	/>
+);
+
+const Showcase = (): ReactElement => (
+	<Standfirst
+		item={{
+			...article,
+			standfirst,
+			display: Display.Showcase,
+			theme: selectPillar(Pillar.News),
+		}}
+	/>
+);
+
+const Comment = (): ReactElement => (
+	<Standfirst
+		item={{
+			...comment,
+			standfirst,
+			theme: selectPillar(Pillar.News),
+		}}
+	/>
+);
+
+const Analysis = (): ReactElement => (
+	<Standfirst
+		item={{
+			...analysis,
+			standfirst,
+			theme: selectPillar(Pillar.News),
+		}}
+	/>
+);
+
+
+const Media = (): ReactElement => (
+	<Standfirst
+		item={{
+			...media,
+			standfirst,
 			theme: selectPillar(Pillar.News),
 		}}
 	/>
@@ -26,4 +83,4 @@ export default {
 	decorators: [withKnobs],
 };
 
-export { Default };
+export { Default, Showcase, Comment, Analysis, Media};
