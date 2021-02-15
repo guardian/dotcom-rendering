d4b066201219f435c4f78ea3761f82a345e2a29d
Oliver Lloyd <email@oliverlloyd.com>
2021-02-15 13:54:58 +0000

    React import was needed here by eslint

--- src/web/components/SubNav/SubNav.tsx
+++ src/web/components/SubNav/SubNav.tsx
@@ -1,4 +1,5 @@
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+
 import { css } from '@emotion/react';
 
 import { text, news, neutral } from '@guardian/src-foundations/palette';
