c19654541921aae5681b655222c256d1dbaff280
Josh Buckland <josh.russell.buckland@gmail.com>
2021-02-15 09:46:34 +0000

    Remove commented lines

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,16 +3,5 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(tsx)"
   ],
-//   webpackFinal: async config => {
-//     config.module.rules.push({
-//         test: /\.(ts|tsx)$/,
-//         use: [
-//             {
-//                 loader: require.resolve('ts-loader'),
-//             },
-//         ],
-//     });
-//     return config;
-//   },
   "addons": ["@storybook/addon-viewport"]
 }
