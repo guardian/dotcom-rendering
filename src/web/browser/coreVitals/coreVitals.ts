c92e0370c1743052887f8584ad983a1cd4d043c1
Mark Addis <mark.addis@guardian.co.uk>
2021-02-15 14:24:20 +0000

    Prettier

--- src/web/browser/coreVitals/coreVitals.ts
+++ src/web/browser/coreVitals/coreVitals.ts
@@ -1,79 +1,74 @@
 import { getCLS, getFID, getLCP, getFCP, getTTFB } from 'web-vitals';
 
-
 // Stops ts assuming json values are null
-const temp : any = null;
+const temp: any = null;
 
 const jsonData = {
-	'page_view_id': '',
-	'received_timestamp': `${new Date().toISOString()}`,
-	'id': 'prod',
-	'received_date': new Date().toISOString().slice(0, 10),
-    'fid': temp,
-    'cls': temp,
-    'lcp': temp,
-    'fcp': temp,
-    'ttfb': temp,
-}
+	page_view_id: '',
+	received_timestamp: `${new Date().toISOString()}`,
+	id: 'prod',
+	received_date: new Date().toISOString().slice(0, 10),
+	fid: temp,
+	cls: temp,
+	lcp: temp,
+	fcp: temp,
+	ttfb: temp,
+};
 
-export const coreVitals = (): void =>
-{
-    type CoreVitalsArgs = {
-        name: string;
-        value: number;
-    };
+export const coreVitals = (): void => {
+	type CoreVitalsArgs = {
+		name: string;
+		value: number;
+	};
 
 	const getValue = ({ name, value }: CoreVitalsArgs): void => {
+		switch (name) {
+			case 'FCP':
+				jsonData.fcp = parseFloat(value.toFixed(6));
+				break;
+			case 'CLS':
+				jsonData.cls = parseFloat(value.toFixed(6));
+				break;
+			case 'LCP':
+				jsonData.lcp = parseFloat(value.toFixed(6));
+				break;
+			case 'FID':
+				jsonData.fid = parseFloat(value.toFixed(6));
+				break;
+			case 'TTFB':
+				jsonData.ttfb = parseFloat(value.toFixed(6));
+				break;
+		}
 
-    switch(name){
-        case('FCP'):
-            jsonData.fcp = parseFloat(value.toFixed(6));
-            break;
-        case('CLS'):
-            jsonData.cls = parseFloat(value.toFixed(6));
-            break;
-        case('LCP'):
-            jsonData.lcp = parseFloat(value.toFixed(6));
-            break;
-        case('FID'):
-            jsonData.fid = parseFloat(value.toFixed(6));
-            break;
-        case('TTFB'):
-            jsonData.ttfb = parseFloat(value.toFixed(6));
-            break;
-	}
-
-	// If CLS has been calculated
-    if(jsonData.cls !== null)
-    {
-        fetch('https://performance-events.guardianapis.com/core-web-vitals', {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            redirect: 'follow',
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-w
-            body: JSON.stringify(jsonData)
-		})
-    };
-}
+		// If CLS has been calculated
+		if (jsonData.cls !== null) {
+			fetch(
+				'https://performance-events.guardianapis.com/core-web-vitals',
+				{
+					method: 'POST', // *GET, POST, PUT, DELETE, etc.
+					mode: 'cors', // no-cors, *cors, same-origin
+					cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+					credentials: 'same-origin', // include, *same-origin, omit
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					redirect: 'follow',
+					referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-w
+					body: JSON.stringify(jsonData),
+				},
+			);
+		}
+	};
 
 	// Set page view and browser ID
-	if(window.guardian && window.guardian.ophan)
-    {
+	if (window.guardian && window.guardian.ophan) {
 		jsonData.page_view_id = window.guardian.ophan.pageViewId;
 		jsonData.id = window.guardian.config.ophan.browserId;
 	}
 
-
 	getCLS(getValue, false);
 	getFID(getValue);
-    getLCP(getValue);
-    getFCP(getValue);
-    getTTFB(getValue);
-
-
+	getLCP(getValue);
+	getFCP(getValue);
+	getTTFB(getValue);
 };
--- src/web/browser/coreVitals/init.ts
+++ src/web/browser/coreVitals/init.ts
@@ -3,8 +3,8 @@ import { startup } from '@root/src/web/browser/startup';
 import { coreVitals } from './coreVitals';
 
 const init = (): Promise<void> => {
-    coreVitals();
-    return Promise.resolve();
+	coreVitals();
+	return Promise.resolve();
 };
 
 startup('coreVitals', null, init);
