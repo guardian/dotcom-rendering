e02190047f1c39ff89ca4e0ad43b594da83e2237
Mahesh Makani <maheshmakani@gmail.com>
2021-02-16 11:32:40 +0000

    remove sign in gate design opt test

--- cypress/integration/e2e/sign-in-gate.spec.js
+++ cypress/integration/e2e/sign-in-gate.spec.js
@@ -4,8 +4,8 @@ import { setLocalBaseUrl } from '../../lib/setLocalBaseUrl.js';
 /* eslint-disable no-undef */
 /* eslint-disable func-names */
 
-describe('Sign In Gate Tests', function () {
-	beforeEach(function () {
+describe('Sign In Gate Tests', function() {
+	beforeEach(function() {
 		setLocalBaseUrl();
 	});
 
@@ -55,8 +55,8 @@ describe('Sign In Gate Tests', function () {
 		scrollToGateForLazyLoading(roughPosition);
 	};
 
-	describe('SignInGateMain', function () {
-		beforeEach(function () {
+	describe('SignInGateMain', function() {
+		beforeEach(function() {
 			disableCMP();
 			// sign in gate main runs from 0-900000 MVT IDs, so 500 forces user into test
 			setMvtCookie('500');
@@ -65,13 +65,13 @@ describe('Sign In Gate Tests', function () {
 			setArticleCount(3);
 		});
 
-		it('should load the sign in gate', function () {
+		it('should load the sign in gate', function() {
 			visitArticleAndScrollToGateForLazyLoad();
 
 			cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
 		});
 
-		it('should not load the sign in gate if the user has not read at least 3 article in a day', function () {
+		it('should not load the sign in gate if the user has not read at least 3 article in a day', function() {
 			setArticleCount(1);
 
 			visitArticleAndScrollToGateForLazyLoad();
@@ -79,7 +79,7 @@ describe('Sign In Gate Tests', function () {
 			cy.get('[data-cy=sign-in-gate-main]').should('not.exist');
 		});
 
-		it('should not load the sign in gate if the user is signed in', function () {
+		it('should not load the sign in gate if the user is signed in', function() {
 			// use GU_U cookie to determine if user is signed in
 			cy.setCookie(
 				'GU_U',
@@ -100,7 +100,7 @@ describe('Sign In Gate Tests', function () {
 			cy.get('[data-cy=sign-in-gate-main]').should('not.exist');
 		});
 
-		it('should not load the sign in gate if the user has already dismissed the gate', function () {
+		it('should not load the sign in gate if the user has already dismissed the gate', function() {
 			localStorage.setItem(
 				'gu.prefs.sign-in-gate',
 				`{
@@ -116,7 +116,7 @@ describe('Sign In Gate Tests', function () {
 			cy.get('[data-cy=sign-in-gate-main]').should('not.exist');
 		});
 
-		it('should not load the sign in gate if the article is not a valid section (membership)', function () {
+		it('should not load the sign in gate if the article is not a valid section (membership)', function() {
 			visitArticleAndScrollToGateForLazyLoad({
 				url:
 					'https://www.theguardian.com/membership/2018/nov/15/support-guardian-readers-future-journalism',
@@ -125,7 +125,7 @@ describe('Sign In Gate Tests', function () {
 			cy.get('[data-cy=sign-in-gate-main]').should('not.exist');
 		});
 
-		it('should not load the sign in gate on a device with an ios9 user agent string', function () {
+		it('should not load the sign in gate on a device with an ios9 user agent string', function() {
 			// can't use visitArticleAndScrollToGateForLazyLoad for this method as overriding user agent
 			cy.visit(
 				'Article?url=https://www.theguardian.com/games/2018/aug/23/nier-automata-yoko-taro-interview',
@@ -143,7 +143,7 @@ describe('Sign In Gate Tests', function () {
 			cy.get('[data-cy=sign-in-gate-main]').should('not.exist');
 		});
 
-		it('should remove gate when the dismiss button is clicked', function () {
+		it('should remove gate when the dismiss button is clicked', function() {
 			visitArticleAndScrollToGateForLazyLoad();
 
 			cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -153,7 +153,7 @@ describe('Sign In Gate Tests', function () {
 			cy.get('[data-cy=sign-in-gate-main]').should('not.exist');
 		});
 
-		it('register button should contain profile.theguardian.com href', function () {
+		it('register button should contain profile.theguardian.com href', function() {
 			visitArticleAndScrollToGateForLazyLoad();
 
 			cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -163,7 +163,7 @@ describe('Sign In Gate Tests', function () {
 				.should('contains', 'profile.theguardian.com');
 		});
 
-		it('sign in link should contain profile.theguardian.com href', function () {
+		it('sign in link should contain profile.theguardian.com href', function() {
 			visitArticleAndScrollToGateForLazyLoad();
 
 			cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -173,7 +173,7 @@ describe('Sign In Gate Tests', function () {
 				.should('contains', 'profile.theguardian.com');
 		});
 
-		it('should show cmp ui when privacy settings link is clicked', function () {
+		it('should show cmp ui when privacy settings link is clicked', function() {
 			visitArticleAndScrollToGateForLazyLoad();
 
 			cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
@@ -181,118 +181,6 @@ describe('Sign In Gate Tests', function () {
 			cy.get('[data-cy=sign-in-gate-main_privacy]').click();
 
 			cy.contains('privacy settings');
-		});
-	});
-
-	describe('Sign In Gate Design Test', function () {
-		beforeEach(function () {
-			disableCMP();
-
-			// set article count to be min number to view gate
-			setArticleCount(3);
-		});
-
-		context('design-opt-control', function () {
-			it('shows the design test control variant', function () {
-				// 700007 - user in control according to https://ab-tests.netlify.app/
-				// audience 0.2, offset 0.7
-				// 6 variants + control (7 total)
-				setMvtCookie('700007');
-
-				visitArticleAndScrollToGateForLazyLoad();
-
-				cy.get('[data-cy=sign-in-gate-main]').should('be.visible');
-			});
-		});
-
-		context('design-opt-variant-1', function () {
-			it('shows the design test no faqs variant', function () {
-				// 700001 - user in variant-1 according to https://ab-tests.netlify.app/
-				// audience 0.2, offset 0.7
-				// 6 variants + control (7 total)
-				setMvtCookie('700001');
-
-				visitArticleAndScrollToGateForLazyLoad();
-
-				cy.get('[data-cy=sign-in-gate-design-opt-variant-1]').should(
-					'be.visible',
-				);
-			});
-		});
-
-		context('design-opt-variant-2', function () {
-			it('shows the design test blue highlight variant', function () {
-				// 700002 - user in variant-2 according to https://ab-tests.netlify.app/
-				// audience 0.2, offset 0.7
-				// 6 variants + control (7 total)
-				setMvtCookie('700002');
-
-				visitArticleAndScrollToGateForLazyLoad();
-
-				cy.get('[data-cy=sign-in-gate-design-opt-variant-2]').should(
-					'be.visible',
-				);
-			});
-		});
-
-		context('design-opt-variant-3', function () {
-			it('shows the design test less whitespace variant', function () {
-				// 700003 - user in variant-3 according to https://ab-tests.netlify.app/
-				// audience 0.2, offset 0.7
-				// 6 variants + control (7 total)
-				setMvtCookie('700003');
-
-				visitArticleAndScrollToGateForLazyLoad();
-
-				cy.get('[data-cy=sign-in-gate-design-opt-variant-3]').should(
-					'be.visible',
-				);
-			});
-		});
-
-		context('design-opt-variant-4', function () {
-			it('shows the design test no line variant', function () {
-				// 700004 - user in variant-4 according to https://ab-tests.netlify.app/
-				// audience 0.2, offset 0.7
-				// 6 variants + control (7 total)
-				setMvtCookie('700004');
-
-				visitArticleAndScrollToGateForLazyLoad();
-
-				cy.get('[data-cy=sign-in-gate-design-opt-variant-4]').should(
-					'be.visible',
-				);
-			});
-		});
-
-		context('design-opt-variant-5', function () {
-			it('shows the design test colour variant', function () {
-				// 700005 - user in variant-5 according to https://ab-tests.netlify.app/
-				// audience 0.2, offset 0.7
-				// 6 variants + control (7 total)
-				setMvtCookie('700005');
-
-				visitArticleAndScrollToGateForLazyLoad();
-
-				cy.get('[data-cy=sign-in-gate-design-opt-variant-5]').should(
-					'be.visible',
-				);
-			});
-		});
-
-		context('design-opt-variant-6', function () {
-			it('shows the design test button variant', function () {
-				// 700006 - user in variant-6 according to https://ab-tests.netlify.app/
-				// audience 0.2, offset 0.7
-				// 6 variants + control (7 total)
-				setMvtCookie('700006');
-
-				visitArticleAndScrollToGateForLazyLoad();
-
-				cy.get('[data-cy=sign-in-gate-design-opt-variant-6]').should(
-					'be.visible',
-				);
-			});
 		});
 	});
 });
