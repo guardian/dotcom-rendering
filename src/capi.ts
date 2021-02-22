2c0c3d76e104509506414b4ffa6240ae59d895ca
Marjan K <marjank@30728.gnm.int>
2021-02-22 17:48:28 +0000

    fix test failure

--- src/capi.ts
+++ src/capi.ts
@@ -192,7 +192,6 @@ const capiEndpoint = (articleId: string, key: string): string => {
 		'show-blocks': 'all',
 		'show-elements': 'all',
 		'show-related': 'true',
-		'show-references': 'all',
 	});
 
 	return `https://content.guardianapis.com/${articleId}?${params.toString()}`;
