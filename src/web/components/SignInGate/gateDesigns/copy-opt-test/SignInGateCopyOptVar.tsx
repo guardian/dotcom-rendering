139b57a584141cb7018203b130bfd779c70ce2b4
Rebecca Thompson <becca.al.thompson@gmail.com>
2021-02-26 09:55:10 +0000

    removing errant semi-colon

--- src/web/components/SignInGate/gateDesigns/copy-opt-test/SignInGateCopyOptVar.tsx
+++ src/web/components/SignInGate/gateDesigns/copy-opt-test/SignInGateCopyOptVar.tsx
@@ -39,7 +39,7 @@ export const SignInGateCopyOptVar = ({
 			<h1 className={headingStyles}>{signInGateCopy?.header}</h1>
 			<p className={bodyBold}>{signInGateCopy?.subHeader}</p>
 			{signInGateCopy?.paragraphs.map((paragraph) => {
-				return <p className={bodyText}>{paragraph}</p>;
+				return <p className={bodyText}>{paragraph}</p>
 			})}
 			;
 			<div className={actionButtons}>
