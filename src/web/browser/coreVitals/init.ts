187f51c5bd5c0cf65015dcdf64f2296f73690af5
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2021-03-02 13:38:40 +0000

    Comment Error

--- src/web/browser/coreVitals/init.ts
+++ src/web/browser/coreVitals/init.ts
@@ -3,7 +3,7 @@ import { startup } from '@root/src/web/browser/startup';
 import { coreVitals } from './coreVitals';
 
 const init = async (): Promise<void> => {
-	// Sample between 1 and 100
+	// Sample 1 out of 100
 	const inSample = Math.floor(Math.random() * 100);
 	if (inSample === 1) {
 		coreVitals();
