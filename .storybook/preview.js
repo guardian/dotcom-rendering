d165c005bb09af0513de4476528ae3eddd036eec
Gareth <gtrufitt@gmail.com>
2021-03-02 17:30:02 +0000

    Prettier mods

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,8 +18,8 @@ Lazy.disabled = isChromatic();
 Picture.disableLazyLoading = isChromatic();
 
 if (isChromatic()) {
-    // Fix the date to prevent false negatives
-    MockDate.set('Sun Jan 10 2021 12:00:00 GMT+0000 (Greenwich Mean Time)');
+	// Fix the date to prevent false negatives
+	MockDate.set('Sun Jan 10 2021 12:00:00 GMT+0000 (Greenwich Mean Time)');
 }
 
 // Add base css for the site
@@ -31,94 +31,93 @@ style.type = 'text/css';
 style.appendChild(document.createTextNode(css));
 
 const guardianViewports = {
-    mobileMedium: {
-        name: 'mobileMedium',
-        styles: {
-            width: '375px',
-            height: '800px',
-        },
-    },
-    mobileLandscape: {
-        name: 'mobileLandscape',
-        styles: {
-            width: '480px',
-            height: '800px',
-        },
-    },
-    phablet: {
-        name: 'phablet',
-        styles: {
-            width: '660px',
-            height: '800px',
-        },
-    },
-    tablet: {
-        name: 'tablet',
-        styles: {
-            width: '740px',
-            height: '800px',
-        },
-    },
-    desktop: {
-        name: 'desktop',
-        styles: {
-            width: '980px',
-            height: '800px',
-        },
-    },
-    leftCol: {
-        name: 'leftCol',
-        styles: {
-            width: '1140px',
-            height: '800px',
-        },
-    },
-    wide: {
-        name: 'wide',
-        styles: {
-            width: '1300px',
-            height: '800px',
-        },
-    },
+	mobileMedium: {
+		name: 'mobileMedium',
+		styles: {
+			width: '375px',
+			height: '800px',
+		},
+	},
+	mobileLandscape: {
+		name: 'mobileLandscape',
+		styles: {
+			width: '480px',
+			height: '800px',
+		},
+	},
+	phablet: {
+		name: 'phablet',
+		styles: {
+			width: '660px',
+			height: '800px',
+		},
+	},
+	tablet: {
+		name: 'tablet',
+		styles: {
+			width: '740px',
+			height: '800px',
+		},
+	},
+	desktop: {
+		name: 'desktop',
+		styles: {
+			width: '980px',
+			height: '800px',
+		},
+	},
+	leftCol: {
+		name: 'leftCol',
+		styles: {
+			width: '1140px',
+			height: '800px',
+		},
+	},
+	wide: {
+		name: 'wide',
+		styles: {
+			width: '1300px',
+			height: '800px',
+		},
+	},
 };
 
 export const viewports = [375, 480, 660, 740, 980, 1140, 1300];
 
 fetchMock
-    .restore()
-    // Comment count
-    .get(
-        'begin:https://api.nextgen.guardianapps.co.uk/discussion/comment-counts.json?shortUrls=',
-        {
-            status: 200,
-            body: commentCount,
-        },
-        {
-            overwriteRoutes: false,
-        },
-    )
-    // Share count
-    .get(
-        'begin:https://api.nextgen.guardianapps.co.uk/sharecount/',
-        {
-            status: 200,
-            body: {
-                path:
-                    'money/2017/mar/10/ministers-to-criminalise-use-of-ticket-tout-harvesting-software',
-                share_count: 273,
-                refreshStatus: true,
-            },
-        },
-        {
-            overwriteRoutes: false,
-        },
-    );
-
+	.restore()
+	// Comment count
+	.get(
+		'begin:https://api.nextgen.guardianapps.co.uk/discussion/comment-counts.json?shortUrls=',
+		{
+			status: 200,
+			body: commentCount,
+		},
+		{
+			overwriteRoutes: false,
+		},
+	)
+	// Share count
+	.get(
+		'begin:https://api.nextgen.guardianapps.co.uk/sharecount/',
+		{
+			status: 200,
+			body: {
+				path:
+					'money/2017/mar/10/ministers-to-criminalise-use-of-ticket-tout-harvesting-software',
+				share_count: 273,
+				refreshStatus: true,
+			},
+		},
+		{
+			overwriteRoutes: false,
+		},
+	);
 
 export const parameters = {
 	viewport: {
-        viewports: guardianViewports,
-        defaultViewport: 'wide',
-    },
-	layout: 'fullscreen'
-}
+		viewports: guardianViewports,
+		defaultViewport: 'wide',
+	},
+	layout: 'fullscreen',
+};
