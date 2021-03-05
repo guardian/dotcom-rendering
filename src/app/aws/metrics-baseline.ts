cd3a53eb65c925ae5613d9da9932f18ce9323f50
Nicolas Long <nicolas.long@theguardian.com>
2021-03-05 15:28:46 +0000

    Re-enable metrics other than disk-space check

--- src/app/aws/metrics-baseline.ts
+++ src/app/aws/metrics-baseline.ts
@@ -1,11 +1,11 @@
-// TODO re-enable after Graviton testing complete (the diskusage module doesn't work on ARM).
+// TODO re-enable disk space checks after Graviton testing complete (the diskusage module doesn't work on ARM).
 
-/* import os from 'os';
-import disk from 'diskusage';
+import os from 'os';
+// import disk from 'diskusage';
 import { BytesMetric, collectAndSendAWSMetrics } from './aws-metrics';
 
 const maxHeapMemory = BytesMetric('rendering', 'PROD', 'max-heap-memory');
-const freeDiskSpace = BytesMetric('rendering', 'PROD', 'free-disk-memory');
+// const freeDiskSpace = BytesMetric('rendering', 'PROD', 'free-disk-memory');
 const usedHeapMemory = BytesMetric('rendering', 'PROD', 'used-heap-memory');
 const freePhysicalMemory = BytesMetric(
 	'rendering',
@@ -25,24 +25,30 @@ collectAndSendAWSMetrics(
 	usedHeapMemory,
 	totalPhysicalMemory,
 	freePhysicalMemory,
-	freeDiskSpace,
+	// freeDiskSpace,
 );
 
 // records system metrics
 
 export const recordBaselineCloudWatchMetrics = () => {
-	disk.check('/', (err, diskinfo) => {
-		if (err) {
-			// eslint-disable-next-line no-console
-			console.error(err);
-		} else {
-			maxHeapMemory.record(process.memoryUsage().heapTotal);
-			usedHeapMemory.record(process.memoryUsage().heapUsed);
-			totalPhysicalMemory.record(os.totalmem());
-			freePhysicalMemory.record(os.freemem());
-			if (diskinfo) {
-				freeDiskSpace.record(diskinfo.free);
-			}
-		}
-	});
-}; */
+	// TODO re-enable once ARM testing complete
+	// disk.check('/', (err, diskinfo) => {
+	// 	if (err) {
+	// 		// eslint-disable-next-line no-console
+	// 		console.error(err);
+	// 	} else {
+	// 		maxHeapMemory.record(process.memoryUsage().heapTotal);
+	// 		usedHeapMemory.record(process.memoryUsage().heapUsed);
+	// 		totalPhysicalMemory.record(os.totalmem());
+	// 		freePhysicalMemory.record(os.freemem());
+	// 		if (diskinfo) {
+	// 			freeDiskSpace.record(diskinfo.free);
+	// 		}
+	// 	}
+	// });
+
+	maxHeapMemory.record(process.memoryUsage().heapTotal);
+	usedHeapMemory.record(process.memoryUsage().heapUsed);
+	totalPhysicalMemory.record(os.totalmem());
+	freePhysicalMemory.record(os.freemem());
+};
