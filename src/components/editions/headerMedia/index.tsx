e86e1c2b6ff665f806f3cbfe76f0e8ff456191b6
Anna Beddow <anna.beddow@gmail.com>
2021-03-08 16:11:20 +0000

    disable dark mode

--- src/components/editions/headerMedia/index.tsx
+++ src/components/editions/headerMedia/index.tsx
@@ -150,7 +150,7 @@ const HeaderMedia: FC<Props> = ({ item }) => {
 						sizes={getImageSizes(format)}
 						format={item}
 						className={some(getImageStyle(image, format))}
-						supportsDarkMode
+						supportsDarkMode={false}
 						lightbox={some({
 							className: 'js-launch-slideshow js-main-image',
 							caption: none,
