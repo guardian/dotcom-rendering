292f18b795f4c84c0f4d9afc09295076551956fc
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2021-03-10 11:12:54 +0000

    update src typography types imports

--- src/components/editions/headline/index.tsx
+++ src/components/editions/headline/index.tsx
@@ -3,13 +3,13 @@
 import { css } from '@emotion/react';
 import type { SerializedStyles } from '@emotion/react';
 import { remSpace } from '@guardian/src-foundations';
-import { from } from '@guardian/src-foundations/mq';
-import { border, neutral } from '@guardian/src-foundations/palette';
-import { headline, titlepiece } from '@guardian/src-foundations/typography';
 import type {
 	FontWeight,
 	LineHeight,
-} from '@guardian/src-foundations/typography/types';
+} from '@guardian/src-foundations/dist/types/typography/types';
+import { from } from '@guardian/src-foundations/mq';
+import { border, neutral } from '@guardian/src-foundations/palette';
+import { headline, titlepiece } from '@guardian/src-foundations/typography';
 import { SvgQuote } from '@guardian/src-icons';
 import type { Format } from '@guardian/types';
 import { Design, Display } from '@guardian/types';
