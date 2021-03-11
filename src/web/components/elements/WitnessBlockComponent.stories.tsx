12baf392aba84188944d023ecb9992cf61ab7b50
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-03-11 16:19:54 +0000

    Fix palette prop error

--- src/web/components/elements/WitnessBlockComponent.stories.tsx
+++ src/web/components/elements/WitnessBlockComponent.stories.tsx
@@ -31,7 +31,11 @@ export const WitnessTextBlockComponentDefault = () => (
 			description='You could dial first and then push in ur 2p. I never had any change and needed a lift. My parents knew the routine, I&#39;d dial and you could get a second or two to shout down the line "Pick me up" Ah how I miss the days of a simple life where no one knew where I was and I could get away with anything as long as I was in the door by 10pm! '
 			authorName="Louise Griffiths"
 			dateCreated="2016-01-29T22:19:51Z"
-			pillar={Pillar.Sport}
+			palette={decidePalette({
+				theme: Pillar.Sport,
+				design: Design.Article,
+				display: Display.Standard,
+			})}
 		/>
 	</div>
 );
