47d8555ec20d1cc542d483b2a076cdebb4e86ab2
Peter Colley <zvvvvt@gmail.com>
2021-03-15 11:48:45 +0000

    prettier and audience offset for mandatory gate

--- cypress/integration/parallel-1/sign-in-gate.spec.js
+++ cypress/integration/parallel-1/sign-in-gate.spec.js
@@ -233,5 +233,4 @@ describe('Sign In Gate Tests', function () {
 			});
 		});
 	});
-
 });
--- src/web/experiments/tests/sign-in-gate-mandatory.ts
+++ src/web/experiments/tests/sign-in-gate-mandatory.ts
@@ -8,7 +8,7 @@ export const signInGateMandoryTest: ABTest = {
 	description:
 		'Compare mandatory gate (an article sigin gate without the dismiss button) with the main signin gate',
 	audience: 0.01,
-	audienceOffset: 0.69,
+	audienceOffset: 0.89,
 	successMeasure: 'Users sign in or create a Guardian account',
 	audienceCriteria:
 		'3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss or reshown after 5 dismisses, not on help, info sections etc. exclude iOS 9 and guardian-live-australia, UK only, only users with specific CMP consents. Suppresses other banners, and appears over epics',
