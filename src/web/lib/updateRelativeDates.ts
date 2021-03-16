c09d92a85e9b51629fb608f855d61458b34cfd4e
Oliver Lloyd <email@oliverlloyd.com>
2021-03-16 14:46:46 +0000

    feat: Added updateRelativeDates function

---
+++ src/web/lib/updateRelativeDates.ts
@@ -0,0 +1,66 @@
+import { makeRelativeDate } from '@root/src/web/lib/dateTime';
+
+const DEFAULT_INTERVAL = 15 * 1000; // 15 seconds
+
+type Options = {
+	interval?: number;
+};
+
+export const updateRelativeDates = (options?: Options): number[] => {
+	if (typeof window === 'undefined') return [];
+	const intervalIds: number[] = [];
+	document
+		.querySelectorAll('time[data-relativeformat]')
+		.forEach((element) => {
+			if (element instanceof HTMLElement) {
+				// Get required values
+				const { relativeformat: relativeFormat } = element.dataset;
+				const absoluteTime = element.getAttribute('datetime');
+				if (!absoluteTime || !relativeFormat) return [];
+
+				// Decide what interval to use
+				const elementInterval =
+					element.dataset.interval &&
+					parseInt(element.dataset.interval, 10);
+				const interval: number =
+					elementInterval || options?.interval || DEFAULT_INTERVAL;
+
+				// Create a new interval for this element
+				intervalIds.push(
+					window.setInterval(() => {
+						let newTime;
+						switch (relativeFormat) {
+							case 'short':
+								newTime = makeRelativeDate(
+									new Date(absoluteTime).getTime(),
+									{
+										format: 'short',
+									},
+								);
+								break;
+							case 'med':
+								newTime = makeRelativeDate(
+									new Date(absoluteTime).getTime(),
+									{
+										format: 'med',
+									},
+								);
+								break;
+							case 'long':
+								newTime = makeRelativeDate(
+									new Date(absoluteTime).getTime(),
+									{
+										format: 'long',
+									},
+								);
+								break;
+						}
+						const oldTime = element.innerText;
+						if (newTime && newTime !== oldTime)
+							element.innerHTML = newTime;
+					}, interval),
+				);
+			}
+		});
+	return intervalIds;
+};
