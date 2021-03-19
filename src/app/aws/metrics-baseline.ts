f572428a93be2f730ce09ca11f434585f5e40525
Nicolas Long <nicolas.long@theguardian.com>
2021-03-19 12:36:09 +0000

    Remove diskusage for now as cleaner

--- src/app/aws/metrics-baseline.ts
+++ src/app/aws/metrics-baseline.ts
@@ -1,10 +1,7 @@
-// TODO replace disk-usage code with an ARM-friendly solution (probably in a separate process)
 import os from 'os';
-// import disk from 'diskusage';
 import { BytesMetric, collectAndSendAWSMetrics } from './aws-metrics';
 
 const maxHeapMemory = BytesMetric('rendering', 'PROD', 'max-heap-memory');
-// const freeDiskSpace = BytesMetric('rendering', 'PROD', 'free-disk-memory');
 const usedHeapMemory = BytesMetric('rendering', 'PROD', 'used-heap-memory');
 const freePhysicalMemory = BytesMetric(
 	'rendering',
@@ -22,25 +19,9 @@ collectAndSendAWSMetrics(
 	usedHeapMemory,
 	totalPhysicalMemory,
 	freePhysicalMemory,
-	// freeDiskSpace,
 );
 
 export const recordBaselineCloudWatchMetrics = () => {
-	// disk.check('/', (err, diskinfo) => {
-	// 	if (err) {
-	// 		// eslint-disable-next-line no-console
-	// 		console.error(err);
-	// 	} else {
-	// 		maxHeapMemory.record(process.memoryUsage().heapTotal);
-	// 		usedHeapMemory.record(process.memoryUsage().heapUsed);
-	// 		totalPhysicalMemory.record(os.totalmem());
-	// 		freePhysicalMemory.record(os.freemem());
-	// 		if (diskinfo) {
-	// 			freeDiskSpace.record(diskinfo.free);
-	// 		}
-	// 	}
-	// });
-
 	maxHeapMemory.record(process.memoryUsage().heapTotal);
 	usedHeapMemory.record(process.memoryUsage().heapUsed);
 	totalPhysicalMemory.record(os.totalmem());
