40cd8ef123d738264836c1d3d60dd526e9f1ebe0
Max Duval <max.duval@guardian.co.uk>
2021-03-23 10:29:15 -0400

    reactive stars
    Co-authored-by: Oliver Lloyd <1336821+oliverlloyd@users.noreply.github.com>

---
+++ src/static/icons/Star.tsx
@@ -0,0 +1,37 @@
+import { neutral } from '@guardian/src-foundations/palette';
+import React from 'react';
+
+export const Star = ({
+	starId = '000',
+	isEmpty,
+}: {
+	starId: string;
+	isEmpty: boolean;
+}) => {
+	if (isEmpty)
+		return (
+			<svg width="14" height="13" viewBox="0 0 14 13">
+				<path
+					id={`star-${starId}`}
+					clipPath={`url(#clip-${starId})`}
+					strokeWidth="2"
+					stroke={neutral[7]}
+					fill="transparent"
+					d="M0 5.2L3.7 8l-1.4 4.6.5.4 3.7-2.8 3.7 2.8.5-.4L9.3 8 13 5.2l-.2-.6H8.2L6.8 0h-.6L4.8 4.6H.2l-.2.6z"
+				/>
+				<clipPath id={`clip-${starId}`}>
+					<use xlinkHref={`#star-${starId}`} stroke="none" />
+				</clipPath>
+			</svg>
+		);
+
+	return (
+		<svg width="14" height="13" viewBox="0 0 14 13">
+			<path
+				fill={neutral[7]}
+				strokeWidth="0"
+				d="M0 5.2L3.7 8l-1.4 4.6.5.4 3.7-2.8 3.7 2.8.5-.4L9.3 8 13 5.2l-.2-.6H8.2L6.8 0h-.6L4.8 4.6H.2l-.2.6z"
+			/>
+		</svg>
+	);
+};
--- src/web/components/StarRating/StarRating.tsx
+++ src/web/components/StarRating/StarRating.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css } from 'emotion';
-import Star from '@frontend/static/icons/star.svg';
+import { Star } from '@frontend/static/icons/Star';
 import { neutral } from '@guardian/src-foundations/palette';
 
 // https://docs.google.com/spreadsheets/d/1QUa5Kh734J4saFc8ERjCYHZu10_-Hj7llNa2rr8urNg/edit?usp=sharing
@@ -53,19 +53,19 @@ export const StarRating: React.FC<{
 }> = ({ rating, size }) => (
 	<div className={determineSize(size)}>
 		<div className={starWrapper}>
-			<Star className={rating < 1 && emptyStar} />
+			<Star starId={`${size}1`} isEmpty={rating < 1} />
 		</div>
 		<div className={starWrapper}>
-			<Star className={rating < 2 && emptyStar} />
+			<Star starId={`${size}2`} isEmpty={rating < 2} />
 		</div>
 		<div className={starWrapper}>
-			<Star className={rating < 3 && emptyStar} />
+			<Star starId={`${size}3`} isEmpty={rating < 3} />
 		</div>
 		<div className={starWrapper}>
-			<Star className={rating < 4 && emptyStar} />
+			<Star starId={`${size}4`} isEmpty={rating < 4} />
 		</div>
 		<div className={starWrapper}>
-			<Star className={rating < 5 && emptyStar} />
+			<Star starId={`${size}5`} isEmpty={rating < 5} />
 		</div>
 	</div>
 );
