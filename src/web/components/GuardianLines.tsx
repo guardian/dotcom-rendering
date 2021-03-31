ccdb44c0368f41aa32dffdf8e00e2d998f3022a1
Oliver Lloyd <email@oliverlloyd.com>
2021-03-31 11:31:52 +0100

    fix: Removed linesCssOverwrite because this is the wrong way to make these changes

--- src/web/components/GuardianLines.tsx
+++ src/web/components/GuardianLines.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import { css } from 'emotion';
 
 import { Lines } from '@guardian/src-ed-lines';
-import { remSpace } from '@guardian/src-foundations';
-
-const linesCssOverwrite = (palette: Palette) => css`
-	> div {
-		background-image: repeating-linear-gradient(
-			to bottom,
-			${palette.border.lines},
-			${palette.border.lines} 1px,
-			transparent 1px,
-			transparent ${remSpace[1]}
-		);
-	}
-`;
 
 export const GuardianLines = ({
-	palette,
+	// TODO: Use palette here once we have the latest version of src-ed-lines that suppprts the color prop,
 	count,
 	effect,
 }: {
 	palette: Palette;
 	count?: 4 | 8;
 	effect?: 'straight' | 'squiggly' | 'dotted';
-}) => (
-	<div className={linesCssOverwrite(palette)}>
-		<Lines count={count} effect={effect} />
-	</div>
-);
+}) => <Lines count={count} effect={effect} />;
