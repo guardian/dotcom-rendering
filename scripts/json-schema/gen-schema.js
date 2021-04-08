a2bcd64bea7a966322beafff937394467b22e031
Gareth <gtrufitt@gmail.com>
2021-04-08 12:29:37 +0100

    Formatting

--- scripts/json-schema/gen-schema.js
+++ scripts/json-schema/gen-schema.js
@@ -1,20 +1,21 @@
 const TJS = require('typescript-json-schema');
 const { resolve } = require('path');
+
 const root = resolve(__dirname, '..', '..');
 const fs = require('fs');
 
-const program = TJS.getProgramFromFiles([
-	resolve(root + '/src/lib/content.d.ts'),
-	resolve(root + '/index.d.ts'),
-], {
-    skipLibCheck: true,
-});
+const program = TJS.getProgramFromFiles(
+	[resolve(`${root}/src/lib/content.d.ts`), resolve(`${root}/index.d.ts`)],
+	{
+		skipLibCheck: true,
+	},
+);
 
 const settings = { rejectDateType: true, required: true };
 const schema = TJS.generateSchema(program, 'CAPIType', settings);
 
 fs.writeFile(
-	root + '/src/model/json-schema.json',
+	`${root}/src/model/json-schema.json`,
 	JSON.stringify(schema, null, 4),
 	'utf8',
 	function (err) {
