47ba5614ff3c3099155407623a0573ee0a3b8db9
Gareth <gtrufitt@gmail.com>
2021-04-14 17:43:29 +0100

    Fetch CAPI Key for interactive atom from the param store

--- src/web/server/cacheForAnniversaryInteractiveAtom.ts
+++ src/web/server/cacheForAnniversaryInteractiveAtom.ts
@@ -1,4 +1,8 @@
 import fetch from 'node-fetch';
+import {
+	getGuardianConfiguration,
+	GuardianConfiguration,
+} from '@frontend/app/aws/aws-parameters';
 
 // WARNING
 // This is a TEMP solution.
@@ -21,20 +25,22 @@ interface AtomResponse {
 	};
 }
 
-export const InteractiveAtomUrl =
-	'https://content.guardianapis.com/atom/interactive/interactives/thrashers/2021/04/g200-article-banner/default?api-key=HOWDOIGETTHIS';
-
+let InteractiveAtomUrl: string | undefined;
 let AnniversaryAtomCache: InteractiveAtomBlockElement | undefined;
+
 export const getAnniversaryAtomCache = ():
 	| InteractiveAtomBlockElement
 	| undefined => AnniversaryAtomCache;
 
 const fetchAnniversaryAtom = (): Promise<void> => {
+	if (InteractiveAtomUrl === undefined) {
+		return Promise.reject(new Error('Interactive Atom URL not set'));
+	}
+
 	return fetch(InteractiveAtomUrl)
 		.then((rawResponse) => rawResponse.json())
 		.then((atom: AtomResponse) => {
 			const { id } = atom?.response?.interactive;
-
 			const {
 				css,
 				mainJS,
@@ -42,11 +48,10 @@ const fetchAnniversaryAtom = (): Promise<void> => {
 			} = atom?.response?.interactive?.data?.interactive;
 
 			if (css && mainJS && html && id) {
-				console.log('in here');
 				AnniversaryAtomCache = {
 					_type:
 						'model.dotcomrendering.pageElements.InteractiveAtomBlockElement',
-					url: InteractiveAtomUrl,
+					url: InteractiveAtomUrl || '',
 					css,
 					js: mainJS,
 					html,
@@ -57,12 +62,6 @@ const fetchAnniversaryAtom = (): Promise<void> => {
 				AnniversaryAtomCache = undefined;
 			}
 		});
-
-	// return fetch(InteractiveAtomUrl)
-	// 	.then((rawResponse) => rawResponse.json())
-	// 	.then((json) => {
-	// 		AnniversaryAtomCache = json;
-	// 	});
 };
 
 const oneMinute = 60_00;
@@ -70,10 +69,17 @@ const refreshAnniversaryAtom = () => {
 	setTimeout(() => {
 		fetchAnniversaryAtom()
 			.then(refreshAnniversaryAtom)
-			.catch((e) => console.error(`fetchAnniversaryAtom - error: ${e}`));
+			.catch((e) => console.log(`fetchAnniversaryAtom - error: ${e}`));
 	}, oneMinute * 2);
 };
 
-fetchAnniversaryAtom()
+getGuardianConfiguration('prod')
+	.then((config: GuardianConfiguration) => {
+		const key = config.getParameter('anniversaryAtomCAPIKey');
+		if (key) {
+			InteractiveAtomUrl = `https://content.guardianapis.com/atom/interactive/interactives/thrashers/2021/04/g200-article-banner/default?api-key=${key}`;
+		}
+	})
+	.then(fetchAnniversaryAtom)
 	.then(refreshAnniversaryAtom)
-	.catch((e) => console.error(`fetchIAnniversaryAtom - error: ${e}`));
+	.catch((e) => console.log(`fetchIAnniversaryAtom - error: ${e}`));
