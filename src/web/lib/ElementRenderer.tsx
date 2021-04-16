946c1cadfc2926beae0b808b0c3b6f423977d68a
Oliver Lloyd <email@oliverlloyd.com>
2021-04-16 09:12:44 +0100

    fix: Pass in `isMainMedia` so we know when not to use `role` here

--- src/web/lib/ElementRenderer.tsx
+++ src/web/lib/ElementRenderer.tsx
@@ -391,6 +391,7 @@ export const ElementRenderer = ({
 		case 'model.dotcomrendering.pageElements.InteractiveBlockElement':
 			return (
 				<Figure
+					isMainMedia={isMainMedia}
 					role={isLiveBlog ? 'inline' : element.role}
 					id={element.elementId}
 				>
@@ -700,6 +701,7 @@ export const ElementRenderer = ({
 		case 'model.dotcomrendering.pageElements.VineBlockElement':
 			return (
 				<Figure
+					isMainMedia={isMainMedia}
 					// No role given by CAPI
 					// eslint-disable-next-line jsx-a11y/aria-role
 					role="inline"
