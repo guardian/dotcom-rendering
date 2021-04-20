dca30b2e4821ae3ad992e9357c0b1c488c19f971
Gareth <gtrufitt@gmail.com>
2021-04-20 15:36:14 +0100

    If the user is opted into the variant of the ab test, do not show the atom

--- src/model/enhance-AnniversaryInteractiveAtom.ts
+++ src/model/enhance-AnniversaryInteractiveAtom.ts
@@ -2,15 +2,14 @@ import { getAnniversaryAtomCache } from '@root/src/web/server/cacheForAnniversar
 
 export const enhanceAnniversaryAtom = (data: CAPIType): CAPIType => {
 	// TODO Add switch
-	const {
-		anniversaryArticleHeader,
-		hideAnniversaryAtom,
-	} = data.config.switches;
+	const { anniversaryArticleHeader } = data.config.switches;
+
+	const { hideAnniversaryAtomVariant } = data.config.abTests;
 
 	// If the main anniversaryArticleHeader switch is ON
-	// and the AB test switch is NOT ON.
-	// Users will be opted-in to the AB Test IF THEY HIDE THE BANNER.
-	if (anniversaryArticleHeader && !hideAnniversaryAtom) {
+	// and the user is NOT in the AB Tests VARIANT.
+	// Users will be opted-in to VARIANT of the AB Test IF THEY HIDE THE BANNER.
+	if (anniversaryArticleHeader && hideAnniversaryAtomVariant !== 'variant') {
 		data.anniversaryInteractiveAtom = getAnniversaryAtomCache();
 	}
 
