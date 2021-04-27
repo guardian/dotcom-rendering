4c179949f01b0d9dc18158e22f914e860e1438e5
William Lacroix <william.j.t.lacroix@gmail.com>
2021-04-27 14:17:38 +0100

    DividerBlockElement spaceAbove prop support

--- src/web/components/elements/DividerBlockComponent.tsx
+++ src/web/components/elements/DividerBlockComponent.tsx
@@ -1,41 +1,45 @@
 import React from 'react';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 
 import { border } from '@guardian/src-foundations/palette';
 
 type Props = {
 	size?: 'full' | 'partial';
+	spaceAbove?: 'tight' | 'loose';
 };
-export const DividerBlockComponent = ({ size = 'partial' }: Props) => {
-	switch (size) {
-		case 'full':
-			return (
-				<hr
-					className={css`
-						width: 100%;
-						height: 1px;
-						border: 0;
-						margin-left: -10px;
-						margin-top: 48px;
-						margin-bottom: 3px;
-						background-color: ${border.secondary};
-					`}
-				/>
-			);
-		case 'partial':
-		default:
-			return (
-				<hr
-					className={css`
-						width: 150px;
-						height: 1px;
-						border: 0;
-						margin-left: -10px;
-						margin-top: 48px;
-						margin-bottom: 3px;
-						background-color: ${border.secondary};
-					`}
-				/>
-			);
-	}
-};
+
+const baseStyles = css`
+	height: 1px;
+	border: 0;
+	margin-left: -10px;
+	margin-bottom: 3px;
+	background-color: ${border.secondary};
+`;
+const sizeFullStyle = css`
+	width: 100%;
+`;
+const sizePartialStyle = css`
+	width: 150px;
+`;
+
+const tightSpaceAboveStyle = css`
+	margin-top: 3px;
+`;
+const looseSpaceAboveStyle = css`
+	margin-top: 48px;
+`;
+
+export const DividerBlockComponent = ({
+	size = 'partial',
+	spaceAbove = 'loose',
+}: Props) => (
+	<hr
+		className={cx(
+			baseStyles,
+			size === 'partial' ? sizePartialStyle : sizeFullStyle,
+			spaceAbove === 'loose'
+				? looseSpaceAboveStyle
+				: tightSpaceAboveStyle,
+		)}
+	/>
+);
