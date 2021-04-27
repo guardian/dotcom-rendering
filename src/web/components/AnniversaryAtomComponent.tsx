d0dca707d6f8983335f20f72022627a217e131c1
Gareth <gtrufitt@gmail.com>
2021-04-27 17:27:47 +0100

    Add spacing at the bottom of the atom

--- src/web/components/AnniversaryAtomComponent.tsx
+++ src/web/components/AnniversaryAtomComponent.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import { css } from 'emotion';
 import { InteractiveAtom } from '@guardian/atoms-rendering';
+import { from } from '@guardian/src-foundations/mq';
 
+const atomStyles = (
+	anniversaryInteractiveAtom: interactiveAtomBlockElement | undefined,
+) => css`
+	/* Temporarily support anniversary atom at top of articles */
+	/* stylelint-disable color-no-hex */
+	background-color: #ffe500;
+	display: ${anniversaryInteractiveAtom ? 'block' : 'none'};
+
+	${from.tablet} {
+		margin-bottom: 25px;
+		position: relative;
+
+		&:after {
+			content: '';
+			height: 25px;
+			background-color: transparent;
+			border-bottom: 1px solid #dcdcdc;
+			width: 100%;
+			display: block;
+			position: absolute;
+			bottom: -25px;
+		}
+	}
+`;
 export const AnniversaryAtomComponent = ({
 	anniversaryInteractiveAtom,
 }: {
 	anniversaryInteractiveAtom: InteractiveAtomBlockElement | undefined;
 }) => {
 	return (
-		<div
-			className={css`
-				/* Temporarily support anniversary atom at top of articles */
-				/* stylelint-disable color-no-hex */
-				background-color: #ffe500;
-				display: ${anniversaryInteractiveAtom ? 'block' : 'none'};
-			`}
-		>
+		<div className={atomStyles(anniversaryInteractiveAtom)}>
 			{anniversaryInteractiveAtom && (
 				<InteractiveAtom
 					html={anniversaryInteractiveAtom.html}
