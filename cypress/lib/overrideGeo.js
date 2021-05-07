694a334c6ae903ee376d6475884fea75d605b65f
Nicolas Long <nicolas.long@theguardian.com>
2021-05-07 17:45:35 +0100

    Fix prettier formatting errors

--- cypress/lib/overrideGeo.js
+++ cypress/lib/overrideGeo.js
@@ -1,6 +1,5 @@
-import { storage} from "@guardian/libs";
+import { storage } from '@guardian/libs';
 
 const KEY = 'gu.geo.override';
 
-export const overrideGeo = (geo) =>
-	storage.local.set(KEY, geo);
+export const overrideGeo = (geo) => storage.local.set(KEY, geo);
