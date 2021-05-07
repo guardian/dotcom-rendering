da68c8cabd68344132770cbe08cb7e767e506f65
Alex Sanders <a@snd.rs>
2021-05-07 12:02:28 +0100

    transpile `@guardian` packages in cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,7 +8,45 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+const webpackPreprocessor = require('cypress-webpack-preprocessor-v5');
+
 module.exports = (on, config) => {
 	config.env = { ...config.env, ...process.env };
+
+	const webpackConfig = {
+		// override default webpack config
+		// https://github.com/cypress-io/cypress/tree/master/npm/webpack-preprocessor#webpackoptions
+		webpackOptions: {
+			mode: 'development',
+			module: {
+				rules: [
+					{
+						test: /\.[jt]sx?|mjs$/,
+						exclude: {
+							and: [/node_modules/],
+							not: [
+								// Include all @guardian modules, except automat-modules
+								/@guardian\/(?!(automat-modules))/,
+
+								// Include the dynamic-import-polyfill
+								/dynamic-import-polyfill/,
+							],
+						},
+						use: [
+							{
+								loader: 'babel-loader',
+								options: {
+									presets: ['@babel/preset-env'],
+								},
+							},
+						],
+					},
+				],
+			},
+		},
+		watchOptions: {},
+	};
+
+	on('file:preprocessor', webpackPreprocessor(webpackConfig));
 	return config;
 };
