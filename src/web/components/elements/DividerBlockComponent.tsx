a0753e7543c928c742a7e15424f4dfb200807305
William Lacroix <william.j.t.lacroix@gmail.com>
2021-05-10 16:42:05 +0100

    Fix negative margin on divider

--- src/web/components/elements/DividerBlockComponent.tsx
+++ src/web/components/elements/DividerBlockComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { css, cx } from 'emotion';
 
+import { from } from '@guardian/src-foundations/mq';
 import { border } from '@guardian/src-foundations/palette';
 import { space } from '@guardian/src-foundations';
 
@@ -12,12 +13,19 @@ type Props = {
 const baseStyles = css`
 	height: 1px;
 	border: 0;
-	margin-left: -10px;
 	margin-bottom: 3px;
 	background-color: ${border.secondary};
 `;
 const sizeFullStyle = css`
 	width: 100%;
+	${from.tablet} {
+		margin-left: -20px;
+		width: calc(100% + 20px);
+	}
+	${from.leftCol} {
+		margin-left: -10px;
+		width: calc(100% + 10px);
+	}
 `;
 const sizePartialStyle = css`
 	width: 150px;
