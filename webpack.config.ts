fb7f33dcd5e83e87ca216a9ee63d14c371a14a17
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-05-13 15:44:21 +0100

    Removed use of Babel for React (#1306)
    - Removed `@babel/preset-react` usage from webpack
    - Removed `@babel/preset-react` and `@emotion/babel-plugin` from deps

--- webpack.config.ts
+++ webpack.config.ts
@@ -122,21 +122,6 @@ const serverConfig = (
 					test: /\.tsx?$/,
 					use: [
 						{
-							loader: 'babel-loader',
-							options: {
-								presets: [
-									[
-										'@babel/preset-react',
-										{
-											runtime: 'automatic',
-											importSource: '@emotion/core',
-										},
-									],
-								],
-								plugins: ['@emotion'],
-							},
-						},
-						{
 							loader: 'ts-loader',
 							options: {
 								configFile: 'config/tsconfig.server.json',
@@ -190,14 +175,6 @@ export const clientConfig: Configuration = {
 						options: {
 							presets: [
 								[
-									'@babel/preset-react',
-									{
-										runtime: 'automatic',
-										importSource: '@emotion/core',
-									},
-								],
-
-								[
 									'@babel/preset-env',
 									{
 										// Babel recommends installing corejs as a peer dependency
@@ -212,7 +189,6 @@ export const clientConfig: Configuration = {
 									},
 								],
 							],
-							plugins: ['@emotion'],
 						},
 					},
 					{
