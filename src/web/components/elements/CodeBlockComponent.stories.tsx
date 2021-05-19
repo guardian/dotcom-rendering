bc8c0110bc4649879aea0d8d89259cb9e9ba48a5
Pascal Honoré <pascal@alseyn.net>
2021-05-19 14:48:23 +0100

    Update code sample in .stories

--- src/web/components/elements/CodeBlockComponent.stories.tsx
+++ src/web/components/elements/CodeBlockComponent.stories.tsx
@@ -11,18 +11,11 @@ export default {
 
 export const CodeStory = () => {
 	const code = `
-export const CodeBlockComponent = ({
-	code,
-	language = 'text',
-}: Props) => {
-	return (
-		<pre className={codeStyles}>
-			<code>
-				<div dangerouslySetInnerHTML={{ __html: code }} />
-			</code>
-		</pre>
-	);
-};
+wget https://github.com/buger/goreplay/releases/download/v0.16.0.2/gor_0.16.0_x64.tar.gz
+
+tar -xzf gor_0.16.0_x64.tar.gz gor
+
+sudo gor --input-raw :80 --output-http http://apiv2.code.co.uk
     `;
 	return (
 		<ContainerLayout>
