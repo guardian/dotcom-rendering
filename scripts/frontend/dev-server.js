5b7b6c18209545645fb39694fb9f22b1efcf37c6
Pascal Honoré <pascal@alseyn.net>
2021-06-03 09:58:22 +0100

    Throw error when url query parameters is missing

--- scripts/frontend/dev-server.js
+++ scripts/frontend/dev-server.js
@@ -13,6 +13,12 @@ const bodyParser = require('body-parser');
 const { siteName, root } = require('./config');
 
 function buildUrlFromQueryParam(req) {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	if (!req.query.url) {
+		throw new Error('The url query parameter is mandatory');
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 	const url = new URL(req.query.url);
 	// searchParams will only work for the first set of query params because 'url' is already a query param itself
 	const searchparams = url.searchParams && url.searchParams.toString();
