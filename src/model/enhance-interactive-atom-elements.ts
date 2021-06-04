9c3fd21187d668a9fc4effeb4a8850ca3d4c3231
Olly <9575458+OllysCoding@users.noreply.github.com>
2021-06-04 12:33:58 +0100

    Refactor interactive atom block element cleaning to use an enhancer

---
+++ src/model/enhance-interactive-atom-elements.ts
@@ -0,0 +1,33 @@
+import { sanitiseHTML } from '@root/src/model/clean';
+
+// Some interactives contain HTML with unclosed tags etc. To
+// preserve (approximate) parity with Frontend we perform some basic
+// cleaning.
+const enhance = (elements: CAPIElement[]): CAPIElement[] => {
+	elements.map((element) => {
+		if (
+			element._type ===
+				'model.dotcomrendering.pageElements.InteractiveAtomBlockElement'
+		)
+			element.html = element.html
+				? sanitiseHTML(element.html)
+				: element.html;
+		return element
+	});
+
+	return elements;
+};
+
+export const enhanceInteractiveAtomElements = (data: CAPIType): CAPIType => {
+	const enhancedBlocks = data.blocks.map((block: Block) => {
+		return {
+			...block,
+			elements: enhance(block.elements),
+		};
+	});
+
+	return {
+		...data,
+		blocks: enhancedBlocks,
+	} as CAPIType;
+};
