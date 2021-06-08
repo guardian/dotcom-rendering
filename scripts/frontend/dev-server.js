2e45fda4143eabdbd3fdff7b171e6ecc1174aedf
Olly <9575458+OllysCoding@users.noreply.github.com>
2021-06-08 10:24:48 +0100

    fix dev server endpoint for AMPInteractives

--- scripts/frontend/dev-server.js
+++ scripts/frontend/dev-server.js
@@ -167,10 +167,7 @@ const go = () => {
 		'/AMPInteractive',
 		async (req, res, next) => {
 			try {
-				const url = buildUrlFromQueryParam(
-					req,
-					defaultAmpInteractiveURL,
-				);
+				const url = buildUrlFromQueryParam(req);
 				const { html, ...config } = await fetch(
 					ampifyUrl(url),
 				).then((article) => article.json());
