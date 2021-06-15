5b30acdea77a52c998315ac9c5e4b4cb89cd8740
Nicolas Long <nicolas.long@theguardian.com>
2021-06-15 15:15:53 +0100

    Fix class selector for interactive legacy styling

--- src/web/layouts/lib/interactiveLegacyStyling.ts
+++ src/web/layouts/lib/interactiveLegacyStyling.ts
@@ -21,7 +21,7 @@ export const interactiveGlobalStyles = css`
 
 	/* There is room for better solution where we don't have to load global styles onto the body.
 		For now this works, but we shouldn't support for it for newly made interactives. */
-	${interactiveLegacyClasses.contentInteractive} {
+	.${interactiveLegacyClasses.contentInteractive} {
 		margin-bottom: 1rem;
 
 		/* stylelint-disable */
