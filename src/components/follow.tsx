656fe25d4aff72349d4bd8710f956c319466a63b
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2021-06-24 14:51:53 +0100

    address pr review

--- src/components/follow.tsx
+++ src/components/follow.tsx
@@ -2,6 +2,8 @@
 
 import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
+import { remSpace } from '@guardian/src-foundations';
+import { neutral } from '@guardian/src-foundations/palette';
 import { textSans } from '@guardian/src-foundations/typography';
 import { Special } from '@guardian/types';
 import type { Format } from '@guardian/types';
@@ -17,6 +19,10 @@ interface Props extends Format {
 	contributors: Contributor[];
 }
 
+interface IconProps {
+	format: Format;
+}
+
 const styles = (kicker: string, inverted: string): SerializedStyles => css`
 	${textSans.small()}
 	color: ${kicker};
@@ -26,27 +32,6 @@ const styles = (kicker: string, inverted: string): SerializedStyles => css`
 	background: none;
 	margin-left: 0;
 	margin-top: 4px;
-
-	svg {
-		width: 24px;
-		height: 24px;
-		margin-bottom: -6px;
-		circle {
-			fill: ${kicker};
-		}
-		path {
-			fill: #fff;
-		}
-
-		${darkModeCss`
-		circle {
-			fill: ${inverted};
-		}
-		path {
-			fill: #121212;
-		}
-		`}
-	}
 
 	${darkModeCss`
         color: ${inverted};
@@ -59,16 +44,42 @@ function getStyles({ theme }: Format): SerializedStyles {
 	return styles(kicker, inverted);
 }
 
-const FollowIcon: FC = () => (
-	<svg xmlns="http://www.w3.org/2000/svg">
-		<circle cx="12" cy="12" r="12" />
-		<path
-			fillRule="evenodd"
-			clipRule="evenodd"
-			d="M11.167 9.833c1.216 0 2.65-1.366 2.65-3.033 0-1.667-.984-2.633-2.65-2.633-1.667 0-2.634.966-2.634 2.633s1.55 3.033 2.634 3.033zm6.5 3.667h-1v-2.167H14.5v-1h2.167V8.167h1v2.166h2.166v1h-2.166V13.5zm-6.5-2.333c.716 0 1.4.05 2.016.166.184 1.734 1.55 3.134 3.25 3.434l.734 2.733-.684.667H5.817l-.65-.667 1.316-5 .684-.683c1.333-.45 2.533-.65 4-.65z"
-		/>
-	</svg>
-);
+const FollowIcon: FC<IconProps> = ({ format }) => {
+	const iconStyles = ({ theme }: Format): SerializedStyles => {
+		const { kicker, inverted } = getThemeStyles(theme);
+		return css`
+			width: ${remSpace[6]};
+			height: ${remSpace[6]};
+			margin-bottom: -0.375rem;
+			circle {
+				fill: ${kicker};
+			}
+			path {
+				fill: #fff;
+			}
+
+			${darkModeCss`
+		circle {
+			fill: ${inverted};
+		}
+		path {
+			fill: ${neutral[7]};
+		}
+		`}
+		`;
+	};
+
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" css={iconStyles(format)}>
+			<circle cx="12" cy="12" r="12" />
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M11.167 9.833c1.216 0 2.65-1.366 2.65-3.033 0-1.667-.984-2.633-2.65-2.633-1.667 0-2.634.966-2.634 2.633s1.55 3.033 2.634 3.033zm6.5 3.667h-1v-2.167H14.5v-1h2.167V8.167h1v2.166h2.166v1h-2.166V13.5zm-6.5-2.333c.716 0 1.4.05 2.016.166.184 1.734 1.55 3.134 3.25 3.434l.734 2.733-.684.667H5.817l-.65-.667 1.316-5 .684-.683c1.333-.45 2.533-.65 4-.65z"
+			/>
+		</svg>
+	);
+};
 
 const Follow: FC<Props> = ({ contributors, ...format }) => {
 	const [contributor] = contributors;
@@ -86,7 +97,7 @@ const Follow: FC<Props> = ({ contributors, ...format }) => {
 				data-display-name={contributor.name}
 			>
 				<span className="js-status">
-					<FollowIcon /> Follow{' '}
+					<FollowIcon format={format} /> Follow{' '}
 				</span>
 				{contributor.name}
 			</button>
