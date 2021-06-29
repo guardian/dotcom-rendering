38fa5cd7cfff78c8b6d27b5865148630699a0b39
Max Duval <max.duval@guardian.co.uk>
2021-06-29 15:02:03 +0100

    add link to frontend ab-tests file

--- src/web/experiments/ab-tests.ts
+++ src/web/experiments/ab-tests.ts
@@ -8,6 +8,8 @@ import {
 } from '@frontend/web/experiments/tests/newsletter-merch-unit-test';
 import { signInGateUsMandatory } from '@root/src/web/experiments/tests/sign-in-gate-us-mandatory';
 
+// keep in sync with ab-tests in frontend
+// https://github.com/guardian/frontend/tree/main/static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
 export const tests: ABTest[] = [
 	abTestTest,
 	signInGateMainVariant,
