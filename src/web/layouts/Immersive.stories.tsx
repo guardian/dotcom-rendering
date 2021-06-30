4701c1f3a7217560bd37556851994cc6b8af6843
Gareth <gtrufitt@gmail.com>
2021-06-30 17:31:00 +0100

    Move hydration state logic to encapsulate

--- src/web/layouts/Immersive.stories.tsx
+++ src/web/layouts/Immersive.stories.tsx
@@ -29,6 +29,7 @@ import { embedIframe } from '@root/src/web/browser/embedIframe/embedIframe';
 import { mockRESTCalls } from '@root/src/web/lib/mockRESTCalls';
 
 import { extractNAV } from '@root/src/model/extract-nav';
+import { fireAndResetHydrationState } from '@root/src/web/components/HydrateOnce';
 import { DecideLayout } from './DecideLayout';
 
 mockRESTCalls();
@@ -67,10 +68,7 @@ const convertToImmersive = (CAPI: CAPIType) => ({
 // the client. We need a separate component so that we can make use of useEffect to ensure
 // the hydrate step only runs once the dom has been rendered.
 const HydratedLayout = ({ ServerCAPI }: { ServerCAPI: CAPIType }) => {
-	const event = document.createEvent('HTMLEvents');
-	event.initEvent('storybook:reset', true, true);
-	window.dispatchEvent(event);
-
+	fireAndResetHydrationState();
 	const NAV = extractNAV(ServerCAPI.nav);
 	useEffect(() => {
 		const CAPI = makeGuardianBrowserCAPI(ServerCAPI);
