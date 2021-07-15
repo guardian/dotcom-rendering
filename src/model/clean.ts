65a16263ae3a3762bc2ce1739053cf40bb7cbf4f
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2021-07-15 13:21:50 +0100

    Fix tsc error

--- src/model/clean.ts
+++ src/model/clean.ts
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import compose from 'compose-function';
 import createDOMPurify from 'dompurify';
 import { JSDOM } from 'jsdom';
@@ -19,6 +20,6 @@ export const clean = compose(bigBullets, (s: string) =>
 	}),
 );
 
-export const sanitiseHTML = (html: string, opts?: any): string =>
+export const sanitiseHTML = (html: string | ReactNode, opts?: any): string =>
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 	DOMPurify.sanitize(html, opts);
