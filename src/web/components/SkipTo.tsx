152bc6ea5aa948536348bee90a34466d1afed1ac
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-07-27 09:21:13 +0100

    refactor: Removed unused fragment

--- src/web/components/SkipTo.tsx
+++ src/web/components/SkipTo.tsx
@@ -9,36 +9,34 @@ type Props = {
 
 export const SkipTo = ({ id, label }: Props) => {
 	return (
-		<>
-			<a
-				href={id}
-				css={css`
-					${textSans.medium()}
-					height: 40px;
-					top: -40px;
-					line-height: 30px;
-					overflow: hidden;
-					padding: 0;
-					position: absolute;
-					background: ${neutral[100]};
-					display: block;
-					text-align: center;
-					margin: 0;
-					text-decoration: none;
+		<a
+			href={id}
+			css={css`
+				${textSans.medium()}
+				height: 40px;
+				top: -40px;
+				line-height: 30px;
+				overflow: hidden;
+				padding: 0;
+				position: absolute;
+				background: ${neutral[100]};
+				display: block;
+				text-align: center;
+				margin: 0;
+				text-decoration: none;
+				color: ${neutral[0]};
+				&:focus,
+				&:active {
+					border: 5px solid ${border.focusHalo};
+					position: static;
+				}
+				&:visited,
+				&:active {
 					color: ${neutral[0]};
-					&:focus,
-					&:active {
-						border: 5px solid ${border.focusHalo};
-						position: static;
-					}
-					&:visited,
-					&:active {
-						color: ${neutral[0]};
-					}
-				`}
-			>
-				{label}
-			</a>
-		</>
+				}
+			`}
+		>
+			{label}
+		</a>
 	);
 };
