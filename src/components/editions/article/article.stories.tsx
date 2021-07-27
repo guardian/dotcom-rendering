44a960178fa1e324cc48f3c844e71a35facb5f92
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2021-07-27 14:45:14 +0100

    settle for 0.4 tolerance

--- src/components/editions/article/article.stories.tsx
+++ src/components/editions/article/article.stories.tsx
@@ -176,7 +176,7 @@ export default {
 	parameters: {
 		layout: 'fullscreen',
 		chromatic: {
-			diffThreshold: 0.35,
+			diffThreshold: 0.4,
 			viewports: [breakpoints.mobile, breakpoints.tablet],
 		},
 	},
