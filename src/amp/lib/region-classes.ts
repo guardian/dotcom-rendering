70f87137fa0a05651c4ad44af1cda53cf7ca5311
Chris Jones <christopher.jones@guardian.co.uk>
2021-07-28 11:52:02 +0100

    Add jsdocs

--- src/amp/lib/region-classes.ts
+++ src/amp/lib/region-classes.ts
@@ -1,23 +1,42 @@
 import { css } from '@emotion/react';
 
+/**
+ * Class that will display an element if the user accesses the AMP page
+ * from Great Britain
+ */
 const ukRegionClass = css`
 	.amp-iso-country-gb & {
 		display: block;
 	}
 `;
 
+/**
+ * Class that will display an element if the user accesses the AMP page from US
+ */
 const usRegionClass = css`
 	.amp-iso-country-us & {
 		display: block;
 	}
 `;
 
+/**
+ * Class that will display an element if the user accesses the AMP page
+ * from Australia
+ */
 const auRegionClass = css`
 	.amp-iso-country-au & {
 		display: block;
 	}
 `;
 
+/**
+ * Class that will display an element if the user accesses the AMP page from
+ * the region denoted as "Rest of World"
+ *
+ * Currently this region is set to any access either from the European Economic
+ * Area or where no AMP geo group is assigned
+ *
+ */
 const rowRegionClass = css`
 	.amp-geo-group-eea & {
 		display: block;
@@ -27,6 +46,13 @@ const rowRegionClass = css`
 	}
 `;
 
+/**
+ * Class that will display an element if the user accesses the AMP page from
+ * the region denoted as "International"
+ *
+ * Currently this region is set to any access from the European Economic
+ * Area EXCEPT Great Britain or where no AMP geo group is assigned
+ */
 const internationalRegionClass = css`
 	.amp-geo-group-eea:not(.amp-iso-country-gb) &,
 	.amp-geo-no-group & {
@@ -34,10 +60,15 @@ const internationalRegionClass = css`
 	}
 `;
 
+/**
+ * Class that can be applied to all regions to ensure that when not in the
+ * correct AMP region all other regions aren't displayed
+ */
 export const noDisplayClass = css`
 	display: none;
 `;
 
+/** Dictionary mapping region code to the associated AMP region style */
 export const regionClasses = {
 	UK: ukRegionClass,
 	US: usRegionClass,
