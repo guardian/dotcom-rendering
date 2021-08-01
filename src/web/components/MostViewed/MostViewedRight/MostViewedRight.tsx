5c8a641c7f49fcc86e633eec4324d96e779dc466
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-08-01 21:41:31 +0100

    feat: Stick MostViewed when there are no ads

--- src/web/components/MostViewed/MostViewedRight/MostViewedRight.tsx
+++ src/web/components/MostViewed/MostViewedRight/MostViewedRight.tsx
@@ -4,6 +4,7 @@ import { headline } from '@guardian/src-foundations/typography';
 import { Lines } from '@guardian/src-ed-lines';
 
 import { useApi } from '@root/src/web/lib/useApi';
+import { useAdBlockInUse } from '@root/src/web/lib/useAdBlockInUse';
 import { decideTrail } from '@root/src/web/lib/decideTrail';
 
 import { MostViewedRightItem } from './MostViewedRightItem';
@@ -13,6 +14,13 @@ const wrapperStyles = css`
 	margin-bottom: 24px;
 `;
 
+// We only stick most viewed when an ads are not showing. We do this
+// to maximise right column space
+const stickyStyles = css`
+	position: sticky;
+	top: 0;
+`;
+
 const headingStyles = css`
 	${headline.xxxsmall({ fontWeight: 'bold' })}
 	margin-bottom: 8px;
@@ -20,9 +28,12 @@ const headingStyles = css`
 
 interface Props {
 	limitItems?: number;
+	isSignedIn?: boolean;
 }
 
-export const MostViewedRight = ({ limitItems = 5 }: Props) => {
+export const MostViewedRight = ({ limitItems = 5, isSignedIn }: Props) => {
+	const adBlockerDetected = useAdBlockInUse();
+
 	const endpointUrl: string =
 		'https://api.nextgen.guardianapps.co.uk/most-read-geo.json?dcr=true';
 	const { data, error } = useApi<CAPITrailTabType>(endpointUrl);
@@ -36,9 +47,13 @@ export const MostViewedRight = ({ limitItems = 5 }: Props) => {
 		const trails: TrailType[] = data.trails
 			.map(decideTrail)
 			.slice(0, limitItems);
+		const stickToTop = adBlockerDetected || isSignedIn;
 		// Look I don't know why data-component is geo-most-popular either, but it is, ok? Ok.
 		return (
-			<div css={wrapperStyles} data-component="geo-most-popular">
+			<div
+				css={[wrapperStyles, stickToTop && stickyStyles]}
+				data-component="geo-most-popular"
+			>
 				<Lines count={4} effect="straight" />
 				<h3 css={headingStyles}>Most viewed</h3>
 				<ul data-link-name="Right hand most popular geo GB">
