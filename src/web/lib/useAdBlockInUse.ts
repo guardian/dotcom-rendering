bc7af4b98406969a65fa5328dd2b9c9b2a8674f1
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-08-02 09:36:48 +0100

    feat: Abstract ad block detection so it's available everywhere

---
+++ src/web/lib/useAdBlockInUse.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+import { isAdBlockInUse } from '@root/src/web/lib/detectAdBlocker';
+
+/**
+ * @description
+ * useAdBlockInUse provides a custom hook to integrate the isAdBlockInUse
+ * promise into a react component
+ * */
+export const useAdBlockInUse = () => {
+	const [isInUse, setIsInUse] = useState<boolean | undefined>();
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-floating-promises
+		isAdBlockInUse().then((blockerDetected) => {
+			// We're using react state here to trigger the rerender, but only
+			// if a blocker was detected
+			if (blockerDetected) setIsInUse(true);
+		});
+	}, []);
+
+	return isInUse;
+};
