a9725e22d099b0cf495e97db370484e401ac57a3
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2021-08-03 16:31:46 +0100

    Remove Sampling

--- src/web/browser/coreVitals/init.ts
+++ src/web/browser/coreVitals/init.ts
@@ -3,12 +3,8 @@ import { startup } from '@root/src/web/browser/startup';
 import { coreVitals } from './coreVitals';
 
 const init = async (): Promise<void> => {
-	// Sample 1 out of 100
-	const inSample = Math.floor(Math.random() * 100);
-	const inLocal = window.location.hostname === 'localhost';
-	if (inSample === 1 || inLocal) {
-		coreVitals();
-	}
+	coreVitals();
+
 	return Promise.resolve();
 };
 
