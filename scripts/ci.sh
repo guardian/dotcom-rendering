d432ea65ec121c2faff84b4c40d51985ebd4dbf4
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2021-08-10 16:46:52 +0100

    Add Bash Script For Team City Build

---
+++ scripts/ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ~/.nvm/nvm.sh
+nvm install
+nvm use
+#Code Validation
+echo bundlesize token $BUNDLESIZE_GITHUB_TOKEN
+make validate-ci
+
+#Cypress Tests
+# see https://docs.cypress.io/guides/guides/continuous-integration.html#Advanced-setup
+# apt-get install xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
+make cypress
+
+#RiffRaff publish
+make riffraff-publish
