280723ecc5fdf170834d4ad6e924f2f13c081dbb
Anna Beddow <anna.beddow@gmail.com>
2021-08-12 11:40:12 +0100

    Add check for contributor before layout change

--- src/components/editions/headline/headline.stories.tsx
+++ src/components/editions/headline/headline.stories.tsx
@@ -1,7 +1,8 @@
 // ----- Imports ----- //
 
-import { Display, Pillar } from '@guardian/types';
+import { Display, none, Pillar, some } from '@guardian/types';
 import { boolean, withKnobs } from '@storybook/addon-knobs';
+import type { Contributor } from 'contributor';
 import {
 	analysis,
 	article,
@@ -11,11 +12,44 @@ import {
 	media,
 	review,
 } from 'fixtures/item';
+import type { Image } from 'image';
 import type { ReactElement } from 'react';
 import { selectPillar } from 'storybookHelpers';
 import Headline from '.';
 
 // ----- Setup ------ //
+
+const srcset =
+	'https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=32&quality=85&fit=bounds&s=100fc280274e40946afb34d4b561ce9f 32w, https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=64&quality=85&fit=bounds&s=b6b6831a7a599a815002b8a4c041342e 64w, https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=128&quality=85&fit=bounds&s=35b6ce614cae19fbdcdefa55a670eda5 128w, https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=192&quality=85&fit=bounds&s=930a05d87b62a1f613ff76f3ee0c97a0 192w, https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=256&quality=85&fit=bounds&s=8c44b90de342114bd3bf6145767d4b31 256w, https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=400&quality=85&fit=bounds&s=8491504dfb944eee7ef173e739cb4f74 400w, https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=600&quality=85&fit=bounds&s=668fc2d7278f6c4a553f806c9b2d47d3 600w';
+
+const image: Image = {
+	src:
+		'https://i.guim.co.uk/img/uploads/2017/10/06/Emma-Brockes,-L.png?width=64&quality=85&fit=bounds&s=b6b6831a7a599a815002b8a4c041342e',
+	srcset: srcset,
+	dpr2Srcset: srcset,
+	alt: some('image'),
+	width: 550,
+	height: 550,
+	role: 1,
+	caption: none,
+	nativeCaption: none,
+	credit: none,
+};
+
+const contributors: Contributor[] = [
+	{
+		id: 'emmabrockes',
+		name: 'Emma Bookes',
+		image: some(image),
+		apiUrl: '',
+	},
+];
+
+const hasContributor = (): { contributors: Contributor[] } => {
+	return {
+		contributors: boolean('Contributors', true) ? contributors : [],
+	};
+};
 
 const isImmersive = (): { display: Display } => {
 	return {
@@ -92,6 +126,7 @@ const Comment = (): ReactElement => (
 		item={{
 			...comment,
 			...isImmersive(),
+			...hasContributor(),
 			theme: selectPillar(Pillar.News),
 		}}
 	/>
