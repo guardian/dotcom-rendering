0ac6efaf2f93c7551f03b89f2b4e08cc3bb5796e
Ravi Lal <arelra@users.noreply.github.com>
2021-08-20 16:27:25 +0100

    Ad targeting should handle adfree user test

--- dotcom-rendering/src/lib/ad-targeting.test.ts
+++ dotcom-rendering/src/lib/ad-targeting.test.ts
@@ -30,36 +30,36 @@ const CAPI = {
 	},
 };
 
-describe('buildAdTargeting', () => {
-	const expectedAdTargeting = ({isSignedIn}: {isSignedIn: string}) => ({
-		adUnit: '/59666047/theguardian.com/money/article/ng',
-		customParams: {
-			cc: 'UK',
-			co: ['rob-davies'],
-			ct: 'article',
-			edition: 'uk',
-			inskin: 'f',
-			k: [
-				'ticket-prices',
-				'consumer-affairs',
-				'internet',
-				'viagogo',
-				'money',
-			],
-			p: 'ng',
-			pa: 'f',
-			s: 'money',
-			sh: 'https://theguardian.com/p/64ak8',
-			su: ['0'],
-			tn: ['news'],
-			url:
-				'/money/2017/mar/10/ministers-to-criminalise-use-of-ticket-tout-harvesting-software',
-			sens: 'f',
-			si: isSignedIn,
-			vl: 0,
-		},
-	});
+const expectedAdTargeting = ({isSignedIn}: {isSignedIn: string}) => ({
+	adUnit: '/59666047/theguardian.com/money/article/ng',
+	customParams: {
+		cc: 'UK',
+		co: ['rob-davies'],
+		ct: 'article',
+		edition: 'uk',
+		inskin: 'f',
+		k: [
+			'ticket-prices',
+			'consumer-affairs',
+			'internet',
+			'viagogo',
+			'money',
+		],
+		p: 'ng',
+		pa: 'f',
+		s: 'money',
+		sh: 'https://theguardian.com/p/64ak8',
+		su: ['0'],
+		tn: ['news'],
+		url:
+			'/money/2017/mar/10/ministers-to-criminalise-use-of-ticket-tout-harvesting-software',
+		sens: 'f',
+		si: isSignedIn,
+		vl: 0,
+	},
+});
 
+describe('buildAdTargeting', () => {
 	it('builds adTargeting correctly', () => {
 		expect(buildAdTargeting(CAPI)).toEqual(expectedAdTargeting({isSignedIn: 'f'}));
 	});
@@ -68,5 +68,11 @@ describe('buildAdTargeting', () => {
 		addCookie('GU_U', 'something');
 		expect(buildAdTargeting(CAPI)).toEqual(expectedAdTargeting({isSignedIn: 't'}));
 		removeCookie('GU_U');
+	});
+
+	it('builds adTargeting correctly for an ad free user', () => {
+		const CAPIAdFree = { ...CAPI, ...{ isAdFreeUser: true }};
+		const expectedAdTargetingAdFree = { disableAds: true };
+		expect(buildAdTargeting(CAPIAdFree)).toEqual(expectedAdTargetingAdFree);
 	});
 });
