140094dbbf45cb5c98ed76273593fd252d300993
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-08-27 14:51:05 +0100

    Introduced a `common-rendering` project
    - Added yarn workspaces to manage multiple projects
    - Removed DCR's `yarn.lock` in favour of the top-level workspaces one
    - Added some example files for the common project
    - Added tsconfig to keep editors happy
    - Updated DCR's `install` script to use top-level yarn

---
+++ common-rendering/src/components/example.stories.tsx
@@ -0,0 +1,25 @@
+// ----- Imports ----- //
+
+import type { FC } from 'react';
+import Example from './example';
+
+// ----- Stories ----- //
+
+const Default: FC = () =>
+    <Example
+        person={{
+            firstName: 'CP',
+            lastName: 'Scott',
+        }}
+    />
+
+// ----- Exports ----- //
+
+export default {
+    component: Example,
+    title: 'Common/Components/Example',
+}
+
+export {
+    Default,
+}
