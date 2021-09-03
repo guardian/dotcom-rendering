c1a8152a82550108dec0814c4061066284798c35
Joe Cowton <joe.cowton@guardian.co.uk>
2021-09-03 09:57:23 +0100

    LIVE-2953: swap undefined for empty string

--- apps-rendering/src/server/footballContent.ts
+++ apps-rendering/src/server/footballContent.ts
@@ -95,7 +95,7 @@ const makeFootballContent = (
 const scorerParser: Parser<Scorer> = map3(makeScorer)(
 	fieldParser('player', stringParser),
 	fieldParser('timeInMinutes', numberParser),
-	oneOf([fieldParser('additionalInfo', stringParser), succeed(undefined)]),
+	oneOf([fieldParser('additionalInfo', stringParser), succeed('')]),
 );
 
 const footballTeamParser: Parser<FootballTeam> = map6(makeFootballTeam)(
@@ -116,7 +116,7 @@ const footballContentParser: Parser<FootballContent> = map7(
 	fieldParser('competitionDisplayName', stringParser),
 	fieldParser('homeTeam', footballTeamParser),
 	fieldParser('awayTeam', footballTeamParser),
-	oneOf([fieldParser('venue', stringParser), succeed(undefined)]),
+	oneOf([fieldParser('venue', stringParser), succeed('')]),
 );
 
 const footballContentParserFor = (
