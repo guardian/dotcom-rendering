90f23204fd3c1685608547047e362fa1e1a26eea
Chris Jones <christopher.jones@guardian.co.uk>
2021-09-06 11:42:43 +0100

    Add some clarifying comments

--- dotcom-rendering/src/amp/components/Ad.tsx
+++ dotcom-rendering/src/amp/components/Ad.tsx
@@ -26,9 +26,24 @@ const ampData = (section: string, contentType: string): string => {
 	return `/${dfpAccountId}/${dfpAdUnitRoot}/amp`;
 };
 
+/**
+ * Determine the Placement ID that is used to look up a given stored bid request
+ *
+ * Stored bid requests are stored by the prebid server instance and each is
+ * keyed by a placement ID. This placement ID corresponds to the tag id parameter
+ * provided on the client
+ *
+ * @param isSticky Whether the ad is sticky - sticky ads have stored bid requests
+ * containing different SSP ids to non-sticky ads
+ * @param adRegion The advertising region - different regions are covered by different
+ * stored bid requests
+ * @returns The placement id for an ad, depending on its ad region and whether
+ * it is sticky
+ */
 const getPlacementId = (isSticky: boolean, adRegion: AdRegion): number => {
 	switch (adRegion) {
 		case 'US': {
+			// In the US use different placement IDs depending on whether ad is sticky
 			return isSticky ? 22138171 : 7;
 		}
 		case 'AU':
@@ -48,8 +63,10 @@ const realTimeConfig = (
 	const preBidServerPrefix = 'https://prebid.adnxs.com/pbs/v1/openrtb2/amp';
 	const permutiveURL =
 		'https://guardian.amp.permutive.com/rtc?type=doubleclick';
-
 	const prebidURL = [
+		// The tag_id in the URL is used to look up the bulk of the request
+		// In this case it corresponds to the placement ID of the bid requests
+		// on the prebid server
 		`${preBidServerPrefix}?tag_id=${placementID}`,
 		'w=ATTR(width)',
 		'h=ATTR(height)',
