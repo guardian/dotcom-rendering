293b974910c1c2238ec8a4998b3c447f78c44762
Olly <9575458+OllysCoding@users.noreply.github.com>
2021-09-06 12:19:47 +0100

    Temporarily remove reset css from storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,15 @@
-import isChromatic from 'chromatic/isChromatic';
-import MockDate from 'mockdate';
+import isChromatic from "chromatic/isChromatic";
+import MockDate from "mockdate";
 
-import { getFontsCss } from '@root/src/lib/fonts-css';
+import { getFontsCss } from "@root/src/lib/fonts-css";
 
-import { defaults } from './default-css';
+// import { defaults } from './default-css';
 
-import 'reset-css';
+// import 'reset-css';
 
-import { Lazy } from '@root/src/web/components/Lazy';
-import { Picture } from '@root/src/web/components/Picture';
-import { mockRESTCalls } from '@root/src/web/lib/mockRESTCalls';
+import { Lazy } from "@root/src/web/components/Lazy";
+import { Picture } from "@root/src/web/components/Picture";
+import { mockRESTCalls } from "@root/src/web/lib/mockRESTCalls";
 
 // Prevent components being lazy rendered when we're taking Chromatic snapshots
 Lazy.disabled = isChromatic();
@@ -17,67 +17,67 @@ Picture.disableLazyLoading = isChromatic();
 
 if (isChromatic()) {
 	// Fix the date to prevent false negatives
-	MockDate.set('Sun Jan 10 2021 12:00:00 GMT+0000 (Greenwich Mean Time)');
+	MockDate.set("Sun Jan 10 2021 12:00:00 GMT+0000 (Greenwich Mean Time)");
 }
 
 mockRESTCalls();
 
 // Add base css for the site
 let css = `${getFontsCss()}${defaults}`;
-let head = document.getElementsByTagName('head')[0];
-let style = document.createElement('style');
+let head = document.getElementsByTagName("head")[0];
+let style = document.createElement("style");
 head.appendChild(style);
-style.type = 'text/css';
+style.type = "text/css";
 style.appendChild(document.createTextNode(css));
 
 const guardianViewports = {
 	mobileMedium: {
-		name: 'mobileMedium',
+		name: "mobileMedium",
 		styles: {
-			width: '375px',
-			height: '800px',
+			width: "375px",
+			height: "800px",
 		},
 	},
 	mobileLandscape: {
-		name: 'mobileLandscape',
+		name: "mobileLandscape",
 		styles: {
-			width: '480px',
-			height: '800px',
+			width: "480px",
+			height: "800px",
 		},
 	},
 	phablet: {
-		name: 'phablet',
+		name: "phablet",
 		styles: {
-			width: '660px',
-			height: '800px',
+			width: "660px",
+			height: "800px",
 		},
 	},
 	tablet: {
-		name: 'tablet',
+		name: "tablet",
 		styles: {
-			width: '740px',
-			height: '800px',
+			width: "740px",
+			height: "800px",
 		},
 	},
 	desktop: {
-		name: 'desktop',
+		name: "desktop",
 		styles: {
-			width: '980px',
-			height: '800px',
+			width: "980px",
+			height: "800px",
 		},
 	},
 	leftCol: {
-		name: 'leftCol',
+		name: "leftCol",
 		styles: {
-			width: '1140px',
-			height: '800px',
+			width: "1140px",
+			height: "800px",
 		},
 	},
 	wide: {
-		name: 'wide',
+		name: "wide",
 		styles: {
-			width: '1300px',
-			height: '800px',
+			width: "1300px",
+			height: "800px",
 		},
 	},
 };
@@ -87,7 +87,7 @@ export const viewports = [375, 480, 660, 740, 980, 1140, 1300];
 export const parameters = {
 	viewport: {
 		viewports: guardianViewports,
-		defaultViewport: 'wide',
+		defaultViewport: "wide",
 	},
-	layout: 'fullscreen',
+	layout: "fullscreen",
 };
