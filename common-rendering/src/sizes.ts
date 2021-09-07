47d6931b651d83f632afa9cf385b5a86397c9433
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-09-07 17:39:29 +0100

    Migrate `image-rendering` into `common-rendering`
    - Added testing dependencies and `@guardian/types`
    - Allowed Jest transformation of guardian packages
    - Tweaked `tsconfig` for import of guardian packages JS and test deps
    - Added the `image-rendering` files, components, stories and tests

---
+++ common-rendering/src/sizes.ts
@@ -0,0 +1,63 @@
+// ----- Imports ----- //
+
+import type { SerializedStyles } from "@emotion/react";
+import { css } from "@emotion/react";
+import type { Breakpoint } from "@guardian/src-foundations/mq";
+import { breakpoints, from } from "@guardian/src-foundations/mq";
+
+// ----- Types ----- //
+
+type Size = {
+  breakpoint: Breakpoint;
+  size: string;
+};
+
+type Sizes = {
+  mediaQueries: Size[];
+  default: string;
+};
+
+// ----- Functions ----- //
+
+const sizesAttribute = (sizes: Sizes): string => {
+  if (sizes.mediaQueries.length === 0) {
+    return sizes.default;
+  }
+
+  const queries = sizes.mediaQueries
+    .map(
+      (query) => `(min-width: ${breakpoints[query.breakpoint]}px) ${query.size}`
+    )
+    .join(", ");
+
+  return `${queries}, ${sizes.default}`;
+};
+
+const dimensions = (size: string, ratio: number): SerializedStyles => css`
+  width: ${size};
+  height: calc(${size} * ${ratio});
+`;
+
+const styles = (
+  sizes: Sizes,
+  width: number,
+  height: number
+): SerializedStyles => {
+  const ratio = height / width;
+
+  return css`
+    ${dimensions(sizes.default, ratio)}
+
+    ${sizes.mediaQueries.map(
+      (query) => css`
+        ${from[query.breakpoint]} {
+          ${dimensions(query.size, ratio)}
+        }
+      `
+    )}
+  `;
+};
+
+// ----- Exports ----- //
+
+export { Sizes, styles, sizesAttribute };
