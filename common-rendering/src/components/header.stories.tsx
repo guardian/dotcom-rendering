db493078c11ee55de2168056e8e8c999d68fc1d4
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2021-09-08 16:03:09 +0100

    Part two of liveblog header development for mobile and tablet

--- common-rendering/src/components/header.stories.tsx
+++ common-rendering/src/components/header.stories.tsx
@@ -21,7 +21,57 @@ const selectPillar = (initial: Pillar): Theme => {
 
 // ----- Stories ----- //
 
-const Default: FC = () => <Header theme={selectPillar(Pillar.Culture)} />;
+const Default: FC = () => (
+	<Header
+		theme={selectPillar(Pillar.News)}
+		title="MPs debate health and social care tax rise after Starmer accuses Johnson of ‘hammering workers’ – UK politics live"
+		credit="Politics live with Andrew Sparrow"
+		standfirst={
+			<>
+				<p>
+					<a href="https://www.theguardian.com/politics/live/2021/sep/08/health-social-care-levy-tax-ni-pmqs-sajid-javid-boris-johnson-politics-live?page=with:block-6138aefd8f0806f924aa786a#block-6138aefd8f0806f924aa786a">
+						MPs debate tax rise
+					</a>{" "}
+					after Keir Starmer atacks Boris Johnson’s tax plans{" "}
+					<a href="https://www.theguardian.com/politics/live/2021/sep/08/health-social-care-levy-tax-ni-pmqs-sajid-javid-boris-johnson-politics-live?page=with:block-61389a988f08d78b4987075b#block-61389a988f08d78b4987075b">
+						during PMQs
+					</a>
+				</p>
+				<ul>
+					<li>
+						<a href="https://www.theguardian.com/society/2021/sep/08/nhs-could-swallow-up-all-the-money-raised-by-new-tax-rise-says-ifs">
+							NHS could ‘swallow up’ all the money raised by new
+							tax rise, says IFS
+						</a>
+					</li>
+					<li>
+						<a href="https://www.theguardian.com/politics/2021/sep/08/starmer-lambasts-boris-johnson-manifesto-pledges-social-care">
+							Starmer lambasts PM for breaking manifesto pledges
+							on social care
+						</a>
+					</li>
+					<li>
+						<a href="https://www.theguardian.com/politics/2021/sep/07/boris-johnson-stakes-reputation-on-12bn-fix-for-health-and-social-care">
+							Boris Johnson stakes reputation on £12bn fix for
+							health and social care
+						</a>
+					</li>
+					<li>
+						<a href="https://www.theguardian.com/media/2021/sep/08/boris-plays-catch-up-what-the-papers-say-about-johnsons-tax-plan">
+							‘Boris plays catch-up’: what the papers say about
+							Johnson’s tax plan
+						</a>
+					</li>
+					<li>
+						<a href="https://www.theguardian.com/world/live/2021/sep/08/coronavirus-live-news-idaho-rationing-healthcare-pandemics-devastating-impact-on-hiv-tb-and-malaria">
+							Coronavirus updates – live
+						</a>
+					</li>
+				</ul>
+			</>
+		}
+	/>
+);
 
 // ----- Exports ----- //
 
--- common-rendering/src/components/header.tsx
+++ common-rendering/src/components/header.tsx
@@ -1,33 +1,116 @@
 import { css } from "@emotion/react";
 import type { SerializedStyles } from "@emotion/react";
-import type { FC } from "react";
-import { body } from "@guardian/src-foundations/typography";
-import { getPillarColour } from "../utils/palette";
-import { HeaderProps } from "../utils/palette";
-import { Theme } from "@guardian/types";
+import type { FC, ReactElement } from "react";
+import { headline } from "@guardian/src-foundations/typography";
+import { space } from "@guardian/src-foundations";
+import { Theme, Pillar } from "@guardian/types";
+import {
+	sport,
+	news,
+	opinion,
+	lifestyle,
+	culture,
+	text,
+} from "@guardian/src-foundations/palette";
 
 type HeaderProps = {
 	theme: Theme;
+	title: string;
+	credit: string;
+	standfirst: ReactElement;
 };
 
 const backgroundTop = (theme: Theme): SerializedStyles => css`
-	background-color: ${getPillarColour(theme, 300)};
+	background-color: ${getPillarTop(theme)};
 	width: 100%;
-	min-height: 100px;
-	${body.medium()};
+	${headline.small()};
+	color: ${text.ctaPrimary};
+	padding: ${space[1]}px ${space[2]}px ${space[5]}px;
+	span {
+		display: block;
+	}
+`;
+
+const creditStyles: SerializedStyles = css`
+	${headline.xxxsmall()};
+	color: ${news[600]};
+	margin-bottom: ${space[1]}px;
 `;
 
 const backgroundBottom = (theme: Theme): SerializedStyles => css`
-	background-color: ${getPillarColour(theme, 200)};
+	padding: ${space[1]}px ${space[2]}px;
+	background-color: ${getPillarBottom(theme)};
 	width: 100%;
 	min-height: 100px;
+	p,
+	a,
+	li {
+		color: ${text.ctaPrimary};
+		${headline.xxxsmall()};
+		line-height: 135%;
+		margin-bottom: ${space[2]}px;
+	}
+
+	li::before {
+		content: "";
+		background-color: ${news[600]};
+		width: 0.75rem;
+		height: 0.75rem;
+		border-radius: 0.5rem;
+		display: inline-block;
+		vertical-align: middle;
+		margin-right: 0.5rem;
+	}
+
+	li {
+		padding-left: 1.25rem;
+		text-indent: -1.25rem;
+	}
+	a {
+		text-decoration: none;
+		padding-bottom: 0.005rem;
+		border-bottom: 1px solid ${news[600]};
+	}
 `;
 
-const Header: FC<HeaderProps> = ({ theme }) => (
+const Header: FC<HeaderProps> = ({ theme, title, credit, standfirst }) => (
 	<>
-		<div css={backgroundTop(theme)} />
-		<div css={backgroundBottom(theme)} />
+		<div css={backgroundTop(theme)}>
+			<span css={creditStyles}>{credit}</span>
+			<span>{title}</span>
+		</div>
+		<div css={backgroundBottom(theme)}>{standfirst}</div>
 	</>
 );
+
+const getPillarTop = (theme: Theme) => {
+	switch (theme) {
+		case Pillar.Sport:
+			return sport[300];
+		case Pillar.Culture:
+			return culture[300];
+		case Pillar.Lifestyle:
+			return lifestyle[300];
+		case Pillar.Opinion:
+			return opinion[300];
+		default:
+			return news[300];
+	}
+};
+
+const getPillarBottom = (theme: Theme) => {
+	switch (theme) {
+		case Pillar.Sport:
+			return sport[100];
+		case Pillar.Culture:
+			return culture[200];
+		case Pillar.Lifestyle:
+			return lifestyle[200];
+		case Pillar.Opinion:
+			return opinion[200];
+		default:
+			return news[200];
+	}
+};
 
 export default Header;
