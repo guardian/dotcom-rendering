8eccbc4e3dc42dd943c27c396367df97ac913d18
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2021-09-22 15:55:17 +0100

    Add Pagination

---
+++ common-rendering/src/components/Hide.tsx
@@ -0,0 +1,42 @@
+import { css } from "@emotion/react";
+import { until, from, Breakpoint } from "@guardian/src-foundations/mq";
+
+interface Props {
+	children: React.ReactNode;
+	when: "above" | "below";
+	breakpoint: Breakpoint;
+}
+
+interface PropsSpan extends Props {
+	el?: "span";
+}
+interface PropsLi extends Props {
+	el: "li";
+}
+
+export const Hide = ({
+	children,
+	when,
+	breakpoint,
+	el,
+}: PropsSpan | PropsLi) => {
+	let whenToHide;
+	if (when === "below") {
+		whenToHide = css`
+			${until[breakpoint]} {
+				display: none;
+			}
+		`;
+	} else {
+		whenToHide = css`
+			${from[breakpoint]} {
+				display: none;
+			}
+		`;
+	}
+	return el === "li" ? (
+		<li css={whenToHide}>{children}</li>
+	) : (
+		<span css={whenToHide}>{children}</span>
+	);
+};
