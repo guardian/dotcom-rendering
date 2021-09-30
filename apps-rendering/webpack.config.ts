a7e6501325a334c5147e6ec5829b40d50e418f06
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-09-30 16:06:12 +0100

    Fixed duplicate react bug caused by externals

--- apps-rendering/webpack.config.ts
+++ apps-rendering/webpack.config.ts
@@ -9,7 +9,6 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import type { Compiler, Configuration, ResolveOptions } from 'webpack';
 import webpack from 'webpack';
 import { WebpackManifestPlugin } from 'webpack-manifest-plugin';
-import nodeExternals from 'webpack-node-externals';
 import { renederedItemsAssetsCss } from './config/rendered-items-assets-styles';
 import { WebpackPluginInstance } from 'webpack';
 
@@ -82,14 +81,6 @@ const serverConfig = (
 		mode,
 		entry: 'server/server.ts',
 		target: 'node',
-		externals: isProd
-			? []
-			: [
-					nodeExternals({
-						allowlist: [/@guardian/],
-					}),
-			  ],
-
 		node: {
 			__dirname: false,
 		},
