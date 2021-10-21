6a17aade9b946649ad820360addfd79e7284a7d0
Marjan K <marjank@30728.gnm.int>
2021-10-21 12:52:42 +0100

    fix metadata story for desktop

--- apps-rendering/src/components/metadata.stories.tsx
+++ apps-rendering/src/components/metadata.stories.tsx
@@ -1,6 +1,8 @@
 // ----- Imports ----- //
 
 import { css } from '@emotion/react';
+import { neutral } from '@guardian/src-foundations';
+import { from } from '@guardian/src-foundations/mq';
 import { live } from 'fixtures/live';
 import type { FC } from 'react';
 import Metadata from './metadata';
@@ -9,9 +11,13 @@ import Metadata from './metadata';
 
 const containerStyles = css`
 	background: rgb(139, 0, 0);
+
+	${from.desktop} {
+		background-color: ${neutral[97]};
+	}
 `;
 
-const Default: FC = () => (
+const LiveblogMetadata: FC = () => (
 	<div css={containerStyles}>
 		<Metadata item={{ ...live }} />
 	</div>
@@ -24,4 +30,4 @@ export default {
 	title: 'AR/Metadata',
 };
 
-export { Default };
+export { LiveblogMetadata };
