dd2d0ded727764a46aa5cdc91df7341bf18c3cfe
Olly <9575458+OllysCoding@users.noreply.github.com>
2021-11-02 17:14:06 +0000

    fix conflicts

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,13 +18,9 @@ Picture.disableLazyLoading = isChromatic();
 
 if (isChromatic()) {
 	// Fix the date to prevent false negatives
-<<<<<<< HEAD
-	MockDate.set("Sun Jan 10 2021 12:00:00 GMT+0000 (Greenwich Mean Time)");
-=======
 	// And not just any date... 200 years! 🎉
 	// https://www.theguardian.com/gnm-press-office/2021/apr/30/the-guardian-celebrates-200-extraordinary-years
 	MockDate.set('Wed May 5 2021 12:00:00 GMT+0000 (Greenwich Mean Time)');
->>>>>>> 418cf8c71c66511fa4bce2768f0ee30c30d50b1a
 }
 
 mockRESTCalls();
