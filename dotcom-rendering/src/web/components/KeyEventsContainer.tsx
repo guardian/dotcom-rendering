9ab4ca4da05c9acdb0feaaac4396995175e3bd8f
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-11-10 10:09:47 +0000

    `KeyEvents` in `LiveLayout` (#3595)
    * fix: Missing key prop
    
    * fix: Jest config to work with common-rendering
    
    * feat: Added KeyEvents to LiveLayout
    
    * chore: I ran prettier
    
    * feat: Removed superfluous caching
    
    Co-authored-by: Olly <olly.namey@theguardian.com>
    
    * fix: Formatting
    
    Co-authored-by: OllysCoding
    
    * fix: Add yarn command back in
    
    Co-authored by @OllysCoding
    
    * chore: Oh my God, how hard is this
    
    Co-Authored-By: Olly <9575458+OllysCoding@users.noreply.github.com>
    
    * fix: Skipping for now while we wait to fix jest config
    
    Co-Authored-By: Olly <9575458+OllysCoding@users.noreply.github.com>
    
    * fix: Added `preserveSymlinks` to jest config to workaround monorepo issues with jest
    
    https://github.com/kulshekhar/ts-jest/issues/1336#issuecomment-570794444
    
    * fix: Added `preserveSymLinks` to tsconfig.test
    
    https://github.com/kulshekhar/ts-jest/issues/1336#issuecomment-570794444
    
    * fix: Restore tests
    
    * fix: Restore previous config
    
    * refactor: Replace reduce with filter > map
    
    Co-authored-by: Olly <olly.namey@theguardian.com>
    Co-authored-by: Olly <9575458+OllysCoding@users.noreply.github.com>

---
+++ dotcom-rendering/src/web/components/KeyEventsContainer.tsx
@@ -0,0 +1,30 @@
+import KeyEvents, {
+	KeyEvent,
+} from '@guardian/common-rendering/src/components/keyEvents';
+
+type Props = {
+	keyEvents: Block[];
+	format: ArticleFormat;
+};
+
+export const KeyEventsContainer = ({ keyEvents, format }: Props) => {
+	const transformedKeyEvents: KeyEvent[] = keyEvents
+		.filter((keyEvent) => {
+			return keyEvent.title && keyEvent.blockFirstPublishedDisplay;
+		})
+		.map((keyEvent) => {
+			return {
+				text: keyEvent.title || '', // We fallback to '' here purely to keep ts happy
+				url: `?page=with:block-${keyEvent.id}#block-${keyEvent.id}`,
+				time: keyEvent.blockFirstPublishedDisplay || '', // We fallback to '' here purely to keep ts happy
+			};
+		});
+
+	return (
+		<KeyEvents
+			theme={format.theme}
+			keyEvents={transformedKeyEvents}
+			supportsDarkMode={false}
+		/>
+	);
+};
