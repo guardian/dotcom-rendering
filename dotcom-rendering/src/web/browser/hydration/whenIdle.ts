d1e9b144b8617544819cc5c8f8b326f06ffa2171
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-11-15 15:15:37 +0000

    Update dotcom-rendering/src/web/browser/hydration/whenIdle.ts
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

--- dotcom-rendering/src/web/browser/hydration/whenIdle.ts
+++ dotcom-rendering/src/web/browser/hydration/whenIdle.ts
@@ -1,7 +1,7 @@
 /**
  * whenIdle exectures the given callback when the browser is 'idle'
  *
- * @param callback Fired when requestIdleCallback runs or after 300ms
+ * @param callback Fired when requestIdleCallback runs. If requestIdleCallback is not available after 300ms
  */
 export const whenIdle = (callback: () => void) => {
 	if ('requestIdleCallback' in window) {
