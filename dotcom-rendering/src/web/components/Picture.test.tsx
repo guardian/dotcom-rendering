9aaefd4464de5dff4abb243eda8487225a3ef82f
Olly <9575458+OllysCoding@users.noreply.github.com>
2021-11-15 20:47:10 +0000

    Prettier fixes

--- dotcom-rendering/src/web/components/Picture.test.tsx
+++ dotcom-rendering/src/web/components/Picture.test.tsx
@@ -1,44 +1,48 @@
 import { breakpoints } from '@guardian/src-foundations/mq';
 
-import { getClosestSetForWidth, getDesiredWidth, getSourcesFromSrcSets } from './Picture'
+import {
+	getClosestSetForWidth,
+	getDesiredWidth,
+	getSourcesFromSrcSets,
+} from './Picture';
 
 const hdpiSources: SrcSetItem[] = [
 	{
 		src: '1',
-		width: 1400
+		width: 1400,
 	},
 	{
 		src: '2',
-		width: 1240
+		width: 1240,
 	},
 	{
 		src: '3',
-		width: 930
+		width: 930,
 	},
 	{
 		src: '4',
-		width: 1290
+		width: 1290,
 	},
-]
+];
 
 const mdpiSources: SrcSetItem[] = [
 	{
 		src: '1',
-		width: 620
+		width: 620,
 	},
 	{
 		src: '2',
-		width: 700
+		width: 700,
 	},
 	{
 		src: '3',
-		width: 465
+		width: 465,
 	},
 	{
 		src: '4',
-		width: 645
+		width: 645,
 	},
-]
+];
 
 /**
  *       mobile: 320
@@ -55,55 +59,97 @@ describe(`Picture`, () => {
 	describe('getClosestSetForWidth', () => {
 		it('Gets the closest source for a given width (hdpi)', () => {
 			// Breakpoints
-			expect(getClosestSetForWidth(breakpoints.mobile * 2, hdpiSources).width).toBe(930)
-			expect(getClosestSetForWidth(breakpoints.mobileMedium * 2, hdpiSources).width).toBe(930)
-			expect(getClosestSetForWidth(breakpoints.mobileLandscape * 2, hdpiSources).width).toBe(1240)
-			expect(getClosestSetForWidth(breakpoints.phablet * 2, hdpiSources).width).toBe(1400)
-			expect(getClosestSetForWidth(breakpoints.tablet * 2, hdpiSources).width).toBe(1400)
-			expect(getClosestSetForWidth(breakpoints.desktop * 2, hdpiSources).width).toBe(1400)
-			expect(getClosestSetForWidth(breakpoints.leftCol * 2, hdpiSources).width).toBe(1400)
-			expect(getClosestSetForWidth(breakpoints.wide * 2, hdpiSources).width).toBe(1400)
+			expect(
+				getClosestSetForWidth(breakpoints.mobile * 2, hdpiSources)
+					.width,
+			).toBe(930);
+			expect(
+				getClosestSetForWidth(breakpoints.mobileMedium * 2, hdpiSources)
+					.width,
+			).toBe(930);
+			expect(
+				getClosestSetForWidth(
+					breakpoints.mobileLandscape * 2,
+					hdpiSources,
+				).width,
+			).toBe(1240);
+			expect(
+				getClosestSetForWidth(breakpoints.phablet * 2, hdpiSources)
+					.width,
+			).toBe(1400);
+			expect(
+				getClosestSetForWidth(breakpoints.tablet * 2, hdpiSources)
+					.width,
+			).toBe(1400);
+			expect(
+				getClosestSetForWidth(breakpoints.desktop * 2, hdpiSources)
+					.width,
+			).toBe(1400);
+			expect(
+				getClosestSetForWidth(breakpoints.leftCol * 2, hdpiSources)
+					.width,
+			).toBe(1400);
+			expect(
+				getClosestSetForWidth(breakpoints.wide * 2, hdpiSources).width,
+			).toBe(1400);
 
 			// Example widths
-			expect(getClosestSetForWidth(620 * 2, hdpiSources).width).toBe(1240)
-		})
+			expect(getClosestSetForWidth(620 * 2, hdpiSources).width).toBe(
+				1240,
+			);
+		});
 
 		it('Gets the closest source for a given width (mdpi)', () => {
 			// Breakpoints
-			expect(getClosestSetForWidth(breakpoints.mobile, mdpiSources).width).toBe(465)
-			expect(getClosestSetForWidth(breakpoints.mobileMedium, mdpiSources).width).toBe(465)
-			expect(getClosestSetForWidth(breakpoints.mobileLandscape, mdpiSources).width).toBe(620)
-			expect(getClosestSetForWidth(breakpoints.phablet, mdpiSources).width).toBe(700)
-			expect(getClosestSetForWidth(breakpoints.tablet, mdpiSources).width).toBe(700)
-			expect(getClosestSetForWidth(breakpoints.desktop, mdpiSources).width).toBe(700)
-			expect(getClosestSetForWidth(breakpoints.leftCol, mdpiSources).width).toBe(700)
-			expect(getClosestSetForWidth(breakpoints.wide, mdpiSources).width).toBe(700)
+			expect(
+				getClosestSetForWidth(breakpoints.mobile, mdpiSources).width,
+			).toBe(465);
+			expect(
+				getClosestSetForWidth(breakpoints.mobileMedium, mdpiSources)
+					.width,
+			).toBe(465);
+			expect(
+				getClosestSetForWidth(breakpoints.mobileLandscape, mdpiSources)
+					.width,
+			).toBe(620);
+			expect(
+				getClosestSetForWidth(breakpoints.phablet, mdpiSources).width,
+			).toBe(700);
+			expect(
+				getClosestSetForWidth(breakpoints.tablet, mdpiSources).width,
+			).toBe(700);
+			expect(
+				getClosestSetForWidth(breakpoints.desktop, mdpiSources).width,
+			).toBe(700);
+			expect(
+				getClosestSetForWidth(breakpoints.leftCol, mdpiSources).width,
+			).toBe(700);
+			expect(
+				getClosestSetForWidth(breakpoints.wide, mdpiSources).width,
+			).toBe(700);
 
 			// Example widths
-			expect(getClosestSetForWidth(620, mdpiSources).width).toBe(620)
-		})
-	})
+			expect(getClosestSetForWidth(620, mdpiSources).width).toBe(620);
+		});
+	});
 
 	describe('getDesiredWidth', () => {
 		it('Returns the correct with for HDPI', () => {
-			expect(getDesiredWidth(200, true)).toBe(400)
-		})
+			expect(getDesiredWidth(200, true)).toBe(400);
+		});
 		it('Returns the correct with for MDPI', () => {
-			expect(getDesiredWidth(200, false)).toBe(200)
-		})
-	})
+			expect(getDesiredWidth(200, false)).toBe(200);
+		});
+	});
 
 	describe('getSourcesFromSrcSets', () => {
 		it('Constructs the string correctly', () => {
 			expect(getSourcesFromSrcSets(hdpiSources)).toBe(
-				'1 1400w,2 1240w,3 930w,4 1290w'
-			)
+				'1 1400w,2 1240w,3 930w,4 1290w',
+			);
 			expect(getSourcesFromSrcSets(mdpiSources)).toBe(
-				'1 620w,2 700w,3 465w,4 645w'
-			)
-		})
-	})
-
-
-
+				'1 620w,2 700w,3 465w,4 645w',
+			);
+		});
+	});
 });
--- dotcom-rendering/src/web/components/Picture.tsx
+++ dotcom-rendering/src/web/components/Picture.tsx
@@ -47,9 +47,7 @@ const getSourcesForRoleAndResolution = (
 		: setsForRole.filter((set) => !set.src.includes('dpr=2'));
 };
 
-const getFallback = (
-	sources: SrcSetItem[],
-): string | undefined => {
+const getFallback = (sources: SrcSetItem[]): string | undefined => {
 	if (sources.length === 0) return undefined;
 	// The assumption here is readers on devices that do not support srcset are likely to be on poor
 	// network connections so we're going to fallback to a small image
@@ -59,10 +57,12 @@ const getFallback = (
 // The image sources we're provided use double-widths for HDPI images
 // We should therefor multiple the width based on if it's HDPI or not
 // e.g { url: '... ?width=500&dpr=2 ...', width: '1000' }
-export const getDesiredWidth = (width: number, hdpi: boolean) => hdpi ? width * 2 : width
+export const getDesiredWidth = (width: number, hdpi: boolean) =>
+	hdpi ? width * 2 : width;
 
 // Turn SrsSets into a string which can be passed to the source element
-export const getSourcesFromSrcSets = (sources: SrcSetItem[]) => sources.map((srcSet) => `${srcSet.src} ${srcSet.width}w`).join(',');
+export const getSourcesFromSrcSets = (sources: SrcSetItem[]) =>
+	sources.map((srcSet) => `${srcSet.src} ${srcSet.width}w`).join(',');
 
 // Returns the size image desired at a particular breakpoint based on the image role, format, and if it's main media.
 const getSizeForBreakpoint = (
@@ -71,7 +71,8 @@ const getSizeForBreakpoint = (
 	isMainMedia: boolean,
 	format: ArticleFormat,
 ): number => {
-	if (format.display === ArticleDisplay.Immersive && isMainMedia) return breakpoint // 100vh
+	if (format.display === ArticleDisplay.Immersive && isMainMedia)
+		return breakpoint; // 100vh
 
 	if (
 		(format.display === ArticleDisplay.Showcase ||
@@ -80,45 +81,60 @@ const getSizeForBreakpoint = (
 	) {
 		// Showcase main media images (which includes numbered list articles) appear
 		// larger than in body showcase images so we use a different set of image sizes
-		if (breakpoint >= breakpoints.wide) return 1020
-		if (breakpoint >= breakpoints.leftCol) return 940
-		if (breakpoint >= breakpoints.tablet) return 700
-		if (breakpoint >= breakpoints.phablet) return 700
-		return breakpoint
+		if (breakpoint >= breakpoints.wide) return 1020;
+		if (breakpoint >= breakpoints.leftCol) return 940;
+		if (breakpoint >= breakpoints.tablet) return 700;
+		if (breakpoint >= breakpoints.phablet) return 700;
+		return breakpoint;
 	}
 
 	switch (role) {
 		case 'inline':
-			if (breakpoint >= breakpoints.tablet && breakpoint < breakpoints.desktop) return 680
-			if (breakpoint >= breakpoints.phablet) return 620
-			return breakpoint // 100vw
+			if (
+				breakpoint >= breakpoints.tablet &&
+				breakpoint < breakpoints.desktop
+			)
+				return 680;
+			if (breakpoint >= breakpoints.phablet) return 620;
+			return breakpoint; // 100vw
 		case 'halfWidth':
-			if (breakpoint >= breakpoints.phablet) return 300
-			return breakpoint / 2 // 50vw
+			if (breakpoint >= breakpoints.phablet) return 300;
+			return breakpoint / 2; // 50vw
 		case 'thumbnail':
-			return 140
+			return 140;
 		case 'immersive':
-			if (breakpoint >= breakpoints.wide) return 1300
-			return breakpoint // 100vw
+			if (breakpoint >= breakpoints.wide) return 1300;
+			return breakpoint; // 100vw
 		case 'supporting':
-			if (breakpoint >= breakpoints.wide) return 380
-			return breakpoint // 100vw
+			if (breakpoint >= breakpoints.wide) return 380;
+			return breakpoint; // 100vw
 		case 'showcase':
-			if (breakpoint >= breakpoints.wide) return 860
-			if (breakpoint >= breakpoints.leftCol) return 780
-			if (breakpoint >= breakpoints.phablet) return 620
-			return breakpoint // 100vw
+			if (breakpoint >= breakpoints.wide) return 860;
+			if (breakpoint >= breakpoints.leftCol) return 780;
+			if (breakpoint >= breakpoints.phablet) return 620;
+			return breakpoint; // 100vw
 	}
 };
 
 // Takes a size & sources, and returns a source set with 1 matching image with the desired size
-const getSourceSetForSize = (size: number, sources: SrcSetItem[], hdpi: boolean) => getSourcesFromSrcSets([getClosestSetForWidth(getDesiredWidth(size, hdpi), sources)])
+const getSourceSetForSize = (
+	size: number,
+	sources: SrcSetItem[],
+	hdpi: boolean,
+) =>
+	getSourcesFromSrcSets([
+		getClosestSetForWidth(getDesiredWidth(size, hdpi), sources),
+	]);
 
 // Create sourcesets for portrait immersive
 // TODO: In a future PR this system will be updated to solve scaling issues with DPR
 const portraitImmersiveSource = (sources: SrcSetItem[], hdpi: boolean) => (
 	<source
-		media={hdpi ? '(orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.25), (orientation: portrait) and (min-resolution: 120dpi)' : '(orientation: portrait)'}
+		media={
+			hdpi
+				? '(orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.25), (orientation: portrait) and (min-resolution: 120dpi)'
+				: '(orientation: portrait)'
+		}
 		// Immersive MainMedia elements fill the height of the viewport, meaning
 		// on mobile devices even though the viewport width is small, we'll need
 		// a larger image to maintain quality. To solve this problem we're using
@@ -128,7 +144,7 @@ const portraitImmersiveSource = (sources: SrcSetItem[], hdpi: boolean) => (
 		sizes="167vh"
 		srcSet={getSourcesFromSrcSets(sources)}
 	/>
-)
+);
 
 export const Picture = ({
 	imageSources,
@@ -140,39 +156,84 @@ export const Picture = ({
 	isMainMedia = false,
 	isLazy = true,
 }: Props) => {
-	const hdpiSourceSets = getSourcesForRoleAndResolution(imageSources, role, 'hdpi',);
-	const mdpiSourcesSets = getSourcesForRoleAndResolution(imageSources, role, 'mdpi');
+	const hdpiSourceSets = getSourcesForRoleAndResolution(
+		imageSources,
+		role,
+		'hdpi',
+	);
+	const mdpiSourcesSets = getSourcesForRoleAndResolution(
+		imageSources,
+		role,
+		'mdpi',
+	);
 	const fallbackSrc = getFallback(hdpiSourceSets);
 
 	return (
 		<picture itemProp="contentUrl">
-			{
-				format.display === ArticleDisplay.Immersive && isMainMedia ?
-					<>
-						{portraitImmersiveSource(hdpiSourceSets, true)}
-						{portraitImmersiveSource(mdpiSourcesSets, false)}
-					</>
-				 : ''
-			}
+			{format.display === ArticleDisplay.Immersive && isMainMedia ? (
+				<>
+					{portraitImmersiveSource(hdpiSourceSets, true)}
+					{portraitImmersiveSource(mdpiSourcesSets, false)}
+				</>
+			) : (
+				''
+			)}
 
 			{
 				// Loop through the breakpoints from highest to lowest, adding '0' as the last option
-				[...Object.values(breakpoints).reverse(), 0].map((breakpoint) =>
-					<>
-						{/* HDPI Source (DPR2) - images in this srcset have `dpr=2&quality=45` in the url */}
-						<source
-							srcSet={getSourceSetForSize(getSizeForBreakpoint(breakpoint, role, isMainMedia, format), hdpiSourceSets, true)}
-							sizes={`${breakpoint || getSizeForBreakpoint(breakpoint, role, isMainMedia, format)}px`}
-							media={`(min-width: ${breakpoint}px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: ${breakpoint}px) and (min-resolution: 120dpi)`}
-						/>
-						{/* MDPI Source - images in this srcset have `quality=85` in the url */}
-						<source
-							srcSet={getSourceSetForSize(getSizeForBreakpoint(breakpoint, role, isMainMedia, format), mdpiSourcesSets, false)}
-							sizes={`${breakpoint || getSizeForBreakpoint(breakpoint, role, isMainMedia, format)}px`}
-							media={`min-width: ${breakpoint}px`}
-						/>
-					</>
-			)}
+				[...Object.values(breakpoints).reverse(), 0].map(
+					(breakpoint) => (
+						<>
+							{/* HDPI Source (DPR2) - images in this srcset have `dpr=2&quality=45` in the url */}
+							<source
+								srcSet={getSourceSetForSize(
+									getSizeForBreakpoint(
+										breakpoint,
+										role,
+										isMainMedia,
+										format,
+									),
+									hdpiSourceSets,
+									true,
+								)}
+								sizes={`${
+									breakpoint ||
+									getSizeForBreakpoint(
+										breakpoint,
+										role,
+										isMainMedia,
+										format,
+									)
+								}px`}
+								media={`(min-width: ${breakpoint}px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: ${breakpoint}px) and (min-resolution: 120dpi)`}
+							/>
+							{/* MDPI Source - images in this srcset have `quality=85` in the url */}
+							<source
+								srcSet={getSourceSetForSize(
+									getSizeForBreakpoint(
+										breakpoint,
+										role,
+										isMainMedia,
+										format,
+									),
+									mdpiSourcesSets,
+									false,
+								)}
+								sizes={`${
+									breakpoint ||
+									getSizeForBreakpoint(
+										breakpoint,
+										role,
+										isMainMedia,
+										format,
+									)
+								}px`}
+								media={`min-width: ${breakpoint}px`}
+							/>
+						</>
+					),
+				)
+			}
 
 			<img
 				alt={alt}
