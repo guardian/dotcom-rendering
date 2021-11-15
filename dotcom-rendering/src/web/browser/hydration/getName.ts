62eca46f4e88ed72ad7e8b60cd8348cbacb20d33
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-11-15 08:48:29 +0000

    chore: Typo

--- dotcom-rendering/src/web/browser/hydration/getName.ts
+++ dotcom-rendering/src/web/browser/hydration/getName.ts
@@ -10,7 +10,7 @@ export const getName = (marker: HTMLElement): string | null => {
 	const name = marker.getAttribute('name');
 	if (!name) {
 		console.error(
-			`🚨 Error - no name attribute supplied. We nned name to know what component to import 🚨`,
+			`🚨 Error - no name attribute supplied. We need name to know what component to import 🚨`,
 		);
 	}
 	return name;
