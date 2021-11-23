b926d1c957fbab4b545653aaf19da61b5f59285e
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2021-11-23 11:37:40 +0000

    Change layout

--- dotcom-rendering/src/web/layouts/StandardLayout.tsx
+++ dotcom-rendering/src/web/layouts/StandardLayout.tsx
@@ -492,14 +492,14 @@ export const StandardLayout = ({ CAPI, NAV, format, palette }: Props) => {
 						</GridItem>
 						<GridItem area="matchtabs" element="aside">
 							<div css={maxWidth}>
-								{format.design === ArticleDesign.DeadBlog ||
-									(ArticleDesign.LiveBlog &&
-										CAPI.matchUrl && (
-											<Placeholder
-												rootId="match-tabs"
-												height={40}
-											/>
-										))}
+								{(format.design === ArticleDesign.DeadBlog ||
+									format.design === ArticleDesign.LiveBlog) &&
+									CAPI.matchUrl && (
+										<Placeholder
+											rootId="match-tabs"
+											height={40}
+										/>
+									)}
 							</div>
 						</GridItem>
 						<GridItem area="headline">
