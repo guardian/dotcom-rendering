1172d3b61286e3cc45bcab2797f4ed6cf47c337d
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-11-28 09:32:56 +0000

    Fix noisy console warning in dev build

--- dotcom-rendering/scripts/webpack/browser.js
+++ dotcom-rendering/scripts/webpack/browser.js
@@ -94,6 +94,7 @@ module.exports = ({ isLegacyJS }) => ({
 											},
 									  ],
 							],
+							compact: true,
 						},
 					},
 					{
--- dotcom-rendering/scripts/webpack/server.js
+++ dotcom-rendering/scripts/webpack/server.js
@@ -55,6 +55,7 @@ module.exports = () => ({
 									},
 								],
 							],
+							compact: true,
 						},
 					},
 					{
