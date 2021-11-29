18cfa14999b0749f41f603d34888462c67dcbd55
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2021-11-29 16:37:42 +0000

    Use GuCDK to define AWS infrastructure

---
+++ apps-rendering/cdk/bin/cdk.ts
@@ -0,0 +1,10 @@
+import 'source-map-support/register';
+import { App } from '@aws-cdk/core';
+import { MobileAppsRendering } from '../lib/mobile-apps-rendering';
+
+const app = new App();
+const cloudFormationStackName = process.env.GU_CFN_STACK_NAME;
+new MobileAppsRendering(app, 'MobileAppsRendering', {
+	stack: 'mobile',
+	cloudFormationStackName,
+});
