5b83f14935c76e7b19adc4cbc5e9b076443d3877
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-11-30 10:55:35 +0000

    feat: Make time relative

---
+++ dotcom-rendering/src/web/browser/relativeTime/init.ts
@@ -0,0 +1,14 @@
+import '../webpackPublicPath';
+import { startup } from '@root/src/web/browser/startup';
+import { updateTimeElements } from './updateTimeElements';
+
+const init = (): Promise<void> => {
+	updateTimeElements();
+	window.setInterval(() => {
+		updateTimeElements();
+	}, 5000);
+
+	return Promise.resolve();
+};
+
+startup('relativeTime', null, init);
