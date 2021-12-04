36dbbed68722324506e562d7543d93b762701d51
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-12-04 16:55:06 +0000

    fix: More resiliance

--- dotcom-rendering/cypress/integration/parallel-5/atom.video.spec.js
+++ dotcom-rendering/cypress/integration/parallel-5/atom.video.spec.js
@@ -32,23 +32,26 @@ describe('YouTube Atom', function () {
 		cmpIframe().contains("It's your choice");
 		cmpIframe().find("[title='Manage my cookies']").click();
 		privacySettingsIframe().contains('Privacy settings');
-		privacySettingsIframe().find("[title='Accept all']").click();
+		privacySettingsIframe()
+			.find("[title='Accept all']", { timeout: 12000 })
+			.click();
 
 		// Wait for hydration
-		cy.scrollTo('bottom', { duration: 500 });
-		cy.get('footer').contains('Support the').should('be.visible');
+		cy.get('[data-component=youtube-atom]')
+			.parent()
+			.should('have.attr', 'gu-hydration', 'complete');
 
 		// Make sure overlay is displayed
 		cy.get(`[data-cy="youtube-overlay"]`).should('be.visible');
-		cy.get('iframe ~ [data-cy="youtube-overlay"]').should('not.exist');
 
 		// Trigger mouseenter event
 		cy.window().its('onYouTubeIframeAPIReady').should('not.exist');
+
 		cy.get(`[data-cy="youtube-overlay"]`).trigger('mouseenter');
 
 		// Wait for youtube to be initialised
 		cy.window()
-			.its('onYouTubeIframeAPIReady', { timeout: 12000 })
+			.its('onYouTubeIframeAPIReady', { timeout: 30000 })
 			.should('exist');
 
 		// YouTube was initialised but the overlay is still showing
@@ -62,7 +65,7 @@ describe('YouTube Atom', function () {
 		// Play video
 		cy.get(`[data-cy="youtube-overlay"]`).click();
 
-		cy.wait('@ophanCall', { timeout: 12000 });
+		cy.wait('@ophanCall', { timeout: 30000 });
 
 		// Video is playing, overlay is gone
 		cy.get(`[data-cy="youtube-overlay"]`).should('not.exist');
@@ -71,20 +74,22 @@ describe('YouTube Atom', function () {
 	it('plays in body videos', function () {
 		// cy.intercept('**youtube**').as('callToYouTube');
 		cy.visit(
-			'/Article?url=https://www.theguardian.com/us-news/2020/oct/29/lordstown-ohio-trump-gm-plant-election',
+			'/Article?url=https://www.theguardian.com/environment/2021/oct/05/volcanoes-are-life-how-the-ocean-is-enriched-by-eruptions-devastating-on-land',
 		);
 		cmpIframe().contains("It's your choice");
 		cmpIframe().find("[title='Manage my cookies']").click();
 		privacySettingsIframe().contains('Privacy settings');
-		privacySettingsIframe().find("[title='Accept all']").click();
+		privacySettingsIframe()
+			.find("[title='Accept all']", { timeout: 12000 })
+			.click();
 
 		// Wait for hydration
-		cy.scrollTo('bottom', { duration: 500 });
-		cy.get('footer').contains('Support the').should('be.visible');
+		cy.get('[data-component=youtube-atom]')
+			.parent()
+			.should('have.attr', 'gu-hydration', 'complete');
 
 		// Make sure overlay is displayed
 		cy.get(`[data-cy="youtube-overlay"]`).should('be.visible');
-		cy.get('iframe ~ [data-cy="youtube-overlay"]').should('not.exist');
 
 		// Trigger mouseenter event
 		cy.get(`[data-cy="youtube-overlay"]`).trigger('touchstart');
@@ -95,52 +100,52 @@ describe('YouTube Atom', function () {
 
 		// Listen for the ophan call made when the video is played
 		interceptPlayEvent(
-			'gu-video-youtube-35f9a38f-c0c5-4bbd-9395-e338c9e97d30',
+			'gu-video-youtube-2bc6f709-865e-49ae-b01b-8fc38eb4e9a7',
 		).as('ophanCall');
 
 		// Play video
 		cy.get(`[data-cy="youtube-overlay"]`).click();
 
-		cy.wait('@ophanCall', { timeout: 12000 });
+		cy.wait('@ophanCall', { timeout: 30000 });
 
 		// // Video is playing, overlay is gone
 		cy.get(`[data-cy="youtube-overlay"]`).should('not.exist');
 	});
 
 	it('still plays the video if the reader rejects consent', function () {
-		cy.intercept('**youtube**').as('callToYouTube');
 		cy.visit(
-			'/Article?url=https://www.theguardian.com/us-news/2020/oct/29/lordstown-ohio-trump-gm-plant-election',
+			'/Article?url=https://www.theguardian.com/environment/2021/oct/05/volcanoes-are-life-how-the-ocean-is-enriched-by-eruptions-devastating-on-land',
 		);
 		cmpIframe().contains("It's your choice");
 		cmpIframe().find("[title='Manage my cookies']").click();
 		privacySettingsIframe().contains('Privacy settings');
-		privacySettingsIframe().find("[title='Reject all']").click();
+		privacySettingsIframe()
+			.find("[title='Reject all']", { timeout: 12000 })
+			.click();
 
 		// Wait for hydration
-		cy.scrollTo('bottom', { duration: 500 });
-		cy.get('footer').contains('Support the').should('be.visible');
+		cy.get('[data-component=youtube-atom]')
+			.parent()
+			.should('have.attr', 'gu-hydration', 'complete');
 
 		// Make sure overlay is displayed
 		cy.get(`[data-cy="youtube-overlay"]`).should('be.visible');
-		cy.get('iframe ~ [data-cy="youtube-overlay"]').should('not.exist');
 
 		// Trigger mouseenter event
 		cy.get(`[data-cy="youtube-overlay"]`).trigger('mouseenter');
-		cy.wait('@callToYouTube');
 
 		// The youtube files were loaded by the overlay is still showing
 		cy.get(`[data-cy="youtube-overlay"]`).should('be.visible');
 
 		// Listen for the ophan call made when the video is played
 		interceptPlayEvent(
-			'gu-video-youtube-35f9a38f-c0c5-4bbd-9395-e338c9e97d30',
+			'gu-video-youtube-2bc6f709-865e-49ae-b01b-8fc38eb4e9a7',
 		).as('ophanCall');
 
 		// Play video
 		cy.get(`[data-cy="youtube-overlay"]`).click();
 
-		cy.wait('@ophanCall', { timeout: 12000 });
+		cy.wait('@ophanCall', { timeout: 30000 });
 
 		// Video is playing, overlay is gone
 		cy.get(`[data-cy="youtube-overlay"]`).should('not.exist');
@@ -153,11 +158,14 @@ describe('YouTube Atom', function () {
 		cmpIframe().contains("It's your choice");
 		cmpIframe().find("[title='Manage my cookies']").click();
 		privacySettingsIframe().contains('Privacy settings');
-		privacySettingsIframe().find("[title='Accept all']").click();
+		privacySettingsIframe()
+			.find("[title='Accept all']", { timeout: 12000 })
+			.click();
 
 		// Wait for hydration
-		cy.scrollTo('bottom', { duration: 500 });
-		cy.get('footer').contains('Support the').should('be.visible');
+		cy.get('[data-component=youtube-atom]')
+			.parent()
+			.should('have.attr', 'gu-hydration', 'complete');
 
 		// Listen for the ophan call made when the video is played
 		interceptPlayEvent(
@@ -167,7 +175,7 @@ describe('YouTube Atom', function () {
 		// Play video (without triggering any pre load events)
 		cy.get(`[data-cy="youtube-overlay"]`).trigger('click');
 
-		cy.wait('@ophanCall', { timeout: 12000 });
+		cy.wait('@ophanCall', { timeout: 30000 });
 
 		// Video is playing, overlay is gone
 		cy.get(`[data-cy="youtube-overlay"]`).should('not.exist');
