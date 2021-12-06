79d80776c3a527a87f3f03a870e2793f2c6ff637
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-12-06 09:17:10 +0000

    refactor: `gu-hydration` > `data-hydrated`

--- dotcom-rendering/src/web/components/HydrateOnce.tsx
+++ dotcom-rendering/src/web/components/HydrateOnce.tsx
@@ -51,7 +51,7 @@ export const HydrateOnce = ({ rootId, children, waitFor = [] }: Props) => {
 	if (!element) return null;
 	start();
 	ReactDOM.hydrate(children, element, () => {
-		element.setAttribute('gu-hydration', 'complete');
+		element.setAttribute('data-hydrated', 'true');
 		end();
 	});
 	setAlreadyHydrated(true);
