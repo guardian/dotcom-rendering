1781df11cfc09dfa7bfd19711c3fafbc5048da3a
Nicolas Long <nicolas.long@theguardian.com>
2021-12-07 15:27:44 +0000

    Lift commands and remove redundant cdk config files

--- apps-rendering/cdk/lib/mobile-apps-rendering.test.ts
+++ apps-rendering/cdk/lib/mobile-apps-rendering.test.ts
@@ -4,27 +4,31 @@ import { App } from '@aws-cdk/core';
 import { MobileAppsRendering } from './mobile-apps-rendering';
 
 describe('The MobileAppsRendering stack', () => {
-  it('matches the snapshot', () => {
-    const app = new App();
-    const stack = new MobileAppsRendering(app, 'MobileAppsRendering', {
-      stack: 'mobile',
-      recordPrefix: "mobile-rendering",
-      asgMinSize: {CODE: 1, PROD: 3},
-      asgMaxSize: {CODE: 2, PROD: 12},
-    });
-    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-  });
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new MobileAppsRendering(app, 'MobileAppsRendering', {
+			stack: 'mobile',
+			recordPrefix: 'mobile-rendering',
+			asgMinSize: { CODE: 1, PROD: 3 },
+			asgMaxSize: { CODE: 2, PROD: 12 },
+		});
+		expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+	});
 });
 
 describe('The MobileAppsRenderingPreview stack', () => {
-  it('matches the snapshot', () => {
-    const app = new App();
-    const stack = new MobileAppsRendering(app, 'MobileAppsRenderingPreview', {
-      stack: 'mobile-preview',
-      recordPrefix: "mobile-preview-rendering",
-      asgMinSize: {CODE: 1, PROD: 1},
-      asgMaxSize: {CODE: 2, PROD: 2},
-    });
-    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-  });
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new MobileAppsRendering(
+			app,
+			'MobileAppsRenderingPreview',
+			{
+				stack: 'mobile-preview',
+				recordPrefix: 'mobile-preview-rendering',
+				asgMinSize: { CODE: 1, PROD: 1 },
+				asgMaxSize: { CODE: 2, PROD: 2 },
+			},
+		);
+		expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+	});
 });
