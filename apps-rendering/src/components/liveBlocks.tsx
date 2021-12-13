67c949d13bb013d9b33c739278d5e85f43e40a78
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2021-12-13 13:16:56 +0000

    Move liveblock header and consume from common

--- apps-rendering/src/components/liveBlocks.tsx
+++ apps-rendering/src/components/liveBlocks.tsx
@@ -1,13 +1,10 @@
 // ----- Imports ----- //
 
 import { css } from '@emotion/react';
-import { FirstPublished } from '@guardian/common-rendering/src/components/FirstPublished';
 import { LastUpdated } from '@guardian/common-rendering/src/components/LastUpdated';
 import LiveBlockContainer from '@guardian/common-rendering/src/components/liveBlockContainer';
 import type { ArticleFormat } from '@guardian/libs';
-import { headline, remSpace } from '@guardian/source-foundations';
 import { OptionKind, partition } from '@guardian/types';
-import { maybeRender } from 'lib';
 import type { LiveBlock } from 'liveBlock';
 import type { FC } from 'react';
 import { renderAll } from 'renderer';
@@ -18,23 +15,6 @@ interface LiveBlocksProps {
 	format: ArticleFormat;
 }
 
-interface BlockTitleProps {
-	title: string;
-}
-
-const BlockTitle: FC<BlockTitleProps> = ({ title }) => {
-	return (
-		<h2
-			css={css`
-				${headline.xxsmall({ fontWeight: 'bold' })}
-				margin-bottom: ${remSpace[2]}px;
-			`}
-		>
-			{title}
-		</h2>
-	);
-};
-
 const LiveBlocks: FC<LiveBlocksProps> = ({ blocks, format }) => {
 	return (
 		<>
@@ -43,33 +23,19 @@ const LiveBlocks: FC<LiveBlocksProps> = ({ blocks, format }) => {
 			{blocks.map((block) => {
 				// TODO: get page number
 				const blockLink = `${1}#block-${block.id}`;
+				const blockFirstPublished = block.firstPublished.kind === OptionKind.Some
+					? Number(block.firstPublished.value)
+					: undefined;
 
 				return (
 					<LiveBlockContainer
 						key={block.id}
 						id={block.id}
 						borderColour="black"
+						blockTitle={block.title}
+						blockFirstPublished={blockFirstPublished}
+						blockLink={blockLink}
 					>
-						<header
-							css={css`
-								padding-right: ${remSpace[3]}px;
-								display: flex;
-								flex-direction: column;
-							`}
-						>
-							{maybeRender(
-								block.firstPublished,
-								(firstPublished) => (
-									<FirstPublished
-										firstPublished={Number(firstPublished)}
-										blockLink={blockLink}
-									/>
-								),
-							)}
-							{block.title !== '' ? (
-								<BlockTitle title={block.title} />
-							) : null}
-						</header>
 
 						{renderAll(format, partition(block.body).oks)}
 
--- common-rendering/src/components/liveBlockContainer.tsx
+++ common-rendering/src/components/liveBlockContainer.tsx
@@ -1,36 +1,77 @@
 import { css } from "@emotion/react";
-
-import { from } from "@guardian/source-foundations";
-import { neutral, space } from "@guardian/source-foundations";
+import { neutral, from, remSpace, headline } from '@guardian/source-foundations';
+import { FirstPublished } from './FirstPublished';
 
 const LEFT_MARGIN_DESKTOP = 60;
-const SIDE_MARGIN = space[5];
+const SIDE_MARGIN = remSpace[5];
 const SIDE_MARGIN_MOBILE = 10;
+
+const Header = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<header
+			css={css`
+				padding-right: ${remSpace[3]};
+				display: flex;
+				flex-direction: column;
+			`}
+		>
+			{children}
+		</header>
+	);
+};
+
+const BlockTitle = ({ title }: { title: string }) => {
+	return (
+		<h2
+			css={css`
+				${headline.xxsmall({ fontWeight: 'bold' })}
+				margin-bottom: ${remSpace[2]};
+			`}
+		>
+			{title}
+		</h2>
+	);
+};
 
 const LiveBlockContainer = ({
 	id,
 	children,
 	borderColour,
+	blockTitle,
+	blockFirstPublished,
+	blockLink,
 }: {
 	id: string;
 	children: React.ReactNode;
 	borderColour: string;
+	blockTitle?: string;
+	blockFirstPublished?: number;
+	blockLink: string;
 }) => {
 	return (
 		<article
 			id={`block-${id}`}
 			key={id}
 			css={css`
-				padding: ${space[2]}px ${SIDE_MARGIN_MOBILE}px;
-				margin-bottom: ${space[3]}px;
+				padding: ${remSpace[2]} ${SIDE_MARGIN_MOBILE}px;
+				margin-bottom: ${remSpace[3]};
 				background: ${neutral[100]};
 				border-top: 1px solid ${borderColour};
 				${from.tablet} {
-					padding: ${space[2]}px ${SIDE_MARGIN}px;
+					padding: ${remSpace[2]} ${SIDE_MARGIN}px;
 					padding-left: ${LEFT_MARGIN_DESKTOP}px;
 				}
 			`}
 		>
+			<Header>
+				{blockFirstPublished && (
+					<FirstPublished
+						firstPublished={blockFirstPublished}
+						blockLink={blockLink}
+					/>
+				)}
+				{blockTitle ? <BlockTitle title={blockTitle} /> : null}
+			</Header>
 			{children}
 		</article>
 	);
