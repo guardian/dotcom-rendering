209b1a0ce4d555f5be6ad77f4187aa221f290649
David Lawes <david.lawes@guardian.co.uk>
2021-12-14 17:56:53 +0000

    add pagination logic for first page
    Co-authored-by: Marjan Kalanaki <marji_sound@yahoo.com>

--- apps-rendering/src/pagination.test.ts
+++ apps-rendering/src/pagination.test.ts
@@ -2,55 +2,6 @@ import { some } from '@guardian/types';
 import { LiveBlock } from 'liveBlock';
 import { getCurrentPage, Pagination, PageReference } from 'pagination';
 
-describe('pagination', () => {
-	it('', () => {
-		const blocks = generateBlocks(10);
-		const pageSize = 10;
-
-		const expectedCurrentPage: PageReference = {
-			blocks: blocks,
-			pageNumber: 1,
-			suffix: '',
-			isArchivePage: false,
-		};
-
-		const expectedPagination: Pagination = {
-			newest: {
-				blocks: blocks,
-				pageNumber: 1,
-				suffix: '',
-				isArchivePage: false,
-			},
-			newer: {
-				blocks: blocks,
-				pageNumber: 1,
-				suffix: '',
-				isArchivePage: false,
-			},
-			oldest: {
-				blocks: blocks,
-				pageNumber: 1,
-				suffix: '',
-				isArchivePage: false,
-			},
-			older: {
-				blocks: blocks,
-				pageNumber: 1,
-				suffix: '',
-				isArchivePage: false,
-			},
-			numberOfPages: 10,
-		};
-
-		expect(getCurrentPage(pageSize, blocks).currentPage).toEqual(
-			expectedCurrentPage,
-		);
-		expect(getCurrentPage(pageSize, blocks).pagination).toEqual(
-			expectedPagination,
-		);
-	});
-});
-
 const generateBlocks = (numberOfBlocks: number): LiveBlock[] => {
 	const block: LiveBlock = {
 		id: '1',
@@ -61,10 +12,139 @@ const generateBlocks = (numberOfBlocks: number): LiveBlock[] => {
 		body: [],
 	};
 
-	const liveBlocks = new Array(numberOfBlocks);
-	liveBlocks.map((_, index) => {
-		return { ...block, id: index };
-	});
+	const liveBlocks = [];
+	for(let i=0; i < numberOfBlocks; i++) {
+		liveBlocks.push({ ...block, id: `${i}` })
+	}
 
-	return liveBlocks;
+	return liveBlocks
 };
+
+describe('pagination', () => {
+	describe('first page', () => {
+		describe('given total number of blocks is less than page size', () => {
+			const blocks = generateBlocks(4);
+			const pageSize = 10;
+			const result = getCurrentPage(pageSize, blocks);
+
+			it('should return expected currentPage object', () => {
+				const expectedCurrentPage: PageReference = {
+					blocks: blocks,
+					pageNumber: 1,
+					suffix: '',
+					isArchivePage: false,
+				};
+
+				expect(result.currentPage).toEqual(expectedCurrentPage);
+			});
+
+			it('should return expected pagination object', () => {
+				const expectedPagination: Pagination = {
+					numberOfPages: 1,
+				};
+
+				expect(result.pagination).toEqual(expectedPagination);
+			});
+		});
+
+		describe('given total number of blocks is greater than page size (only 1 page)', () => {
+			const blocks = generateBlocks(15);
+			const pageSize = 10;
+			const result = getCurrentPage(pageSize, blocks);
+
+			it('should return expected currentPage object', () => {
+				const expectedCurrentPage: PageReference = {
+					blocks: blocks,
+					pageNumber: 1,
+					suffix: '',
+					isArchivePage: false,
+				};
+
+				expect(result.currentPage).toEqual(expectedCurrentPage);
+			});
+
+			it('should return expected pagination object', () => {
+				const expectedPagination: Pagination = {
+					numberOfPages: 1,
+				};
+
+				expect(result.pagination).toEqual(expectedPagination);
+			});
+		});
+
+		describe('given total number of blocks is greater than page size (2 pages)', () => {
+			const blocks = generateBlocks(29);
+			const pageSize = 10;
+			const result = getCurrentPage(pageSize, blocks);
+
+			it('should return expected currentPage object', () => {
+				const expectedCurrentPage: PageReference = {
+					blocks: blocks.slice(0, 19),
+					pageNumber: 1,
+					suffix: '',
+					isArchivePage: false,
+				};
+
+				expect(result.currentPage).toEqual(expectedCurrentPage);
+			});
+
+			it('should return expected pagination object', () => {
+				const expectedPagination: Pagination = {
+					oldest: {
+						blocks: blocks.slice(19, 29),
+						pageNumber: 2,
+						suffix: '',
+						isArchivePage: false,
+					},
+					older: {
+						blocks: blocks.slice(19, 29),
+						pageNumber: 2,
+						suffix: '',
+						isArchivePage: false,
+					},
+					numberOfPages: 2,
+				};
+
+				expect(result.pagination).toEqual(expectedPagination);
+			});
+		});
+
+		describe('given total number of blocks is greater than page size (3 pages)', () => {
+			const blocks = generateBlocks(35);
+			const pageSize = 10;
+			const result = getCurrentPage(pageSize, blocks);
+
+			it('should return expected currentPage object', () => {
+				const expectedCurrentPage: PageReference = {
+					blocks: blocks.slice(0, 15),
+					pageNumber: 1,
+					suffix: '',
+					isArchivePage: false,
+				};
+
+				expect(result.currentPage).toEqual(expectedCurrentPage);
+			});
+
+			it('should return expected pagination object', () => {
+				const expectedPagination: Pagination = {
+					oldest: {
+						blocks: blocks.slice(25, 35),
+						pageNumber: 3,
+						suffix: '',
+						isArchivePage: false,
+					},
+					older: {
+						blocks: blocks.slice(15, 25),
+						pageNumber: 2,
+						suffix: '',
+						isArchivePage: false,
+					},
+					numberOfPages: 3,
+				};
+
+				expect(result.pagination).toEqual(expectedPagination);
+			});
+		});
+	});
+});
+
--- apps-rendering/src/pagination.ts
+++ apps-rendering/src/pagination.ts
@@ -1,4 +1,3 @@
-import type { Option } from '@guardian/types';
 import type { LiveBlock } from 'liveBlock';
 
 export type PageReference = {
@@ -9,67 +8,103 @@ export type PageReference = {
 };
 
 export type Pagination = {
-	newest: PageReference;
-	newer: PageReference;
-	oldest: PageReference;
-	older: PageReference;
+	newest?: PageReference;
+	newer?: PageReference;
+	oldest?: PageReference;
+	older?: PageReference;
 	numberOfPages: number;
 };
 
 export type LiveBlogCurrentPage = {
 	currentPage: PageReference;
-	pagination: Option<Pagination>;
+	pagination: Pagination;
 };
 
-const firstPage = (
-	blocks: LiveBlock[],
-	filterKeyEvents: boolean,
-): PageReference => {
+const getFirstPage = (blocks: LiveBlock[], pageSize: number, pageNumber: number): PageReference => {
+	const { remainder } = getPages(pageSize, blocks)
+	console.log(getPages(pageSize, blocks))
+	const sliceAt = remainder + pageSize;
+
 	return {
-		blocks: blocks,
-		pageNumber: 1,
-		suffix: `?filterKeyEvents=${filterKeyEvents}`,
+		blocks:  blocks.slice(0, sliceAt),
+		pageNumber,
+		suffix: '',
 		isArchivePage: false,
 	};
-};
+}
+
+const getOldestPage = (blocks: LiveBlock[], pageSize: number): PageReference | undefined => {
+	const { numberOfPages } = getPages(pageSize, blocks);
+
+	if (numberOfPages === 1) return undefined;
+
+	return {
+		blocks: blocks.slice(blocks.length - pageSize, blocks.length),
+		pageNumber: numberOfPages,
+		suffix: '',
+		isArchivePage: false,
+	}
+}
+
+const getOlderPage = (blocks: LiveBlock[], pageSize: number, pageNumber: number): PageReference | undefined => {
+
+	const { firstPageLength, numberOfPages } = getPages(pageSize, blocks)
+	// start slice from: end of the current page -> how many blocks first page has
+	// end slice at: end of current page + page size
+
+	if (numberOfPages === 1) return undefined;
+
+	return {
+		blocks: blocks.slice(firstPageLength, firstPageLength + pageSize),
+		pageNumber,
+		suffix: '',
+		isArchivePage: false,
+	}
+}
+
+// const firstPage = (
+// 	blocks: LiveBlock[],
+// 	filterKeyEvents: boolean,
+// ): PageReference => {
+// 	return {
+// 		blocks: blocks,
+// 		pageNumber: 1,
+// 		suffix: `?filterKeyEvents=${filterKeyEvents}`,
+// 		isArchivePage: false,
+// 	};
+// };
+
+
 
 const getPages = (pageSize: number, blocks: LiveBlock[]) => {
-	const length = blocks.length;
-	const remainder = length % pageSize;
+	const remainder = blocks.length % pageSize;
+	return {
+		length : blocks.length,
+		remainder,
+		numberOfPages : getNumberOfPages(pageSize, blocks),
+		firstPageLength : pageSize + remainder,
+	}
+};
+
+const getNumberOfPages = (pageSize: number, blocks: LiveBlock[]): number => {
+	const noPages = Math.floor(blocks.length / pageSize);
+
+	if (noPages < 1) return 1;
+	return noPages;
 };
 
 export const getCurrentPage = (
 	pageSize: number,
 	blocks: LiveBlock[],
 ): LiveBlogCurrentPage => {
+	let currentPage = 1;
+
 	return {
-		currentPage: {
-			blocks: blocks,
-			pageNumber: 1,
-			suffix: '',
-			isArchivePage: false,
-		},
+		currentPage: getFirstPage(blocks, pageSize, currentPage),
 		pagination: {
-			newest: firstPage(blocks, false),
-			newer: {
-				blocks: blocks,
-				pageNumber: 1,
-				suffix: '',
-				isArchivePage: false,
-			},
-			oldest: {
-				blocks: blocks,
-				pageNumber: 1,
-				suffix: '',
-				isArchivePage: false,
-			},
-			older: {
-				blocks: blocks,
-				pageNumber: 1,
-				suffix: '',
-				isArchivePage: false,
-			},
-			numberOfPages: 10,
+			older: getOlderPage(blocks, pageSize, currentPage + 1),
+			oldest: getOldestPage(blocks, pageSize),
+			numberOfPages: getNumberOfPages(pageSize, blocks),
 		},
 	};
 };
