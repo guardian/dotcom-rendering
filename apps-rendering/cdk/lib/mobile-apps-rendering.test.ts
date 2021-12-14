1a5a1468b22c56d6b004019c352457c516c53b04
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2021-12-14 12:48:31 +0000

    Fix indentation

--- apps-rendering/cdk/lib/mobile-apps-rendering.test.ts
+++ apps-rendering/cdk/lib/mobile-apps-rendering.test.ts
@@ -4,14 +4,14 @@ import { App } from '@aws-cdk/core';
 import { MobileAppsRendering } from './mobile-apps-rendering';
 
 describe('The MobileAppsRendering stack', () => {
-	it('matches the snapshot', () => {
-		const app = new App();
-		const stack = new MobileAppsRendering(app, 'MobileAppsRendering', {
-			stack: 'mobile',
+  it('matches the snapshot', () => {
+    const app = new App();
+    const stack = new MobileAppsRendering(app, 'MobileAppsRendering', {
+      stack: 'mobile',
       recordPrefix: "mobile-rendering",
-		});
-		expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-	});
+    });
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
 });
 
 describe('The MobileAppsRenderingPreview stack', () => {
