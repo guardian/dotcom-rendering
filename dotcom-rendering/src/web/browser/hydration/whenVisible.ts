88bfa5f20f28f4105c2b753a6b2c7241e00275ac
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-12-16 12:46:56 +0000

    refactor: `marker` > `element`

--- dotcom-rendering/src/web/browser/hydration/whenVisible.ts
+++ dotcom-rendering/src/web/browser/hydration/whenVisible.ts
@@ -2,10 +2,10 @@
  * Use this function to delay execution of something until an element is visible
  * in the viewport
  *
- * @param marker : The html element that we want to observe;
- * @param callback : This is fired when the marker is visible in the viewport
+ * @param element : The html element that we want to observe;
+ * @param callback : This is fired when the element is visible in the viewport
  */
-export const whenVisible = (marker: HTMLElement, callback: () => void) => {
+export const whenVisible = (element: HTMLElement, callback: () => void) => {
 	if ('IntersectionObserver' in window) {
 		const io = new IntersectionObserver(([entry]) => {
 			if (!entry.isIntersecting) return;
@@ -14,7 +14,7 @@ export const whenVisible = (marker: HTMLElement, callback: () => void) => {
 			callback();
 		});
 
-		io.observe(marker);
+		io.observe(element);
 	} else {
 		// IntersectionObserver is not supported so failover to calling back at the end of the call stack
 		setTimeout(() => callback(), 0);
