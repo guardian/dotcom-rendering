4805d0eb2eb60214bfd655d64bfef1eea7437dbe
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-12-16 12:45:58 +0000

    feat: Added fallback for when there's no support

--- dotcom-rendering/src/web/browser/hydration/whenVisible.ts
+++ dotcom-rendering/src/web/browser/hydration/whenVisible.ts
@@ -6,12 +6,17 @@
  * @param callback : This is fired when the marker is visible in the viewport
  */
 export const whenVisible = (marker: HTMLElement, callback: () => void) => {
-	const io = new IntersectionObserver(([entry]) => {
-		if (!entry.isIntersecting) return;
-		// Disconnect this IntersectionObserver once seen
-		io.disconnect();
-		callback();
-	});
+	if ('IntersectionObserver' in window) {
+		const io = new IntersectionObserver(([entry]) => {
+			if (!entry.isIntersecting) return;
+			// Disconnect this IntersectionObserver once seen
+			io.disconnect();
+			callback();
+		});
 
-	io.observe(marker);
+		io.observe(marker);
+	} else {
+		// IntersectionObserver is not supported so failover to calling back at the end of the call stack
+		setTimeout(() => callback(), 0);
+	}
 };
