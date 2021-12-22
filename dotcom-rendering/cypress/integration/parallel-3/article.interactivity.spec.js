7512a942581e6215f36054492cdec1b2bad8d56b
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-12-22 18:21:24 +0000

    fix: Select first sub nav here

--- dotcom-rendering/cypress/integration/parallel-3/article.interactivity.spec.js
+++ dotcom-rendering/cypress/integration/parallel-3/article.interactivity.spec.js
@@ -155,7 +155,7 @@ describe('Interactivity', function () {
 				);
 			});
 
-			it('should expand the subnav when "More" is clicked', function () {
+			it.only('should expand the subnav when "More" is clicked', function () {
 				cy.viewport('iphone-x');
 				cy.visit(`/Article?url=${articleUrl}`);
 				// Wait for hydration
@@ -165,6 +165,7 @@ describe('Interactivity', function () {
 				// Click Show in the first sub nav
 				cy.get('[data-cy=subnav-toggle]').first().click();
 				cy.get('[data-cy=sub-nav]')
+					.first()
 					.contains('Tech')
 					.should('be.visible');
 			});
