85fccc8fe11ec54a8c9f5ee11a1a192540658497
Ravi Lal <arelra@users.noreply.github.com>
2022-01-10 19:18:56 +0000

    Remove babel plugins handled by swc

--- dotcom-rendering/babel.config.js
+++ dotcom-rendering/babel.config.js
@@ -1,22 +1,6 @@
 module.exports = {
     plugins: [
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-transform-react-jsx',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        [
-            'module-resolver',
-            {
-                root: ['./'],
-                alias: {
-                    '@root': '.',
-                    '@frontend': './src',
-                },
-            },
-        ],
         'babel-plugin-px-to-rem',
-        '@babel/plugin-transform-runtime',
         '@loadable/babel-plugin',
 		'@emotion/babel-plugin'
     ],
