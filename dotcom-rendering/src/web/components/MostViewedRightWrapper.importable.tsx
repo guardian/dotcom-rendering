53bbefa53cb87b38fb86d2e7083c085e38ed93f6
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-01-12 16:20:48 +0000

    refactor: We no longer need these flex styles here

--- dotcom-rendering/src/web/components/MostViewedRightWrapper.importable.tsx
+++ dotcom-rendering/src/web/components/MostViewedRightWrapper.importable.tsx
@@ -16,8 +16,6 @@ const MOSTVIEWED_STICKY_HEIGHT = 1059;
 // Styling the data island root so it stretches to cover the full height available in the container.
 // Requires us to subtract the height of its sibling in the container (StickyAd).
 const stretchWrapperHeight = css`
-	display: flex;
-	flex-direction: column;
 	height: ${`calc(100% - ${MOSTVIEWED_STICKY_HEIGHT}px)`};
 `;
 
