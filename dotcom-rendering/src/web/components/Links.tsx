e7c0fab29ab77eb5db1480eba11eb7577358857b
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-01-14 12:33:29 +0000

    hide supporter revenue links in footer

--- dotcom-rendering/src/web/components/Links.tsx
+++ dotcom-rendering/src/web/components/Links.tsx
@@ -203,18 +203,6 @@ export const Links = ({
 	];
 	return (
 		<div data-print-layout="hide" css={linksStyles}>
-			{/* {showSupporterCTA && supporterCTA !== '' && (
-				<>
-					<div css={seperatorStyles} />
-					<a
-						href={supporterCTA}
-						css={[linkTablet({ showAtTablet: false }), linkStyles]}
-						data-link-name="nav2 : supporter-cta"
-					>
-						Subscriptions
-					</a>
-				</>
-			)} */}
 			 <div css={seperatorStyles} />
 			<a
 				href="https://support.theguardian.com/subscribe/weekly?INTCMP=header_supporter_cta&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%22header_supporter_cta%22%7D"
