55a717e9bb158ee8f9d8ab32f2d7def2764aae19
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2022-01-24 13:03:50 +0000

    Added "modern" to footer to distinguish AR

--- apps-rendering/src/components/footerContent.tsx
+++ apps-rendering/src/components/footerContent.tsx
@@ -44,7 +44,7 @@ const FooterContent: FC<Props> = ({ isCcpa }) => {
 	return (
 		<div id="js-footer">
 			&#169; {currentYear} Guardian News and Media Limited or its
-			affiliated companies. All rights reserved.
+			affiliated companies. All rights reserved. (modern)
 			<br />
 			<PrivacySettings isCcpa={isCcpa} />
 			<a
