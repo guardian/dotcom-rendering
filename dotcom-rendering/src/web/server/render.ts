eb46f4c754d2dc0f9c204b94253aedd43a28e091
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-01-27 10:52:45 +0000

    fix: Use the validated `data` var here instead

--- dotcom-rendering/src/web/server/render.ts
+++ dotcom-rendering/src/web/server/render.ts
@@ -14,10 +14,10 @@ export const renderArticle = (
 	try {
 		const data = validateAsCAPIType(body);
 		const CAPI = {
-			...body,
+			...data,
 			blocks: enhanceBlocks(data.blocks, data.format),
 			standfirst: enhanceStandfirst(data.standfirst),
-		} as CAPIType;
+		};
 		const resp = document({
 			data: {
 				CAPI,
