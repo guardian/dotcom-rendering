3597fce007af9f20c0c0f1b7d38c33a060613d89
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-01-27 12:12:36 +0000

    `enhanceCAPI` > `enhanceBlocks` (#3880)
    * WIP: Refactor enhance CAPI to enhance blocks
    
    * fix: Removed unused import
    
    * chore: Linting
    
    * refactor: `render` uses new patterns
    
    * fix: `InteractiveContentsBlockComponent` story
    
    * refactor: Revert changes to `enhancePlaceholders`
    
    * refactor: Removed `enhancePlaceholders` function
    
    We ( @HarryFischer and I) have agreed to revisit this problem and reconsider other solutions to bringing structured data into interactives. Ideally we would be solving this issue higher up the stack, not in the (multiple) rendering platforms
    
    * fix: Missing alias on import
    
    * fix: Missing alias on import (drive by)
    
    * fix: Tests
    
    * fix: Rename this file to stop jest trying to run it
    
    * refactor: Moved `transformDots` into its own file
    
    * refactor: Removed unused code
    
    * refactor: `addDividers` > `enhanceDividers`
    
    * umm: Okay yarn, if you need to do this then I'm down with it
    
    * refactor: `add-dots` > `enhance-dots`
    
    * refactor: Remove templating code and docs as we no longer offer this
    
    * fix: Use the validated `data` var here instead
    
    Co-authored-by: Olly <9575458+OllysCoding@users.noreply.github.com>

---
+++ dotcom-rendering/src/model/enhance-dots.test.ts
@@ -0,0 +1,100 @@
+import { enhanceDots } from './enhance-dots';
+
+const blockMetaData = {
+	id: '123',
+	primaryDateLine: 'Wed 9 Dec 2020 06.30 GMT',
+	secondaryDateLine: 'Last modified on Wed 9 Dec 2020 13.40 GMT',
+};
+
+describe('Middot Tests', () => {
+	it('Output should not be the same as input as dot has been replaced', () => {
+		const input: Block[] = [
+			{
+				...blockMetaData,
+				elements: [
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>I am the first paragraph</p>',
+					},
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>• I should have a dot.</p>',
+					},
+				],
+			},
+		];
+
+		const expectedOutput: Block[] = [
+			{
+				...blockMetaData,
+				elements: [
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>I am the first paragraph</p>',
+					},
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p><span data-dcr-style="bullet"></span> I should have have a dot.</p>',
+					},
+				],
+			},
+		];
+
+		expect(enhanceDots(input)).not.toBe(expectedOutput);
+	});
+
+	it('It does not incorrectly replace * with dot spans', () => {
+		const input: Block[] = [
+			{
+				...blockMetaData,
+				elements: [
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>I am the first paragraph</p>',
+					},
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>*</p>',
+					},
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>I am text.</p>',
+					},
+				],
+			},
+		];
+
+		const expectedOutput: Block[] = [
+			{
+				...blockMetaData,
+				elements: [
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>I am the first paragraph</p>',
+					},
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>*</p>',
+					},
+
+					{
+						_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+						elementId: 'mockId',
+						html: '<p>I am text.</p>',
+					},
+				],
+			},
+		];
+
+		expect(enhanceDots(input)).toEqual(expectedOutput);
+	});
+});
