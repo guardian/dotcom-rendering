6110b79505dbe15e1db06cdb0ce38e45a0f72be2
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-02-02 15:29:17 +0000

    fix: Adding id back to interactives

--- dotcom-rendering/src/web/components/ArticleBody.tsx
+++ dotcom-rendering/src/web/components/ArticleBody.tsx
@@ -112,6 +112,7 @@ export const ArticleBody = ({
 		<div
 			// eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
 			tabIndex={0}
+			id="maincontent"
 			css={[
 				isInteractive ? null : bodyPadding,
 				globalH2Styles(format.display),
