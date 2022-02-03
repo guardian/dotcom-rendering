a3a8eb52b3e3c8a2dc2e12dc844a394beab20d82
Max Duval <max.duval@theguardian.com>
2022-02-03 16:09:47 +0000

    convert reporter plugin to TS

--- dotcom-rendering/scripts/webpack/gu-stats-report-plugin.ts
+++ dotcom-rendering/scripts/webpack/gu-stats-report-plugin.ts
@@ -1,9 +1,32 @@
-const fetch = require('node-fetch');
-const os = require('os');
-const { exec } = require('child_process');
+import fetch from 'node-fetch';
+import os from 'os';
+import { exec } from 'child_process';
+import { Compiler, Stats } from 'webpack';
+
+type Config = Partial<{
+	displayDisclaimer: boolean;
+	buildName: string;
+	project: string;
+	team: string;
+	sessionId: string;
+}>;
 
 class GuStatsReportPlugin {
-	constructor(config) {
+	buildName?: string;
+
+	project?: string;
+
+	team?: string;
+
+	sessionId?: string;
+
+	buildCount: number;
+
+	gitBranch?: string;
+
+	gitHash?: string;
+
+	constructor(config?: Config) {
 		this.buildName = config?.buildName;
 		this.project = config?.project;
 		this.team = config?.team;
@@ -48,8 +71,8 @@ class GuStatsReportPlugin {
 		});
 	}
 
-	apply(compiler) {
-		const onDone = (stats) => {
+	apply(compiler: Compiler) {
+		const onDone = (stats: Stats) => {
 			// Increment the buildCount
 			this.buildCount += 1;
 
@@ -58,72 +81,82 @@ class GuStatsReportPlugin {
 					'[gu-stats-report] Unable to report stats - invalid config',
 				);
 
+			const formatter = new Intl.NumberFormat('en-GB');
+
+			const payload = {
+				label: 'buildstats',
+				properties: [
+					{
+						name: 'project',
+						value: 'dotcom-rendering',
+					},
+					{
+						name: 'team',
+						value: 'dotcom',
+					},
+					{
+						name: 'buildName',
+						value: this.buildName,
+					},
+					{
+						name: 'buildCount',
+						value: this.buildCount,
+					},
+					{
+						name: 'gitHash',
+						value: this.gitHash || 'unknown',
+					},
+					{
+						name: 'gitBranch',
+						value: this.gitBranch || 'unknown',
+					},
+					{
+						name: 'sessionId',
+						value: this.sessionId,
+					},
+					{
+						name: 'cpus',
+						value: os.cpus().length,
+					},
+					{
+						name: 'memoryKb',
+						value: Math.round(os.totalmem() / 1024),
+					},
+				],
+				metrics: [
+					{
+						name: 'buildTime',
+						value:
+							stats.compilation.endTime -
+								stats.compilation.startTime || 0,
+					},
+					{
+						name: 'memoryUsageKb',
+						// Memory usage in kb to 2dp
+						// Why use RSS? https://stackoverflow.com/questions/12023359/what-do-the-return-values-of-node-js-process-memoryusage-stand-for
+						value:
+							Math.round(
+								(process.memoryUsage().rss / 1024) * 100,
+							) / 100,
+					},
+				],
+			};
+
 			const URL = 'https://logs.guardianapis.com/log';
 			fetch(URL, {
 				method: 'POST',
-				body: JSON.stringify({
-					label: 'buildstats',
-					properties: [
-						{
-							name: 'project',
-							value: 'dotcom-rendering',
-						},
-						{
-							name: 'team',
-							value: 'dotcom',
-						},
-						{
-							name: 'buildName',
-							value: this.buildName,
-						},
-						{
-							name: 'buildCount',
-							value: this.buildCount,
-						},
-						{
-							name: 'gitHash',
-							value: this.gitHash || 'unknown',
-						},
-						{
-							name: 'gitBranch',
-							value: this.gitBranch || 'unknown',
-						},
-						{
-							name: 'sessionId',
-							value: this.sessionId,
-						},
-						{
-							name: 'cpus',
-							value: os.cpus().length,
-						},
-						{
-							name: 'memoryKb',
-							value: Math.round(os.totalmem() / 1024),
-						},
-					],
-					metrics: [
-						{
-							name: 'buildTime',
-							value:
-								stats.compilation.endTime -
-									stats.compilation.startTime || 0,
-						},
-						{
-							name: 'memoryUsageKb',
-							// Memory usage in kb to 2dp
-							// Why use RSS? https://stackoverflow.com/questions/12023359/what-do-the-return-values-of-node-js-process-memoryusage-stand-for
-							value:
-								Math.round(
-									(process.memoryUsage().rss / 1024) * 100,
-								) / 100,
-						},
-					],
-				}),
+				body: JSON.stringify(payload),
 			})
 				.then(({ ok, status }) =>
 					ok
 						? console.log(
 								`[gu-stats-report] Stats reported for '${this.buildName}' build. (Session build count - ${this.buildCount})`,
+								...payload.metrics.map(
+									(metric) =>
+										`${metric.name}: ${formatter.format(
+											metric.value,
+										)}`,
+								),
 						  )
 						: console.error(
 								`[gu-stats-report] ${this.buildName} (${this.buildCount}): Failed to report stats (${status})`,
@@ -143,4 +176,4 @@ class GuStatsReportPlugin {
 	}
 }
 
-module.exports = GuStatsReportPlugin;
+export default GuStatsReportPlugin;
