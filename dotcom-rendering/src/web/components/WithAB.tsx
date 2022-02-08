617985e38833d5e70e23ec3f9a97726c43726810
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-02-08 16:20:52 +0000

    refactor: Created `WithAB` wrapper

---
+++ dotcom-rendering/src/web/components/WithAB.tsx
@@ -0,0 +1,42 @@
+import { CoreAPIConfig, OphanAPIConfig } from '@guardian/ab-core/dist/types';
+import { ABProvider } from '@guardian/ab-react';
+import { getCypressSwitches } from '../experiments/cypress-switches';
+import { getForcedParticipationsFromUrl } from '../lib/getAbUrlHash';
+
+type Props = {
+	arrayOfTestObjects: CoreAPIConfig['arrayOfTestObjects'];
+	abTestSwitches: CoreAPIConfig['abTestSwitches'];
+	pageIsSensitive: CoreAPIConfig['pageIsSensitive'];
+	mvtId: CoreAPIConfig['mvtId'];
+	ophanRecord: OphanAPIConfig['ophanRecord'];
+	children: JSX.Element;
+};
+export const WithAB = ({
+	arrayOfTestObjects,
+	abTestSwitches,
+	pageIsSensitive,
+	mvtId,
+	ophanRecord,
+	children,
+}: Props) => {
+	const windowHash = window && window.location && window.location.hash;
+	// Get the forced switches to use for when running within cypress
+	// Is empty object if not in cypress
+	const cypressAbSwitches = getCypressSwitches();
+	return (
+		<ABProvider
+			arrayOfTestObjects={arrayOfTestObjects}
+			abTestSwitches={{
+				...abTestSwitches,
+				...cypressAbSwitches, // by adding cypress switches below CAPI, we can override any production switch in Cypress
+			}}
+			pageIsSensitive={pageIsSensitive}
+			mvtMaxValue={1000000}
+			mvtId={mvtId}
+			ophanRecord={ophanRecord}
+			forcedTestVariants={getForcedParticipationsFromUrl(windowHash)}
+		>
+			{children}
+		</ABProvider>
+	);
+};
