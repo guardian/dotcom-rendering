ace6b45162e2009195c4aa06915a4e4bb744ed74
Ravi Lal <arelra@users.noreply.github.com>
2022-02-09 18:51:58 +0000

    Remove outdated webpack comments re babel/preset-modules

--- dotcom-rendering/scripts/webpack/webpack.config.browser.js
+++ dotcom-rendering/scripts/webpack/webpack.config.browser.js
@@ -5,7 +5,6 @@ const PROD = process.env.NODE_ENV === 'production';
 const DEV = process.env.NODE_ENV === 'development';
 const GITHUB = process.env.CI_ENV === 'github';
 
-// We need to distinguish files compiled by @babel/preset-env with the prefix "legacy"
 const generateName = (isLegacyJS) => {
 	const legacyString = isLegacyJS ? '.legacy' : '';
 	const chunkhashString = PROD && !GITHUB ? '.[chunkhash]' : '';
@@ -59,9 +58,6 @@ module.exports = ({ isLegacyJS, sessionId }) => ({
 						options: {
 							presets: [
 								'@babel/preset-react',
-								// @babel/preset-env is used for legacy browsers
-								// @babel/preset-modules is used for modern browsers
-								// this allows us to reduce bundle sizes
 								isLegacyJS
 									? [
 											'@babel/preset-env',
