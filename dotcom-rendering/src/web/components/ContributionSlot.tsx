2821e7df597573ffece867163188bb98f2d752b3
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-02-10 20:08:35 +0000

    refactor: Created `ShadyPie`

--- dotcom-rendering/src/web/components/ContributionSlot.tsx
+++ dotcom-rendering/src/web/components/ContributionSlot.tsx
@@ -1,18 +1,7 @@
 import { css } from '@emotion/react';
 import { getCookie } from '@guardian/libs';
+import { ShadyPie } from './ShadyPie';
 import { WhenAdBlockInUse } from './WhenAdBlockInUse';
-
-const params = new URLSearchParams();
-params.set(
-	'acquisitionData',
-	JSON.stringify({
-		componentType: 'ACQUISITIONS_OTHER',
-		source: 'GUARDIAN_WEB',
-		campaignCode: 'shady_pie_open_2019',
-		componentId: 'shady_pie_open_2019',
-	}),
-);
-params.set('INTCMP', 'shady_pie_open_2019');
 
 export const ContributionSlot = ({
 	shouldHideReaderRevenue,
@@ -32,15 +21,7 @@ export const ContributionSlot = ({
 					top: 0;
 				`}
 			>
-				<a
-					href={`https://support.theguardian.com/uk/subscribe/digital?${params.toString()}`}
-				>
-					<img
-						src="https://uploads.guim.co.uk/2020/10/02/Digisubs_MPU_c1_my_opt.png"
-						width="300"
-						alt=""
-					/>
-				</a>
+				<ShadyPie />
 			</div>
 		</WhenAdBlockInUse>
 	);
