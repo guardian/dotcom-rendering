18bba024c355e11e3e143e90745c986cc6aa3b07
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2022-02-11 11:23:38 +0000

    Update commercial metrics code to work with refactored commercial-core

--- dotcom-rendering/src/web/components/CommercialMetrics.importable.tsx
+++ dotcom-rendering/src/web/components/CommercialMetrics.importable.tsx
@@ -1,8 +1,7 @@
 import type { ABTest } from '@guardian/ab-core';
-import { sendCommercialMetrics } from '@guardian/commercial-core';
+import { initCommercialMetrics, bypassCommercialMetricsSampling } from '@guardian/commercial-core';
 import { getCookie } from '@guardian/libs';
 import { useAB } from '@guardian/ab-react';
-import { useDocumentVisibilityState } from '../lib/useDocumentHidden';
 import { useAdBlockInUse } from '../lib/useAdBlockInUse';
 import { WithABProvider } from './WithABProvider';
 import { useOnce } from '../lib/useOnce';
@@ -20,11 +19,8 @@ const CommercialMetricsWithAB = ({ enabled }: { enabled: boolean }) => {
 	const pageViewId = window.guardian?.config?.ophan?.pageViewId;
 	const browserId = getCookie({ name: 'bwid', shouldMemoize: true });
 	const ABTestAPI = useAB();
-	const visibilityState = useDocumentVisibilityState();
 
 	const adBlockerInUse = useAdBlockInUse();
-	// only send metrics when visibility state changes to hidden;
-	const isHidden = visibilityState === 'hidden' || undefined;
 
 	useOnce(() => {
 		// Only send metrics if the switch is enabled
@@ -37,23 +33,25 @@ const CommercialMetricsWithAB = ({ enabled }: { enabled: boolean }) => {
 		const shouldForceMetrics = ABTestAPI.allRunnableTests(tests).some(
 			(test) => testsToForceMetrics.map((t) => t.id).includes(test.id),
 		);
-		const userIsInSamplingGroup = Math.random() <= 1 / 100;
+
 		const isDev =
 			window.guardian.config.page.isDev ||
 			window.location.hostname.includes('localhost');
 
-		if (isDev || shouldForceMetrics || userIsInSamplingGroup) {
-			sendCommercialMetrics(
-				pageViewId,
-				browserId || undefined,
-				isDev,
-				adBlockerInUse,
-			);
-			// TODO: capture CWV also, to ensure commercial performance
-			// doesn’t come at the expense of user experience.
-			// See https://git.io/JP68Q in `frontend`
+		initCommercialMetrics(
+			pageViewId,
+			browserId || undefined,
+			isDev,
+			adBlockerInUse,
+		);
+		// TODO: capture CWV also, to ensure commercial performance
+		// doesn’t come at the expense of user experience.
+		// See https://git.io/JP68Q in `frontend`
+
+		if (shouldForceMetrics) {
+			bypassCommercialMetricsSampling();
 		}
-	}, [ABTestAPI, pageViewId, adBlockerInUse, isHidden, enabled]);
+	}, [ABTestAPI, pageViewId, adBlockerInUse, enabled]);
 
 	// We don’t render anything
 	return null;
