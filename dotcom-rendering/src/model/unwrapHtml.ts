6d97b738f5f1db662abb5bd0f281270c87d83bd5
Max Duval <max.duval@theguardian.com>
2022-02-21 11:49:33 +0000

    refactor(unwrapHtml): Narrow types and better doc
    The existing method was notoriously hard to reason about.
    This changes makes the public API more narrowly typed.
    When specifying a prefix or suffix, the compiler will complain if the
    strings do not match opening and closing HTML tag names.

--- dotcom-rendering/src/model/unwrapHtml.ts
+++ dotcom-rendering/src/model/unwrapHtml.ts
@@ -1,48 +1,55 @@
-const stripTags = (html: string, prefix: string, suffix: string) => {
-	return html.slice(prefix.length, html.length - suffix.length);
-};
+type HTMLTag = keyof HTMLElementTagNameMap;
+
+type Prefix = `<${HTMLTag}${'' | ` ${string}`}>`;
+type Suffix = `</${HTMLTag}>`;
+
+const stripTags = (html: string, prefix: Prefix, suffix: Suffix) =>
+	html.slice(prefix.length, html.length - suffix.length);
 
 /**
-unwrapHtml will check whether the html passed in starts with any of the prefixes
-and ends with the suffixes and will then return the html with the prefix and
-suffix stripped off, as well as the matching element that we want to 'rewrap'
-the html in
+ * This method unwraps a string of HTML from an enclosing tag.
+ *
+ * If the HTML passed in matches any of the fixes, in order, we will unwrap.
+ * A fix matches if it starts with the prefix and ends with the suffix provided.
+ *
+ * - `willUnwrap === true`:
+ * 	Return the html with the prefix and suffix stripped off,
+ * 	as well as the matching element that we want to rewrap the HTML in.
+ *
+ * - `willUnwrap === false`:
+ * 	Return the original HTML string and the `p` unwrappedElement.
  */
 export const unwrapHtml = ({
 	fixes,
 	html,
 }: {
-	fixes: { prefix: string; suffix: string; unwrappedElement?: string }[];
+	fixes: {
+		prefix: Prefix;
+		suffix: Suffix;
+		unwrappedElement?: HTMLTag;
+	}[];
 	html: string;
 }): {
 	unwrappedHtml: string;
 	willUnwrap: boolean;
-	unwrappedElement: string;
+	unwrappedElement: HTMLTag;
 } => {
-	const fixFilteredToMatchingHtmlTags = fixes.filter(
+	const matchingFix = fixes.find(
 		({ prefix, suffix }) =>
 			html.startsWith(prefix) && html.endsWith(suffix),
 	);
 
-	// We only unwrap if we have a match on the start and end html tags to one of the passed 'fixes'
-	const willUnwrap = fixFilteredToMatchingHtmlTags.length === 1;
-	const matchingFix =
-		fixFilteredToMatchingHtmlTags && fixFilteredToMatchingHtmlTags[0];
+	const unwrappedHtml = matchingFix
+		? // Chop off the start and end tags if we have matches to unwrap the html
+		  stripTags(html, matchingFix.prefix, matchingFix.suffix)
+		: html;
 
-	const unwrappedHtml =
-		(willUnwrap &&
-			matchingFix &&
-			// Chop off the start and end tags if we have matches to unwrap the html
-			stripTags(html, matchingFix.prefix, matchingFix.suffix)) ||
-		html;
-
-	// span is the default unwrappedElement
-	const unwrappedElement =
-		(willUnwrap && matchingFix && matchingFix.unwrappedElement) || 'p';
+	/** `p` is the default unwrappedElement */
+	const unwrappedElement = matchingFix?.unwrappedElement || 'p';
 
 	return {
 		unwrappedHtml,
-		willUnwrap,
+		willUnwrap: !!matchingFix,
 		unwrappedElement,
 	};
 };
