abd712f855c67e0a2c1d6c255e2fd59825f5c523
Chris Jones <christopher.jones@guardian.co.uk>
2022-02-22 13:29:34 +0000

    Put short urls into buckets

--- dotcom-rendering/src/amp/components/ContentABTest.tsx
+++ dotcom-rendering/src/amp/components/ContentABTest.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import sha256 from 'crypto-js/sha256';
+import { isString } from '@guardian/libs';
 
 export enum TestGroup {
 	A = 0,
@@ -7,15 +9,46 @@ export enum TestGroup {
 	D = 3,
 }
 
+// Compute the total number of groups from the enum above
+// Note we have to filter out numbers due to the implementation of enums
+const NUM_GROUPS = Object.values(TestGroup).filter((x) => isString(x)).length;
+
 interface TestContext {
-	group: TestGroup;
+	group: TestGroup | undefined;
 }
 
-const getGroup = (shortUrlId: string) => {
-	console.log(`Using ${shortUrlId} to compute the AMP test group`);
-	console.log(`Except for now we'll harcode as Group A`);
-	const group = TestGroup.A;
-	return group;
+export const getGroup = (shortUrlId: string): TestGroup | undefined => {
+	const hashedShortUrlId = sha256(shortUrlId).words.reduce(
+		(acc, w) => acc + w,
+		0,
+	);
+
+	// Do we need a BigInt?
+	// console.log(
+	// 	num,
+	// 	hash.words.reduce<bigint>((acc, w) => acc + BigInt(w), 0n),
+	// );
+
+	const group = Math.abs(hashedShortUrlId % NUM_GROUPS);
+
+	if (group in TestGroup) {
+		switch (group) {
+			case 0: {
+				return TestGroup.A;
+			}
+			case 1: {
+				return TestGroup.B;
+			}
+			case 2: {
+				return TestGroup.C;
+			}
+			case 3: {
+				return TestGroup.D;
+			}
+		}
+	}
+
+	return undefined;
 };
 
 const Context = React.createContext<TestContext | undefined>(undefined);
