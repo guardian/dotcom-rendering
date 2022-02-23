817e74e0bf836331843e9bdf67730d0a0277da39
Ashleigh Carr <ashcorr20@gmail.com>
2022-02-23 14:00:14 +0000

    refactor: Refactors getArticlecount to be a bit simpler

--- dotcom-rendering/src/lib/article-count.ts
+++ dotcom-rendering/src/lib/article-count.ts
@@ -7,16 +7,9 @@ import { incrementDailyArticleCount } from "../web/lib/dailyArticleCount";
 // We should monitor this function call to ensure it only happens within an
 // article pages when other pages are supported by DCR.
 export const getArticleCount = async (pageId: string, keywordIds: string) => {
+	const hasOptedOut = await hasOptedOutOfArticleCount();
+
 	if (!window.guardian.articleCount) {
-		const hasOptedOut = await hasOptedOutOfArticleCount();
-
-		// Due to hasOptedOut being gotten asynchronously, I think its possible that multiple
-		// calls to getArticleCount might pass the initial window check before either one is able to set the articleCount.
-		// I believe the browser should handle the rest of the javascript synchronously and not cause a race condition.
-		if (!window.guardian.articleCount) {
-			return window.guardian.articleCount;
-		}
-
 		if (!hasOptedOut) {
 			incrementDailyArticleCount();
 			incrementWeeklyArticleCount(
