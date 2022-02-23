6d33ed78821b8f4d971ac6101ad9140ba4527ad4
Ashleigh Carr <ashcorr20@gmail.com>
2022-02-23 13:49:34 +0000

    fix: Thread safety in articleCount

--- dotcom-rendering/src/lib/article-count.ts
+++ dotcom-rendering/src/lib/article-count.ts
@@ -10,6 +10,13 @@ export const getArticleCount = async (pageId: string, keywordIds: string) => {
 	if (!window.guardian.articleCount) {
 		const hasOptedOut = await hasOptedOutOfArticleCount();
 
+		// Due to hasOptedOut being gotten asynchronously, I think its possible that multiple
+		// calls to getArticleCount might pass the initial window check before either one is able to set the articleCount.
+		// I believe the browser should handle the rest of the javascript synchronously and not cause a race condition.
+		if (!window.guardian.articleCount) {
+			return window.guardian.articleCount;
+		}
+
 		if (!hasOptedOut) {
 			incrementDailyArticleCount();
 			incrementWeeklyArticleCount(
