97526fbdb026b6edbafebe7b640a6fbed1a71f62
Ashleigh Carr <ashcorr20@gmail.com>
2022-02-24 15:29:54 +0000

    chore: Update commenting and add type to function

--- dotcom-rendering/src/lib/article-count.ts
+++ dotcom-rendering/src/lib/article-count.ts
@@ -3,17 +3,20 @@ import {
 	getWeeklyArticleHistory,
 	incrementWeeklyArticleCount,
 } from '@guardian/support-dotcom-components';
+import { WeeklyArticleHistory } from '@guardian/support-dotcom-components/dist/dotcom/src/types';
 import { hasOptedOutOfArticleCount } from '../web/lib/contributions';
 import { incrementDailyArticleCount } from '../web/lib/dailyArticleCount';
 
-// Log an article view using the Slot Machine client lib
 // We should monitor this function call to ensure it only happens within an
 // article pages when other pages are supported by DCR.
-export const getArticleCount = async (pageId: string, keywordIds: string) => {
+export const getArticleCount = async (
+	pageId: string,
+	keywordIds: string,
+): Promise<WeeklyArticleHistory | undefined> => {
 	const hasOptedOut = await hasOptedOutOfArticleCount();
 
-	// hasOptedOut could potentially be moved into the scope of the below if condition
-	// but this could potentially (?) create a race condition where whilst one invocation of getArticleCount
+	// hasOptedOut needs to be done before we check if articleCount is set in the window
+	// This is because a potential race condition where one invocation of getArticleCount
 	// is waiting for hasOptedOut another invocation might receive it and increment the article count.
 	if (!window.guardian.articleCount) {
 		if (!hasOptedOut) {
