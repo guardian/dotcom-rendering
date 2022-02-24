38cd800175bc363352ebabde427dec51158f5f48
Simon Byford <simon.byford@guardian.co.uk>
2022-02-24 12:39:39 +0000

    Change `sign-in-gate` placeholder to `div`
    Spacefinder was incorrectly considering it when getting candidates for ad insertion

--- dotcom-rendering/src/web/lib/withSignInGateSlot.tsx
+++ dotcom-rendering/src/web/lib/withSignInGateSlot.tsx
@@ -1,7 +1,7 @@
-// This placeholder p is used by the SignInGate component to insert the sign in gate into the appropriate location within body of an article,
+// This placeholder div is used by the SignInGate component to insert the sign in gate into the appropriate location within body of an article,
 // if the SignInGateSelector determines a gate should be rendered.
 
-const SignInGateSlot = <p id="sign-in-gate" />;
+const SignInGateSlot = <div id="sign-in-gate" />;
 
 export const withSignInGateSlot = (
 	articleElements: (JSX.Element | null | undefined)[],
