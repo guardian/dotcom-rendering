1e0d88427c587ccbf66711841e820516acde776e
Anna Beddow <anna.beddow@gmail.com>
2022-03-02 09:47:03 +0000

    Remove broken story

--- dotcom-rendering/src/web/components/PinnedPost.stories.tsx
+++ dotcom-rendering/src/web/components/PinnedPost.stories.tsx
@@ -64,25 +64,28 @@ export default {
 	},
 };
 
-export const MinimumHeight = () => {
+export const Expandable = () => {
 	const block: Block = {
 		...baseBlock,
 		elements: [
 			{
-				elementId: 'ae950f92-bc9b-4725-bac2-94fce86d8191',
+				elementId: '14ffdfde-113a-4270-afca-d34436dca56e',
 				_type: 'model.dotcomrendering.pageElements.TextBlockElement',
-				html: '<p>Now that Perseverance persevered through the “seven minutes of terror” – a new era of space exploration has officially begun. </p>',
+				html: '<p>That’s it for our live coverage of Nasa’s celebratory news conference and Q&amp;A following the successful landing of the rover Perseverance on Mars. </p>',
 			},
 			{
-				elementId: 'b49c134e-54d6-4445-a3fe-bb52a1370375',
-				role: 'thumbnail',
-				prefix: 'Related: ',
-				_type: 'model.dotcomrendering.pageElements.RichLinkBlockElement',
-				text: 'Ireland election: latest results',
-				url: 'https://www.theguardian.com/world/2020/feb/10/ireland-election-latest-results-live-sinn-fein-fine-gael-fianna-fail',
+				elementId: '1e877282-4d8f-45b0-8b2a-a27060dfc7f5',
+				_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+				html: '<p>To recap:</p>',
+			},
+			{
+				elementId: 'b48f6547-8346-416d-a8be-2e0e6e254087',
+				_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+				html: '<ul> \n <li>The rover is “healthy” and undergoing systems testing.</li> \n <li>It already has beamed back stunning photos from the surface of <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> promising significant scientific discoveries ahead.</li> \n <li>The images include the first color images beamed directly from <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> as opposed to images touched up later.</li> \n <li>The rover documented its own touchdown via an ingenious system of booster rockets and a “space crane”.</li> \n <li>It landed in a “pool-table flat” crater in a prime location for searching for traces of ancient life.</li> \n <li>The wheeled rover could begin to move around its new home as early as late February.</li> \n <li>The rover’s mini helicopter could launch as early as April.</li> \n <li>Its broad mission is to stay on <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> for a couple years, gather data and harvest samples to be collected and returned to Earth on a future mission.</li> \n <li>The point is to determine whether there was life on <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> and subsidiary questions.</li> \n <li>The team at Nasa is very happy and excited, “on cloud nine” in a “weird, dreamlike state”... with lots of work ahead.</li> \n<li>The rover is “healthy” and undergoing systems testing.</li> \n <li>It already has beamed back stunning photos from the surface of <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> promising significant scientific discoveries ahead.</li> \n <li>The images include the first color images beamed directly from <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> as opposed to images touched up later.</li> \n <li>The rover documented its own touchdown via an ingenious system of booster rockets and a “space crane”.</li> \n <li>It landed in a “pool-table flat” crater in a prime location for searching for traces of ancient life.</li> \n <li>The wheeled rover could begin to move around its new home as early as late February.</li> \n <li>The rover’s mini helicopter could launch as early as April.</li> \n <li>Its broad mission is to stay on <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> for a couple years, gather data and harvest samples to be collected and returned to Earth on a future mission.</li> \n <li>The point is to determine whether there was life on <a href="https://www.theguardian.com/science/mars" data-component="auto-linked-tag">Mars</a> and subsidiary questions.</li> \n <li>The team at Nasa is very happy and excited, “on cloud nine” in a “weird, dreamlike state”... with lots of work ahead.</li> \n</ul>',
 			},
 		],
 	};
+
 	return (
 		<Wrapper>
 			<PinnedPost
@@ -103,4 +106,4 @@ export const MinimumHeight = () => {
 		</Wrapper>
 	);
 };
-MinimumHeight.story = { name: 'with minimum height' };
+Expandable.story = { name: 'with expandable button' };
