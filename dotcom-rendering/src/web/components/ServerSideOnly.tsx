fee89f0d2482e998f77004186bccad8f59214d01
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-03-02 16:04:39 +0000

    Add documentation to ServerSideOnly

--- dotcom-rendering/src/web/components/ServerSideOnly.tsx
+++ dotcom-rendering/src/web/components/ServerSideOnly.tsx
@@ -2,12 +2,26 @@ type Props = {
 	children: React.ReactNode;
 };
 
+/**
+ * ServerSideOnly - Use this if you have some JSX you want to be rendered on the server - but not hydrated on the client
+ *
+ * How does this work? When on the server, this component will render its children within a <div> normally.
+ * 	Once on the client, it will render a <div> with an empty dangerouslySetInnerHTML. This communicates to (p)react that there
+ * 	should be HTML in the <div>, but because it's set with dangerouslySetInnerHTML, (p)react will not parse it as part of hydration.
+ *
+ * When do I need this? DCR will only hyrdrate code that lives within islands -
+ * 	you should only need to use this component if you have specific code within an island you do not want to be hydrated.
+ *
+ * Note: Keep in mind this function prevents hydration, but does not prevent code from being included in the webpack bundle.
+ * 	Be cautious in understanding *where* your imports are, to avoid including redundant code in the client bundle.
+ */
 export const ServerSideOnly = ({ children }: Props) => {
 	const isClient = typeof window !== 'undefined';
 
 	return isClient ? (
+		// We do not need to set the actual html within the div, as (p)react is ambivalent towards the content
 		<div dangerouslySetInnerHTML={{ __html: '' }} />
 	) : (
-		<div> {children}</div>
+		<div>{children}</div>
 	);
 };
