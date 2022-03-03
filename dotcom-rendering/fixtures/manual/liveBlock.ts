c6e0d90a57582ebebd0ec9a87a1361e5162d441d
Anna Beddow <anna.beddow@gmail.com>
2022-03-03 09:32:13 +0000

    Add liveblog fixture

---
+++ dotcom-rendering/fixtures/manual/liveBlock.ts
@@ -0,0 +1,24 @@
+export const baseBlock: Block = {
+	secondaryDateLine: 'First published on Fri 19 Feb 2021 17.20 GMT',
+	blockFirstPublishedDisplay: '19.30 GMT',
+	blockLastUpdated: 1613763519000,
+	firstPublishedDisplay: '17.20 GMT',
+	lastUpdatedDisplay: '19.47 GMT',
+	blockCreatedOnDisplay: '19.19 GMT',
+	blockLastUpdatedDisplay: '19.38 GMT',
+	firstPublished: 1613755208000,
+	createdOn: 1613762399000,
+	primaryDateLine: 'Fri 19 Feb 2021 19.41 GMT',
+	blockCreatedOn: 1613762399000,
+	blockFirstPublished: 1613763003000,
+	createdOnDisplay: '19.19 GMT',
+	lastUpdated: 1613764059000,
+	elements: [
+		{
+			elementId: '4ac2fcd8-284c-4038-91a1-093811f389ba',
+			_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+			html: '<p>Nasa scientists have worked for years to support this mission, and kept things going despite the ongoing coronavirus disruption. </p>',
+		},
+	],
+	id: '60300f5f8f08ad21ea60071e',
+};
