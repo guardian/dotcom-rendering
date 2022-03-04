14c90c2cdc76772c5ccd8830875e150dd4e23c57
David Lawes <david.lawes@guardian.co.uk>
2022-03-04 15:37:18 +0000

    pass through gu headers when fetching content
    For CI we may want to override the region (e.g. force a UK article
    to load regardless of server location). We couldn't do this because
    only the url was being extracted from the request. This change
    extracts and forwards GU headers.

--- dotcom-rendering/src/server/lib/get-content-from-url.js
+++ dotcom-rendering/src/server/lib/get-content-from-url.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-async function getContentFromURL(_url) {
+async function getContentFromURL(_url, _headers) {
 	try {
 		if (!_url) {
 			throw new Error('The url query parameter is mandatory');
@@ -14,9 +14,20 @@ async function getContentFromURL(_url) {
 		// Reconstruct the parsed url adding .json?dcr which we need to force dcr to return json
 		const jsonUrl = `${url.origin}${url.pathname}.json?dcr=true&${searchparams}`;
 
-		const { html, ...config } = await fetch(jsonUrl).then((article) =>
-			article.json(),
-		);
+		// Explicitly pass through GU headers - this enables us to override properties such as region in CI
+		let guHeaders = {};
+		if (_headers) {
+			guHeaders = Object.keys(_headers)
+				.filter((key) => key.toLowerCase().startsWith('x-gu-'))
+				.reduce((result, key) => {
+					result[key] = _headers[key];
+					return result;
+				}, {});
+		}
+
+		const { html, ...config } = await fetch(jsonUrl, {
+			headers: guHeaders,
+		}).then((article) => article.json());
 
 		return config;
 	} catch (error) {
@@ -55,7 +66,7 @@ exports.getContentFromURLMiddleware = async (req, res, next) => {
 		if (req.path.startsWith('/AMP')) {
 			url = url.replace('www', 'amp');
 		}
-		req.body = await getContentFromURL(url);
+		req.body = await getContentFromURL(url, req.headers);
 	}
 	next();
 };
