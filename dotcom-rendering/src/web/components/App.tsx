3a9e7d1b73c45f018ce74885da2f88c4b350b416
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-03-14 16:44:44 +0000

    Move reader revenue dev utils into an island

--- dotcom-rendering/src/web/components/App.tsx
+++ dotcom-rendering/src/web/components/App.tsx
@@ -10,7 +10,6 @@ import { Portal } from './Portal';
 import { useOnce } from '../lib/useOnce';
 
 import { incrementAlreadyVisited } from '../lib/alreadyVisited';
-import { ReaderRevenueDevUtils } from '../lib/readerRevenueDevUtils';
 
 import { buildBrazeMessages } from '../lib/braze/buildBrazeMessages';
 import { getOphanRecordFunction } from '../browser/ophan/ophan';
@@ -34,40 +33,6 @@ export const App = ({ CAPI }: Props) => {
 	useEffect(() => {
 		incrementAlreadyVisited();
 	}, []);
-
-	useEffect(() => {
-		// Used internally only, so only import each function on demand
-		const loadAndRun =
-			<K extends keyof ReaderRevenueDevUtils>(key: K) =>
-			(asExistingSupporter: boolean) =>
-				import(
-					/* webpackChunkName: "readerRevenueDevUtils" */ '../lib/readerRevenueDevUtils'
-				)
-					.then((utils) =>
-						utils[key](
-							asExistingSupporter,
-							CAPI.shouldHideReaderRevenue,
-						),
-					)
-					/* eslint-disable no-console */
-					.catch((error) =>
-						console.log(
-							'Error loading readerRevenueDevUtils',
-							error,
-						),
-					);
-		/* eslint-enable no-console */
-
-		if (window && window.guardian) {
-			window.guardian.readerRevenue = {
-				changeGeolocation: loadAndRun('changeGeolocation'),
-				showMeTheEpic: loadAndRun('showMeTheEpic'),
-				showMeTheBanner: loadAndRun('showMeTheBanner'),
-				showNextVariant: loadAndRun('showNextVariant'),
-				showPreviousVariant: loadAndRun('showPreviousVariant'),
-			};
-		}
-	}, [CAPI.shouldHideReaderRevenue]);
 
 	useOnce(() => {
 		setBrazeMessages(buildBrazeMessages(CAPI.config.idApiUrl));
---
+++ dotcom-rendering/src/web/components/ReaderRevenueDev.importable.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import type { ReaderRevenueDevUtils } from '../lib/readerRevenueDevUtils';
+
+type Props = {
+	shouldHideReaderRevenue: boolean;
+};
+
+export const ReaderRevenueDev = ({ shouldHideReaderRevenue }: Props) => {
+	console.log('hydrating reader r dev');
+	useEffect(() => {
+		// Used internally only, so only import each function on demand
+		const loadAndRun =
+			<K extends keyof ReaderRevenueDevUtils>(key: K) =>
+			(asExistingSupporter: boolean) =>
+				import(
+					/* webpackChunkName: "readerRevenueDevUtils" */ '../lib/readerRevenueDevUtils'
+				)
+					.then((utils) =>
+						utils[key](
+							asExistingSupporter,
+							shouldHideReaderRevenue,
+						),
+					)
+					/* eslint-disable no-console */
+					.catch((error) =>
+						console.log(
+							'Error loading readerRevenueDevUtils',
+							error,
+						),
+					);
+		/* eslint-enable no-console */
+
+		if (window && window.guardian) {
+			window.guardian.readerRevenue = {
+				changeGeolocation: loadAndRun('changeGeolocation'),
+				showMeTheEpic: loadAndRun('showMeTheEpic'),
+				showMeTheBanner: loadAndRun('showMeTheBanner'),
+				showNextVariant: loadAndRun('showNextVariant'),
+				showPreviousVariant: loadAndRun('showPreviousVariant'),
+			};
+		}
+	}, [shouldHideReaderRevenue]);
+
+	return null;
+};
