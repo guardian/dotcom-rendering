90b245ad89e7e47187b2c02ebec2fb4756fffca2
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-03-15 12:15:54 +0000

    `ArticleCount` (#4250)
    * feat: Created `AlreadyVisited`

--- dotcom-rendering/src/web/components/App.tsx
+++ dotcom-rendering/src/web/components/App.tsx
@@ -1,16 +1,8 @@
-import { useEffect } from 'react';
-
 import { log } from '@guardian/libs';
-
-import { incrementAlreadyVisited } from '../lib/alreadyVisited';
 
 let renderCount = 0;
 export const App = () => {
 	log('dotcom', `App.tsx render #${(renderCount += 1)}`);
-
-	useEffect(() => {
-		incrementAlreadyVisited();
-	}, []);
 
 	return null;
 };
