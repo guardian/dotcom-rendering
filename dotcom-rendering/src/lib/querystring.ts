592f8efcad6b23a1c4a998707c55df2d121a4ce4
George B <705427+georgeblahblah@users.noreply.github.com>
2022-04-08 13:48:40 +0100

    Refactor constructQuery to use URLSearchParams

--- dotcom-rendering/src/lib/querystring.ts
+++ dotcom-rendering/src/lib/querystring.ts
@@ -1,12 +1,4 @@
 const constructQuery = (query: { [key: string]: any }): string =>
-	Object.keys(query)
-		.map((param: string) => {
-			const value = query[param];
-			const queryValue = Array.isArray(value)
-				? value.map((v) => encodeURIComponent(v)).join(',')
-				: encodeURIComponent(value);
-			return `${param}=${queryValue}`;
-		})
-		.join('&');
+	new URLSearchParams(query).toString();
 
 export { constructQuery };
