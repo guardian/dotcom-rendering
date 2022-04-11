b57172af2b235543727bd8d8669b7b2305327d85
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-04-11 12:38:00 +0100

    feat: `FixedLargeSlowXIV` container type

---
+++ dotcom-rendering/src/web/components/FixedLargeSlowXIV.stories.tsx
@@ -0,0 +1,37 @@
+import { breakpoints } from '@guardian/source-foundations';
+import { ContainerLayout } from './ContainerLayout';
+import { FixedLargeSlowXIV } from './FixedLargeSlowXIV';
+
+import { trails } from '../../../fixtures/manual/trails';
+
+export default {
+	component: FixedLargeSlowXIV,
+	title: 'Components/FixedLargeSlowXIV',
+	parameters: {
+		chromatic: {
+			viewports: [
+				breakpoints.mobile,
+				breakpoints.mobileMedium,
+				breakpoints.mobileLandscape,
+				breakpoints.phablet,
+				breakpoints.tablet,
+				breakpoints.desktop,
+				breakpoints.leftCol,
+				breakpoints.wide,
+			],
+		},
+	},
+};
+
+export const Default = () => (
+	<ContainerLayout
+		title="FixedLargeSlowXIV"
+		showTopBorder={true}
+		sideBorders={true}
+		padContent={false}
+		centralBorder="partial"
+	>
+		<FixedLargeSlowXIV trails={trails} />
+	</ContainerLayout>
+);
+Default.story = { name: 'FixedLargeSlowXIV' };
