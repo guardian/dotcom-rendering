a93250da600ff28f9758acb91e274e66c146c85a
Max Duval <max.duval@theguardian.com>
2022-04-14 12:23:31 +0100

    refactor(enhanceCards): use type predicate (#4582)
    Use `filter` and `map` rather than forEach.
    The alternatives are:
    - for … of loop, which we should have a linter for if we prefer it
    - using reduce, which we said no to since #2597

--- dotcom-rendering/src/model/enhanceCollections.ts
+++ dotcom-rendering/src/model/enhanceCollections.ts
@@ -1,11 +1,14 @@
 import { decideFormat } from '../web/lib/decideFormat';
 
-const enhanceCards = (collections: FEFrontCard[]): DCRFrontCard[] => {
-	const enhanced: DCRFrontCard[] = [];
-	collections.forEach((faciaCard) => {
-		if (faciaCard.format) {
+const enhanceCards = (collections: FEFrontCard[]): DCRFrontCard[] =>
+	collections
+		.filter(
+			(card: FEFrontCard): card is FEFrontCard & { format: CAPIFormat } =>
+				!!card.format,
+		)
+		.map((faciaCard) => {
 			const format = decideFormat(faciaCard.format);
-			enhanced.push({
+			return {
 				format,
 				url: faciaCard.header.url,
 				headline: faciaCard.header.headline,
@@ -18,11 +21,8 @@ const enhanceCards = (collections: FEFrontCard[]): DCRFrontCard[] => {
 					?.allImages[0].url,
 				kickerText:
 					faciaCard.header.kicker?.item?.properties.kickerText,
-			});
-		}
-	});
-	return enhanced;
-};
+			};
+		});
 
 export const enhanceCollections = (
 	collections: FECollectionType[],
