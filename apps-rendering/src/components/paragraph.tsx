6105aa77b596018790c6ba14bf349adacdf29527
Mario Savarese <mario.savarese@theguardian.com>
2022-04-20 17:12:02 +0100

    Lint

--- apps-rendering/src/components/paragraph.tsx
+++ apps-rendering/src/components/paragraph.tsx
@@ -3,8 +3,7 @@
 import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
 import { text } from '@guardian/common-rendering/src/editorialPalette/text';
-import type { ArticleFormat } from '@guardian/libs';
-import { ArticleDesign, ArticleSpecial } from '@guardian/libs';
+import { ArticleDesign, ArticleFormat, ArticleSpecial } from '@guardian/libs';
 import {
 	body,
 	headline,
