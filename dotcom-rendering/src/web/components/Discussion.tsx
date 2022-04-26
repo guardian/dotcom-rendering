14b41bc93b42211aaf55e9dc8afb9f75b81dc183
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-04-26 08:39:56 +0100

    Reveal `Discussion` (#4669)
    * feat: Fix height of comments container
    
    * feat: Added `@guardian/discussion-rendering@10.0.0-0` prerelease`
    
    * fix: Don't use vh to set a fixed height
    
    * fix: Add 300px of height to accommodate double mpu ads here
    
    * Revert "fix: Add 300px of height to accommodate double mpu ads here"
    
    This reverts commit 53824d1dd4951765b2fa17dbaa821eb82895b3b9.
    
    * feat: Render the entire discussion if it's not very big
    
    * deps: Added @guardian/discussion-rendering `v10.0.0`
    
    * refactor: Reduce animation time from 4 seconds to 2
    
    * chore: Add a small delay to the Discussion story to allow it to load
    
    * fix: Added `pauseAnimationAtEnd` to Discussion story
    
    * fix: Use the correct structure when setting chromatic parameters

--- dotcom-rendering/src/web/components/Discussion.tsx
+++ dotcom-rendering/src/web/components/Discussion.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 
 import { joinUrl } from '@guardian/libs';
-import { space } from '@guardian/source-foundations';
+import { neutral, space } from '@guardian/source-foundations';
 import { App as Comments } from '@guardian/discussion-rendering';
+import { EditorialButton } from '@guardian/source-react-components-development-kitchen';
+import { SvgPlus } from '@guardian/source-react-components';
 import { SignedInAs } from './SignedInAs';
 import { Hide } from './Hide';
 import { getCommentContext } from '../lib/getCommentContext';
@@ -19,6 +21,45 @@ export type Props = {
 	enableDiscussionSwitch: boolean;
 	user?: UserProfile;
 };
+
+const overlayStyles = css`
+	background-image: linear-gradient(
+		0deg,
+		${neutral[100]},
+		${neutral[100]} 40%,
+		rgba(255, 255, 255, 0)
+	);
+	height: 80px;
+	position: absolute;
+	bottom: 0;
+	right: 0;
+	width: 100%;
+	display: block;
+`;
+
+const revealStyles = css`
+	/* We're using classnames here because we add and remove these classes
+   using plain javascript */
+	.reveal {
+		animation: ${keyframes`
+		0% { opacity: 0; }
+		100% { opacity: 1; }
+	`} 2s ease-out;
+	}
+	.pending {
+		display: none;
+	}
+`;
+
+const fixHeight = css`
+	max-height: 280px;
+	min-height: 280px;
+	overflow: hidden;
+`;
+
+const positionRelative = css`
+	position: relative;
+`;
 
 const commentIdFromUrl = () => {
 	if (typeof window === 'undefined') return;
@@ -91,44 +132,83 @@ export const Discussion = ({
 		}
 	}, [hasCommentsHash]);
 
+	useEffect(() => {
+		const pendingElements = document?.querySelectorAll<HTMLElement>(
+			'.discussion > .pending',
+		);
+		pendingElements?.forEach((element) => {
+			element.classList.add('reveal');
+			element.classList.remove('pending');
+		});
+	}, []);
+
+	useEffect(() => {
+		// There's no point showing the view more button if there isn't much more to view
+		if (commentCount === 0 || commentCount === 1 || commentCount === 2) {
+			setIsExpanded(true);
+		}
+	}, [commentCount]);
+
 	return (
 		<>
-			<Hide when="above" breakpoint="leftCol">
-				<div
-					data-cy="discussion"
-					css={css`
-						padding-bottom: ${space[2]}px;
-					`}
-				>
-					<SignedInAs
-						palette={palette}
-						enableDiscussionSwitch={enableDiscussionSwitch}
+			<div
+				css={[positionRelative, revealStyles, !isExpanded && fixHeight]}
+				className="discussion"
+			>
+				<div className="pending">
+					<Hide when="above" breakpoint="leftCol">
+						<div
+							data-cy="discussion"
+							css={css`
+								padding-bottom: ${space[2]}px;
+							`}
+						>
+							<SignedInAs
+								palette={palette}
+								enableDiscussionSwitch={enableDiscussionSwitch}
+								user={user}
+								commentCount={commentCount}
+								isClosedForComments={isClosedForComments}
+							/>
+						</div>
+					</Hide>
+					<Comments
 						user={user}
-						commentCount={commentCount}
-						isClosedForComments={isClosedForComments}
+						baseUrl={discussionApiUrl}
+						pillar={format.theme}
+						initialPage={commentPage}
+						pageSizeOverride={commentPageSize}
+						isClosedForComments={
+							isClosedForComments || !enableDiscussionSwitch
+						}
+						orderByOverride={commentOrderBy}
+						shortUrl={shortUrlId}
+						additionalHeaders={{
+							'D2-X-UID': discussionD2Uid,
+							'GU-Client': discussionApiClientHeader,
+						}}
+						expanded={isExpanded}
+						commentToScrollTo={hashCommentId}
+						onPermalinkClick={handlePermalink}
+						apiKey="dotcom-rendering"
+						onExpand={() => {
+							setIsExpanded(true);
+						}}
 					/>
+					{!isExpanded && (
+						<div id="discussion-overlay" css={overlayStyles} />
+					)}
 				</div>
-			</Hide>
-			<Comments
-				user={user}
-				baseUrl={discussionApiUrl}
-				pillar={format.theme}
-				initialPage={commentPage}
-				pageSizeOverride={commentPageSize}
-				isClosedForComments={
-					isClosedForComments || !enableDiscussionSwitch
-				}
-				orderByOverride={commentOrderBy}
-				shortUrl={shortUrlId}
-				additionalHeaders={{
-					'D2-X-UID': discussionD2Uid,
-					'GU-Client': discussionApiClientHeader,
-				}}
-				expanded={isExpanded}
-				commentToScrollTo={hashCommentId}
-				onPermalinkClick={handlePermalink}
-				apiKey="dotcom-rendering"
-			/>
+			</div>
+			{!isExpanded && (
+				<EditorialButton
+					format={format}
+					onClick={() => setIsExpanded(true)}
+					icon={<SvgPlus />}
+				>
+					View more comments
+				</EditorialButton>
+			)}
 		</>
 	);
 };
