2ca5785eb5797e06226ab83a4af38164b15e435b
Ravi Lal <arelra@users.noreply.github.com>
2022-04-26 19:22:05 +0100

    Update Inline1ContainerSizing test config

--- dotcom-rendering/src/web/experiments/tests/inline1-container-sizing.ts
+++ dotcom-rendering/src/web/experiments/tests/inline1-container-sizing.ts
@@ -3,10 +3,10 @@ import type { ABTest } from '@guardian/ab-core';
 export const inline1ContainerSizing: ABTest = {
 	id: 'Inline1ContainerSizing',
 	start: '2022-04-26',
-	expiry: '2022-05-03',
+	expiry: '2022-05-24',
 	author: 'arelra',
 	description:
-		'Test the impact on CLS of fixing the inline1 ad container to full width',
+		'Tests the impact on CLS of fixing the inline1 ad container to full width',
 	audience: 20 / 100,
 	audienceOffset: 30 / 100,
 	successMeasure: 'Reduced CLS',
