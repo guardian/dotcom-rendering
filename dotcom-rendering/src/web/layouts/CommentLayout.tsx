f9c67e0cd0c705a8a2b9eb2d56e4c131305d7879
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-04-27 09:04:23 +0100

    single front door v3 updates

--- dotcom-rendering/src/web/layouts/CommentLayout.tsx
+++ dotcom-rendering/src/web/layouts/CommentLayout.tsx
@@ -340,10 +340,6 @@ export const CommentLayout = ({
 								edition={CAPIArticle.editionId}
 								idUrl={CAPIArticle.config.idUrl}
 								mmaUrl={CAPIArticle.config.mmaUrl}
-								supporterCTA={
-									CAPIArticle.nav.readerRevenueLinks.header
-										.supporter
-								}
 								discussionApiUrl={
 									CAPIArticle.config.discussionApiUrl
 								}
@@ -803,11 +799,6 @@ export const CommentLayout = ({
 					pageFooter={CAPIArticle.pageFooter}
 					pillar={format.theme}
 					pillars={NAV.pillars}
-					urls={CAPIArticle.nav.readerRevenueLinks.header}
-					edition={CAPIArticle.editionId}
-					contributionsServiceUrl={
-						CAPIArticle.contributionsServiceUrl
-					}
 				/>
 			</ElementContainer>
 
--- dotcom-rendering/src/web/layouts/ShowcaseLayout.tsx
+++ dotcom-rendering/src/web/layouts/ShowcaseLayout.tsx
@@ -281,10 +281,6 @@ export const ShowcaseLayout = ({
 									edition={CAPIArticle.editionId}
 									idUrl={CAPIArticle.config.idUrl}
 									mmaUrl={CAPIArticle.config.mmaUrl}
-									supporterCTA={
-										CAPIArticle.nav.readerRevenueLinks
-											.header.supporter
-									}
 									discussionApiUrl={
 										CAPIArticle.config.discussionApiUrl
 									}
@@ -775,11 +771,6 @@ export const ShowcaseLayout = ({
 					pageFooter={CAPIArticle.pageFooter}
 					pillar={format.theme}
 					pillars={NAV.pillars}
-					urls={CAPIArticle.nav.readerRevenueLinks.header}
-					edition={CAPIArticle.editionId}
-					contributionsServiceUrl={
-						CAPIArticle.contributionsServiceUrl
-					}
 				/>
 			</ElementContainer>
 
