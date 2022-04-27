2ebcd02d0b5afb00b72554b9d1ff8589dd21ae17
Ashleigh Carr <ashcorr20@gmail.com>
2022-04-27 13:32:35 +0000

    feat: Add fixed/small/slow-IV container

---
+++ dotcom-rendering/src/web/components/FixedSmallSlowIV.tsx
@@ -0,0 +1,57 @@
+import { ArticleDesign } from '@guardian/libs';
+import { Card } from './Card/Card';
+import { LI } from './Card/components/LI';
+import { UL } from './Card/components/UL';
+
+type Props = {
+	trails: TrailType[];
+};
+
+export const FixedSmallSlowIV = ({ trails }: Props) => {
+	const slicedTrails = trails.slice(0, 4);
+
+	return (
+		<UL direction="row">
+			{slicedTrails.map((trail, index) => {
+				return (
+					<LI
+						percentage="25%"
+						padSides={true}
+						showDivider={index > 0}
+						padBottomOnMobile={true}
+					>
+						<Card
+							linkTo={trail.url}
+							format={trail.format}
+							headlineText={trail.headline}
+							trailText={trail.trailText}
+							headlineSize="medium"
+							byline={trail.byline}
+							showByline={trail.showByline}
+							showQuotes={
+								trail.format.design === ArticleDesign.Comment ||
+								trail.format.design === ArticleDesign.Letter
+							}
+							webPublicationDate={trail.webPublicationDate}
+							kickerText={trail.kickerText}
+							showPulsingDot={
+								trail.format.design === ArticleDesign.LiveBlog
+							}
+							showSlash={true}
+							showClock={false}
+							imageUrl={trail.image}
+							imagePosition="top"
+							imagePositionOnMobile="left"
+							imageSize="medium"
+							mediaType={trail.mediaType}
+							mediaDuration={trail.mediaDuration}
+							commentCount={trail.commentCount}
+							starRating={trail.starRating}
+							branding={trail.branding}
+						/>
+					</LI>
+				);
+			})}
+		</UL>
+	);
+};
