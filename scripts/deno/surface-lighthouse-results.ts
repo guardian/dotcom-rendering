c877bd7866c817d0152e2a66c1b0d1a0102485f0
Joshua <joshuathomaslieberman@gmail.com>
2022-04-28 15:00:45 +0100

    work in progress
    co-author: @mxdvl
    co-author: @bryophyta

--- scripts/deno/surface-lighthouse-results.ts
+++ scripts/deno/surface-lighthouse-results.ts
@@ -1,9 +1,18 @@
 import { Octokit } from "https://cdn.skypack.dev/octokit?dts";
 
+type Comment = {
+  body: string;
+  id: number;
+};
+
+type ListCommentResponse = {
+  data: Comment[];
+};
+
 const token = Deno.env.get("GITHUB_TOKEN");
 if (!token) {
-	console.warn("Missing GITHUB_TOKEN");
-	Deno.exit(1);
+  console.warn("Missing GITHUB_TOKEN");
+  Deno.exit(1);
 }
 
 // const path = Deno.env.get("GITHUB_EVENT_PATH") ?? "";
@@ -13,52 +22,76 @@ if (!token) {
 // }
 
 const octokit = new Octokit({ auth: token });
-// const event = JSON.parse(Deno.readTextFileSync(path)) ?? {pull_request: {number: 4759}};
-const event = {pull_request: {number: 4759}}
-
+// const event = JSON.parse(Deno.readTextFileSync(path));
+const event = { pull_request: { number: 4759 } };
+const issue_number = event.pull_request.number;
 interface Result {
-	actual: number;
-	expected: number;
-	passed: boolean;
-	operator: string;
-	auditTitle: string;
+  actual: number;
+  expected: number;
+  passed: boolean;
+  operator: string;
+  auditTitle: string;
 }
 
-async function postLighthouseResults() {
-  try {
-    console.log("reading results...");
+const createLighthouseResultsMd = (): string => {
+  const data: Result[] = JSON.parse(
+    Deno.readTextFileSync(
+      "dotcom-rendering/.lighthouseci/assertion-results.json",
+    ),
+  );
 
-    const data: Result[] = JSON.parse(
-      await Deno.readTextFile("dotcom-rendering/.lighthouseci/assertion-results.json")
-    );
+  const auditCount = data.length;
+  const failedAuditCount = data.filter((result) => !result.passed).length;
+  const resultsTemplateString = data.map((result) =>
+    `| ${result.auditTitle} | ${
+      result.passed ? "✅" : "❌"
+    } | ${result.expected} | ${result.actual} |`
+  );
 
-    const auditCount = data.length;
-    const failedAuditCount = data.filter((result) => !result.passed).length;
-	console.log("FILTERED", data);
-	const resultsTemplateString = data.map(result => `| ${result.auditTitle} | ${result.passed ? '✅' : '❌'} | ${result.expected} | ${result.actual} |`);
+  return [
+    `⚡️ Lighthouse report for the changes in this PR:`,
+    "| Category | Passed | Expected | Actual |",
+    "| --- | --- | --- | --- |",
+    ...resultsTemplateString,
+    " ",
+    `🔥 ${failedAuditCount} of ${auditCount} audits failed.`,
+  ].join("\n");
+};
 
-	const comment = [
-		`⚡️ Lighthouse report for the changes in this PR:`,
-		'| Category | Passed | Expected | Actual |',
-		'| --- | --- |--- | --- | --- |',
-		...resultsTemplateString,
-		' ',
-		`🔥 ${failedAuditCount} of ${auditCount} audits failed.`,
-	].join('\n')
-	
-	console.log(comment);
+const getCommentID = async (): Promise<number | null> => {
+  const { data: comments }: ListCommentResponse = await octokit.rest.issues
+    .listComments({
+      owner: "guardian",
+      repo: "dotcom-rendering",
+      issue_number: 4759,
+    });
 
-	await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
-		owner: "guardian",
-		repo: "dotcom-rendering",
-		issue_number: event.pull_request.number,
-		body: comment,
-	});
+  console.log(comments);
 
+  const comment = comments.find(
+    (comment) => comment.body.includes("⚡️ Lighthouse report"),
+  );
 
-  } catch (error) {
-    console.error("there was an error:", error.message);
-  }
+  return comment?.id ?? null;
+};
+
+try {
+  const lighthouseResults = createLighthouseResultsMd();
+  const commentID = await getCommentID();
+
+  commentID
+    ? await octokit.rest.issues.updateComment({
+      owner: "guardian",
+      repo: "dotcom-rendering",
+      comment_id: commentID,
+      body: lighthouseResults,
+    })
+    : await octokit.rest.issues.createComment({
+      owner: "guardian",
+      repo: "dotcom-rendering",
+      issue_number,
+      body: lighthouseResults,
+    });
+} catch (error) {
+  console.error("there was an error:", error.message);
 }
-
-postLighthouseResults();
