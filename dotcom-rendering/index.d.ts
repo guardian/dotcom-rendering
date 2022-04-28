943cf74f0a64658d325571a9cd369dbd5a5b3ab4
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-04-28 07:04:57 +0100

    fix: `standfirst` > `trailText`

--- dotcom-rendering/index.d.ts
+++ dotcom-rendering/index.d.ts
@@ -741,7 +741,7 @@ type DCRFrontCard = {
 	format: ArticleFormat;
 	url: string;
 	headline: string;
-	standfirst?: string;
+	trailText?: string;
 	webPublicationDate?: string;
 	image?: string;
 	kickerText?: string;
