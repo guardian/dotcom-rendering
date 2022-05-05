c438846fdd39dd0b6447362896d8b71f7876419d
David Blatcher <dv.bltchr@gmail.com>
2022-05-05 16:17:22 +0100

    remove retired Header and Footer props

--- dotcom-rendering/src/web/layouts/NewsletterSignupLayout.tsx
+++ dotcom-rendering/src/web/layouts/NewsletterSignupLayout.tsx
@@ -98,10 +98,6 @@ export const NewsletterSignupLayout = ({
 							edition={CAPIArticle.editionId}
 							idUrl={CAPIArticle.config.idUrl}
 							mmaUrl={CAPIArticle.config.mmaUrl}
-							supporterCTA={
-								CAPIArticle.nav.readerRevenueLinks.header
-									.supporter
-							}
 							discussionApiUrl={
 								CAPIArticle.config.discussionApiUrl
 							}
@@ -269,11 +265,6 @@ export const NewsletterSignupLayout = ({
 					pageFooter={CAPIArticle.pageFooter}
 					pillar={format.theme}
 					pillars={NAV.pillars}
-					urls={CAPIArticle.nav.readerRevenueLinks.header}
-					edition={CAPIArticle.editionId}
-					contributionsServiceUrl={
-						CAPIArticle.contributionsServiceUrl
-					}
 				/>
 			</ElementContainer>
 
