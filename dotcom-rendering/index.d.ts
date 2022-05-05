54d6b440bcfa8348d7089a16d2e402f7f871b08e
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2022-05-05 12:29:48 +0100

    Temporarily support old and new media designs (#4824)
    - Add new CAPI media Designs
    - Parse new CAPI media Designs to old libs `Design.Media`

--- dotcom-rendering/index.d.ts
+++ dotcom-rendering/index.d.ts
@@ -27,7 +27,11 @@ type CAPITheme = ThemePillar | ThemeSpecial;
 // https://github.com/guardian/content-api-scala-client/blob/master/client/src/main/scala/com.gu.contentapi.client/utils/format/Design.scala
 type CAPIDesign =
 	| 'ArticleDesign'
+	// Temporarily accept both the old MediaDesign and the new ones
 	| 'MediaDesign'
+	| 'GalleryDesign'
+	| 'AudioDesign'
+	| 'VideoDesign'
 	| 'ReviewDesign'
 	| 'AnalysisDesign'
 	| 'CommentDesign'
