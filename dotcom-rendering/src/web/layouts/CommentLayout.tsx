c7c3e149250d1489e9274135fbe6c33df7aeeedd
Silvija Blaslov <55602675+silvacb@users.noreply.github.com>
2022-05-05 14:16:02 +0100

    Ab sb/remove anniversary logo (#4690)
    * Removed logo switch check from component
    
    * Removed unused imports
    
    * Removed Anniversary and Edition prop & styling
    
    * Remove isAnniversary flag
    
    * Reverted Guardian 200 logo for amp.
    
    * Add best website logo svg
    
    * Change logo for best website logo
    
    * fix linting
    
    * bump source-react-components
    
    * Replace logo with source
    
    * Refactor styles
    
    * Replace logo with Source in AMP
    
    * Remove unused static logos
    
    * update source
    
    * fix height of header to account for new svg
    
    * fix(Logo): let height determine itself
    
    Co-authored-by: Alina Boghiu <alina.boghiu@gmail.com>
    
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>
    Co-authored-by: Alina Boghiu <alina.boghiu@gmail.com>
    Co-authored-by: Max Duval <max.duval@theguardian.com>

--- dotcom-rendering/src/web/layouts/CommentLayout.tsx
+++ dotcom-rendering/src/web/layouts/CommentLayout.tsx
@@ -345,10 +345,6 @@ export const CommentLayout = ({
 								discussionApiUrl={
 									CAPIArticle.config.discussionApiUrl
 								}
-								isAnniversary={
-									CAPIArticle.config.switches
-										.anniversaryHeaderSvg
-								}
 								urls={CAPIArticle.nav.readerRevenueLinks.header}
 								remoteHeader={
 									CAPIArticle.config.switches.remoteHeader
