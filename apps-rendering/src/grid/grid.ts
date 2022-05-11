9f5d1c2e6e31179dc15110b12cea78fda43e4e84
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2022-05-11 18:37:58 +0100

    Lint fix

--- apps-rendering/src/grid/grid.ts
+++ apps-rendering/src/grid/grid.ts
@@ -8,22 +8,26 @@ import { from } from '@guardian/source-foundations';
  * Named CSS grid lines, based on the three columns commonly used for Guardian
  * layouts.
  */
-type Line
-    = 'viewport-start'
-    | 'left-column-start'
-    | 'left-column-end'
-    | 'centre-column-start'
-    | 'centre-column-end'
-    | 'right-column-start'
-    | 'right-column-end'
-    | 'viewport-end'
-    ;
+type Line =
+	| 'viewport-start'
+	| 'left-column-start'
+	| 'left-column-end'
+	| 'centre-column-start'
+	| 'centre-column-end'
+	| 'right-column-start'
+	| 'right-column-end'
+	| 'viewport-end';
 
-const mobileColumns = '[viewport-start] 0px [centre-column-start] repeat(4, 1fr) [centre-column-end] 0px [viewport-end]';
-const tabletColumns = '[viewport-start] 1fr [centre-column-start] repeat(12, 40px) [centre-column-end] 1fr [viewport-end]';
-const desktopColumns = '[viewport-start] 1fr [centre-column-start] repeat(8, 60px) [centre-column-end right-column-start] repeat(4, 60px) [right-column-end] 1fr [viewport-end]';
-const leftColColumns = '[viewport-start] 1fr [left-column-start] repeat(2, 60px) [left-column-end centre-column-start] repeat(8, 60px) [centre-column-end right-column-start] repeat(4, 60px) [right-column-end] 1fr [viewport-end]';
-const wideColumns = '[viewport-start] 1fr [left-column-start] repeat(3, 60px) [left-column-end centre-column-start] repeat(8, 60px) [centre-column-end] 60px [right-column-start] repeat(4, 60px) [right-column-end] 1fr [viewport-end]';
+const mobileColumns =
+	'[viewport-start] 0px [centre-column-start] repeat(4, 1fr) [centre-column-end] 0px [viewport-end]';
+const tabletColumns =
+	'[viewport-start] 1fr [centre-column-start] repeat(12, 40px) [centre-column-end] 1fr [viewport-end]';
+const desktopColumns =
+	'[viewport-start] 1fr [centre-column-start] repeat(8, 60px) [centre-column-end right-column-start] repeat(4, 60px) [right-column-end] 1fr [viewport-end]';
+const leftColColumns =
+	'[viewport-start] 1fr [left-column-start] repeat(2, 60px) [left-column-end centre-column-start] repeat(8, 60px) [centre-column-end right-column-start] repeat(4, 60px) [right-column-end] 1fr [viewport-end]';
+const wideColumns =
+	'[viewport-start] 1fr [left-column-start] repeat(3, 60px) [left-column-end centre-column-start] repeat(8, 60px) [centre-column-end] 60px [right-column-start] repeat(4, 60px) [right-column-end] 1fr [viewport-end]';
 const mobileColumnGap = '12px';
 const columnGap = '20px';
 
@@ -53,7 +57,7 @@ const container = `
     ${from.wide} {
         grid-template-columns: ${wideColumns};
     }
-`
+`;
 
 // ----- Grid Item Styles ----- //
 
@@ -81,12 +85,12 @@ const allColumns = `
  * @param from The grid line to start from, either a `Line` name or a number.
  * @param to The grid line to end at, either a `Line` name or a number.
  * @returns {string} CSS to place the element on the grid.
- * 
+ *
  * @example <caption>Will place the element in the centre column.</caption>
  * const styles = css`
  *   ${grid.between('centre-column-start', 'centre-column-end')}
  * `;
- * 
+ *
  * @example <caption>Will place the element between lines 3 and 5.</caption>
  * const styles = css`
  *   ${grid.between(3, 5)}
@@ -102,7 +106,7 @@ const between = (from: Line | number, to: Line | number): string => `
  * @param start The grid line to start from, either a `Line` name or a number.
  * @param span The number of columns to span.
  * @returns {string} CSS to place the element on the grid.
- * 
+ *
  * @example <caption>The element will span 3 columns from the line.</caption>
  * const styles = css`
  *   ${grid.span('centre-column-start', 3)}
@@ -117,64 +121,62 @@ const span = (start: Line | number, span: number): string => `
  * on the Guardian Grid see https://theguardian.design/2a1e5182b/p/41be19-grids
  */
 const grid = {
-    /**
-     * CSS to set up the grid container. It applies `display: grid;` etc.
-     * 
-     * @example
-     * const Component = () =>
-     *   <div css={css`${grid.container}`}>
-     *     <h1 css={css`grid-row: 1;`}>Headline</h1>
-     *     <p css={css`grid-row: 2;`}>Standfirst</p>
-     *   </div>
-     */
-    container,
-    /**
-     * Place the element into one of the common Guardian layout columns. The
-     * breakpoints at which they're available are as follows:
-     * 
-     * - **Centre** exists for all breakpoints
-     * - **Left** exists from the `leftCol` breakpoint up
-     * - **Right** exists from the `desktop` breakpoint up
-     * - **All** means take up the entire width of the viewport, "all" columns,
-     * and exists for every breakpoint
-     */
-    column: {
-        /**
-         * Place the element into the centre column. Available for all
-         * breakpoints.
-         */
-        centre: centreColumn,
-        /**
-         * Place the element into the left column. Available for `leftCol`
-         * and above breakpoints.
-         */
-        left: leftColumn,
-        /**
-         * Place the element into the right column. Available for `desktop`
-         * and above breakpoints.
-         */
-        right: rightColumn,
-        /**
-         * Ask the element to take up the entire width of the viewport.
-         * Available for all breakpoints.
-         */
-        all: allColumns,
-    },
-    between,
-    span,
-    /**
-     * The gap between grid columns from the `mobileLandscape` breakpoint up.
-     */
-    columnGap,
-    /**
-     * The gap between grid columns when below the `mobileLandscape`
-     * breakpoint.
-     */
-    mobileColumnGap,
-}
+	/**
+	 * CSS to set up the grid container. It applies `display: grid;` etc.
+	 *
+	 * @example
+	 * const Component = () =>
+	 *   <div css={css`${grid.container}`}>
+	 *     <h1 css={css`grid-row: 1;`}>Headline</h1>
+	 *     <p css={css`grid-row: 2;`}>Standfirst</p>
+	 *   </div>
+	 */
+	container,
+	/**
+	 * Place the element into one of the common Guardian layout columns. The
+	 * breakpoints at which they're available are as follows:
+	 *
+	 * - **Centre** exists for all breakpoints
+	 * - **Left** exists from the `leftCol` breakpoint up
+	 * - **Right** exists from the `desktop` breakpoint up
+	 * - **All** means take up the entire width of the viewport, "all" columns,
+	 * and exists for every breakpoint
+	 */
+	column: {
+		/**
+		 * Place the element into the centre column. Available for all
+		 * breakpoints.
+		 */
+		centre: centreColumn,
+		/**
+		 * Place the element into the left column. Available for `leftCol`
+		 * and above breakpoints.
+		 */
+		left: leftColumn,
+		/**
+		 * Place the element into the right column. Available for `desktop`
+		 * and above breakpoints.
+		 */
+		right: rightColumn,
+		/**
+		 * Ask the element to take up the entire width of the viewport.
+		 * Available for all breakpoints.
+		 */
+		all: allColumns,
+	},
+	between,
+	span,
+	/**
+	 * The gap between grid columns from the `mobileLandscape` breakpoint up.
+	 */
+	columnGap,
+	/**
+	 * The gap between grid columns when below the `mobileLandscape`
+	 * breakpoint.
+	 */
+	mobileColumnGap,
+};
 
 // ----- Exports ----- //
 
-export {
-    grid,
-}
+export { grid };
