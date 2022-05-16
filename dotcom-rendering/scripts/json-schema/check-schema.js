84a86b9335ad03c888afd2189002e5907f73e25c
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-05-16 09:58:45 +0100

    WIP gha summary

--- dotcom-rendering/scripts/json-schema/check-schema.js
+++ dotcom-rendering/scripts/json-schema/check-schema.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const core = require('@actions/core');
+const fs = require('fs');
 
 const root = path.resolve(__dirname, '..', '..');
-const fs = require('fs');
 const { generateArticleSchema, generateFrontSchema } = require('./gen-schema');
 
 const existingArticleSchema = fs.readFileSync(
@@ -13,14 +14,25 @@ const existingFrontSchema = fs.readFileSync(
 	{ encoding: 'utf-8' },
 );
 
-const articleSchema = generateArticleSchema();
-const frontSchema = generateFrontSchema();
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
+	const articleSchema = generateArticleSchema();
+	const frontSchema = generateFrontSchema();
 
-if (
-	existingArticleSchema !== articleSchema ||
-	existingFrontSchema !== frontSchema
-) {
-	throw new Error('Schemas do not match ... please run "make gen-schema"');
-} else {
-	console.log('Schemas match!');
-}
+	if (
+		existingArticleSchema !== articleSchema ||
+		existingFrontSchema !== frontSchema
+	) {
+		await core.summary
+			.addHeading('Schema Check Results')
+			.addRaw('Schemas do not match ❌ \n Please run:')
+			.addCodeBlock('make gen-schema')
+			.addDetails('Why do I need to do this?', `TODO!`)
+			.write();
+		throw new Error(
+			'Schemas do not match ... please run "make gen-schema"',
+		);
+	} else {
+		console.log('Schemas match!');
+	}
+})();
