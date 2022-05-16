e8341959c3035c728c40b9ffac1d2eb8e51dc8be
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-05-16 14:41:59 +0100

    Update regex to prevent selecting multiple islands in 1, add depuplication

--- dotcom-rendering/src/web/server/extractIslands.ts
+++ dotcom-rendering/src/web/server/extractIslands.ts
@@ -1,15 +1,21 @@
 const regex = new RegExp(
 	// Gets the name of any island where expediteLoading is set to 'true'
 	// e.g <gu-island name="MyComponent" props="..." expediteLoading="true"></gu-island>
-	/<gu-island name="([a-zA-Z]+)".*expediteLoading="true".*><\/gu-island>/,
-	'gm',
+	/<gu-island name="([a-zA-Z]+)"[^>]*expediteLoading="true"[^>]*><\/gu-island>/g,
 );
+
+// https://stackoverflow.com/a/9229821/17210412
+const unique = (arr: string[]): string[] => {
+	return [...new Set(arr)];
+};
 
 export const extractExpeditedIslands = (html: string): string[] => {
 	const matches = html.matchAll(regex);
 
-	return Array.from(matches).map((match) => {
-		// MyComponent.importable becomes MyComponent-importable during the build
-		return `${match[1]}-importable`;
-	});
+	return unique(
+		Array.from(matches).map((match) => {
+			// MyComponent.importable becomes MyComponent-importable during the build
+			return `${match[1]}-importable`;
+		}),
+	);
 };
