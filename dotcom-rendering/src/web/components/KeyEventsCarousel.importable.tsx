bf106c635fad1403e1a63ecf96cd847d61837e66
Anna Beddow <anna.beddow@gmail.com>
2022-05-19 14:52:18 +0100

    Simplify carousel

--- dotcom-rendering/src/web/components/KeyEventsCarousel.importable.tsx
+++ dotcom-rendering/src/web/components/KeyEventsCarousel.importable.tsx
@@ -1,12 +1,13 @@
 import { css } from '@emotion/react';
 import { ArticleFormat } from '@guardian/libs';
 import {
+	Button,
+	buttonThemeBrandAlt,
 	Hide,
 	SvgChevronLeftSingle,
 	SvgChevronRightSingle,
 } from '@guardian/source-react-components';
-import { useEffect, useState } from 'react';
-import { neutral, brandAlt, space } from '@guardian/source-foundations';
+import { space } from '@guardian/source-foundations';
 import { KeyEventCard } from './KeyEventCard';
 
 interface Props {
@@ -35,117 +36,48 @@ const containerStyles = css`
 	flex-direction: row;
 	align-items: stretch;
 	width: fit-content;
-	margin-bottom: 34px;
-`;
-
-const buttonContainerStyles = css`
-	display: flex;
-	height: 34px;
-	margin-top: 10px;
-	margin-bottom: ${space[5]}px;
-	position: absolute;
-	bottom: 0;
-	justify-content: space-between;
-	width: 100%;
-	padding-right: 40px;
+	margin-bottom: ${space[12]}px;
+	position: relative;
 `;
 
 const buttonStyles = css`
-	border: 0 none;
-	border-radius: 100%;
-	height: 34px;
-	width: 34px;
-	cursor: pointer;
-	padding: 0;
-	background-color: ${neutral[0]};
-	bottom: 4px;
+	position: absolute;
+	bottom: 0;
+	margin-bottom: ${space[4]}px;
+	background-color: ${buttonThemeBrandAlt.button.backgroundPrimary};
 	&:active,
 	&:hover {
 		outline: none;
-		background-color: ${brandAlt[400]};
-		svg {
-			fill: ${neutral[7]};
-		}
-	}
-
-	&:focus {
-		outline: none;
-	}
-
-	svg {
-		fill: ${neutral[100]};
-		height: 34px;
+		background-color: ${buttonThemeBrandAlt.button.backgroundPrimaryHover};
 	}
 `;
 
-const disabledButtonStyles = css`
-	background-color: ${neutral[60]};
-	cursor: default;
-
-	&:hover,
-	&:focus {
-		background-color: ${neutral[60]};
-
-		svg {
-			fill: ${neutral[100]};
-		}
-	}
+const leftButton = css`
+	left: ${space[5]}px;
 `;
+
+const rightButton = css`
+	right: ${space[5]}px;
+`;
+
+const isServer = typeof window === 'undefined';
+
+const carousel: HTMLElement | null = !isServer
+	? window.document.getElementById('key-event-carousel')
+	: null;
 
 export const KeyEventsCarousel = ({
 	keyEvents,
 	filterKeyEvents,
 	format,
 }: Props) => {
-	const [activeIndex, setActiveIndex] = useState(0);
-	const [carousel, setCarousel] = useState<HTMLElement>();
-	const [cards, setCards] = useState<Element[]>();
 	const cardWidth = 200;
-
-	// we only want to count cards fully in view so we round down.
-	const cardsOnScreen = carousel
-		? Math.floor(carousel.clientWidth / cardWidth)
-		: 0;
-
-	const isFirstCard = activeIndex === cardsOnScreen;
-	const isLastCard = activeIndex === keyEvents.length - 1;
-
-	useEffect(() => {
-		const carouselElement = document.getElementById('key-event-carousel');
-		const cardsArray = Array.from(
-			document.querySelectorAll('#key-event-card'),
-		);
-		if (carouselElement) setCarousel(carouselElement);
-		if (cardsArray) setCards(cardsArray);
-	}, []);
-
-	useEffect(() => {
-		const handleIntersect = (entries: IntersectionObserverEntry[]) => {
-			const entry = entries.find((e) => e.isIntersecting);
-			const index = cards?.findIndex((e) => e === entry?.target);
-			if (index && index !== -1) setActiveIndex(index);
-		};
-
-		const observer = new window.IntersectionObserver(handleIntersect, {
-			root: carousel,
-			rootMargin: '0% 0% 0% -100%',
-		});
-
-		cards?.forEach((el) => {
-			observer.observe(el);
-		});
-
-		return () => observer.disconnect();
-	}, [activeIndex, cards, carousel]);
-
 	const goPrevious = () => {
-		if (carousel && !isFirstCard) carousel.scrollLeft -= cardWidth;
+		if (carousel) carousel.scrollLeft -= cardWidth;
 	};
-
 	const goNext = () => {
-		if (carousel && !isLastCard) carousel.scrollLeft += cardWidth;
+		if (carousel) carousel.scrollLeft += cardWidth;
 	};
-
 	const transformedKeyEvents = keyEvents
 		.filter((keyEvent) => {
 			return keyEvent.title && keyEvent.blockFirstPublished;
@@ -173,30 +105,24 @@ export const KeyEventsCarousel = ({
 			</ul>
 			<Hide until="desktop">
 				{keyEvents.length > 6 && (
-					<span css={buttonContainerStyles}>
-						<button
-							css={[
-								buttonStyles,
-								isFirstCard && disabledButtonStyles,
-							]}
-							type="button"
-							aria-label="Move key events carousel backwards"
+					<>
+						<Button
+							hideLabel={true}
+							cssOverrides={[buttonStyles, leftButton]}
+							iconSide="left"
+							icon={<SvgChevronLeftSingle />}
 							onClick={goPrevious}
-						>
-							<SvgChevronLeftSingle />
-						</button>
-						<button
-							css={[
-								buttonStyles,
-								isLastCard && disabledButtonStyles,
-							]}
-							type="button"
-							aria-label="Move key events carousel forwards"
+							aria-label="Move key events carousel backwards"
+						/>
+						<Button
+							hideLabel={true}
+							cssOverrides={[buttonStyles, rightButton]}
+							iconSide="left"
+							icon={<SvgChevronRightSingle />}
 							onClick={goNext}
-						>
-							<SvgChevronRightSingle />
-						</button>
-					</span>
+							aria-label="Move key events carousel forwards"
+						/>
+					</>
 				)}
 			</Hide>
 		</div>
