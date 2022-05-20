502fa61f2e35aead79af40d8637d1d717eddfbaa
Max Duval <max.duval@theguardian.com>
2022-05-20 14:56:25 +0100

    refactor(eslint): reorganise rules in eslint config

--- dotcom-rendering/.eslintrc.js
+++ dotcom-rendering/.eslintrc.js
@@ -1,5 +1,36 @@
 const transitionRules = require('./eslint-guardian');
 
+/** TODO: Review these */
+const rulesToReview = {
+	'import/no-extraneous-dependencies': 'warn', // 683 problems
+	'react/jsx-one-expression-per-line': 'warn', // 325 problems
+	'consistent-return': 'warn', // 51 problems
+	'default-case': 'warn', // 50 problems
+	'react/no-danger': 'warn', // 48 problems
+	'no-underscore-dangle': 'warn', // 45 problems
+	'react/no-array-index-key': 'warn', // 34 problems
+	'react/button-has-type': 'warn', // 23 problems
+	'@typescript-eslint/require-await': 'warn', // 22 problems
+	'react/jsx-curly-newline': 'warn', // 8 problems
+	'no-case-declarations': 'warn', // 7 problems
+	'no-useless-escape': 'warn', // 6 problems
+	'react/jsx-no-target-blank': 'warn', // 3 problems
+};
+
+const rulesToRemove = {
+	'@typescript-eslint/explicit-module-boundary-types': 'off',
+	'@typescript-eslint/no-unsafe-call': 'off',
+	'@typescript-eslint/no-unsafe-assignment': 'off',
+	'@typescript-eslint/no-unsafe-return': 'off',
+	'@typescript-eslint/ban-ts-comment': 'off',
+	'@typescript-eslint/restrict-template-expressions': 'off',
+	'no-console': 'off',
+	'no-shadow': 'off',
+
+	'@typescript-eslint/explicit-function-return-type': 'off',
+	'@typescript-eslint/no-inferrable-types': 'off',
+};
+
 module.exports = {
 	env: {
 		browser: true,
@@ -53,19 +84,6 @@ module.exports = {
 		'react/jsx-indent-props': [2, 'tab'],
 		'react/prop-types': [0],
 		'react/jsx-boolean-value': [2, 'always'],
-		'import/prefer-default-export': 'off',
-		// TODO: remove
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-unsafe-call': 'off',
-		'@typescript-eslint/no-unsafe-assignment': 'off',
-		'@typescript-eslint/no-unsafe-return': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'@typescript-eslint/restrict-template-expressions': 'off',
-		'no-console': 'off',
-		'no-shadow': 'off',
-
-		'@typescript-eslint/explicit-function-return-type': [0],
-		'@typescript-eslint/no-inferrable-types': [0],
 
 		// Fixed as part of @guardian-eslint move May 2022
 		'array-callback-return': 'error',
@@ -73,24 +91,8 @@ module.exports = {
 		'no-empty-pattern': 'error',
 		'no-param-reassign': 'error',
 
-		// TODO, review these (1314 problems)
-		'import/no-extraneous-dependencies': 'warn', // 683 problems
-		'react/jsx-one-expression-per-line': 'warn', // 325 problems
-		'consistent-return': 'warn', // 51 problems
-		'default-case': 'warn', // 50 problems
-		'react/no-danger': 'warn', // 48 problems
-		'no-underscore-dangle': 'warn', // 45 problems
-		'react/no-array-index-key': 'warn', // 34 problems
-		'react/button-has-type': 'warn', // 23 problems
-		'@typescript-eslint/require-await': 'warn', // 22 problems
-		'react/jsx-curly-newline': 'warn', // 8 problems
-		'no-case-declarations': 'warn', // 7 problems
-		'no-useless-escape': 'warn', // 6 problems
-		'no-empty-pattern': 'warn', // 4 problems
-		'react/jsx-no-target-blank': 'warn', // 3 problems
-		'array-callback-return': 'warn', // 2 problems
-		'global-require': 'warn', // 2 problems
-		/** These rules will be disabled one-by-one */
+		...rulesToReview,
+		...rulesToRemove,
 		...transitionRules,
 	},
 	settings: {
