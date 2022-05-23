f6a23393d71dc021f9c7d2d951883accf2344194
iainjchambers-guardian <99400613+iainjchambers-guardian@users.noreply.github.com>
2022-05-23 12:27:58 +0100

    Add headline refactor (#4699)
    * Add headline refactor
    
    
    Add headline refactor
    
    
    Fixed storybook and styles
    
    
    Moved more components
    
    
    Add byline
    
    
    Second time's the charm
    
    
    Fixes
    
    
    Fix interview headline
    
    * Lint fix
    
    * Fixed font sizes and TS issues
    
    * Lint and style fixes
    
    * Interview headline fixes
    
    * More headline fixes
    
    * Import type fix
    
    Co-authored-by: Iain Chambers <iain.j.chambers.freelance@guardian.co.uk>

---
+++ apps-rendering/src/components/Headline/InterviewHeadline.tsx
@@ -0,0 +1,71 @@
+import { css } from '@emotion/react';
+import {
+	background,
+	text,
+} from '@guardian/common-rendering/src/editorialPalette';
+import { from, headline, remSpace } from '@guardian/source-foundations';
+import HeadlineByline from 'components/HeadlineByline';
+import HeadlineTag from 'components/HeadlineTag';
+import type { Item } from 'item';
+import { getFormat } from 'item';
+import { darkModeCss } from 'styles';
+import { defaultStyles } from './Headline.defaults';
+
+interface Props {
+	item: Item;
+}
+
+const interviewStyles = css`
+	${headline.small({ fontWeight: 'bold' })}
+	line-height: 1.25;
+	${from.tablet} {
+		${headline.medium({ fontWeight: 'bold' })}
+		line-height: 1.25;
+	}
+`;
+
+const InterviewHeadline: React.FC<Props> = ({ item }) => {
+	const format = getFormat(item);
+
+	return (
+		<div
+			css={css`
+				padding-left: 0;
+				padding-right: ${remSpace[12]};
+				position: relative;
+				${from.wide} {
+					margin: 0 auto;
+				}
+				${from.phablet} {
+					width: 38.75rem;
+				}
+			`}
+		>
+			<HeadlineTag tagText="Interview" format={format} />
+			<h1 css={css(defaultStyles(item), interviewStyles)}>
+				<span
+					css={css`
+						color: ${text.headline(format)};
+						background-color: ${background.headline(format)};
+						position: relative;
+						white-space: pre-wrap;
+						padding: 0 ${remSpace[3]} ${remSpace[1]};
+						display: inline;
+						box-decoration-break: clone;
+						${from.wide} {
+							padding: 0 ${remSpace[2]} ${remSpace[1]};
+						}
+						${darkModeCss`
+							background-color: ${background.headlineDark(format)};
+						`};
+					`}
+				>
+					{item.headline}
+				</span>
+			</h1>
+			<HeadlineByline bylineHtml={item.bylineHtml} format={format} />
+		</div>
+	);
+};
+
+export default InterviewHeadline;
