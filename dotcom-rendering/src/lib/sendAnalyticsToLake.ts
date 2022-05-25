1121e4e7d0ba70781ea68794d679594295f74d33
Joshua <joshuathomaslieberman@gmail.com>
2022-05-25 10:54:35 +0100

    Adding the analytics sending code

---
+++ dotcom-rendering/src/lib/sendAnalyticsToLake.ts
@@ -0,0 +1,113 @@
+/** Nominal data */
+type Properties = Record<string, string>;
+/** Numerical data */
+type Metrics = Record<string, number>;
+
+type Flatten<R extends Record<string, string | number>> = Array<{
+	name: keyof R;
+	value: R[keyof R];
+}>;
+
+/** This turns an object’s key-value pairs into an array objects
+ * with `name` and `value` keys.
+ *
+ * @example
+ * ({
+ *   label: 'libs',
+ *   network: 'wifi',
+ * }) => [
+ *   {
+ *		name: 'label',
+ *		value: 'libs',
+ *   },
+ *		name: 'network',
+ *		value: 'wifi',
+ *   },
+ * ]
+ */
+const flatten = <Value extends string | number>(obj: Record<string, Value>) =>
+	Object.entries(obj).map(([name, value]) => ({
+		name,
+		value,
+	}));
+
+type Payload = {
+	label: string;
+	properties: Flatten<Properties>;
+	metrics: Flatten<Metrics>;
+};
+
+const generateJSONPayload = (
+	label: string,
+	properties: Properties,
+	metrics: Metrics,
+): string => {
+	const payload: Payload = {
+		label,
+		properties: flatten(properties),
+		metrics: flatten(metrics),
+	};
+
+	return JSON.stringify(payload);
+};
+
+/** Helper method to get default logging endpoints */
+const getLoggingEndpoint = (
+	isDev: boolean,
+): `https://logs.${string}guardianapis.com/log` =>
+	isDev
+		? 'https://logs.code.dev-guardianapis.com/log'
+		: 'https://logs.guardianapis.com/log';
+
+type Data = {
+	label: string;
+	endpoint: string;
+	metrics?: Metrics;
+	properties?: Properties;
+};
+
+/**
+ * Record log to our Data Lake.
+ * ⚠️ Make sure we have consent for collecting this data.
+ * If in doubt, reach out to [@guardian/transparency-consent](https://github.com/orgs/guardian/teams/transparency-consent)
+ *
+ * Send nominal and numerical data:
+ * - Nominal datum is of type `string`
+ * - Numerical datum is of type `number`
+ *
+ * @param {Data} options The data to send
+ * @param {string} options.label Used to identify the data (in BigQuery)
+ * @param {string} options.endpoint The endpoint to send the data to.
+ * @param {Properties} [options.properties] Nominal data. Defaults to an empty object.
+ * @param {Metrics} [options.metrics] Numerical data. Defaults to an empty object.
+ * @returns {boolean} Whether sending the data has been successfully queued.
+ */
+export const recordLog = ({
+	label,
+	endpoint,
+	properties = {},
+	metrics = {},
+}: Data): boolean => {
+	if (typeof label !== 'string' || label.trim() === '') return false;
+	if (!endpoint) return false;
+
+	const body = generateJSONPayload(label, properties, metrics);
+
+	if ('sendBeacon' in navigator) {
+		return navigator.sendBeacon(
+			endpoint,
+			generateJSONPayload(label, properties, metrics),
+		);
+	}
+
+	try {
+		void fetch(endpoint, {
+			body,
+		});
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
+
+export { getLoggingEndpoint };
--- dotcom-rendering/src/web/components/ScrollTracker.importable.tsx
+++ dotcom-rendering/src/web/components/ScrollTracker.importable.tsx
@@ -1,24 +1,38 @@
 import debounce from 'lodash.debounce';
 import { useEffect, useState } from 'react';
+import { getLoggingEndpoint, recordLog } from 'src/lib/sendAnalyticsToLake';
 
-type Data = {
-	label?: string;
-	properties: Record<string, string>;
-	metrics: Record<string, number>;
+/**
+ * MainContentHeight measures from the top of the window to the bottom of the #maincontent element
+ * It will change slightly depending on what content is loaded */
+const getMainContentHeight = (): number => {
+	const body: HTMLElement = document.body;
+	const mainContent = document.querySelector<HTMLElement>('#maincontent');
+	if (!mainContent) return -1;
+
+	return (
+		mainContent.getBoundingClientRect().bottom -
+		body.getBoundingClientRect().top
+	);
 };
 
 export const ScrollTracker = () => {
+	const debounceRate = 500;
+
 	// React hooks for tracking viewport and scroll position
-	const [{ viewportWidth, viewportHeight }, setViewport] = useState({
-		viewportWidth: document.documentElement.clientWidth,
-		viewportHeight: document.documentElement.clientHeight,
-	});
+	const [{ viewportWidth, viewportHeight, mainContentHeight }, setViewport] =
+		useState({
+			viewportWidth: document.documentElement.clientWidth,
+			viewportHeight: document.documentElement.clientHeight,
+			mainContentHeight: getMainContentHeight(),
+		});
 
 	useEffect(function debouncedResizeListener() {
 		const viewportListener = debounce(() => {
 			setViewport({
 				viewportWidth: document.documentElement.clientWidth,
 				viewportHeight: document.documentElement.clientHeight,
+				mainContentHeight: getMainContentHeight(),
 			});
 		}, 500);
 
@@ -38,7 +52,7 @@ export const ScrollTracker = () => {
 				setScrollDepth(
 					Math.floor(Math.max(scrollDepth, scrollPosition)),
 				);
-			}, 500);
+			}, debounceRate);
 
 			window.addEventListener('scroll', scrollListener);
 
@@ -49,32 +63,18 @@ export const ScrollTracker = () => {
 		[scrollDepth, viewportHeight],
 	);
 
-	// MainContentHeight measures from the top of the window to the bottom of the #maincontent element
-	// It will change slightly depending on what content is loaded
-	const getMainContentHeight = (): number => {
-		const body: HTMLElement = document.body;
-		const mainContent = document.querySelector<HTMLElement>('#maincontent');
-		if (!mainContent) return -1;
-
-		return (
-			mainContent.getBoundingClientRect().bottom -
-			body.getBoundingClientRect().top
-		);
-	};
-
-	const mainContentHeight: number = getMainContentHeight();
-
-	// mainContentScroll depth is compared to the height of mainContent
-	// to determine how far the user has scrolled in editorial content
-	// It can be exceeded, as captured by totalScrollDepth
+	/**
+	 * mainContentScroll depth is compared to the height of mainContent
+	 * to determine how far the user has scrolled in editorial content
+	 * It can be exceeded, as captured by totalScrollDepth */
 	const mainContentScrollDepth: number = Math.min(
 		mainContentHeight,
 		scrollDepth,
 	);
 	const totalScrollDepth: number = scrollDepth;
 
-	const payload: Data = {
-		label: 'scrollDepth',
+	const payload = {
+		label: 'dotComScrollDepth',
 		properties: {
 			path: window.location.pathname,
 		},
@@ -89,6 +89,10 @@ export const ScrollTracker = () => {
 
 	// TBD: Send the payload into the analytics pipeline
 	console.log(payload);
+	recordLog({
+		...payload,
+		endpoint: getLoggingEndpoint(true),
+	});
 
 	// don’t render anything
 	return null;
