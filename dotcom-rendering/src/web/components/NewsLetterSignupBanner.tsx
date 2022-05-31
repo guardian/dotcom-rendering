466bce22b2852f55123b3cc3648e7eee9aba99cc
David Blatcher <dv.bltchr@gmail.com>
2022-05-31 14:21:30 +0100

    use separated guardian logo and newsletters badge past LeftCol

--- dotcom-rendering/src/web/components/NewsLetterSignupBanner.tsx
+++ dotcom-rendering/src/web/components/NewsLetterSignupBanner.tsx
@@ -1,34 +1,21 @@
 import { css } from '@emotion/react';
-import { until, brand, neutral } from '@guardian/source-foundations';
+import { brand, neutral, space, until } from '@guardian/source-foundations';
 import { SvgGuardianLogo } from '@guardian/source-react-components';
-import { ElementContainer } from './ElementContainer';
+import { ContainerLayout } from './ContainerLayout';
 import { NewsletterBadge } from './NewslettersBadge';
 
-const stackedWrapperStyle = css`
-	padding-top: 10px;
-	padding-bottom: 10px;
-	display: flex;
-	justify-content: flex-end;
-	align-items: flex-start;
+const singleColumnWrapperStyle = css`
+	display: none;
 	flex-direction: column;
-	width: 230px;
+	justify-content: flex-start;
+	max-width: 200px;
 
-	${until.wide} {
-		width: 150px;
+	${until.desktop} {
+		max-width: 170px;
 	}
 
 	${until.leftCol} {
-		width: 170px;
-		margin-left: 10px;
-	}
-
-	${until.desktop} {
-		justify-content: flex-start;
-		width: 170px;
-	}
-
-	svg {
-		max-width: 100%;
+		display: flex;
 	}
 
 	svg:nth-of-type(1) {
@@ -36,20 +23,60 @@ const stackedWrapperStyle = css`
 	}
 
 	svg:nth-of-type(2) {
-		padding-top: 4px;
+		padding-top: ${space[1]}px;
 		width: 100%;
 	}
 `;
 
+const rightColWrapperStyle = css`
+	max-width: 200px;
+	height: 100%;
+	display: flex;
+	align-items: flex-end;
+
+	svg {
+		width: 100%;
+		position: relative;
+		bottom: -11%; // aligns the bottom line of the text within (not the border) with the logo on the left
+	}
+
+	${until.leftCol} {
+		display: none;
+	}
+
+	${until.wide} {
+		max-width: 160px;
+		svg {
+			bottom: -12.25%;
+		}
+	}
+`;
+
+const leftColWrapperStyle = css`
+	display: flex;
+	justify-content: flex-end;
+	margin: ${space[2]}px 0 ${space[9]}px;
+
+	svg {
+		max-width: 100%;
+	}
+`;
+
 export const NewsLetterSignupBanner = () => (
-	<ElementContainer
-		padded={true}
-		showSideBorders={false}
+	<ContainerLayout
 		backgroundColour={brand[400]}
+		leftContent={
+			<div css={leftColWrapperStyle}>
+				<SvgGuardianLogo textColor={neutral[100]} width={200} />
+			</div>
+		}
 	>
-		<div css={stackedWrapperStyle}>
-			<SvgGuardianLogo textColor={neutral[100]} />
+		<div css={singleColumnWrapperStyle}>
+			<SvgGuardianLogo textColor={neutral[100]} width={200} />
 			<NewsletterBadge />
 		</div>
-	</ElementContainer>
+		<div css={rightColWrapperStyle}>
+			<NewsletterBadge />
+		</div>
+	</ContainerLayout>
 );
