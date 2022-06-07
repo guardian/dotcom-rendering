6cb299652f9d093d065e0a6f8cbe028873fc9666
Ravi Lal <arelra@users.noreply.github.com>
2022-06-07 08:45:08 +0100

    Migrate Cypress from v7.4.0 to v10.0.2

---
+++ dotcom-rendering/cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1500,
+  viewportHeight: 860,
+  video: false,
+  chromeWebSecurity: false,
+  blockHosts: [
+    '*ophan.theguardian.com',
+    'pixel.adsafeprotected.com',
+    '*permutive.com',
+    '*adnxs.com',
+    '*adsystem.com',
+    '*casalemedia.com',
+    '*pubmatic.com',
+    '*360yield.com',
+    '*omnitagjs.com',
+    '*the-ozone-project.com',
+    '*openx.net',
+  ],
+  retries: 2,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:9000/',
+  },
+})
