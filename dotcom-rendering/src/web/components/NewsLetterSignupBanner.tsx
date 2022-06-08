ed55c5149f29c1c5292a02e8331fce1fe42e4a36
David Blatcher <dv.bltchr@gmail.com>
2022-06-08 09:56:32 +0100

    rework banner css

--- dotcom-rendering/src/web/components/NewsLetterSignupBanner.tsx
+++ dotcom-rendering/src/web/components/NewsLetterSignupBanner.tsx
@@ -23,27 +23,35 @@ const mainColWrapperStyle = css`
 	${until.desktop} {
 		max-width: 170px;
 	}
+`;
 
-	svg:nth-of-type(1) {
-		width: 65%;
+const mainColGuardianLogoContainerStyle = css`
+	svg {
 		display: none;
 
 		${until.leftCol} {
+			width: 65%;
 			display: block;
 		}
 	}
+`;
 
-	svg:nth-of-type(2) {
-		padding-top: ${space[1]}px;
+// the negative bottom values at the two column layout are to
+// align the baseline of the text in the newsletters badge svg
+// with the guardaina logo in the leftCol, rather than aligning
+// the bottom of the SVG frame (design requirement)
+const mainColNewsLettersBadgeContainerStyle = css`
+	svg {
 		width: 100%;
 		position: relative;
-		bottom: -11%;
+		bottom: -10.75px;
 
 		${until.wide} {
-			bottom: -12.25%;
+			bottom: -9.5px;
 		}
 
 		${until.leftCol} {
+			padding-top: ${space[1]}px;
 			bottom: 0;
 		}
 	}
@@ -52,7 +60,8 @@ const mainColWrapperStyle = css`
 const leftColWrapperStyle = css`
 	display: flex;
 	justify-content: flex-end;
-	margin: ${space[2]}px 0 ${space[9]}px;
+	margin-top: ${space[2]}px;
+	margin-bottom: ${space[9]}px;
 
 	svg {
 		max-width: 100%;
@@ -69,8 +78,12 @@ export const NewsLetterSignupBanner = () => (
 		}
 	>
 		<div css={mainColWrapperStyle}>
-			<SvgGuardianLogo textColor={neutral[100]} width={200} />
-			<NewsletterBadge />
+			<span css={mainColGuardianLogoContainerStyle}>
+				<SvgGuardianLogo textColor={neutral[100]} width={200} />
+			</span>
+			<span css={mainColNewsLettersBadgeContainerStyle}>
+				<NewsletterBadge />
+			</span>
 		</div>
 	</ContainerLayout>
 );
