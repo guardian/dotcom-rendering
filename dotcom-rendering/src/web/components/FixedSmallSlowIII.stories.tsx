1cf2bc83f216e3e4e391a50597276179a4bd9733
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-06-09 15:25:55 +0100

    Add FixedSmallSlowIII stories

---
+++ dotcom-rendering/src/web/components/FixedSmallSlowIII.stories.tsx
@@ -0,0 +1,67 @@
+import { breakpoints } from '@guardian/source-foundations';
+import { trails } from '../../../fixtures/manual/trails';
+import { ContainerLayout } from './ContainerLayout';
+import { FixedSmallSlowIII } from './FixedSmallSlowIII';
+
+export default {
+	component: FixedSmallSlowIII,
+	title: 'Components/FixedSmallSlowIII',
+	parameters: {
+		chromatic: {
+			viewports: [
+				breakpoints.mobile,
+				breakpoints.mobileMedium,
+				breakpoints.mobileLandscape,
+				breakpoints.phablet,
+				breakpoints.tablet,
+				breakpoints.desktop,
+				breakpoints.leftCol,
+				breakpoints.wide,
+			],
+		},
+	},
+};
+
+export const Default = () => (
+	<ContainerLayout
+		title="FixedSmallSlowIII"
+		showTopBorder={true}
+		sideBorders={true}
+		padContent={false}
+		centralBorder="partial"
+	>
+		<FixedSmallSlowIII
+			collectionId="abc"
+			trails={trails}
+			showAge={true}
+			hasMore={false}
+		/>
+	</ContainerLayout>
+);
+Default.story = { name: 'FixedSmallSlowIII' };
+
+export const Mobile = () => (
+	<ContainerLayout
+		title="FixedSmallSlowIII"
+		showTopBorder={true}
+		sideBorders={true}
+		padContent={false}
+		centralBorder="partial"
+	>
+		<FixedSmallSlowIII
+			collectionId="abc"
+			trails={trails}
+			showAge={true}
+			hasMore={false}
+		/>
+	</ContainerLayout>
+);
+Mobile.story = {
+	name: 'with mobile viewport',
+	parameters: {
+		viewport: { defaultViewport: 'mobileMedium' },
+		chromatic: {
+			viewports: [breakpoints.mobile],
+		},
+	},
+};
