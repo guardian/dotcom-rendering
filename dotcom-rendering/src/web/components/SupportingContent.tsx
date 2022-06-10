9bf5d4aa481d353cc244d2bff95be1eeb65e0898
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-06-10 16:19:58 +0100

    Remove unused import

--- dotcom-rendering/src/web/components/SupportingContent.tsx
+++ dotcom-rendering/src/web/components/SupportingContent.tsx
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 import { ArticleDesign } from '@guardian/libs';
 import { from, until } from '@guardian/source-foundations';
-import React from 'react';
 import { CardHeadline } from './CardHeadline';
 
 type Alignment = 'vertical' | 'horizontal';
