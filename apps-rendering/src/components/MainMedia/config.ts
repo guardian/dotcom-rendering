98103e420c59acebdc3180426f7fac654d490ec0
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2022-06-10 17:40:13 +0100

    Lint fix

--- apps-rendering/src/components/MainMedia/config.ts
+++ apps-rendering/src/components/MainMedia/config.ts
@@ -2,7 +2,7 @@
 
 /**
  * Used to connect a caption to the main media for screen readers.
- * 
+ *
  * In immersives the caption for the main media is rendered further down the
  * page. We give that caption an id and use `aria-labelledby` on the main media
  * `figure` so that screen readers can connect the two.
