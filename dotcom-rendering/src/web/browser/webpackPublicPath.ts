6c62124141f7e23b54aacb3f5ca2513b9bf28719
Chris Jones <christopher.jones@guardian.co.uk>
2022-06-17 12:28:32 +0100

    Use absolute asset paths when in dev mode

--- dotcom-rendering/src/web/browser/webpackPublicPath.ts
+++ dotcom-rendering/src/web/browser/webpackPublicPath.ts
@@ -1,7 +1,17 @@
+const decidePublicPath = () => {
+	const isDev = process.env.NODE_ENV === 'development';
+	if (isDev && window.location.hostname === 'localhost') {
+		return 'http://localhost:3030/assets/';
+	} else if (
+		window.location.hostname === (process.env.HOSTNAME ?? 'localhost')
+	) {
+		return '/assets/';
+	} else {
+		return `${window.guardian.config.frontendAssetsFullURL}assets/`;
+	}
+};
+
 // allows us to define public path dynamically
 // dynamic imports will use this as the base to find their assets
 // https://webpack.js.org/guides/public-path/#on-the-fly
-__webpack_public_path__ =
-	window.location.hostname === (process.env.HOSTNAME || 'localhost')
-		? '/assets/'
-		: `${window.guardian.config.frontendAssetsFullURL}assets/`;
+__webpack_public_path__ = decidePublicPath();
