0b6d3c8880f806370aa183a5f0ffdc1b097aee89
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-06-20 10:38:16 +0100

    update ab test definition
    - increase audience size to 20%
    - rename variant 8 -> control

--- dotcom-rendering/src/web/experiments/tests/commercial-lazy-load-margin-reloaded.ts
+++ dotcom-rendering/src/web/experiments/tests/commercial-lazy-load-margin-reloaded.ts
@@ -7,13 +7,17 @@ export const commercialLazyLoadMarginReloaded: ABTest = {
 		'Once again test various margins at which ads are lazily-loaded in order to find the optimal one, this time using values between 0% and 70% of the viewport height',
 	start: '2022-06-20',
 	expiry: '2022-07-04',
-	audience: 10 / 100,
+	audience: 20 / 100,
 	audienceOffset: 10 / 100,
 	audienceCriteria: 'All pageviews',
 	successMeasure: 'Ad ratio, viewability, and CLS remain constant or improve',
 	idealOutcome:
 		'One of the variants shows a marked improvement in all of the metrics that we are considering',
 	variants: [
+		{
+			id: 'control',
+			test: (): void => {},
+		},
 		{
 			id: 'variant-1',
 			test: (): void => {},
@@ -40,10 +44,6 @@ export const commercialLazyLoadMarginReloaded: ABTest = {
 		},
 		{
 			id: 'variant-7',
-			test: (): void => {},
-		},
-		{
-			id: 'variant-8',
 			test: (): void => {},
 		},
 	],
