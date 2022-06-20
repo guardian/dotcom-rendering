30ba9d785ce1f999a7e3a826e281deed74c01c0f
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2022-06-20 12:06:16 +0100

    Lint fix

--- apps-rendering/src/components/MainMedia/ImmersiveCaption.tsx
+++ apps-rendering/src/components/MainMedia/ImmersiveCaption.tsx
@@ -1,61 +1,69 @@
 // ----- Imports ----- //
 
-import { MainMedia, MainMediaKind } from 'mainMedia';
-import type { FC } from 'react';
-import { Option, OptionKind } from '@guardian/types';
-import { maybeRender } from 'lib';
-import { ArticleFormat } from '@guardian/libs';
-import Caption from 'components/caption';
-import { immersiveCaptionId } from './MainMedia.defaults';
 import { css } from '@emotion/react';
-import { from, neutral, remSpace, textSans } from '@guardian/source-foundations';
+import type { ArticleFormat } from '@guardian/libs';
+import {
+	from,
+	neutral,
+	remSpace,
+	textSans,
+} from '@guardian/source-foundations';
+import type { Option } from '@guardian/types';
+import { OptionKind } from '@guardian/types';
+import Caption from 'components/caption';
 import { grid } from 'grid/grid';
+import { maybeRender } from 'lib';
+import type { MainMedia } from 'mainMedia';
+import { MainMediaKind } from 'mainMedia';
+import type { FC } from 'react';
+import { immersiveCaptionId } from './MainMedia.defaults';
 
 // ----- Component ----- //
 
 const styles = css`
-    ${textSans.xsmall()}
-    color: ${neutral[46]};
-    ${grid.column.centre}
-    padding-top: ${remSpace[9]};
+	${textSans.xsmall()}
+	color: ${neutral[46]};
+	${grid.column.centre}
+	padding-top: ${remSpace[9]};
 
-    ${from.leftCol} {
-        ${grid.column.left}
-        grid-row: 4;
-        padding-top: ${remSpace[1]};
-    }
+	${from.leftCol} {
+		${grid.column.left}
+		grid-row: 4;
+		padding-top: ${remSpace[1]};
+	}
 `;
 
 type Props = {
-    mainMedia: Option<MainMedia>;
-    format: ArticleFormat;
-}
+	mainMedia: Option<MainMedia>;
+	format: ArticleFormat;
+};
 
 const ImmersiveCaption: FC<Props> = ({ mainMedia, format }) =>
-    maybeRender(mainMedia, (media) => {
-        if (media.kind === MainMediaKind.Video) {
-            return null;
-        }
+	maybeRender(mainMedia, (media) => {
+		if (media.kind === MainMediaKind.Video) {
+			return null;
+		}
 
-        const { caption, credit} = media.image;
+		const { caption, credit } = media.image;
 
-        if (
-            caption.kind === OptionKind.None &&
-            credit.kind === OptionKind.None
-        ) {
-            return null;
-        }
-    
-        return (
-            <p id={immersiveCaptionId} css={styles}>
-                {maybeRender(caption, (cap) =>
-                    <Caption caption={cap} format={format} />
-                )}
-                {' '}
-                {maybeRender(credit, (cred) => <>{cred}</>)}
-            </p>
-        );
-    });
+		if (
+			caption.kind === OptionKind.None &&
+			credit.kind === OptionKind.None
+		) {
+			return null;
+		}
+
+		return (
+			<p id={immersiveCaptionId} css={styles}>
+				{maybeRender(caption, (cap) => (
+					<Caption caption={cap} format={format} />
+				))}{' '}
+				{maybeRender(credit, (cred) => (
+					<>{cred}</>
+				))}
+			</p>
+		);
+	});
 
 // ----- Exports ----- //
 
--- apps-rendering/src/components/Metadata/ImmersiveMetadata.tsx
+++ apps-rendering/src/components/Metadata/ImmersiveMetadata.tsx
@@ -1,45 +1,45 @@
 // ----- Imports ----- //
 
 import { css } from '@emotion/react';
-import { ArticleFormat } from '@guardian/libs';
+import type { ArticleFormat } from '@guardian/libs';
+import { from } from '@guardian/source-foundations';
+import type { Option } from '@guardian/types';
 import CommentCount from 'components/CommentCount';
 import Dateline from 'components/Dateline';
 import Follow from 'components/Follow';
-import { FC } from 'react';
-import type { Option } from '@guardian/types';
-import { Contributor } from 'contributor';
+import type { Contributor } from 'contributor';
 import { grid } from 'grid/grid';
-import { from } from '@guardian/source-foundations';
+import type { FC } from 'react';
 
 // ----- Component ----- //
 
 const styles = css`
-    display: flex;
-    ${grid.column.centre}
+	display: flex;
+	${grid.column.centre}
 
-    ${from.leftCol} {
-        ${grid.column.left}
-    }
+	${from.leftCol} {
+		${grid.column.left}
+	}
 `;
 
 const textStyles = css`
-    flex-grow: 1;
+	flex-grow: 1;
 `;
 
 type Props = {
-    format: ArticleFormat;
-    publishDate: Option<Date>;
-    contributors: Contributor[];
-    commentCount: Option<number>;
-    commentable: boolean;
-}
+	format: ArticleFormat;
+	publishDate: Option<Date>;
+	contributors: Contributor[];
+	commentCount: Option<number>;
+	commentable: boolean;
+};
 
 const ImmersiveMetadata: FC<Props> = ({
-    format,
-    publishDate,
-    contributors,
-    commentCount,
-    commentable,
+	format,
+	publishDate,
+	contributors,
+	commentCount,
+	commentable,
 }) => (
 	<div css={styles}>
 		<div css={textStyles}>
@@ -47,10 +47,10 @@ const ImmersiveMetadata: FC<Props> = ({
 			<Follow format={format} contributors={contributors} />
 		</div>
 		<CommentCount
-            count={commentCount}
-            {...format}
-            commentable={commentable}
-        />
+			count={commentCount}
+			{...format}
+			commentable={commentable}
+		/>
 	</div>
 );
 
