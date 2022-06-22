f021df8fbe7bdf7020818f0b91e755431955117f
Silvija Blaslov <sblasl01@mail.bbk.ac.uk>
2022-06-22 10:20:36 +0100

    Fix typo

--- dotcom-rendering/src/web/layouts/LiveLayout.tsx
+++ dotcom-rendering/src/web/layouts/LiveLayout.tsx
@@ -1126,6 +1126,7 @@ export const LiveLayout = ({ CAPIArticle, NAV, format }: Props) => {
 											/>
 										</ArticleContainer>
 									</Accordion>
+									)}
 								</div>
 							</GridItem>
 							<GridItem area="right-column">
