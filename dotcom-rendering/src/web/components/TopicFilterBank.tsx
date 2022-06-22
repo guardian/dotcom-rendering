552fc98fb99992564fb3f9ddb70fd515322a697f
Anna Beddow <anna.beddow@gmail.com>
2022-06-22 09:43:31 +0100

    Add styling

--- dotcom-rendering/src/web/components/TopicFilterBank.tsx
+++ dotcom-rendering/src/web/components/TopicFilterBank.tsx
@@ -1,16 +1,46 @@
+import { css } from '@emotion/react';
+import { headline, news, textSans } from '@guardian/source-foundations';
+import { Button } from '@guardian/source-react-components';
+
 type Props = {
 	topics: Topic[];
 };
 
+const containerStyles = css`
+	padding: 12px 0;
+`;
+const headlineStyles = css`
+	${headline.xxxsmall({ fontWeight: 'bold', lineHeight: 'tight' })}
+`;
+
+const headlineAccentStyles = css`
+	${textSans.small({ fontWeight: 'bold', lineHeight: 'tight' })};
+`;
+
 export const TopicFilterBank = ({ topics }: Props) => {
-	console.log(topics);
 	return (
-		<div>
-			<div>
-				Filters (<span>BETA</span>):
+		<div css={containerStyles}>
+			<div css={headlineStyles}>
+				Filters{' '}
+				<span css={headlineAccentStyles}>
+					(
+					<span
+						css={css`
+							color: ${news[300]};
+						`}
+					>
+						BETA
+					</span>
+					)
+				</span>
+				:
 			</div>
 			{topics.map((topic) => {
-				return <h1>{topic.value}</h1>;
+				return (
+					<Button>
+						{topic.value} ({topic.count})
+					</Button>
+				);
 			})}
 		</div>
 	);
