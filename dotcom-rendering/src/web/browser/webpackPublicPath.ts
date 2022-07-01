5581b321528a50f9f3b2e5cbdf6af2636536c13d
Chris Jones <christopher.jones@guardian.co.uk>
2022-07-01 08:39:37 +0100

    Import decidePublicPath from separate file
    This is so that we don't have to deal with issues that arise from
    using `__webpack_public_path` in a Jest context.

--- dotcom-rendering/src/web/browser/webpackPublicPath.ts
+++ dotcom-rendering/src/web/browser/webpackPublicPath.ts
@@ -1,26 +1,7 @@
-/**
- * Determine the path that webpack should use as base for dynamic imports
- *
- * @returns The webpack public path to use
- */
-const decidePublicPath = () => {
-	const isDev = process.env.NODE_ENV === 'development';
-	if (isDev && window.location.hostname === 'localhost') {
-		// Use the absolute path when in development mode and running locally
-		// Ensures asset paths are correct relative to Frontend
-		return 'http://localhost:3030/assets/';
-	} else if (
-		window.location.hostname === (process.env.HOSTNAME ?? 'localhost')
-	) {
-		// Use a relative path when hostname is set or using localhost in non-dev environments
-		// e.g. when using a reverse proxy, or running in CI
-		return '/assets/';
-	} else {
-		return `${window.guardian.config.frontendAssetsFullURL}assets/`;
-	}
-};
-
 // allows us to define public path dynamically
 // dynamic imports will use this as the base to find their assets
+
+import { decidePublicPath } from './decidePublicPath';
+
 // https://webpack.js.org/guides/public-path/#on-the-fly
 __webpack_public_path__ = decidePublicPath();
