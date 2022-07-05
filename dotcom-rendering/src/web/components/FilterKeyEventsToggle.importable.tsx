67ff13b605e53e0f6133364617bc54eb4c1a2d7a
Silvija Blaslov <sblasl01@mail.bbk.ac.uk>
2022-07-05 16:39:21 +0100

    Pass tooltip prop to FilterKeyEventsToggle

--- dotcom-rendering/src/web/components/FilterKeyEventsToggle.importable.tsx
+++ dotcom-rendering/src/web/components/FilterKeyEventsToggle.importable.tsx
@@ -23,7 +23,7 @@ const toggleWrapperStyles = css`
 interface Props {
 	filterKeyEvents: boolean;
 	id: 'filter-toggle-mobile' | 'filter-toggle-desktop';
-	tooltip: true;
+	tooltip: boolean;
 }
 
 export const FilterKeyEventsToggle = ({ filterKeyEvents, id }: Props) => {
