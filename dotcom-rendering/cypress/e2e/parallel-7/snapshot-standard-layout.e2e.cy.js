cd96505e0a521956ee8f635948baf126b10898f5
Ravi <7014230+arelra@users.noreply.github.com>
2022-07-07 11:15:37 +0100

    Improve test descriptions

--- dotcom-rendering/cypress/e2e/parallel-7/snapshot-standard-layout.e2e.cy.js
+++ dotcom-rendering/cypress/e2e/parallel-7/snapshot-standard-layout.e2e.cy.js
@@ -3,13 +3,13 @@ import { setLocalBaseUrl } from '../../lib/setLocalBaseUrl.js';
 import { breakpoints } from '@guardian/source-foundations';
 import '@percy/cypress';
 
-describe('E2E Page rendering', function () {
+describe('Snapshot standard layout', function () {
 	beforeEach(function () {
 		disableCMP();
 		setLocalBaseUrl();
 	});
 
-	it('Snapshot standard layout', function () {
+	it('Snapshot all breakpoints', function () {
 		cy.visit(
 			'/Article?url=https://www.theguardian.com/uk-news/2020/dec/04/edinburgh-hit-by-thundersnow-as-sonic-boom-wakes-residents',
 		);
