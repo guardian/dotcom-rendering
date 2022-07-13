583da32b099d9144524bac1e3ce53fd5452eca22
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2022-07-13 14:48:13 +0100

    Fixed tests

--- apps-rendering/src/components/Tags/Tags.test.tsx
+++ apps-rendering/src/components/Tags/Tags.test.tsx
@@ -16,15 +16,16 @@ describe('Tags component renders as expected', () => {
 		const tags = renderer.create(
 			<Tags item={article} />,
 		);
-		const link = tags.root.findByType('a');
-		expect(link.props.href).toBe(article.tags[0].webUrl);
+		const links = tags.root.findAllByType('a');
+		expect(links[0].props.href).toBe(article.tags[0].webUrl);
 	});
 
 	it('Renders tag title', () => {
 		const tags = renderer.create(
 			<Tags item={article} />,
 		);
-		expect(tags.root.findByType('a').children.includes(article.tags[0].webTitle)).toBe(
+		const links = tags.root.findAllByType('a')
+		expect(links[0].children.includes(article.tags[0].webTitle)).toBe(
 			true,
 		);
 	});
@@ -33,6 +34,6 @@ describe('Tags component renders as expected', () => {
 		const tags = renderer.create(
 			<Tags item={article} />,
 		);
-		expect(tags.root.findAllByType('li').length).toBe(7);
+		expect(tags.root.findAllByType('li').length).toBe(6);
 	});
 });
