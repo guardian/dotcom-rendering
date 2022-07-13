58112f39c0df9ac8cf91342f4eb7ddbb75872c43
David Blatcher <dv.bltchr@gmail.com>
2022-07-13 12:15:14 +0100

    enhancer places signup element in the middle of the article

--- dotcom-rendering/src/model/enhance-newsletter-signup.ts
+++ dotcom-rendering/src/model/enhance-newsletter-signup.ts
@@ -1,3 +1,9 @@
+// places the NewsletterSignup in the middle of the array
+// TO DO - more sophisicated placement when business logic is decided.
+const findIndexToInsertAt = (elements: CAPIElement[]): number => {
+	return Math.floor(elements.length / 2);
+};
+
 export const enhanceNewsletterSignup = (
 	blocks: Block[],
 	newsletter?: Newsletter,
@@ -6,7 +12,9 @@ export const enhanceNewsletterSignup = (
 		return blocks;
 	}
 
-	blocks[0]?.elements.unshift({
+	const { elements } = blocks[0];
+
+	elements.splice(findIndexToInsertAt(elements), 0, {
 		_type: 'model.dotcomrendering.pageElements.NewsletterSignupBlockElement',
 		newsletter: newsletter,
 		elementId: newsletter.elementId,
