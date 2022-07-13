7dbf7f2f9b198d10007c5942f97c97e892a21989
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-07-13 10:00:22 +0100

    feat: Use a different layout when the primary (first) card of `dynamic/package` is boosted

--- dotcom-rendering/src/web/components/DynamicPackage.stories.tsx
+++ dotcom-rendering/src/web/components/DynamicPackage.stories.tsx
@@ -22,7 +22,7 @@ export default {
 	},
 };
 
-export const Default = () => (
+export const Three = () => (
 	<ContainerLayout
 		title="DynamicPackage"
 		showTopBorder={true}
@@ -31,12 +31,119 @@ export const Default = () => (
 		centralBorder="partial"
 	>
 		<DynamicPackage
-			trails={trails}
-			showAge={true}
+			trails={[...trails].slice(0, 3)}
 			containerPalette="LongRunningPalette"
 		/>
 	</ContainerLayout>
 );
-Default.story = {
-	name: 'DynamicPackage',
+Three.story = {
+	name: 'With three cards',
+};
+
+export const Four = () => (
+	<ContainerLayout
+		title="DynamicPackage"
+		showTopBorder={true}
+		sideBorders={true}
+		padContent={false}
+		centralBorder="partial"
+	>
+		<DynamicPackage
+			trails={[...trails].slice(0, 4)}
+			containerPalette="LongRunningPalette"
+		/>
+	</ContainerLayout>
+);
+Four.story = {
+	name: 'With four cards',
+};
+
+export const Five = () => (
+	<ContainerLayout
+		title="DynamicPackage"
+		showTopBorder={true}
+		sideBorders={true}
+		padContent={false}
+		centralBorder="partial"
+	>
+		<DynamicPackage
+			trails={[...trails].slice(0, 5)}
+			containerPalette="LongRunningPalette"
+		/>
+	</ContainerLayout>
+);
+Five.story = {
+	name: 'With five cards',
+};
+
+export const Boosted3 = () => {
+	const primary = [...trails].slice(0)[0];
+	const remaining = [...trails].slice(1, 3);
+
+	return (
+		<ContainerLayout
+			title="DynamicPackage"
+			showTopBorder={true}
+			sideBorders={true}
+			padContent={false}
+			centralBorder="partial"
+		>
+			<DynamicPackage
+				trails={[{ ...primary, isBoosted: true }, ...remaining]}
+				showAge={true}
+				containerPalette="LongRunningPalette"
+			/>
+		</ContainerLayout>
+	);
+};
+Boosted3.story = {
+	name: 'With three cards - boosted',
+};
+
+export const Boosted4 = () => {
+	const primary = [...trails].slice(0)[0];
+	const remaining = [...trails].slice(1, 4);
+
+	return (
+		<ContainerLayout
+			title="DynamicPackage"
+			showTopBorder={true}
+			sideBorders={true}
+			padContent={false}
+			centralBorder="partial"
+		>
+			<DynamicPackage
+				trails={[{ ...primary, isBoosted: true }, ...remaining]}
+				showAge={true}
+				containerPalette="LongRunningPalette"
+			/>
+		</ContainerLayout>
+	);
+};
+Boosted4.story = {
+	name: 'With four cards - boosted',
+};
+
+export const Boosted5 = () => {
+	const primary = [...trails].slice(0)[0];
+	const remaining = [...trails].slice(1, 5);
+
+	return (
+		<ContainerLayout
+			title="DynamicPackage"
+			showTopBorder={true}
+			sideBorders={true}
+			padContent={false}
+			centralBorder="partial"
+		>
+			<DynamicPackage
+				trails={[{ ...primary, isBoosted: true }, ...remaining]}
+				showAge={true}
+				containerPalette="LongRunningPalette"
+			/>
+		</ContainerLayout>
+	);
+};
+Boosted5.story = {
+	name: 'With five cards - boosted',
 };
