6d082fd718a3da5094ac015c49a7160f6a53adf4
David Blatcher <dv.bltchr@gmail.com>
2022-07-14 16:07:56 +0100

    place the signup block between two paragraph elements if possible

--- dotcom-rendering/src/model/enhance-newsletter-signup.ts
+++ dotcom-rendering/src/model/enhance-newsletter-signup.ts
@@ -1,7 +1,41 @@
-// places the NewsletterSignup in the middle of the array
-// TO DO - more sophisicated placement when business logic is decided.
+/**
+ * Finds a position in the elements array that is at or near the middle
+ * and bewteen two paragraph elements. Returns the middle position if
+ * no between paragraphs is found.
+ *
+ * @param elements the elements in the article block
+ * @returns an index to place the NewsletterSignup component at
+ */
 const findIndexToInsertAt = (elements: CAPIElement[]): number => {
-	return Math.floor(elements.length / 2);
+	const isBetweenParagraphs: { (index: number): boolean } = (index) =>
+		elements[index - 1]?._type ===
+			'model.dotcomrendering.pageElements.TextBlockElement' &&
+		elements[index]?._type ===
+			'model.dotcomrendering.pageElements.TextBlockElement';
+
+	const targetIndex = Math.floor(elements.length / 2);
+
+	// If the target position is between two paragraphs, place the NewsletterSignup there
+	if (isBetweenParagraphs(targetIndex)) {
+		return targetIndex;
+	}
+
+	const maxDistance = Math.floor(elements.length / 5);
+
+	// find the positions within the elements that are between two paragraphs
+	// and within the maxDistance
+	const possiblePositions = elements
+		.slice(1)
+		.map((element, index) => ({
+			index,
+			distance: Math.abs(index - targetIndex),
+			okay: isBetweenParagraphs(index),
+		}))
+		.filter((spot) => spot.okay && spot.distance <= maxDistance)
+		.sort((a, b) => a.distance - b.distance); // closest spot first
+
+	// Place at the closest spot between two paragraphs - if there are none, place at the target position
+	return possiblePositions[0]?.index || targetIndex;
 };
 
 export const enhanceNewsletterSignup = (
