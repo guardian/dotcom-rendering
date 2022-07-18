3b1347fea833bfd4b00cbafdd8e9a38102329347
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-07-18 08:50:53 +0100

    fix: Replace `Card` with `FrontCard` after merge issue

--- dotcom-rendering/src/web/components/DynamicPackage.tsx
+++ dotcom-rendering/src/web/components/DynamicPackage.tsx
@@ -20,40 +20,14 @@ export const DynamicPackage = ({
 			<>
 				<UL direction="row">
 					<LI padSides={true}>
-						<Card
+						<FrontCard
+							trail={primary}
 							containerPalette={containerPalette}
-							containerType="dynamic/package"
 							showAge={showAge}
-							linkTo={primary.url}
-							format={primary.format}
-							headlineText={primary.headline}
 							headlineSize="ginormous"
-							byline={primary.byline}
-							showByline={primary.showByline}
-							showQuotes={
-								primary.format.design ===
-									ArticleDesign.Comment ||
-								primary.format.design === ArticleDesign.Letter
-							}
-							webPublicationDate={primary.webPublicationDate}
-							kickerText={primary.kickerText}
-							showPulsingDot={
-								primary.format.design === ArticleDesign.LiveBlog
-							}
-							showSlash={true}
-							showClock={false}
-							imageUrl={primary.image}
 							imagePosition="bottom"
 							imagePositionOnMobile="bottom"
 							imageSize="large"
-							mediaType={primary.mediaType}
-							mediaDuration={primary.mediaDuration}
-							starRating={primary.starRating}
-							branding={primary.branding}
-							supportingContent={primary.supportingContent}
-							dataLinkName={primary.dataLinkName}
-							snapData={primary.snapData}
-							discussionId={primary.discussionId}
 							isDynamo={true}
 						/>
 					</LI>
@@ -71,39 +45,11 @@ export const DynamicPackage = ({
 								}
 								padBottomOnMobile={false}
 							>
-								<Card
+								<FrontCard
+									trail={card}
 									containerPalette={containerPalette}
-									containerType="dynamic/package"
 									showAge={showAge}
-									linkTo={card.url}
-									format={card.format}
-									headlineText={card.headline}
 									headlineSize="medium"
-									byline={card.byline}
-									showByline={card.showByline}
-									showQuotes={
-										card.format.design ===
-											ArticleDesign.Comment ||
-										card.format.design ===
-											ArticleDesign.Letter
-									}
-									webPublicationDate={card.webPublicationDate}
-									kickerText={card.kickerText}
-									showPulsingDot={
-										card.format.design ===
-										ArticleDesign.LiveBlog
-									}
-									showSlash={true}
-									showClock={false}
-									imageUrl={card.image}
-									mediaType={card.mediaType}
-									mediaDuration={card.mediaDuration}
-									starRating={card.starRating}
-									branding={card.branding}
-									supportingContent={card.supportingContent}
-									dataLinkName={card.dataLinkName}
-									snapData={card.snapData}
-									discussionId={card.discussionId}
 								/>
 							</LI>
 						);
@@ -120,7 +66,6 @@ export const DynamicPackage = ({
 						trail={primary}
 						containerPalette={containerPalette}
 						showAge={showAge}
-						containerType="dynamic/package"
 						headlineSize="huge"
 						imagePosition="bottom"
 						imagePositionOnMobile="bottom"
@@ -151,7 +96,6 @@ export const DynamicPackage = ({
 										trail={card}
 										containerPalette={containerPalette}
 										showAge={showAge}
-										containerType="dynamic/package"
 										imageUrl={
 											// Always show the image on the first card and only
 											// on the second if there are two items in two
