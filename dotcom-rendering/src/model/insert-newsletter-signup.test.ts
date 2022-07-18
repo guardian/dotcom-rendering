f8b5f82f02ac0611e59993cb62beec0782318bc3
David Blatcher <dv.bltchr@gmail.com>
2022-07-18 17:02:09 +0100

    add test for the findInsertIndex function

--- dotcom-rendering/src/model/insert-newsletter-signup.test.ts
+++ dotcom-rendering/src/model/insert-newsletter-signup.test.ts
@@ -1,7 +1,10 @@
 import { Live as exampleLiveBlog } from '../../fixtures/generated/articles/Live';
 import { Quiz as exampleQuiz } from '../../fixtures/generated/articles/Quiz';
 import { Standard as exampleStandard } from '../../fixtures/generated/articles/Standard';
-import { insertNewsletterSignup } from './insert-newsletter-signup';
+import {
+	findInsertIndex,
+	insertNewsletterSignup,
+} from './insert-newsletter-signup';
 
 const sampleNewsletter: Newsletter = {
 	listId: 123,
@@ -25,7 +28,7 @@ const sampleNewsletterSignupBlockElement: CAPIElement = {
 const toElementTypeLists = (blocks: Block[]): string[][] =>
 	blocks.map((block) => block.elements.map((element) => element._type));
 
-describe('Enhance Newsletter Signups', () => {
+describe('Insert Newsletter Signups', () => {
 	it('makes no change if there is no newsletter', () => {
 		const elementTypesBeforeEnhancement = toElementTypeLists(
 			exampleStandard.blocks,
@@ -88,5 +91,47 @@ describe('Enhance Newsletter Signups', () => {
 						'model.dotcomrendering.pageElements.NewsletterSignupBlockElement',
 				),
 		).toBeFalsy();
+	});
+});
+
+const SubheadingBlockElement = {
+	_type: 'model.dotcomrendering.pageElements.SubheadingBlockElement',
+} as CAPIElement;
+const PullquoteBlockElement = {
+	_type: 'model.dotcomrendering.pageElements.PullquoteBlockElement',
+} as CAPIElement;
+const TextBlockElement = {
+	_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+} as CAPIElement;
+const testElementsOne = [
+	PullquoteBlockElement,
+	SubheadingBlockElement,
+	PullquoteBlockElement,
+	TextBlockElement,
+	TextBlockElement,
+	TextBlockElement,
+];
+const testElementsTwo = [
+	TextBlockElement,
+	SubheadingBlockElement,
+	TextBlockElement,
+	SubheadingBlockElement,
+	TextBlockElement,
+	TextBlockElement,
+	TextBlockElement,
+] as CAPIElement[];
+
+describe('Find index to insert Newsletter Signup', () => {
+	it('will move the shortst distance from the target to be in a good spot', () => {
+		expect(findInsertIndex(testElementsTwo, 2, 100)).toBe(1);
+	});
+	it('will place at the target index when max distance is 0', () => {
+		expect(findInsertIndex(testElementsOne, 1, 0)).toBe(1);
+	});
+	it('will move to find a place at least two spaces after any pullquote, if there is one within the max distance', () => {
+		expect(findInsertIndex(testElementsOne, 2, 3)).toBe(5);
+	});
+	it('will move not move futher than the max distance to find a better place than the target', () => {
+		expect(findInsertIndex(testElementsOne, 2, 1)).toBe(2);
 	});
 });
