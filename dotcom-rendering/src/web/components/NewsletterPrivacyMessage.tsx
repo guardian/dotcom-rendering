b35116f3122f3ccb5feae89422df3f53c027f479
David Blatcher <dv.bltchr@gmail.com>
2022-07-19 19:09:39 +0100

    remove unnecessary fragment

--- dotcom-rendering/src/web/components/NewsletterPrivacyMessage.tsx
+++ dotcom-rendering/src/web/components/NewsletterPrivacyMessage.tsx
@@ -21,35 +21,33 @@ const termsStyle = css`
 `;
 
 export const NewsletterPrivacyMessage = () => (
-	<>
-		<span css={termsStyle}>
-			<strong>Privacy Notice: </strong>
-			Newsletters may contain info about charities, online ads, and
-			content funded by outside parties. For more information see our{' '}
-			<Link
-				data-ignore="global-link-styling"
-				href="https://www.theguardian.com/help/privacy-policy"
-				rel="noopener noreferrer"
-			>
-				privacy policy
-			</Link>
-			. We use Google reCaptcha to protect our website and the Google{' '}
-			<Link
-				data-ignore="global-link-styling"
-				href="https://policies.google.com/privacy"
-				rel="noopener noreferrer"
-			>
-				Privacy Policy
-			</Link>{' '}
-			and{' '}
-			<Link
-				data-ignore="global-link-styling"
-				href="https://policies.google.com/terms"
-				rel="noopener noreferrer"
-			>
-				Terms of Service
-			</Link>{' '}
-			apply.
-		</span>
-	</>
+	<span css={termsStyle}>
+		<strong>Privacy Notice: </strong>
+		Newsletters may contain info about charities, online ads, and content
+		funded by outside parties. For more information see our{' '}
+		<Link
+			data-ignore="global-link-styling"
+			href="https://www.theguardian.com/help/privacy-policy"
+			rel="noopener noreferrer"
+		>
+			privacy policy
+		</Link>
+		. We use Google reCaptcha to protect our website and the Google{' '}
+		<Link
+			data-ignore="global-link-styling"
+			href="https://policies.google.com/privacy"
+			rel="noopener noreferrer"
+		>
+			Privacy Policy
+		</Link>{' '}
+		and{' '}
+		<Link
+			data-ignore="global-link-styling"
+			href="https://policies.google.com/terms"
+			rel="noopener noreferrer"
+		>
+			Terms of Service
+		</Link>{' '}
+		apply.
+	</span>
 );
