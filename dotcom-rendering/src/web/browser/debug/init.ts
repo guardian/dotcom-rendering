b6461c55fceaa6fd487b61b6145f334c8b63f22a
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-07-22 16:10:56 +0100

    Add support for DCR debug bookmarklet

--- dotcom-rendering/src/web/browser/debug/init.ts
+++ dotcom-rendering/src/web/browser/debug/init.ts
@@ -1,5 +1,7 @@
+// @ts-expect-error: Cannot find module
 import debugCss from './debug.css';
 
 const style = document.createElement('style');
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- We know this will be a string
 style.innerHTML = debugCss;
 document.body.appendChild(style);
