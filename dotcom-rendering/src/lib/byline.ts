c2028af311c8105d5d03c28c6ea2bea92af00470
Max Duval <max.duval@theguardian.com>
2022-07-25 19:55:54 +0100

    fix(byline): handle more cases
    - empty bylines
    - bylines with location info

--- dotcom-rendering/src/lib/byline.ts
+++ dotcom-rendering/src/lib/byline.ts
@@ -12,8 +12,9 @@ export const isContributor = (tag: TagType): tag is ContributorTag =>
  * such as their picture, bio or social media handle.
  *
  * An article has a sole contributor only if all of the following are true:
- * - there is only one `Contributor` tag
- * - that tag’s `title` is the same as the article byline
+ * - there is a byline which is not an empty string
+ * - the byline does not include the word “and”
+ * - there is a `Contributor` tag whose title is included in the byline
  *
  * @returns A sole contributor if there is one, `undefined` otherwise.
  */
@@ -21,17 +22,12 @@ export const getSoleContributor = (
 	tags: TagType[],
 	byline: string | undefined,
 ): ContributorTag | undefined => {
-	/**
-	 * Want to read more about array destructuring and the rest syntax?
-	 * Here, soleContributor is of type `ContributorTag | undefined`
-	 * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#syntax
-	 * @see https://javascript.info/destructuring-assignment#the-rest
-	 */
-	const [soleContributor, ...otherContributors] = tags.filter(isContributor);
+	if (!byline) return undefined;
+	if (byline.includes(' and ')) return undefined;
 
-	if (otherContributors.length !== 0) return undefined;
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- it could be `undefined`, but we don’t have `strictNullChecks` enabled
-	if (soleContributor?.title !== byline) return undefined;
+	const soleContributor = tags
+		.filter(isContributor)
+		.find(({ title }) => byline.includes(title));
 
 	return soleContributor;
 };
