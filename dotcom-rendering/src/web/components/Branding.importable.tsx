aba36298821b7739da91677dfc66e94088cf2923
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-07-27 15:57:43 +0100

    Fix contrasts for branding on liveblogs mobile
    The background colour for the meta on liveblogs is dark on mobile, light
    on desktop.
    
    This commit uses light colours and the light variant of the logo on
    smaller breakpoints. (Largely following the pattern already used in
    `Contributor.tsx`.

--- dotcom-rendering/src/web/components/Branding.importable.tsx
+++ dotcom-rendering/src/web/components/Branding.importable.tsx
@@ -1,46 +1,132 @@
 import { css } from '@emotion/react';
-import { neutral, textSans } from '@guardian/source-foundations';
+import { ArticleDesign } from '@guardian/libs';
+import { neutral, textSans, until } from '@guardian/source-foundations';
 import { trackSponsorLogoLinkClick } from '../browser/ga/ga';
 
 const brandingStyle = css`
 	padding-bottom: 10px;
 `;
 
-const brandingLabelStyle = css`
-	${textSans.xxsmall()};
-	color: ${neutral[20]};
-`;
+// for liveblog smaller breakpoints article meta is located in the same
+// container as standfirst and needs the same styling as standfirst
+function brandingLabelStyle(palette: Palette, format: ArticleFormat) {
+	const invariantStyles = css`
+		${textSans.xxsmall()}
+	`;
+
+	switch (format.design) {
+		case ArticleDesign.LiveBlog: {
+			return [
+				invariantStyles,
+				css`
+					color: ${neutral[20]};
+
+					${until.desktop} {
+						color: ${palette.text.standfirst};
+					}
+
+					a {
+						color: ${neutral[20]};
+
+						${until.desktop} {
+							color: ${palette.text.standfirst};
+						}
+					}
+				`,
+			];
+		}
+		default: {
+			return [
+				invariantStyles,
+				css`
+					color: ${neutral[20]};
+
+					a {
+						color: ${neutral[20]};
+					}
+				`,
+			];
+		}
+	}
+}
 
 const brandingLogoStyle = css`
 	padding: 10px 0;
 
 	display: block;
-	img {
+`;
+
+// for liveblog smaller breakpoints article meta is located in the same
+// container as standfirst and needs the same styling as standfirst
+const brandingAboutLink = (palette: Palette, format: ArticleFormat) => {
+	const invariantStyles = css`
+		${textSans.xxsmall()}
 		display: block;
+		text-decoration: none;
+		&:hover {
+			text-decoration: underline;
+		}
+	`;
+
+	switch (format.design) {
+		case ArticleDesign.LiveBlog: {
+			return [
+				invariantStyles,
+				css`
+					color: ${palette.text.branding};
+					${until.desktop} {
+						color: ${palette.text.standfirst};
+					}
+					a {
+						color: ${palette.text.branding};
+						${until.desktop} {
+							color: ${palette.text.standfirst};
+						}
+					}
+				`,
+			];
+		}
+		default: {
+			return [
+				invariantStyles,
+				css`
+					color: ${palette.text.branding};
+					a {
+						color: ${palette.text.branding};
+					}
+				`,
+			];
+		}
+	}
+};
+
+const hiddenUntilDesktop = css`
+	${until.desktop} {
+		display: none;
 	}
 `;
 
-const brandingAboutLink = (palette: Palette) => css`
-	color: ${palette.text.branding};
-	${textSans.xxsmall()}
-	display: block;
-	text-decoration: none;
-	&:hover {
-		text-decoration: underline;
+const hiddenFromDesktop = css`
+	display: none;
+	${until.desktop} {
+		display: block;
 	}
 `;
 
 type Props = {
 	branding: Branding;
 	palette: Palette;
+	format: ArticleFormat;
 };
 
-export const Branding = ({ branding, palette }: Props) => {
+export const Branding = ({ branding, palette, format }: Props) => {
 	const sponsorId = branding.sponsorName.toLowerCase();
 
 	return (
 		<div css={brandingStyle}>
-			<div css={brandingLabelStyle}>{branding.logo.label}</div>
+			<div css={brandingLabelStyle(palette, format)}>
+				{branding.logo.label}
+			</div>
 			<div css={brandingLogoStyle}>
 				<a
 					href={branding.logo.link}
@@ -51,6 +137,17 @@ export const Branding = ({ branding, palette }: Props) => {
 					data-cy="branding-logo"
 				>
 					<img
+						css={hiddenFromDesktop}
+						width={branding.logo.dimensions.width}
+						height={branding.logo.dimensions.height}
+						src={
+							branding.logoForDarkBackground?.src ??
+							branding.logo.src
+						}
+						alt={branding.sponsorName}
+					/>
+					<img
+						css={hiddenUntilDesktop}
 						width={branding.logo.dimensions.width}
 						height={branding.logo.dimensions.height}
 						src={branding.logo.src}
@@ -59,7 +156,10 @@ export const Branding = ({ branding, palette }: Props) => {
 				</a>
 			</div>
 
-			<a href={branding.aboutThisLink} css={brandingAboutLink(palette)}>
+			<a
+				href={branding.aboutThisLink}
+				css={brandingAboutLink(palette, format)}
+			>
 				About this content
 			</a>
 		</div>
