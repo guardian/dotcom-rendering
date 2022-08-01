f553a2c7ea803ab3b2bc891744c0236644ce6d71
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-08-01 14:10:48 +0100

    Update dotcom-rendering/src/model/enhanceTreats.ts
    Co-authored-by: Max Duval <max.duval@theguardian.com>

--- dotcom-rendering/src/model/enhanceTreats.ts
+++ dotcom-rendering/src/model/enhanceTreats.ts
@@ -1,9 +1,7 @@
 import type { FEFrontCard, TreatType } from '../types/front';
 
 export const enhanceTreats = (treats: FEFrontCard[]): TreatType[] =>
-	treats.map((treat) => {
-		return {
-			text: treat.header.headline,
-			linkTo: treat.properties.href ?? treat.header.url,
-		};
-	});
+	treats.map((treat) => ({
+		text: treat.header.headline,
+		linkTo: treat.properties.href ?? treat.header.url,
+	}));
