24a82d6a5b565a3a18860320178a5686a366c8a1
David Blatcher <dv.bltchr@gmail.com>
2022-08-03 16:21:28 +0100

    only remove the old embeds for newsletters promoted with a new sign up block

--- dotcom-rendering/src/model/remove-old-signup-embeds.ts
+++ dotcom-rendering/src/model/remove-old-signup-embeds.ts
@@ -1,28 +1,45 @@
-const hasNewsletterSignupBlockElement = (block: Block): boolean =>
-	!!block.elements.find(
-		(element) =>
-			element._type ===
-			'model.dotcomrendering.pageElements.NewsletterSignupBlockElement',
-	);
+import { JSDOM } from 'jsdom';
 
-// TO DO - smarter HTML checking using regex?
 const isEmailEmbed = (element: CAPIElement): boolean =>
 	element._type === 'model.dotcomrendering.pageElements.EmbedBlockElement' &&
 	element.html.includes('<iframe') &&
 	element.html.includes('email/form');
 
+const getPromotedNewsletters = (elements: CAPIElement[]): Newsletter[] =>
+	elements
+		.filter(
+			(element) =>
+				element._type ===
+				'model.dotcomrendering.pageElements.NewsletterSignupBlockElement',
+		)
+		.map((element) => (element as NewsletterSignupBlockElement).newsletter);
+
+const getEmbedIframeSrc = (embed: EmbedBlockElement): string | undefined =>
+	JSDOM.fragment(embed.html).querySelector('iframe')?.getAttribute('src') ||
+	undefined;
+
+
 const remove = (elements: CAPIElement[]): CAPIElement[] => {
 	const emailEmbeds = elements.filter(isEmailEmbed) as EmbedBlockElement[];
-	const embedstoRemove = emailEmbeds as CAPIElement[]
-	return elements.filter(element => !embedstoRemove.includes(element))
+	if (emailEmbeds.length === 0) {
+		return elements;
+	}
+	const promotedNewsletters = getPromotedNewsletters(elements);
+
+	const elementsToRemove = emailEmbeds.filter((embed) => {
+		const iframeSrc = getEmbedIframeSrc(embed);
+		// The iframe src for an email embed ends with either the listId or
+		// identityName of the newsletter.
+		return !iframeSrc || promotedNewsletters.some((newsletter) =>
+			iframeSrc?.includes(newsletter.identityName) || iframeSrc.includes(newsletter.listId.toString()),
+		);
+	}) as CAPIElement[];
+
+	return elements.filter((element) => !elementsToRemove.includes(element));
 };
 
 export const removeOldSignUpEmbeds = (blocks: Block[]): Block[] =>
-	blocks.map((block: Block) =>
-		hasNewsletterSignupBlockElement(block)
-			? {
-					...block,
-					elements: remove(block.elements),
-			  }
-			: block,
-	);
+	blocks.map((block: Block) => ({
+		...block,
+		elements: remove(block.elements),
+	}));
