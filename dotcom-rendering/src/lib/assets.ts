81da90111e7c84181654aa150d6ed98cec535df2
Max Duval <max.duval@theguardian.com>
2022-08-04 10:56:43 +0100

    fixup! feat(server): load arbitrary manifest

--- dotcom-rendering/src/lib/assets.ts
+++ dotcom-rendering/src/lib/assets.ts
@@ -1,5 +1,5 @@
-import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 interface AssetHash {
 	[key: string]: string;
