6317d0e1f2dd77fa35d74992791f62ea46c982d7
David Blatcher <dv.bltchr@gmail.com>
2022-08-05 15:21:19 +0100

    reduce form element sizes and margin placement

--- dotcom-rendering/__mocks__/SecureSignupMock.tsx
+++ dotcom-rendering/__mocks__/SecureSignupMock.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { neutral, space } from '@guardian/source-foundations';
+import { neutral, space, textSans } from '@guardian/source-foundations';
 import { Button, Label, TextInput } from '@guardian/source-react-components';
 import { NewsletterPrivacyMessage } from '../src/web/components/NewsletterPrivacyMessage';
 
@@ -9,21 +9,34 @@ export const SecureSignup = ({ newsletterId }: Props) => {
 	return (
 		<div title="This is a mock for the SecureSignup component which is not compatible with storybook. Changes to the real component will not be automatically reflected in this mock.">
 			<form id={`secure-signup-${newsletterId}`}>
-				<Label text="Enter your email address" />
+				<Label
+					text="Enter your email address"
+					css={css`
+						div {
+							${textSans.xsmall({ fontWeight: 'bold' })}
+						}
+					`}
+				/>
 
 				<div
 					css={css`
 						display: flex;
 						flex-direction: row;
-						align-items: flex-end;
+						align-items: flex-start;
 						flex-wrap: wrap;
 					`}
 				>
 					<div
 						css={css`
 							margin-right: ${space[3]}px;
+							margin-bottom: ${space[2]}px;
 							flex-basis: 335px;
 							flex-shrink: 1;
+
+							input {
+								height: 36px;
+								margin-top: 0;
+							}
 						`}
 					>
 						<TextInput
@@ -35,6 +48,7 @@ export const SecureSignup = ({ newsletterId }: Props) => {
 					</div>
 					<Button
 						disabled={true}
+						size="small"
 						cssOverrides={css`
 							justify-content: center;
 							background-color: ${neutral[0]};
@@ -43,7 +57,7 @@ export const SecureSignup = ({ newsletterId }: Props) => {
 							}
 							flex-basis: 118px;
 							flex-shrink: 0;
-							margin-top: ${space[2]}px;
+							margin-bottom: ${space[2]}px;
 						`}
 						type="submit"
 					>
@@ -51,13 +65,7 @@ export const SecureSignup = ({ newsletterId }: Props) => {
 					</Button>
 				</div>
 			</form>
-			<div
-				css={css`
-					margin-top: ${space[2]}px;
-				`}
-			>
-				<NewsletterPrivacyMessage />
-			</div>
+			<NewsletterPrivacyMessage />
 		</div>
 	);
 };
--- dotcom-rendering/src/web/components/SecureSignup.tsx
+++ dotcom-rendering/src/web/components/SecureSignup.tsx
@@ -1,7 +1,7 @@
 import createCache from '@emotion/cache';
 import { CacheProvider, css } from '@emotion/react';
 import createEmotionServer from '@emotion/server/create-instance';
-import { neutral, space } from '@guardian/source-foundations';
+import { neutral, space, textSans } from '@guardian/source-foundations';
 import { Button, Label, TextInput } from '@guardian/source-react-components';
 import { renderToString } from 'react-dom/server';
 import { Island } from './Island';
@@ -43,21 +43,34 @@ const generateForm = (
 	const html = renderToString(
 		<CacheProvider value={cache}>
 			<form id={`secure-signup-${newsletterId}`}>
-				<Label text="Enter your email address" />
+				<Label
+					text="Enter your email address"
+					css={css`
+						div {
+							${textSans.xsmall({ fontWeight: 'bold' })}
+						}
+					`}
+				/>
 
 				<div
 					css={css`
 						display: flex;
 						flex-direction: row;
-						align-items: flex-end;
+						align-items: flex-start;
 						flex-wrap: wrap;
 					`}
 				>
 					<div
 						css={css`
 							margin-right: ${space[3]}px;
+							margin-bottom: ${space[2]}px;
 							flex-basis: 335px;
 							flex-shrink: 1;
+
+							input {
+								height: 36px;
+								margin-top: 0;
+							}
 						`}
 					>
 						<TextInput
@@ -68,6 +81,7 @@ const generateForm = (
 						/>
 					</div>
 					<Button
+						size="small"
 						cssOverrides={css`
 							justify-content: center;
 							background-color: ${neutral[0]};
@@ -76,7 +90,7 @@ const generateForm = (
 							}
 							flex-basis: 118px;
 							flex-shrink: 0;
-							margin-top: ${space[2]}px;
+							margin-bottom: ${space[2]}px;
 						`}
 						type="submit"
 					>
@@ -100,7 +114,7 @@ export const SecureSignup = ({ newsletterId, successDescription }: Props) => {
 			<Island
 				clientOnly={true}
 				deferUntil={'idle'}
-				placeholderHeight={75}
+				placeholderHeight={65}
 			>
 				<SecureSignupIframe
 					html={html}
@@ -109,13 +123,7 @@ export const SecureSignup = ({ newsletterId, successDescription }: Props) => {
 					successDescription={successDescription}
 				/>
 			</Island>
-			<div
-				css={css`
-					margin-top: ${space[2]}px;
-				`}
-			>
-				<NewsletterPrivacyMessage />
-			</div>
+			<NewsletterPrivacyMessage />
 		</>
 	);
 };
