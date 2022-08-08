6d3505653e8218e9eaf70246b4437c5443d54f66
Charlotte <43961396+cemms1@users.noreply.github.com>
2022-08-08 12:30:29 +0100

    refactor: create signup form component for better storybook mocking

--- dotcom-rendering/__mocks__/SecureSignupMock.tsx
+++ dotcom-rendering/__mocks__/SecureSignupMock.tsx
@@ -1,63 +1,30 @@
 import { css } from '@emotion/react';
-import { neutral, space } from '@guardian/source-foundations';
-import { Button, Label, TextInput } from '@guardian/source-react-components';
+import { space } from '@guardian/source-foundations';
 import { NewsletterPrivacyMessage } from '../src/web/components/NewsletterPrivacyMessage';
+import { NewsletterSignupForm } from '../src/web/components/NewsletterSignupForm';
 
-type Props = { newsletterId: string; successDescription: string };
+type Props = {
+	newsletterId: string;
+	successDescription: string;
+	hidePrivacyMessage?: boolean;
+};
 
-export const SecureSignup = ({ newsletterId }: Props) => {
+export const SecureSignup = ({
+	newsletterId,
+	hidePrivacyMessage = false,
+}: Props) => {
 	return (
-		<div title="This is a mock for the SecureSignup component which is not compatible with storybook. Changes to the real component will not be automatically reflected in this mock.">
-			<form id={`secure-signup-${newsletterId}`}>
-				<Label text="Enter your email address" />
-
+		<div title="This is a mock for the SecureSignup component which is not compatible with storybook. Changes to the real component may not be automatically reflected in this mock.">
+			<NewsletterSignupForm newsletterId={newsletterId} />
+			{!hidePrivacyMessage && (
 				<div
 					css={css`
-						display: flex;
-						flex-direction: row;
-						align-items: flex-end;
-						flex-wrap: wrap;
+						margin-top: ${space[2]}px;
 					`}
 				>
-					<div
-						css={css`
-							margin-right: ${space[3]}px;
-							flex-basis: 335px;
-							flex-shrink: 1;
-						`}
-					>
-						<TextInput
-							hideLabel={true}
-							name="email"
-							label="Enter your email address"
-							type="email"
-						/>
-					</div>
-					<Button
-						disabled={true}
-						cssOverrides={css`
-							justify-content: center;
-							background-color: ${neutral[0]};
-							:hover {
-								background-color: ${neutral[20]};
-							}
-							flex-basis: 118px;
-							flex-shrink: 0;
-							margin-top: ${space[2]}px;
-						`}
-						type="submit"
-					>
-						Sign up
-					</Button>
+					<NewsletterPrivacyMessage />
 				</div>
-			</form>
-			<div
-				css={css`
-					margin-top: ${space[2]}px;
-				`}
-			>
-				<NewsletterPrivacyMessage />
-			</div>
+			)}
 		</div>
 	);
 };
