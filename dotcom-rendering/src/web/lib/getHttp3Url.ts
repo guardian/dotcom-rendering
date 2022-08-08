371dd4beed037fadb8c426d6bb6de313b9760ddc
Paul Roberts <paul.roberts@theguardian.com>
2022-08-08 16:13:54 +0100

    getHttp3Url catch malformed URL exception
    and just return it unmodified because in that case we can't do anything
    anyway

--- dotcom-rendering/src/web/lib/getHttp3Url.ts
+++ dotcom-rendering/src/web/lib/getHttp3Url.ts
@@ -1,7 +1,15 @@
 // Evaluating the performance of HTTP3 over HTTP2, this file can be removed once the experiment is concluded.
 // See: https://github.com/guardian/dotcom-rendering/pull/5394
 export const getHttp3Url = (url: string): string => {
-	const urlObject = new URL(url);
-	urlObject.searchParams.append('http3', 'true');
-	return urlObject.toString();
+	try {
+		const urlObject = new URL(url);
+		urlObject.searchParams.append('http3', 'true');
+		return urlObject.toString();
+	} catch (e) {
+		if (e instanceof TypeError) {
+			return url;
+		} else {
+			throw e;
+		}
+	}
 };
