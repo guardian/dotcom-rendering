d8a4f06267a43719f1b38416ff8600ee9b6e63ef
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-08-09 16:07:52 +0100

    Refactor: Add focus styling to Fronts Cards
    - Add focusHalo to CardLink on focus.
    - Remove unneeded styling from CardLink:
      - Styling of CardLink itself was redundant because opacity was set to 0.
      - The styling applied indirectly to `.image-overlay` was redundant because it
      didn't actually select anything (perhaps copied mistakenly from CardWrapper?)

--- dotcom-rendering/src/web/components/Card/components/CardLink.tsx
+++ dotcom-rendering/src/web/components/Card/components/CardLink.tsx
@@ -1,9 +1,6 @@
 import { css } from '@emotion/react';
-import type { ArticleFormat } from '@guardian/libs';
-import { ArticleDesign, ArticleSpecial } from '@guardian/libs';
-import { neutral } from '@guardian/source-foundations';
+import { focusHalo } from '@guardian/source-foundations';
 import type { DCRContainerPalette } from '../../../../types/front';
-import { decidePalette } from '../../../lib/decidePalette';
 import { getZIndex } from '../../../lib/getZIndex';
 
 const fauxLinkStyles = css`
@@ -13,104 +10,46 @@ const fauxLinkStyles = css`
 	right: 0;
 	bottom: 0;
 	left: 0;
-	opacity: 0;
 `;
 
-const linkStyles = (format: ArticleFormat, palette: Palette) => {
-	const baseLinkStyles = css`
-		display: flex;
-		/* a tag specific styles */
-		color: inherit;
-		text-decoration: none;
-		background-color: ${palette.background.card};
+const baseLinkStyles = css`
+	display: flex;
+	/* a tag specific styles */
+	color: inherit;
+	text-decoration: none;
+	background-color: transparent;
 
-		/* The whole card is one link so we card level styles here */
-		width: 100%;
+	/* The whole card is one link so we card level styles here */
+	width: 100%;
 
-		/* Sometimes a headline contains it's own link so we use the
+	/* Sometimes a headline contains it's own link so we use the
        approach described below to deal with nested links
        See: https://css-tricks.com/nested-links/ */
-		:before {
-			content: '';
-			position: absolute;
-			left: 0;
-			top: 0;
-			right: 0;
-			bottom: 0;
-		}
-
-		:hover .image-overlay {
-			position: absolute;
-			top: 0;
-			width: 100%;
-			height: 100%;
-			left: 0;
-			background-color: ${neutral[7]};
-			opacity: 0.1;
-		}
-	`;
-
-	if (format.theme === ArticleSpecial.SpecialReport) {
-		return css`
-			${baseLinkStyles};
-			:hover {
-				filter: brightness(90%);
-			}
-		`;
+	:before {
+		content: '';
+		position: absolute;
+		left: 0;
+		top: 0;
+		right: 0;
+		bottom: 0;
 	}
 
-	switch (format.design) {
-		case ArticleDesign.Editorial:
-		case ArticleDesign.Letter:
-		case ArticleDesign.Comment:
-			return css`
-				${baseLinkStyles};
-				:hover {
-					/* TODO: This colour is hard coded here because it does not yet
-                           exist in source-foundations. Once it's been added, please
-                           remove this. @siadcock is aware. */
-					/* stylelint-disable-next-line color-no-hex */
-					background-color: #fdf0e8;
-				}
-			`;
-		case ArticleDesign.Gallery:
-		case ArticleDesign.Audio:
-		case ArticleDesign.Video:
-		case ArticleDesign.LiveBlog:
-			return css`
-				${baseLinkStyles};
-				:hover {
-					filter: brightness(90%);
-				}
-			`;
-		default:
-			return css`
-				${baseLinkStyles};
-				:hover {
-					background-color: ${neutral[93]};
-				}
-			`;
+	:focus {
+		${focusHalo};
 	}
-};
+`;
 
 type Props = {
 	linkTo: string;
-	format: ArticleFormat;
 	containerPalette?: DCRContainerPalette;
 	dataLinkName?: string;
 };
 
-export const CardLink = ({
-	linkTo,
-	format,
-	containerPalette,
-	dataLinkName = 'article',
-}: Props) => {
-	const palette = decidePalette(format, containerPalette);
+export const CardLink = ({ linkTo, dataLinkName = 'article' }: Props) => {
 	return (
 		<a
 			href={linkTo}
-			css={[fauxLinkStyles, linkStyles(format, palette)]}
+			css={[fauxLinkStyles, baseLinkStyles]}
 			data-link-name={dataLinkName}
 		/>
 	);
