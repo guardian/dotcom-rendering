50297a7aa41d9350cb919e80dbb49b7a160756c0
David Blatcher <dv.bltchr@gmail.com>
2022-08-11 10:20:11 +0100

    can insert the newsletter inside the first TextElement

--- apps-rendering/src/server/insertNewsletter.ts
+++ apps-rendering/src/server/insertNewsletter.ts
@@ -1,20 +1,57 @@
 import { Newsletter, Item } from 'item';
 import { Option, Result, ResultKind } from '@guardian/types';
-import { BodyElement } from 'bodyElement';
+import { BodyElement, NewsletterSignUp, Text } from 'bodyElement';
 import { ElementKind } from 'bodyElementKind';
 import { ArticleDesign } from '@guardian/libs';
 import { logger } from 'logger';
+import { JSDOM } from 'jsdom';
 
-	// PROBLEM
-	// in AR, sets of paragraphs are combined into one Text BodyElement
-	// may need to call this function BEFORE the content.blocks.elements
-	// are combined into an Item :(
+// The DCR-enhanced capi response breaks out the TextElements
+// into individual paragraphs, but in AR, they have the original
+// CAPI structure where each continual block of html text is a
+// single TextElement.
 
-const findInsertPosition = (
-	body: Result<string, BodyElement>[],
-): number | null => {
+const wrapInResult = (element: BodyElement): Result<string, BodyElement> => ({
+	kind: ResultKind.Ok,
+	value: element,
+});
 
-	return 1;
+const isTextResult = (_: Result<string, BodyElement>): boolean =>
+	_.kind === ResultKind.Ok && _.value.kind === ElementKind.Text;
+
+const splitTextElementAndInsertBetweenParts = (
+	textElement: Text,
+	elementToInsert: BodyElement,
+	fraction = 1 / 2,
+): [Text, BodyElement, Text] => {
+	// uses a clone of the original doc to avoid mutations
+	const childNodeArray = Array.from(textElement.doc.cloneNode(true).childNodes);
+	const splitPoint = childNodeArray.length * fraction;
+
+	const newDocs: [DocumentFragment, DocumentFragment] = [
+		JSDOM.fragment(''),
+		JSDOM.fragment(''),
+	];
+
+	childNodeArray.slice(0, Math.floor(splitPoint)).forEach((element) => {
+		newDocs[0].appendChild(element);
+	});
+
+	childNodeArray.slice(Math.floor(splitPoint)).forEach((element) => {
+		newDocs[1].appendChild(element);
+	});
+
+	return [
+		{
+			kind: ElementKind.Text,
+			doc: newDocs[0],
+		},
+		elementToInsert,
+		{
+			kind: ElementKind.Text,
+			doc: newDocs[1],
+		},
+	];
 };
 
 const tryToinsertNewsletterIntoStandard = (
@@ -22,9 +59,12 @@ const tryToinsertNewsletterIntoStandard = (
 	newsletter: Newsletter,
 	internalShortId: Option<string>,
 ): Array<Result<string, BodyElement>> => {
-	const insertPosition = findInsertPosition(body);
+	const textElementResults = body.filter(isTextResult) as {
+		kind: ResultKind.Ok;
+		value: Text;
+	}[];
 
-	if (insertPosition === null) {
+	if (textElementResults.length === 0) {
 		logger.warn(
 			`Unable to find suitable place for NewsletterSignupBlockElement`,
 			internalShortId,
@@ -32,18 +72,22 @@ const tryToinsertNewsletterIntoStandard = (
 		return body;
 	}
 
-	const resultToInsert: Result<string, BodyElement> = {
-		kind: ResultKind.Ok,
-		value: {
-			kind: ElementKind.NewsletterSignUp,
-			...newsletter,
-		},
+	const indexOfFirstTextResult = body.findIndex(isTextResult);
+
+	const newsletterElement: NewsletterSignUp = {
+		kind: ElementKind.NewsletterSignUp,
+		...newsletter,
 	};
+	const textWithNewsletter = splitTextElementAndInsertBetweenParts(
+		textElementResults[0].value,
+		newsletterElement,
+		6 / 10,
+	);
 
 	return [
-		...body.slice(0, insertPosition),
-		resultToInsert,
-		...body.slice(insertPosition),
+		...body.slice(0, indexOfFirstTextResult),
+		...textWithNewsletter.map(wrapInResult),
+		...body.slice(indexOfFirstTextResult),
 	];
 };
 
