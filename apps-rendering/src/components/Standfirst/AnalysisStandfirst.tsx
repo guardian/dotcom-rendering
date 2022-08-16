a8be91d3798e64e865ce424862ca2b008095abf0
Anna Beddow <anna.beddow@gmail.com>
2022-08-16 14:43:30 +0100

    Add analysis standfirst variant

---
+++ apps-rendering/src/components/Standfirst/AnalysisStandfirst.tsx
@@ -0,0 +1,27 @@
+import { css } from '@emotion/react';
+import { headline } from '@guardian/source-foundations';
+import type { Item } from 'item';
+import { getFormat } from 'item';
+import DefaultStandfirst, { defaultStyles } from './Standfirst.defaults';
+
+interface Props {
+	item: Item;
+}
+
+const AnalysisStandfirst: React.FC<Props> = ({ item }) => (
+	<>
+		<p
+			css={css`
+				${headline.xxxsmall({ fontWeight: 'bold' })}
+			`}
+		>
+			Analysis
+		</p>
+		<DefaultStandfirst
+			item={item}
+			css={css(defaultStyles(getFormat(item)))}
+		/>
+	</>
+);
+
+export default AnalysisStandfirst;
