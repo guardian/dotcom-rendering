64590c43b91dc315d2339b259fccbcde01ec0c11
David Blatcher <dv.bltchr@gmail.com>
2022-08-16 15:22:50 +0100

    import order lint

--- apps-rendering/src/components/NewsletterSignup/index.tsx
+++ apps-rendering/src/components/NewsletterSignup/index.tsx
@@ -15,10 +15,10 @@ import {
 } from '@guardian/source-foundations';
 import type { NewsletterSignUp } from 'bodyElement';
 import type { FC } from 'react';
+import { darkModeCss } from 'styles';
 import EmailSignupForm from './EmailSignupForm';
 import PrivacyWording from './PrivacyWording';
 import SvgNewsletter from './SvgNewsletter';
-import { darkModeCss } from 'styles';
 
 // ----- Component ----- //
 
