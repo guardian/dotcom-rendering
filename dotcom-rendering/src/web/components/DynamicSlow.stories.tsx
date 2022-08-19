64153a641d71a1220390ae5d66e09712245abea0
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-08-19 08:13:18 +0100

    chore: Created a story showing avatars in different contexts

--- dotcom-rendering/src/web/components/DynamicSlow.stories.tsx
+++ dotcom-rendering/src/web/components/DynamicSlow.stories.tsx
@@ -1,3 +1,4 @@
+import { ArticleDesign, ArticleDisplay, ArticlePillar } from '@guardian/libs';
 import { breakpoints } from '@guardian/source-foundations';
 import { trails } from '../../../fixtures/manual/trails';
 import { DynamicSlow } from './DynamicSlow';
@@ -6,20 +7,6 @@ import { Section } from './Section';
 export default {
 	component: DynamicSlow,
 	title: 'Components/DynamicSlow',
-	parameters: {
-		chromatic: {
-			viewports: [
-				breakpoints.mobile,
-				breakpoints.mobileMedium,
-				breakpoints.mobileLandscape,
-				breakpoints.phablet,
-				breakpoints.tablet,
-				breakpoints.desktop,
-				breakpoints.leftCol,
-				breakpoints.wide,
-			],
-		},
-	},
 };
 
 export const Default = () => (
@@ -27,4 +14,39 @@ export const Default = () => (
 		<DynamicSlow trails={trails} showAge={true} />
 	</Section>
 );
-Default.story = { name: 'DynamicSlow' };
+Default.story = {
+	name: 'Default',
+	chromatic: {
+		viewports: [
+			breakpoints.mobile,
+			breakpoints.mobileMedium,
+			breakpoints.mobileLandscape,
+			breakpoints.phablet,
+			breakpoints.tablet,
+			breakpoints.desktop,
+			breakpoints.leftCol,
+			breakpoints.wide,
+		],
+	},
+};
+
+export const Avatar = () => {
+	const avatarTrails = trails.map((trail) => {
+		return {
+			...trail,
+			avatarUrl:
+				'https://i.guim.co.uk/img/uploads/2017/10/06/George-Monbiot,-L.png?width=173&quality=85&auto=format&fit=max&s=be5b0d3f3aa55682e4930057fc3929a3',
+			format: {
+				display: ArticleDisplay.Standard,
+				design: ArticleDesign.Comment,
+				theme: ArticlePillar.Opinion,
+			},
+		};
+	});
+	return (
+		<Section title="DynamicSlow" padContent={false} centralBorder="partial">
+			<DynamicSlow trails={avatarTrails} showAge={true} />
+		</Section>
+	);
+};
+Avatar.story = { name: 'with avatars' };
