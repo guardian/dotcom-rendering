caa050a24abedd135e8d92f4c6f4ba20e02c84c8
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-08-22 08:17:13 +0100

    refactor: `DesignTag` to make its own decisions based on format

--- dotcom-rendering/src/web/components/DesignTag.tsx
+++ dotcom-rendering/src/web/components/DesignTag.tsx
@@ -1,30 +1,30 @@
 import { css } from '@emotion/react';
+import { ArticleDesign } from '@guardian/libs';
 import { headline, space } from '@guardian/source-foundations';
+import type { Palette } from '../../types/palette';
 import { decidePalette } from '../lib/decidePalette';
 
-export const DesignTag = ({
-	children,
-	format,
-}: {
-	children: React.ReactNode;
-	format: ArticleFormat;
-}) => {
+const tagStyles = (palette: Palette) => css`
+	background-color: ${palette.background.designTag};
+	color: ${palette.text.designTag};
+	display: inline-block;
+	padding-left: ${space[2]}px;
+	padding-right: ${space[2]}px;
+	padding-bottom: ${space[1]}px;
+	margin-top: ${space[2]}px;
+	margin-bottom: ${space[1]}px;
+	${headline.xxsmall({ fontWeight: 'bold' })}
+`;
+
+export const DesignTag = ({ format }: { format: ArticleFormat }) => {
 	const palette = decidePalette(format);
-	return (
-		<div
-			css={css`
-				background-color: ${palette.background.designTag};
-				color: ${palette.text.designTag};
-				display: inline-block;
-				padding-left: ${space[2]}px;
-				padding-right: ${space[2]}px;
-				padding-bottom: ${space[1]}px;
-				margin-top: ${space[2]}px;
-				margin-bottom: ${space[1]}px;
-				${headline.xxsmall({ fontWeight: 'bold' })}
-			`}
-		>
-			{children}
-		</div>
-	);
+
+	switch (format.design) {
+		case ArticleDesign.Analysis:
+			return <div css={tagStyles(palette)}>Analysis</div>;
+		case ArticleDesign.Explainer:
+			return <div css={tagStyles(palette)}>Explainer</div>;
+		default:
+			return null;
+	}
 };
