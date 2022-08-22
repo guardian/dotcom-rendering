d56867c9173519556693c6a5ac344a6f7bfe614c
Anna Beddow <anna.beddow@gmail.com>
2022-08-22 09:58:50 +0100

    Feat: use sluggified unique text as id

--- dotcom-rendering/src/model/enhance-H2s.ts
+++ dotcom-rendering/src/model/enhance-H2s.ts
@@ -1,4 +1,32 @@
+import { JSDOM } from 'jsdom';
+
+const extractText = (element: SubheadingBlockElement): string => {
+	const frag = JSDOM.fragment(element.html);
+	if (!frag.firstElementChild) return '';
+	return frag.textContent?.trim() ?? '';
+};
+
+export const validateSlug = (slug: string, array: string[]) => {
+	if (array.includes(slug)) {
+		const occurenceCount = array.filter((currentItem) => currentItem == slug).length;
+		return `${slug}-${occurenceCount}`;
+	}
+	return slug;
+};
+
+const slugify = (text: string) => {
+	return text
+		.toString() // Cast to string (optional)
+		.normalize('NFKD') // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+		.toLowerCase() // Convert the string to lowercase letters
+		.trim() // Remove whitespace from both sides of a string (optional)
+		.replace(/\s+/g, '-') // Replace spaces with -
+		.replace(/[^\w\-]+/g, '') // Remove all non-word chars
+		.replace(/\-\-+/g, '-'); // Replace multiple - with single -
+};
+
 const enhance = (elements: CAPIElement[]): CAPIElement[] => {
+	const slugifiedIds: string[] = []
 	const enhanced: CAPIElement[] = [];
 	elements.forEach((element) => {
 		// If this element is a subheading, insert the element ID as its ID
@@ -6,10 +34,14 @@ const enhance = (elements: CAPIElement[]): CAPIElement[] => {
 			element._type ===
 			'model.dotcomrendering.pageElements.SubheadingBlockElement'
 		) {
+			const text = extractText(element);
+			const slug = text ? slugify(text) : element.elementId;
+			const validatedSlug = validateSlug(slug, slugifiedIds);
+			slugifiedIds.push(slug)
 			const withId = element.html.replace(
 				'<h2>',
 				// add ID to H2 element
-				`<h2 id='${element.elementId}'>`,
+				`<h2 id='${validatedSlug}'>`,
 			);
 			enhanced.push({
 				...element,
