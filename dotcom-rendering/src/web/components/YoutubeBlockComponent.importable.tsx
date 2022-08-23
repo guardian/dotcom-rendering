f197d2f3eaf1df7be05f93f58585a3b33ca0034f
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-08-23 17:03:40 +0100

    remove log

--- dotcom-rendering/src/web/components/YoutubeBlockComponent.importable.tsx
+++ dotcom-rendering/src/web/components/YoutubeBlockComponent.importable.tsx
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 import { YoutubeAtom } from '@guardian/atoms-rendering';
 import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
-import { log } from '@guardian/libs';
 import { body, neutral, space } from '@guardian/source-foundations';
 import { SvgAlertRound } from '@guardian/source-react-components';
 import { useEffect, useState } from 'react';
@@ -100,7 +99,6 @@ export const YoutubeBlockComponent = ({
 		'IntegrateIMA',
 		'variant',
 	);
-	log('commercial', { userInImaTestVariant });
 	const imaAdTagUrl =
 		'https://pubads.g.doubleclick.net/gampad/live/ads?iu=/59666047/theguardian.com&description_url=[placeholder]&tfcd=0&npa=0&sz=400x300&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=&vad_type=linear&cust_params=at%3Dfixed-puppies';
 
