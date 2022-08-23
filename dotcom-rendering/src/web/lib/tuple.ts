b9121a762e85a2aeed8d12de9746c182e1c0ddb9
Max Duval <max.duval@theguardian.com>
2022-08-23 10:06:01 +0100

    feat: create tuple type-guards
    These type-guards can prevent unchecked indexed accesses

---
+++ dotcom-rendering/src/web/lib/tuple.ts
@@ -0,0 +1,49 @@
+/**
+ * Type-guard that check whether an array contains at least a single item.
+ *
+ * If `true`, the type is narrowed from T[] to the [T, ...T[]] tuple,
+ * which safely allows accessing the first item.
+ */
+export const nonEmpty = <T>(arr: T[]): arr is [T, ...T[]] => arr.length >= 1;
+
+/** A tuple of up to 12 items. Larger tuples will not be narrowed */
+type Tuple<T, N extends number> = N extends 12
+	? [T, T, T, T, T, T, T, T, T, T, T, T]
+	: N extends 11
+	? [T, T, T, T, T, T, T, T, T, T, T]
+	: N extends 10
+	? [T, T, T, T, T, T, T, T, T, T]
+	: N extends 9
+	? [T, T, T, T, T, T, T, T, T]
+	: N extends 8
+	? [T, T, T, T, T, T, T, T]
+	: N extends 7
+	? [T, T, T, T, T, T, T]
+	: N extends 6
+	? [T, T, T, T, T, T]
+	: N extends 5
+	? [T, T, T, T, T]
+	: N extends 4
+	? [T, T, T, T]
+	: N extends 3
+	? [T, T, T]
+	: N extends 2
+	? [T, T]
+	: N extends 1
+	? [T]
+	: N extends 0
+	? []
+	: T[];
+
+/**
+ * Type-guard for whether an array is a tuple of exact length.
+ *
+ * Only tuples of 12 elements or less will be narrowed.
+ */
+export const isTuple = <
+	T,
+	N extends 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12,
+>(
+	arr: T[],
+	count: N,
+): arr is Tuple<T, N> => arr.length === count;
