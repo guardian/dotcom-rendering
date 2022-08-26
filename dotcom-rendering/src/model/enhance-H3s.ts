ad98637cd0d04863603746f183940ffc1e7ff2f5
Alina Boghiu <alina_boghiu@31517.gnm.int>
2022-08-26 12:37:53 +0100

    type methods to avoid type checks and use map instead of foreach

--- dotcom-rendering/src/model/enhance-H3s.ts
+++ dotcom-rendering/src/model/enhance-H3s.ts
@@ -1,15 +1,17 @@
 import { JSDOM } from 'jsdom';
 
-const isFalseH3 = (element: CAPIElement): boolean => {
-	if (!element) return false;
-	// Checks if this element is a 'false h3' based on the convention: <p><strong><H3 text</strong></p>
-	if (
-		element._type !== 'model.dotcomrendering.pageElements.TextBlockElement'
-	) {
-		return false;
-	}
+/**
+ * Checks if this element is a 'false h3' based on the convention: <p><strong>H3 text</strong></p>
+ */
+
+const isTextBlockElement = (
+	element: CAPIElement,
+): element is TextBlockElement =>
+	element._type === 'model.dotcomrendering.pageElements.TextBlockElement';
+
+const isFalseH3 = (element: TextBlockElement): element is FalseH3 => {
 	const frag = JSDOM.fragment(element.html);
-	if (!frag || !frag.firstElementChild) return false;
+	if (!frag.firstElementChild) return false;
 	const html = frag.firstElementChild.outerHTML;
 	// The following things must be true for an element to be a faux H3
 	const hasPwrapper = frag.firstElementChild.nodeName === 'P';
@@ -29,47 +31,38 @@ const isFalseH3 = (element: CAPIElement): boolean => {
 	);
 };
 
-const extractH3 = (element: CAPIElement): string => {
-	// Extract the text based on the convention: <p><strong><H3 text</strong></p>
-	const textElement = element as TextBlockElement;
-	const frag = JSDOM.fragment(textElement.html);
-	if (isFalseH3(element)) {
-		return (
-			frag.firstElementChild?.innerHTML
-				.replaceAll('<strong>', '')
-				.replaceAll('</strong>', '') ?? ''
-		);
-	}
-	return '';
+interface FalseH3 extends TextBlockElement {
+	html: `<p><strong>${string}</strong></p>`;
+}
+
+const extractH3 = (element: FalseH3): string => {
+	const frag = JSDOM.fragment(element.html);
+	return (
+		frag.firstElementChild?.innerHTML
+			.replaceAll('<strong>', '')
+			.replaceAll('</strong>', '') ?? ''
+	);
 };
 
-const enhance = (elements: CAPIElement[]): CAPIElement[] => {
-	/**
-	 * Why not just add H3s in Composer?
-	 * Truth is, you can't. So to get around this there's a convention that says if
-	 * you insert <p><strong>Faux H3!</strong>,</p> then we replace it with a h3 tag
-	 * instead.
-	 */
-	const withH3s: CAPIElement[] = [];
-	elements.forEach((thisElement) => {
-		if (
-			thisElement._type ===
-				'model.dotcomrendering.pageElements.TextBlockElement' &&
-			isFalseH3(thisElement)
-		) {
+/**
+ * Why not just add H3s in Composer?
+ * Truth is, you can't. So to get around this there's a convention that says if
+ * you insert <p><strong>Faux H3!</strong>,</p> then we replace it with a h3 tag
+ * instead.
+ */
+const enhance = (elements: CAPIElement[]): CAPIElement[] =>
+	elements.map((thisElement) => {
+		if (isTextBlockElement(thisElement) && isFalseH3(thisElement)) {
 			const h3Text = extractH3(thisElement);
 
-			withH3s.push({
+			return {
 				...thisElement,
 				html: `<h3>${h3Text}</h3>`,
-			});
+			};
 		} else {
-			// Pass through
-			withH3s.push(thisElement);
+			return thisElement;
 		}
 	});
-	return withH3s;
-};
 
 export const enhanceH3s = (blocks: Block[], format: CAPIFormat): Block[] => {
 	const hasChapteredUI =
