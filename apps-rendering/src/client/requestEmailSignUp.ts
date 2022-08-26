2d0c381c4459a41d15e58183912e12ed5534b02c
David Blatcher <dv.bltchr@gmail.com>
2022-08-26 11:20:21 +0100

    update comment on fak request function

--- apps-rendering/src/client/requestEmailSignUp.ts
+++ apps-rendering/src/client/requestEmailSignUp.ts
@@ -1,9 +1,20 @@
-// TO DO - add the domain for the sign-up endpoint to the content security policy
-// 'connect-src' attribute.
-//apps-rendering/src/server/csp.ts
+
+/**
+ * Simulate an async request to an email sign-up service.
+ *
+ * TO DO - replace this function with an actual implementation.
+ * This is expected to be implemented using a new Bridget function
+ * accessed through the native API, but potentially could also involve
+ * the weblayer directly fetching a response from Consent(the identity
+ * API).
+ *
+ * @param emailAddress an unverified email address
+ * @param newsletterIdentityName the identityName of a newsletter
+ * @returns an object with a status code (500 if the email address has no "." character, otherwise 200) and message string.
+ */
 export async function fakeRequestToEmailSignupService(
 	emailAddress: string,
-	newsletterId: string,
+	newsletterIdentityName: string,
 ): Promise<{ status: number; message?: string }> {
 	await new Promise((r) => {
 		setTimeout(r, 1000);
@@ -16,5 +27,5 @@ export async function fakeRequestToEmailSignupService(
 		};
 	}
 
-	return { status: 200, message: `fake signed up to ${newsletterId}` };
+	return { status: 200, message: `fake signed up to ${newsletterIdentityName}` };
 }
