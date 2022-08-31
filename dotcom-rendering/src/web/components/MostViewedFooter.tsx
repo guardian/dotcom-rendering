1c96fe1286e6693f6f9a9c5b67a2e8a9f9b2d302
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-08-31 11:35:26 +0100

    refactor: Extract and create `MostViewedFooter`

---
+++ dotcom-rendering/src/web/components/MostViewedFooter.tsx
@@ -0,0 +1,89 @@
+import { css } from '@emotion/react';
+import type { Breakpoint } from '@guardian/source-foundations';
+import { border, from } from '@guardian/source-foundations';
+import type { Palette } from '../../types/palette';
+import type {
+	CAPITrailTabType,
+	CAPITrailType,
+	TrailTabType,
+} from '../../types/trails';
+import { decideTrail } from '../lib/decideTrail';
+import { MostViewedFooterGrid } from './MostViewedFooterGrid';
+import { MostViewedFooterSecondTierItem } from './MostViewedFooterSecondTierItem';
+
+type Props = {
+	tabs: CAPITrailTabType[];
+	mostCommented?: CAPITrailType;
+	mostShared?: CAPITrailType;
+	abTestCypressDataAttr: string;
+	variantFromRunnable: string;
+	sectionName?: string;
+	palette: Palette;
+};
+
+const stackBelow = (breakpoint: Breakpoint) => css`
+	display: flex;
+	flex-direction: column;
+
+	${from[breakpoint]} {
+		flex-direction: row;
+	}
+`;
+
+const secondTierStyles = css`
+	border-left: 1px solid ${border.secondary};
+	border-right: 1px solid ${border.secondary};
+
+	${from.tablet} {
+		padding-top: 24px;
+	}
+`;
+
+function transformTabs(tabs: CAPITrailTabType[]): TrailTabType[] {
+	return tabs.map((tab) => ({
+		...tab,
+		trails: tab.trails.map((trail) => decideTrail(trail)),
+	}));
+}
+
+export const MostViewedFooter = ({
+	tabs,
+	mostCommented,
+	mostShared,
+	abTestCypressDataAttr,
+	variantFromRunnable,
+	sectionName,
+	palette,
+}: Props) => {
+	return (
+		<div
+			css={css`
+				width: 100%;
+			`}
+			data-cy="mostviewed-footer"
+			data-cy-ab-user-in-variant={abTestCypressDataAttr}
+			data-cy-ab-runnable-test={variantFromRunnable}
+		>
+			<MostViewedFooterGrid
+				data={transformTabs(tabs)}
+				sectionName={sectionName}
+				palette={palette}
+			/>
+			<div css={[stackBelow('tablet'), secondTierStyles]}>
+				{mostCommented && (
+					<MostViewedFooterSecondTierItem
+						trail={decideTrail(mostCommented)}
+						title="Most commented"
+						showRightBorder={true}
+					/>
+				)}
+				{mostShared && (
+					<MostViewedFooterSecondTierItem
+						trail={decideTrail(mostShared)}
+						title="Most shared"
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
--- dotcom-rendering/src/web/components/MostViewedFooterData.importable.tsx
+++ dotcom-rendering/src/web/components/MostViewedFooterData.importable.tsx
@@ -1,19 +1,10 @@
-import { css } from '@emotion/react';
-import type { Breakpoint } from '@guardian/source-foundations';
-import { border, from } from '@guardian/source-foundations';
 import { joinUrl } from '../../lib/joinUrl';
 import type { Palette } from '../../types/palette';
-import type {
-	CAPITrailTabType,
-	CAPITrailType,
-	TrailTabType,
-} from '../../types/trails';
+import type { CAPITrailTabType, CAPITrailType } from '../../types/trails';
 import { abTestTest } from '../experiments/tests/ab-test-test';
-import { decideTrail } from '../lib/decideTrail';
 import { useAB } from '../lib/useAB';
 import { useApi } from '../lib/useApi';
-import { MostViewedFooterGrid } from './MostViewedFooterGrid';
-import { MostViewedFooterSecondTierItem } from './MostViewedFooterSecondTierItem';
+import { MostViewedFooter } from './MostViewedFooter';
 
 interface Props {
 	sectionName?: string;
@@ -21,37 +12,12 @@ interface Props {
 	ajaxUrl: string;
 }
 
-const stackBelow = (breakpoint: Breakpoint) => css`
-	display: flex;
-	flex-direction: column;
-
-	${from[breakpoint]} {
-		flex-direction: row;
-	}
-`;
-
-const secondTierStyles = css`
-	border-left: 1px solid ${border.secondary};
-	border-right: 1px solid ${border.secondary};
-
-	${from.tablet} {
-		padding-top: 24px;
-	}
-`;
-
 function buildSectionUrl(ajaxUrl: string, sectionName?: string) {
 	const sectionsWithoutPopular = ['info', 'global'];
 	const hasSection =
 		sectionName && !sectionsWithoutPopular.includes(sectionName);
 	const endpoint = `/most-read${hasSection ? `/${sectionName}` : ''}.json`;
 	return joinUrl([ajaxUrl, `${endpoint}?dcr=true`]);
-}
-
-function transformTabs(tabs: CAPITrailTabType[]): TrailTabType[] {
-	return tabs.map((tab) => ({
-		...tab,
-		trails: tab.trails.map((trail) => decideTrail(trail)),
-	}));
 }
 
 interface MostViewedFooterPayloadType {
@@ -76,8 +42,7 @@ export const MostViewedFooterData = ({
 			'ab-test-variant') ||
 		'ab-test-not-in-test';
 	const runnableTest = ABTestAPI?.runnableTest(abTestTest);
-	const variantFromRunnable =
-		(runnableTest && runnableTest.variantToRun.id) || 'not-runnable';
+	const variantFromRunnable = runnableTest?.variantToRun.id || 'not-runnable';
 
 	const url = buildSectionUrl(ajaxUrl, sectionName);
 	const { data, error } = useApi<
@@ -92,35 +57,17 @@ export const MostViewedFooterData = ({
 	if (data) {
 		const tabs = 'tabs' in data ? data.tabs : data;
 		return (
-			<div
-				css={css`
-					width: 100%;
-				`}
-				data-cy="mostviewed-footer"
-				data-cy-ab-user-in-variant={abTestCypressDataAttr}
-				data-cy-ab-runnable-test={variantFromRunnable}
-			>
-				<MostViewedFooterGrid
-					data={transformTabs(tabs)}
-					sectionName={sectionName}
-					palette={palette}
-				/>
-				<div css={[stackBelow('tablet'), secondTierStyles]}>
-					{'mostCommented' in data && (
-						<MostViewedFooterSecondTierItem
-							trail={decideTrail(data.mostCommented)}
-							title="Most commented"
-							showRightBorder={true}
-						/>
-					)}
-					{'mostShared' in data && (
-						<MostViewedFooterSecondTierItem
-							trail={decideTrail(data.mostShared)}
-							title="Most shared"
-						/>
-					)}
-				</div>
-			</div>
+			<MostViewedFooter
+				tabs={tabs}
+				mostCommented={
+					'mostCommented' in data ? data.mostCommented : undefined
+				}
+				mostShared={'mostShared' in data ? data.mostShared : undefined}
+				abTestCypressDataAttr={abTestCypressDataAttr}
+				variantFromRunnable={variantFromRunnable}
+				sectionName={sectionName}
+				palette={palette}
+			/>
 		);
 	}
 
