8db1a200309cb1e945abbe7f3024a96c44b8f98d
David Blatcher <dv.bltchr@gmail.com>
2022-09-12 18:01:43 +0100

    first version of insertion logic for article model

--- apps-rendering/src/server/insertNewsletterIntoItem.ts
+++ apps-rendering/src/server/insertNewsletterIntoItem.ts
@@ -3,11 +3,14 @@
 import type { Newsletter } from '@guardian/apps-rendering-api-models/newsletter';
 import { ArticleDesign } from '@guardian/libs';
 import { OptionKind } from '@guardian/types';
-import type { Body, NewsletterSignUp } from 'bodyElement';
+import type { Body, BodyElement, NewsletterSignUp } from 'bodyElement';
 import { ElementKind } from 'bodyElementKind';
 import type { Item } from 'item';
 import { Result } from 'result';
 import { stringToPillar } from 'themeStyles';
+
+// ----- type ----- //
+type BodyResult = Result<string, BodyElement>;
 
 // ----- Constants ----- //
 
@@ -20,6 +23,41 @@ const TEST_NEWSLETTER: Newsletter = {
 	theme: 'opinion',
 	successDescription: 'signed up',
 };
+
+// ----- pure functions ---//
+
+function isNonEmptyText(result: BodyResult): boolean {
+	if (result.isOk() && result.value.kind === ElementKind.Text) {
+		const { textContent } = result.value.doc;
+		return textContent ? textContent.trim().length > 0 : false;
+	}
+	return false;
+}
+
+function isWhiteSpace(result: BodyResult): boolean {
+	if (result.isOk() && result.value.kind === ElementKind.Text) {
+		const { textContent } = result.value.doc;
+		return textContent ? textContent.trim().length === 0 : false;
+	}
+	return false;
+}
+
+function describeResult(result: BodyResult): string {
+	if (isNonEmptyText(result)) {
+		const node =
+			result.isOk() && result.value.kind === ElementKind.Text
+				? result.value.doc
+				: undefined;
+		return `TEXT, [${node}] "${node?.textContent}"`;
+	}
+	if (isWhiteSpace(result)) {
+		return '-WHITE SPACE-';
+	}
+	if (result.isOk()) {
+		return `[KIND:${result.value.kind}]`;
+	}
+	return 'error';
+}
 
 // ----- Procedures ----- //
 
@@ -40,11 +78,95 @@ function buildBodyElement(item: Item): NewsletterSignUp | undefined {
 	};
 }
 
+/**
+ * 	Desired Rules:
+ *   - Hide embeds in articles that are less than 300 words (DO SERVER SIDE!)
+ *	 - Prevent sign up form from appearing straight after bold text
+ *	 - Prevent sign up from from appearing under headings
+ *	 - Prevent embeds from rendering in heavily formatted articles
+ *	 - Must have plain body text above and below
+ * @param body
+ * @returns the index to insert at
+ */
+function findInsertIndex(body: Body): number {
+	// Item.Body can contain 'Error' Results as well as 'ok' Results
+	// these are not rendered, but cannot be excluded from the array
+	// so need to be ignored for the purpose of choosing an index.
+
+	// Item.Body models white space between HTML elements as Text BodyElements
+	// These need to be ignore for the purpose of the placemenr rules
+	// IE this is not a valid placement - the SignUp appears direcly below the
+	// Image even though there is a "Text" between them.
+	//
+	// [Text]
+	// [Text]
+	// [Image]
+	// [Text](whitespace)
+	// <-------------------- [SignUp]
+	// [Text]
+
+	// note - the min and max should ignore whitespace
+	// and error, but do not.
+	const minIndex = 8;
+	const maxIndex = body.length - 8;
+
+	function findPreviousOkThatIsNotWhiteSpace(
+		index: number,
+	): BodyResult | undefined {
+		return body
+			.slice(0, index)
+			.reverse()
+			.find((result) => result.isOk() && !isWhiteSpace(result));
+	}
+
+	function findNextOkThatIsNotWhiteSpace(
+		index: number,
+	): BodyResult | undefined {
+		return body
+			.slice(index)
+			.find((result) => result.isOk() && !isWhiteSpace(result));
+	}
+
+	function isASuitableIndex(index: number): boolean {
+		if (index > maxIndex || index < minIndex) {
+			return false;
+		}
+		const contentBefore = findPreviousOkThatIsNotWhiteSpace(index);
+		const contentAfter = findNextOkThatIsNotWhiteSpace(index);
+		if (!contentBefore || !contentAfter) {
+			return false;
+		}
+		return isNonEmptyText(contentAfter) && isNonEmptyText(contentBefore);
+	}
+
+	const suitabilityMap = body.map((_, index) => isASuitableIndex(index));
+
+	body.forEach((result, index) => {
+		console.log(
+			`[${index}]`,
+			isASuitableIndex(index),
+			describeResult(result),
+		);
+	});
+
+	const bestIndex = suitabilityMap.lastIndexOf(true);
+	console.log({ bestIndex });
+	return bestIndex;
+}
+
 function insertNewsletterIntoBody(
 	item: Item & { body: Body },
 	newsletterSignUp: NewsletterSignUp,
 ): Item {
-	item.body.unshift(Result.ok(newsletterSignUp));
+	const insertIndex = findInsertIndex(item.body);
+	if (insertIndex === -1) {
+		return item;
+	}
+	item.body = [
+		...item.body.slice(0, insertIndex),
+		Result.ok(newsletterSignUp) as BodyResult,
+		...item.body.slice(insertIndex),
+	];
 	return item;
 }
 
