eeaad8db682100754b92e2bb01dc8d22d2ec1cc9
Chris Jones <chrislomaxjones96@gmail.com>
2022-09-13 13:12:45 +0100

    Add new logic for Permutive

--- dotcom-rendering/src/amp/components/Permutive.tsx
+++ dotcom-rendering/src/amp/components/Permutive.tsx
@@ -7,5 +7,70 @@ export interface PermutiveModel {
 }
 
 export const Permutive = ({ apiKey, projectId, payload }: PermutiveModel) => {
-	return null;
+	const permutiveConfig = `
+		<script type="application/json">
+			{
+				"apiKey": "${apiKey}",
+				"projectId": "${projectId}",
+				"environment": "production"
+			}
+		</script>`;
+
+	const permutiveAmpScript = `
+		<script type="application/json">
+			{
+				"extraUrlParams": ${JSON.stringify(payload)}
+			}
+		</script>`;
+
+	const permutiveCachedTargetingScript = `
+		exportFunction('ct', () => {let c = JSON.parse(localStorage.getItem('_pdfps'));let i = localStorage.getItem('permutive-id');return {targeting:{permutive: c ? c : [], puid: i}, ppid: i};})
+	`;
+
+	const pampScript = `
+		fetch('https://cdn.permutive.app/queries/${projectId}-amp.json').then(r => r.json()).then(d => AMP.setState({permutiveConfig: {ampJson: d}}));
+	`;
+
+	return (
+		<>
+			<amp-state
+				id="permutiveConfig"
+				dangerouslySetInnerHTML={{ __html: permutiveConfig }}
+			/>
+			<amp-analytics
+				type="permutive-ampscript"
+				dangerouslySetInnerHTML={{ __html: permutiveAmpScript }}
+			/>
+			<amp-script
+				id="permutiveCachedTargeting"
+				sandboxed=""
+				script="permutiveCachedTargetingScript"
+			></amp-script>
+			<script
+				id="permutiveCachedTargetingScript"
+				type="text/plain"
+				// @ts-expect-error -- Do as Permutive instructs
+				target="amp-script"
+				dangerouslySetInnerHTML={{
+					__html: permutiveCachedTargetingScript,
+				}}
+			></script>
+			<amp-script sandboxed="" script="pamp-json"></amp-script>
+			<script
+				id="pamp-json"
+				type="text/plain"
+				// @ts-expect-error -- Do as Permutive instructs
+				target="amp-script"
+				dangerouslySetInnerHTML={{
+					__html: pampScript,
+				}}
+			></script>
+			<amp-script
+				data-block-on-consent={true}
+				id="permutiveSdk"
+				sandboxed=""
+				src={`https://cdn.permutive.app/ampsdk/${projectId}-ampscript.js`}
+			></amp-script>
+		</>
+	);
 };
