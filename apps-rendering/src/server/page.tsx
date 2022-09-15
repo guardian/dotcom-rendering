73a2b8d7ad6f1a5e9c54b0d5b53837b4eedef990
David Blatcher <dv.bltchr@gmail.com>
2022-09-15 13:32:17 +0100

    remove call to insertNewsletter function in Page module

--- apps-rendering/src/server/page.tsx
+++ apps-rendering/src/server/page.tsx
@@ -23,7 +23,6 @@ import type { ReactElement } from 'react';
 import { renderToString } from 'react-dom/server';
 import { csp } from 'server/csp';
 import { pageFonts } from 'styles';
-import { insertNewsletterIntoItem } from './insertNewsletterIntoItem';
 
 // ----- Types ----- //
 
@@ -170,7 +169,6 @@ function render(
 	const item = fromCapi({ docParser, salt: imageSalt })(request, page);
 	const clientScript = map(getAssetLocation)(scriptName(item));
 	const thirdPartyEmbeds = getThirdPartyEmbeds(request.content);
-	insertNewsletterIntoItem(item);
 	const body = renderBody(item, request);
 	const inlineStyles = requiresInlineStyles(request.content);
 	const head = renderHead(
