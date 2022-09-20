e565a8ef6d690089c791a3072fb44c5bc566f67a
George B <705427+georgeblahblah@users.noreply.github.com>
2022-09-20 10:48:59 +0100

    refactor(themeStyles): delete unused exports

--- apps-rendering/src/themeStyles.ts
+++ apps-rendering/src/themeStyles.ts
@@ -5,116 +5,8 @@
 
 import type { ArticleTheme } from '@guardian/libs';
 import { ArticlePillar, ArticleSpecial } from '@guardian/libs';
-import {
-	culture,
-	lifestyle,
-	news,
-	opinion,
-	specialReport,
-	sport,
-} from '@guardian/source-foundations';
 
 // ----- Types ----- //
-
-/**
- * @deprecated Use the `editorialPalette` module from `common-rendering` instead
- */
-interface ThemeStyles {
-	kicker: string;
-	inverted: string;
-	liveblogKicker: string;
-	liveblogBackground: string;
-	liveblogDarkBackground: string;
-	link: string;
-	cameraIcon: string;
-	cameraIconBackground: string;
-}
-
-type ThemeColours = {
-	[theme in ArticleTheme]: ThemeStyles;
-};
-
-/**
- * @deprecated Use the `editorialPalette` module from `common-rendering` instead
- */
-export const themeColours: ThemeColours = {
-	[ArticlePillar.News]: {
-		kicker: news[400],
-		inverted: news[500],
-		liveblogKicker: news[600],
-		liveblogBackground: news[200],
-		liveblogDarkBackground: news[100],
-		link: news[300],
-		cameraIcon: news[800],
-		cameraIconBackground: news[400],
-	},
-	[ArticlePillar.Opinion]: {
-		kicker: opinion[400],
-		inverted: opinion[500],
-		liveblogKicker: opinion[600],
-		liveblogBackground: opinion[200],
-		liveblogDarkBackground: opinion[100],
-		link: opinion[300],
-		cameraIcon: opinion[800],
-		cameraIconBackground: opinion[400],
-	},
-	[ArticlePillar.Sport]: {
-		kicker: sport[400],
-		inverted: sport[500],
-		liveblogKicker: sport[600],
-		liveblogBackground: sport[200],
-		liveblogDarkBackground: sport[100],
-		link: sport[300],
-		cameraIcon: sport[800],
-		cameraIconBackground: sport[400],
-	},
-	[ArticlePillar.Culture]: {
-		kicker: culture[400],
-		inverted: culture[500],
-		liveblogKicker: culture[600],
-		liveblogBackground: culture[200],
-		liveblogDarkBackground: culture[100],
-		link: culture[300],
-		cameraIcon: culture[800],
-		cameraIconBackground: culture[400],
-	},
-	[ArticlePillar.Lifestyle]: {
-		kicker: lifestyle[400],
-		inverted: lifestyle[500],
-		liveblogKicker: lifestyle[500],
-		liveblogBackground: lifestyle[200],
-		liveblogDarkBackground: lifestyle[100],
-		link: lifestyle[300],
-		cameraIcon: lifestyle[800],
-		cameraIconBackground: lifestyle[400],
-	},
-	[ArticleSpecial.SpecialReport]: {
-		kicker: specialReport[400],
-		inverted: specialReport[500],
-		liveblogKicker: specialReport[500],
-		liveblogBackground: specialReport[200],
-		liveblogDarkBackground: specialReport[100],
-		link: specialReport[300],
-		cameraIcon: specialReport[800],
-		cameraIconBackground: specialReport[400],
-	},
-	[ArticleSpecial.Labs]: {
-		kicker: specialReport[400],
-		inverted: specialReport[500],
-		liveblogKicker: specialReport[500],
-		liveblogBackground: specialReport[200],
-		liveblogDarkBackground: specialReport[100],
-		link: specialReport[300],
-		cameraIcon: specialReport[800],
-		cameraIconBackground: specialReport[400],
-	},
-};
-
-/**
- * @deprecated Use the `editorialPalette` module from `common-rendering` instead
- */
-const getThemeStyles = (theme: ArticleTheme): ThemeStyles =>
-	themeColours[theme];
 
 function themeFromString(theme: string | undefined): ArticlePillar {
 	switch (theme) {
@@ -176,10 +68,4 @@ const stringToPillar = (pillar: string): ArticlePillar => {
 
 // ----- Exports ----- //
 
-export {
-	getThemeStyles,
-	themeFromString,
-	themeToPillarString,
-	themeToPillar,
-	stringToPillar,
-};
+export { themeFromString, themeToPillarString, themeToPillar, stringToPillar };
