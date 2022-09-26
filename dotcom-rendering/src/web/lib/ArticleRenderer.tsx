a4b3a59ac4590debff379eeeb94940fd8ac3e229
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-09-26 16:45:30 +0100

    float container rather than slot for im

--- dotcom-rendering/src/web/lib/ArticleRenderer.tsx
+++ dotcom-rendering/src/web/lib/ArticleRenderer.tsx
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 import type { ArticleFormat } from '@guardian/libs';
 import { ArticleDesign } from '@guardian/libs';
-import { from } from '@guardian/source-foundations';
 import type { ServerSideTests, Switches } from '../../types/config';
 import {
 	adCollapseStyles,
@@ -24,27 +23,6 @@ const commercialPosition = css`
 const adStylesDynamic = css`
 	${adLabelStyles}
 	${adCollapseStyles}
-
-	.ad-slot--im {
-		float: left;
-		width: 130px;
-		${from.mobileLandscape} {
-			width: 220px;
-		}
-
-		&:not(.ad-slot--rendered) {
-			width: 0;
-			height: 0;
-		}
-
-		&.ad-slot--rendered {
-			margin: 5px 10px 6px 0;
-			${from.mobileLandscape} {
-				margin-bottom: 12px;
-				margin-right: 20px;
-			}
-		}
-	}
 `;
 
 export const ArticleRenderer: React.FC<{
