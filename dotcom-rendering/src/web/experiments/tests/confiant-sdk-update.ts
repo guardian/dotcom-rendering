c22c76958b746d3a5e37e748da02340e2a7fbd2d
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-10-04 17:31:49 +0100

    update test expiry

--- dotcom-rendering/src/web/experiments/tests/confiant-sdk-update.ts
+++ dotcom-rendering/src/web/experiments/tests/confiant-sdk-update.ts
@@ -3,7 +3,7 @@ import type { ABTest } from '@guardian/ab-core';
 export const confiantSDKUpdateTest: ABTest = {
 	id: 'ConfiantSDKUpdate',
 	start: '2022-10-04',
-	expiry: '2022-10-30',
+	expiry: '2022-10-28',
 	author: 'Jake Lee Kennedy',
 	description: 'Test the new Confiant SDK, to share with the Confiant team',
 	audience: 0,
