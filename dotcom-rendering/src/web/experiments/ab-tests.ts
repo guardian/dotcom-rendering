0b727bd06d6ec7733b14b626a9005f562a37f00f
vanessa <32312712+vlbee@users.noreply.github.com>
2022-10-05 12:14:45 +0100

    Add AB test to test twostage bucketing

--- dotcom-rendering/src/web/experiments/ab-tests.ts
+++ dotcom-rendering/src/web/experiments/ab-tests.ts
@@ -10,12 +10,7 @@ import {
 import { shadyPieClickThrough } from './tests/shady-pie-click-through';
 import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
-import {
-	signInGateMandatoryLongTestRunAunz,
-	signInGateMandatoryLongTestRunEu,
-	signInGateMandatoryLongTestRunNa,
-	signInGateMandatoryLongTestRunUk,
-} from './tests/sign-in-gate-mandatory-long-testrun';
+import { signInGateMandatoryLongBucketingTestRun } from './tests/sign-in-gate-mandatory-long-testrun';
 
 // keep in sync with ab-tests in frontend
 // https://github.com/guardian/frontend/tree/main/static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -27,10 +22,7 @@ export const tests: ABTest[] = [
 	newsletterMerchUnitLighthouseVariants,
 	consentlessAds,
 	integrateIMA,
-	signInGateMandatoryLongTestRunUk,
-	signInGateMandatoryLongTestRunAunz,
-	signInGateMandatoryLongTestRunEu,
-	signInGateMandatoryLongTestRunNa,
 	shadyPieClickThrough,
 	confiantSDKUpdateTest,
+	signInGateMandatoryLongBucketingTestRun,
 ];
