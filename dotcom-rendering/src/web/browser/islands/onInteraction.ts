2078571e523cfe5c0b60759593932104d870f769
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-10-10 11:57:59 +0100

    Update dotcom-rendering/src/web/browser/islands/onInteraction.ts
    Co-authored-by: Oliver Lloyd <oliverlloyd@users.noreply.github.com>

--- dotcom-rendering/src/web/browser/islands/onInteraction.ts
+++ dotcom-rendering/src/web/browser/islands/onInteraction.ts
@@ -1,6 +1,6 @@
 /**
- * Use this function to delay execution of something until an element is visible
- * in the viewport
+ * Use this function to delay execution of something until an element is interacted
+ * with
  *
  * @param element : The html element that we want to wait for an interaction on;
  * @param callback : This is fired when the element is clicked on
