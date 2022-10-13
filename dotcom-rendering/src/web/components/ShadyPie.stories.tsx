7f09ad544e573f20f8f0b2c2962b62e5b527a517
Emma Imber <emma-jo.imber@guardian.co.uk>
2022-10-13 10:46:37 +0100

    Remove labs variants of shady pie story

--- dotcom-rendering/src/web/components/ShadyPie.stories.tsx
+++ dotcom-rendering/src/web/components/ShadyPie.stories.tsx
@@ -1,12 +1,4 @@
-//import type { ArticleFormat } from '@guardian/libs';
-//import { ArticleDesign, ArticleDisplay, ArticlePillar } from '@guardian/libs';
 import { ShadyPie } from './ShadyPie';
-
-/*const labsFormat: ArticleFormat = {
-	theme: ArticlePillar.Lifestyle,
-	design: ArticleDesign.Standard,
-	display: ArticleDisplay.Standard,
-};*/
 
 export default {
 	component: ShadyPie,
@@ -23,27 +15,3 @@ export const Default = () => {
 Default.story = {
 	name: 'Default',
 };
-
-//commenting out these components as we update the logic around shady pie for the ab test
-/*export const UKLabs = () => {
-	return (
-		<div style={{ width: '300px' }}>
-			<ShadyPie format={labsFormat} editionId={'UK'} />
-		</div>
-	);
-};
-UKLabs.story = {
-	name: 'UK Labs',
-};
-
-export const USLabs = () => {
-	return (
-		<div style={{ width: '300px' }}>
-			<ShadyPie format={labsFormat} editionId={'US'} />
-		</div>
-	);
-};
-USLabs.story = {
-	name: 'US Labs',
-};
-*/
