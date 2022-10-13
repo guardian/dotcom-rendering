0e99f38c943fd0a00af9ed8413c6b0ecc0168b3d
Ashleigh Carr <ashcorr20@gmail.com>
2022-10-13 17:09:51 +0100

    refactors: Migrate generateImageURL to lib

--- dotcom-rendering/src/web/components/Picture.tsx
+++ dotcom-rendering/src/web/components/Picture.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import { ArticleDesign, ArticleDisplay } from '@guardian/libs';
 import { breakpoints } from '@guardian/source-foundations';
 import React from 'react';
+import { generateImageURL } from '../../lib/generateImageURL';
 
 /**
  * Working on this file? Checkout out 027-pictures.md & 029-signing-image-urls.md for background information & context
@@ -159,42 +160,6 @@ const decideImageWidths = ({
 				];
 		}
 	}
-};
-
-/**
- * Generates a URL for calling the Fastly Image Optimiser.
- *
- * @see https://developer.fastly.com/reference/io/
- * @see https://github.com/guardian/fastly-image-service/blob/main/fastly-io_guim_co_uk/src/main/resources/varnish/main.vcl
- *
- */
-const generateImageURL = ({
-	master,
-	imageWidth,
-	resolution,
-}: {
-	master: string;
-	imageWidth: number;
-	resolution: 'low' | 'high';
-}): string => {
-	const url = new URL(master);
-	const [service] = url.hostname.split('.');
-
-	const params = new URLSearchParams({
-		width: imageWidth.toString(),
-		// Why 45 and 85?
-		// This numbers have been picked in 2018 as the right
-		// balance between image fidelity and file size
-		// https://github.com/guardian/fastly-image-service/pull/35
-		...(resolution === 'high'
-			? { quality: '45', dpr: '2' }
-			: { quality: '85', dpr: '1' }),
-		s: 'none',
-	});
-
-	return `https://i.guim.co.uk/img/${service}${
-		url.pathname
-	}?${params.toString()}`;
 };
 
 const descendingByBreakpoint = (a: ImageWidthType, b: ImageWidthType) => {
