2673d4415fadbe821b2600a844ec2be463af24e7
Sophie <sophiemacmillan93@gmail.com>
2022-10-20 16:37:00 +0100

    Replace react-testing-library tests with storybook/chromatic

--- dotcom-rendering/src/web/components/TopicFilterBank.test.tsx
+++ dotcom-rendering/src/web/components/TopicFilterBank.test.tsx
@@ -1,10 +1,4 @@
-import { ArticleDesign, ArticleDisplay, ArticlePillar } from '@guardian/libs';
-import { render, screen } from '@testing-library/react';
-import { SingleKeyEvent } from '../../../fixtures/manual/live-blog-key-events';
-import {
-	hasRelevantTopics,
-	TopicFilterBank,
-} from './TopicFilterBank.importable';
+import { hasRelevantTopics } from './TopicFilterBank.importable';
 
 describe('hasRelevantTopics', () => {
 	describe('should be false', () => {
@@ -33,63 +27,5 @@ describe('hasRelevantTopics', () => {
 				]),
 			).toBe(true);
 		});
-	});
-});
-
-describe('TopicFilterBank', () => {
-	const renderTopicFilterBank = (
-		keyEvents: Block[],
-		availableTopics?: Topic[],
-	) => {
-		render(
-			<TopicFilterBank
-				availableTopics={availableTopics}
-				keyEvents={keyEvents}
-				selectedTopics={[]}
-				format={{
-					theme: ArticlePillar.Lifestyle,
-					design: ArticleDesign.Interactive,
-					display: ArticleDisplay.Immersive,
-				}}
-				filterKeyEvents={false}
-				id="key-events-carousel-desktop"
-			/>,
-		);
-	};
-
-	it('It renders a button for Key Events', () => {
-		renderTopicFilterBank(SingleKeyEvent, []);
-		expect(screen.getByText('Filters')).toBeInTheDocument();
-		expect(
-			screen.getByRole('button', { name: 'Activate Key events filter' }),
-		).toBeInTheDocument();
-	});
-
-	it('It should render a button for each topic with count greater than 2', () => {
-		renderTopicFilterBank(
-			[],
-			[
-				{ type: 'PERSON', value: 'Liz Truss', count: 3 },
-				{ type: 'PERSON', value: 'Iain Duncan Smith', count: 2 },
-			],
-		);
-
-		expect(screen.getByText('Filters')).toBeInTheDocument();
-		expect(
-			screen.getByRole('button', { name: 'Activate Liz Truss filter' }),
-		).toBeInTheDocument();
-		expect(
-			screen.queryByRole('button', {
-				name: 'Activate Iain Duncan Smith filter',
-			}),
-		).not.toBeInTheDocument();
-	});
-
-	it('It should not show filters when there are no relevant topics or key events', () => {
-		renderTopicFilterBank(
-			[],
-			[{ type: 'PERSON', value: 'Iain Duncan Smith', count: 2 }],
-		);
-		expect(screen.queryByText('Filters')).not.toBeInTheDocument();
 	});
 });
