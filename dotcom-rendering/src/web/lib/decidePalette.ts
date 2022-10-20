6035e3d41be6978178fcfa7d8a0506e859dd5939
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2022-10-20 12:36:06 +0100

    Update decidePalette.ts

--- dotcom-rendering/src/web/lib/decidePalette.ts
+++ dotcom-rendering/src/web/lib/decidePalette.ts
@@ -6,8 +6,10 @@ import {
 	ArticleSpecial,
 } from '@guardian/libs';
 import {
+	border,
 	brand,
 	brandAlt,
+	brandAltBackground,
 	culture,
 	labs,
 	lifestyle,
@@ -655,8 +657,6 @@ const textCardKicker = (format: ArticleFormat): string => {
 					return culture[500];
 				case ArticleSpecial.Labs:
 					return labs[400];
-				case ArticleSpecial.SpecialReportAlt:
-					return news[600];
 			}
 		default:
 			return pillarPalette[format.theme].main;
