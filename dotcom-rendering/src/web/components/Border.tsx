c11a75feaa4da961b25055410991d80696c5c355
Ioanna Kokkini <ioannakok@hotmail.com>
2022-10-25 18:40:55 +0100

    Add border style for SpecialReportAlt

--- dotcom-rendering/src/web/components/Border.tsx
+++ dotcom-rendering/src/web/components/Border.tsx
@@ -1,14 +1,26 @@
 import { css } from '@emotion/react';
+import { ArticleSpecial } from '@guardian/libs';
 import { from } from '@guardian/source-foundations';
 import { decidePalette } from '../lib/decidePalette';
 
-export const Border = ({ format }: { format: ArticleFormat }) => (
-	<div
-		css={css`
+const borderStyles = (format: ArticleFormat) => {
+	if (format.theme === ArticleSpecial.SpecialReportAlt) {
+		return css`
 			${from.leftCol} {
-				border-left: 1px solid ${decidePalette(format).border.article};
+				border-left: 1px solid rgba(60, 60, 60, 0.3);
 				height: 100%;
 			}
-		`}
-	/>
+		`;
+	}
+
+	return css`
+		${from.leftCol} {
+			border-left: 1px solid ${decidePalette(format).border.article};
+			height: 100%;
+		}
+	`;
+};
+
+export const Border = ({ format }: { format: ArticleFormat }) => (
+	<div css={borderStyles(format)} />
 );
