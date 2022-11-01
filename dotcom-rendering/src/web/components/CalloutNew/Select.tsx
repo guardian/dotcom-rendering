f8a1b6244e2dea22f111e4a82fcf61a8d54b2d07
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-11-01 12:27:27 +0000

    added select box

--- dotcom-rendering/src/web/components/CalloutNew/Select.tsx
+++ dotcom-rendering/src/web/components/CalloutNew/Select.tsx
@@ -1,4 +1,11 @@
+import { css } from '@emotion/react';
+import { space } from '@guardian/source-foundations';
+import { Select as SourceSelect } from '@guardian/source-react-components';
 import { FieldLabel } from './FieldLabel';
+
+const textInputStyles = css`
+	margin-top: ${space[2]}px;
+`;
 
 type Props = {
 	formField: CampaignFieldSelect;
@@ -9,7 +16,16 @@ type Props = {
 export const Select = ({ formField, formData, setFormData }: Props) => (
 	<>
 		<FieldLabel formField={formField} />
-		<select
+		<SourceSelect
+			label={''}
+			children={formField.options.map((option, index) => (
+				<option key={index} value={option.value}>
+					{option.value}
+				</option>
+			))}
+		/>
+		{/* <select
+			css={textInputStyles}
 			data-testid={`form-field-${formField.id}`}
 			required={formField.required}
 			value={
@@ -29,6 +45,6 @@ export const Select = ({ formField, formData, setFormData }: Props) => (
 					{option.value}
 				</option>
 			))}
-		</select>
+		</select> */}
 	</>
 );
