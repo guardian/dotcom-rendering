896b580170b96c266bbc9eeaff0d746868da23ae
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-11-01 17:05:24 +0000

    created new callout block component

---
+++ dotcom-rendering/src/web/components/CalloutBlockComponentNew.stories.tsx
@@ -0,0 +1,16 @@
+import { CalloutBlockComponent } from './CalloutBlockComponentNew';
+
+export default {
+	component: CalloutBlockComponent,
+	title: 'Components/CalloutBlockComponentNew',
+};
+
+export const Default = () => {
+	return (
+		<div>Hello, world</div>
+			{/* <CalloutBlockComponent />; */}
+
+	);
+};
+
+Default.story = { name: 'default' };
---
+++ dotcom-rendering/src/web/components/CalloutBlockComponentNew.tsx
@@ -0,0 +1,3 @@
+export const CalloutBlockComponent = () => {
+	<div>Hello, world</div>;
+};
