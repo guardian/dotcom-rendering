c654cd6cc8c7218ee0a88bbcff6208d2b5f5fdc2
Sophie <sophiemacmillan93@gmail.com>
2022-11-02 14:03:53 +0000

    Implement an ExpandingWrapper Component

--- apps-rendering/src/client/article.ts
+++ apps-rendering/src/client/article.ts
@@ -291,14 +291,14 @@ function readFile(file: Blob): Promise<string> {
 function callouts(): void {
 	const callouts = Array.from(document.querySelectorAll('.js-callout'));
 	callouts.forEach((callout) => {
-		const buttons = Array.from(
-			callout.querySelectorAll('.js-callout-expand'),
-		);
-		buttons.forEach((button) => {
-			button.addEventListener('click', () => {
-				callout.toggleAttribute('open');
-			});
-		});
+		// const buttons = Array.from(
+		// 	callout.querySelectorAll('.js-callout-expand'),
+		// );
+		// buttons.forEach((button) => {
+		// 	button.addEventListener('click', () => {
+		// 		callout.toggleAttribute('open');
+		// 	});
+		// });
 
 		const form = callout.querySelector('form');
 		if (!form) return;
