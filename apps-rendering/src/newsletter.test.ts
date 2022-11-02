58ff49a01cd51bcb320cab421e601f5c99a1dc90
David Blatcher <30567854+dblatcher@users.noreply.github.com>
2022-11-02 15:47:05 +0000

    Dblatcher/apps email sign up placement 2 (#5993)
    * outline insert newsletter module
    
    * first version of insertion logic for article model
    
    * use an enum to categorise element reulsts
    
    * use the result cateogries in the logic
    
    * refactor index logic, go overboard on comments
    
    * remove custom type
    
    * move find index function into 'pure functions' section
    
    * add logging
    
    * don't allow the block to be placed after a paragraph containing a single strong element
    
    * add function to set the range within 3 spaces of the middle
    
    * use variables to control the debug logging
    
    * rename Enum
    
    * linting
    
    * add test coverage
    
    * account for future placement of ads, rearrange comment
    
    * refactor logging to separate function
    
    * simplify adSlot code, linting
    
    * refactor categorise function to use switch on ElementKind
    
    * write up placement rules in function description
    
    * remove test newsletter
    
    * remove call to insertNewsletter function in Page module
    
    * add missing placement rule to the function description
    
    * change log level
    
    * test for promotedNewsletter before buildBodyElement
    
    * refactor inserting functions to use return Results and not mutate objects
    
    * don't try to predict adslots
    
    * add TO DO to remove debugging
    
    * import lint
    
    * remove debug logging
    
    * update comments about adslots
    
    * simplify function defintions for generating bodies
    
    * remove unnecessary quote on enum
    
    * refactor findInsertIndex to not test every position in the target range
    
    * revise tests to account for insertNewsletterIntoItem no longer modifying the item passed to it
    
    * use functions to make the test body objects easier to visualise
    
    * change to getPillarFromId
    
    * refactor insert function to use ElementCategoryAndIndex type
    
    * move and rename module and test
    
    * add further placement test. rename argumen for clarity

---
+++ apps-rendering/src/newsletter.test.ts
@@ -0,0 +1,173 @@
+/**
+ * @jest-environment jsdom
+ */
+import { Newsletter } from '@guardian/apps-rendering-api-models/newsletter';
+import { none, some } from '@guardian/types';
+import type { Body, BodyElement } from 'bodyElement';
+import { ElementKind } from 'bodyElementKind';
+import type { DeadBlog, Quiz, Standard } from 'item';
+import { Optional } from 'optional';
+import { Result } from 'result';
+import { quiz, article } from 'fixtures/item';
+import { deadBlog } from 'fixtures/live';
+import { insertNewsletterIntoItem } from './newsletter';
+
+const TEST_NEWSLETTER: Newsletter = {
+	identityName: 'patriarchy',
+	description:
+		'Reviewing the most important stories on feminism and sexism and those fighting for equality',
+	name: 'The Week in Patriarchy',
+	frequency: 'Weekly',
+	theme: 'opinion',
+	successDescription: 'signed up',
+};
+
+const makeTextElementNode = (html: string, outerTag = 'p'): Node => {
+	const doc = new DocumentFragment();
+	const el = document.createElement(outerTag);
+	el.innerHTML = html;
+	doc.appendChild(el);
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- this value is not `null`
+	return doc.firstChild!;
+};
+
+const makeParagraphResult = (html: string): Result<string, BodyElement> =>
+	Result.ok({
+		kind: ElementKind.Text,
+		doc: makeTextElementNode(html),
+	});
+const makeHeadingResult = (
+	html: string,
+	id: string,
+): Result<string, BodyElement> =>
+	Result.ok({
+		kind: ElementKind.HeadingTwo,
+		id: Optional.some(id),
+		doc: makeTextElementNode(html, 'h2'),
+	});
+const makePullquoteResult = (): Result<string, BodyElement> =>
+	Result.ok({
+		kind: ElementKind.Pullquote,
+		quote: 'Why should the crown be allowed to carry on with a feudal system just because they want to?',
+		attribution: { kind: 0, value: 'Jane Giddins' },
+	});
+
+const makeBodyWithPlaceToInsert = (): Body => [
+	makeParagraphResult('Introductory paragraph.'),
+	makeHeadingResult('first subheading', 'first-subheading'),
+	makeParagraphResult('Another paragraph.'),
+	makeHeadingResult('Another heading', 'another-heading'),
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+	makeHeadingResult('Another heading', 'another-heading'),
+	makeParagraphResult('Another paragraph.'),
+	makePullquoteResult(),
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+];
+
+const makeBodyWithNoPlacesToInsert = (): Body => [
+	makeParagraphResult('Introductory paragraph.'),
+	makeHeadingResult('first subheading', 'first-subheading'),
+	makeParagraphResult('Another paragraph.'),
+	makeHeadingResult('Another heading', 'another-heading'),
+	makeParagraphResult('Another paragraph.'),
+	makeHeadingResult('Another heading', 'another-heading'),
+	makeParagraphResult('Another paragraph.'),
+	makePullquoteResult(),
+];
+
+const makeBodyWithNoPlacesToInsertInTheTargetZone = (): Body => [
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makePullquoteResult(),
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+	makeParagraphResult('Another paragraph.'),
+];
+
+describe('Insert Newsletter Signups', () => {
+	it('makes no change if there is no newsletter', () => {
+		const originalBody = makeBodyWithPlaceToInsert();
+		const newItem = insertNewsletterIntoItem({
+			...article,
+			body: originalBody,
+			promotedNewsletter: none,
+		}) as Standard;
+		expect(originalBody).toEqual(newItem.body);
+	});
+
+	it('makes no change if there are no suitable places in the body', () => {
+		const originalBody = makeBodyWithNoPlacesToInsert();
+		const newItem = insertNewsletterIntoItem({
+			...article,
+			body: originalBody,
+			promotedNewsletter: some(TEST_NEWSLETTER),
+		}) as Standard;
+		expect(newItem.body).toEqual(originalBody);
+	});
+
+	it('will not insert a NewsletterSignupBlockElement outside the target range', () => {
+		const originalBody = makeBodyWithNoPlacesToInsertInTheTargetZone();
+		const newItem = insertNewsletterIntoItem({
+			...article,
+			body: originalBody,
+			promotedNewsletter: some(TEST_NEWSLETTER),
+		}) as Standard;
+		expect(newItem.body).toEqual(originalBody);
+	});
+
+	it('inserts a NewsletterSignupBlockElement to a standard article if there is a newsletter', () => {
+		const newItem = insertNewsletterIntoItem({
+			...article,
+			body: makeBodyWithPlaceToInsert(),
+			promotedNewsletter: some(TEST_NEWSLETTER),
+		}) as Standard;
+
+		const insertedElement = newItem.body.find(
+			(result) =>
+				result.isOk() &&
+				result.value.kind === ElementKind.NewsletterSignUp,
+		);
+		expect(insertedElement).toBeTruthy();
+	});
+
+	it('will not insert a NewsletterSignupBlockElement into a blog', () => {
+		const blogWithPromotedNewsletter: DeadBlog = {
+			...deadBlog,
+			blocks: deadBlog.blocks.map((block) => ({
+				...block,
+				body: makeBodyWithPlaceToInsert(),
+			})),
+			promotedNewsletter: some(TEST_NEWSLETTER),
+		};
+		const newItem = insertNewsletterIntoItem(blogWithPromotedNewsletter);
+		expect(blogWithPromotedNewsletter).toEqual(newItem);
+	});
+
+	it('will not insert a NewsletterSignupBlockElement into a quiz', () => {
+		const originalBody = makeBodyWithPlaceToInsert();
+		const newItem = insertNewsletterIntoItem({
+			...quiz,
+			body: originalBody,
+			promotedNewsletter: some(TEST_NEWSLETTER),
+		}) as Quiz;
+		expect(newItem.body).toEqual(originalBody);
+	});
+});
