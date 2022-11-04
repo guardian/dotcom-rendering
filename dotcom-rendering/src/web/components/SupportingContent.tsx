3568b4553e95a34bc5b4651e5482cbf491cab770
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2022-11-04 15:08:00 +0000

    add containerPalette

--- dotcom-rendering/src/web/components/SupportingContent.tsx
+++ dotcom-rendering/src/web/components/SupportingContent.tsx
@@ -1,6 +1,9 @@
 import { css } from '@emotion/react';
 import { from, until } from '@guardian/source-foundations';
-import type { DCRSupportingContent } from '../../types/front';
+import type {
+	DCRContainerPalette,
+	DCRSupportingContent,
+} from '../../types/front';
 import { CardHeadline } from './CardHeadline';
 
 type Alignment = 'vertical' | 'horizontal';
@@ -8,6 +11,7 @@ type Alignment = 'vertical' | 'horizontal';
 type Props = {
 	supportingContent: DCRSupportingContent[];
 	alignment: Alignment;
+	containerPalette?: DCRContainerPalette;
 };
 
 const wrapperStyles = css`
@@ -58,7 +62,11 @@ const bottomMargin = css`
 	}
 `;
 
-export const SupportingContent = ({ supportingContent, alignment }: Props) => {
+export const SupportingContent = ({
+	supportingContent,
+	alignment,
+	containerPalette,
+}: Props) => {
 	return (
 		<ul css={[wrapperStyles, directionStyles(alignment)]}>
 			{supportingContent.map((subLink: DCRSupportingContent, index) => {
@@ -84,6 +92,7 @@ export const SupportingContent = ({ supportingContent, alignment }: Props) => {
 							showSlash={false}
 							showLine={true}
 							linkTo={subLink.url}
+							containerPalette={containerPalette}
 						/>
 					</li>
 				);
