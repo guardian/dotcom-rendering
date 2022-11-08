f9919634156b9c66e4164a432b4c6568740d0372
Pete F <pete.faulconbridge@theguardian.com>
2022-11-08 15:31:54 +0000

    Fix merge conflicts

--- dotcom-rendering/src/web/components/ShowMore.importable.tsx
+++ dotcom-rendering/src/web/components/ShowMore.importable.tsx
@@ -1,5 +1,4 @@
 import { css } from '@emotion/react';
-<<<<<<< HEAD
 import {
 	from,
 	neutral,
@@ -18,14 +17,6 @@ import { enhanceCards } from '../../model/enhanceCards';
 import { shouldPadWrappableRows } from '../lib/dynamicSlices';
 import { useApi } from '../lib/useApi';
 import { useOnce } from '../lib/useOnce';
-=======
-import { from, space } from '@guardian/source-foundations';
-import { Button, SvgMinus, SvgPlus } from '@guardian/source-react-components';
-import { useState } from 'react';
-import type { FEFrontCard } from 'src/types/front';
-import { enhanceCards } from '../../model/enhanceCards';
-import { useApi } from '../lib/useApi';
->>>>>>> ccfeaf550 (WIP: Add ShowMore.importable component)
 import { LI } from './Card/components/LI';
 import { UL } from './Card/components/UL';
 import { FrontCard } from './FrontCard';
@@ -40,11 +31,7 @@ function decideButtonText({
 	containerTitle: string;
 }) {
 	if (isOpen && loading) return 'Loading';
-<<<<<<< HEAD
 	if (isOpen) return `Less ${containerTitle}`;
-=======
-	if (isOpen) return 'Less';
->>>>>>> ccfeaf550 (WIP: Add ShowMore.importable component)
 	return `More ${containerTitle}`;
 }
 
@@ -52,7 +39,6 @@ type Props = {
 	containerTitle: string;
 	path: string;
 	containerId: string;
-<<<<<<< HEAD
 	showAge: boolean;
 	baseUrl: string;
 	containerPalette?: DCRContainerPalette;
@@ -84,16 +70,6 @@ export const ShowMore = ({
 
 		setExistingCardLinks(containerLinks);
 	}, []);
-=======
-};
-
-export const ShowMore = ({ containerTitle, path, containerId }: Props) => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	function toggleOpen() {
-		setIsOpen(!isOpen);
-	}
->>>>>>> ccfeaf550 (WIP: Add ShowMore.importable component)
 
 	/** We only pass an actual URL to SWR when 'showMore' is true.
 	 * Toggling 'showMore' will trigger a re-render
@@ -102,7 +78,6 @@ export const ShowMore = ({ containerTitle, path, containerId }: Props) => {
 	const url = isOpen
 		? `${baseUrl}/${path}/show-more/${containerId}.json?dcr=true`
 		: undefined;
-<<<<<<< HEAD
 	const { data, error, loading } = useApi<FEFrontCard[]>(url);
 
 	const filteredData =
@@ -210,57 +185,6 @@ export const ShowMore = ({ containerTitle, path, containerId }: Props) => {
 					</InlineError>
 				)}
 			</div>
-=======
-	const { data, loading } = useApi<FEFrontCard[]>(url);
-
-	/**
-		@todo: Semantics for the button and the new container (sub-container?)
-		@todo: Desired focus behaviour on expand/collapse?
-		@todo: Appropriate semantics for indicating state (aria-live, aria-expanded etc.)?
-		@todo: can we avoid hard-coding the API URL?
-		@todo: handle errors
-		@todo: deduping
-			@todo: get visibleCardIds from DOM instead of as a prop
-		@todo: rename props to match Front type?
-		@todo: make sure that the new content doesn't shift the top of the viewport when it loads
-	*/
-
-	return (
-		<>
-			{data && (
-				<>
-					<div
-						css={css`
-							height: ${space[3]}px;
-						`}
-					/>
-					<UL>
-						{enhanceCards(data).map((trail) => (
-							<LI key={trail.url} padSides={true}>
-								<FrontCard trail={trail} imageUrl={undefined} />
-							</LI>
-						))}
-					</UL>
-				</>
-			)}
-			<Button
-				priority="tertiary"
-				size="xsmall"
-				icon={isOpen && !loading ? <SvgMinus /> : <SvgPlus />}
-				iconSide="left"
-				onClick={toggleOpen}
-				cssOverrides={css`
-					margin-top: ${space[3]}px;
-					${from.tablet} {
-						margin-left: 10px;
-					}
-				`}
-				disabled={loading}
-				data-cy={`show-more-button-${containerId}`}
-			>
-				{decideButtonText({ isOpen, loading, containerTitle })}
-			</Button>
->>>>>>> ccfeaf550 (WIP: Add ShowMore.importable component)
 		</>
 	);
 };
