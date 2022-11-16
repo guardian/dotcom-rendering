c57854988543902aa77ee5710847683e64038d57
Joe Cowton <joe.cowton@guardian.co.uk>
2022-11-16 13:36:26 +0000

    fix storybook by adding story

--- storybooks/.storybook/main.js
+++ storybooks/.storybook/main.js
@@ -2,10 +2,18 @@ module.exports = {
 	core: {
 		builder: 'webpack5',
 	},
+	stories: ['../test.stories.tsx'],
+	features: {
+		buildStoriesJson: true,
+	},
 	refs: () => ({
 		'common-rendering': {
 			title: 'common-rendering',
-			url: 'http://localhost:4001/',
+			url: 'http://localhost:4001',
+		},
+		'dotcom-rendering': {
+			title: 'dotcom-rendering',
+			url: 'http://localhost:4002',
 		},
 	}),
 };
---
+++ storybooks/test.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Mock = () => <div>hello</div>;
+
+export default {
+	component: Mock,
+	title: 'MockStory',
+};
+
+export const mock = () => <Mock />;
