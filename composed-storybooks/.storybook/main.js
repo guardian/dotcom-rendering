c10a5a5d6c9a735b2c88bd379df0bd6c9a447ab2
Alex Sanders <alex@sndrs.dev>
2022-11-16 16:45:06 +0000

    add all storybooks to componsed

--- composed-storybooks/.storybook/main.js
+++ composed-storybooks/.storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
 	core: {
 		builder: 'webpack5',
 	},
-	stories: ['../test.stories.tsx'],
+	stories: ['../Readme.stories.jsx'],
 	features: {
 		buildStoriesJson: true,
 	},
@@ -14,6 +14,10 @@ module.exports = {
 		'dotcom-rendering': {
 			title: 'dotcom-rendering',
 			url: 'http://localhost:4002',
+		},
+		'apps-rendering': {
+			title: 'apps-rendering',
+			url: 'http://localhost:4003',
 		},
 	}),
 };
