785de2851d4ebe06429495d5386b799133208454
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2022-11-21 17:54:24 +0000

    Add dynamo sublink styling

--- dotcom-rendering/src/web/components/SupportingContent.tsx
+++ dotcom-rendering/src/web/components/SupportingContent.tsx
@@ -64,13 +64,61 @@ const bottomMargin = css`
 	}
 `;
 
+const dynamoLiStyles = css`
+	margin-left: 10px;
+	margin-bottom: 12px;
+	padding-top: 5px;
+	background-color: rgba(237, 237, 237, 0.9);
+	&:first-of-type {
+		margin-left: 0px;
+	}
+	padding: 0.3125rem 0.3125rem 0.625rem;
+`;
+
+const dynamoWrapperStyles = css`
+	display: flex;
+	position: absolute;
+	justify-content: space-between;
+	bottom: 0;
+	right: 0;
+	margin-left: 5px;
+	margin-right: 5px;
+	flex-direction: column;
+	${from.phablet} {
+		flex-direction: row;
+	}
+`;
+
 export const SupportingContent = ({
 	supportingContent,
 	alignment,
 	containerPalette,
 	isDynamo,
 }: Props) => {
-	return (
+	return isDynamo ? (
+		<ul css={[dynamoWrapperStyles]}>
+			{supportingContent.map((subLink: DCRSupportingContent) => {
+				// The model has this property as optional but it is very likely
+				// to exist
+				if (!subLink.headline) return null;
+				return (
+					<li key={subLink.url} css={[dynamoLiStyles]}>
+						<CardHeadline
+							format={subLink.format}
+							size="tiny"
+							showSlash={false}
+							showLine={true}
+							linkTo={subLink.url}
+							containerPalette={containerPalette}
+							isDynamo={isDynamo}
+							headlineText={subLink.headline}
+							kickerText={subLink.kickerText}
+						/>
+					</li>
+				);
+			})}
+		</ul>
+	) : (
 		<ul css={[wrapperStyles, directionStyles(alignment)]}>
 			{supportingContent.map((subLink: DCRSupportingContent, index) => {
 				// The model has this property as optional but it is very likely
