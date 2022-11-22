6f885664724f8bcd580b4533e60d5646b6ac4256
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-11-22 14:50:42 +0000

    using margin shorthand

--- apps-rendering/src/components/Pullquote/index.tsx
+++ apps-rendering/src/components/Pullquote/index.tsx
@@ -13,10 +13,9 @@ import { darkModeCss } from 'styles';
 const styles = (format: ArticleFormat): SerializedStyles => {
 	return css`
 		color: ${text.pullquote(format)};
-		margin: 0;
+		margin: 0 0 ${remSpace[5]};
 		${headline.xsmall({ fontWeight: 'light' })};
 		${darkModeCss`color: ${text.pullquoteDark(format)};`}
-		margin-bottom: ${remSpace[5]};
 	`;
 };
 
