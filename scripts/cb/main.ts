6be59862aa8569b99354fac1e4ee8a028b6dca64
Chris Jones <chrislomaxjones96@gmail.com>
2022-11-23 11:44:27 +0000

    wip

--- scripts/cb/main.ts
+++ scripts/cb/main.ts
@@ -1,9 +1,52 @@
+// import { octokit } from '../deno/github.ts';
+// import type { EventPayloadMap } from 'https://cdn.skypack.dev/@octokit/webhooks-types?dts';
+
 type Metric = {
 	key: string;
 	value: string;
 };
 
-type MetricsLogFile = {};
+type MetricsLogFile = Metric[];
+
+// const isPullRequestEvent = (
+// 	payload: EventPayloadMap[keyof EventPayloadMap],
+// ): payload is EventPayloadMap['pull_request'] =>
+// 	//@ts-expect-error -- We’re actually checking the type
+// 	typeof payload?.pull_request?.number === 'number';
+
+// const getCommentID = async (): Promise<number | null> => {
+// 	/**
+// 	 * https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
+// 	 */
+// 	const payload: EventPayloadMap['push' | 'pull_request'] = JSON.parse(
+// 		Deno.readTextFileSync(path),
+// 	);
+
+// 	const issue_number = isPullRequestEvent(payload)
+// 		? payload.pull_request.number
+// 		: undefined;
+
+// 	if (!issue_number) {
+// 		throw Error('Not a pull request event');
+// 	}
+
+// 	const GIHUB_PARAMS = {
+// 		owner: 'guardian',
+// 		repo: 'dotcom-rendering',
+// 		issue_number,
+// 	} as const;
+
+// 	if (!octokit) return null;
+// 	const { data: comments } = await octokit.rest.issues.listComments({
+// 		...GIHUB_PARAMS,
+// 	});
+
+// 	const comment = comments.find((comment) =>
+// 		comment.body?.includes(REPORT_TITLE),
+// 	);
+
+// 	return comment?.id ?? null;
+// };
 
 const loadMetrics = async (filename: string): Promise<MetricsLogFile> => {
 	const decoder = new TextDecoder('utf-8');
@@ -11,6 +54,18 @@ const loadMetrics = async (filename: string): Promise<MetricsLogFile> => {
 	return JSON.parse(decoder.decode(data)) as MetricsLogFile;
 };
 
-const metrics = await loadMetrics('metrics.log');
+const main = async () => {
+	/** Path for workflow event */
+	const path = Deno.env.get('GITHUB_EVENT_PATH');
+	if (!path) throw new Error('Missing GITHUB_EVENT_PATH');
 
-console.log(metrics);
+	// Obtain the metrics
+	const metrics = await loadMetrics('metrics.log');
+
+	// TODO send the metrics to the remote server for aggregation!
+
+	// Present the metrics!
+	console.log(metrics);
+};
+
+await main();
