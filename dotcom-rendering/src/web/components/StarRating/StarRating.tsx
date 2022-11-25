5b09d3c5331c7d631e0a1fe7dce8d176fafc8654
Pete F <pete.faulconbridge@theguardian.com>
2022-11-25 15:25:44 +0000

    Temp: replace StarRating with Source

--- dotcom-rendering/src/web/components/StarRating/StarRating.tsx
+++ dotcom-rendering/src/web/components/StarRating/StarRating.tsx
@@ -1,60 +1,62 @@
-import { css } from '@emotion/react';
-import { Star } from '../../../static/icons/Star';
-import type { RatingSizeType } from '../../../types/content';
+// import { css } from '@emotion/react';
+// import { Star } from '../../../static/icons/Star';
+// import type { RatingSizeType } from '../../../types/content';
 
-const starWrapper = css`
-	display: inline-block;
-	padding: 1px;
-`;
+export { StarRating } from '@guardian/source-react-components-development-kitchen';
 
-const determineSize = (size: RatingSizeType) => {
-	switch (size) {
-		case 'small':
-			return css`
-				padding: 1px;
-				svg {
-					width: 12px;
-					height: 12px;
-				}
-			`;
-		case 'medium':
-			return css`
-				padding: 1px;
-				svg {
-					width: 16px;
-					height: 16px;
-				}
-			`;
-		case 'large':
-			return css`
-				padding: 2px;
-				svg {
-					width: 20px;
-					height: 20px;
-				}
-			`;
-	}
-};
+// const starWrapper = css`
+// 	display: inline-block;
+// 	padding: 1px;
+// `;
 
-export const StarRating: React.FC<{
-	rating: number;
-	size: RatingSizeType;
-}> = ({ rating, size }) => (
-	<div css={determineSize(size)}>
-		<div css={starWrapper}>
-			<Star starId={`${size}1`} isEmpty={rating < 1} />
-		</div>
-		<div css={starWrapper}>
-			<Star starId={`${size}2`} isEmpty={rating < 2} />
-		</div>
-		<div css={starWrapper}>
-			<Star starId={`${size}3`} isEmpty={rating < 3} />
-		</div>
-		<div css={starWrapper}>
-			<Star starId={`${size}4`} isEmpty={rating < 4} />
-		</div>
-		<div css={starWrapper}>
-			<Star starId={`${size}5`} isEmpty={rating < 5} />
-		</div>
-	</div>
-);
+// const determineSize = (size: RatingSizeType) => {
+// 	switch (size) {
+// 		case 'small':
+// 			return css`
+// 				padding: 1px;
+// 				svg {
+// 					width: 12px;
+// 					height: 12px;
+// 				}
+// 			`;
+// 		case 'medium':
+// 			return css`
+// 				padding: 1px;
+// 				svg {
+// 					width: 16px;
+// 					height: 16px;
+// 				}
+// 			`;
+// 		case 'large':
+// 			return css`
+// 				padding: 2px;
+// 				svg {
+// 					width: 20px;
+// 					height: 20px;
+// 				}
+// 			`;
+// 	}
+// };
+
+// export const StarRating: React.FC<{
+// 	rating: number;
+// 	size: RatingSizeType;
+// }> = ({ rating, size }) => (
+// 	<div css={determineSize(size)}>
+// 		<div css={starWrapper}>
+// 			<Star starId={`${size}1`} isEmpty={rating < 1} />
+// 		</div>
+// 		<div css={starWrapper}>
+// 			<Star starId={`${size}2`} isEmpty={rating < 2} />
+// 		</div>
+// 		<div css={starWrapper}>
+// 			<Star starId={`${size}3`} isEmpty={rating < 3} />
+// 		</div>
+// 		<div css={starWrapper}>
+// 			<Star starId={`${size}4`} isEmpty={rating < 4} />
+// 		</div>
+// 		<div css={starWrapper}>
+// 			<Star starId={`${size}5`} isEmpty={rating < 5} />
+// 		</div>
+// 	</div>
+// );
