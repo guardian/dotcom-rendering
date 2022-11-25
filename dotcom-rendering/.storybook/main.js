11623faa865deac5ee0b1b7193604fae174cd2c8
Joe Cowton <joe.cowton@guardian.co.uk>
2022-11-25 10:28:50 +0000

    Revert "Reinstate module resoloution for CR in dcr"
    This reverts commit e42c223c164b000f87d17d2fcf9714cba9716dd4.

--- dotcom-rendering/.storybook/main.js
+++ dotcom-rendering/.storybook/main.js
@@ -124,11 +124,6 @@ const webpackConfig = (config) => {
 		use: ['desvg-loader/react', 'svg-loader'],
 	});
 
-	config.resolve.modules = [
-		...((config && config.resolve && config.resolve.modules) || []),
-		path.resolve(__dirname, '../../common-rendering/src'),
-	];
-
 	config.resolve.alias = {
 		...config.resolve.alias,
 	};
