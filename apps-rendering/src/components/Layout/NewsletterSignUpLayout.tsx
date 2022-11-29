79809a81dee90bae470b49eafde8df2a2cfbab19
David Blatcher <dv.bltchr@gmail.com>
2022-11-29 13:19:08 +0000

    render newsletter content if Item has a promotedNewsletter

--- apps-rendering/src/components/Layout/NewsletterSignUpLayout.tsx
+++ apps-rendering/src/components/Layout/NewsletterSignUpLayout.tsx
@@ -4,7 +4,12 @@ import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
 import { background } from '@guardian/common-rendering/src/editorialPalette';
 import type { ArticleFormat } from '@guardian/libs';
-import { until } from '@guardian/source-foundations';
+import {
+	brandAlt,
+	remSpace,
+	textSans,
+	until,
+} from '@guardian/source-foundations';
 import Body from 'components/ArticleBody';
 import Footer from 'components/Footer';
 import Headline from 'components/Headline';
@@ -17,6 +22,8 @@ import type { Item } from 'item';
 import type { FC, ReactNode } from 'react';
 import { darkModeCss, onwardStyles } from 'styles';
 import InPageNewsletterSignup from 'components/InPageNewsletterSignup';
+import { OptionKind } from '@guardian/types/dist/option';
+import { SvgClock, SvgNewsletter } from '@guardian/source-react-components';
 
 // ----- Styles ----- //
 const backgroundStyles = (format: ArticleFormat): SerializedStyles => css`
@@ -42,6 +49,38 @@ const contentRow = css`
 	${grid.column.centre};
 `;
 
+const frequencyBlockStyles = css`
+	display: flex;
+	margin-bottom: ${remSpace[2]};
+
+	span {
+		margin-left: ${remSpace[1]};
+		${textSans.xsmall()}
+
+		b {
+			${textSans.xsmall({ fontWeight: 'bold' })}
+		}
+	}
+`;
+
+const detailBlockStyles = css`
+	display: flex;
+	align-items: center;
+	margin-bottom: ${remSpace[2]};
+
+	svg {
+		background-color: ${brandAlt[400]};
+		border-radius: 50%;
+		margin-right: ${remSpace[2]};
+		width: ${remSpace[6]};
+		padding: 0.125rem;
+	}
+
+	b {
+		${textSans.xsmall({ fontWeight: 'bold' })}
+	}
+`;
+
 // ----- Component ----- //
 interface Props {
 	item: Item;
@@ -50,6 +89,10 @@ interface Props {
 
 const NewsletterSignUpLayout: FC<Props> = ({ item, children }) => {
 	const format = getFormat(item);
+	const newsletter =
+		item.promotedNewsletter.kind === OptionKind.Some
+			? item.promotedNewsletter.value
+			: undefined;
 
 	return (
 		<main css={backgroundStyles(format)}>
@@ -61,23 +104,39 @@ const NewsletterSignUpLayout: FC<Props> = ({ item, children }) => {
 					/>
 				</header>
 				<section css={contentRow}>
+					{newsletter && (
+						<div css={detailBlockStyles}>
+							<SvgNewsletter size="xsmall" />
+							<b>{newsletter.frequency}</b>
+							{/* TO DO - this should be the regional focus, but property is not on the MAPI type */}
+						</div>
+					)}
 					<Headline item={item} />
 					<Standfirst item={item} />
 
-					<InPageNewsletterSignup
-						newsletter={{
-							identityName: 'patriarchy',
-							description:
-								'Reviewing the most important stories on feminism and sexism and those fighting for equality',
-							name: 'The Week in Patriarchy',
-							frequency: 'Weekly',
-							theme: 'opinion',
-							successDescription: 'signed up',
-						}}
-						format={getFormat(item)}
-						defaultTo={'form'}
-						fallbackContent={<Body format={item}>{children}</Body>}
-					/>
+					{newsletter && (
+						<div css={frequencyBlockStyles}>
+							<SvgClock size="xsmall" />
+
+							<span>
+								You'll receive this newsletter{' '}
+								<b>{newsletter.frequency}</b>
+							</span>
+						</div>
+					)}
+
+					{newsletter ? (
+						<InPageNewsletterSignup
+							newsletter={newsletter}
+							format={getFormat(item)}
+							defaultTo={'form'} // TO DO - remove for production
+							fallbackContent={
+								<Body format={item}>{children}</Body>
+							}
+						/>
+					) : (
+						<Body format={item}>{children}</Body>
+					)}
 				</section>
 			</article>
 
