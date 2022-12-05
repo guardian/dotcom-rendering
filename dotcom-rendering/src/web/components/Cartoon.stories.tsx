a4d9ec87a38b5185c521d3207efa4759c3543bc5
Pete F <pete.faulconbridge@theguardian.com>
2022-12-05 13:22:45 +0000

    Spike cartoons data model
    Demo showing how we might render a cartoon from the sort of data model
    being discussed in the Cartoons working group.
    
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>

--- dotcom-rendering/src/web/components/Cartoon.stories.tsx
+++ dotcom-rendering/src/web/components/Cartoon.stories.tsx
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react';
+import { fontWeights } from '@guardian/source-foundations';
 import type { CartoonType, Variant } from './Cartoon';
 import { Cartoon } from './Cartoon';
 
@@ -14,19 +16,20 @@ const variant: Variant = {
 			height: 2074,
 			width: 3110,
 			mimeType: 'image/jpeg',
-			file: 'https://media.guim.co.uk/7cffd9d6809318a9d92c719c473d193caf95d601/0_0_3110_2074/master/3110.jpg',
+			file: 'https://media.guim.co.uk/e0fa5bdfa0e6910b9b58ccc98c3247217566bf9f/0_0_3110_2074/3110.jpg',
 		},
 	],
 };
 
 const fixture1: CartoonType = {
 	type: 'cartoon',
-	caption: 'hello world',
+	caption: "Cartoon data model spike demo",
 	displayCredit: true,
-	alt: 'string',
-	credit: 'string',
+	alt: 'This is alt text',
+	credit: 'Artist name, 2022',
 	variants: [variant],
-	transcript: '<strong>Strong text</strong><br /><a href="theguardian.com">a link</a><br /><ul><li>list</li><li>items</li></ul>',
+	transcript:
+		'<strong>Strong text</strong><br /><a href="theguardian.com">a link</a><br /><ul><li>list</li><li>items</li></ul>',
 };
 
 export default {
@@ -34,8 +37,25 @@ export default {
 	title: 'Components/Cartoon',
 };
 
-export const Default = () => <Cartoon cartoon={fixture1} />;
+export const Default = () => (
+	<section
+		css={css`
+			padding: 50px 0;
+			max-width: 400px;
+			margin: 0 auto;
+			/* to demo that the transcript markup inherits styling */
+			& li {
+				list-style-type: disc;
+			}
+			& strong {
+				font-weight: ${fontWeights["bold"]};
+			}
+		`}
+	>
+		<Cartoon cartoon={fixture1} />
+	</section>
+);
 
 Default.story = {
-	name: 'for videos',
+	name: 'Data model demo',
 };
--- dotcom-rendering/src/web/components/Cartoon.tsx
+++ dotcom-rendering/src/web/components/Cartoon.tsx
@@ -3,6 +3,10 @@ import { from, until } from '@guardian/source-foundations';
 
 type ViewportSize = 'small' | 'medium' | 'large';
 
+/**
+ * This doesn't exactly map onto CAPI or DCR models, but the exact structure
+ * can be adjusted to fit.
+ */
 type Image = {
 	mimeType: 'image/jpeg' | 'image/png' | 'image/svg+xml';
 	file: string;
@@ -10,6 +14,11 @@ type Image = {
 	height: number;
 };
 
+/**
+ * @todo: we probably want to rename this type; the idea is that a 'variant'
+ * is a set of images which corresponds to a 'crop' for a given screen size
+ * (e.g. one large image for desktop, or a set of 5 crops for mobile, etc.)
+ */
 export type Variant = {
 	viewportSize: 'small' | 'medium' | 'large';
 	images: Image[];
@@ -29,15 +38,17 @@ type Props = {
 	cartoon: CartoonType;
 };
 
+/**
+ * We'll almost certainly need some CSS to show/hide the right images for a given breakpoint,
+ * (i.e. we can't rely on the media queries in a <picture> tag, and we don't want to use js unless
+ * it's necessary).
+ * The exact implementation is tbc, but the logic sketched in this function is accounting for the
+ * possibility that e.g. variants haven't been provided for all of the possible ViewportSizes.
+ */
 function decideBreakpointStyles(
 	viewportSize: ViewportSize,
 	viewports: ViewportSize[],
 ) {
-	// const sizeMapping = {
-	// 	'small': 0,
-	// 	'medium': 1,
-	// 	'large': 2,
-	// }
 	switch (viewportSize) {
 		case 'small':
 			if (viewports.includes('medium')) {
@@ -65,11 +76,15 @@ function decideBreakpointStyles(
 
 export const Cartoon = ({ cartoon }: Props) => {
 	return (
-		<section>
+		<>
 			<figure
+				/**
+				 * Just minimal styling while we explore the data model.
+				 */
 				css={css`
 					display: flex;
 					flex-direction: column;
+					gap: 12px;
 				`}
 			>
 				<figcaption>{cartoon.caption}</figcaption>
@@ -84,6 +99,6 @@ export const Cartoon = ({ cartoon }: Props) => {
 				})}
 			</figure>
 			<div dangerouslySetInnerHTML={{ __html: cartoon.transcript }} />
-		</section>
+		</>
 	);
 };
