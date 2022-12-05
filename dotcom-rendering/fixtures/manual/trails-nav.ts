d2140dd7b2aafa07dd4a031a0bbe1093a8c40df6
Ashleigh Carr <ashcorr20@gmail.com>
2022-12-05 14:16:31 +0000

    fix: Use correct image urls for trail fixtures

--- dotcom-rendering/fixtures/manual/trails-nav.ts
+++ dotcom-rendering/fixtures/manual/trails-nav.ts
@@ -15,7 +15,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/athletics',
 		headline: 'Athletics',
-		image: 'https://i.guim.co.uk/img/media/49c197838ba04557dbdd6f5077a7a98afe5bd299/0_133_3275_1964/master/3275.jpg?width=300&quality=85&auto=format&fit=max&s=4b26b77fc091ef5db9f380bc734b26ed',
+		image: 'https://i.guim.co.uk/img/media/49c197838ba04557dbdd6f5077a7a98afe5bd299/0_133_3275_1964/master/3275.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -26,7 +26,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/olympic-games-2020',
 		headline: 'Olympic Games',
-		image: 'https://i.guim.co.uk/img/media/690a725111db015304868628774a70aa1e2b18c5/702_902_2735_1640/master/2735.jpg?width=300&quality=85&auto=format&fit=max&s=0f3fe86c622b176b526f7be376787a86',
+		image: 'https://i.guim.co.uk/img/media/690a725111db015304868628774a70aa1e2b18c5/702_902_2735_1640/master/2735.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -37,7 +37,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/motorsports',
 		headline: 'Motor sports',
-		image: 'https://i.guim.co.uk/img/media/503c85b6cd242a8a79bea3fb6a6d5545fa33a903/3_59_1393_835/master/1393.jpg?width=300&quality=85&auto=format&fit=max&s=70d3023ca0f1cfcb59f8e49bb047175f',
+		image: 'https://i.guim.co.uk/img/media/503c85b6cd242a8a79bea3fb6a6d5545fa33a903/3_59_1393_835/master/1393.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -48,7 +48,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/chess',
 		headline: 'Chess',
-		image: 'https://i.guim.co.uk/img/media/5c41d87e7155700711aef81c2034b0d9f2818e3b/0_268_4992_2995/master/4992.jpg?width=300&quality=85&auto=format&fit=max&s=cb3eb0d7128e7375276dc591f5225c94',
+		image: 'https://i.guim.co.uk/img/media/5c41d87e7155700711aef81c2034b0d9f2818e3b/0_268_4992_2995/master/4992.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -59,7 +59,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/australia-sport',
 		headline: 'Australia Sport',
-		image: 'https://i.guim.co.uk/img/media/56b2199f0d43210a106d45f1a6510995c7999cb4/261_0_3683_2210/master/3683.jpg?width=300&quality=85&auto=format&fit=max&s=0fa097e8729de42de85c0983cd3c3d31',
+		image: 'https://i.guim.co.uk/img/media/56b2199f0d43210a106d45f1a6510995c7999cb4/261_0_3683_2210/master/3683.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -70,7 +70,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/sailing',
 		headline: 'Sailing',
-		image: 'https://i.guim.co.uk/img/media/1121e6cddc5de615c739575f102b2ffd709b2912/173_248_3208_1925/master/3208.jpg?width=300&quality=85&auto=format&fit=max&s=3180f188a4986fdd9efcb2e1af6b9b1f',
+		image: 'https://i.guim.co.uk/img/media/1121e6cddc5de615c739575f102b2ffd709b2912/173_248_3208_1925/master/3208.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -81,7 +81,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/snooker',
 		headline: 'Snooker',
-		image: 'https://i.guim.co.uk/img/media/a5f620941b6c1ddfbf6f8fe6fbb2e9f9da2a79c9/101_119_3399_2039/master/3399.jpg?width=300&quality=85&auto=format&fit=max&s=3a71d1ca236ef7670fbfedf17ea20ea2',
+		image: 'https://i.guim.co.uk/img/media/a5f620941b6c1ddfbf6f8fe6fbb2e9f9da2a79c9/101_119_3399_2039/master/3399.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -92,7 +92,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/darts',
 		headline: 'Darts',
-		image: 'https://i.guim.co.uk/img/media/049a6192c14f31032d3f42313051903eea55ae18/213_51_4717_2830/master/4717.jpg?width=300&quality=85&auto=format&fit=max&s=a6e663fe87d9e36697df9d1cf37922fd',
+		image: 'https://i.guim.co.uk/img/media/049a6192c14f31032d3f42313051903eea55ae18/213_51_4717_2830/master/4717.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
@@ -103,7 +103,7 @@ export const trails: [
 	{
 		url: 'https://www.theguardian.com/sport/disability-sport',
 		headline: 'Disability Sport',
-		image: 'https://i.guim.co.uk/img/media/cfdbb1d5668fdefac1166f3e445e58c2afdebc58/246_20_4893_2935/master/4893.jpg?width=300&quality=85&auto=format&fit=max&s=cec0de19d4918099fc9c52198b4a3f39',
+		image: 'https://i.guim.co.uk/img/media/cfdbb1d5668fdefac1166f3e445e58c2afdebc58/246_20_4893_2935/master/4893.jpg',
 		format: {
 			theme: ArticlePillar.News,
 			design: ArticleDesign.Standard,
