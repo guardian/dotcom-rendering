f518a74a7f54f6b6d1f64b4441e289cc9e661dea
Ashleigh Carr <ashcorr20@gmail.com>
2022-12-06 10:24:21 +0000

    feat: Revert moving generateImageUrl to lib and instead use generateSources for minicard images

--- dotcom-rendering/src/web/components/MiniCard.tsx
+++ dotcom-rendering/src/web/components/MiniCard.tsx
@@ -1,11 +1,11 @@
 import { css } from '@emotion/react';
-import { body } from '@guardian/source-foundations';
+import { body, breakpoints } from '@guardian/source-foundations';
 import { Link } from '@guardian/source-react-components';
 import type { DCRContainerPalette } from 'src/types/front';
 import type { TrailType } from 'src/types/trails';
-import { generateImageURL } from '../../lib/generateImageURL';
 import { ContainerOverrides } from '../../types/palette';
 import { decideContainerOverrides } from '../lib/decideContainerOverrides';
+import { generateSources } from './Picture';
 
 const imageStyles = css`
 	width: 120px;
@@ -29,6 +29,33 @@ type Props = {
 	containerPalette?: DCRContainerPalette;
 };
 
+type MiniCardPictureProps = {
+	image: string;
+	alt: string;
+};
+
+const MiniCardPicture = ({ image, alt }: MiniCardPictureProps) => {
+	const sources = generateSources(image, [
+		{ breakpoint: breakpoints.desktop, width: 120 },
+	]);
+
+	const [source] = sources.slice(-1);
+
+	return (
+		<picture css={[imageStyles]}>
+			{/* High resolution (HDPI) sources*/}
+			<source
+				srcSet={source.hiResUrl}
+				media={`(-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi)`}
+			/>
+			{/* Low resolution (MDPI) source*/}
+			<source srcSet={source.lowResUrl} />
+
+			<img alt={alt} src={source.lowResUrl} />
+		</picture>
+	);
+};
+
 /**
  * A simpler form of card that does not have any of the complexities of a normal card such as sublinks, kickers, format, etc.
  *
@@ -38,14 +65,6 @@ export const MiniCard = ({ trail, showImage, containerPalette }: Props) => {
 	const containerOverrides =
 		containerPalette && decideContainerOverrides(containerPalette);
 
-	const image =
-		trail.image &&
-		generateImageURL({
-			master: trail.image,
-			imageWidth: 300,
-			resolution: 'low',
-		});
-
 	return (
 		<Link
 			href={trail.url}
@@ -53,8 +72,8 @@ export const MiniCard = ({ trail, showImage, containerPalette }: Props) => {
 			subdued={true}
 			cssOverrides={[linkStyles, linkOverrideStyles(containerOverrides)]}
 		>
-			{showImage && (
-				<img css={imageStyles} src={image} alt={trail.headline} />
+			{showImage && trail.image && (
+				<MiniCardPicture image={trail.image} alt={trail.headline} />
 			)}
 			<span>{trail.headline}</span>
 		</Link>
--- dotcom-rendering/src/web/components/Picture.tsx
+++ dotcom-rendering/src/web/components/Picture.tsx
@@ -2,7 +2,6 @@ import { css } from '@emotion/react';
 import { ArticleDesign, ArticleDisplay } from '@guardian/libs';
 import { breakpoints } from '@guardian/source-foundations';
 import React from 'react';
-import { generateImageURL } from '../../lib/generateImageURL';
 import type { RoleType } from '../../types/content';
 
 /**
@@ -161,6 +160,42 @@ const decideImageWidths = ({
 				];
 		}
 	}
+};
+
+/**
+ * Generates a URL for calling the Fastly Image Optimiser.
+ *
+ * @see https://developer.fastly.com/reference/io/
+ * @see https://github.com/guardian/fastly-image-service/blob/main/fastly-io_guim_co_uk/src/main/resources/varnish/main.vcl
+ *
+ */
+const generateImageURL = ({
+	master,
+	imageWidth,
+	resolution,
+}: {
+	master: string;
+	imageWidth: number;
+	resolution: 'low' | 'high';
+}): string => {
+	const url = new URL(master);
+	const [service] = url.hostname.split('.');
+
+	const params = new URLSearchParams({
+		width: imageWidth.toString(),
+		// Why 45 and 85?
+		// This numbers have been picked in 2018 as the right
+		// balance between image fidelity and file size
+		// https://github.com/guardian/fastly-image-service/pull/35
+		...(resolution === 'high'
+			? { quality: '45', dpr: '2' }
+			: { quality: '85', dpr: '1' }),
+		s: 'none',
+	});
+
+	return `https://i.guim.co.uk/img/${service}${
+		url.pathname
+	}?${params.toString()}`;
 };
 
 const descendingByBreakpoint = (a: ImageWidthType, b: ImageWidthType) => {
