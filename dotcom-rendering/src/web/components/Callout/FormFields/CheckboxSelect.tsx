2740f618bc51754d975650d0e4943a094f295594
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-12-07 16:44:37 +0000

    updated checkbox select

--- dotcom-rendering/src/web/components/Callout/FormFields/CheckboxSelect.tsx
+++ dotcom-rendering/src/web/components/Callout/FormFields/CheckboxSelect.tsx
@@ -1,5 +1,5 @@
 import { Checkbox, CheckboxGroup } from '@guardian/source-react-components';
-import { CampaignFieldCheckbox } from '../../../types/content';
+import { CampaignFieldCheckbox } from '../../../../types/content';
 import { FieldLabel } from './FieldLabel';
 
 type Props = {
