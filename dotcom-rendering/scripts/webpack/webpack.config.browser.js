b065062bfc6936672caeb7f22e7034a02cbc6ec7
Ravi <7014230+arelra@users.noreply.github.com>
2022-12-08 10:29:29 +0000

    Temporary commit to run SWC builds against CI

--- dotcom-rendering/scripts/webpack/webpack.config.browser.js
+++ dotcom-rendering/scripts/webpack/webpack.config.browser.js
@@ -48,6 +48,7 @@ const getLoaders = (bundle) => {
 					},
 				},
 			];
+		case 'modern':
 		case 'variant':
 			return [
 				{
@@ -62,33 +63,33 @@ const getLoaders = (bundle) => {
 					},
 				},
 			];
-		case 'modern':
-			return [
-				{
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							'@babel/preset-react',
-							[
-								'@babel/preset-env',
-								{
-									bugfixes: true,
-									targets:
-										'extends @guardian/browserslist-config',
-								},
-							],
-						],
-						compact: true,
-					},
-				},
-				{
-					loader: 'ts-loader',
-					options: {
-						configFile: 'tsconfig.build.json',
-						transpileOnly: true,
-					},
-				},
-			];
+		// case 'modern':
+		// 	return [
+		// 		{
+		// 			loader: 'babel-loader',
+		// 			options: {
+		// 				presets: [
+		// 					'@babel/preset-react',
+		// 					[
+		// 						'@babel/preset-env',
+		// 						{
+		// 							bugfixes: true,
+		// 							targets:
+		// 								'extends @guardian/browserslist-config',
+		// 						},
+		// 					],
+		// 				],
+		// 				compact: true,
+		// 			},
+		// 		},
+		// 		{
+		// 			loader: 'ts-loader',
+		// 			options: {
+		// 				configFile: 'tsconfig.build.json',
+		// 				transpileOnly: true,
+		// 			},
+		// 		},
+		// 	];
 	}
 };
 
