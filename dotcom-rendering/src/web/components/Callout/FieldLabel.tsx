9e91d8a812c4480d2f17433475d5fb55bc1c8115
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-12-12 10:48:43 +0000

    Revert "Revert "Add validation errors to form fields for callouts""

--- dotcom-rendering/src/web/components/Callout/FieldLabel.tsx
+++ dotcom-rendering/src/web/components/Callout/FieldLabel.tsx
@@ -1,6 +1,10 @@
 import { css } from '@emotion/react';
-import { neutral, textSans } from '@guardian/source-foundations';
-import { CampaignFieldType } from '../../../types/content';
+import {
+	neutral,
+	textSans,
+	visuallyHidden,
+} from '@guardian/source-foundations';
+import type { CampaignFieldType } from 'src/types/content';
 
 const fieldLabelStyles = css`
 	${textSans.medium({ fontWeight: 'bold' })}
@@ -16,15 +20,22 @@ const optionalTextStyles = css`
 	padding-left: 5px;
 `;
 
-export const FieldLabel = ({ formField }: { formField: CampaignFieldType }) => (
-	<label css={fieldLabelStyles} htmlFor={formField.name}>
+type Props = {
+	formField: CampaignFieldType;
+	hideLabel?: boolean;
+};
+
+export const FieldLabel = ({ formField, hideLabel }: Props) => (
+	<label
+		css={[fieldLabelStyles, hideLabel && visuallyHidden]}
+		htmlFor={formField.name}
+		hidden={formField.hideLabel}
+	>
 		{formField.label}
 		{!formField.required && <span css={optionalTextStyles}>Optional</span>}
 		{!!formField.description && (
 			<div>
-				<span css={fieldDescription}>
-					{`(${formField.description})`}
-				</span>
+				<span css={fieldDescription}>{`${formField.description}`}</span>
 			</div>
 		)}
 	</label>
--- dotcom-rendering/src/web/components/Callout/MultiSelect.tsx
+++ dotcom-rendering/src/web/components/Callout/MultiSelect.tsx
@@ -1,11 +1,12 @@
 import type {
 	CampaignFieldCheckbox,
 	CampaignFieldRadio,
-} from '../../../types/content';
+} from 'src/types/content';
 import { CheckboxSelect } from './CheckboxSelect';
 import { RadioSelect } from './RadioSelect';
 
 type Props = {
+	validationErrors?: { [key in string]: string };
 	formField: CampaignFieldCheckbox | CampaignFieldRadio;
 	formData: { [key in string]: any };
 	setFormData: React.Dispatch<React.SetStateAction<{ [x: string]: any }>>;
@@ -13,6 +14,7 @@ type Props = {
 };
 
 export const MultiSelect = ({
+	validationErrors,
 	formField,
 	formData,
 	setFormData,
@@ -21,12 +23,14 @@ export const MultiSelect = ({
 	<div data-testid={`form-field-${formField.id}`}>
 		{multiple ? (
 			<CheckboxSelect
+				validationErrors={validationErrors ?? undefined}
 				formField={formField as CampaignFieldCheckbox}
 				formData={formData}
 				setFormData={setFormData}
 			/>
 		) : (
 			<RadioSelect
+				validationErrors={validationErrors ?? undefined}
 				formField={formField as CampaignFieldRadio}
 				formData={formData}
 				setFormData={setFormData}
