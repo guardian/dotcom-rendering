a4d98d64acfe03b663a8b1de17ec5aa63c1636ff
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-12-15 15:11:02 +0000

    add share functionality to share component

--- dotcom-rendering/src/web/components/Callout/CalloutShare.tsx
+++ dotcom-rendering/src/web/components/Callout/CalloutShare.tsx
@@ -1,21 +1,24 @@
 import { css } from '@emotion/react';
 import { textSans } from '@guardian/source-foundations';
-import { SvgShareCallout } from '@guardian/source-react-components';
+import { Button, SvgShareCallout } from '@guardian/source-react-components';
+import { useState } from 'react';
 import { decidePalette } from '../../lib/decidePalette';
 
 const shareCalloutStyles = css`
 	display: flex;
 	align-items: center;
 `;
-const shareCalloutTextStyles = (format: ArticleFormat) =>
+const shareCalloutTextStyles = css`
+	display: inline-block;
+	${textSans.xsmall()}
+`;
+
+const shareCalloutLinkStyles = (format: ArticleFormat) =>
 	css`
-		a {
-			color: ${decidePalette(format).text.richLink};
-			border-bottom: 1px solid ${decidePalette(format).text.richLink};
-			text-decoration: none;
-		}
-		display: inline-block;
-		${textSans.xsmall()}
+		color: ${decidePalette(format).text.richLink};
+		border-bottom: 1px solid ${decidePalette(format).text.richLink};
+		text-decoration: none;
+		font-weight: normal;
 	`;
 
 const placeholderCircle = css`
@@ -28,17 +31,37 @@ interface Props {
 }
 
 export const CalloutShare = ({ format }: Props) => {
+	const [isCopied, setIsCopied] = useState(false);
+
+	const onShare = async () => {
+		const url = window.location.href;
+		if ('clipboard' in navigator) {
+			await navigator.clipboard.writeText(url);
+			setIsCopied(true);
+			setTimeout(() => setIsCopied(false), 2000);
+		}
+	};
+	if (typeof window === 'undefined' || typeof navigator === 'undefined')
+		return <></>;
+
 	return (
 		<>
 			<div css={shareCalloutStyles}>
 				<div css={placeholderCircle}>
 					<SvgShareCallout />
 				</div>
-				<div css={shareCalloutTextStyles(format)}>
+				<div css={shareCalloutTextStyles}>
 					Know others who are affected?{' '}
-					<a href="https://www.theguardian.com/tone/callout">
+					<Button
+						size="xsmall"
+						priority="subdued"
+						onClick={onShare}
+						css={shareCalloutLinkStyles(format)}
+					>
+						{' '}
 						Please share this callout.
-					</a>
+					</Button>
+					{isCopied && <em> Link copied to clipboard</em>}
 				</div>
 			</div>
 		</>
