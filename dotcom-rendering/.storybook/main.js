7a17fa72507dd79e7bdf6a22f39132b17b3fb956
Max Duval <max.duval@theguardian.com>
2022-12-19 15:33:47 +0000

    refactor(storybook): static dir config
    remove deprecated CLI option
    https://storybook.js.org/docs/react/configure/images-and-assets#serving-static-files-via-storybook-configuration

--- dotcom-rendering/.storybook/main.js
+++ dotcom-rendering/.storybook/main.js
@@ -18,6 +18,7 @@ module.exports = {
 		builder: 'webpack5',
 	},
 	stories: ['../src/**/*.stories.@(tsx)', '../stories/**/*.stories.@(tsx)'],
+	staticDirs: ['../src/static'],
 	addons: [
 		'@storybook/addon-essentials',
 		'storybook-addon-turbo-build',
