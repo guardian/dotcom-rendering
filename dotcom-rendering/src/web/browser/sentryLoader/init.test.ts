302604fafb0656887af3ece31dbb3315bd191b38
Ravi <7014230+arelra@users.noreply.github.com>
2022-12-21 19:16:42 +0000

    Reword Sentry test

--- dotcom-rendering/src/web/browser/sentryLoader/init.test.ts
+++ dotcom-rendering/src/web/browser/sentryLoader/init.test.ts
@@ -3,8 +3,8 @@ import { isSentryEnabled } from './init';
 // Stubbed to prevent parsing of __webpack_public_path__
 jest.mock('./loadSentry', () => ({ loadSentry: jest.fn() }));
 
-describe('Load Sentry when it passes loading conditions', () => {
-	it('does NOT load Sentry when enableSentryReporting switch is false', () => {
+describe('Enable Sentry when it passes loading conditions', () => {
+	it('does NOT enable Sentry when enableSentryReporting switch is false', () => {
 		expect(
 			isSentryEnabled({
 				isDev: false,
@@ -14,7 +14,7 @@ describe('Load Sentry when it passes loading conditions', () => {
 			}),
 		).toEqual(false);
 	});
-	it('does NOT load Sentry when in development environment', () => {
+	it('does NOT enable Sentry when in development environment', () => {
 		expect(
 			isSentryEnabled({
 				isDev: true,
@@ -24,7 +24,7 @@ describe('Load Sentry when it passes loading conditions', () => {
 			}),
 		).toEqual(false);
 	});
-	it('does load Sentry when the user is in the browser bundle variant test', () => {
+	it('does enable Sentry when the user is in the browser bundle variant test', () => {
 		expect(
 			isSentryEnabled({
 				isDev: false,
@@ -34,7 +34,7 @@ describe('Load Sentry when it passes loading conditions', () => {
 			}),
 		).toEqual(true);
 	});
-	it('does load Sentry for 1% of users', () => {
+	it('does enable Sentry for 1% of users', () => {
 		expect(
 			isSentryEnabled({
 				isDev: false,
