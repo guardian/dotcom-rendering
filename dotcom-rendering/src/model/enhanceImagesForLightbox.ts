2dea314c5d6ea7a03f861f30abe04cc5b146846f
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-12-30 16:40:28 +0000

    feat: We don't want to repeat the same image twice

--- dotcom-rendering/src/model/enhanceImagesForLightbox.ts
+++ dotcom-rendering/src/model/enhanceImagesForLightbox.ts
@@ -3,7 +3,7 @@ import type { CAPIElement, ImageBlockElement } from '../types/content';
 export const enhanceImagesForLightbox = (
 	blocks: Block[],
 	mainMediaElements: CAPIElement[],
-): ImageBlockElement[] | undefined => {
+): ImageBlockElement[] => {
 	const images: ImageBlockElement[] = [];
 	mainMediaElements.forEach((element) => {
 		if (
@@ -31,5 +31,22 @@ export const enhanceImagesForLightbox = (
 			}
 		});
 	});
-	return images;
+	// On some articles the main media is repeated as an element in the article body so
+	// we deduplicate the array here
+	const alreadySeen: string[] = [];
+	const uniqueImages: ImageBlockElement[] = [];
+	images.forEach((image) => {
+		const imageId = image.media.allImages[0]?.url.split('/')[3];
+		if (!imageId) {
+			// It's unlikely that we would not have an imageId here but if we do we fail
+			// open, passing the image through
+			uniqueImages.push(image);
+			return;
+		}
+		if (!alreadySeen.includes(imageId)) {
+			uniqueImages.push(image);
+			alreadySeen.push(imageId);
+		}
+	});
+	return uniqueImages;
 };
