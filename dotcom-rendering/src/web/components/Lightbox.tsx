c00f87bac70fb0ce769d223955dff2aae07151b1
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-12-31 08:33:20 +0000

    feat: Default to showing info

--- dotcom-rendering/src/web/components/Lightbox.tsx
+++ dotcom-rendering/src/web/components/Lightbox.tsx
@@ -396,7 +396,7 @@ export const Lightbox = ({ format, images }: Props) => {
 							</span>
 						</button>
 					</nav>
-					<ul className="hide-info" css={ulStyles}>
+					<ul css={ulStyles}>
 						{images.map((image, index) => {
 							// Legacy images do not have a master so we fallback to the largest available
 							const master: string | undefined =
