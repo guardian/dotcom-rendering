525a164aeb3c8f596a4cdfdf69de8e891e048f37
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-12-31 13:19:37 +0000

    merged pr 2

--- dotcom-rendering/src/web/components/CalloutEmbed/Form.stories.tsx
+++ dotcom-rendering/src/web/components/CalloutEmbed/Form.stories.tsx
@@ -1,50 +1,60 @@
-import { ArticleDesign, ArticleDisplay, ArticlePillar } from '@guardian/libs';
-import { calloutCampaign } from '../../../../fixtures/manual/calloutCampaignV2';
+import { css } from '@emotion/react';
+import {
+	calloutCampaign,
+	calloutCampaignOnlyTwoRadio,
+} from '../../../../fixtures/manual/calloutCampaign';
 import { Form } from './Form';
-
-const mockFormatNews = {
-	display: ArticleDisplay.Standard,
-	design: ArticleDesign.Standard,
-	theme: ArticlePillar.News,
-};
-
-const mockFormatOpinion = {
-	display: ArticleDisplay.Standard,
-	design: ArticleDesign.Standard,
-	theme: ArticlePillar.Opinion,
-};
 
 export default {
 	component: Form,
 	title: 'Components/Callout/Form',
 };
 
-export const News = () => {
+export const Default = () => {
 	return (
-		<>
-			<Form
-				format={mockFormatNews}
-				formFields={calloutCampaign.formFields}
-				onSubmit={() => {}}
-			/>
-			;
-		</>
+		<div
+			css={css`
+				width: 630px;
+				padding: 15px;
+			`}
+		>
+			<Form formFields={calloutCampaign.formFields} onSubmit={() => {}} />
+		</div>
 	);
 };
+Default.story = { name: 'default' };
 
-News.story = { name: 'News' };
-
-export const Opinion = () => {
+export const WithOnlyTwoRadio = () => {
 	return (
-		<>
+		<div
+			css={css`
+				width: 630px;
+				padding: 15px;
+			`}
+		>
 			<Form
-				format={mockFormatOpinion}
-				formFields={calloutCampaign.formFields}
+				formFields={calloutCampaignOnlyTwoRadio.formFields}
 				onSubmit={() => {}}
 			/>
-			;
-		</>
+		</div>
 	);
 };
+WithOnlyTwoRadio.story = { name: 'with only two radio' };
 
-Opinion.story = { name: 'Opinion' };
+export const WithError = () => {
+	return (
+		<div
+			css={css`
+				width: 630px;
+				padding: 15px;
+			`}
+		>
+			<Form
+				formFields={calloutCampaign.formFields}
+				onSubmit={() => {}}
+				error="I am a form error"
+			/>
+		</div>
+	);
+};
+WithError.story = { name: 'with errors' };
