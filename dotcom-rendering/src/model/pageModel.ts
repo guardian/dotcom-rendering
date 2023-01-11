9df16323b9c76c64253a1a2573adb8c4de6f0f83
David Blatcher <dv.bltchr@gmail.com>
2023-01-11 17:43:34 +0000

    lint page model imports

--- dotcom-rendering/src/model/pageModel.ts
+++ dotcom-rendering/src/model/pageModel.ts
@@ -1,9 +1,9 @@
-import type { NavType } from 'src/model/extract-nav';
-import type { Newsletter } from 'src/types/content';
-import type { TagType } from 'src/types/tag';
-import type { EditionId } from 'src/web/lib/edition';
+import type { NavType } from '../model/extract-nav';
 import type { ConfigType } from '../types/config';
+import type { Newsletter } from '../types/content';
 import type { FooterType } from '../types/footer';
+import type { TagType } from '../types/tag';
+import type { EditionId } from '../web/lib/edition';
 
 export type BasePageModel = {
 	webTitle: string;
