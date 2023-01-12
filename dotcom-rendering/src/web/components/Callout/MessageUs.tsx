1cf2c7b590879c0ab08fe83f65ce4f71e70c185d
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-01-12 11:20:33 +0000

    updated numbers and target/rel on message us component

--- dotcom-rendering/src/web/components/Callout/MessageUs.tsx
+++ dotcom-rendering/src/web/components/Callout/MessageUs.tsx
@@ -33,20 +33,20 @@ const WHATSAPP_GUIDANCE_URL =
 const TELEGRAM_GUIDANCE_URL =
 	'https://www.theguardian.com/info/2022/mar/15/telegram-sharing-stories-with-the-guardian';
 const SECURE_DROP_URL = 'https://www.theguardian.com/securedrop';
-const CONTACT_NUMBER = '+447766780300';
-const SIGNAL_NUMBER = '+447766780300';
+const WHATSAPP_SIGNAL_NUMBER = '+447766780300';
+const TELEGRAM_NUMBER = '+447799 322095';
 
-const OPEN_WHATSAPP_URL = `https://wa.me/${CONTACT_NUMBER}`;
-const OPEN_TELEGRAM_URL = `https://telegram.me/${CONTACT_NUMBER}`;
-const OPEN_SIGNAL_URL = `https://signal.me/#p/${SIGNAL_NUMBER}`;
+const OPEN_WHATSAPP_URL = `https://wa.me/${WHATSAPP_SIGNAL_NUMBER}`;
+const OPEN_SIGNAL_URL = `https://signal.me/#p/${WHATSAPP_SIGNAL_NUMBER}`;
+const OPEN_TELEGRAM_URL = `https://telegram.me/${TELEGRAM_NUMBER}`;
 
 export const MessageUs = () => {
 	return (
 		<div className="js-message-us-tab">
 			<p css={descriptionStyles}>
-				You can contact us on WhatsApp or Signal at {SIGNAL_NUMBER} or
-				Telegram at {CONTACT_NUMBER}. For more information, please see
-				our guidance on{' '}
+				You can contact us on WhatsApp or Signal at{' '}
+				{WHATSAPP_SIGNAL_NUMBER} or Telegram at {TELEGRAM_NUMBER}. For
+				more information, please see our guidance on{' '}
 				<a href={WHATSAPP_GUIDANCE_URL}>contacting us via WhatsApp</a>{' '}
 				and our guidance on{' '}
 				<a href={TELEGRAM_GUIDANCE_URL}>contacting us via Telegram</a>.
@@ -62,7 +62,7 @@ export const MessageUs = () => {
 				priority="primary"
 				icon={<SvgWhatsApp />}
 				href={OPEN_WHATSAPP_URL}
-				rel="noopener`"
+				target="_blank"
 				nudgeIcon={false}
 			>
 				Message us on Whatsapp
@@ -73,7 +73,7 @@ export const MessageUs = () => {
 				type="submit"
 				priority="primary"
 				href={OPEN_TELEGRAM_URL}
-				rel="noopener`"
+				target="_blank"
 			>
 				Message us on Telegram
 			</LinkButton>
@@ -88,7 +88,7 @@ export const MessageUs = () => {
 				type="submit"
 				priority="primary"
 				href={OPEN_SIGNAL_URL}
-				rel="noopener`"
+				target="_blank"
 			>
 				Message us on Signal
 			</LinkButton>
