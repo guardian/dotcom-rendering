c702cc130328999c7716bc63801a50695e18b998
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-01-13 11:39:50 +0000

    Fixed reference bug

--- dotcom-rendering/src/amp/components/Analytics.tsx
+++ dotcom-rendering/src/amp/components/Analytics.tsx
@@ -32,22 +32,22 @@ export const Analytics = ({
 		// The componentEvent is the encoded version of componentObject using https://www.urlencoder.org/
 		`<amp-analytics data-block-on-consent>
 			<script type="application/json">
-      {
-        "requests": {
-          "pageviewWithConsent": "\${z}&componentEvent=\${componentEvent}"
-        },
-        "triggers": {
-          "trackPageview": {
-            "on": "visible",
-            "request": "pageviewWithConsent",
-            "vars" : {
-              "additionalBase": "https://ophan.theguardian.com/img/2?viewId=\${ophanPageViewId}&ampViewId=\${pageViewId64}",
-						  "componentObject": {"component":{"componentType":"CONSENT","products":[],"labels":["08:\${consentState}","09:\${consentString}"]},"action":"MANAGE_CONSENT"},
-              "componentEvent": "%7B%22component%22%3A%7B%22componentType%22%3A%22CONSENT%22%2C%22products%22%3A%5B%5D%2C%22labels%22%3A%5B%2208%3A%5C%24%7B\${consentState}%7D%22%2C%2209%3A%5C%24%7B\${consentString}%7D%22%5D%7D%2C%22action%22%3A%22MANAGE_CONSENT%22%7D"
-            }
-          }
-        }
-      }
+				{
+					"requests": {
+						"base":  "https://ophan.theguardian.com/img/2?viewId=\${ophanPageViewId}&ampViewId=\${pageViewId64}",
+						"pageviewWithConsent": "\${base}&componentEvent=\${componentEvent}"
+					},
+					"triggers": {
+					"trackPageview": {
+						"on": "visible",
+						"request": "pageviewWithConsent",
+						"vars" : {
+							"componentObject": {"component":{"componentType":"CONSENT","products":[],"labels":["08:\${consentState}","09:\${consentString}"]},"action":"MANAGE_CONSENT"},
+							"componentEvent": "%7B%22component%22%3A%7B%22componentType%22%3A%22CONSENT%22%2C%22products%22%3A%5B%5D%2C%22labels%22%3A%5B%2208%3A%5C%24%7B\${consentState}%7D%22%2C%2209%3A%5C%24%7B\${consentString}%7D%22%5D%7D%2C%22action%22%3A%22MANAGE_CONSENT%22%7D"
+						}
+					}
+					}
+				}
 			</script>
 		</amp-analytics>`,
 		`<amp-analytics data-block-on-consent type="googleanalytics" id="google-analytics">
