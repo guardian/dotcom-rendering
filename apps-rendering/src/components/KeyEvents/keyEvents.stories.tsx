d48de030c2c86e698b3e59fc9bb8968c3ebdee08
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2023-01-16 18:35:38 +0000

    Refactor for the linter

--- apps-rendering/src/components/KeyEvents/keyEvents.stories.tsx
+++ apps-rendering/src/components/KeyEvents/keyEvents.stories.tsx
@@ -1,56 +1,67 @@
 // ----- Imports ----- //
 
-import { KeyEvent } from ".";
-import KeyEvents from ".";
-import { ArticleDesign, ArticleDisplay, ArticleFormat, ArticlePillar, ArticleSpecial, ArticleTheme } from "@guardian/libs";
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
+import type { ArticleFormat, ArticleTheme } from '@guardian/libs';
+import {
+	ArticleDesign,
+	ArticleDisplay,
+	ArticlePillar,
+	ArticleSpecial,
+} from '@guardian/libs';
+import type { ReactElement } from 'react';
+import type { KeyEvent } from '.';
+import KeyEvents from '.';
 
 // ----- Stories ----- //
 
 const events: KeyEvent[] = [
 	{
 		date: new Date(1 * 60 * 1000),
-		text: "Gold for Uganda",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Gold for Uganda',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 	{
 		date: new Date(2 * 60 * 1000),
-		text: "Ben Maher goes into the gold medal sport in the equestrian jumps",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Ben Maher goes into the gold medal sport in the equestrian jumps',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 	{
 		date: new Date(3 * 60 * 1000),
-		text: "Gold for Uganda",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Gold for Uganda',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 	{
 		date: new Date(5 * 60 * 1000),
-		text: "Gold for Uganda",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Gold for Uganda',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 	{
 		date: new Date(9 * 60 * 1000),
-		text: "Jodie Williams qualifies for the 400m final",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Jodie Williams qualifies for the 400m final',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 	{
 		date: new Date(15 * 60 * 1000),
-		text: "Gold for Uganda",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Gold for Uganda',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 	{
 		date: new Date(20 * 60 * 1000),
-		text: "Gold for Uganda",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Gold for Uganda',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 	{
 		date: new Date(35 * 60 * 1000),
-		text: "Gold for Uganda",
-		url: "https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy",
+		text: 'Gold for Uganda',
+		url: 'https://www.theguardian.com/environment/2021/sep/01/opec-member-urges-oil-producers-to-focus-more-on-renewable-energy',
 	},
 ];
 
-const KeyEventComp = (dark: boolean, format: ArticleFormat, title: string) => (
+const KeyEventComp = (
+	dark: boolean,
+	format: ArticleFormat,
+	title: string,
+): ReactElement => (
 	<div
 		css={css`
 			flex-grow: 1;
@@ -61,16 +72,16 @@ const KeyEventComp = (dark: boolean, format: ArticleFormat, title: string) => (
 	</div>
 );
 
-const getFormat = (theme: ArticleTheme) => {
+const getFormat = (theme: ArticleTheme): ArticleFormat => {
 	return {
 		design: ArticleDesign.Standard,
 		display: ArticleDisplay.Standard,
 		theme: theme,
-	}
-}
+	};
+};
 
-const keyEventWithTheme = (dark: boolean) => () =>
-	(
+const keyEventWithTheme = (dark: boolean): (() => ReactElement) => {
+	const KeyEvent = (): ReactElement => (
 		<div
 			css={css`
 				display: flex;
@@ -79,15 +90,26 @@ const keyEventWithTheme = (dark: boolean) => () =>
 				flex-wrap: wrap;
 			`}
 		>
-			{KeyEventComp(dark, getFormat(ArticlePillar.News), "News")}
-			{KeyEventComp(dark, getFormat(ArticlePillar.Culture), "Culture")}
-			{KeyEventComp(dark, getFormat(ArticlePillar.Lifestyle), "Lifestyle")}
-			{KeyEventComp(dark, getFormat(ArticlePillar.Opinion), "Opinion")}
-			{KeyEventComp(dark, getFormat(ArticlePillar.Sport), "Sport")}
-			{KeyEventComp(dark, getFormat(ArticleSpecial.Labs), "Labs")}
-			{KeyEventComp(dark, getFormat(ArticleSpecial.SpecialReport), "SpecialReport")}
+			{KeyEventComp(dark, getFormat(ArticlePillar.News), 'News')}
+			{KeyEventComp(dark, getFormat(ArticlePillar.Culture), 'Culture')}
+			{KeyEventComp(
+				dark,
+				getFormat(ArticlePillar.Lifestyle),
+				'Lifestyle',
+			)}
+			{KeyEventComp(dark, getFormat(ArticlePillar.Opinion), 'Opinion')}
+			{KeyEventComp(dark, getFormat(ArticlePillar.Sport), 'Sport')}
+			{KeyEventComp(dark, getFormat(ArticleSpecial.Labs), 'Labs')}
+			{KeyEventComp(
+				dark,
+				getFormat(ArticleSpecial.SpecialReport),
+				'SpecialReport',
+			)}
 		</div>
 	);
+
+	return KeyEvent;
+};
 
 const Default = keyEventWithTheme(false);
 const Dark = keyEventWithTheme(true);
@@ -96,7 +118,7 @@ const Dark = keyEventWithTheme(true);
 
 export default {
 	component: KeyEvents,
-	title: "AR/KeyEvents",
+	title: 'AR/KeyEvents',
 };
 
 export { Default, Dark };
