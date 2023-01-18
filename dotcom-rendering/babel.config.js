9e30b0cfbaaf1fdd62b96afa4882be6c286ffb4a
Ravi <7014230+arelra@users.noreply.github.com>
2023-01-18 13:28:58 +0000

    Remove polyfill-corejs from babel.config

--- dotcom-rendering/babel.config.js
+++ dotcom-rendering/babel.config.js
@@ -8,7 +8,6 @@ module.exports = {
 		'babel-plugin-px-to-rem',
 		'@babel/plugin-transform-runtime',
 		'@emotion/babel-plugin',
-		["polyfill-corejs3", { "method": "usage-global", "version": "3.20" }]
 	],
 
 	env: {
