ac90171a07f10abbb5aea60540e18fd74d4b999d
Sophie <sophiemacmillan93@gmail.com>
2023-01-19 08:29:21 +0000

    AR - Update bullet function to be darker grey for analysis pieces

--- common-rendering/src/editorialPalette/background.ts
+++ common-rendering/src/editorialPalette/background.ts
@@ -245,25 +245,31 @@ const standfirstDark = ({ design, theme }: ArticleFormat): Colour => {
 	}
 };
 
-const bullet = (format: ArticleFormat): Colour => {
-	switch (format.theme) {
-		case ArticlePillar.News:
-			return news[400];
-		case ArticlePillar.Lifestyle:
-			return lifestyle[400];
-		case ArticlePillar.Sport:
-			return sport[400];
-		case ArticlePillar.Culture:
-			return culture[400];
-		case ArticlePillar.Opinion:
-			return opinion[400];
-		case ArticleSpecial.Labs:
-			return labs[400];
-		case ArticleSpecial.SpecialReport:
-			return specialReport[400];
-		case ArticleSpecial.SpecialReportAlt:
-			return news[400];
+const bullet = (
+	format: ArticleFormat,
+	returnPillarColour: boolean = true,
+): Colour => {
+	if (returnPillarColour) {
+		switch (format.theme) {
+			case ArticlePillar.News:
+				return news[400];
+			case ArticlePillar.Lifestyle:
+				return lifestyle[400];
+			case ArticlePillar.Sport:
+				return sport[400];
+			case ArticlePillar.Culture:
+				return culture[400];
+			case ArticlePillar.Opinion:
+				return opinion[400];
+			case ArticleSpecial.Labs:
+				return labs[400];
+			case ArticleSpecial.SpecialReport:
+				return specialReport[400];
+			case ArticleSpecial.SpecialReportAlt:
+				return news[400];
+		}
 	}
+	return format.design === ArticleDesign.Analysis ? neutral[60] : neutral[86];
 };
 
 const bulletDark = (
