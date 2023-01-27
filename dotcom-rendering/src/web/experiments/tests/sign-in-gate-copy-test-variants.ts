e2c63b7f98dabfe0ff3c3dfb9601c3ddb1c21d05
Lindsey Dew <lindsey.megan.dew@gmail.com>
2023-01-27 16:43:28 +0000

    Run prettier

--- dotcom-rendering/src/web/experiments/tests/sign-in-gate-copy-test-variants.ts
+++ dotcom-rendering/src/web/experiments/tests/sign-in-gate-copy-test-variants.ts
@@ -9,7 +9,8 @@ export const signInGateCopyTest: ABTest = {
 	audience: 0.1,
 	audienceOffset: 0.0,
 	successMeasure: 'Users sign in or create a Guardian account',
-	audienceCriteria: '3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
+	audienceCriteria:
+		'3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
 	dataLinkNames: 'SignInGateCopyTest',
 	idealOutcome:
 		'Increase the number of users signed in whilst running at a reasonable scale',
