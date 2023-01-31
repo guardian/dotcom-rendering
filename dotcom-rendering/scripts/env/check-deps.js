c74578e88e6c9a53363f863511197a26a762a7f4
Max Duval <max.duval@theguardian.com>
2023-01-31 16:50:42 +0000

    fix: healthy package.json
    - missing peer dependencies are specified
    - use tilde range for type dependencies
    - enforce it with CI
    - added list of known issues
    
    - bump some dependencies:
      - dompurify to v2.4

--- dotcom-rendering/scripts/env/check-deps.js
+++ dotcom-rendering/scripts/env/check-deps.js
@@ -3,7 +3,7 @@ const pkg = require('../../package.json');
 if (pkg.devDependencies) {
 	const { warn, log } = require('./log');
 
-	warn('Don’t use devDependencies');
+	warn('Don’t use devDependencies, dependencies only!');
 	log('See https://github.com/guardian/dotcom-rendering/pull/4001');
 	process.exit(1);
 }
