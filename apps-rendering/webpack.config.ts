c351a3b87a5fe4bee11cdb1dab9e987a52e415f5
George B <705427+georgeblahblah@users.noreply.github.com>
2023-02-07 16:10:53 +0000

    use 'current' to target the current node version
    More info: https://babeljs.io/docs/en/options#targetsnode

--- apps-rendering/webpack.config.ts
+++ apps-rendering/webpack.config.ts
@@ -121,7 +121,7 @@ const serverConfig = (
 									[
 										'@babel/preset-env',
 										{
-											targets: { node: '16' },
+											targets: { node: 'current' },
 										},
 									],
 								],
