ad9c93aa6d5b9631ae0f36346395b5b882a5d0fd
Lindsey Dew <lindsey.megan.dew@gmail.com>
2023-02-08 17:09:31 +0000

    Add control in the test

--- dotcom-rendering/src/web/experiments/tests/sign-in-gate-copy-test-variants.ts
+++ dotcom-rendering/src/web/experiments/tests/sign-in-gate-copy-test-variants.ts
@@ -6,14 +6,14 @@ export const signInGateCopyTestJan2023: ABTest = {
 	expiry: '2025-12-01',
 	author: 'Lindsey Dew',
 	description: 'Test varying the copy in the call to action for sign in gate',
-	audience: 0.2,
+	audience: 0.1,
 	audienceOffset: 0.0,
 	successMeasure: 'Users sign in or create a Guardian account',
 	audienceCriteria:
 		'3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
 	dataLinkNames: 'SignInGateCopyTest',
 	idealOutcome:
-		'Increase the number of users signed in whilst running at a reasonable scale',
+		'One variants performs at least 2% better than the control and/OR 10% better than the other variant. Neither variant performs 5% worse than the control',
 	showForSensitive: false,
 	canRun: () => true,
 	variants: [
@@ -25,5 +25,9 @@ export const signInGateCopyTestJan2023: ABTest = {
 			id: 'take-a-moment',
 			test: (): void => {},
 		},
+		{
+			id: 'sign-in-copy-control',
+			test: (): void => {},
+		}
 	],
 };
