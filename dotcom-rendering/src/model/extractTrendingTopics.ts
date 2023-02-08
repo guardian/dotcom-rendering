0179e9ae7bc5060ca4bd717f227df307977d0506
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2023-02-08 15:47:53 +0000

    clean up

--- dotcom-rendering/src/model/extractTrendingTopics.ts
+++ dotcom-rendering/src/model/extractTrendingTopics.ts
@@ -1,37 +1,36 @@
 import type { FECollectionType, FEFrontCard } from '../types/front';
 import type { FETagType } from '../types/tag';
-
+//returns all the trails from the collections
 const allTrails = (collections: FECollectionType[]): FEFrontCard[] =>
 	collections.flatMap((collection) => [
 		...collection.curated,
 		...collection.backfill,
 	]);
-
-const allTrailsDeduped = (allTrailsInput: FEFrontCard[]): FEFrontCard[] =>
-	allTrailsInput.filter((trailOne) =>
-		allTrailsInput.findIndex(
+//removes duplicates from the allTrails
+const dedupeTrails = (trails: FEFrontCard[]): FEFrontCard[] =>
+	trails.filter((trailOne) =>
+		trails.findIndex(
 			(trailTwo) => trailOne.card.id === trailTwo.card.id,
 		),
 	);
-
+//type guard function that checks if a value is defined
 const notUndefined = (value: FETagType | undefined): value is FETagType =>
 	value !== undefined;
-
-const allTags = (allTrailsDedupedInput: FEFrontCard[]): FETagType[] =>
-	allTrailsDedupedInput
-		.flatMap((trail) => trail.properties.maybeContent?.tags.tags)
-		.filter(notUndefined);
-//keywords not to include
-const isKeyword = (tag: FETagType) =>
-	tag.properties.paidContentType?.includes('Keyword') ??
-	tag.properties.paidContentType?.includes('Topic') ??
-	tag.properties.tagType === 'Keyword';
-
-const desiredTags = (allTagsInput: FETagType[]): FETagType[] =>
-	allTagsInput.filter(isKeyword);
+//returns desired tags
+const getTags = (trails: FEFrontCard[]): FETagType[] =>
+  trails
+    .flatMap((trail) => trail.properties.maybeContent?.tags.tags)
+    .filter(notUndefined)
+    .filter((tag) => {
+      return (
+        tag.properties.paidContentType?.includes("Keyword") ??
+        tag.properties.paidContentType?.includes("Topic") ??
+        tag.properties.tagType === "Keyword"
+      );
+    });
 //We do not wish to include section tags that follow the pattern "UK/UK"
-const removeSection = (desiredTagsInput: FETagType[]): FETagType[] =>
-	desiredTagsInput.filter(
+const removeSection = (tags: FETagType[]): FETagType[] =>
+	tags.filter(
 		(x) => new Set(x.properties.id.split('/')).size === 2,
 	);
 //We only want the top 5 tags
@@ -54,21 +53,18 @@ const filterTopFive = (tag: FETagType[]): FETagType[] => {
 
 	return tag.filter((x) => topFiveIds.includes(x.properties.id));
 };
-//We only want unique tags
-const topFive = (removeSectionInput: FETagType[]): FETagType[] =>
-	filterTopFive(removeSectionInput);
 
-const topFiveUnique = (topFiveInput: FETagType[]): FETagType[] =>
-	[...new Set(topFiveInput.map((item) => item.properties.id))]
-		.map((id) => topFiveInput.find((item) => item.properties.id === id))
+const topFiveUnique = (tag: FETagType[]): FETagType[] =>
+	[...new Set(tag.map((item) => item.properties.id))]
+		.map((id) => tag.find((item) => item.properties.id === id))
 		.filter(notUndefined);
 
 export const extractTrendingTopics = (
 	collections: FECollectionType[],
 ): FETagType[] => {
-	const dedupedTrails = allTrailsDeduped(allTrails(collections));
+	const dedupedTrails = dedupeTrails(allTrails(collections));
 	const filteredTags = topFiveUnique(
-		topFive(removeSection(desiredTags(allTags(dedupedTrails)))),
+		filterTopFive(removeSection(getTags(dedupedTrails))),
 	);
 	return filteredTags;
 };
