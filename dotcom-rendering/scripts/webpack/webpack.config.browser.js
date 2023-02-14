9473647d23036131e6fca49737b80683ec754fc0
Olly <9575458+OllysCoding@users.noreply.github.com>
2023-02-14 16:40:51 +0000

    feat: Add 'apps' webpack build for client/browser code

--- dotcom-rendering/scripts/webpack/webpack.config.browser.js
+++ dotcom-rendering/scripts/webpack/webpack.config.browser.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const swcConfig = require('./.swcrc.json');
 const { getBrowserTargets } = require('./browser-targets');
@@ -6,7 +7,7 @@ const GuStatsReportPlugin = require('./plugins/gu-stats-report-plugin');
 const DEV = process.env.NODE_ENV === 'development';
 
 /**
- * @param {'legacy' | 'modern' | 'variant'} bundle
+ * @param {'legacy' | 'modern' | 'variant' | 'apps'} bundle
  * @returns {string}
  */
 const generateName = (bundle) => {
@@ -15,7 +16,7 @@ const generateName = (bundle) => {
 };
 
 /**
- * @param {'legacy' | 'modern' | 'variant'} bundle
+ * @param {'legacy' | 'modern' | 'variant' | 'apps'} bundle
  * @returns {string}
  */
 const getLoaders = (bundle) => {
@@ -89,11 +90,37 @@ const getLoaders = (bundle) => {
 					},
 				},
 			];
+		case 'apps':
+			return [
+				{
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-react',
+							[
+								'@babel/preset-env',
+								{
+									bugfixes: true,
+									targets: ['android >= 5', 'ios >= 12'],
+								},
+							],
+						],
+						compact: true,
+					},
+				},
+				{
+					loader: 'ts-loader',
+					options: {
+						configFile: 'tsconfig.build.json',
+						transpileOnly: true,
+					},
+				},
+			];
 	}
 };
 
 /**
- * @param {{ bundle: 'legacy' | 'modern'  | 'variant', sessionId: string }} options
+ * @param {{ bundle: 'legacy' | 'modern'  | 'variant' | 'apps', sessionId: string }} options
  * @returns {import('webpack').Configuration}
  */
 module.exports = ({ bundle, sessionId }) => ({
@@ -101,32 +128,36 @@ module.exports = ({ bundle, sessionId }) => ({
 		index: './src/web/browser/index.ts',
 		debug: './src/web/browser/debug/index.ts',
 	},
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				// our own chunk, which is shared between all bundles
-				frameworks: {
-					test: /[\\/]node_modules[\\/](preact|react-is|hoist-non-react-statistics|swr|@emotion|stylis)[\\/]/,
-					chunks: 'all',
-					name: 'frameworks',
-					enforce: true,
-				},
-				// defining our own chunk above overrides the webpack defaults,
-				// so now we restore them
-				// https://webpack.js.org/plugins/split-chunks-plugin/#optimizationsplitchunks
-				defaultVendors: {
-					test: /[\\/]node_modules[\\/]/,
-					priority: -10,
-					reuseExistingChunk: true,
-				},
-				default: {
-					minChunks: 2,
-					priority: -20,
-					reuseExistingChunk: true,
-				},
-			},
-		},
-	},
+	optimization:
+		// We don't need chunk optimization for apps as we use the 'LimitChunkCountPlugin' to produce just 1 chunk
+		bundle === 'apps'
+			? undefined
+			: {
+					splitChunks: {
+						cacheGroups: {
+							// our own chunk, which is shared between all bundles
+							frameworks: {
+								test: /[\\/]node_modules[\\/](preact|react-is|hoist-non-react-statistics|swr|@emotion|stylis)[\\/]/,
+								chunks: 'all',
+								name: 'frameworks',
+								enforce: true,
+							},
+							// defining our own chunk above overrides the webpack defaults,
+							// so now we restore them
+							// https://webpack.js.org/plugins/split-chunks-plugin/#optimizationsplitchunks
+							defaultVendors: {
+								test: /[\\/]node_modules[\\/]/,
+								priority: -10,
+								reuseExistingChunk: true,
+							},
+							default: {
+								minChunks: 2,
+								priority: -20,
+								reuseExistingChunk: true,
+							},
+						},
+					},
+			  },
 	output: {
 		filename: (data) => {
 			// We don't want to hash the debug script so it can be used in bookmarklets
@@ -140,6 +171,16 @@ module.exports = ({ bundle, sessionId }) => ({
 		new WebpackManifestPlugin({
 			fileName: `manifest.${bundle}.json`,
 		}),
+		...(bundle === 'apps'
+			? [
+					new webpack.optimize.LimitChunkCountPlugin({
+						maxChunks: 1,
+					}),
+					new webpack.ProvidePlugin({
+						Buffer: ['buffer', 'Buffer'],
+					}),
+			  ]
+			: []),
 		...(DEV
 			? [
 					new GuStatsReportPlugin({
