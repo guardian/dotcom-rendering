9cb5d3915d6f3d76fddc467b776b4c7fefc4370d
Alex Sanders <alex@sndrs.dev>
2023-02-16 12:19:34 +0000

    do not load the commercial bundle for ad free users

--- dotcom-rendering/src/web/server/frontToHtml.tsx
+++ dotcom-rendering/src/web/server/frontToHtml.tsx
@@ -1,3 +1,4 @@
+import { isString } from '@guardian/libs';
 import {
 	BUILD_VARIANT,
 	dcrJavascriptBundle,
@@ -53,14 +54,18 @@ export const frontToHtml = ({ front }: Props): string => {
 	 * Please talk to the dotcom platform team before adding more.
 	 * Scripts will be executed in the order they appear in this array
 	 */
+	const loadCommercial = !front.isAdFreeUser;
 	const scriptTags = generateScriptTags(
 		[
 			polyfillIO,
 			...getScriptArrayFromFile('frameworks.js'),
 			...getScriptArrayFromFile('index.js'),
-			process.env.COMMERCIAL_BUNDLE_URL ??
-				front.config.commercialBundleUrl,
-		].map((script) => (offerHttp3 ? getHttp3Url(script) : script)),
+			loadCommercial &&
+				(process.env.COMMERCIAL_BUNDLE_URL ??
+					front.config.commercialBundleUrl),
+		]
+			.filter(isString)
+			.map((script) => (offerHttp3 ? getHttp3Url(script) : script)),
 	);
 
 	/**
