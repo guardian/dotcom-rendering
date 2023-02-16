0e2924cd6f854d31893f15be9540c832e0e23159
Olly <9575458+OllysCoding@users.noreply.github.com>
2023-02-16 16:48:49 +0000

    refactor: CardDefault should not support avatars

--- dotcom-rendering/src/web/components/FixedLargeSlowXIV.tsx
+++ dotcom-rendering/src/web/components/FixedLargeSlowXIV.tsx
@@ -1,13 +1,14 @@
 import type { DCRContainerPalette, DCRFrontCard } from '../../types/front';
-import { shouldPadWrappableRows } from '../lib/dynamicSlices';
-import { LI } from './Card/components/LI';
-import { UL } from './Card/components/UL';
-import { FrontCard } from './FrontCard';
 import {
 	Card25Media25,
 	Card25Media25SmallHeadline,
 	Card75Media50Right,
+	CardDefault,
 } from '../lib/cardWrappers';
+import { shouldPadWrappableRows } from '../lib/dynamicSlices';
+import { LI } from './Card/components/LI';
+import { UL } from './Card/components/UL';
+import { FrontCard } from './FrontCard';
 
 type Props = {
 	trails: DCRFrontCard[];
@@ -92,14 +93,7 @@ export const FixedLargeSlowXIV = ({
 							)}
 							key={card.url}
 						>
-							<FrontCard
-								trail={card}
-								starRating={card.starRating}
-								containerPalette={containerPalette}
-								showAge={showAge}
-								headlineSize="small"
-								imageUrl={undefined}
-							/>
+							<CardDefault trail={card} />
 						</LI>
 					);
 				})}
