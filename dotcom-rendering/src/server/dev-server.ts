c7a4f00689aeb9715950f7f67df055f47dbec8a5
Max Duval <max.duval@theguardian.com>
2023-02-21 12:57:45 +0000

    fix: handle legacy URLs
    Gracefully handle the ways things used
    to be done with a redirect.

--- dotcom-rendering/src/server/dev-server.ts
+++ dotcom-rendering/src/server/dev-server.ts
@@ -24,6 +24,13 @@ export const devServer = (): Handler => {
 	return (req, res, next) => {
 		const path = req.path.split('/')[1];
 
+		// handle urls with the ?url=… query param
+		const url = new URL(req.url, `http://localhost:3030/`);
+		const sourceUrl = url.searchParams.get('url');
+		if (sourceUrl !== null) {
+			return res.redirect(url.pathname + '/' + sourceUrl);
+		}
+
 		switch (path) {
 			case 'Article':
 				return handleArticle(req, res, next);
