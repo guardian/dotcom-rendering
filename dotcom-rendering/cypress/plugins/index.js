c44444166550a996ac661f2643051b2ab0cfa02a
Lindsey Dew <lindsey.megan.dew@gmail.com>
2023-02-22 12:11:56 +0000

    Remove the need to import src code to cypress tests

--- dotcom-rendering/cypress/plugins/index.js
+++ dotcom-rendering/cypress/plugins/index.js
@@ -7,7 +7,6 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-import path from 'path';
 
 const webpackPreprocessor = require('cypress-webpack-preprocessor-v5');
 
@@ -15,31 +14,9 @@ module.exports = (on, config) => {
 	config.env = { ...config.env, ...process.env };
 
 	const webpackConfig = webpackPreprocessor.defaultOptions;
-	// Adding this here so that we can use the `makeWindowGuardian` method
-	// to override switches configuration
-	webpackConfig.webpackOptions.resolve = {
-		extensions: ['.ts', '.js'],
-		alias: {
-			windowGuardian$: path.resolve(
-				__dirname,
-				`../../src/model/window-guardian.ts`,
-			),
-		},
-	};
-	const rules = webpackConfig.webpackOptions.module.rules;
-	rules[0].exclude =
+	webpackConfig.webpackOptions.module.rules[0].exclude =
 		require('../../scripts/webpack/webpack.config.browser').babelExclude;
 
-	rules.push({
-		test: /\.ts$/,
-		exclude: ['/node_modules/'],
-		loader: 'ts-loader',
-		options: {
-			compilerOptions: {
-				noEmit: false,
-			},
-		},
-	});
 	on('file:preprocessor', webpackPreprocessor(webpackConfig));
 	return config;
 };
