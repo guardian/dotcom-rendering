54139253f5c73b34071e6b292c6d89d788680233
George B <705427+georgeblahblah@users.noreply.github.com>
2023-02-23 11:03:47 +0000

    add the client scripts paths to the `Link` header

--- dotcom-rendering/src/apps/server/articleToHtml.tsx
+++ dotcom-rendering/src/apps/server/articleToHtml.tsx
@@ -1,26 +1,29 @@
-import { generateScriptTags, getScriptsFromManifest } from '../../lib/assets';
+import { getScriptsFromManifest } from '../../lib/assets';
 import { FEArticleType } from '../../types/frontend';
 import { renderToStringWithEmotion } from '../../web/lib/emotion';
 import { pageTemplate } from './pageTemplate';
 
-export const articleToHtml = (article: FEArticleType): string => {
+export const articleToHtml = (
+	article: FEArticleType,
+): {
+	clientScripts: string[];
+	html: string;
+} => {
 	const { html, extractedCss } = renderToStringWithEmotion(<></>);
 
 	const getScriptArrayFromFile = getScriptsFromManifest({
 		platform: 'apps',
 	});
-	const polyfillIO =
-		'https://assets.guim.co.uk/polyfill.io/v3/polyfill.min.js?rum=0&features=es6,es7,es2017,es2018,es2019,default-3.6,HTMLPictureElement,IntersectionObserver,IntersectionObserverEntry,URLSearchParams,fetch,NodeList.prototype.forEach,navigator.sendBeacon,performance.now,Promise.allSettled&flags=gated&callback=guardianPolyfilled&unknown=polyfill&cacheClear=1';
 
-	const scriptTags = generateScriptTags([
-		polyfillIO,
-		...getScriptArrayFromFile('index.js'),
-	]);
-
-	return pageTemplate({
+	const clientScripts = getScriptArrayFromFile('index.js');
+	const renderedPage = pageTemplate({
 		css: extractedCss,
 		html,
 		title: article.webTitle,
-		scriptTags,
+		clientScripts,
 	});
+	return {
+		clientScripts,
+		html: renderedPage,
+	};
 };
--- dotcom-rendering/src/apps/server/pageTemplate.ts
+++ dotcom-rendering/src/apps/server/pageTemplate.ts
@@ -1,26 +1,30 @@
-
 import { resets } from '@guardian/source-foundations';
 
 export const pageTemplate = ({
 	css,
 	html,
 	title = 'The Guardian',
-	scriptTags,
+	clientScripts,
 }: {
-	css: string,
-	html: string,
-	title?: string,
-	scriptTags: string[],
+	css: string;
+	html: string;
+	title?: string;
+	clientScripts: string[];
 }): string => {
-	const androidFontSizeStylesheetPath = '/fontSize.css'
+	const favicon =
+		process.env.NODE_ENV === 'production'
+			? 'favicon-32x32.ico'
+			: 'favicon-32x32-dev-yellow.ico';
+	const androidFontSizeStylesheetPath = '/fontSize.css';
 	return `<!doctype html>
 		<html lang="en">
 			<head>
 				<title>${title}</title>
 
 				<meta charset="utf-8">
-				<meta name="description" content=${title} />
+				<meta name="description" content="${title}" />
 
+                <link rel="icon" href="https://static.guim.co.uk/images/${favicon}">
 				<link rel="stylesheet" type="text/css" href="${androidFontSizeStylesheetPath}" />
 
                 <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
@@ -28,7 +32,7 @@ export const pageTemplate = ({
 				<meta id="twitter-theme" name="twitter:widgets:theme" content="light" />
 				<meta name="twitter:dnt" content="on" />
 
-				${scriptTags.join('\n')}
+				${clientScripts.map((cs) => `<script src="${cs}"></script>`).join('\n')}
                 <style>${resets.resetCSS}</style>
 				${css}
 			</head>
@@ -36,5 +40,5 @@ export const pageTemplate = ({
 				${html}
 			</body>
 		</html>
-	`
-}
+	`;
+};
