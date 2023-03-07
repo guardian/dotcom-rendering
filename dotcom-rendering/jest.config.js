501f8bebc804139c2624715cac83504e3d038ba3
Ravi <7014230+arelra@users.noreply.github.com>
2023-03-07 00:34:29 +0000

    Create separate jest.config.js

---
+++ dotcom-rendering/jest.config.js
@@ -0,0 +1,37 @@
+const swcConfig = require('./scripts/webpack/.swcrc.json');
+
+module.exports = {
+	"preset": "ts-jest/presets/js-with-ts",
+	"testEnvironment": "jest-environment-jsdom-sixteen",
+	"moduleFileExtensions": [
+		"ts",
+		"tsx",
+		"js"
+	],
+	"transform": {
+		"^.+\\.(ts|tsx)$": ["@swc/jest", swcConfig]
+	},
+	"globals": {
+		"ts-jest": {
+			"diagnostics": false,
+			"tsConfig": "tsconfig.test.json"
+		}
+	},
+	"testMatch": [
+		"**/*.test.+(ts|tsx|js)"
+	],
+	"setupFilesAfterEnv": [
+		"<rootDir>/scripts/jest/setup.ts"
+	],
+	"moduleNameMapper": {
+		"^svgs/(.*)$": "<rootDir>/__mocks__/svgMock.tsx",
+		"^(.*)\\.svg$": "<rootDir>/__mocks__/svgMock.tsx"
+	},
+	"testResultsProcessor": "jest-teamcity-reporter",
+	"transformIgnorePatterns": [
+		"/node_modules/(?!@guardian/)"
+	],
+	"collectCoverageFrom": [
+		"src/**/*.{ts,tsx}"
+	]
+}
