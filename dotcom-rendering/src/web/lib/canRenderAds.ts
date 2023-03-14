94e8275acddab4b16b07cf986f8b13c0fcfe530b
Alex Sanders <alex@sndrs.dev>
2023-03-14 15:57:57 +0000

    create `canRenderAds` lib

---
+++ dotcom-rendering/src/web/lib/canRenderAds.ts
@@ -0,0 +1,25 @@
+import type { DCRFrontType } from '../../types/front';
+import type { FEArticleType } from '../../types/frontend';
+
+/**
+ * Checks the page for a number of conditions that should
+ * prevent ads from being displayed.
+ */
+export const canRenderAds = (
+	pageData: FEArticleType | DCRFrontType,
+): boolean => {
+	if (pageData.isAdFreeUser) {
+		return false;
+	}
+
+	// DCRFrontType doesn't have a shouldHideAds property
+	if ('shouldHideAds' in pageData && pageData.shouldHideAds) {
+		return false;
+	}
+
+	if (pageData.config.abTests.poorDeviceConnectivityVariant === 'variant') {
+		return false;
+	}
+
+	return true;
+};
