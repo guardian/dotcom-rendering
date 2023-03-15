1feddb76b78e3658c07b9a1510227500f9b1097f
Joe Cowton <joe.cowton@guardian.co.uk>
2023-03-15 17:49:57 +0000

    add missing import

--- dotcom-rendering/src/web/components/CalloutEmbed/Form.test.tsx
+++ dotcom-rendering/src/web/components/CalloutEmbed/Form.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import type {
 	CampaignFieldCheckbox,
