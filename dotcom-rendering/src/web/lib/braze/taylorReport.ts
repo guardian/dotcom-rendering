b349b1764c1d516fe9265a14d98b9297a7e4c3e9
Tom Forbes <tom.forbes@theguardian.com>
2023-03-16 14:14:49 +0000

    switch

--- dotcom-rendering/src/web/lib/braze/taylorReport.ts
+++ dotcom-rendering/src/web/lib/braze/taylorReport.ts
@@ -1,12 +1,7 @@
 import { TagType } from '../../../types/tag';
 
-const taylorReportIsLive = (now: Date): boolean => {
-	const start = Date.parse('2023-03-24');
-	const end = Date.parse('2023-04-05');
-	return now.valueOf() >= start && now.valueOf() < end;
-};
-
 const tagId = ''; //TODO
 
 export const suppressForTaylorReport = (tags: TagType[]): boolean =>
-	taylorReportIsLive(new Date()) && !!tags.find((tag) => tag.id === tagId);
+	window.guardian.config.switches.brazeTaylorReport === true &&
+	!!tags.find((tag) => tag.id === tagId);
