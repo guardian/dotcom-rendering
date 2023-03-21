a9ff4bc13edec95f8b42a52ba811f0b23c9eee84
Tom Forbes <tom.forbes@theguardian.com>
2023-03-21 15:13:45 +0000

    fix tag check

--- dotcom-rendering/src/web/lib/braze/taylorReport.ts
+++ dotcom-rendering/src/web/lib/braze/taylorReport.ts
@@ -4,4 +4,4 @@ const tagId = 'environment/environment'; //TODO
 
 export const suppressForTaylorReport = (tags: TagType[]): boolean =>
 	window.guardian.config.switches.brazeTaylorReport === true &&
-	!!tags.find((tag) => tag.id === tagId);
+	!tags.find((tag) => tag.id === tagId);
