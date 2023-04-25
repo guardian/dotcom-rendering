ceadb64d6b450a147c0e66021b0687ca6dc8ffdf
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-04-25 16:11:30 +0100

    ensure ad content is centred

--- dotcom-rendering/src/web/components/ArticleContainer.tsx
+++ dotcom-rendering/src/web/components/ArticleContainer.tsx
@@ -95,6 +95,12 @@ const adStyles = css`
 				/* Ensures slots do not take on 100% of the container height, allowing them to be sticky in containers */
 				align-self: flex-start;
 			}
+
+			/* ensure that the ad slot is centred */
+			.ad-slot__content {
+				margin-left: auto;
+				margin-right: auto;
+			}
 		}
 
 		.ad-slot--interscroller {
