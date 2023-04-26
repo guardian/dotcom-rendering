a2287a8ea0cc61cd5feab597e2a8cfc3af55cc9f
Alex Sanders <alex@sndrs.dev>
2023-04-26 17:46:47 +0100

    don’t do this is we can’t use css vars
    just don’t even get near animating body opacity

--- dotcom-rendering/src/web/bork/fcp.ts
+++ dotcom-rendering/src/web/bork/fcp.ts
@@ -2,7 +2,10 @@ export const fcp = (): void => {
 	try {
 		const hashKey = 'bork-fcp-';
 		const hash = window.location.hash.substring(1);
-		if (hash.startsWith(hashKey)) {
+		if (
+			hash.startsWith(hashKey) &&
+			CSS.supports('animation-duration', 'var(--fake-var)')
+		) {
 			const root = document.documentElement;
 			root.style.setProperty(
 				'--bork-fcp-amount',
