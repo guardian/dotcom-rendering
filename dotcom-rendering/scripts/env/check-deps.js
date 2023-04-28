02e8072c8223e5a9321663748ad95f9357b65ac5
Max Duval <max.duval@theguardian.com>
2023-04-28 17:35:15 +0100

    feat: pin packages & enforce it
    `package.json` is the human-readable document, that should
    unequivocally indicate which version of a dependency is installed.
    
    https://docs.renovatebot.com/dependency-pinning/#why-pin-dependencies

--- dotcom-rendering/scripts/env/check-deps.js
+++ dotcom-rendering/scripts/env/check-deps.js
@@ -1,9 +1,33 @@
+const fs = require('fs');
+const lockfile = require('@yarnpkg/lockfile');
 const pkg = require('../../package.json');
+const { warn, log } = require('./log');
 
 if (pkg.devDependencies) {
-	const { warn, log } = require('./log');
-
 	warn('Don’t use devDependencies');
 	log('See https://github.com/guardian/dotcom-rendering/pull/4001');
 	process.exit(1);
 }
+
+/** @type {{ object: import('@types/yarnpkg__lockfile').LockFileObject }}*/
+const { object: json } = lockfile.parse(
+	fs.readFileSync('../yarn.lock', 'utf8'),
+);
+
+const knownNonSemver = /** @type {const} */ ([
+	'https://github.com/guardian/babel-plugin-px-to-rem#v0.1.0',
+]);
+
+const mismatches = Object.entries(pkg.dependencies)
+	.filter(([name, version]) => {
+		const pinned = json[name + '@' + version]?.version;
+		return version !== pinned;
+	})
+	.filter(([, version]) => !knownNonSemver.includes(version));
+
+if (mismatches.length) warn('All dependencies should be pinned');
+for (const [name, version] of mismatches) {
+	warn(`You must fix: ${name}@${String(version)}`);
+}
+
+if (mismatches.length !== 0) process.exit(mismatches.length);
