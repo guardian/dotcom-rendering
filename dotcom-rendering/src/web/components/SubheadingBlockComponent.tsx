076bbee42e739676826113232a7cc115b9e14327
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2023-05-03 15:27:00 +0100

    Refactor out rewrappedcomponent

--- dotcom-rendering/src/web/components/SubheadingBlockComponent.tsx
+++ dotcom-rendering/src/web/components/SubheadingBlockComponent.tsx
@@ -1,19 +1,36 @@
-import { unwrapHtml } from '../../model/unwrapHtml';
-import { RewrappedComponent } from './RewrappedComponent';
+import { jsx } from '@emotion/react';
+import { JSDOM } from 'jsdom';
+import type { ReactNode } from 'react';
+import { Fragment } from 'react';
 
 type Props = { html: string };
 
-export const SubheadingBlockComponent = ({ html }: Props) => {
-	const { willUnwrap: isUnwrapped, unwrappedHtml } = unwrapHtml({
-		fixes: [{ prefix: '<h2>', suffix: '</h2>' }],
-		html,
-	});
+const parseHtml = (html: string): DocumentFragment => JSDOM.fragment(html);
 
-	return (
-		<RewrappedComponent
-			isUnwrapped={isUnwrapped}
-			html={unwrappedHtml}
-			tagName="h2"
-		/>
-	);
+function isElement(node: Node): node is Element {
+	return node.nodeType === 1;
+}
+
+const getAttrs = (node: Node): NamedNodeMap | undefined =>
+	isElement(node) ? node.attributes : undefined;
+
+const buildElementTree = (node: Node): ReactNode => {
+	if (node.nodeType === node.ELEMENT_NODE) {
+		const element = node as Element;
+		return jsx(element.tagName.toLowerCase(), {
+			id: getAttrs(node)?.getNamedItem('id')?.value,
+			children: Array.from(node.childNodes).map(buildElementTree),
+		});
+	} else if (node.nodeType === node.TEXT_NODE) {
+		return node.textContent;
+	} else {
+		return null;
+	}
+};
+
+export const SubheadingBlockComponent = ({ html }: Props) => {
+	const fragment = parseHtml(html);
+	return jsx(Fragment, {
+		children: Array.from(fragment.childNodes).map(buildElementTree),
+	});
 };
