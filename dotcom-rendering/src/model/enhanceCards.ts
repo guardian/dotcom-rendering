a93cd219eeb1c74c6a5b93b974b96a3d86c1d6e1
Olly <9575458+OllysCoding@users.noreply.github.com>
2023-05-18 13:15:49 +0100

    fix: Add support for kickers on index pages

--- dotcom-rendering/src/model/enhanceCards.ts
+++ dotcom-rendering/src/model/enhanceCards.ts
@@ -134,9 +134,14 @@ const decideMediaType = (format: ArticleFormat): MediaType | undefined => {
 };
 
 const decideKicker = (trail: FEFrontCard) => {
-	return trail.properties.isBreaking
-		? 'Breaking news'
-		: trail.header.kicker?.item?.properties.kickerText;
+	if (trail.properties.isBreaking) {
+		return 'Breaking news';
+	}
+
+	return (
+		trail.header.kicker?.item?.properties.kickerText ??
+		trail.header.seriesOrBlogKicker?.name
+	);
 };
 
 const enhanceTags = (tags: FETagType[]): TagType[] => {
