2ea70b66c76cad3f4f3c485d25e58f5f89f14236
Alina Boghiu <alina.boghiu@gmail.com>
2023-05-22 14:22:28 +0100

    missed allowing for optional overrides in a couple of places

--- dotcom-rendering/src/web/components/MiniCard.tsx
+++ dotcom-rendering/src/web/components/MiniCard.tsx
@@ -20,7 +20,7 @@ const linkStyles = css`
 `;
 
 const linkOverrideStyles = (containerOverrides?: ContainerOverrides) => css`
-	${containerOverrides && `color: ${containerOverrides.text.cardHeadline}`}
+	${containerOverrides && `color: ${containerOverrides.text?.cardHeadline}`}
 `;
 
 type Props = {
