54615470ccd016ae960e9c8e7d17c48303c96100
Alina Boghiu <alina.boghiu@gmail.com>
2023-05-29 17:12:15 +0100

    add missing attributes in LabSection story

--- dotcom-rendering/src/web/components/LabsSection.stories.tsx
+++ dotcom-rendering/src/web/components/LabsSection.stories.tsx
@@ -56,6 +56,9 @@ export const WithoutBadgeStory = () => {
 			sectionId={''}
 			collectionId={''}
 			pageId={''}
+			ophanComponentName={''}
+			ophanComponentLink={''}
+			containerPalette={'EventPalette'}
 		>
 			<Placeholder />
 		</LabsSection>
