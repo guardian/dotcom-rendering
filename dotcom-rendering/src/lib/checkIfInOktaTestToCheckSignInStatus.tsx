3a69b743b729d7f82cd73ecbb2de21ec9ea7b402
Georges Lebreton <georges.lebreton@guardian.co.uk>
2023-05-31 18:38:05 +0100

    added window & document undefined checks

--- dotcom-rendering/src/lib/checkIfInOktaTestToCheckSignInStatus.tsx
+++ dotcom-rendering/src/lib/checkIfInOktaTestToCheckSignInStatus.tsx
@@ -2,9 +2,13 @@ import { getCookie } from '@guardian/libs';
 import { CheckUserSignInStatus } from './identity';
 
 export function checkIfInOktaTestToCheckSignInStatus() {
-	const isInOktaExperiment = window.guardian.config.switches.okta == true;
-	const isSignedIn = isInOktaExperiment
-		? CheckUserSignInStatus()
-		: !!getCookie({ name: 'GU_U', shouldMemoize: true });
-	return isSignedIn;
+	if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+		const isInOktaExperiment = window.guardian.config.switches.okta == true;
+		const isSignedIn = isInOktaExperiment
+			? CheckUserSignInStatus()
+			: !!getCookie({ name: 'GU_U', shouldMemoize: true });
+		return isSignedIn;
+	} else {
+		return false;
+	}
 }
