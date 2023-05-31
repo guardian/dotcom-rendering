3bf2a8fee44fba40c55c061a10d68aa2a3e37407
Georges Lebreton <georges.lebreton@guardian.co.uk>
2023-05-31 16:27:57 +0100

    further tweak to check if in okta function

--- dotcom-rendering/src/lib/checkIfInOktaTestToCheckSignInStatus.tsx
+++ dotcom-rendering/src/lib/checkIfInOktaTestToCheckSignInStatus.tsx
@@ -1,7 +1,7 @@
 import { getCookie } from '@guardian/libs';
 import { CheckUserSignInStatus } from './identity';
 
-export function checkIfInOkta() {
+export function checkIfInOktaTestToCheckSignInStatus() {
 	const isInOktaExperiment = window.guardian.config.switches.okta == true;
 	const isSignedIn = isInOktaExperiment
 		? CheckUserSignInStatus()
