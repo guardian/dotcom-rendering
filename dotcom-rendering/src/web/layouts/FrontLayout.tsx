16bcedc31a9e3424c626f7281b4dfbe1e9284c97
Ioanna Kokkini <ioannakok@hotmail.com>
2023-05-31 19:44:29 +0100

    Do not show side borders in thrashers
    This achieves parity with frontend and does not mix with the thrashers' own customised borders.

--- dotcom-rendering/src/web/layouts/FrontLayout.tsx
+++ dotcom-rendering/src/web/layouts/FrontLayout.tsx
@@ -282,7 +282,7 @@ export const FrontLayout = ({ front, NAV }: Props) => {
 											fullWidth={true}
 											padSides={false}
 											showTopBorder={false}
-											showSideBorders={true}
+											showSideBorders={false}
 											ophanComponentLink={
 												ophanComponentLink
 											}
