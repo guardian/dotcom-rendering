39bbaa11ccc2c03fe713c438e8560a294aa5a78d
Georges Lebreton <georges.lebreton@guardian.co.uk>
2023-06-01 16:30:40 +0100

    trying magic comment for dynamic okta import

--- dotcom-rendering/src/lib/checkIfInOktaTestToCheckSignInStatus.tsx
+++ dotcom-rendering/src/lib/checkIfInOktaTestToCheckSignInStatus.tsx
@@ -1,15 +1,26 @@
 import { getCookie } from '@guardian/libs';
 import { CheckUserSignInStatus } from './identity';
 
+function useOkta() {
+	import(
+		/* webpackChunkName: "3044.modern.75ac511357ae17baae47.js" */ '@okta/okta-auth-js'
+	);
+	const isSignedIn = CheckUserSignInStatus();
+	return isSignedIn;
+}
+
+function useCurrent() {
+	const isSignedIn = !!getCookie({ name: 'GU_U', shouldMemoize: true });
+	return isSignedIn;
+}
+
 export function checkIfInOktaTestToCheckSignInStatus() {
 	if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-		const isInOktaExperiment = getCookie({
+		const isInOktaExperiment = !!getCookie({
 			name: 'X-GU-Experiment-0perc-E=true',
 			shouldMemoize: true,
 		});
-		const isSignedIn = isInOktaExperiment
-			? CheckUserSignInStatus()
-			: !!getCookie({ name: 'GU_U', shouldMemoize: true });
+		const isSignedIn = isInOktaExperiment ? useOkta() : useCurrent();
 		return isSignedIn;
 	} else {
 		return false;
