a85297f09d78fb5c1545f545d4042f42e1765875
Georges Lebreton <georges.lebreton@guardian.co.uk>
2023-06-12 12:44:14 +0100

    using identityauth library

--- dotcom-rendering/src/lib/identity.ts
+++ dotcom-rendering/src/lib/identity.ts
@@ -1,148 +1,53 @@
-import { getCookie } from '@guardian/libs';
-import type { AuthState, OktaAuthOptions } from '@okta/okta-auth-js';
-import { OktaAuth } from '@okta/okta-auth-js';
+import { IdentityAuth } from '@guardian/identity-auth';
 
 console.log('identity loaded');
 
-// interface OktaAuthContextType {
-// 	oktaAuth: OktaAuth;
-// 	authState: AuthState | null;
-// }
-
 function getStage() {
-	if (typeof window !== 'undefined' && !window.guardian.config.isDev) {
+	if (!window.guardian.config.isDev) {
+		console.log('here');
 		return window.guardian.config.stage;
 	} else return 'DEV';
 }
 
-const stage = getStage();
-
-console.log('stage', stage);
-
-const CLIENT_ID =
-	stage === 'PROD' ? '0oa79m1fmgzrtaHc1417' : '0oa53x6k5wGYXOGzm0x7';
-
-const ISSUER =
+const determineISSUER = (stage: StageType) =>
 	stage === 'PROD'
 		? 'https://profile.theguardian.com/oauth2/aus3xgj525jYQRowl417'
 		: 'https://profile.code.dev-theguardian.com/oauth2/aus3v9gla95Toj0EE0x7';
+const determineCLIENT_ID = (stage: StageType) =>
+	stage === 'PROD' ? '0oa79m1fmgzrtaHc1417' : '0oa53x6k5wGYXOGzm0x7';
 
-function determineREDIRECT_URI() {
+const determineREDIRECT_URI = (stage: StageType) => {
 	switch (stage) {
 		case 'PROD':
 			return 'https://www.theguardian.com/ ';
 		case 'CODE':
 			return 'https://m.code.dev-theguardian.com/';
 		case 'DEV':
+		default:
 			return 'http://localhost:3030/';
 	}
-}
-
-const REDIRECT_URI = determineREDIRECT_URI();
-console.log('REDIRECT_URI', REDIRECT_URI);
-
-const config: OktaAuthOptions = {
-	clientId: CLIENT_ID,
-	issuer: ISSUER,
-	redirectUri: REDIRECT_URI,
-	scopes: ['openid', 'profile', 'email'],
-	pkce: true,
 };
 
-const oktaAuthInit = new OktaAuth(config);
+let identityAuth: IdentityAuth | undefined = undefined;
 
-async function getSignedInStatus(oktaAuth: OktaAuth, authState: AuthState) {
-	console.log('using isLoggedIn function');
-	if (ifSignedOutClearTokens(oktaAuth)) {
-		console.log('signed out and cleared tokens');
-		return false;
+function getIdentityAuth() {
+	if (identityAuth === undefined) {
+		const stage = getStage();
+		console.log('stage', stage);
+
+		identityAuth = new IdentityAuth({
+			issuer: determineISSUER(stage),
+			clientId: determineCLIENT_ID(stage),
+			redirectUri: determineREDIRECT_URI(stage),
+			scopes: ['openid', 'profile', 'email'], // and any other scopes you need
+		});
 	}
-	return await checkIfSignedIn(authState, oktaAuth);
+	return identityAuth;
 }
-
-function ifSignedOutClearTokens(oktaAuth: OktaAuth) {
-	if (getCookie({ name: 'GU_SO' })) {
-		oktaAuth.tokenManager.clear();
-		return true;
-	}
-	return false;
-}
-
-async function checkIfSignedIn(authState: AuthState, oktaAuth: OktaAuth) {
-	if (authState.isAuthenticated) {
-		console.log('is authenticated with tokens');
-		return true;
-	}
-	if (getCookie({ name: 'GU_U' })) {
-		console.log('has GU_U cookie');
-		try {
-			const res = await oktaAuth.token.getWithoutPrompt();
-			console.log('got refreshed tokens');
-			console.log(res.tokens);
-			oktaAuth.tokenManager.setTokens(res.tokens);
-			return true;
-		} catch (err: unknown) {
-			console.log('reached token error');
-			console.error(err);
-			return false;
-		}
-	}
-	return false;
-}
-
-const initOktaAuth = async (): Promise<OktaAuth> => {
-	console.log('useOktaAuth called');
-	const oktaAuth = oktaAuthInit;
-	await oktaAuth.start();
-	return oktaAuth;
-
-	// const [authState, setAuthState] = React.useState<AuthState | null>(() => {
-	// 	return oktaAuth.authStateManager.getAuthState();
-	// });
-	// React.useEffect(() => {
-	// 	async function oktaAuthState() {
-	// 		// Update Security provider with latest authState
-	// 		const currentAuthState = oktaAuth.authStateManager.getAuthState();
-	// 		if (currentAuthState !== authState) {
-	// 			setAuthState(currentAuthState);
-	// 		}
-	// 		const handler = (newAuthState: AuthState) => {
-	// 			setAuthState(newAuthState);
-	// 		};
-	// 		oktaAuth.authStateManager.subscribe(handler);
-
-	// 		// Trigger an initial change event to make sure authState is latest
-	// 		await oktaAuth.start();
-
-	// 		return () => {
-	// 			oktaAuth.authStateManager.unsubscribe(handler);
-	// 		};
-	// 	}
-	// 	oktaAuthState().catch(() => {
-	// 		console.error('error in oktaAuthState');
-	// 	});
-	// }, [oktaAuth, authState]);
-	// return { oktaAuth, authState };
-};
 
 export async function isSignedInWithOkta(): Promise<boolean> {
 	console.info('using okta for sign in check');
-	const oktaAuth = await initOktaAuth();
-	const authState = oktaAuth.authStateManager.getAuthState();
-	return authState ? getSignedInStatus(oktaAuth, authState) : false;
 
-	// const [isSignedIn, setIsSignedIn] = useState(false);
-	// useEffect(() => {
-	// 	(async () => {
-	// 		if (authState) {
-	// 			setIsSignedIn(await getSignedInStatus(oktaAuth, authState));
-	// 		}
-	// 	})().catch(() => {
-	// 		console.error('error in oktaAuthState');
-	// 	});
-	// }, [authState, oktaAuth]);
-
-	// console.log('CheckUserSignInStatus');
-	// console.log(isSignedIn);
-	// return isSignedIn;
+	const isLoggedIn = await getIdentityAuth().isSignedIn();
+	return isLoggedIn;
 }
