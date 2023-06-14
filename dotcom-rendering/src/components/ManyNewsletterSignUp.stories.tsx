7702f8a4eaf64cf12b4249486356441074e46a51
David Blatcher <dv.bltchr@gmail.com>
2023-06-14 09:25:46 +0100

    add story

---
+++ dotcom-rendering/src/components/ManyNewsletterSignUp.stories.tsx
@@ -0,0 +1,50 @@
+import { GroupedNewslettersList } from './GroupedNewsletterList';
+import { ManyNewsletterSignUp } from './ManyNewsletterSignUp.importable';
+
+export const Default = () => {
+	return (
+		<>
+			<GroupedNewslettersList
+				groupedNewsletters={{
+					groups: [
+						{
+							title: 'test',
+							newsletters: [
+								{
+									listId: 1,
+									identityName: 'test-1',
+									name: 'test newsletter 1',
+									description:
+										'Select at least one newsletter to make the ManyNewsletterSignUp appear',
+									frequency: 'weekly',
+									theme: 'news',
+									group: '',
+									successDescription: 'not used',
+								},
+								{
+									listId: 2,
+									identityName: 'test-2',
+									name: 'test newsletter 2',
+									description:
+										'Select at least one newsletter to make the ManyNewsletterSignUp appear',
+									frequency: 'monthly',
+									theme: 'news',
+									group: '',
+									successDescription: 'not used',
+								},
+							],
+						},
+					],
+				}}
+			/>
+			<ManyNewsletterSignUp />
+		</>
+	);
+};
+Default.storyName = 'ManyNewsletterSignUp - one example';
+
+export default {
+	component: Default,
+	title: 'Components/ManyNewsletterSignUp',
+	chromatic: { diffThreshold: 0.7 },
+};
