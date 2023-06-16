a8da9af8418077e7f04a324686b457ed826a3e2c
Charlotte <43961396+cemms1@users.noreply.github.com>
2023-06-16 12:46:54 +0100

    feat: add badges and badge types from frontend

--- dotcom-rendering/src/types/badge.ts
+++ dotcom-rendering/src/types/badge.ts
@@ -1,6 +1,18 @@
 export interface DCRBadgeType {
 	imageSrc: string;
+	/** Href can be an internal path including a series tag or a link to an external sponsor page */
 	href: string;
+}
+
+export interface FEBadgeType {
+	seriesTag: string;
+	imageSrc: string;
+}
+
+export interface FESpecialBadgeType {
+	salt: string;
+	hashedTag: string;
+	imageSrc: string;
 }
 
 export interface FEArticleBadgeType {
