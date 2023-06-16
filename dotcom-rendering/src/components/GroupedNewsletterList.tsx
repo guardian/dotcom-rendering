0a73ed57e313e4bf2ff70309bc045c4092fccd62
David Blatcher <30567854+dblatcher@users.noreply.github.com>
2023-06-16 10:46:51 +0100

    Newsletter page select and submit components (#7916)
    * set up grouping enhancement mechanism
    
    * can group based on static data
    
    * more efficient default grouping
    
    * add US grouping
    
    * add AU grouping
    
    * use switch on editionId
    
    * delete the old list component
    
    * temporary styleing to indicate the general shape of the page
    
    * update validation and enhancement
    
    * toggle mechanism with minimal hydration
    
    * style buttons to respond to state
    
    * align buttons to the bottom of the card
    
    * simulate sign up behaviour
    
    * css format and props name
    
    * use ts file instead of json for defining the static group
    
    * fixed position section and remove all button
    
    * state for email address
    
    * refactor the form to a sub-component
    
    * responsive form styling
    
    * use sticky positioning if available
    
    * desktop and mobile versions of the clear button
    
    * remove privacy message from the heading
    
    * add story
    
    * placeholder aync function to stand in for api request
    
    * button wrapper names were the wrong way round
    
    * responsive caption component
    
    * use Flex component
    
    * button styling
    
    * defined style for succes message
    
    * refactor form component to own file
    
    * button aria label responds to state changes
    
    * aria mark-up for form
    
    * correct story label

--- dotcom-rendering/src/components/GroupedNewsletterList.tsx
+++ dotcom-rendering/src/components/GroupedNewsletterList.tsx
@@ -4,6 +4,11 @@ import {
 	palette,
 	space,
 } from '@guardian/source-foundations';
+import {
+	Button,
+	SvgCheckmark,
+	SvgPlus,
+} from '@guardian/source-react-components';
 import type { GroupedNewsletters } from '../types/newslettersPage';
 import { NewsletterDetail } from './NewsletterDetail';
 import { Section } from './Section';
@@ -12,16 +17,25 @@ interface Props {
 	groupedNewsletters: GroupedNewsletters;
 }
 
+export const BUTTON_ROLE = 'GroupedNewslettersList-sign-up-button';
+export const BUTTON_SELECTED_CLASS =
+	'js-GroupedNewslettersList-sign-up-button--selected';
+const ICON_PLUS_CLASS = 'js-GroupedNewslettersList-sign-up-button__plus-icon';
+const ICON_TICK_CLASS = 'js-GroupedNewslettersList-sign-up-button__tick-icon';
+
 const groupContainerStyle = css`
 	display: flex;
 	flex-wrap: wrap;
 `;
 
 const groupItemStyle = css`
+	display: flex;
+	flex-direction: column;
 	flex-basis: 200px;
 	margin-right: ${space[3]}px;
 	margin-bottom: ${space[3]}px;
-	padding: ${space[1] / 2}px;
+	padding: ${space[1]}px;
+	padding-bottom: ${space[2]}px;
 	min-height: 215px;
 	background-color: ${palette.neutral[97]};
 
@@ -31,6 +45,46 @@ const groupItemStyle = css`
 	}
 `;
 
+const buttonHolderStyle = css`
+	margin-top: auto;
+`;
+
+const buttonStyle = css`
+	background-color: ${palette.neutral[100]};
+	color: ${palette.neutral[7]};
+	border-color: ${palette.neutral[7]};
+	.${ICON_TICK_CLASS} {
+		display: none;
+	}
+
+	&.${BUTTON_SELECTED_CLASS} {
+		background-color: ${palette.neutral[7]};
+		color: ${palette.neutral[100]};
+
+		.${ICON_PLUS_CLASS} {
+			display: none;
+		}
+		.${ICON_TICK_CLASS} {
+			display: inline;
+		}
+	}
+
+	&:hover {
+		background-color: ${palette.neutral[86]};
+	}
+
+	&.${BUTTON_SELECTED_CLASS}:hover {
+		background-color: ${palette.neutral[46]};
+		border-color: ${palette.neutral[46]};
+	}
+`;
+
+const getButtonInitialAriaAttribute = (newsletterName: string) => ({
+	'aria-label': `add ${newsletterName} to subscribe list`,
+	'data-aria-label-when-unchecked': `add ${newsletterName} to subscribe list`,
+	'data-aria-label-when-checked': `remove ${newsletterName} from subscribe list`,
+});
+
 export const GroupedNewslettersList = ({ groupedNewsletters }: Props) => {
 	return (
 		<>
@@ -39,13 +93,53 @@ export const GroupedNewslettersList = ({ groupedNewsletters }: Props) => {
 					<div css={groupContainerStyle}>
 						{group.newsletters.map((newsletter) => {
 							return (
-								<div key={newsletter.name} css={groupItemStyle}>
+								<article
+									key={newsletter.name}
+									css={groupItemStyle}
+									aria-label={newsletter.name}
+								>
 									<NewsletterDetail
 										text={newsletter.frequency}
 									/>
 									<h3>{newsletter.name}</h3>
 									<p>{newsletter.description}</p>
-								</div>
+
+									<div css={buttonHolderStyle}>
+										<Button
+											{...getButtonInitialAriaAttribute(
+												newsletter.name,
+											)}
+											priority="tertiary"
+											size="xsmall"
+											iconSide="left"
+											icon={
+												<>
+													<span
+														className={
+															ICON_PLUS_CLASS
+														}
+													>
+														<SvgPlus />
+													</span>
+													<span
+														className={
+															ICON_TICK_CLASS
+														}
+													>
+														<SvgCheckmark />
+													</span>
+												</>
+											}
+											cssOverrides={buttonStyle}
+											data-newsletter-id={
+												newsletter.identityName
+											}
+											data-role={BUTTON_ROLE}
+										>
+											Sign up
+										</Button>
+									</div>
+								</article>
 							);
 						})}
 					</div>
---
+++ dotcom-rendering/src/components/ManyNewsletterSignUp.stories.tsx
@@ -0,0 +1,49 @@
+import { GroupedNewslettersList } from './GroupedNewsletterList';
+import { ManyNewsletterSignUp } from './ManyNewsletterSignUp.importable';
+
+export const Default = () => {
+	return (
+		<>
+			<GroupedNewslettersList
+				groupedNewsletters={{
+					groups: [
+						{
+							title: 'test',
+							newsletters: [
+								{
+									listId: 1,
+									identityName: 'test-1',
+									name: 'test newsletter 1',
+									description:
+										'Select at least one newsletter to make the ManyNewsletterSignUp appear',
+									frequency: 'weekly',
+									theme: 'news',
+									group: '',
+									successDescription: 'not used',
+								},
+								{
+									listId: 2,
+									identityName: 'test-2',
+									name: 'test newsletter 2',
+									description:
+										'Including the word "example" in the email before submitting will display the forms fail state.',
+									frequency: 'monthly',
+									theme: 'news',
+									group: '',
+									successDescription: 'not used',
+								},
+							],
+						},
+					],
+				}}
+			/>
+			<ManyNewsletterSignUp apiEndpoint="" />
+		</>
+	);
+};
+
+export default {
+	component: Default,
+	title: 'Components/ManyNewsletterSignUp',
+	chromatic: { diffThreshold: 0.7 },
+};
