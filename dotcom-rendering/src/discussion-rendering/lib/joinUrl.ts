e9a76823cb341e4cd2ee2ce1b3ed84929bbc9da4
George B <705427+georgeblahblah@users.noreply.github.com>
2023-06-27 16:28:46 +0100

    refactor `joinUrl` to use `startsWith`/`endsWith`

--- dotcom-rendering/src/discussion-rendering/lib/joinUrl.ts
+++ dotcom-rendering/src/discussion-rendering/lib/joinUrl.ts
@@ -1,19 +1,24 @@
 import { isNonNullable } from '@guardian/libs';
 
+/**
+ * Remove any leading or trailing slashes from all parts and then join cleanly on
+ * a single slash - prevents malformed urls
+ */
 export const joinUrl = (parts: string[]): string => {
-	// Remove any leading or trailing slashes from all parts and then join cleanly on
-	// a single slash - prevents malformed urls
 	const trimmed = parts
 		.filter(isNonNullable) // Filter any falsey parts
 		.map((part) => {
 			// Trim left
-			if (part.substr(0, 1) === '/') return part.slice(1);
+			if (part.startsWith('/')) {
+				return part.slice(1);
+			}
 			return part;
 		})
 		.map((part) => {
 			// Trim right
-			if (part.substr(part.length - 1, 1) === '/')
+			if (part.endsWith('/')) {
 				return part.slice(0, -1);
+			}
 			return part;
 		});
 
