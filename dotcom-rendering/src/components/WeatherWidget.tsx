8c5ab3e1639c9900fed49598984cd6e15199c396
Alex Sanders <alex@sndrs.dev>
2023-06-29 14:51:47 +0100

    add `WeatherWidget`

---
+++ dotcom-rendering/src/components/WeatherWidget.tsx
@@ -0,0 +1,220 @@
+import { css, keyframes } from '@emotion/react';
+import {
+	between,
+	from,
+	textSans,
+	until,
+	visuallyHidden,
+} from '@guardian/source-foundations';
+import { SvgExternal } from '@guardian/source-react-components';
+import type { FEFrontConfigType } from '../types/front';
+import { WeatherWidgetSlot } from './WeatherWidgetSlot';
+
+const visuallyHiddenCSS = css`
+	${visuallyHidden}
+`;
+
+const widgetCSS = css`
+	animation: ${keyframes`from {	opacity: 0;	} to {	opacity: 1;	}`} 250ms;
+	--border: 1px solid #dcdcdc;
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	align-items: center;
+	letter-spacing: -0.56px;
+`;
+
+const locationCSS = css`
+	flex: 1;
+	${textSans.medium()};
+	padding: 12px 0;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+
+	${until.tablet} {
+		flex-basis: 100%;
+		border-bottom: var(--border);
+	}
+
+	${between.tablet.and.leftCol} {
+		padding-right: 1ch;
+	}
+
+	${from.leftCol} {
+		flex-basis: 100%;
+		border-bottom: var(--border);
+	}
+`;
+
+const nowCSS = css`
+	${until.tablet} {
+		justify-content: flex-end;
+		flex-basis: 100%;
+		border-bottom: var(--border);
+		order: -1;
+		padding-bottom: 4px;
+	}
+
+	${from.leftCol} {
+		flex-basis: 100%;
+		border-bottom: var(--border);
+		padding-top: 8px;
+		padding-bottom: 24px;
+	}
+`;
+
+const slotCSS = css`
+	display: flex;
+	flex: 1;
+	padding-top: 4px;
+
+	border-left: var(--border);
+	padding-left: 4px;
+
+	${until.mobileLandscape} {
+		&.forecast-4 {
+			display: none;
+		}
+	}
+
+	${until.tablet} {
+		&.now,
+		&.forecast-1 {
+			border-left: none;
+		}
+	}
+
+	${between.tablet.and.desktop} {
+		padding-left: 4px;
+
+		&.forecast-3,
+		&.forecast-4 {
+			display: none;
+		}
+	}
+
+	${between.desktop.and.wide} {
+		&.forecast-4 {
+			display: none;
+		}
+	}
+
+	${from.leftCol} {
+		&.now,
+		&.forecast-1 {
+			border-left: none;
+		}
+	}
+`;
+
+const linkCSS = css`
+	a {
+		${textSans.small()};
+		color: #121212;
+		text-decoration: none;
+		display: block;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+
+	${until.tablet} {
+		flex-basis: 100%;
+		padding-top: 12px;
+	}
+
+	${from.leftCol} {
+		padding-top: 24px;
+		flex-basis: 100%;
+	}
+`;
+
+const ExternalLinkIcon = () => (
+	<div
+		css={css`
+			background-color: black;
+			height: 24px;
+			width: 24px;
+			margin-left: 0.5ch;
+			border-radius: 100%;
+
+			svg {
+				fill: white;
+				height: 18px;
+				margin-top: 2px;
+				margin-left: 3px;
+			}
+		`}
+	>
+		<SvgExternal />
+	</div>
+);
+
+export type TemperatureProps = {
+	value: number;
+	unit: 'C' | 'F';
+};
+
+type ForecastProps = {
+	icon: number;
+	description: string;
+	link: string;
+	temperature: {
+		metric: TemperatureProps;
+		imperial: TemperatureProps;
+	};
+	time?: string;
+};
+
+export interface WeatherWidgetProps {
+	location: string;
+	now: ForecastProps;
+	forecast: [ForecastProps, ForecastProps, ForecastProps, ForecastProps];
+	edition: FEFrontConfigType['edition'];
+}
+
+export const WeatherWidget = ({
+	location,
+	now,
+	forecast,
+	edition,
+}: WeatherWidgetProps) => {
+	const isUS = edition === 'US';
+	return (
+		<aside css={widgetCSS}>
+			<div css={locationCSS}>{location}</div>
+
+			<p css={visuallyHiddenCSS}>Today’s weather for {location}:</p>
+
+			{/* Current weather */}
+			<div css={[nowCSS, slotCSS]} className="now">
+				<WeatherWidgetSlot {...now} isUS={isUS} />
+			</div>
+
+			{/* Forecast slots */}
+			<div css={slotCSS} className="forecast-1">
+				<WeatherWidgetSlot isUS={isUS} {...forecast[0]} />
+			</div>
+			<div css={slotCSS} className="forecast-2">
+				<WeatherWidgetSlot isUS={isUS} {...forecast[1]} />
+			</div>
+			<div css={slotCSS} className="forecast-3">
+				<WeatherWidgetSlot isUS={isUS} {...forecast[2]} />
+			</div>
+			<div css={slotCSS} className="forecast-4">
+				<WeatherWidgetSlot isUS={isUS} {...forecast[3]} />
+			</div>
+			<div css={linkCSS}>
+				<a href={now.link}>
+					View full forecast <ExternalLinkIcon />
+				</a>
+			</div>
+		</aside>
+	);
+};
---
+++ dotcom-rendering/src/components/WeatherWidgetSlot.tsx
@@ -0,0 +1,173 @@
+import type { SerializedStyles } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
+import { isUndefined } from '@guardian/libs';
+import {
+	from,
+	textSans,
+	until,
+	visuallyHidden,
+} from '@guardian/source-foundations';
+import { lazy, Suspense } from 'react';
+import type { TemperatureProps } from './WeatherWidget';
+
+const formatTemperature = ({ value, unit }: TemperatureProps) =>
+	`${value}°${unit.toLocaleUpperCase()}`;
+
+const formatTime = (time: string, isUS: boolean) =>
+	isUS
+		? new Date(time).toLocaleTimeString('en-US', {
+				hour: 'numeric',
+		  })
+		: new Date(time).toLocaleTimeString(undefined, {
+				hour: '2-digit',
+				minute: '2-digit',
+		  });
+
+const visuallyHiddenCSS = css`
+	${visuallyHidden}
+`;
+
+const slotCSS = css`
+	animation: ${keyframes`from {	opacity: 0;	} to {	opacity: 1;	}`} 250ms;
+	position: relative;
+	display: inline-flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: center;
+	padding-left: 40px;
+	min-height: 40px; /* icon height (32) + 4 + 4 */
+
+	${from.leftCol} {
+		padding-left: 0;
+	}
+`;
+
+const timeCSS = css`
+	${textSans.xxsmall()};
+	display: block;
+`;
+
+const nowCSS = [
+	timeCSS,
+	css`
+		${until.tablet} {
+			display: none;
+		}
+
+		${from.leftCol} {
+			padding-bottom: 4px;
+		}
+	`,
+];
+
+const tempCSS = (isNow: boolean) => [
+	css`
+		display: block;
+		${textSans.medium()};
+
+		${from.leftCol} {
+			order: 1;
+			padding-top: 4px;
+		}
+	`,
+	isNow &&
+		css`
+			${from.leftCol} {
+				${textSans.xlarge()};
+				line-height: 1;
+			}
+			${from.leftCol} {
+				${textSans.xlarge()};
+				line-height: 1;
+			}
+		`,
+];
+
+const iconCSS = css`
+	position: absolute;
+	top: 50%;
+	left: 0px;
+	margin-top: -16px;
+
+	${from.leftCol} {
+		position: static;
+		margin-top: 0;
+	}
+`;
+
+const LoadingIcon = () => (
+	<span
+		css={css`
+			height: 32px;
+			width: 32px;
+			background-color: #f5f5f5;
+		`}
+	></span>
+);
+
+export type WeatherWidgetSlotProps = {
+	icon: number;
+	description: string;
+	link: string;
+	temperature: {
+		metric: TemperatureProps;
+		imperial: TemperatureProps;
+	};
+	time?: string;
+	isUS: boolean;
+	css?: SerializedStyles;
+};
+
+export const WeatherWidgetSlot = ({
+	icon,
+	temperature,
+	time,
+	description,
+	isUS,
+	...props
+}: WeatherWidgetSlotProps) => {
+	const isNow = isUndefined(time);
+
+	const Icon = lazy(() =>
+		import(`../static/icons/weather/weather-${icon}.svg`).then(
+			({ default: Component }) => {
+				return {
+					default: () => (
+						<Component
+							css={iconCSS}
+							aria-hidden={true}
+							className="icon"
+						/>
+					),
+				};
+			},
+		),
+	);
+
+	return (
+		<p css={slotCSS} {...props}>
+			<span css={visuallyHiddenCSS}>
+				{isNow ? 'The current weather' : 'The forecast for'}
+			</span>
+			{isNow ? (
+				<span css={nowCSS} aria-hidden={true}>
+					Now
+				</span>
+			) : (
+				<time css={timeCSS} dateTime={time}>
+					{formatTime(time, isUS)}
+				</time>
+			)}
+			<span css={visuallyHiddenCSS}>is</span>
+			<span css={tempCSS(isNow)} className="temp">
+				{formatTemperature(
+					isUS ? temperature.imperial : temperature.metric,
+				)}
+			</span>
+			<span css={visuallyHiddenCSS}>, {description.toLowerCase()}.</span>
+			<Suspense fallback={<LoadingIcon />}>
+				<Icon />
+			</Suspense>
+		</p>
+	);
+};
