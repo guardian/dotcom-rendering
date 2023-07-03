f772abcbfa3b6db709b088ddca029769baff86f2
Ashleigh Carr <ashcorr20@gmail.com>
2023-07-03 10:49:26 +0100

    Fix linting errors in `Snap.tsx`

--- dotcom-rendering/src/components/Snap.tsx
+++ dotcom-rendering/src/components/Snap.tsx
@@ -1,12 +1,12 @@
 import { css } from '@emotion/react';
 import type { DCRSnapType } from '../types/front';
 
+// Some thrashers don't have "width: 100%" applied to their first element which causes them to not correctly take up their space
 const snapStyles = css`
 	overflow: hidden;
 	position: relative;
 	display: flex;
 
-	// Some thrashers don't have "width: 100%" applied to their first element which causes them to not correctly take up their space
 	> * {
 		width: 100%;
 	}
