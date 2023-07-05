1d4ba8a4a95b7b16e14fb69c752bca8b9449a28c
Joe Cowton <joe.cowton@guardian.co.uk>
2023-07-05 11:35:22 +0100

    revert yarn.lock

--- apps-rendering/src/articleFormat.ts
+++ apps-rendering/src/articleFormat.ts
@@ -121,7 +121,7 @@ const themeToPillar = (theme: ArticleTheme): ArticlePillar => {
  * @param design An {@linkcode ArticleDesign}
  * @returns A string representation of `ArticleDesign`
  */
-const designToString = (design: ArticleDesign): string | undefined => {
+const designToString = (design: ArticleDesign): string => {
 	switch (design) {
 		case ArticleDesign.Standard:
 			return 'Standard';
