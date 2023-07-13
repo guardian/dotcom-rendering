5d981eb3a7b39c7a369130755fcbb669eac19de9
Joe Cowton <77005274+joecowton1@users.noreply.github.com>
2023-07-13 11:27:51 +0100

    Update dotcom-rendering/src/components/AudioAtom/AudioAtom.tsx
    Co-authored-by: George B <705427+georgeblahblah@users.noreply.github.com>

--- dotcom-rendering/src/components/AudioAtom/AudioAtom.tsx
+++ dotcom-rendering/src/components/AudioAtom/AudioAtom.tsx
@@ -343,7 +343,7 @@ export const AudioAtom = ({
 					<p>
 						Sorry your browser does not support audio - but you can
 						download here and listen
-						https://audio.guim.co.uk/2020/05/05-61553-gnl.fw.200505.jf.ch7DW.mp3
+						${urlTouse}
 					</p>
 				</audio>
 				<div css={audioControlsStyle}>
