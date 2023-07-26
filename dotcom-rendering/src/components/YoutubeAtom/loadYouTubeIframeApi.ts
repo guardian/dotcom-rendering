a86825a173de51ca3dec532a21314f6288401b31
Joe Cowton <joe.cowton@guardian.co.uk>
2023-07-26 14:04:48 +0100

    Add ? to Window.YT

--- dotcom-rendering/src/components/YoutubeAtom/loadYouTubeIframeApi.ts
+++ dotcom-rendering/src/components/YoutubeAtom/loadYouTubeIframeApi.ts
@@ -65,10 +65,12 @@ const youtubeAPIReady = () => {
 
 const loadYouTubeAPI = (enableIma = false): Promise<typeof YT> => {
 	/* If another part of the code has already loaded youtube api, return early. */
-	// if (window.YT.Player instanceof Function) {
-	// 	log('dotcom', 'loadYT: returning window.YT');
-	// 	return Promise.resolve(window.YT);
-	// }
+
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- This is needed to avoid a type error when window.YT is undefined
+	if (window.YT?.Player instanceof Function) {
+		log('dotcom', 'loadYT: returning window.YT');
+		return Promise.resolve(window.YT);
+	}
 
 	/* Create youtubeAPIReady promise before loading scripts so that
 	 * window.onYouTubeIframeAPIReady is guaranteed to be defined
