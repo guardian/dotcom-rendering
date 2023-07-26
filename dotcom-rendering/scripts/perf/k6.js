7fb98129fd84bbbd8e64757b7756a15df9b89a26
Ravi <7014230+arelra@users.noreply.github.com>
2023-07-26 23:30:24 +0100

    Add basic k6 script

---
+++ dotcom-rendering/scripts/perf/k6.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check } from 'scripts/perf/k6';
+
+export default function () {
+	const response = http.get(
+		`http://localhost:9000/Article/https://theguardian.com/games/2018/aug/23/nier-automata-yoko-taro-interview`,
+	);
+	check(response, {
+		'is status 200': (r) => r.status === 200,
+	});
+}
+
+export const options = {
+	vus: 5,
+	duration: '10s',
+};
