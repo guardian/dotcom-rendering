ffd0e6ee17ea1657ee3c61eb4756d56f33427819
Charlotte <charlotte.emms@theguardian.com>
2023-07-31 14:46:05 +0100

    infra: use VPC from ID param rather than via cloudformation

--- dotcom-rendering/cdk/lib/dotcom-rendering.ts
+++ dotcom-rendering/cdk/lib/dotcom-rendering.ts
@@ -1,19 +1,29 @@
 import { join } from 'path';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuVpc } from '@guardian/cdk/lib/constructs/ec2';
 import type { App } from 'aws-cdk-lib';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
 export class DotcomRendering extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
+
+		const vpc = GuVpc.fromIdParameter(this, 'vpc');
+
+		const cfnParameters = {
+			VpcId: vpc.vpcId,
+		};
+
 		const yamlTemplateFilePath = join(
 			__dirname,
 			'../..',
 			'cloudformation.yml',
 		);
+
 		new CfnInclude(this, 'YamlTemplate', {
 			templateFile: yamlTemplateFilePath,
+			parameters: cfnParameters,
 		});
 	}
 }
