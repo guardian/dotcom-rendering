c62d4210e9bbbb6baa86ad24045970afe6707e0f
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2023-07-31 17:02:17 +0100

    Add InternalLoadBalancerSecurityGroup

--- dotcom-rendering/cdk/lib/dotcom-rendering.ts
+++ dotcom-rendering/cdk/lib/dotcom-rendering.ts
@@ -1,21 +1,30 @@
 import { join } from 'path';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { GuVpc } from '@guardian/cdk/lib/constructs/ec2';
+import { GuSecurityGroup, GuVpc } from '@guardian/cdk/lib/constructs/ec2';
 import type { App } from 'aws-cdk-lib';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
+type DCRProps = GuStackProps & {app: string};
 export class DotcomRendering extends GuStack {
-	constructor(scope: App, id: string, props: GuStackProps) {
+	constructor(scope: App, id: string, props: DCRProps ) {
 		super(scope, id, props);
 
 		const vpc = GuVpc.fromIdParameter(this, 'vpc', {
 			vpcCidrBlock: '10.248.136.0/22',
 		});
 
+		const lbSecurityGroup = new GuSecurityGroup(this, "InternalLoadBalancerSecurityGroup", {
+			app: props.app,
+			vpc,
+			securityGroupName: "lbSecurityGroup",
+			// ingresses: [//we need to add this ],
+		});
+
 		const cfnParameters = {
 			VpcId: vpc.vpcId,
 			VPCIpBlock: vpc.vpcCidrBlock,
+			InternalLoadBalancerSecurityGroup: lbSecurityGroup,
 		};
 
 		const yamlTemplateFilePath = join(
