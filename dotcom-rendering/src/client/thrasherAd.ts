d6bc1732650a91106a75f8de62776f62e1d45aeb
Ravi <7014230+arelra@users.noreply.github.com>
2023-07-31 18:32:20 +0100

    Use slot name external

--- dotcom-rendering/src/client/thrasherAd.ts
+++ dotcom-rendering/src/client/thrasherAd.ts
@@ -3,18 +3,15 @@ export const thrasherAd = (): Promise<void> => {
 	return new Promise((resolve) => {
 		const el = document.getElementById('thrasher-ad');
 		const adContainer = document.createElement('div');
-		console.log('el: ', el);
 		adContainer.setAttribute('class', 'ad-slot-container');
 		const ad = document.createElement('div');
-		ad.setAttribute('id', 'dfp-ad--sponsored1');
-		ad.setAttribute('data-link-name', 'ad slot sponsored1');
-		ad.setAttribute('data-name', 'sponsored1');
+		ad.setAttribute('id', 'dfp-ad--external1');
+		ad.setAttribute('data-link-name', 'ad slot external1');
+		ad.setAttribute('data-name', 'external1');
 		ad.setAttribute('aria-hidden', 'true');
-		ad.setAttribute('class', 'js-ad-slot ad-slot ad-slot--sponsored1');
+		ad.setAttribute('class', 'js-ad-slot ad-slot ad-slot--external1');
 		ad.setAttribute('data-label-show', 'true');
-		console.log('ad: ', ad);
 		adContainer.appendChild(ad);
-		console.log('adContainer: ', adContainer);
 		el?.appendChild(adContainer);
 		resolve();
 	});
