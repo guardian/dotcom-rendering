61226fe2dc4206a138aef8b77eae8c9c293c09ec
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2023-08-01 16:02:56 +0100

    add ingress rules

--- dotcom-rendering/cdk/lib/dotcom-rendering.ts
+++ dotcom-rendering/cdk/lib/dotcom-rendering.ts
@@ -3,6 +3,7 @@ import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuSecurityGroup, GuVpc } from '@guardian/cdk/lib/constructs/ec2';
 import type { App } from 'aws-cdk-lib';
+import { Peer } from 'aws-cdk-lib/aws-ec2';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
 type DCRProps = GuStackProps & {app: string};
@@ -18,7 +19,18 @@ export class DotcomRendering extends GuStack {
 			app: props.app,
 			vpc,
 			securityGroupName: "lbSecurityGroup",
-			// ingresses: [//we need to add this ],
+			ingresses: [
+				{
+					range: Peer.ipv4(vpc.vpcCidrBlock),
+					port: 80,
+					description: 'Allows HTTP inbound connections from within the VPC',
+				  },
+				  {
+					range: Peer.ipv4(vpc.vpcCidrBlock),
+					port: 443,
+					description: 'Allows HTTP inbound connections from within the VPC',
+				  },
+			  ],
 		});
 
 		const cfnParameters = {
