3e95a092d63b27d3166ae0490726fb06e7637185
Parisa Tork <parisa_tork@yahoo.co.uk>
2023-08-02 22:10:25 +0100

    Migrate InstanceSecurityGroup to CDK

--- dotcom-rendering/cdk/lib/dotcom-rendering.ts
+++ dotcom-rendering/cdk/lib/dotcom-rendering.ts
@@ -1,12 +1,17 @@
 import { join } from 'path';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { GuVpc } from '@guardian/cdk/lib/constructs/ec2';
+import { GuVpc, GuSecurityGroup } from '@guardian/cdk/lib/constructs/ec2';
+import { Peer } from 'aws-cdk-lib/aws-ec2';
 import type { App } from 'aws-cdk-lib';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
+interface DCRProps extends GuStackProps {
+	app: string;
+}
+
 export class DotcomRendering extends GuStack {
-	constructor(scope: App, id: string, props: GuStackProps) {
+	constructor(scope: App, id: string, props: DCRProps) {
 		super(scope, id, props);
 
 		const vpc = GuVpc.fromIdParameter(this, 'vpc', {
@@ -24,9 +29,30 @@ export class DotcomRendering extends GuStack {
 			'cloudformation.yml',
 		);
 
+		const instanceSecurityGroup = new GuSecurityGroup(
+			this,
+			'InstanceSecurityGroup',
+			{
+				app: props.app,
+				description:
+					'rendering instance',
+				vpc,
+				ingresses: [
+					{
+						range: Peer.ipv4(vpc.vpcCidrBlock),
+						port: 9000,
+						description: 'TCP 9000 ingress',
+					},
+				],
+			},
+		);
+
 		new CfnInclude(this, 'YamlTemplate', {
 			templateFile: yamlTemplateFilePath,
 			parameters: cfnParameters,
+			VpcId: vpc.vpcId,
+			VPCIpBlock: vpc.vpcCidrBlock,
+			InstanceSecurityGroup: instanceSecurityGroup.securityGroupId,
 		});
 	}
 }
