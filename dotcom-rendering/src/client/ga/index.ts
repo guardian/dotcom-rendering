583842c60e61a6a1c1829039f72fb677de53e462
Max Duval <max.duval@theguardian.com>
2023-08-03 11:58:59 +0100

    feat(ga): representative task duration
    the script loading time now reports the once the script has actually
    been loaded or not depending on consent

--- dotcom-rendering/src/client/ga/index.ts
+++ dotcom-rendering/src/client/ga/index.ts
@@ -6,39 +6,41 @@ import type { ConsentState } from '@guardian/consent-management-platform/dist/ty
 import { loadScript, log } from '@guardian/libs';
 import { init, sendPageView } from './ga';
 
-export const ga = (): Promise<void> => {
-	// Check if we have consent for GA so that if the reader removes consent for tracking we
-	// remove ga from the page
-	onConsentChange((consentState: ConsentState) => {
-		const consentGivenForGA = getConsentFor(
-			'google-analytics',
-			consentState,
-		);
-		if (consentGivenForGA) {
-			loadScript('https://www.google-analytics.com/analytics.js')
-				.then(() => {
-					log('dotcom', 'GA script loaded');
+export const ga = (): Promise<void> =>
+	new Promise((resolve) => {
+		// Check if we have consent for GA so that if the reader removes consent for tracking we
+		// remove ga from the page
 
-					init();
-					sendPageView();
-				})
-				.catch((e) => {
-					// We don't need to log script loading errors (these will mostly be adblock, etc),
-					if (!String(e).includes('Error loading script')) {
-						// This is primarily for logging errors with our GA code.
-						window.guardian.modules.sentry.reportError(
-							e instanceof Error ? e : new Error(e),
-							'ga',
-						);
-					}
-				});
-		} else {
-			// Disable Google Analytics
-			// @ts-expect-error -- We should never be able to directly set things to the global window object
-			// but in this case we want to stub things for testing, so it's ok to ignore this rule
-			window.ga = null;
-		}
+		onConsentChange((consentState: ConsentState) => {
+			const consentGivenForGA = getConsentFor(
+				'google-analytics',
+				consentState,
+			);
+			if (consentGivenForGA) {
+				loadScript('https://www.google-analytics.com/analytics.js')
+					.then(() => {
+						log('dotcom', 'GA script loaded');
+
+						init();
+						sendPageView();
+					})
+					.catch((e) => {
+						// We don't need to log script loading errors (these will mostly be adblock, etc),
+						if (!String(e).includes('Error loading script')) {
+							// This is primarily for logging errors with our GA code.
+							window.guardian.modules.sentry.reportError(
+								e instanceof Error ? e : new Error(e),
+								'ga',
+							);
+						}
+					})
+					.finally(() => resolve());
+			} else {
+				// Disable Google Analytics
+				// @ts-expect-error -- We should never be able to directly set things to the global window object
+				// but in this case we want to stub things for testing, so it's ok to ignore this rule
+				window.ga = null;
+				resolve();
+			}
+		});
 	});
-
-	return Promise.resolve();
-};
