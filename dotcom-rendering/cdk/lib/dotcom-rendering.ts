5373ac6c5e9062ea08d19ed7eec9a6a9b7c8d66b
Parisa Tork <parisa_tork@yahoo.co.uk>
2023-08-07 17:04:14 +0100

    Initial internal lb migration implementation
    Co-authored-by: DanielCliftonGuardian 110032454+DanielCliftonGuardian@users.noreply.github.com
    Co-authored-by: Charlotte charlotte.emms@theguardian.com

--- dotcom-rendering/cdk/lib/dotcom-rendering.ts
+++ dotcom-rendering/cdk/lib/dotcom-rendering.ts
@@ -3,7 +3,10 @@ import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuSecurityGroup, GuVpc } from '@guardian/cdk/lib/constructs/ec2';
 import type { App } from 'aws-cdk-lib';
+import { Duration } from "aws-cdk-lib";
+import { GuClassicLoadBalancer } from '@guardian/cdk/lib/constructs/loadbalancing';
 import { Peer } from 'aws-cdk-lib/aws-ec2';
+import { LoadBalancingProtocol } from "aws-cdk-lib/aws-elasticloadbalancing";
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
 interface DCRProps extends GuStackProps {
@@ -70,6 +73,38 @@ export class DotcomRendering extends GuStack {
 			reason: 'Retaining a stateful resource previously defined in YAML',
 		});
 
+		const lb = new GuClassicLoadBalancer(this, 'InternalLoadBalancer', {
+			app: props.app,
+			vpc: vpc,
+			listeners: [{
+				internalProtocol: LoadBalancingProtocol.HTTP,
+				internalPort: 80,
+				externalProtocol: LoadBalancingProtocol.HTTP,
+				externalPort: 9000,
+			}],
+			healthCheck: {
+				port: 9000,
+				path: '/_healthcheck',
+				healthyThreshold: 2,
+				unhealthyThreshold: 10,
+				interval: Duration.seconds(30),
+				timeout: Duration.seconds(10),
+			},
+			subnetSelection: { subnets: vpc.publicSubnets },
+			crossZone: true,
+			accessLoggingPolicy: {
+				enabled: true,
+				emitInterval: 5,
+				s3BucketName: 'gu-elb-logs',
+				s3BucketPrefix: `ELBLogs/${props.stack}/${props.app}/${props.stage}`,
+			},
+		});
+
+		this.overrideLogicalId(lb, {
+			logicalId: 'InternalLoadBalancer',
+			reason: 'Retaining a stateful resource previously defined in YAML',
+		});
+
 		const yamlTemplateFilePath = join(
 			__dirname,
 			'../..',
@@ -83,6 +118,7 @@ export class DotcomRendering extends GuStack {
 				VPCIpBlock: vpc.vpcCidrBlock,
 				InternalLoadBalancerSecurityGroup: lbSecurityGroup.securityGroupId,
 				InstanceSecurityGroup: instanceSecurityGroup.securityGroupId,
+				InternalLoadBalancer: lb.idWithApp,
 			}
 		});
 	}
