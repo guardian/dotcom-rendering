7842e98beddfaf179759d48da50858231c505da4
Max Duval <max.duval@theguardian.com>
2023-08-15 14:21:07 +0100

    feat(scheduler): meaningful island hydration
    include the time to load the non-deferred islands
    on the page as part of the startup/boot time

--- dotcom-rendering/src/client/islands/index.ts
+++ dotcom-rendering/src/client/islands/index.ts
@@ -1,8 +1,11 @@
+import { getEmotionCache } from './emotion';
 import { initHydration } from './initHydration';
 
-export const islands = (): Promise<void> => {
-	const elements = document.querySelectorAll('gu-island');
-	initHydration(elements);
-
-	return Promise.resolve();
+export const islands = async (): Promise<void> => {
+	// Get the emotion cache which is shared between islands
+	const emotionCache = getEmotionCache();
+	const elements = document.querySelectorAll<HTMLElement>('gu-island');
+	await Promise.all(
+		[...elements].map((element) => initHydration(element, emotionCache)),
+	);
 };
