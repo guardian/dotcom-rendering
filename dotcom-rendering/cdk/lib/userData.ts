61391222f93c754003c0d640d370f6fbbb07eb8c
Charlotte <charlotte.emms@theguardian.com>
2023-08-15 13:17:37 +0100

    change usedata to array join

--- dotcom-rendering/cdk/lib/userData.ts
+++ dotcom-rendering/cdk/lib/userData.ts
@@ -13,28 +13,33 @@ export const getUserData = ({
 	region,
 	stage,
 	elkStreamId,
-}: UserDataProps): string => `#!/bin/bash -ev
+}: UserDataProps): string => {
+	const userData = [
+		`#!/bin/bash -ev`,
 
-groupadd frontend
-useradd -r -m -s /usr/bin/nologin -g frontend dotcom-rendering
-usermod -a -G frontend aws-kinesis-agent-user
-cd /home/dotcom-rendering
+		`groupadd frontend`,
+		`useradd -r -m -s /usr/bin/nologin -g frontend dotcom-rendering`,
+		`usermod -a -G frontend aws-kinesis-agent-user`,
+		`cd /home/dotcom-rendering`,
 
-aws --region eu-west-1 s3 cp s3://aws-frontend-artifacts/frontend/${stage}/${app}/${app}.zip ./
-unzip -q ${app}.zip -d ${app}
+		`aws --region eu-west-1 s3 cp s3://aws-frontend-artifacts/frontend/${stage}/${app}/${app}.zip ./`,
+		`unzip -q ${app}.zip -d ${app}`,
 
-chown -R dotcom-rendering:frontend ${app}
+		`chown -R dotcom-rendering:frontend ${app}`,
 
-cd ${app}
+		`cd ${app}`,
 
-export TERM=xterm-256color
-export NODE_ENV=production
-export GU_STAGE=${stage}
+		`export TERM=xterm-256color`,
+		`export NODE_ENV=production`,
+		`export GU_STAGE=${stage}`,
 
-mkdir /var/log/dotcom-rendering
-chown -R dotcom-rendering:frontend /var/log/dotcom-rendering
+		`mkdir /var/log/dotcom-rendering`,
+		`chown -R dotcom-rendering:frontend /var/log/dotcom-rendering`,
 
-make start-prod
+		`make start-prod`,
 
-/opt/aws-kinesis-agent/configure-aws-kinesis-agent ${region} ${elkStreamId} /var/log/dotcom-rendering/dotcom-rendering.log
-`;
+		`/opt/aws-kinesis-agent/configure-aws-kinesis-agent ${region} ${elkStreamId} /var/log/dotcom-rendering/dotcom-rendering.log`,
+	].join('\n');
+
+	return userData;
+};
