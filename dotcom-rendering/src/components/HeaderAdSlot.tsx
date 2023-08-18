d4b81c43987a4836ba7932ccdae1dd99bee72f19
Dominik Lander <dominik.lander@guardian.co.uk>
2023-08-18 11:48:13 +0100

    Consider container border when calculating default height

--- dotcom-rendering/src/components/HeaderAdSlot.tsx
+++ dotcom-rendering/src/components/HeaderAdSlot.tsx
@@ -11,13 +11,16 @@ const headerWrapper = css`
 const { TOP_ABOVE_NAV_HEIGHT, AD_LABEL_HEIGHT } = constants;
 
 const padding = space[4] + 2; // 18px - currently being reviewed
+const borderBottomHeight = 1;
+const headerMinHeight =
+	TOP_ABOVE_NAV_HEIGHT + padding + AD_LABEL_HEIGHT + borderBottomHeight;
 
 const headerAdWrapper = css`
 	z-index: 1080;
 	width: 100%;
 	background-color: ${neutral[97]};
-	min-height: ${TOP_ABOVE_NAV_HEIGHT + padding + AD_LABEL_HEIGHT}px;
-	border-bottom: 1px solid ${border.secondary};
+	min-height: ${headerMinHeight}px;
+	border-bottom: ${borderBottomHeight}px solid ${border.secondary};
 	padding-bottom: ${padding}px;
 
 	display: flex;
