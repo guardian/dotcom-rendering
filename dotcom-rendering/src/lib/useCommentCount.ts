5b32337bd05b6e87a0673ea93a4f3c47d3b9e4a6
Max Duval <max.duval@theguardian.com>
2023-08-18 14:07:27 +0100

    refactor(useCommentCount): simplify logic

--- dotcom-rendering/src/lib/useCommentCount.ts
+++ dotcom-rendering/src/lib/useCommentCount.ts
@@ -4,32 +4,35 @@ import { useApi } from './useApi';
 
 type CommentCounts = Record<string, number>;
 
-const getInitialSetFromDOMAttribute = (attribute: string) =>
-	[...document.querySelectorAll(`[${attribute}]`)]
-		.map((element) => element.getAttribute(attribute))
-		.filter(isNonNullable);
+const DATA_ATTRIBUTE = 'data-discussion-id';
 
-const uniqueDiscussionIds = new Set<string>(
-	isServer ? [] : getInitialSetFromDOMAttribute('data-discussion-id'),
-);
+const uniqueDiscussionIds = isServer
+	? undefined
+	: new Set<string>(
+			// create an initial set of IDs by reading what is in the DOM
+			[...document.querySelectorAll(`[${DATA_ATTRIBUTE}]`)]
+				.map((element) => element.getAttribute(DATA_ATTRIBUTE))
+				.filter(isNonNullable),
+	  );
+
+const getUrl = (base: string) =>
+	uniqueDiscussionIds
+		? `${base}/getCommentCounts?${new URLSearchParams({
+				'short-urls': [...uniqueDiscussionIds]
+					.sort() // ensures identical sets produce the same query parameter
+					.join(','),
+		  }).toString()}`
+		: undefined;
 
 export const useCommentCount = (
 	discussionApiUrl: string,
 	shortUrl: string,
 ): number | undefined => {
-	if (!isServer) {
-		uniqueDiscussionIds.add(shortUrl);
-	}
+	uniqueDiscussionIds?.add(shortUrl);
 
-	const searchParams = new URLSearchParams({
-		'short-urls': [...uniqueDiscussionIds]
-			.sort() // ensures identical sets produce the same query parameter
-			.join(','),
-	});
-	const url = `${discussionApiUrl}/getCommentCounts?${searchParams.toString()}`;
-	const { data } = useApi<CommentCounts>(url, {
+	const { data } = useApi<CommentCounts>(getUrl(discussionApiUrl), {
 		// Discussion reponses have a long cache (~300s)
-		refreshInterval: isServer ? 0 : 27_000,
+		refreshInterval: 27_000,
 	});
 
 	return data?.[shortUrl];
