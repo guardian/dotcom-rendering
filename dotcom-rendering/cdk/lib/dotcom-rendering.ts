16804a0257a7f9472a79a6ae064892167f88da69
Charlotte <charlotte.emms@theguardian.com>
2023-08-23 18:10:52 +0100

    infra: add backend 5xx alarm to cdk

--- dotcom-rendering/cdk/lib/dotcom-rendering.ts
+++ dotcom-rendering/cdk/lib/dotcom-rendering.ts
@@ -27,6 +27,7 @@ import { LoadBalancingProtocol } from 'aws-cdk-lib/aws-elasticloadbalancing';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 import type { DCRProps } from './types';
 import { getUserData } from './userData';
+import { CfnAlarm } from 'aws-cdk-lib/aws-cloudwatch';
 
 export class DotcomRendering extends GuStack {
 	constructor(scope: App, id: string, props: DCRProps) {
@@ -245,6 +246,31 @@ export class DotcomRendering extends GuStack {
 			'../..',
 			'cloudformation.yml',
 		);
+
+		const criticalAlertsTopicArn = `arn:aws:sns:${region}:${this.account}:Frontend-PROD-CriticalAlerts`;
+		const backend5XXAlarmThreshold = 100;
+		const backend5XXAlarmPeriod = 60;
+		const backend5XXConsecutivePeriod = 5;
+
+		new CfnAlarm(this, 'Backend5xxAlarm', {
+			actionsEnabled: stage === 'PROD',
+			alarmDescription: `Alarm if 5XX backend errors are greater than ${backend5XXAlarmThreshold} over last ${backend5XXAlarmPeriod} seconds`,
+			comparisonOperator: 'GreaterThanOrEqualToThreshold',
+			dimensions: [
+				{
+					name: 'LoadBalancerName',
+					value: loadBalancer.loadBalancerName,
+				},
+			],
+			metricName: 'HTTPCode_Backend_5XX',
+			namespace: 'AWS/ELB',
+			evaluationPeriods: backend5XXConsecutivePeriod,
+			period: backend5XXAlarmPeriod,
+			statistic: 'Sum',
+			threshold: backend5XXAlarmThreshold,
+			alarmActions: [criticalAlertsTopicArn],
+			okActions: [criticalAlertsTopicArn],
+		});
 
 		new CfnInclude(this, 'YamlTemplate', {
 			templateFile: yamlTemplateFilePath,
