6382d79716bec7c698476eb75ecd64df9a5a8b95
Anna Beddow <anna.beddow@gmail.com>
2023-08-31 09:22:59 +0100

    remove testing

--- dotcom-rendering/src/components/EmailSignup.tsx
+++ dotcom-rendering/src/components/EmailSignup.tsx
@@ -11,7 +11,7 @@ import { buildDetailText } from '../lib/buildNewsletterSignUpText';
 import { NewsletterDetail } from './NewsletterDetail';
 import { SecureSignup } from './SecureSignup';
 
-type EmailSignupProps = {
+type Props = {
 	identityName: string;
 	name: string;
 	description: string;
@@ -83,7 +83,7 @@ export const EmailSignup = ({
 	successDescription,
 	theme,
 	hidePrivacyMessage,
-}: EmailSignupProps) => {
+}: Props) => {
 	return (
 		<aside css={containerStyles} aria-label="newsletter promotion">
 			<div css={stackBelowTabletStyles}>
