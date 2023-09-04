0d60c5f8c99905635d0e1dc71ca47a9b3a7c3cdf
oliverabrahams <ollie.abrahams@guardian.co.uk>
2023-09-04 14:27:04 +0100

    updated tests

--- dotcom-rendering/src/components/EuropeLandingModal.test.tsx
+++ dotcom-rendering/src/components/EuropeLandingModal.test.tsx
@@ -20,32 +20,56 @@ describe('getModalType', () => {
 	});
 
 	it('should return "NoModal" if modalDismissedCookie is set', () => {
-		mockGetCookie.mockReturnValueOnce('US').mockReturnValueOnce('true');
+		mockGetCookie.mockImplementation((arg) => {
+			if (arg.name === 'GU_geo_country') return 'US';
+			if (arg.name === 'GU_eu_modal_dismissed') return 'true';
+			return null;
+		});
 		expect(getModalType()).toEqual('NoModal');
 	});
 
 	it('should return "ModalSwitched" if editionCookie is INT and country is in COE', () => {
-		mockGetCookie.mockReturnValueOnce('INT').mockReturnValueOnce('DE');
+		mockGetCookie.mockImplementation((arg) => {
+			if (arg.name === 'GU_EDITION') return 'INT';
+			if (arg.name === 'GU_geo_country') return 'DE'; // Assuming 'DE' (Germany) is in COE
+			return null;
+		});
 		expect(getModalType()).toEqual('ModalSwitched');
 	});
 
 	it('should return "ModalDoYouWantToSwitch" if editionCookie is INT, country is not in COE or UK/US/AUS', () => {
-		mockGetCookie.mockReturnValueOnce('INT').mockReturnValueOnce('JP');
+		mockGetCookie.mockImplementation((arg) => {
+			if (arg.name === 'GU_EDITION') return 'INT';
+			if (arg.name === 'GU_geo_country') return 'JP'; // Assuming 'JP' (Japan) is not in COE, UK, US, or AUS
+			return null;
+		});
 		expect(getModalType()).toEqual('ModalDoYouWantToSwitch');
 	});
 
 	it('should return "ModalDoYouWantToSwitch" if editionCookie is UK/US/AUS and country is in COE', () => {
-		mockGetCookie.mockReturnValueOnce('UK').mockReturnValueOnce('DE');
+		mockGetCookie.mockImplementation((arg) => {
+			if (arg.name === 'GU_EDITION') return 'UK';
+			if (arg.name === 'GU_geo_country') return 'DE'; // Assuming 'DE' (Germany) is in COE
+			return null;
+		});
 		expect(getModalType()).toEqual('ModalDoYouWantToSwitch');
 	});
 
 	it('should return "ModalNowSeeing" if editionCookie is not set and country is in COE', () => {
-		mockGetCookie.mockReturnValueOnce(null).mockReturnValueOnce('DE');
+		mockGetCookie.mockImplementation((arg) => {
+			if (arg.name === 'GU_EDITION') return null;
+			if (arg.name === 'GU_geo_country') return 'DE'; // Assuming 'DE' (Germany) is in COE
+			return null;
+		});
 		expect(getModalType()).toEqual('ModalNowSeeing');
 	});
 
 	it('should return "NoModal" for all other cases', () => {
-		mockGetCookie.mockReturnValueOnce('UK').mockReturnValueOnce('US');
+		mockGetCookie.mockImplementation((arg) => {
+			if (arg.name === 'GU_EDITION') return 'UK';
+			if (arg.name === 'GU_geo_country') return 'US'; // Assuming 'US' is not in COE
+			return null;
+		});
 		expect(getModalType()).toEqual('NoModal');
 	});
 });
