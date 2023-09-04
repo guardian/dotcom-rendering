746c92fea704f9b395efe1bdd674d503a464ac75
oliverabrahams <ollie.abrahams@guardian.co.uk>
2023-09-04 14:21:50 +0100

    added tests

--- dotcom-rendering/src/components/EuropeLandingModal.test.tsx
+++ dotcom-rendering/src/components/EuropeLandingModal.test.tsx
@@ -25,27 +25,27 @@ describe('getModalType', () => {
 	});
 
 	it('should return "ModalSwitched" if editionCookie is INT and country is in COE', () => {
-		mockGetCookie.mockReturnValueOnce('INT').mockReturnValueOnce('DE'); // Assuming 'DE' (Germany) is in COE
+		mockGetCookie.mockReturnValueOnce('INT').mockReturnValueOnce('DE');
 		expect(getModalType()).toEqual('ModalSwitched');
 	});
 
 	it('should return "ModalDoYouWantToSwitch" if editionCookie is INT, country is not in COE or UK/US/AUS', () => {
-		mockGetCookie.mockReturnValueOnce('INT').mockReturnValueOnce('JP'); // Assuming 'JP' (Japan) is not in COE, UK, US, or AUS
+		mockGetCookie.mockReturnValueOnce('INT').mockReturnValueOnce('JP');
 		expect(getModalType()).toEqual('ModalDoYouWantToSwitch');
 	});
 
 	it('should return "ModalDoYouWantToSwitch" if editionCookie is UK/US/AUS and country is in COE', () => {
-		mockGetCookie.mockReturnValueOnce('UK').mockReturnValueOnce('DE'); // Assuming 'DE' (Germany) is in COE
+		mockGetCookie.mockReturnValueOnce('UK').mockReturnValueOnce('DE');
 		expect(getModalType()).toEqual('ModalDoYouWantToSwitch');
 	});
 
 	it('should return "ModalNowSeeing" if editionCookie is not set and country is in COE', () => {
-		mockGetCookie.mockReturnValueOnce(null).mockReturnValueOnce('DE'); // Assuming 'DE' (Germany) is in COE
+		mockGetCookie.mockReturnValueOnce(null).mockReturnValueOnce('DE');
 		expect(getModalType()).toEqual('ModalNowSeeing');
 	});
 
 	it('should return "NoModal" for all other cases', () => {
-		mockGetCookie.mockReturnValueOnce('UK').mockReturnValueOnce('US'); // Assuming 'US' is not in COE
+		mockGetCookie.mockReturnValueOnce('UK').mockReturnValueOnce('US');
 		expect(getModalType()).toEqual('NoModal');
 	});
 });
