32634184a1a9186d7cfcbd05fe8c0379b0f5e11c
Samantha Gottlieb <samanthagottlieb31@gmail.com>
2023-09-05 15:39:08 +0100

    Create social sign in gate component template
    Copying the existing SignInGateMain component as a starting point

---
+++ dotcom-rendering/src/components/SignInGate/gateDesigns/SignInGateRealSocial.tsx
@@ -0,0 +1,137 @@
+import { cmp } from '@guardian/consent-management-platform';
+import { Button, Link, LinkButton } from '@guardian/source-react-components';
+import { trackLink } from '../componentEventTracking';
+import type { SignInGateProps } from '../types';
+import {
+	actionButtons,
+	bodyBold,
+	bodySeparator,
+	bodyText,
+	faq,
+	firstParagraphOverlay,
+	headingStyles,
+	hideElementsCss,
+	laterButton,
+	privacyLink,
+	registerButton,
+	signInGateContainer,
+	signInHeader,
+	signInLink,
+} from './shared';
+
+export const SignInGateRealSocial = ({
+	signInUrl,
+	registerUrl,
+	guUrl,
+	dismissGate,
+	abTest,
+	ophanComponentId,
+	isMandatory = false,
+}: SignInGateProps) => {
+	return (
+		<div css={signInGateContainer} data-cy="sign-in-gate-main">
+			<style>{hideElementsCss}</style>
+			<div css={firstParagraphOverlay} />
+			<h1 css={headingStyles}>You need to register to keep reading</h1>
+			<p css={bodyBold}>
+				It’s still free to read – this is not a paywall
+			</p>
+			<p css={bodyText}>
+				We’re committed to keeping our quality reporting open. By
+				registering and providing us with insight into your preferences,
+				you’re helping us to engage with you more deeply, and that
+				allows us to keep our journalism free for all. You’ll always be
+				able to control your own{' '}
+				<button
+					data-cy="sign-in-gate-main_privacy"
+					css={privacyLink}
+					onClick={() => {
+						cmp.showPrivacyManager();
+						trackLink(ophanComponentId, 'privacy', abTest);
+					}}
+				>
+					privacy settings
+				</button>
+				.
+			</p>
+			<div css={actionButtons}>
+				<LinkButton
+					data-cy="sign-in-gate-main_register"
+					data-ignore="global-link-styling"
+					css={registerButton}
+					priority="primary"
+					size="small"
+					href={registerUrl}
+					onClick={() => {
+						trackLink(ophanComponentId, 'register-link', abTest);
+					}}
+				>
+					Register for free
+				</LinkButton>
+				{!isMandatory && (
+					<Button
+						data-cy="sign-in-gate-main_dismiss"
+						data-ignore="global-link-styling"
+						css={laterButton}
+						priority="subdued"
+						size="small"
+						onClick={() => {
+							dismissGate();
+							trackLink(ophanComponentId, 'not-now', abTest);
+						}}
+					>
+						I’ll do it later
+					</Button>
+				)}
+			</div>
+
+			<p css={[bodySeparator, bodyBold, signInHeader]}>
+				Have a subscription? Made a contribution? Already registered?
+			</p>
+
+			<Link
+				data-cy="sign-in-gate-main_signin"
+				data-ignore="global-link-styling"
+				css={signInLink}
+				href={signInUrl}
+				onClick={() => {
+					trackLink(ophanComponentId, 'sign-in-link', abTest);
+				}}
+			>
+				Sign In
+			</Link>
+
+			<div css={faq}>
+				<Link
+					data-ignore="global-link-styling"
+					href={`${guUrl}/membership/2019/dec/20/signing-in-to-the-guardian`}
+					onClick={() => {
+						trackLink(ophanComponentId, 'how-link', abTest);
+					}}
+				>
+					Why register & how does it help?
+				</Link>
+
+				<Link
+					data-ignore="global-link-styling"
+					href={`${guUrl}/info/2014/nov/03/why-your-data-matters-to-us-full-text`}
+					onClick={() => {
+						trackLink(ophanComponentId, 'why-link', abTest);
+					}}
+				>
+					How will my information & data be used?
+				</Link>
+
+				<Link
+					data-ignore="global-link-styling"
+					href={`${guUrl}/help/identity-faq`}
+					onClick={() => {
+						trackLink(ophanComponentId, 'help-link', abTest);
+					}}
+				>
+					Get help with registering or signing in
+				</Link>
+			</div>
+		</div>
+	);
+};
