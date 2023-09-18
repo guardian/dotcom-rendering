ca975b20f3bdff8e278dc831a2742d86eb5e0b2f
Chris Jones <chrislomaxjones96@gmail.com>
2023-09-18 12:17:37 +0100

    Separate region classes for advertising and edition

--- dotcom-rendering/src/lib/region-classes.amp.ts
+++ dotcom-rendering/src/lib/region-classes.amp.ts
@@ -1,96 +1,67 @@
+import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
+import type { EditionId } from './edition';
+import type { AdvertisingRegion } from './real-time-config.amp';
 
-// Array of possible ad regions
-export const adRegions = ['UK', 'US', 'AU', 'INT', 'EUR'] as const;
+export const advertisingRegionClasses: {
+	[region in AdvertisingRegion]: SerializedStyles;
+} = {
+	UK: css`
+		display: none;
+		.amp-iso-country-gb & {
+			display: block;
+		}
+	`,
+	US: css`
+		display: none;
+		.amp-iso-country-us & {
+			display: block;
+		}
+	`,
+	AU: css`
+		display: none;
+		.amp-iso-country-au & {
+			display: block;
+		}
+	`,
+	ROW: css`
+		display: none;
+		:not(.amp-iso-country-gb, .amp-iso-country-us, .amp-iso-country-au) & {
+			display: block;
+		}
+	`,
+};
 
-export type AdRegion = (typeof adRegions)[number];
-
-/**
- * Class that will display an element if the user accesses the AMP page
- * from Great Britain
- */
-const ukRegionClass = css`
-	display: none;
-	.amp-iso-country-gb & {
-		display: block;
-	}
-`;
-
-/**
- * Class that will display an element if the user accesses the AMP page from US
- */
-const usRegionClass = css`
-	display: none;
-	.amp-iso-country-us & {
-		display: block;
-	}
-`;
-
-/**
- * Class that will display an element if the user accesses the AMP page
- * from the Australia geo group (including New Zealand)
- */
-const auRegionClass = css`
-	display: none;
-	.amp-geo-group-au & {
-		display: block;
-	}
-`;
-
-/**
- * Class that will display an element if the user accesses the AMP page from
- * the region denoted as "Rest of World"
- *
- * Currently this region is set to any access either from the European Economic
- * Area or where no AMP geo group is assigned
- *
- */
-const rowRegionClass = css`
-	display: none;
-	.amp-geo-group-eea & {
-		display: block;
-	}
-	.amp-geo-no-group & {
-		display: block;
-	}
-`;
-
-/**
- * Class that will display an element if the user accesses the AMP page from
- * the region denoted as "International"
- *
- * Currently this region is set to any access from the European Economic
- * Area EXCEPT Great Britain or where no AMP geo group is assigned
- */
-const internationalRegionClass = css`
-	display: none;
-	.amp-geo-group-eea:not(.amp-iso-country-gb) &,
-	.amp-geo-no-group & {
-		display: block;
-	}
-`;
-
-/**
- * Class that *should* display an element if the user accesses the AMP page from
- * the region denoted as "Europe"
- *
- * Currently we do not want to show any european ads and this will always fall back to international
- */
-const europeRegionClass = css`
-	display: none;
-`;
-
-/**
- * Dictionary mapping region code to the associated AMP region style
- *
- * E.g. Applying `regionClasses["US"]` to an element will only display the
- * element if the user is accessing the AMP page from the US
- * */
-export const regionClasses = {
-	UK: ukRegionClass,
-	US: usRegionClass,
-	AU: auRegionClass,
-	INT: internationalRegionClass,
-	EUR: europeRegionClass,
-	ROW: rowRegionClass,
+export const editionRegionClasses: {
+	[editionId in EditionId]: SerializedStyles;
+} = {
+	UK: css`
+		display: none;
+		.amp-iso-country-gb & {
+			display: block;
+		}
+	`,
+	US: css`
+		display: none;
+		.amp-iso-country-us & {
+			display: block;
+		}
+	`,
+	AU: css`
+		display: none;
+		.amp-iso-country-au & {
+			display: block;
+		}
+	`,
+	EUR: css`
+		display: none;
+		/* TODO add Europe */
+	`,
+	INT: css`
+		display: none;
+		/* TODO add Europe */
+		:not(.amp-iso-country-gb, .amp-iso-country-us, .amp-iso-country-au) & {
+			display: block;
+		}
+	`,
 };
