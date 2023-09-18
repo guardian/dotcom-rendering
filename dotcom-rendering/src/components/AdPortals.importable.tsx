98a937f3c1617a10f8b8edb9ce15093a3937f7b2
Charlotte <charlotte.emms@theguardian.com>
2023-09-18 12:58:51 +0100

    move resize observer to initial useEffect, use ref instead of state for non rendering changes
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>

--- dotcom-rendering/src/components/AdPortals.importable.tsx
+++ dotcom-rendering/src/components/AdPortals.importable.tsx
@@ -1,20 +1,23 @@
 import { AdSlot as BridgetAdSlot } from '@guardian/bridget/AdSlot';
-import { RefObject, createRef, useEffect, useRef, useState } from 'react';
-import { createPortal } from 'react-dom';
-import { AdPlaceholderSlot } from './AdPlaceholderSlot.apps';
-import { getCommercialClient } from '../lib/bridgetApi';
-import { isNonNullable } from '@guardian/libs';
 import type { IRect as BridgetRect } from '@guardian/bridget/Rect';
+import { isNonNullable } from '@guardian/libs';
+import type { RefObject } from 'react';
+import { createRef, useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { getCommercialClient } from '../lib/bridgetApi';
+import { AdPlaceholderSlot } from './AdPlaceholderSlot.apps';
 
 const calculateAdPosition = (element: Element): BridgetRect => {
 	const elementRect = element.getBoundingClientRect();
 
-	const scrollLeft = document.scrollingElement !== null
-		? document.scrollingElement.scrollLeft
-		: document.body.scrollLeft;
-	const scrollTop = document.scrollingElement !== null
-		? document.scrollingElement.scrollTop
-		: document.body.scrollTop;
+	const scrollLeft =
+		document.scrollingElement !== null
+			? document.scrollingElement.scrollLeft
+			: document.body.scrollLeft;
+	const scrollTop =
+		document.scrollingElement !== null
+			? document.scrollingElement.scrollTop
+			: document.body.scrollTop;
 
 	return {
 		x: elementRect.left + scrollLeft,
@@ -22,33 +25,38 @@ const calculateAdPosition = (element: Element): BridgetRect => {
 		width: elementRect.width,
 		height: elementRect.height,
 	};
-}
+};
 
 const positionsEqual = (a: BridgetRect, b: BridgetRect): boolean =>
-	a.height === b.height &&
-	a.width === b.width &&
-	a.x === b.x &&
-	a.y === b.y
+	a.height === b.height && a.width === b.width && a.x === b.x && a.y === b.y;
 
 const getIsPremium = () => Promise.resolve(false);
 
-const calculateAdPositions = (adSlots: RefObject<HTMLDivElement>[]): BridgetAdSlot[] =>
+const calculateAdPositions = (
+	adSlots: RefObject<HTMLDivElement>[],
+): BridgetAdSlot[] =>
 	adSlots
-		.map(slot => slot.current)
+		.map((slot) => slot.current)
 		.filter(isNonNullable)
-		.map(slot => new BridgetAdSlot({
-			rect: calculateAdPosition(slot),
-			isSquare: false,
-		}))
+		.map(
+			(slot) =>
+				new BridgetAdSlot({
+					rect: calculateAdPosition(slot),
+					isSquare: false,
+				}),
+		);
 
-const adsHaveMoved = (oldPositions: BridgetAdSlot[], newPositions: BridgetAdSlot[]): boolean =>
+const adsHaveMoved = (
+	oldPositions: BridgetAdSlot[],
+	newPositions: BridgetAdSlot[],
+): boolean =>
 	newPositions.some((newPosition, idx) => {
 		const oldPositionRect = oldPositions[idx]?.rect;
 
-		if (oldPositionRect !== undefined) {
-			return !positionsEqual(newPosition.rect, oldPositionRect);
-		} else {
+		if (oldPositionRect === undefined) {
 			return false;
+		} else {
+			return !positionsEqual(newPosition.rect, oldPositionRect);
 		}
 	});
 
@@ -58,39 +66,75 @@ export const AdPortals = () => {
 	// References to client-side rendered ad slots.
 	const adSlots = useRef<RefObject<HTMLDivElement>[]>([]);
 	// Positions of client-side rendered ad slots.
-	const [adPositions, setAdPositions] = useState<BridgetAdSlot[]>([]);
+	const adPositions = useRef<BridgetAdSlot[]>([]);
 	// The height of the body element.
-	const [bodyHeight, setBodyHeight] = useState(0);
+	const bodyHeight = useRef(0);
 
 	/**
 	 * Setup Ads
-	 * 
+	 *
 	 * After first render, and only after first render, check if the user has
 	 * the premium tier. If not, set up ads:
 	 * - Look for ad placeholder elements in the page and store them
 	 * - Set up references to client-side rendered ad slots
 	 * - Store the height of the body element
+	 *
+	 *
+	 * Update Ads
+	 *
+	 * Ask the native layer to update the positions of the adverts whenever the
+	 * body element's height changes, because this typically means the ad slot
+	 * positions will have changed.
+	 *
+	 * A resize observer is used for this.
+	 *
+	 * TODO: debounce this to prevent calling the native layer too often?
 	 */
 	useEffect(() => {
+		let resizeObserver: ResizeObserver | undefined = undefined;
+
 		void getIsPremium().then((isPremium) => {
 			if (!isPremium) {
 				const placeholders = Array.from(
-					document.getElementsByClassName(
-						'ad-portal-placeholder',
-					),
+					document.getElementsByClassName('ad-portal-placeholder'),
 				);
 
 				setAdPlaceholders(placeholders);
-				placeholders.forEach((_, i) => adSlots.current[i] = createRef());
+				for (const [i, _] of placeholders.entries()) {
+					adSlots.current[i] = createRef();
+				}
+
+				resizeObserver = new ResizeObserver((entries) => {
+					if (
+						entries[0] !== undefined &&
+						entries[0].target.clientHeight !== bodyHeight.current
+					) {
+						const newAdPositions = calculateAdPositions(
+							adSlots.current,
+						);
+
+						if (adsHaveMoved(adPositions.current, newAdPositions)) {
+							void getCommercialClient().updateAdverts(
+								newAdPositions,
+							);
+							adPositions.current = newAdPositions;
+						}
+
+						bodyHeight.current = entries[0].target.clientHeight;
+					}
+				});
+
+				resizeObserver.observe(document.body);
 			}
 		});
 
-		setBodyHeight(document.body.clientHeight);
+		bodyHeight.current = document.body.clientHeight;
+		return () => resizeObserver?.disconnect();
 	}, []);
 
 	/**
 	 * Insert Ads
-	 * 
+	 *
 	 * Once the list of ad placeholders is available, the last render should
 	 * have populated the client-side ad slots. We can use references to those
 	 * ad slots to calculate their positions, and pass these to the native
@@ -100,49 +144,9 @@ export const AdPortals = () => {
 		if (adPlaceholders.length !== 0) {
 			const newAdPositions = calculateAdPositions(adSlots.current);
 			void getCommercialClient().insertAdverts(newAdPositions);
-			setAdPositions(newAdPositions);
+			adPositions.current = newAdPositions;
 		}
-		
 	}, [adPlaceholders]);
-
-	/**
-	 * Update Ads
-	 * 
-	 * Ask the native layer to update the positions of the adverts whenever the
-	 * body element's height changes, because this typically means the ad slot
-	 * positions will have changed.
-	 *
-	 * A resize observer is used for this. Because it makes use of a callback
-	 * function, which closes over the `bodyHeight` and `adPositions` states,
-	 * we need to re-create it when either of those dependencies change.
-	 * 
-	 * TODO: debounce this to prevent calling the native layer too often?
-	 */
-	useEffect(() => {
-		if (adPositions.length === 0) {
-			return undefined;
-		}
-
-		const resizeObserver = new ResizeObserver((entries) => {
-			if (
-				entries[0] !== undefined &&
-				entries[0].target.clientHeight !== bodyHeight
-			) {
-				const newAdPositions = calculateAdPositions(adSlots.current);
-				
-				if (adsHaveMoved(adPositions, newAdPositions)) {
-					void getCommercialClient().updateAdverts(newAdPositions);
-					setAdPositions(newAdPositions);
-				}
-
-				setBodyHeight(entries[0].target.clientHeight);
-			}
-		});
-
-		resizeObserver.observe(document.body);
-
-		return () => resizeObserver.disconnect();
-	}, [bodyHeight, adPositions]);
 
 	return (
 		<>
