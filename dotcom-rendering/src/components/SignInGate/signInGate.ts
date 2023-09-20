1a18e767bd2e667ca5d523b49f825f16092a19a4
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-09-20 15:53:14 +0100

    Remove the sept test

--- dotcom-rendering/src/components/SignInGate/signInGate.ts
+++ dotcom-rendering/src/components/SignInGate/signInGate.ts
@@ -1,12 +1,9 @@
 import type { ABTest } from '@guardian/ab-core';
-// Sign in Gate A/B Tests
-import { signInGateCopyTestRepeatSept2023 } from '../../experiments/tests/sign-in-gate-copy-test-variants';
 import { signInGateMainControl } from '../../experiments/tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from '../../experiments/tests/sign-in-gate-main-variant';
 // Sign in Gate Types
 import { signInGateComponent as gateMainControl } from './gates/main-control';
 import { signInGateComponent as gateMainVariant } from './gates/main-variant';
-import { signInGateCopyTestRepeatSept2023Component } from './gates/sign-in-gate-copy-test-repeat-sept2023';
 import type { SignInGateTestMap } from './types';
 
 /* When adding a new test, you need to add the test name to the tests array below,
@@ -17,14 +14,11 @@ import type { SignInGateTestMap } from './types';
 export const signInGateTests: ReadonlyArray<ABTest> = [
 	signInGateMainVariant,
 	signInGateMainControl,
-	signInGateCopyTestRepeatSept2023,
 ];
 
 export const signInGateTestVariantToGateMapping: SignInGateTestMap = {
 	'main-control-5': gateMainControl,
 	'main-variant-5': gateMainVariant,
-	'quick-and-easy': signInGateCopyTestRepeatSept2023Component,
-	'take-a-moment': signInGateCopyTestRepeatSept2023Component,
 };
 
 // Component Id does not need to match gate test name, as ab test info passed separately to ophan
@@ -32,5 +26,4 @@ export const signInGateTestVariantToGateMapping: SignInGateTestMap = {
 export const signInGateTestIdToComponentId: { [key: string]: string } = {
 	SignInGateMainVariant: 'main_variant_5',
 	SignInGateMainControl: 'main_control_5',
-	SignInGateCopyTestRepeatSept2023: 'sign_in_gate_copy_test_repeat_sept2023',
 };
