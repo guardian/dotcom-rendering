1725a894d68fa2f7d1903135c13ed620d35c1fba
Ashleigh Carr <ashcorr20@gmail.com>
2023-09-26 11:43:39 +0100

    Split DCR across multiple cores without using PM2

--- dotcom-rendering/src/server/index.ts
+++ dotcom-rendering/src/server/index.ts
@@ -1,5 +1,7 @@
 import { devServer } from './dev-server';
 import { prodServer } from './prod-server';
+import cluster from 'node:cluster';
+import os from 'node:os';
 
 // this export is expected by webpack-hot-server-middleware
 // not used in prod
@@ -8,5 +10,11 @@ export default devServer;
 
 // this is the actual production server
 if (process.env.NODE_ENV === 'production') {
-	prodServer();
+	if (cluster.isPrimary) {
+		for (let i = 0; i < os.cpus().length; i++) {
+			cluster.fork();
+		}
+	} else {
+		prodServer();
+	}
 }
