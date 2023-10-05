e191b126bd49659acd68fc46ccada567a8ae4a64
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-10-05 15:45:35 +0100

    Add CSS for YouTube IMA ad container

--- dotcom-rendering/src/components/MaintainAspectRatio.tsx
+++ dotcom-rendering/src/components/MaintainAspectRatio.tsx
@@ -21,6 +21,24 @@ export const MaintainAspectRatio = ({ height, width, children }: Props) => (
 				top: 0;
 				left: 0;
 			}
+
+			/*
+                The IMA script gives the following styles to the ad container element:
+                    width: [pixel value equal to the youtube iframe's width]
+                    height: [pixel value equal to the youtube iframe's height]
+                    position: relative;
+                    display: block;
+                We need to override these styles to make sure that the ad container overlays
+                the youtube player exactly and to avoid a player-sized white space underneath the ad.
+            */
+			[data-atom-type='ima-ad-container'] {
+				width: 100%;
+				height: 100%;
+				position: absolute !important;
+				top: 0;
+				left: 0;
+				display: none;
+			}
 		`}
 	>
 		{children}
