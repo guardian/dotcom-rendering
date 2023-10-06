5a362b1ab80fce04a69442594ff76cc80427f30a
Max Duval <max.duval@theguardian.com>
2023-10-06 12:14:17 +0100

    refactor(adaptiveSite): more robust methods
    - ensure that we resolve as early as possible
    - ensure no extra work is done if unnecessary
    - no eager evaluation of values at module import
    
    Co-authored-by: Alex Sanders <alex@sndrs.dev>

--- dotcom-rendering/src/client/poorPerformanceMonitoring.ts
+++ dotcom-rendering/src/client/poorPerformanceMonitoring.ts
@@ -1,69 +1,87 @@
 import { log } from '@guardian/libs';
 import { getOphan } from './ophan/ophan';
 
-const TTFB_THRESHOLD = 1200;
-const FCP_THRESHOLD = 2400;
-
 const logPerformanceInfo = (name: string, data?: unknown) =>
 	log('openJournalism', '⏱', name, data);
 
-let memoizedFCP: number | undefined;
-/** First contentful paint is the best single indicator of core web vitals label */
-const getFirstContentfulPaint = async () =>
-	(memoizedFCP ??= await new Promise<number>((resolve) => {
-		new PerformanceObserver((entries, observer) => {
-			for (const { name, startTime, duration } of entries.getEntries()) {
-				if (name === 'first-contentful-paint') {
-					logPerformanceInfo('paint', { name, startTime, duration });
-					observer.disconnect();
-					resolve(startTime);
-				}
-			}
-		}).observe({
-			type: 'paint',
-			buffered: true,
-		});
-	}));
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Glossary/First_contentful_paint
+ *
+ * @note Assumes it is above the threshold if the browser does not support the API.
+ */
+const isFirstContentfulPaintAboveThreshold = async (threshold = 2400) => {
+	try {
+		const fcp = await new Promise<number>((resolve) => {
+			// resolve this promise early if we know we’ve passed the threshold
+			setTimeout(() => resolve(Infinity), threshold - performance.now());
 
-let memoizedTTFB: number | undefined;
-const getTimeToFirstByte = async () =>
-	(memoizedTTFB ??= await new Promise<number>((resolve, reject) => {
+			new PerformanceObserver((entries, observer) => {
+				for (const entry of entries.getEntries()) {
+					if (entry.name !== 'first-contentful-paint') return;
+
+					logPerformanceInfo('paint', entry);
+					observer.disconnect();
+					resolve(entry.startTime);
+				}
+			}).observe({
+				type: 'paint',
+				buffered: true,
+			});
+		});
+
+		return fcp > threshold;
+	} catch (error) {
+		return true;
+	}
+};
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Glossary/Time_to_first_byte
+ *
+ * @note Assumes it is above the threshold if the browser does not support the API.
+ */
+const isTimeToFirstByteAboveThreshold = (threshold = 1200) => {
+	try {
 		// Time to first byte is a widely supported performance marker
 		// which is the second-best indicator of core web vitals label
 		const [nav] = window.performance.getEntriesByType('navigation');
-		if (nav) {
-			logPerformanceInfo('navigation', {
-				domContentLoadedEventEnd: nav.domContentLoadedEventEnd,
-				type: nav.type,
-				responseEnd: nav.responseEnd,
-			});
-			resolve(nav.responseStart - nav.startTime);
-		} else {
-			reject('unable to get navigation information');
-		}
-	}));
+		if (!nav) return true;
 
-/** Whether or not the current page is running more slowly than acceptable */
-export const isPerformingPoorly = Promise.all([
-	getFirstContentfulPaint(),
-	getTimeToFirstByte(),
-])
-	.then(([fcp, ttfb]) => ttfb > TTFB_THRESHOLD && fcp > FCP_THRESHOLD)
-	.catch(() => true);
+		logPerformanceInfo('navigation', {
+			domContentLoadedEventEnd: nav.domContentLoadedEventEnd,
+			type: nav.type,
+			responseEnd: nav.responseEnd,
+		});
+		return nav.responseStart - nav.startTime > threshold;
+	} catch (error) {
+		return true;
+	}
+};
+
+let memoizedPerformingPoorly: boolean | undefined;
+/**
+ * Whether or not the current page is running more slowly than acceptable.
+ *
+ * First contentful paint and time to first byte are good indicators of
+ * whether a page will get at least one “poor” core web vital.
+ *
+ * @see https://web.dev/vitals/#core-web-vitals
+ */
+export const isPerformingPoorly = async (): Promise<boolean> =>
+	(memoizedPerformingPoorly ??=
+		isTimeToFirstByteAboveThreshold() &&
+		(await isFirstContentfulPaintAboveThreshold()));
 
 /** If the current page is performing poorly, record it in Ophan */
 export const recordPoorPerformance = async (): Promise<void> => {
 	try {
-		if (await isPerformingPoorly) {
+		if (await isPerformingPoorly()) {
 			/** Not sure here if we should duplicate “dotcom-rendering” */
 			const experiences = [
 				'dotcom-rendering',
 				'poor-page-performance',
 			].join(',');
-			log(
-				'openJournalism',
-				`🐌 Poor page performance (TTFB>${TTFB_THRESHOLD} && FCP>${FCP_THRESHOLD})`,
-			);
+			log('openJournalism', `🐌 Poor page performance`);
 			const { record } = await getOphan();
 			record({ experiences });
 		}
