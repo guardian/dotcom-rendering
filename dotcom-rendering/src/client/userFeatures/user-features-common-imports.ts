940aa086b8bcc69c381fcd772a576d53ba9276e1
Ravi <7014230+arelra@users.noreply.github.com>
2023-10-16 16:30:42 +0100

    tidy up user-features-common-imports

--- dotcom-rendering/src/client/userFeatures/user-features-common-imports.ts
+++ dotcom-rendering/src/client/userFeatures/user-features-common-imports.ts
@@ -1,9 +1,6 @@
 import { getCookie, setCookie } from '@guardian/libs';
-import EventEmitter from 'wolfy87-eventemitter';
 
-const mediator = new EventEmitter();
-
-//from /lib/cookie
+// from lib/cookie
 const timeInDaysFromNow = (daysFromNow: number): string => {
 	const tmpDate = new Date();
 	tmpDate.setDate(tmpDate.getDate() + daysFromNow);
@@ -18,8 +15,8 @@ const cookieIsExpiredOrMissing = (cookieName: string): boolean => {
 	return timeNow >= expiryTime;
 };
 
-// -------- manage-ad-free-cookies-DS
-// cookie to trigger server-side ad-freeness
+// from manage-ad-free-cookies
+
 const AD_FREE_USER_COOKIE = 'GU_AF1';
 
 const getAdFreeCookie = (): string | null =>
@@ -46,7 +43,7 @@ const setAdFreeCookie = (daysToLive = 1): void => {
 	});
 };
 
-// fetch.json.ts - DS --lib/utils/fetch-json
+// from lib/utils/fetch-json
 
 /**
  * Check that path is a path-absolute-URL string as described in https://url.spec.whatwg.org/#path-absolute-url-string
@@ -94,7 +91,7 @@ const noop = (..._args: unknown[]): void => {
 	// do nothing
 };
 
-// from time-utils.ts -- lib/utils/time-utils
+// from lib/utils/time-utils
 // from and to should be Epoch time in milliseconds
 const dateDiffDays = (from: number, to: number): number => {
 	const oneDayMs = 1000 * 60 * 60 * 24;
@@ -110,8 +107,7 @@ const isExpired = (testExpiry: string): boolean => {
 	return startOfToday > expiryDate;
 };
 
-// TYPES!!!! imports
-// types/dates
+// from types/dates
 
 const dates = {
 	1: '01',
@@ -146,6 +142,7 @@ const dates = {
 	30: '30',
 	31: '31',
 } as const;
+
 const months = {
 	1: '01',
 	2: '02',
@@ -160,6 +157,7 @@ const months = {
 	11: '11',
 	12: '12',
 } as const;
+
 type LocalDate =
 	`${number}-${(typeof months)[keyof typeof months]}-${(typeof dates)[keyof typeof dates]}`;
 
@@ -169,65 +167,7 @@ const getLocalDate = (year: number, month: number, date: number): LocalDate => {
 	}`;
 };
 
-// API IMPORTS
-
-// Types info coming from https://github.com/guardian/discussion-rendering/blob/fc14c26db73bfec8a04ff7a503ed9f90f1a1a8ad/src/types.ts
-
-// const idApiRoot =
-// 	window.guardian.config.page.idApiUrl ?? '/ID_API_ROOT_URL_NOT_FOUND';
-
-mediator.emit('module:identity:api:loaded');
-
-/**
- * Fetch the logged in user's Google Tag ID from IDAPI
- * @returns one of:
- * - string - the user's Google Tag ID
- * - null - if the request failed
- */
-// const fetchGoogleTagIdFromApi = (): Promise<string | null> =>
-// 	fetch(`${idApiRoot}/user/me/identifiers`, {
-// 		mode: 'cors',
-// 		credentials: 'include',
-// 	})
-// 		.then((resp) => {
-// 			if (resp.status === 200) {
-// 				return resp.json() as Promise<{ googleTagId: string }>;
-// 			}
-// 			throw resp.status;
-// 		})
-// 		.then((json) => json.googleTagId)
-// 		.catch((e) => {
-// 			console.log('failed to get Identity user identifiers', e);
-// 			return null;
-// 		});
-
-/**
- * Decide request options based on an {@link AuthStatus}. Requests to authenticated APIs require different options depending on whether
- * you are in the Okta experiment or not.
- * @param authStatus
- * @returns where `authStatus` is:
- *
- * `SignedInWithCookies`:
- * - set the `credentials` option to `"include"`
- *
- * `SignedInWithOkta`:
- * - set the `Authorization` header with a Bearer Access Token
- * - set the `X-GU-IS-OAUTH` header to `true`
- */
-
-/**
- * Get the user's Google Tag ID
- *
- * If enrolled in the Okta experiment, return the value from the ID token
- * `google_tag_id` claim
- * Otherwise, fetch the Google Tag ID from IDAPI
- * @returns one of:
- * - string, if the user is enrolled in the Okta experiment or the fetch to
- *   IDAPI was successful
- * - null, if the user is signed out or the fetch to IDAPI failed
- */
-
-// from membership.ts - from types/membership';
+// from types/membership'
 /**
  * This type is manually kept in sync with the Membership API:
  * https://github.com/guardian/members-data-api/blob/a48acdebed6a334ceb4336ece275b9cf9b3d6bb7/membership-attribute-service/app/models/Attributes.scala#L134-L151
@@ -255,8 +195,6 @@ export type UserFeaturesResponse = {
 };
 
 export type { LocalDate };
-
-export {};
 
 export {
 	dateDiffDays,
