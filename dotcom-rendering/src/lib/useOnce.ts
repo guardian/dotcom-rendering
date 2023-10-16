be6464ac27e5c4b823360764947edf62f91f480c
Ravi <7014230+arelra@users.noreply.github.com>
2023-10-16 11:22:43 +0100

    Add type check to useOnce to check deps array length

--- dotcom-rendering/src/lib/useOnce.ts
+++ dotcom-rendering/src/lib/useOnce.ts
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react';
 
+type ArrayOfAtLeastOne<T> = [T, ...T[]];
+
 /**
  * Ensures that the given task is only run once and only after all items in waitFor are defined
  * @param {Function} task - The task to execute once
  * @param {Array} waitFor - An array of variables that must be defined before the task is executed
  * */
-export const useOnce = (task: () => void, waitFor: unknown[]): void => {
+export const useOnce = (
+	task: () => void,
+	waitFor: ArrayOfAtLeastOne<unknown>,
+): void => {
 	const [alreadyRun, setAlreadyRun] = useState(false);
 	const isReady = waitFor.every((dep) => dep !== undefined);
-	if (waitFor.length === 0) {
-		throw new Error(
-			'useOnce must be passed at least one variable to wait for',
-		);
-	}
 	useEffect(() => {
 		if (!alreadyRun && isReady) {
 			task();
