fe240adc44202c6c1c0bb40ce23543ccc8f67c0e
Max Duval <max.duval@theguardian.com>
2023-10-18 08:20:56 +0100

    Fix drop caps with invisible elements (#9167)

--- dotcom-rendering/src/components/TextBlockComponent.tsx
+++ dotcom-rendering/src/components/TextBlockComponent.tsx
@@ -258,13 +258,15 @@ const buildElementTree =
 						: undefined;
 					if (
 						dropCappedSentence &&
+						// This node is the first node
+						node.previousSibling === null &&
 						// The node is the first in this text block
 						node.parentNode?.parentNode?.firstChild?.contains(
 							node,
 						) &&
 						// The node is at the root of the document avoiding nodes like <a>
 						// tags embedded in <p> tags dropping their cap
-						node.parentNode?.parentNode?.nodeName ===
+						node.parentNode.parentNode.nodeName ===
 							'#document-fragment'
 					) {
 						const { dropCap, restOfSentence } = dropCappedSentence;
