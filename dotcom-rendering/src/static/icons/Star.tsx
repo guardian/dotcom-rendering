970e81e350ff4f4da64d366328a71c84479eaa39
Sophie <sophiemacmillan93@gmail.com>
2023-10-19 15:03:04 +0100

    Apply dark mode to StarRatingBlock

--- dotcom-rendering/src/static/icons/Star.tsx
+++ dotcom-rendering/src/static/icons/Star.tsx
@@ -14,7 +14,7 @@ export const Star = ({
 					id={`star-${starId}`}
 					clipPath={`url(#clip-${starId})`}
 					strokeWidth="2"
-					stroke={palette('--star-rating')}
+					stroke={palette('--star-rating-fill')}
 					fill="transparent"
 					d="M0 5.2L3.7 8l-1.4 4.6.5.4 3.7-2.8 3.7 2.8.5-.4L9.3 8 13 5.2l-.2-.6H8.2L6.8 0h-.6L4.8 4.6H.2l-.2.6z"
 				/>
@@ -27,7 +27,7 @@ export const Star = ({
 	return (
 		<svg width="14" height="13" viewBox="0 0 14 13">
 			<path
-				fill={palette('--star-rating')}
+				fill={palette('--star-rating-fill')}
 				strokeWidth="0"
 				d="M0 5.2L3.7 8l-1.4 4.6.5.4 3.7-2.8 3.7 2.8.5-.4L9.3 8 13 5.2l-.2-.6H8.2L6.8 0h-.6L4.8 4.6H.2l-.2.6z"
 			/>
