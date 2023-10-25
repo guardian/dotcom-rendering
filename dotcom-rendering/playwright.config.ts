0a25d00eeb3d2cdacab2956d137bc3530f666c59
Ravi <7014230+arelra@users.noreply.github.com>
2023-10-25 15:27:23 +0100

    change reporter

--- dotcom-rendering/playwright.config.ts
+++ dotcom-rendering/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 	/* Max parallel tests on CI. */
 	workers: process.env.CI ? 2 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: 'html',
+	reporter: [['line'], ['html', { open: 'never' }]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
