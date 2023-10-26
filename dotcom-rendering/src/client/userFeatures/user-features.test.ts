e763f40d5c69429d623c4844146b29c7a7201ab1
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2023-10-26 16:44:36 +0100

    updated and uncommented unit test

--- dotcom-rendering/src/client/userFeatures/user-features.test.ts
+++ dotcom-rendering/src/client/userFeatures/user-features.test.ts
@@ -95,104 +95,104 @@ beforeAll(() => {
 	window.guardian.config.page.userAttributesApiUrl = '';
 });
 
-// describe('Refreshing the features data', () => {
-// 	describe('If user signed in', () => {
-// 		beforeEach(() => {
-// 			jest.resetAllMocks();
-// 			isUserLoggedInOktaRefactor.mockResolvedValue(true);
-// 			getAuthStatus.mockResolvedValue({
-// 				kind: 'SignedInWithOkta',
-// 			} as AuthStatus);
-// 			fetchJsonSpy.mockReturnValue(Promise.resolve());
-// 		});
+describe('Refreshing the features data', () => {
+	describe('If user signed in', () => {
+		beforeEach(() => {
+			jest.resetAllMocks();
+			isUserLoggedInOktaRefactor.mockResolvedValue(true);
+			getAuthStatus.mockResolvedValue({
+				kind: 'SignedInWithOkta',
+			} as AuthStatus);
+			fetchJsonSpy.mockReturnValue(Promise.resolve());
+		});
 
-// 		it('Performs an update if the user has missing data', async () => {
-// 			deleteAllFeaturesData();
-// 			await refresh();
-// 			expect(fetchJsonSpy).toHaveBeenCalledTimes(1);
-// 		});
+		it('Performs an update if the user has missing data', async () => {
+			deleteAllFeaturesData();
+			await refresh();
+			expect(fetchJsonSpy).toHaveBeenCalledTimes(1);
+		});
 
-// 		it('Performs an update if the user has expired data', async () => {
-// 			setAllFeaturesData({ isExpired: true });
-// 			await refresh();
-// 			expect(fetchJsonSpy).toHaveBeenCalledTimes(1);
-// 		});
+		it('Performs an update if the user has expired data', async () => {
+			setAllFeaturesData({ isExpired: true });
+			await refresh();
+			expect(fetchJsonSpy).toHaveBeenCalledTimes(1);
+		});
 
-// 		it('Does not delete the data just because it has expired', async () => {
-// 			setAllFeaturesData({ isExpired: true });
-// 			await refresh();
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
-// 			).toBe('true');
-// 			expect(
-// 				getCookie({
-// 					name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
-// 				}),
-// 			).toBe('true');
-// 			expect(
-// 				getCookie({
-// 					name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
-// 				}),
-// 			).toEqual(expect.stringMatching(/\d{13}/));
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
-// 			).toEqual(expect.stringMatching(/\d{13}/));
-// 		});
+		it('Does not delete the data just because it has expired', async () => {
+			setAllFeaturesData({ isExpired: true });
+			await refresh();
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
+			).toBe('true');
+			expect(
+				getCookie({
+					name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
+				}),
+			).toBe('true');
+			expect(
+				getCookie({
+					name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
+				}),
+			).toEqual(expect.stringMatching(/\d{13}/));
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
+			).toEqual(expect.stringMatching(/\d{13}/));
+		});
 
-// 		it('Does not perform update if user has fresh feature data', async () => {
-// 			setAllFeaturesData({ isExpired: false });
-// 			await refresh();
-// 			expect(fetchJsonSpy).not.toHaveBeenCalled();
-// 		});
+		it('Does not perform update if user has fresh feature data', async () => {
+			setAllFeaturesData({ isExpired: false });
+			await refresh();
+			expect(fetchJsonSpy).not.toHaveBeenCalled();
+		});
 
-// 		it('Performs an update if membership-frontend wipes just the paying-member cookie', async () => {
-// 			// Set everything except paying-member cookie
-// 			setAllFeaturesData({ isExpired: true });
-// 			removeCookie({name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE});
+		it('Performs an update if membership-frontend wipes just the paying-member cookie', async () => {
+			// Set everything except paying-member cookie
+			setAllFeaturesData({ isExpired: true });
+			removeCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE });
 
-// 			await refresh();
-// 			expect(fetchJsonSpy).toHaveBeenCalledTimes(1);
-// 		});
-// 	});
+			await refresh();
+			expect(fetchJsonSpy).toHaveBeenCalledTimes(1);
+		});
+	});
+});
+describe('If user signed out', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+		isUserLoggedInOktaRefactor.mockResolvedValue(false);
+		fetchJsonSpy.mockReturnValue(Promise.resolve());
+	});
 
-// });
-// describe('If user signed out', () => {
-// 	beforeEach(() => {
-// 		jest.resetAllMocks();
-// 		isUserLoggedInOktaRefactor.mockResolvedValue(false);
-// 		fetchJsonSpy.mockReturnValue(Promise.resolve());
-// 	});
+	it('Does not perform update, even if feature data missing', async () => {
+		deleteAllFeaturesData();
+		await refresh();
+		expect(fetchJsonSpy).not.toHaveBeenCalled();
+	});
 
-// 	it('Does not perform update, even if feature data missing', async () => {
-// 		deleteAllFeaturesData();
-// 		await refresh();
-// 		expect(fetchJsonSpy).not.toHaveBeenCalled();
-// 	});
+	it('Deletes leftover feature data', async () => {
+		setAllFeaturesData({ isExpired: false });
+		await refresh();
+		expect(
+			getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
+		).toBeNull();
+		expect(
+			getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
+		).toBeNull();
+		expect(
+			getCookie({
+				name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
+			}),
+		).toBeNull();
+		expect(
+			getCookie({ name: PERSISTENCE_KEYS.DIGITAL_SUBSCRIBER_COOKIE }),
+		).toBeNull();
+		expect(
+			getCookie({
+				name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
+			}),
+		).toBeNull();
+	});
+});
 
-// 	it('Deletes leftover feature data', async () => {
-// 		setAllFeaturesData({ isExpired: false });
-// 		await refresh();
-// 		expect(
-// 			getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
-// 		).toBeNull();
-// 		expect(
-// 			getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
-// 		).toBeNull();
-// 		expect(
-// 			getCookie({
-// 				name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
-// 			}),
-// 		).toBeNull();
-// 		expect(
-// 			getCookie({ name: PERSISTENCE_KEYS.DIGITAL_SUBSCRIBER_COOKIE }),
-// 		).toBeNull();
-// 		expect(
-// 			getCookie({
-// 				name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
-// 			}),
-// 		).toBeNull();
-// 	});
-// });
 describe('The isDigitalSubscriber getter', () => {
 	it('Is false when the user is logged out', () => {
 		jest.resetAllMocks();
@@ -229,117 +229,117 @@ describe('The isDigitalSubscriber getter', () => {
 	});
 });
 
-// describe('Storing new feature data', () => {
-// 	beforeEach(() => {
-// 		const mockResponse = {
-// 			userId: 'abc',
-// 			showSupportMessaging: false,
-// 			contentAccess: {
-// 				member: false,
-// 				paidMember: false,
-// 				recurringContributor: false,
-// 				digitalPack: false,
-// 				paperSubscriber: false,
-// 				guardianWeeklySubscriber: false,
-// 			},
-// 		};
+describe('Storing new feature data', () => {
+	beforeEach(() => {
+		const mockResponse = {
+			userId: 'abc',
+			showSupportMessaging: false,
+			contentAccess: {
+				member: false,
+				paidMember: false,
+				recurringContributor: false,
+				digitalPack: false,
+				paperSubscriber: false,
+				guardianWeeklySubscriber: false,
+			},
+		};
 
-// 		jest.resetAllMocks();
-// 		fetchJsonSpy.mockReturnValue(Promise.resolve(mockResponse));
-// 		deleteAllFeaturesData();
-// 		isUserLoggedInOktaRefactor.mockResolvedValue(true);
-// 		getAuthStatus.mockResolvedValue({
-// 			kind: 'SignedInWithOkta',
-// 		} as AuthStatus);
-// 	});
+		jest.resetAllMocks();
+		fetchJsonSpy.mockReturnValue(Promise.resolve(mockResponse));
+		deleteAllFeaturesData();
+		isUserLoggedInOktaRefactor.mockResolvedValue(true);
+		getAuthStatus.mockResolvedValue({
+			kind: 'SignedInWithOkta',
+		} as AuthStatus);
+	});
 
-// 	it('Puts the paying-member state and ad-free state in appropriate cookie', () => {
-// 		fetchJsonSpy.mockReturnValueOnce(
-// 			Promise.resolve({
-// 				contentAccess: {
-// 					paidMember: false,
-// 					recurringContributor: false,
-// 					digitalPack: false,
-// 				},
-// 				adFree: false,
-// 			}),
-// 		);
-// 		return refresh().then(() => {
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
-// 			).toBe('false');
-// 			expect(
-// 				getCookie({
-// 					name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
-// 				}),
-// 			).toBe('false');
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.DIGITAL_SUBSCRIBER_COOKIE }),
-// 			).toBe('false');
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
-// 			).toBeNull();
-// 		});
-// 	});
+	it('Puts the paying-member state and ad-free state in appropriate cookie', () => {
+		fetchJsonSpy.mockReturnValueOnce(
+			Promise.resolve({
+				contentAccess: {
+					paidMember: false,
+					recurringContributor: false,
+					digitalPack: false,
+				},
+				adFree: false,
+			}),
+		);
+		return refresh().then(() => {
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
+			).toBe('false');
+			expect(
+				getCookie({
+					name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
+				}),
+			).toBe('false');
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.DIGITAL_SUBSCRIBER_COOKIE }),
+			).toBe('false');
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
+			).toBeNull();
+		});
+	});
 
-// 	it('Puts the paying-member state and ad-free state in appropriate cookie', () => {
-// 		fetchJsonSpy.mockReturnValueOnce(
-// 			Promise.resolve({
-// 				contentAccess: {
-// 					paidMember: true,
-// 					recurringContributor: true,
-// 					digitalPack: true,
-// 				},
-// 				adFree: true,
-// 			}),
-// 		);
-// 		return refresh().then(() => {
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
-// 			).toBe('true');
-// 			expect(
-// 				getCookie({
-// 					name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
-// 				}),
-// 			).toBe('true');
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.DIGITAL_SUBSCRIBER_COOKIE }),
-// 			).toBe('true');
-// 			expect(
-// 				getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
-// 			).toBeTruthy();
-// 			expect(
-// 				Number.isNaN(
-// 					parseInt(
-// 						// @ts-expect-error -- we’re testing it
-// 						getCookie({
-// 							name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE,
-// 						}),
-// 						10,
-// 					),
-// 				),
-// 			).toBe(false);
-// 		});
-// 	});
+	it('Puts the paying-member state and ad-free state in appropriate cookie', () => {
+		fetchJsonSpy.mockReturnValueOnce(
+			Promise.resolve({
+				contentAccess: {
+					paidMember: true,
+					recurringContributor: true,
+					digitalPack: true,
+				},
+				adFree: true,
+			}),
+		);
+		return refresh().then(() => {
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.PAYING_MEMBER_COOKIE }),
+			).toBe('true');
+			expect(
+				getCookie({
+					name: PERSISTENCE_KEYS.RECURRING_CONTRIBUTOR_COOKIE,
+				}),
+			).toBe('true');
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.DIGITAL_SUBSCRIBER_COOKIE }),
+			).toBe('true');
+			expect(
+				getCookie({ name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE }),
+			).toBeTruthy();
+			expect(
+				Number.isNaN(
+					parseInt(
+						// @ts-expect-error -- we’re testing it
+						getCookie({
+							name: PERSISTENCE_KEYS.AD_FREE_USER_COOKIE,
+						}),
+						10,
+					),
+				),
+			).toBe(false);
+		});
+	});
 
-// 	it('Puts an expiry date in an accompanying cookie', () =>
-// 		refresh().then(() => {
-// 			const expiryDate = getCookie({
-// 				name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
-// 			});
-// 			expect(expiryDate).toBeTruthy();
-// 			// @ts-expect-error -- we’re testing it
-// 			expect(Number.isNaN(parseInt(expiryDate, 10))).toBe(false);
-// 		}));
+	it('Puts an expiry date in an accompanying cookie', () =>
+		refresh().then(() => {
+			const expiryDate = getCookie({
+				name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
+			});
+			expect(expiryDate).toBeTruthy();
+			// @ts-expect-error -- we’re testing it
+			expect(Number.isNaN(parseInt(expiryDate, 10))).toBe(false);
+		}));
 
-// 	it('The expiry date is in the future', () =>
-// 		refresh().then(() => {
-// 			const expiryDateString = getCookie({
-// 				name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
-// 			});
-// 			// @ts-expect-error -- we’re testing it
-// 			const expiryDateEpoch = parseInt(expiryDateString, 10);
-// 			const currentTimeEpoch = new Date().getTime();
-// 			expect(currentTimeEpoch < expiryDateEpoch).toBe(true);
-// 		}));
-// });
+	it('The expiry date is in the future', () =>
+		refresh().then(() => {
+			const expiryDateString = getCookie({
+				name: PERSISTENCE_KEYS.USER_FEATURES_EXPIRY_COOKIE,
+			});
+			// @ts-expect-error -- we’re testing it
+			const expiryDateEpoch = parseInt(expiryDateString, 10);
+			const currentTimeEpoch = new Date().getTime();
+			expect(currentTimeEpoch < expiryDateEpoch).toBe(true);
+		}));
+});
