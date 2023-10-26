d86126600f7eef6d27a0fde1005dd6d4c54b9e15
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2023-10-26 15:41:13 +0100

    add e2e cypress test for user-features

--- dotcom-rendering/cypress/e2e/parallel-6/user.features.cy.js
+++ dotcom-rendering/cypress/e2e/parallel-6/user.features.cy.js
@@ -1,22 +1,9 @@
-// Test that cookies are set correctly when a user is signed in.
-// Test that cookies are removed when a user signs out.
-// Test that API requests for user data are made and handled correctly.
-// Test that support messaging is displayed or hidden based on user subscription status.
-// Test that functions like isDigitalSubscriber and isPayingMember return the expected values based on the presence of cookies.
-// Test the behavior of utility functions related to date calculations.
-// Test that the correct user data is sent to Ophan.
-
 /* eslint-disable no-undef */
 /* eslint-disable func-names */
-import { cmpIframe } from '../../lib/cmpIframe';
-import { disableCMP } from '../../lib/disableCMP';
-import { fakeLogin } from '../../lib/fakeLogin';
 import { Standard } from '../../lib/articles';
-import { setLocalBaseUrl } from '../../lib/setLocalBaseUrl';
-import { privacySettingsIframe } from '../../lib/privacySettingsIframe';
 
 const visitArticleNoOkta = () =>
-	cy.visit('http://localhost:3030/Article', {
+	cy.visit('http://localhost:3030/Article/', {
 		method: 'POST',
 		body: JSON.stringify({
 			...Standard,
@@ -40,43 +27,27 @@ const visitArticleNoOkta = () =>
 	});
 
 describe('User cookies tests', function () {
-	// it("Should check if a user is logged in and has required cookies", function() {
-	//   // Mock user login status using Cypress cookies
-	// //   disableCMP();
-	// cy.visit(`Article/${articleUrl}`);
-	//   cy.setCookie("GU_AF1", "true"); // Simulate user being ad-free
-	//   cy.setCookie("gu_paying_member", "true"); // Simulate user being a paying member
-	//   // Reload the page to apply the cookies
-	// //   cy.reload();
-	// cmpIframe().contains("It's your choice");
-	// cmpIframe().find('button.sp_choice_type_12').click();
-	// privacySettingsIframe().contains('Privacy settings');
-	// privacySettingsIframe().find("[title='Accept all']").click();
-	// cy.get('#dfp-ad--top-above-nav').should('not.exist');
-
-	// });
-	// it("Should check if a user is logged out and has no cookies", function() {
-	// 	// Mock user being logged out and having no cookies
-	// 	// cy.clearCookies();
-	// 	cy.visit(`Article/${articleUrl}`);
-	// 	cy.getCookie("GU_AF1").should("not.exist"); // Check if GU_AF1 cookie has been deleted
-	//     cy.getCookie("gu_paying_member").should("not.exist");
-	// 	// Reload the page to apply the changes
-	// 	// cy.reload();
-
-	// 	cmpIframe().contains("It's your choice");
-	// 	// privacySettingsIframe().contains('Privacy settings');
-	// 	// privacySettingsIframe().find("[title='Reject all']").click();
-
-	// 	cy.get('#dfp-ad--top-above-nav').should('exist');
-	//   });
-
 	it(`Request to user features API is sent when no user features expiry cookie`, function () {
-		// set the GU_U cookie to simulate a logged in user
 		cy.setCookie('GU_U', 'true', { log: true });
 
-		// set the gu_digital_subscriber cookie to simulate a digital subscriber
-		// cy.setCookie('gu_digital_subscriber', 'true', { log: true });
+		cy.intercept(
+			'https://members-data-api.theguardian.com/user-attributes/me',
+		).as('getUserFeatures');
+
+		visitArticleNoOkta();
+
+		cy.wait('@getUserFeatures', { timeout: 30000 });
+		cy.getCookie('GU_U').should('exist');
+	});
+
+	it(`Request to user features API is sent when user features cookie has expired`, function () {
+		cy.setCookie('GU_U', 'true', { log: true });
+
+		const currentTimestamp = new Date().getTime();
+		cy.clock(currentTimestamp); // Freeze time at a specific date
+		const expirationTimestamp = currentTimestamp - 24 * 60 * 60 * 1000; // 24 hours ago from now (frozen time)
+
+		cy.setCookie('gu_user_features_expiry', expirationTimestamp.toString());
 
 		cy.intercept(
 			'https://members-data-api.theguardian.com/user-attributes/me',
@@ -87,7 +58,43 @@ describe('User cookies tests', function () {
 		cy.wait('@getUserFeatures', { timeout: 30000 });
 	});
 
-	it(`Request to user features API is sent when user features cookie has expired`, function () {});
+	it(`Existing old cookie data is deleted when the user is signed out`, function () {
+		cy.setCookie('GU_U', 'true', { log: true });
 
-	it(`Existing old cookie data is deleted when the user is signed out`, function () {});
+		// Setting various cookies to simulate deletion process
+		cy.clearCookie('GU_U', { log: true });
+
+		cy.setCookie('gu_user_features_expiry', 'true', { log: true });
+		cy.setCookie('GU_AF1', 'true', { log: true });
+		cy.setCookie('gu_paying_member', 'true', { log: true });
+		cy.setCookie('gu_digital_subscriber', 'true', { log: true });
+
+		cy.intercept(
+			'https://members-data-api.theguardian.com/user-attributes/me',
+		).as('getUserFeatures');
+
+		visitArticleNoOkta();
+
+		cy.getCookie('gu_user_features_expiry').should('not.exist');
+		cy.getCookie('GU_AF1').should('not.exist');
+		cy.getCookie('gu_paying_member').should('not.exist');
+		cy.getCookie('gu_digital_subscriber').should('not.exist');
+	});
 });
+
+// cy.setCookie('GU_U', 'true', { log: true }): This sets the 'GU_U' cookie with the value 'true'. The { log: true } option is just for logging the action.
+
+// cy.intercept('https://members-data-api.theguardian.com/user-attributes/me').as('getUserFeatures'): You intercept any request to the specified URL. This means that if your application makes a request to this URL, it will be intercepted instead of going to the actual URL. The intercepted request doesn't reach the original endpoint.
+
+// visitArticleNoOkta(): You then visit a page using the cy.visit command. If this page triggers a request to 'https://members-data-api.theguardian.com/user-attributes/me', it will be intercepted.
+
+// cy.wait('@getUserFeatures', { timeout: 30000 }): You wait for the '@getUserFeatures' alias (the intercepted request) to complete. This command will wait for the intercept to resolve or for the specified timeout (30 seconds in this case). The response for the intercepted request is expected to be handled during this waiting period.
+
+// cy.getCookie('GU_U').should('exist'): After waiting for the intercept to complete, you check if the 'GU_U' cookie exists.
+
+// In your test, the purpose of the cy.intercept command is to intercept HTTP requests to the specified URL (in your case, https://members-data-api.theguardian.com/user-attributes/me). This interception allows you to control the behavior of the request and response without making an actual network request to the external server.
+
+// While your specific test code for intercepting the URL doesn't define a custom response, the primary purpose of this interception, in this context, is to block the real network request from being made during the test. By doing so, you can isolate your test from external dependencies, making it more predictable and consistent.
+
+// That's correct. In your test scenario, where the request to https://members-data-api.theguardian.com/user-attributes/me is blocked by cy.intercept, none of the code within the persistResponse function is executed because there is no response from the server to persist.
+// Exactly. The control in your test is that the request to https://members-data-api.theguardian.com/user-attributes/me is blocked, which means no new data (cookies) is being set in the browser during the test. This allows you to isolate the specific functionality that depends on the response from this API endpoint without making real network requests and affecting the state of your browser's cookies. It's a controlled testing environment.
