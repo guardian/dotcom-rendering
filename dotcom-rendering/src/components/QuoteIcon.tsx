e0ec9b5cb5b001c18995250b7b0470c9a67831a3
Ioanna Kokkini <ioannakok@hotmail.com>
2023-11-01 09:33:47 +0000

    Make changes to the `QuoteIcon` svg
    It used to have some margins around it. Now the svg finishes where the quote icon finishes, so it's easier to manipulate the space around it if needed.
    
    Co-authored-by: Harry Fischer <harry.fischer@guardian.co.uk>

--- dotcom-rendering/src/components/QuoteIcon.tsx
+++ dotcom-rendering/src/components/QuoteIcon.tsx
@@ -1,10 +1,11 @@
 import { css } from '@emotion/react';
 
 const quoteStyles = (colour: string) => css`
-	height: 0.95em;
-	width: 1.5em;
+	height: 0.75em;
+	width: auto;
 	vertical-align: baseline;
 	fill: ${colour};
+	margin-right: 4px;
 `;
 
 type Props = {
@@ -16,7 +17,7 @@ type Props = {
  */
 export const QuoteIcon = ({ colour }: Props) => (
 	/* This viewBox is narrower than Source’s SvgQuote */
-	<svg viewBox="4 4 24 16" css={quoteStyles(colour)}>
-		<path d="M9.2776 8H14.0473C13.4732 12.5489 12.9653 17.0095 12.7445 22H4C4.79495 17.142 6.4511 12.5489 9.2776 8ZM20.3852 8H25.0887C24.5808 12.5489 24.0067 17.0095 23.7859 22H15.0635C15.9688 17.142 17.5587 12.5489 20.3852 8Z" />
+	<svg viewBox="0 0 22 14" css={quoteStyles(colour)}>
+		<path d="M5.255 0h4.75c-.572 4.53-1.077 8.972-1.297 13.941H0C.792 9.104 2.44 4.53 5.255 0Zm11.061 0H21c-.506 4.53-1.077 8.972-1.297 13.941h-8.686c.902-4.837 2.485-9.411 5.3-13.941Z" />
 	</svg>
 );
