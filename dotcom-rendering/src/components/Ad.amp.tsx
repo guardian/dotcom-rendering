d89831c8cb5d6948af457773d738860c27855b72
Emma Imber <emma-jo.imber@guardian.co.uk>
2023-11-03 10:29:47 +0000

    Reverse recent AMP ad changes

--- dotcom-rendering/src/components/Ad.amp.tsx
+++ dotcom-rendering/src/components/Ad.amp.tsx
@@ -111,8 +111,7 @@ export const Ad = ({
 			// corresponding primary size.
 			data-multi-size-validation="false"
 			data-npa-on-unknown-consent={true}
-			data-lazy-fetch="true"
-			data-loading-strategy="0.5"
+			data-loading-strategy="prefer-viewability-over-views"
 			data-enable-refresh="30"
 			layout="fixed"
 			type="doubleclick"
