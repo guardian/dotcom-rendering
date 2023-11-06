256ed9945b850ef0fcdba5d8304f03382413f885
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-06 15:57:31 +0000

    chore: run codemod on src/server/aws.ts

--- apps-rendering/src/server/aws.ts
+++ apps-rendering/src/server/aws.ts
@@ -1,12 +1,28 @@
 import AWS from 'aws-sdk';
+import { fromIni, fromInstanceMetadata } from "@aws-sdk/credential-providers";
+import { chain as providerChain } from "@smithy/property-provider";
+import { SSM } from "@aws-sdk/client-ssm";
 import { Region } from './appIdentity';
 
-const credentialProvider = new AWS.CredentialProviderChain([
-	(): AWS.Credentials => new AWS.SharedIniFileCredentials({ profile: 'mobile' }),
-	(): AWS.Credentials => new AWS.EC2MetadataCredentials(),
+const credentialProvider = // JS SDK v3 switched credential providers from classes to functions.
+// The CredentialProviderChain is now a chain of providers.
+// Reference: https://www.npmjs.com/package/@aws-sdk/credential-providers
+providerChain([
+	(): AWS.Credentials => // JS SDK v3 switched credential providers from classes to functions.
+    // This is the closest approximation from codemod of what your application needs.
+    // Reference: https://www.npmjs.com/package/@aws-sdk/credential-providers
+    fromIni({ profile: 'mobile' }),
+	(): AWS.Credentials => // JS SDK v3 switched credential providers from classes to functions.
+    // This is the closest approximation from codemod of what your application needs.
+    // Reference: https://www.npmjs.com/package/@aws-sdk/credential-providers
+    fromInstanceMetadata(),
 ]);
 
-export const ssm: AWS.SSM = new AWS.SSM({
-	region: Region,
-	credentialProvider: credentialProvider,
+export const ssm: SSM = new SSM({
+    region: Region,
+
+    // The transformation for credentialProvider is not implemented.
+    // Refer to UPGRADING.md on aws-sdk-js-v3 for changes needed.
+    // Please create/upvote feature request on aws-sdk-js-codemod for credentialProvider.
+    credentialProvider: credentialProvider
 });
