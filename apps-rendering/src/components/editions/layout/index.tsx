c4a043a30ca32f23fbf7e2c79ee5d2fdb438cec0
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2023-11-10 16:51:45 +0000

    remove large spacing below cartoon images

--- apps-rendering/src/components/editions/layout/index.tsx
+++ apps-rendering/src/components/editions/layout/index.tsx
@@ -12,7 +12,7 @@ import {
 	remSpace,
 } from '@guardian/source-foundations';
 import type { Item } from 'item';
-import { isPicture } from 'item';
+import { hasCartoon, isPicture } from 'item';
 import type { FC } from 'react';
 import { renderEditionsElements } from 'renderer';
 import Header from '../header';
@@ -205,7 +205,9 @@ const Layout: FC<Props> = ({ item }) => {
 						css={[
 							bodyWrapperStyles,
 							articleStyles,
-							isPicture(item.tags) && extendedBodyStyles,
+							(
+								isPicture(item.tags) && !hasCartoon(item.mainMedia)
+							) && extendedBodyStyles,
 							item.design === ArticleDesign.Gallery ||
 							item.design === ArticleDesign.Picture
 								? galleryWrapperStyles
