5354f3d17abb7adfb69dd66b61ab71279147fa50
Max Duval <max.duval@theguardian.com>
2023-11-15 18:34:58 +0000

    refactor(SVG): use SVGR instead of desvg
    ensure “icon” mode to keep viewBox attribute
    
    Address a 3 year old comment to get away from this dependency

--- dotcom-rendering/.storybook/main.ts
+++ dotcom-rendering/.storybook/main.ts
@@ -6,6 +6,7 @@ import {
 } from '../scripts/webpack/webpack.config.client';
 import { saveStories } from '../scripts/gen-stories/get-stories.mjs';
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import { svgr } from '../scripts/webpack/svg.cjs';
 
 // Generate dynamic Card and Layout stories
 saveStories();
@@ -123,10 +124,7 @@ const webpackConfig = (config: Configuration) => {
 		typeof fileLoaderRule !== 'string' &&
 		(fileLoaderRule.exclude = /\.svg$/);
 
-	rules.push({
-		test: /\.svg$/,
-		use: ['desvg-loader/react', 'svg-loader'],
-	});
+	rules.push(svgr);
 	config.resolve.modules = [
 		...((config && config.resolve && config.resolve.modules) || []),
 	];
