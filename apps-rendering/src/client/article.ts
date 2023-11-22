6c8028ff734e8cf4349446a11a9dd3e2093f19d5
Alex Sanders <alex@sndrs.dev>
2023-11-22 10:58:56 +0000

    fix AR linting errors

--- apps-rendering/src/client/article.ts
+++ apps-rendering/src/client/article.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-deprecated -- currently still working in 17 mode */
+
 // ----- Imports ----- //
 
 import type { Client as NotificationsClient } from '@guardian/bridget/Notifications';
