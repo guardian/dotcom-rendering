f2839051a64a5c15b8b9990f7cb118e7f72487fb
Charlotte <charlotte.emms@theguardian.com>
2023-11-22 09:10:53 +0000

    feat: add margaret sullivan newsletter for us team

--- dotcom-rendering/src/model/newsletter-grouping.ts
+++ dotcom-rendering/src/model/newsletter-grouping.ts
@@ -56,6 +56,7 @@ export const groups: Partial<Record<EditionId, StaticGroups>> = {
 			title: 'The world explained',
 			newsletters: [
 				'trump-on-trial',
+				'follow-margaret-sullivan',
 				'global-dispatch',
 				'documentaries',
 				'her-stage',
@@ -108,6 +109,7 @@ export const groups: Partial<Record<EditionId, StaticGroups>> = {
 				'headlines-europe',
 				'green-light', // Down to Earth
 				'trump-on-trial',
+				'follow-margaret-sullivan',
 				'best-of-opinion-us',
 				'patriarchy',
 				'bookmarks',
@@ -242,6 +244,7 @@ export const groups: Partial<Record<EditionId, StaticGroups>> = {
 			title: 'The world explained',
 			newsletters: [
 				'trump-on-trial',
+				'follow-margaret-sullivan',
 				'global-dispatch',
 				'cotton-capital',
 				'documentaries',
