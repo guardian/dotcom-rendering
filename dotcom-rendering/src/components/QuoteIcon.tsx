2166c8a80522847ff50e625fda977b610941b583
Ioanna Kokkini <ioannakok@hotmail.com>
2023-11-23 14:31:30 +0000

    Revert "refactor: Remove `quoteStyles` method from `QuoteIcon` svg"
    This reverts commit 7ff77d80f1e8fa0e35a541665b12c8dcaccce2a8.

--- dotcom-rendering/src/components/QuoteIcon.tsx
+++ dotcom-rendering/src/components/QuoteIcon.tsx
@@ -1,9 +1,10 @@
 import { css } from '@emotion/react';
 
-const base = css`
+const quoteStyles = (colour: string) => css`
 	height: 0.7em;
 	width: auto;
 	vertical-align: baseline;
+	fill: ${colour};
 	margin-right: 4px;
 `;
 
@@ -16,7 +17,7 @@ type Props = {
  */
 export const QuoteIcon = ({ colour }: Props) => (
 	/* This viewBox is narrower than Source’s SvgQuote */
-	<svg viewBox="0 0 22 14" css={base} style={{ color: colour }}>
+	<svg viewBox="0 0 22 14" css={quoteStyles(colour)}>
 		<path d="M5.255 0h4.75c-.572 4.53-1.077 8.972-1.297 13.941H0C.792 9.104 2.44 4.53 5.255 0Zm11.061 0H21c-.506 4.53-1.077 8.972-1.297 13.941h-8.686c.902-4.837 2.485-9.411 5.3-13.941Z" />
 	</svg>
 );
