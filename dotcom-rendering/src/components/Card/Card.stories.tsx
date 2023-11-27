9e95a4eba0e230afce066cb67792618a29cb8dd9
Charlotte <charlotte.emms@theguardian.com>
2023-11-27 16:52:31 +0000

    chore:undo change to remove react from Card.stories file

--- dotcom-rendering/src/components/Card/Card.stories.tsx
+++ dotcom-rendering/src/components/Card/Card.stories.tsx
@@ -6,6 +6,7 @@ import {
 	Pillar,
 } from '@guardian/libs';
 import { from } from '@guardian/source-foundations';
+import React from 'react';
 import { splitTheme } from '../../../.storybook/decorators/splitThemeDecorator';
 import { lightDecorator } from '../../../.storybook/decorators/themeDecorator';
 import type { MainMedia } from '../../types/mainMedia';
