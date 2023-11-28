771dce15c6ec3bdb5fee350c297415f880997db8
David Blatcher <dv.bltchr@gmail.com>
2023-11-28 15:10:25 +0000

    add (most) of the content from the existing sign-up page design

--- dotcom-rendering/src/layouts/NewslettersDetailPageLayout.tsx
+++ dotcom-rendering/src/layouts/NewslettersDetailPageLayout.tsx
@@ -1,17 +1,36 @@
+import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
+import { ArticleDesign, ArticleDisplay, Pillar } from '@guardian/libs';
 import {
 	brandBackground,
 	brandBorder,
 	brandLine,
+	from,
+	headline,
 	palette,
+	space,
+	textSans,
+	until,
 } from '@guardian/source-foundations';
+import {
+	Column,
+	Columns,
+	Hide,
+	SvgGuardianLogo,
+} from '@guardian/source-react-components';
 import { StraightLines } from '@guardian/source-react-components-development-kitchen';
+import { CardPicture } from '../components/CardPicture';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { HeaderAdSlot } from '../components/HeaderAdSlot';
 import { Island } from '../components/Island';
 import { Nav } from '../components/Nav/Nav';
+import { NewsletterBadge } from '../components/NewsletterBadge';
+import { NewsletterDetail } from '../components/NewsletterDetail';
+import { NewsletterFrequency } from '../components/NewsletterFrequency';
 import { Section } from '../components/Section';
+import { SecureSignup } from '../components/SecureSignup';
+import { ShareIcons } from '../components/ShareIcons';
 import { SubNav } from '../components/SubNav.importable';
 import { pillarFromCurrentLink } from '../lib/layoutHelpers';
 import type { NavType } from '../model/extract-nav';
@@ -22,6 +41,115 @@ type Props = {
 	newsletterDetailPage: DCRNewsletterDetailPageType;
 	NAV: NavType;
 };
+
+const titleStyle = css`
+	${headline.medium()};
+	padding-bottom: ${space[6]}px;
+`;
+const descriptionStyle = css`
+	${headline.xsmall({ fontWeight: 'light' })};
+`;
+
+const mainColWrapperStyle = css`
+	display: flex;
+	height: 100%;
+	flex-direction: column;
+	justify-content: flex-end;
+	max-width: 200px;
+
+	${until.wide} {
+		max-width: 160px;
+	}
+
+	${until.leftCol} {
+		justify-content: flex-start;
+		max-width: 200px;
+	}
+
+	${until.desktop} {
+		max-width: 170px;
+	}
+`;
+
+// the negative bottom values at the two column layout are to
+// align the baseline of the text in the newsletters badge svg
+// with the Guardian logo in the leftCol, rather than aligning
+// the bottom of the SVG frame (design requirement)
+const mainColNewsLettersBadgeContainerStyle = css`
+	svg {
+		width: 100%;
+		position: relative;
+		bottom: -10px;
+
+		${until.wide} {
+			bottom: -8px;
+		}
+
+		${until.leftCol} {
+			padding-top: ${space[1]}px;
+			bottom: 0;
+		}
+	}
+`;
+
+const leftColWrapperStyle = css`
+	display: flex;
+	justify-content: flex-end;
+	margin-top: ${space[2]}px;
+`;
+
+const mainGraphicWrapperStyle = css`
+	border-radius: ${space[2]}px;
+	overflow: hidden;
+	margin-bottom: ${space[4]}px;
+
+	margin-top: ${space[4]}px;
+
+	${from.desktop} {
+		margin-top: ${space[9]}px;
+	}
+
+	${from.leftCol} {
+		margin-top: ${space[2]}px;
+	}
+`;
+
+const guardianLogoContainerStyle = css`
+	svg {
+		max-width: 100%;
+		display: flex;
+
+		${until.leftCol} {
+			width: 65%;
+			display: block;
+		}
+	}
+`;
+
+const topMarginStyle = (marginTop: number = space[2]): SerializedStyles => css`
+	margin-top: ${marginTop}px;
+`;
+
+const shareSpanStyle = css`
+	${textSans.medium({ fontWeight: 'bold' })};
+	margin-right: ${space[4]}px;
+`;
+
+const shareDivStyle = css`
+	display: flex;
+	align-items: center;
+	margin-top: ${space[4]}px;
+	margin-bottom: ${space[4]}px;
+`;
+
+const frequencyDivStyle = css`
+	margin-top: ${space[2]}px;
+	margin-bottom: ${space[2]}px;
+`;
+
+const regionalFocusDivStyle = css`
+	margin-bottom: ${space[2]}px;
+`;
 
 export const NewsletterDetailPageLayout = ({
 	newsletterDetailPage,
@@ -40,6 +168,11 @@ export const NewsletterDetailPageLayout = ({
 
 	const contributionsServiceUrl =
 		process.env.SDC_URL ?? pageContributionsServiceUrl;
+
+	const regionalFocusText = newsletterDetailPage.newsletter.regionFocus
+		? `${newsletterDetailPage.newsletter.regionFocus} Focused`
+		: '';
+	const showRegionalFocus = Boolean(regionalFocusText);
 
 	return (
 		<>
@@ -150,19 +283,145 @@ export const NewsletterDetailPageLayout = ({
 			</div>
 
 			<main data-layout="NewsletterDetailPageLayout" id="maincontent">
-				<Section>
-					<h1>
-						Newsletter name: {newsletterDetailPage.newsletter.name}
-					</h1>
-					<p>
-						description:{' '}
-						{newsletterDetailPage.newsletter.description}
-					</p>
-					<p>Current nav link: {NAV.currentNavLink}</p>
+				<Section
+					showTopBorder={false}
+					showSideBorders={false}
+					innerBackgroundColour={brandBackground.primary}
+					leftContent={
+						<div css={leftColWrapperStyle}>
+							<Hide until="leftCol">
+								<span css={guardianLogoContainerStyle}>
+									<SvgGuardianLogo
+										textColor={palette.neutral[100]}
+										width={200}
+									/>
+								</span>
+							</Hide>
+						</div>
+					}
+				>
+					<div css={mainColWrapperStyle}>
+						<Hide from="leftCol">
+							<span css={guardianLogoContainerStyle}>
+								<SvgGuardianLogo
+									textColor={palette.neutral[100]}
+									width={200}
+								/>
+							</span>
+						</Hide>
 
-					<pre>{JSON.stringify(NAV)}</pre>
+						<span css={mainColNewsLettersBadgeContainerStyle}>
+							<NewsletterBadge />
+						</span>
+					</div>
+				</Section>
+
+				<Section
+					centralBorder="full"
+					showTopBorder={false}
+					stretchRight={true}
+					leftContent={
+						showRegionalFocus && (
+							<div css={topMarginStyle(space[4])}>
+								<NewsletterDetail text={regionalFocusText} />
+							</div>
+						)
+					}
+				>
+					<Columns collapseUntil="desktop">
+						<Column width={[1, 1, 5 / 8, 1 / 2, 1 / 2]}>
+							{!!newsletterDetailPage.newsletter.regionFocus && (
+								<Hide from="leftCol">
+									{showRegionalFocus && (
+										<div css={regionalFocusDivStyle}>
+											<NewsletterDetail
+												text={regionalFocusText}
+											/>
+										</div>
+									)}
+								</Hide>
+							)}
+
+							<h1 css={titleStyle}>
+								{newsletterDetailPage.newsletter.name}
+							</h1>
+							<p css={descriptionStyle}>
+								{newsletterDetailPage.newsletter.description}
+							</p>
+
+							<div css={shareDivStyle}>
+								<span css={shareSpanStyle}>
+									Tell your friends
+								</span>
+								<ShareIcons
+									pageId={newsletterDetailPage.canonicalUrl}
+									webTitle={newsletterDetailPage.webTitle}
+									format={{
+										theme: Pillar.News,
+										design: ArticleDesign.NewsletterSignup,
+										display: ArticleDisplay.Standard,
+									}}
+									displayIcons={[
+										'facebook',
+										'twitter',
+										'email',
+									]}
+									size="medium"
+									context="ArticleMeta"
+								/>
+							</div>
+
+							<div css={frequencyDivStyle}>
+								<NewsletterFrequency
+									frequency={
+										newsletterDetailPage.newsletter
+											.frequency
+									}
+								/>
+							</div>
+
+							<SecureSignup
+								name={newsletterDetailPage.newsletter.name}
+								newsletterId={
+									newsletterDetailPage.newsletter.identityName
+								}
+								successDescription={
+									newsletterDetailPage.newsletter
+										.successDescription
+								}
+								hidePrivacyMessage={false}
+							/>
+						</Column>
+
+						<Column width={[1, 1, 3 / 8, 1 / 2, 1 / 2]}>
+							<div css={mainGraphicWrapperStyle}>
+								{newsletterDetailPage.newsletter
+									.illustrationCard ? (
+									<CardPicture
+										imageSize="medium"
+										alt=""
+										mainImage={
+											newsletterDetailPage.newsletter
+												.illustrationCard
+										}
+										loading="eager"
+									/>
+								) : (
+									<p>No image</p>
+								)}
+							</div>
+						</Column>
+					</Columns>
 				</Section>
 			</main>
+			<aside>
+				<Section leftContent={'Other newsletters'}>
+					<h2 css={titleStyle}>You might also like...</h2>
+					<p css={descriptionStyle}>
+						Placeholder for content about other newsletters.
+					</p>
+				</Section>
+			</aside>
 
 			<Section
 				fullWidth={true}
