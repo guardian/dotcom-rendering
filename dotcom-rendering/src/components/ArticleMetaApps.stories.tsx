469c857257e389fc1fab7e8ff1af48fdf36f5420
vanessa <32312712+vlbee@users.noreply.github.com>
2023-11-29 16:45:53 +0000

    Add ArticleMetaApps and stories

---
+++ dotcom-rendering/src/components/ArticleMetaApps.stories.tsx
@@ -0,0 +1,270 @@
+import { css } from '@emotion/react';
+import { ArticleDesign, ArticleDisplay, Pillar } from '@guardian/libs';
+import type { StoryObj } from '@storybook/react';
+import { lightDecorator } from '../../.storybook/decorators/themeDecorator';
+import { ArticleMeta as ArticleMetaApps } from './ArticleMetaApps';
+
+type StoryArgs = { format: ArticleFormat; isCommentable: boolean };
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => (
+	<div
+		css={css`
+			width: 220px;
+			padding: 20px;
+		`}
+	>
+		{children}
+	</div>
+);
+
+const tagsWithLargeBylineImage = [
+	{
+		id: 'profile/lanre-bakare',
+		type: 'Contributor',
+		title: 'Lanre Bakare',
+		twitterHandle: 'lanre_bakare',
+		bylineImageUrl:
+			'https://uploads.guim.co.uk/2017/10/06/Lanre-Bakare,-L.png',
+		bylineLargeImageUrl:
+			'https://uploads.guim.co.uk/2017/10/06/Lanre-Bakare,-L.png',
+	},
+];
+
+const tagsWithSmallBylineImage = [
+	{
+		id: 'profile/nicola-slawson',
+		type: 'Contributor',
+		title: 'Nicola Slawson',
+		bylineImageUrl:
+			'https://uploads.guim.co.uk/2016/11/01/Nicola_Slawson.jpg',
+	},
+];
+
+const tagsWithByTwoContributors = [
+	{
+		id: 'profile/lanre-bakare',
+		type: 'Contributor',
+		title: 'Lanre Bakare',
+		twitterHandle: 'lanre_bakare',
+		bylineImageUrl:
+			'https://uploads.guim.co.uk/2017/10/06/Lanre-Bakare,-L.png',
+		bylineLargeImageUrl:
+			'https://uploads.guim.co.uk/2017/10/06/Lanre-Bakare,-L.png',
+	},
+	{
+		id: 'profile/lanre-bakare',
+		type: 'Contributor',
+		title: 'Lanre Bakare',
+	},
+];
+
+export default {
+	component: ArticleMetaApps,
+	title: 'Components/ArticleMetaApps',
+};
+
+const defaultFormat: ArticleFormat = {
+	display: ArticleDisplay.Standard,
+	design: ArticleDesign.Standard,
+	theme: Pillar.News,
+};
+
+const avatarFormat: ArticleFormat = {
+	display: ArticleDisplay.Standard,
+	design: ArticleDesign.Feature,
+	theme: Pillar.News,
+};
+
+const commentFormat: ArticleFormat = {
+	display: ArticleDisplay.Standard,
+	design: ArticleDesign.Standard,
+	theme: Pillar.Opinion,
+};
+
+const immersiveFormat: ArticleFormat = {
+	display: ArticleDisplay.Immersive,
+	design: ArticleDesign.Standard,
+	theme: Pillar.News,
+};
+
+export const ArticleAppsWithFollowStory: StoryObj = ({
+	format,
+	isCommentable,
+}: StoryArgs) => {
+	return (
+		<Wrapper>
+			<ArticleMetaApps
+				format={format}
+				pageId=""
+				webTitle=""
+				byline="Lanre Bakare Chief music writer"
+				tags={tagsWithLargeBylineImage}
+				primaryDateline="Sun 12 Jan 2020 18.00 GMT"
+				secondaryDateline="Last modified on Sun 12 Jan 2020 21.00 GMT"
+				isCommentable={isCommentable}
+				discussionApiUrl="https://discussion.theguardian.com/discussion-api"
+				shortUrlId="/p/zemg8"
+				ajaxUrl=""
+			/>
+		</Wrapper>
+	);
+};
+/** @see /dotcom-rendering/docs/development/storybook.md */
+ArticleAppsWithFollowStory.args = { format: defaultFormat };
+ArticleAppsWithFollowStory.parameters = { config: { renderingTarget: 'Apps' } };
+ArticleAppsWithFollowStory.decorators = [lightDecorator([defaultFormat])];
+
+export const ArticleAppsWithFollowStoryNoTitle: StoryObj = ({
+	format,
+	isCommentable,
+}: StoryArgs) => {
+	return (
+		<Wrapper>
+			<ArticleMetaApps
+				format={format}
+				pageId=""
+				webTitle=""
+				byline="Lanre Bakare"
+				tags={tagsWithLargeBylineImage}
+				primaryDateline="Sun 12 Jan 2020 18.00 GMT"
+				secondaryDateline="Last modified on Sun 12 Jan 2020 21.00 GMT"
+				isCommentable={isCommentable}
+				discussionApiUrl="https://discussion.theguardian.com/discussion-api"
+				shortUrlId="/p/zemg8"
+				ajaxUrl=""
+			/>
+		</Wrapper>
+	);
+};
+/** @see /dotcom-rendering/docs/development/storybook.md */
+ArticleAppsWithFollowStoryNoTitle.args = { format: defaultFormat };
+ArticleAppsWithFollowStoryNoTitle.parameters = {
+	config: { renderingTarget: 'Apps' },
+};
+ArticleAppsWithFollowStoryNoTitle.decorators = [
+	lightDecorator([defaultFormat]),
+];
+
+export const ArticleAppsWithAvatarAndFollowStory: StoryObj = ({
+	format,
+	isCommentable = true,
+}: StoryArgs) => {
+	return (
+		<Wrapper>
+			<ArticleMetaApps
+				format={format}
+				pageId=""
+				webTitle=""
+				byline="Lanre Bakare Chief music writer"
+				tags={tagsWithLargeBylineImage}
+				primaryDateline="Sun 12 Jan 2020 18.00 GMT"
+				secondaryDateline="Last modified on Sun 12 Jan 2020 21.00 GMT"
+				isCommentable={isCommentable}
+				discussionApiUrl="https://discussion.theguardian.com/discussion-api"
+				shortUrlId="/p/zemg8"
+				ajaxUrl=""
+			/>
+		</Wrapper>
+	);
+};
+/** @see /dotcom-rendering/docs/development/storybook.md */
+ArticleAppsWithAvatarAndFollowStory.args = { format: defaultFormat };
+ArticleAppsWithAvatarAndFollowStory.parameters = {
+	config: { renderingTarget: 'Apps' },
+};
+ArticleAppsWithAvatarAndFollowStory.decorators = [
+	lightDecorator([avatarFormat]),
+];
+
+export const ArticleAppsWithAvatarNoTitleAndFollowStory: StoryObj = ({
+	format,
+	isCommentable = true,
+}: StoryArgs) => {
+	return (
+		<Wrapper>
+			<ArticleMetaApps
+				format={format}
+				pageId=""
+				webTitle=""
+				byline="Lanre Bakare"
+				tags={tagsWithLargeBylineImage}
+				primaryDateline="Sun 12 Jan 2020 18.00 GMT"
+				secondaryDateline="Last modified on Sun 12 Jan 2020 21.00 GMT"
+				isCommentable={isCommentable}
+				discussionApiUrl="https://discussion.theguardian.com/discussion-api"
+				shortUrlId="/p/zemg8"
+				ajaxUrl=""
+			/>
+		</Wrapper>
+	);
+};
+/** @see /dotcom-rendering/docs/development/storybook.md */
+ArticleAppsWithAvatarNoTitleAndFollowStory.args = { format: defaultFormat };
+ArticleAppsWithAvatarNoTitleAndFollowStory.parameters = {
+	config: { renderingTarget: 'Apps' },
+};
+ArticleAppsWithAvatarNoTitleAndFollowStory.decorators = [
+	lightDecorator([avatarFormat]),
+];
+
+export const ArticleAppsImmersiveAndFollowStory: StoryObj = ({
+	format,
+	isCommentable,
+}: StoryArgs) => {
+	return (
+		<Wrapper>
+			<ArticleMetaApps
+				format={format}
+				pageId=""
+				webTitle=""
+				byline="Lanre Bakare"
+				tags={tagsWithLargeBylineImage}
+				primaryDateline="Sun 12 Jan 2020 18.00 GMT"
+				secondaryDateline="Last modified on Sun 12 Jan 2020 21.00 GMT"
+				isCommentable={isCommentable}
+				discussionApiUrl="https://discussion.theguardian.com/discussion-api"
+				shortUrlId="/p/zemg8"
+				ajaxUrl=""
+			/>
+		</Wrapper>
+	);
+};
+/** @see /dotcom-rendering/docs/development/storybook.md */
+ArticleAppsImmersiveAndFollowStory.args = { format: defaultFormat };
+ArticleAppsImmersiveAndFollowStory.parameters = {
+	config: { renderingTarget: 'Apps' },
+};
+ArticleAppsImmersiveAndFollowStory.decorators = [
+	lightDecorator([immersiveFormat]),
+];
+
+export const ArticleAppsWithMultipleContributos: StoryObj = ({
+	format,
+	isCommentable,
+}: StoryArgs) => {
+	return (
+		<Wrapper>
+			<ArticleMetaApps
+				format={format}
+				pageId=""
+				webTitle=""
+				byline="Lanre Bakare"
+				tags={tagsWithByTwoContributors}
+				primaryDateline="Sun 12 Jan 2020 18.00 GMT"
+				secondaryDateline="Last modified on Sun 12 Jan 2020 21.00 GMT"
+				isCommentable={isCommentable}
+				discussionApiUrl="https://discussion.theguardian.com/discussion-api"
+				shortUrlId="/p/zemg8"
+				ajaxUrl=""
+			/>
+		</Wrapper>
+	);
+};
+/** @see /dotcom-rendering/docs/development/storybook.md */
+ArticleAppsWithMultipleContributos.args = { format: defaultFormat };
+ArticleAppsWithMultipleContributos.parameters = {
+	config: { renderingTarget: 'Apps' },
+};
+ArticleAppsWithMultipleContributos.decorators = [
+	lightDecorator([defaultFormat]),
+];
---
+++ dotcom-rendering/src/components/ArticleMetaApps.tsx
@@ -0,0 +1,466 @@
+import { css } from '@emotion/react';
+import type { ArticleFormat } from '@guardian/libs';
+import { ArticleDesign, ArticleDisplay, ArticleSpecial } from '@guardian/libs';
+import {
+	between,
+	border,
+	from,
+	space,
+	until,
+} from '@guardian/source-foundations';
+import { StraightLines } from '@guardian/source-react-components-development-kitchen';
+import { interactiveLegacyClasses } from '../layouts/lib/interactiveLegacyStyling';
+import { getSoleContributor } from '../lib/byline';
+import { decidePalette } from '../lib/decidePalette';
+import type { Branding as BrandingType } from '../types/branding';
+import type { Palette } from '../types/palette';
+import type { TagType } from '../types/tag';
+import { Avatar } from './Avatar';
+import { Branding } from './Branding.importable';
+import { CommentCount } from './CommentCount.importable';
+import { useConfig } from './ConfigContext';
+import { Contributor } from './Contributor';
+import { Dateline } from './Dateline';
+import { Island } from './Island';
+import { SendAMessage } from './SendAMessage.importable';
+import { ShareIcons } from './ShareIcons';
+
+type Props = {
+	format: ArticleFormat;
+	pageId: string;
+	webTitle: string;
+	byline?: string;
+	tags: TagType[];
+	primaryDateline: string;
+	secondaryDateline: string;
+	branding?: BrandingType;
+	discussionApiUrl: string;
+	shortUrlId: string;
+	isCommentable: boolean;
+	ajaxUrl: string;
+	messageUs?: MessageUs;
+};
+
+const meta = (format: ArticleFormat) => {
+	if (
+		format.design === ArticleDesign.LiveBlog ||
+		format.design === ArticleDesign.DeadBlog
+	) {
+		return css`
+			${between.tablet.and.leftCol} {
+				order: 3;
+			}
+
+			padding-top: 2px;
+		`;
+	}
+
+	return css`
+		${between.tablet.and.leftCol} {
+			order: 3;
+		}
+
+		${until.mobileLandscape} {
+			padding-left: 10px;
+			padding-right: 10px;
+		}
+
+		${from.mobileLandscape} {
+			padding-left: 20px;
+			padding-right: 20px;
+		}
+
+		${from.phablet} {
+			padding-left: 0px;
+			padding-right: 0px;
+		}
+
+		padding-top: 2px;
+	`;
+};
+
+const metaFlex = css`
+	margin-bottom: 6px;
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
+`;
+
+const stretchLines = css`
+	display: block;
+
+	${until.phablet} {
+		margin-left: -20px;
+		margin-right: -20px;
+	}
+	${until.mobileLandscape} {
+		margin-left: -10px;
+		margin-right: -10px;
+	}
+`;
+
+const borderColourWhenBackgroundDark = css`
+	${until.desktop} {
+		border-top: 1px solid rgba(255, 255, 255, 0.4);
+	}
+`;
+
+const metaExtras = (palette: Palette, isPictureContent: boolean) => css`
+	border-top: 1px solid ${palette.border.article};
+	flex-grow: 1;
+	padding-top: 6px;
+
+	${!isPictureContent && until.phablet} {
+		margin-left: -20px;
+		margin-right: -20px;
+		padding-left: 20px;
+		padding-right: 20px;
+	}
+
+	${!isPictureContent && until.phablet} {
+		margin-left: -10px;
+		margin-right: -10px;
+		padding-left: 10px;
+		padding-right: 10px;
+	}
+
+	${between.leftCol.and.wide} {
+		padding-bottom: 6px;
+	}
+`;
+
+const metaNumbers = (palette: Palette, isPictureContent: boolean) => css`
+	border-top: 1px solid ${palette.border.article};
+	display: flex;
+	flex-grow: 1;
+
+	justify-content: flex-end;
+	${between.leftCol.and.wide} {
+		justify-content: flex-start;
+	}
+
+	${!isPictureContent && until.phablet} {
+		margin-left: -20px;
+		margin-right: -20px;
+		padding-left: 20px;
+		padding-right: 20px;
+	}
+
+	${!isPictureContent && until.phablet} {
+		margin-left: -10px;
+		margin-right: -10px;
+		padding-left: 10px;
+		padding-right: 10px;
+	}
+`;
+
+const metaContainer = (format: ArticleFormat) => {
+	const defaultMargins = css`
+		${until.phablet} {
+			margin-left: -20px;
+			margin-right: -20px;
+		}
+		${until.mobileLandscape} {
+			margin-left: -10px;
+			margin-right: -10px;
+		}
+	`;
+	switch (format.display) {
+		case ArticleDisplay.Immersive:
+		case ArticleDisplay.Showcase:
+		case ArticleDisplay.NumberedList:
+		case ArticleDisplay.Standard: {
+			switch (format.design) {
+				case ArticleDesign.PhotoEssay:
+					return format.theme === ArticleSpecial.Labs
+						? defaultMargins
+						: css`
+								${until.phablet} {
+									margin-left: -20px;
+									margin-right: -20px;
+								}
+								${until.mobileLandscape} {
+									margin-left: -10px;
+									margin-right: -10px;
+								}
+								${from.leftCol} {
+									margin-left: 20px;
+								}
+								${from.wide} {
+									margin-left: 40px;
+								}
+						  `;
+				case ArticleDesign.LiveBlog:
+				case ArticleDesign.DeadBlog: {
+					return '';
+				}
+				default:
+					return defaultMargins;
+			}
+		}
+	}
+};
+
+const shouldShowAvatar = (format: ArticleFormat) => {
+	switch (format.display) {
+		case ArticleDisplay.Immersive:
+			return false;
+		case ArticleDisplay.Showcase:
+		case ArticleDisplay.NumberedList:
+		case ArticleDisplay.Standard: {
+			switch (format.design) {
+				case ArticleDesign.Feature:
+				case ArticleDesign.Review:
+				case ArticleDesign.Recipe:
+				case ArticleDesign.Interview:
+					return true;
+				default:
+					return false;
+			}
+		}
+		default:
+			return false;
+	}
+};
+
+const shouldShowContributor = (format: ArticleFormat) => {
+	switch (format.display) {
+		case ArticleDisplay.NumberedList:
+			return true;
+		case ArticleDisplay.Immersive:
+			return false;
+		case ArticleDisplay.Showcase:
+		case ArticleDisplay.Standard: {
+			switch (format.design) {
+				case ArticleDesign.Comment:
+				case ArticleDesign.Editorial:
+				case ArticleDesign.Analysis:
+					return false;
+				default:
+					return true;
+			}
+		}
+		default:
+			return false;
+	}
+};
+
+const MetaAvatarContainer = ({ children }: { children: React.ReactNode }) => (
+	<div
+		css={css`
+			width: 140px;
+			height: 140px;
+			margin-top: 6px;
+			margin-right: 10px;
+			margin-bottom: 12px;
+			margin-left: 0px;
+
+			${until.leftCol} {
+				width: 60px;
+				height: 60px;
+				margin-top: 3px;
+				margin-right: 10px;
+				margin-bottom: 12px;
+				margin-left: 0px;
+			}
+		`}
+	>
+		{children}
+	</div>
+);
+
+const RowBelowLeftCol = ({ children }: { children: React.ReactNode }) => (
+	<div
+		css={css`
+			display: flex;
+			flex-direction: column;
+
+			${until.leftCol} {
+				flex-direction: row;
+			}
+		`}
+	>
+		{children}
+	</div>
+);
+
+const metaExtrasLiveBlog = css`
+	${until.phablet} {
+		margin-right: 0;
+	}
+`;
+
+const metaNumbersExtrasLiveBlog = css`
+	${until.phablet} {
+		margin-left: 0;
+	}
+`;
+
+export const ArticleMeta = ({
+	branding,
+	format,
+	pageId,
+	webTitle,
+	byline,
+	tags,
+	primaryDateline,
+	secondaryDateline,
+	discussionApiUrl,
+	shortUrlId,
+	isCommentable,
+	ajaxUrl,
+	messageUs,
+}: Props) => {
+	const soleContributor = getSoleContributor(tags, byline);
+	const authorName = soleContributor?.title ?? 'Author Image';
+
+	const avatarUrl = shouldShowAvatar(format)
+		? soleContributor?.bylineLargeImageUrl
+		: undefined;
+	const isInteractive = format.design === ArticleDesign.Interactive;
+
+	const palette = decidePalette(format);
+
+	const isPictureContent = format.design === ArticleDesign.Picture;
+
+	const { renderingTarget } = useConfig();
+	console.log('renderingTarget', renderingTarget);
+
+	return (
+		<div
+			className={
+				isInteractive ? interactiveLegacyClasses.metaContainer : ''
+			}
+			css={metaContainer(format)}
+		>
+			<div css={meta(format)}>
+				{branding && (
+					<Island priority="feature" defer={{ until: 'visible' }}>
+						<Branding branding={branding} format={format} />
+					</Island>
+				)}
+				{format.theme === ArticleSpecial.Labs ? (
+					<div>
+						<StraightLines
+							cssOverrides={stretchLines}
+							count={1}
+							color={border.primary}
+						/>
+						<div
+							css={css`
+								height: ${space[1]}px;
+							`}
+						/>
+					</div>
+				) : (
+					''
+				)}
+				<RowBelowLeftCol>
+					<>
+						{!!avatarUrl && (
+							<MetaAvatarContainer>
+								<Avatar src={avatarUrl} alt={authorName} />
+							</MetaAvatarContainer>
+						)}
+
+						<div>
+							{shouldShowContributor(format) && !!byline && (
+								<Contributor
+									byline={byline}
+									tags={tags}
+									format={format}
+								/>
+							)}
+							{messageUs &&
+								format.design === ArticleDesign.LiveBlog && (
+									<Island
+										priority="feature"
+										defer={{ until: 'visible' }}
+									>
+										<SendAMessage
+											formFields={messageUs.formFields}
+											formId={messageUs.formId}
+											format={format}
+											pageId={pageId}
+										/>
+									</Island>
+								)}
+							<Dateline
+								primaryDateline={primaryDateline}
+								secondaryDateline={secondaryDateline}
+								format={format}
+							/>
+						</div>
+					</>
+				</RowBelowLeftCol>
+
+				<div data-print-layout="hide" css={metaFlex}>
+					{renderingTarget === 'Web' && (
+						<div
+							className={
+								isInteractive
+									? interactiveLegacyClasses.shareIcons
+									: ''
+							}
+							css={[
+								metaExtras(palette, isPictureContent),
+								format.design === ArticleDesign.LiveBlog &&
+									css(
+										borderColourWhenBackgroundDark,
+										metaExtrasLiveBlog,
+									),
+							]}
+						>
+							<ShareIcons
+								pageId={pageId}
+								webTitle={webTitle}
+								format={format}
+								displayIcons={['facebook', 'twitter', 'email']}
+								size="medium"
+								context="ArticleMeta"
+							/>
+						</div>
+					)}
+					<div
+						className={
+							isInteractive
+								? interactiveLegacyClasses.shareAndCommentCounts
+								: ''
+						}
+						css={[
+							metaNumbers(palette, isPictureContent),
+							format.design === ArticleDesign.LiveBlog &&
+								css(
+									borderColourWhenBackgroundDark,
+									metaNumbersExtrasLiveBlog,
+								),
+						]}
+					>
+						<div
+							css={css`
+								display: flex;
+								flex-direction: row;
+								align-items: flex-start;
+							`}
+						>
+							<div>
+								{isCommentable && (
+									<Island
+										priority="feature"
+										defer={{ until: 'idle' }}
+									>
+										<CommentCount
+											discussionApiUrl={discussionApiUrl}
+											shortUrlId={shortUrlId}
+											format={format}
+										/>
+									</Island>
+								)}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
