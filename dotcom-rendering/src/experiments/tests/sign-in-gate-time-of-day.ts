34853249af233f99e1c8af830d2de7a92b55527b
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-12-04 12:23:54 +0000

    Update the test definition

--- dotcom-rendering/src/experiments/tests/sign-in-gate-time-of-day.ts
+++ dotcom-rendering/src/experiments/tests/sign-in-gate-time-of-day.ts
@@ -2,12 +2,12 @@ import type { ABTest } from '@guardian/ab-core';
 
 export const signInGateTimesOfDay: ABTest = {
 	id: 'SignInGateTimesOfDay',
-	start: '2023-10-23',
-	expiry: '2023-11-23',
+	start: '2023-12-04',
+	expiry: '2023-12-09',
 	author: 'Lindsey Dew',
 	description: 'Show sign in gate more frequently to users in the morning',
-	audience: 0.5,
-	audienceOffset: 0.4,
+	audience: 0.45,
+	audienceOffset: 0.45,
 	successMeasure: 'Increase registration and sign ins by 5%',
 	audienceCriteria:
 		'1st article of the day, max 5 articles, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
