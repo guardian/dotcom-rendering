6b0ec92aaa340e8f3e83837a0f266801edc29c03
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2023-12-06 18:55:51 +0000

    Added dark mode to the audio atom
    The audio atom now uses the new `palette` module for colours rather than `decidePalette`, and dark mode colours have been added.
    
    This also updates the paths of the play/pause icons to use the background colour (i.e. appear transparent), rather than white, to match how other, similar icons appear elsewhere.
    
    The stories have been updated to CSF 3, and now make use of the `splitTheme` decorator to generate multiple atoms, using different colour schemes and formats, in the same story.

--- dotcom-rendering/src/components/AudioAtom/AudioAtom.stories.tsx
+++ dotcom-rendering/src/components/AudioAtom/AudioAtom.stories.tsx
@@ -1,80 +1,24 @@
-import { css } from '@emotion/react';
-import { ArticleDesign, ArticleDisplay, Pillar } from '@guardian/libs';
-import { AudioAtom } from './AudioAtom';
+import { AudioAtom as AudioAtomComponent } from './AudioAtom';
+import type { Meta, StoryObj } from '@storybook/react';
+import { splitTheme } from '../../../.storybook/decorators/splitThemeDecorator';
 
-export default {
-	title: 'Components/AudioAtom',
-	component: AudioAtom,
+const meta: Meta<typeof AudioAtomComponent> = {
+	title: 'Components/Audio Atom',
+	component: AudioAtomComponent,
 };
 
-export const SportStory = (): JSX.Element => {
-	return (
-		<div
-			css={css`
-				width: 700px;
-				padding: 15px;
-			`}
-		>
-			<AudioAtom
-				id="d6d509cf-ca10-407f-8913-e16a3712f415"
-				trackUrl="https://audio.guim.co.uk/2020/05/05-61553-gnl.fw.200505.jf.ch7DW.mp3"
-				kicker="Football Weekly Extra Extra"
-				title="Q&A and Detective Wilson"
-				format={{
-					display: ArticleDisplay.Standard,
-					design: ArticleDesign.Standard,
-					theme: Pillar.Sport,
-				}}
-				duration={849}
-			/>
-		</div>
-	);
+type Story = StoryObj<typeof AudioAtomComponent>;
+
+export const AudioAtom: Story = {
+	args: {
+		id: 'd6d509cf-ca10-407f-8913-e16a3712f415',
+		trackUrl:
+			'https://audio.guim.co.uk/2020/05/05-61553-gnl.fw.200505.jf.ch7DW.mp3',
+		kicker: 'Football Weekly Extra Extra',
+		title: 'Q&A and Detective Wilson',
+		duration: 849,
+	},
+	decorators: [splitTheme()],
 };
 
-export const NewsStory = (): JSX.Element => {
-	return (
-		<div
-			css={css`
-				width: 700px;
-				padding: 15px;
-			`}
-		>
-			<AudioAtom
-				id="d6d509cf-ca10-407f-8913-e16a3712f415"
-				trackUrl="https://audio.guim.co.uk/2020/05/05-61553-gnl.fw.200505.jf.ch7DW.mp3"
-				kicker="Football Weekly Extra Extra"
-				title="Q&A and Detective Wilson"
-				format={{
-					display: ArticleDisplay.Standard,
-					design: ArticleDesign.Standard,
-					theme: Pillar.News,
-				}}
-				duration={849}
-			/>
-		</div>
-	);
-};
-
-export const CultureStory = (): JSX.Element => {
-	return (
-		<div
-			css={css`
-				width: 700px;
-				padding: 15px;
-			`}
-		>
-			<AudioAtom
-				id="d6d509cf-ca10-407f-8913-e16a3712f415"
-				trackUrl="https://audio.guim.co.uk/2020/05/05-61553-gnl.fw.200505.jf.ch7DW.mp3"
-				kicker="Football Weekly Extra Extra"
-				title="Q&A and Detective Wilson"
-				format={{
-					display: ArticleDisplay.Standard,
-					design: ArticleDesign.Standard,
-					theme: Pillar.Culture,
-				}}
-				duration={849}
-			/>
-		</div>
-	);
-};
+export default meta;
