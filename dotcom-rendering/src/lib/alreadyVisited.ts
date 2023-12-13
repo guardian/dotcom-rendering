8e79d61c9f34f3394502a478a1d158646d44179b
Tom Forbes <tom.forbes@theguardian.com>
2023-12-13 10:27:18 +0000

    use canTarget

--- dotcom-rendering/src/lib/alreadyVisited.ts
+++ dotcom-rendering/src/lib/alreadyVisited.ts
@@ -1,26 +1,9 @@
 import { onConsent } from '@guardian/consent-management-platform';
-import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
 
 /**
  * This local storage item is used to target ads if a user has the correct consents
  */
 const AlreadyVisitedKey = 'gu.alreadyVisited';
-
-// Matches consents in the Commercial repo: https://github.com/guardian/commercial/blob/16c4358c727e7a5bdbd4e85aea8a08ac3aa80c0b/src/core/targeting/personalised.ts#L125
-const hasConsentForAlreadyVisitedCount = (
-	consentState: ConsentState,
-): boolean => {
-	if (consentState.tcfv2) {
-		if (consentState.tcfv2.consents['1']) return true;
-	}
-	if (consentState.ccpa) {
-		if (!consentState.ccpa.doNotSell) return true;
-	}
-	if (consentState.aus) {
-		if (consentState.aus.personalisedAdvertising) return true;
-	}
-	return false;
-};
 
 const getAlreadyVisitedCount = (): number => {
 	const alreadyVisited = parseInt(
@@ -36,7 +19,7 @@ const setAlreadyVisited = (count: number): void => {
 
 export const incrementAlreadyVisited = (): Promise<void> =>
 	onConsent().then((consentState) => {
-		if (hasConsentForAlreadyVisitedCount(consentState)) {
+		if (consentState.canTarget) {
 			const alreadyVisited = getAlreadyVisitedCount();
 			setAlreadyVisited(alreadyVisited + 1);
 		} else {
