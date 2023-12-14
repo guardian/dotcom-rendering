0215c0a47fb24458049ad2e3e871894a073df0f3
Sophie <26366706+sophie-macmillan@users.noreply.github.com>
2023-12-14 11:05:36 +0000

    Darkmode - Report Abuse Form (#9880)
    * Remove  from discusison PillarButton
    
    * Update PillarButton.stories
    
    * Update FirstCommentWelcome stories
    
    * Update CommentForm.stories.tsx
    
    * Add decorator to CommentContainer stories
    
    * Remove unneeded Row from PillarButton stories
    
    * Darkmode - report abuse
    
    * Remove unused import

--- dotcom-rendering/src/components/Discussion/AbuseReportForm.stories.tsx
+++ dotcom-rendering/src/components/Discussion/AbuseReportForm.stories.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import { ArticleDesign, ArticleDisplay, Pillar } from '@guardian/libs';
+import { splitTheme } from '../../../.storybook/decorators/splitThemeDecorator';
 import { AbuseReportForm } from './AbuseReportForm';
 
 export default { title: 'Discussion/Abuse Report Form' };
@@ -16,13 +17,17 @@ export const Dialog = () => (
 	<div css={wrapperStyles}>
 		<AbuseReportForm
 			toggleSetShowForm={() => {}}
-			format={{
-				display: ArticleDisplay.Standard,
-				design: ArticleDesign.Standard,
-				theme: Pillar.Sport,
-			}}
 			commentId={123}
 			authStatus={undefined}
 		/>
 	</div>
 );
+Dialog.decorators = [
+	splitTheme([
+		{
+			display: ArticleDisplay.Standard,
+			design: ArticleDesign.Standard,
+			theme: Pillar.Sport,
+		},
+	]),
+];
