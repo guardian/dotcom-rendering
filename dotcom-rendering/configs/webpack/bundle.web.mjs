74d850411b18e5c916ee2eb44e73976c8727c23e
Alex Sanders <alex@sndrs.dev>
2023-12-18 15:32:07 +0000

    Proposal for a new way of configuring webpack (#8777)
    * add a proposal for a new way of configuring webpack
    
    A way of using webpack webpack so that:
    
    1. there is a one-to-one mapping between webpack bundle configs and built bundles e.g. 
        - `bundle.apps.mjs` -> `dist/apps/*`
        - `bundle.web.mjs` -> `dist/web/*`
        - `bundle.web.variant.mjs` -> `dist/web.variant/*`
    1. the main way-in to build the bundles is an orthodox, top level `webpack.config` which composes the bundle configs
    3. bundle configs themselves live in the `configs/webpack` directory
    4. each bundle config should be a functioning, viable, standalone config
    1. building the configs generates a TypeScript manifest of the bundles in the app, for type safety on the server
    2. organises `dist` into bundle directories, rather than by filename 
    
    ---------
    
    Co-authored-by: Max Duval <max.duval@theguardian.com>
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

---
+++ dotcom-rendering/configs/webpack/bundle.web.mjs
@@ -0,0 +1,108 @@
+// This config is part of a proposed new webpack setup, that provides a
+// one-to-one relation between ./configs/webpack/bundle.*.mjs configs and output
+// bundles.
+//
+// It's not in use yet, and neither is the ./configs dir.
+//
+// If you want to edit the working webpack config, look in:
+// ../../scripts/webpack/webpack.config.js.
+
+import { WebpackManifestPlugin } from 'webpack-manifest-plugin';
+import { merge } from 'webpack-merge';
+import { getBrowserTargets } from '../../webpack/browser-targets.js';
+import { base } from './base.mjs';
+
+/** @type {import("webpack").Configuration} */
+export const web = {
+	entry: {
+		index: './src/client/main.web.ts',
+		debug: './src/client/debug/debug.ts',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.[jt]sx?|mjs$/,
+				exclude: {
+					and: [/node_modules/],
+					not: [
+						// Include all @guardian modules, except automat-modules
+						/@guardian\/(?!(automat-modules))/,
+						// Include the dynamic-import-polyfill
+						/dynamic-import-polyfill/,
+					],
+				},
+				use: [
+					{
+						loader: 'swc-loader',
+						options: {
+							jsc: {
+								parser: {
+									syntax: 'typescript',
+									tsx: true,
+									decorators: false,
+									dynamicImport: true,
+								},
+								transform: {
+									react: {
+										runtime: 'automatic',
+										importSource: '@emotion/react',
+									},
+								},
+							},
+							sourceMaps: true,
+							env: {
+								dynamicImport: true,
+								targets: getBrowserTargets(),
+							},
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/,
+				use: ['to-string-loader', 'css-loader'],
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: '@svgr/webpack',
+						options: {
+							/** this ensures that we keep the viewBox for imported SVGs */
+							svgo: false,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [new WebpackManifestPlugin()],
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				// our own chunk, which is shared between all bundles
+				frameworks: {
+					test: /[\\/]node_modules[\\/](preact|react-is|hoist-non-react-statistics|swr|@emotion|stylis)[\\/]/,
+					chunks: 'all',
+					name: 'frameworks',
+					enforce: true,
+				},
+				// defining our own chunk above overrides the webpack defaults,
+				// so now we restore them
+				// https://webpack.js.org/plugins/split-chunks-plugin/#optimizationsplitchunks
+				defaultVendors: {
+					test: /[\\/]node_modules[\\/]/,
+					priority: -10,
+					reuseExistingChunk: true,
+				},
+				default: {
+					minChunks: 2,
+					priority: -20,
+					reuseExistingChunk: true,
+				},
+			},
+		},
+	},
+};
+
+export default merge(base(import.meta.url), web);
