2154e57654262fe5e288e129062944868535a59a
Anna Beddow <anna.beddow@gmail.com>
2023-12-18 17:47:25 +0000

    Use all themes in stories

--- dotcom-rendering/src/components/QandaAtom.stories.tsx
+++ dotcom-rendering/src/components/QandaAtom.stories.tsx
@@ -1,10 +1,13 @@
+import { ArticleDesign, ArticleDisplay } from '@guardian/libs';
 import type { Meta, StoryObj } from '@storybook/react';
 import { splitTheme } from '../../.storybook/decorators/splitThemeDecorator';
 import {
 	imageStoryExpanded,
 	imageStoryWithCreditExpanded,
 } from '../../fixtures/manual/qandaAtom';
+import { getAllThemes } from '../lib/format';
 import { QandaAtom as QandaAtomComponent } from './QandaAtom.importable';
+
 const meta: Meta<typeof QandaAtomComponent> = {
 	title: 'Components/Q and A Atom',
 	component: QandaAtomComponent,
@@ -14,17 +17,38 @@ type Story = StoryObj<typeof QandaAtomComponent>;
 
 export const NewsStoryExpanded: Story = {
 	args: { ...imageStoryExpanded },
-	decorators: [splitTheme()],
+	decorators: [
+		splitTheme(
+			getAllThemes({
+				display: ArticleDisplay.Standard,
+				design: ArticleDesign.Standard,
+			}),
+		),
+	],
 };
 
 export const ListStoryExpanded: Story = {
 	args: { ...imageStoryExpanded },
-	decorators: [splitTheme()],
+	decorators: [
+		splitTheme(
+			getAllThemes({
+				display: ArticleDisplay.Standard,
+				design: ArticleDesign.Standard,
+			}),
+		),
+	],
 };
 
 export const ImageStoryWithCreditExpanded: Story = {
 	args: { ...imageStoryWithCreditExpanded },
-	decorators: [splitTheme()],
+	decorators: [
+		splitTheme(
+			getAllThemes({
+				display: ArticleDisplay.Standard,
+				design: ArticleDesign.Standard,
+			}),
+		),
+	],
 };
 
 export default meta;
