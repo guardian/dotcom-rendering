44e17670317dbc6244bb62f4731cd066429ba72c
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-12-19 10:41:18 +0000

    Port the rest of the tests to the component

--- dotcom-rendering/src/components/SignInGate/gateDesigns/SignInGateMainCheckoutComplete.test.tsx
+++ dotcom-rendering/src/components/SignInGate/gateDesigns/SignInGateMainCheckoutComplete.test.tsx
@@ -1,16 +1,22 @@
 import { render } from '@testing-library/react';
-import { ConfigProvider } from '../../ConfigContext';
-import { SignInGateMainCheckoutComplete } from './SignInGateMainCheckoutComplete';
 import {
 	COMPLETE_REGISTRATION_BUTTON,
+	SIGN_IN_BUTTON,
 	SIGN_IN_INCENTIVES_DIGITAL,
+	SIGN_IN_INCENTIVES_NON_DIGITAL,
 	SUBSCRIPTION_HEADER,
+	SUPPORTER_HEADER,
 } from '../../../lib/signInAfterCheckOutText';
+import { ConfigProvider } from '../../ConfigContext';
+import type { CheckoutCompleteCookieData } from '../types';
+import { SignInGateMainCheckoutComplete } from './SignInGateMainCheckoutComplete';
 
 describe('SignInGateMainCheckoutComplete', () => {
+	const REGISTER_URL = 'https://profile.theguardian.com/register?returnUrl=';
+	const SIGNIN_URL = 'https://profile.theguardian.com/signin?returnUrl=';
 	const signInGateProps = {
-		signInUrl: 'https://profile.theguardian.com/signin?returnUrl=',
-		registerUrl: '/register',
+		signInUrl: SIGNIN_URL,
+		registerUrl: REGISTER_URL,
 		guUrl: 'guUrl',
 		dismissGate: () => {
 			return;
@@ -18,25 +24,27 @@ describe('SignInGateMainCheckoutComplete', () => {
 		ophanComponentId: 'componentId',
 		personaliseSignInGateAfterCheckoutSwitch: true,
 	};
+	const SignInAfterCheckoutComponent = (
+		checkoutCompleteCookieData: CheckoutCompleteCookieData,
+	) => {
+		return (
+			<ConfigProvider
+				value={{ renderingTarget: 'Web', darkModeAvailable: false }}
+			>
+				<SignInGateMainCheckoutComplete
+					{...signInGateProps}
+					checkoutCompleteCookieData={checkoutCompleteCookieData}
+				/>
+			</ConfigProvider>
+		);
+	};
 	describe('it should display the correct copy based on the following variations', () => {
-		const REGISTER_URL =
-			'https://profile.theguardian.com/register?returnUrl=';
-
 		it('user is new and has a digital subscription', () => {
 			const { container } = render(
-				<ConfigProvider
-					value={{ renderingTarget: 'Web', darkModeAvailable: false }}
-				>
-					<SignInGateMainCheckoutComplete
-						{...signInGateProps}
-						{...{
-							checkoutCompleteCookieData: {
-								userType: 'new',
-								product: 'SupporterPlus',
-							},
-						}}
-					/>
-				</ConfigProvider>,
+				SignInAfterCheckoutComponent({
+					userType: 'new',
+					product: 'SupporterPlus',
+				}),
 			);
 			const headline = container.querySelector('h1');
 			const bulletPoints = container.querySelectorAll('li');
@@ -45,6 +53,7 @@ describe('SignInGateMainCheckoutComplete', () => {
 			);
 
 			expect(headline).toHaveTextContent(SUBSCRIPTION_HEADER);
+
 			for (const [index, bullet] of bulletPoints.entries()) {
 				expect(bullet).toHaveTextContent(
 					SIGN_IN_INCENTIVES_DIGITAL[index],
@@ -52,6 +61,126 @@ describe('SignInGateMainCheckoutComplete', () => {
 			}
 			expect(button).toHaveTextContent(COMPLETE_REGISTRATION_BUTTON);
 			expect(button).toHaveAttribute('href', REGISTER_URL);
+		});
+
+		it('user is new and has a paper subscription', () => {
+			const { container } = render(
+				SignInAfterCheckoutComponent({
+					userType: 'guest',
+					product: 'Paper',
+				}),
+			);
+			const headline = container.querySelector('h1');
+			const bulletPoints = container.querySelectorAll('li');
+			const button = container.querySelector(
+				`[data-testid="sign-in-gate-main_register"]`,
+			);
+
+			expect(headline).toHaveTextContent(SUBSCRIPTION_HEADER);
+
+			for (const [index, bullet] of bulletPoints.entries()) {
+				expect(bullet).toHaveTextContent(
+					SIGN_IN_INCENTIVES_NON_DIGITAL[index],
+				);
+			}
+			expect(button).toHaveTextContent(COMPLETE_REGISTRATION_BUTTON);
+			expect(button).toHaveAttribute('href', REGISTER_URL);
+		});
+
+		it('user is new and has a contributor', () => {
+			const { container } = render(
+				SignInAfterCheckoutComponent({
+					userType: 'new',
+					product: 'Contribution',
+				}),
+			);
+			const headline = container.querySelector('h1');
+			const bulletPoints = container.querySelectorAll('li');
+			const button = container.querySelector(
+				`[data-testid="sign-in-gate-main_register"]`,
+			);
+
+			expect(headline).toHaveTextContent(SUPPORTER_HEADER);
+
+			for (const [index, bullet] of bulletPoints.entries()) {
+				expect(bullet).toHaveTextContent(
+					SIGN_IN_INCENTIVES_NON_DIGITAL[index],
+				);
+			}
+			expect(button).toHaveTextContent(COMPLETE_REGISTRATION_BUTTON);
+			expect(button).toHaveAttribute('href', REGISTER_URL);
+		});
+
+		it('user is existing and has a digital subscription', () => {
+			const { container } = render(
+				SignInAfterCheckoutComponent({
+					userType: 'current',
+					product: 'SupporterPlus',
+				}),
+			);
+			const headline = container.querySelector('h1');
+			const bulletPoints = container.querySelectorAll('li');
+			const button = container.querySelector(
+				`[data-testid="sign-in-gate-main_register"]`,
+			);
+
+			expect(headline).toHaveTextContent(SUBSCRIPTION_HEADER);
+
+			for (const [index, bullet] of bulletPoints.entries()) {
+				expect(bullet).toHaveTextContent(
+					SIGN_IN_INCENTIVES_DIGITAL[index],
+				);
+			}
+			expect(button).toHaveTextContent(SIGN_IN_BUTTON);
+			expect(button).toHaveAttribute('href', SIGNIN_URL);
+		});
+
+		it('user is existing and has a paper subscription', () => {
+			const { container } = render(
+				SignInAfterCheckoutComponent({
+					userType: 'current',
+					product: 'Paper',
+				}),
+			);
+			const headline = container.querySelector('h1');
+			const bulletPoints = container.querySelectorAll('li');
+			const button = container.querySelector(
+				`[data-testid="sign-in-gate-main_register"]`,
+			);
+
+			expect(headline).toHaveTextContent(SUBSCRIPTION_HEADER);
+
+			for (const [index, bullet] of bulletPoints.entries()) {
+				expect(bullet).toHaveTextContent(
+					SIGN_IN_INCENTIVES_NON_DIGITAL[index],
+				);
+			}
+			expect(button).toHaveTextContent(SIGN_IN_BUTTON);
+			expect(button).toHaveAttribute('href', SIGNIN_URL);
+		});
+
+		it('user is existing and has is a contributor', () => {
+			const { container } = render(
+				SignInAfterCheckoutComponent({
+					userType: 'current',
+					product: 'Contribution',
+				}),
+			);
+			const headline = container.querySelector('h1');
+			const bulletPoints = container.querySelectorAll('li');
+			const button = container.querySelector(
+				`[data-testid="sign-in-gate-main_register"]`,
+			);
+
+			expect(headline).toHaveTextContent(SUPPORTER_HEADER);
+
+			for (const [index, bullet] of bulletPoints.entries()) {
+				expect(bullet).toHaveTextContent(
+					SIGN_IN_INCENTIVES_NON_DIGITAL[index],
+				);
+			}
+			expect(button).toHaveTextContent(SIGN_IN_BUTTON);
+			expect(button).toHaveAttribute('href', SIGNIN_URL);
 		});
 	});
 });
