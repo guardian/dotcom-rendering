4cfe6cd5b231b2e07aa5ed8ae6a362202ce3f957
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-12-19 10:19:57 +0000

    Move the text constants to its own file

--- dotcom-rendering/src/components/SignInGate/gateDesigns/SignInGateMainCheckoutComplete.test.tsx
+++ dotcom-rendering/src/components/SignInGate/gateDesigns/SignInGateMainCheckoutComplete.test.tsx
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
 import { ConfigProvider } from '../../ConfigContext';
 import { SignInGateMainCheckoutComplete } from './SignInGateMainCheckoutComplete';
+import {
+	COMPLETE_REGISTRATION_BUTTON,
+	SIGN_IN_INCENTIVES_DIGITAL,
+	SUBSCRIPTION_HEADER,
+} from '../../../lib/signInAfterCheckOutText';
 
 describe('SignInGateMainCheckoutComplete', () => {
 	const signInGateProps = {
@@ -16,11 +21,6 @@ describe('SignInGateMainCheckoutComplete', () => {
 	describe('it should display the correct copy based on the following variations', () => {
 		const REGISTER_URL =
 			'https://profile.theguardian.com/register?returnUrl=';
-		const SIGN_IN_INCENTIVES_DIGITAL = [
-			'Supporter rewards – unlock the benefits of your support',
-			'Incisive analysis and original reporting direct to your inbox, with our newsletters',
-			'Get involved in the discussion – comment on stories',
-		];
 
 		it('user is new and has a digital subscription', () => {
 			const { container } = render(
@@ -44,13 +44,13 @@ describe('SignInGateMainCheckoutComplete', () => {
 				`[data-testid="sign-in-gate-main_register"]`,
 			);
 
-			expect(headline).toHaveTextContent('Thank you for subscribing');
+			expect(headline).toHaveTextContent(SUBSCRIPTION_HEADER);
 			for (const [index, bullet] of bulletPoints.entries()) {
 				expect(bullet).toHaveTextContent(
 					SIGN_IN_INCENTIVES_DIGITAL[index],
 				);
 			}
-			expect(button).toHaveTextContent('Complete registration');
+			expect(button).toHaveTextContent(COMPLETE_REGISTRATION_BUTTON);
 			expect(button).toHaveAttribute('href', REGISTER_URL);
 		});
 	});
