ac517398d545d3e21004a875b9b6b9651b39553c
Ravi <7014230+arelra@users.noreply.github.com>
2023-12-20 12:40:28 +0000

    Cluster mode
    Co-authored-by: Ioanna Kokkini <ioannakok@users.noreply.github.com>
    Co-authored-by: George B <705427+georgeblahblah@users.noreply.github.com>

--- dotcom-rendering/src/server/server.ts
+++ dotcom-rendering/src/server/server.ts
@@ -1,3 +1,5 @@
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
 import { devServer } from './server.dev';
 import { prodServer } from './server.prod';
 
@@ -8,5 +10,27 @@ export default devServer;
 
 // this is the actual production server
 if (process.env.NODE_ENV === 'production') {
-	prodServer();
+	const totalCPUs = availableParallelism();
+	if (cluster.isPrimary) {
+		// eslint-disable-next-line no-console -- testing
+		console.log(`Number of CPUs is ${totalCPUs}`);
+		// eslint-disable-next-line no-console -- testing
+		console.log(`Primary ${process.pid} is running`);
+
+		for (let i = 0; i < totalCPUs; i++) {
+			cluster.fork();
+		}
+
+		cluster.on('exit', (worker) => {
+			// eslint-disable-next-line no-console -- testing
+			console.log(
+				`worker died with pid: ${worker.process.pid ?? 'unknown pid'}`,
+			);
+			// eslint-disable-next-line no-console -- testing
+			console.log('Forking another worker');
+			cluster.fork();
+		});
+	} else {
+		prodServer();
+	}
 }
