9363cc5f16bc062b912d2c7507fa21ecbe5ffec5
Alina Boghiu <alina.boghiu@gmail.com>
2023-12-20 16:45:16 +0000

    test t4g.small in CODE

--- dotcom-rendering/cdk/bin/cdk.ts
+++ dotcom-rendering/cdk/bin/cdk.ts
@@ -23,7 +23,7 @@ new DotcomRendering(app, 'DotcomRendering-CODE', {
 	stage: 'CODE',
 	minCapacity: 1,
 	maxCapacity: 4,
-	instanceType: 't4g.micro',
+	instanceType: 't4g.small',
 });
 
 new DotcomRendering(app, 'DotcomRendering-front-web-CODE', {
