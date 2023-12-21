43befb4d77d3f807adbbae2392593716c25a5a26
Ravi <7014230+arelra@users.noreply.github.com>
2023-12-21 14:25:35 +0000

    Don't json parse the load test article

--- dotcom-rendering/scripts/perf/k6/k6.mjs
+++ dotcom-rendering/scripts/perf/k6/k6.mjs
@@ -2,19 +2,18 @@ import { check } from 'k6';
 import { SharedArray } from 'k6/data';
 import http from 'k6/http';
 
-const PORT = 3030;
+const PORT = 9000;
 
 /** @type {import('k6/data').SharedArray} */
 const jsonPayload = new SharedArray('jsonPayload', function () {
-	const f = JSON.parse(open('./article-nier-automata.json'));
-	const string = JSON.stringify(f);
-	return [string];
+	const article = open('./article-nier-automata.json');
+	return [article];
 });
 
 /** @type {import('k6/options').Options} */
 export const options = {
 	vus: 10,
-	duration: '60s',
+	duration: '10s',
 };
 
 // eslint-disable-next-line import/no-default-export -- k6 requires a default export
