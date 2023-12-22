8bb42dc91ce77c40637ab4b747628eeee265aeb1
Tom Forbes <tom.forbes@theguardian.com>
2023-12-22 09:39:48 +0000

    Remove dvh from banner wrapper

--- dotcom-rendering/src/layouts/lib/stickiness.tsx
+++ dotcom-rendering/src/layouts/lib/stickiness.tsx
@@ -37,7 +37,6 @@ const bannerWrapper = css`
 	bottom: 0;
 	${getZIndexImportant('banner')}
 	max-height: 80vh;
-	max-height: 80dvh;
 	overflow: visible;
 	/* stylelint-disable-next-line declaration-no-important */
 	width: 100% !important;
