93269406184d0044f49d1452c5a2084aa7066b04
Alina Boghiu <alina.boghiu@gmail.com>
2023-12-27 14:37:46 +0000

    Allow typos in Youtube ids for AMP (#10003)
    * Youtube ignores characters after the 11th so we can allow longer ids
    
    * update youtube regex tests to allow for longer ids

--- dotcom-rendering/src/lib/get-video-id.amp.test.ts
+++ dotcom-rendering/src/lib/get-video-id.amp.test.ts
@@ -2,12 +2,16 @@ import { getIdFromUrl } from './get-video-id.amp';
 
 describe('getIdFromUrl', () => {
 	it('Returns matching ID for YouTube formats', () => {
-		const youtubeRegEx = '^[a-zA-Z0-9_-]{11}$';
+		const youtubeRegEx = '^[a-zA-Z0-9_-]{11,}$';
 
 		const formats = [
 			{
 				url: 'http://www.youtube.com/ytscreeningroom?v=NRHEIGHTx8I',
 				id: 'NRHEIGHTx8I',
+			},
+			{
+				url: 'http://www.youtube.com/ytscreeningroom?v=NRHEIGHTx8Ixyz',
+				id: 'NRHEIGHTx8Ixyz',
 			},
 			{
 				url: 'http://www.youtube.com/ytscreeningroom?v=NRH_IGHTx8I',
