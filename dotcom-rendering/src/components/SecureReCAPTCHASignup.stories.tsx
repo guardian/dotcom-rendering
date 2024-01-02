02332e6aea9fba11a51157688672e7f4ec62af46
Anna Beddow <anna.beddow@gmail.com>
2024-01-02 15:56:03 +0000

    Add stories for newsletter signup

---
+++ dotcom-rendering/src/components/SecureReCAPTCHASignup.stories.tsx
@@ -0,0 +1,34 @@
+import { WebEmailSignUp as SignupComponent } from './WebEmailSignup.tsx';
+import type { Meta, StoryObj } from '@storybook/react';
+const meta: Meta<typeof SignupComponent> = {
+	title: 'Components/Newsletter Signup',
+	component: SignupComponent,
+};
+
+const defaultArgs = {
+	index: 10,
+	identityName: 'the-recap',
+	description:
+		'The best of our sports journalism from the past seven days and a heads-up on the weekend’s action',
+	name: 'The Recap',
+	frequency: 'Weekly',
+	successDescription: "We'll send you The Recap every week",
+	theme: 'sport',
+};
+type Story = StoryObj<typeof SignupComponent>;
+
+export const DefaultStory: Story = {
+	args: {
+		hidePrivacyMessage: true,
+		...defaultArgs,
+	},
+};
+
+export const DefaultStoryWithPrivacy: Story = {
+	args: {
+		hidePrivacyMessage: false,
+		...defaultArgs,
+	},
+};
+
+export default meta;
