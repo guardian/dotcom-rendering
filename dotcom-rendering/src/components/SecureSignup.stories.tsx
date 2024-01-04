0143e07356bb5a73ad3314f22d1d5c74e00f5e0d
Anna Beddow <anna.beddow@gmail.com>
2024-01-04 10:40:51 +0000

    Fix test

--- dotcom-rendering/src/components/SecureSignup.stories.tsx
+++ dotcom-rendering/src/components/SecureSignup.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { WebEmailSignUp as SignupComponent } from './EmailSignUpWrapper';
+import { EmailSignUpWrapper as SignupComponent } from './EmailSignUpWrapper';
 
 const meta: Meta<typeof SignupComponent> = {
 	title: 'Components/Newsletter Signup',
