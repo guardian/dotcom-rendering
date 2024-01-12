bc5456d11e962e73aa661b280511197111e7140c
Tom Forbes <tom.forbes@theguardian.com>
2024-01-12 08:56:59 +0000

    Simpify assets path selection

--- dotcom-rendering/src/client/decidePublicPath.test.ts
+++ dotcom-rendering/src/client/decidePublicPath.test.ts
@@ -1,14 +1,5 @@
 import { decidePublicPath } from './decidePublicPath';
 
-const mockHostname = (hostname: string | undefined) => {
-	Object.defineProperty(window, 'location', {
-		value: {
-			hostname,
-		},
-		writable: true,
-	});
-};
-
 const mockFrontendAssetsFullURL = (frontendAssetsFullURL: string) => {
 	Object.defineProperty(window, 'guardian', {
 		value: {
@@ -24,54 +15,22 @@ describe('decidePublicPath', () => {
 	beforeEach(() => {
 		jest.resetModules();
 
-		// Ensures we always set a hostname in the test
-		// and the default hostname isn't accidentally used
-		mockHostname(undefined);
-
 		mockFrontendAssetsFullURL('https://assets.guim.co.uk/');
 
 		process.env = { NODE_ENV: undefined, HOSTNAME: undefined };
 	});
 
-	it('localhost with development flag', () => {
+	it('with development flag', () => {
 		process.env.NODE_ENV = 'development';
-		mockHostname('localhost');
-		expect(decidePublicPath()).toEqual('http://localhost:3030/assets/');
+		expect(decidePublicPath()).toEqual('/assets/');
 	});
 
-	it('localhost with production flag', () => {
+	it('with production flag', () => {
 		process.env.NODE_ENV = 'production';
-		mockHostname('localhost');
-		expect(decidePublicPath()).toEqual('/assets/');
+		expect(decidePublicPath()).toEqual('https://assets.guim.co.uk/assets/');
 	});
 
-	it('localhost with no flag', () => {
-		mockHostname('localhost');
-		expect(decidePublicPath()).toEqual('/assets/');
-	});
-
-	it('hostname set with development flag', () => {
-		process.env.NODE_ENV = 'development';
-		process.env.HOSTNAME = 'some-other-hostname.com';
-		mockHostname('some-other-hostname.com');
-		expect(decidePublicPath()).toEqual('/assets/');
-	});
-
-	it('hostname set with production flag', () => {
-		process.env.NODE_ENV = 'production';
-		process.env.HOSTNAME = 'some-other-hostname.com';
-		mockHostname('some-other-hostname.com');
-		expect(decidePublicPath()).toEqual('/assets/');
-	});
-
-	it('hostname set with no flag', () => {
-		process.env.HOSTNAME = 'some-other-hostname.com';
-		mockHostname('some-other-hostname.com');
-		expect(decidePublicPath()).toEqual('/assets/');
-	});
-
-	it('no hostname env or flag set', () => {
-		mockHostname('theguardian.com');
+	it('with no flag', () => {
 		expect(decidePublicPath()).toEqual('https://assets.guim.co.uk/assets/');
 	});
 });
--- dotcom-rendering/src/client/decidePublicPath.ts
+++ dotcom-rendering/src/client/decidePublicPath.ts
@@ -5,17 +5,7 @@
  */
 export const decidePublicPath = (): string => {
 	const isDev = process.env.NODE_ENV === 'development';
-	if (isDev && window.location.hostname === 'localhost') {
-		// Use the absolute path when in development mode and running locally
-		// Ensures asset paths are correct relative to Frontend
-		return 'http://localhost:3030/assets/';
-	} else if (
-		window.location.hostname === (process.env.HOSTNAME ?? 'localhost')
-	) {
-		// Use a relative path when hostname is set or using localhost in non-dev environments
-		// e.g. when using a reverse proxy, or running in CI
-		return '/assets/';
-	} else {
-		return `${window.guardian.config.frontendAssetsFullURL}assets/`;
-	}
+	return isDev
+		? '/assets/'
+		: `${window.guardian.config.frontendAssetsFullURL}assets/`;
 };
