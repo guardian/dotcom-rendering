8b0b062834ead968222199e03c218b97dafe9bcb
Charlotte <charlotte.emms@theguardian.com>
2024-01-15 16:43:04 +0000

    chore: remove unused and deprecated babel plugin

--- dotcom-rendering/babel.config.js
+++ dotcom-rendering/babel.config.js
@@ -4,7 +4,6 @@ module.exports = {
 		// They can dramatically alter the build size
 		'@babel/plugin-syntax-dynamic-import',
 		'@babel/plugin-transform-react-jsx',
-		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-proposal-optional-chaining',
 		'@babel/plugin-proposal-nullish-coalescing-operator',
 		'babel-plugin-px-to-rem',
