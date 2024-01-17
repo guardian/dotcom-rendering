0ca767e34b47b51a8f854daeb9cdff44b134ddf6
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-01-17 18:58:24 +0000

    Reintroduce article metadata from `leftCol` breakpoint upwards on apps
    Fixes a bug where the article metadata was visible below this breakpoint but hidden above it. Uses the `ArticleMeta` component from this breakpoint, as the new `ArticleMetaApps` component isn't supported at this width.

--- dotcom-rendering/src/layouts/CommentLayout.tsx
+++ dotcom-rendering/src/layouts/CommentLayout.tsx
@@ -569,32 +569,62 @@ export const CommentLayout = (props: WebProps | AppsProps) => {
 						<GridItem area="meta" element="aside">
 							<div css={maxWidth}>
 								{isApps ? (
-									<Hide when="above" breakpoint="leftCol">
-										<ArticleMetaApps
-											branding={branding}
-											format={format}
-											pageId={article.pageId}
-											webTitle={article.webTitle}
-											byline={article.byline}
-											tags={article.tags}
-											primaryDateline={
-												article.webPublicationDateDisplay
-											}
-											secondaryDateline={
-												article.webPublicationSecondaryDateDisplay
-											}
-											isCommentable={
-												article.isCommentable
-											}
-											discussionApiUrl={
-												article.config.discussionApiUrl
-											}
-											shortUrlId={
-												article.config.shortUrlId
-											}
-											ajaxUrl={article.config.ajaxUrl}
-										></ArticleMetaApps>
-									</Hide>
+									<>
+										<Hide when="above" breakpoint="leftCol">
+											<ArticleMetaApps
+												branding={branding}
+												format={format}
+												pageId={article.pageId}
+												webTitle={article.webTitle}
+												byline={article.byline}
+												tags={article.tags}
+												primaryDateline={
+													article.webPublicationDateDisplay
+												}
+												secondaryDateline={
+													article.webPublicationSecondaryDateDisplay
+												}
+												isCommentable={
+													article.isCommentable
+												}
+												discussionApiUrl={
+													article.config
+														.discussionApiUrl
+												}
+												shortUrlId={
+													article.config.shortUrlId
+												}
+												ajaxUrl={article.config.ajaxUrl}
+											></ArticleMetaApps>
+										</Hide>
+										<Hide when="below" breakpoint="leftCol">
+											<ArticleMeta
+												branding={branding}
+												format={format}
+												pageId={article.pageId}
+												webTitle={article.webTitle}
+												byline={article.byline}
+												tags={article.tags}
+												primaryDateline={
+													article.webPublicationDateDisplay
+												}
+												secondaryDateline={
+													article.webPublicationSecondaryDateDisplay
+												}
+												isCommentable={
+													article.isCommentable
+												}
+												discussionApiUrl={
+													article.config
+														.discussionApiUrl
+												}
+												shortUrlId={
+													article.config.shortUrlId
+												}
+												ajaxUrl={article.config.ajaxUrl}
+											/>
+										</Hide>
+									</>
 								) : (
 									<ArticleMeta
 										branding={branding}
