84c795d9cd2482f2f73e3c2e9e7091e7b1af3cb9
Tom Forbes <tom.forbes@theguardian.com>
2024-01-17 11:09:12 +0000

    Simpify assets path selection (#10186)
    * Simpify assets path selection
    
    * undo timeout change
    
    * better tests
    
    * support CI

--- dotcom-rendering/src/client/decidePublicPath.ts
+++ dotcom-rendering/src/client/decidePublicPath.ts
@@ -5,17 +5,9 @@
  */
 export const decidePublicPath = (): string => {
 	const isDev = process.env.NODE_ENV === 'development';
-	if (isDev && window.location.hostname === 'localhost') {
-		// Use the absolute path when in development mode and running locally
-		// Ensures asset paths are correct relative to Frontend
-		return 'http://localhost:3030/assets/';
-	} else if (
-		window.location.hostname === (process.env.HOSTNAME ?? 'localhost')
-	) {
-		// Use a relative path when hostname is set or using localhost in non-dev environments
-		// e.g. when using a reverse proxy, or running in CI
-		return '/assets/';
-	} else {
-		return `${window.guardian.config.frontendAssetsFullURL}assets/`;
-	}
+	const isLocalHost = window.location.hostname === 'localhost';
+	// Use relative path if running locally or in CI
+	return isDev || isLocalHost
+		? '/assets/'
+		: `${window.guardian.config.frontendAssetsFullURL}assets/`;
 };
