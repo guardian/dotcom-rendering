fe1767caac0b834ffc2484f45b803d08ede19f20
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-01-19 15:02:28 +0000

    Removed component event as new api is moving to url params instead of object

--- dotcom-rendering/src/components/Analytics.amp.tsx
+++ dotcom-rendering/src/components/Analytics.amp.tsx
@@ -14,14 +14,6 @@ type Props = {
 	analytics: AnalyticsModel;
 };
 
-const baseComponentEvent = encodeURI(
-	JSON.stringify({
-		consentJurisdiction: 'CONSENT_JURISDICTION',
-		consentUUID: 'CONSENT_UUID',
-		consent: 'CONSENT_STRING',
-	}),
-);
-
 export const Analytics = ({
 	analytics: {
 		gaTracker,
@@ -36,32 +28,21 @@ export const Analytics = ({
 	},
 }: Props) => {
 	const scripts: string[] = [
-		`<amp-analytics config="https://ophan.theguardian.com/amp.json" data-credentials="include" ></amp-analytics>`,
-		`<amp-analytics config="https://ophan.theguardian.com/amp.json" data-block-on-consent="_till_responded" data-credentials="include">
+		// `<amp-analytics config="https://ophan.theguardian.com/amp.json" data-credentials="include" ></amp-analytics>`,
+		`<amp-analytics config="https://ophan.theguardian.com/amp.json" data-credentials="include" data-block-on-consent="_till_responded" data-credentials="include">
 			<script type="application/json">
 				{
 					"requests": {
 						"pageViewWithConsentTest": "http://localhost:8001/receive?componentEvent=\${componentEvent}",
 						"pageViewWithConsentTestParams": "http://localhost:8001/receive?consentJurisdiction=\${consentJurisdiction}&consentUUID=\${consentUUID}&consent=\${consent}",
-						"pageViewWithConsent": "\${additionalBase}&componentEvent=\${componentEvent}"
+						"pageViewWithConsent": "\${additionalBase}&consentJurisdiction=\${consentJurisdiction}&consentUUID=\${consentUUID}&consent=\${consent}"
 					},
 					"triggers": {
 						"trackPageviewTcf": {
 							"on": "visible",
-							"request": "pageViewWithConsentTestParams",
+							"request": "pageViewWithConsent",
 							"selector": ".amp-geo-group-tcfv2",
 							"vars": {
-								"componentEvent": "${baseComponentEvent
-									.replace('CONSENT_JURISDICTION', 'TCF')
-									.replace(
-										'CONSENT_UUID',
-										'${clientId(consentUUID)}',
-									)
-									.replace(
-										'CONSENT_STRING',
-										'${consentString}',
-									)
-									.toString()}",
 								"consentUUID": "\${clientId(consentUUID)}",
 								"consent": "\${consentString}",
 								"consentJurisdiction": "TCF"
@@ -70,20 +51,9 @@ export const Analytics = ({
 						},
 						"trackPageviewCcpa": {
 							"on": "visible",
-							"request": "pageViewWithConsentTest",
+							"request": "pageViewWithConsentTestParams",
 							"selector": ".amp-geo-group-ccpa",
 							"vars": {
-								"componentEvent": "${baseComponentEvent
-									.replace('CONSENT_JURISDICTION', 'CCPA')
-									.replace(
-										'CONSENT_UUID',
-										'${clientId(ccpaUUID)}',
-									)
-									.replace(
-										'CONSENT_STRING',
-										'$EQUALS(${consentState}, sufficient)',
-									)
-									.toString()}",
 								"consentUUID": "\${clientId(ccpaUUID)}",
 								"consent": "$EQUALS(\${consentState}, sufficient)",
 								"consentJurisdiction": "CCPA"
@@ -92,31 +62,18 @@ export const Analytics = ({
 						},
 						"trackPageviewAus": {
 							"on": "visible",
-							"request": "pageViewWithConsentTest",
+							"request": "pageViewWithConsentTestParams",
 							"selector": ".amp-geo-group-aus",
 							"vars": {
-								"componentEvent": "${baseComponentEvent
-									.replace('CONSENT_JURISDICTION', 'AUS')
-									.replace(
-										'CONSENT_UUID',
-										'${clientId(ccpaUUID)}',
-									)
-									.replace(
-										'CONSENT_STRING',
-										'$EQUALS(${consentState}, sufficient)',
-									)
-									.toString()}",
 								"consentUUID": "\${clientId(ccpaUUID)}",
 								"consent": "$EQUALS(\${consentState}, sufficient)",
 								"consentJurisdiction": "AUS"
-
 							}
 						}
 					},
 					"transport": {
 						"beacon": false,
 						"xhrpost": false,
-						"useBody": false,
 						"image": true
 					}
 				}
