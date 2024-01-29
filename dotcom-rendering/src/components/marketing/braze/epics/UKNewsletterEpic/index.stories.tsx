75a5ea18e69c8f97de15d973b2889d346021bbd8
Tom Wey <tom.wey@theguardian.com>
2024-01-29 17:10:02 +0000

    Fixing stories

--- dotcom-rendering/src/components/marketing/braze/epics/UKNewsletterEpic/index.stories.tsx
+++ dotcom-rendering/src/components/marketing/braze/epics/UKNewsletterEpic/index.stories.tsx
@@ -1,96 +1,67 @@
-import React, { ReactElement } from 'react';
 import { BrazeEndOfArticleComponent } from '../../BrazeEndOfArticleComponent';
 import {
-	StorybookWrapper,
-	mockSubscribe,
-	mockFetchEmail,
 	mockButtonClick,
 	mockComponentEvent,
-} from '../utils/StorybookWrapper';
-import { knobsData } from '../utils/knobsData';
-import {
-	coreArgTypes,
-	ophanComponentIdArgType,
-} from '../storybookCommon/argTypes';
-import type { BrazeMessageProps } from '.';
+	mockFetchEmail,
+	mockSubscribe,
+} from '../../storybook/mocks';
 
-export default {
-	component: 'UKNewsletterEpic',
-	title: 'EndOfArticle/UKNewsletterEpic',
-	parameters: {
-		articleContext: {
-			disable: false,
-		},
-	},
-	argTypes: {
-		...coreArgTypes,
-		...ophanComponentIdArgType,
-		header: {
-			name: 'header',
-			type: { name: 'string', required: true },
-			description: 'Header text',
-		},
-		frequency: {
-			name: 'frequency',
-			type: { name: 'string', required: true },
-			description: 'Text description of how often the email is sent',
-		},
-		paragraph1: {
-			name: 'paragraph1',
-			type: { name: 'string', required: true },
-			description: 'First paragraph',
-		},
-		paragraph2: {
-			name: 'paragraph2',
-			type: { name: 'string', required: false },
-			description: 'Second paragraph',
-		},
-	},
-};
-
-const StoryTemplate = (
-	args: BrazeMessageProps & { componentName: string },
-): ReactElement | null => {
+export const defaultStory = () => {
 	const brazeMessageProps = {
-		header: args.header,
-		frequency: args.frequency,
-		paragraph1: args.paragraph1,
-		paragraph2: args.paragraph2,
-		ophanComponentId: args.ophanComponentId,
+		slotName: 'EndOfArticle',
+		header: 'The Morning Briefing',
+		frequency: 'Every day',
+		paragraph1:
+			'Whether it’s the latest manoeuvring in global politics or the ‘and finally’ story that everyone’s talking about, you’ll be bang up to date with the news that counts.',
+		paragraph2:
+			'We thought you should know this newsletter may contain information about Guardian products and services.',
+		componentName: 'UKNewsletterEpic',
+		ophanComponentId: 'example_ophan_component_id',
 	};
 
-	knobsData.set({ ...brazeMessageProps, componentName: args.componentName });
-
 	return (
-		<StorybookWrapper>
-			<BrazeEndOfArticleComponent
-				componentName={args.componentName}
-				brazeMessageProps={brazeMessageProps}
-				subscribeToNewsletter={() => mockSubscribe('4156')}
-				fetchEmail={() => mockFetchEmail()}
-				logButtonClickWithBraze={(internalButtonId) =>
-					mockButtonClick(internalButtonId)
-				}
-				submitComponentEvent={(componentEvent) =>
-					mockComponentEvent(componentEvent)
-				}
-			/>
-		</StorybookWrapper>
+		<BrazeEndOfArticleComponent
+			componentName={'UKNewsletterEpic'}
+			brazeMessageProps={brazeMessageProps}
+			subscribeToNewsletter={() => mockSubscribe('4156')}
+			fetchEmail={() => mockFetchEmail()}
+			logButtonClickWithBraze={(internalButtonId) =>
+				mockButtonClick(internalButtonId)
+			}
+			submitComponentEvent={(componentEvent) =>
+				mockComponentEvent(componentEvent)
+			}
+		/>
 	);
 };
+defaultStory.storyName = 'default';
 
-export const DefaultStory = StoryTemplate.bind({});
+export const Failure = () => {
+	const brazeMessageProps = {
+		slotName: 'EndOfArticle',
+		header: 'The Morning Briefing',
+		frequency: 'Every day',
+		paragraph1:
+			'Whether it’s the latest manoeuvring in global politics or the ‘and finally’ story that everyone’s talking about, you’ll be bang up to date with the news that counts.',
+		paragraph2:
+			'We thought you should know this newsletter may contain information about Guardian products and services.',
+		componentName: 'UKNewsletterEpic',
+		ophanComponentId: 'example_ophan_component_id',
+	};
 
-DefaultStory.args = {
-	slotName: 'EndOfArticle',
-	header: 'The Morning Briefing',
-	frequency: 'Every day',
-	paragraph1:
-		'Whether it’s the latest manoeuvring in global politics or the ‘and finally’ story that everyone’s talking about, you’ll be bang up to date with the news that counts.',
-	paragraph2:
-		'We thought you should know this newsletter may contain information about Guardian products and services.',
-	componentName: 'UKNewsletterEpic',
-	ophanComponentId: 'example_ophan_component_id',
+	return (
+		<BrazeEndOfArticleComponent
+			componentName={'UKNewsletterEpic'}
+			brazeMessageProps={brazeMessageProps}
+			subscribeToNewsletter={() => mockSubscribe('4156')}
+			fetchEmail={() => mockFetchEmail()}
+			logButtonClickWithBraze={(internalButtonId) =>
+				mockButtonClick(internalButtonId)
+			}
+			submitComponentEvent={(componentEvent) =>
+				mockComponentEvent(componentEvent)
+			}
+		/>
+	);
 };
-
-DefaultStory.storyName = 'UKNewsletterEpic';
+Failure.storyName = 'Newsletter subscribe failure';
