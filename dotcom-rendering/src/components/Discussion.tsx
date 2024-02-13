c039d12d5eed7bb3d4b18f0bef7b7d71b4bd9e31
Max Duval <max.duval@theguardian.com>
2024-02-13 11:20:20 +0000

    feat(Discussion): add an abort controller (#10511)
    whenever the `useEffect` where we call
    `getDiscussion` is re-run, we want to ensure that
    a preceding call does not respond after the latest
    run in order to ensure the data is always correct

--- dotcom-rendering/src/components/Discussion.tsx
+++ dotcom-rendering/src/components/Discussion.tsx
@@ -400,11 +400,14 @@ export const Discussion = ({
 	useEffect(() => {
 		dispatch({ type: 'setLoading', loading: true });
 
-		void getDiscussion(
-			shortUrlId,
-			commentPage,
-			remapToValidFilters(filters, hashCommentId),
-		)
+		const controller = new AbortController();
+
+		void getDiscussion({
+			shortUrl: shortUrlId,
+			page: commentPage,
+			filters: remapToValidFilters(filters, hashCommentId),
+			signal: controller.signal,
+		})
 			.then((result) => {
 				if (result.kind === 'error') {
 					console.error(`getDiscussion - error: ${result.error}`);
@@ -424,6 +427,8 @@ export const Discussion = ({
 			.catch(() => {
 				// do nothing
 			});
+
+		return () => controller.abort();
 	}, [commentPage, filters, hashCommentId, shortUrlId]);
 
 	const validFilters = remapToValidFilters(filters, hashCommentId);
