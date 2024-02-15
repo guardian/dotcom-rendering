202c82de0d9071da533892a79510d44404665f5f
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-02-15 15:59:03 +0000

    Update AnalyticsIframe.amp.tsx

--- dotcom-rendering/src/components/AnalyticsIframe.amp.tsx
+++ dotcom-rendering/src/components/AnalyticsIframe.amp.tsx
@@ -17,7 +17,7 @@ export const AnalyticsIframe = ({ url }: Props) => {
 				return (
 					<amp-iframe
 						class={prebidIframeStyle}
-						data-block-on-consent="_till_responded" // TODO: TO CONFIRM
+						data-block-on-consent="_till_responded"
 						title="Analytics Iframe"
 						height="1"
 						width="1"
