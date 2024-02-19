a56eff155e8c86afebf0cc952ab19fafb122462a
Alina Boghiu <alina.boghiu@gmail.com>
2024-02-19 11:36:21 +0000

    increase evaluation period to 5 minutes to allow for spiky response time

--- dotcom-rendering/cdk/lib/renderingStack.ts
+++ dotcom-rendering/cdk/lib/renderingStack.ts
@@ -175,7 +175,7 @@ export class RenderingCDKStack extends CDKStack {
 					metric: latencyMetric,
 					scalingSteps: props.scaling.policy.scalingStepsOut,
 					adjustmentType: AdjustmentType.PERCENT_CHANGE_IN_CAPACITY,
-					evaluationPeriods: 5,
+					evaluationPeriods: 10,
 				},
 			);
 
