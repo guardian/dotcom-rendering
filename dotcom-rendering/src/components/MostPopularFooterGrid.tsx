20c5288d5a4b087371aad0eed584a52afe2de82d
Alina Boghiu <alina.boghiu@gmail.com>
2024-02-21 14:40:44 +0000

    Add learn more link to deeply read description

--- dotcom-rendering/src/components/MostPopularFooterGrid.tsx
+++ dotcom-rendering/src/components/MostPopularFooterGrid.tsx
@@ -8,6 +8,7 @@ import {
 } from '@guardian/source-foundations';
 import type { TrailTabType, TrailType } from '../types/trails';
 import { MostViewedFooterItem } from './MostViewedFooterItem';
+import { Link } from '@guardian/source-react-components';
 
 const gridContainerStyle = css`
 	display: grid;
@@ -176,7 +177,14 @@ export const MostPopularFooterGrid = ({
 				>
 					<h3 css={titleStyle}>{deeplyRead.heading}</h3>
 					<div css={descriptionStyle}>
-						What readers are spending time with
+						What readers are spending time with (
+						<Link
+							css={descriptionStyle}
+							href="https://www.theguardian.com/info/2024/feb/15/what-is-the-deeply-read-list"
+						>
+							Learn more
+						</Link>
+						)
 					</div>
 				</div>
 				<ol css={displayContent}>
