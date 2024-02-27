d4e1d34d71e84b60028123e30d023996579f9364
Anna Beddow <anna.beddow@gmail.com>
2024-02-27 12:51:14 +0000

    use dark mode colours on button background

--- dotcom-rendering/src/palette.ts
+++ dotcom-rendering/src/palette.ts
@@ -4705,7 +4705,7 @@ const discussionSubduedDark: PaletteFunction = () => sourcePalette.neutral[60];
 const discussionLinkLight: PaletteFunction = () => sourcePalette.brand[500];
 const discussionLinkDark: PaletteFunction = () => sourcePalette.brand[800];
 
-const discussionPrimaryButtonBackground: PaletteFunction = ({ theme }) => {
+const discussionPrimaryButtonBackgroundLight: PaletteFunction = ({ theme }) => {
 	switch (theme) {
 		case Pillar.News:
 			return sourcePalette.news[300];
@@ -4721,6 +4721,21 @@ const discussionPrimaryButtonBackground: PaletteFunction = ({ theme }) => {
 			return sourcePalette.labs[300];
 		default:
 			return sourcePalette.news[300];
+	}
+};
+
+const discussionPrimaryButtonBackgroundDark: PaletteFunction = ({ theme }) => {
+	switch (theme) {
+		case Pillar.News:
+		case Pillar.Lifestyle:
+		case Pillar.Sport:
+		case Pillar.Culture:
+		case Pillar.Opinion:
+			return pillarPalette(theme, 500);
+		case ArticleSpecial.Labs:
+			return sourcePalette.labs[400];
+		default:
+			return sourcePalette.news[500];
 	}
 };
 
@@ -5779,8 +5794,8 @@ const paletteColours = {
 		dark: discussionLinkDark,
 	},
 	'--discussion-primary-button-background': {
-		light: discussionPrimaryButtonBackground,
-		dark: discussionPrimaryButtonBackground,
+		light: discussionPrimaryButtonBackgroundLight,
+		dark: discussionPrimaryButtonBackgroundDark,
 	},
 	'--discussion-button-background-hover': {
 		light: discussionButtonHover,
