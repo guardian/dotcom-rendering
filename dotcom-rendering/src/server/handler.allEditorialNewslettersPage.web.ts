e3729504b6ea9f8951790d30f647a1f291d8acd0
Charlotte <charlotte.emms@theguardian.com>
2024-03-04 12:07:18 +0000

    force error for email newsletters page

--- dotcom-rendering/src/server/handler.allEditorialNewslettersPage.web.ts
+++ dotcom-rendering/src/server/handler.allEditorialNewslettersPage.web.ts
@@ -1,19 +1,21 @@
 import type { RequestHandler } from 'express';
-import { enhanceNewslettersPage } from '../model/enhance-newsletters-page';
-import { validateAsAllEditorialNewslettersPageType } from '../model/validate';
-import { makePrefetchHeader } from './lib/header';
+// import { enhanceNewslettersPage } from '../model/enhance-newsletters-page';
+// import { validateAsAllEditorialNewslettersPageType } from '../model/validate';
+// import { makePrefetchHeader } from './lib/header';
 import { recordTypeAndPlatform } from './lib/logging-store';
-import { renderEditorialNewslettersPage } from './render.allEditorialNewslettersPage.web';
+// import { renderEditorialNewslettersPage } from './render.allEditorialNewslettersPage.web';
 
 export const handleAllEditorialNewslettersPage: RequestHandler = (
-	{ body },
+	_,
+	// { body },
 	res,
 ) => {
 	recordTypeAndPlatform('newsletters');
-	const feNewslettersData = validateAsAllEditorialNewslettersPageType(body);
-	const newslettersPage = enhanceNewslettersPage(feNewslettersData);
-	const { html, prefetchScripts } = renderEditorialNewslettersPage({
-		newslettersPage,
-	});
-	res.status(200).set('Link', makePrefetchHeader(prefetchScripts)).send(html);
+	// const feNewslettersData = validateAsAllEditorialNewslettersPageType(body);
+	// const newslettersPage = enhanceNewslettersPage(feNewslettersData);
+	// const { html, prefetchScripts } = renderEditorialNewslettersPage({
+	// 	newslettersPage,
+	// });
+	// res.status(200).set('Link', makePrefetchHeader(prefetchScripts)).send(html);
+	res.status(500).send('Fake 500 error for testing purposes.');
 };
