a3b71e60fd787925b0bed471aec36df1454c11ea
Alina Boghiu <alina.boghiu@gmail.com>
2024-03-06 16:42:26 +0000

    render elements inside a key takeaway

--- dotcom-rendering/src/components/KeyTakeaways.stories.tsx
+++ dotcom-rendering/src/components/KeyTakeaways.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { KeyTakeaways } from './KeyTakeaways';
+import { TextBlockElement } from '../types/content';
 
 const meta: Meta<typeof KeyTakeaways> = {
 	component: KeyTakeaways,
@@ -10,4 +11,23 @@ export default meta;
 
 type Story = StoryObj<typeof KeyTakeaways>;
 
-export const Default: Story = {};
+const html =
+	'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesquepharetra libero nec varius feugiat. Nulla commodo sagittis erat amalesuada. Ut iaculis interdum eros, et tristique ex. In veldignissim arcu. Nulla nisi urna, laoreet a aliquam at, viverra eueros. Proin imperdiet pellentesque turpis sed luctus. Donecdignissim lacus in risus fermentum maximus eu vel justo. Duis nontortor ac elit dapibus imperdiet ut at risus. Etiam pretium, odioeget accumsan venenatis, tortor mi aliquet nisl, vel ullamcorperneque nulla vel elit. Etiam porta mauris nec sagittis luctus.</p>';
+
+const testTextElement: TextBlockElement = {
+	_type: 'model.dotcomrendering.pageElements.TextBlockElement',
+	elementId: 'test-text-element-id-1',
+	dropCap: false,
+	html,
+};
+
+export const Default: Story = {
+	args: {
+		keyTakeaways: [
+			{
+				title: 'my first key takeaway',
+				body: [testTextElement],
+			},
+		],
+	},
+};
--- dotcom-rendering/src/components/KeyTakeaways.tsx
+++ dotcom-rendering/src/components/KeyTakeaways.tsx
@@ -1,18 +1,94 @@
 import { KeyTakeaway } from '../types/content';
+import { RenderArticleElement } from '../lib/renderElement';
+import { ServerSideTests, Switches } from '../types/config';
+import { EditionId } from '../lib/edition';
+import { ArticleFormat } from '@guardian/libs';
 
-type Props = {
+interface CommonProps {
+	format: ArticleFormat;
+	ajaxUrl: string;
+	host: string;
+	pageId: string;
+	isAdFreeUser: boolean;
+	isSensitive: boolean;
+	abTests: ServerSideTests;
+	switches: Switches;
+	editionId: EditionId;
+}
+
+interface KeyTakeawaysProps extends CommonProps {
 	keyTakeaways: KeyTakeaway[];
-};
+}
 
-const KeyTakeawayComponent = (props: { keyTakeaway: KeyTakeaway }) => {
-	return <h2>{props.keyTakeaway.title}</h2>;
-};
+interface KeyTakeawayProps extends CommonProps {
+	keyTakeaway: KeyTakeaway;
+}
 
-export const KeyTakeaways = (props: Props) => {
+const KeyTakeawayComponent = ({
+	keyTakeaway,
+	format,
+	ajaxUrl,
+	host,
+	pageId,
+	isAdFreeUser,
+	isSensitive,
+	switches,
+	abTests,
+	editionId,
+}: KeyTakeawayProps) => {
 	return (
 		<>
-			{props.keyTakeaways.map((keyTakeaway) => (
-				<KeyTakeawayComponent keyTakeaway={keyTakeaway} />
+			<h2>{keyTakeaway.title}</h2>
+			{keyTakeaway.body.map((element, index) => (
+				<RenderArticleElement
+					// eslint-disable-next-line react/no-array-index-key -- This is only rendered once so we can safely use index to suppress the warning
+					key={index}
+					format={format}
+					element={element}
+					ajaxUrl={ajaxUrl}
+					host={host}
+					index={index}
+					isMainMedia={index === 0}
+					pageId={pageId}
+					webTitle={keyTakeaway.title}
+					isAdFreeUser={isAdFreeUser}
+					isSensitive={isSensitive}
+					switches={switches}
+					abTests={abTests}
+					editionId={editionId}
+				/>
+			))}
+		</>
+	);
+};
+
+export const KeyTakeaways = ({
+	keyTakeaways,
+	format,
+	ajaxUrl,
+	host,
+	pageId,
+	isAdFreeUser,
+	isSensitive,
+	switches,
+	abTests,
+	editionId,
+}: KeyTakeawaysProps) => {
+	return (
+		<>
+			{keyTakeaways.map((keyTakeaway) => (
+				<KeyTakeawayComponent
+					keyTakeaway={keyTakeaway}
+					format={format}
+					ajaxUrl={ajaxUrl}
+					host={host}
+					pageId={pageId}
+					isAdFreeUser={isAdFreeUser}
+					isSensitive={isSensitive}
+					switches={switches}
+					abTests={abTests}
+					editionId={editionId}
+				/>
 			))}
 		</>
 	);
