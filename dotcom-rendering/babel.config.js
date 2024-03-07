1d23ca7597b0cfd48a0d4192dc2b364201270511
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-03-07 14:09:52 +0000

    Remove `babel-plugin-px-to-rem` (#10860)
    We don't believe it's used for most bundles. It is used for the legacy bundle, but we don't believe it works there anyway.
    
    Co-authored-by: Charlotte Emms <43961396+cemms1@users.noreply.github.com>
    Co-authored-by: Daniel Clifton <110032454+DanielCliftonGuardian@users.noreply.github.com>
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>
    Co-authored-by: Ioanna Kokkini <ioannakok@users.noreply.github.com>
    Co-authored-by: George B <705427+georgeblahblah@users.noreply.github.com>

--- dotcom-rendering/babel.config.js
+++ dotcom-rendering/babel.config.js
@@ -7,7 +7,6 @@ module.exports = {
 		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-proposal-optional-chaining',
 		'@babel/plugin-proposal-nullish-coalescing-operator',
-		'babel-plugin-px-to-rem',
 		'@babel/plugin-transform-runtime',
 		'@emotion/babel-plugin',
 	],
