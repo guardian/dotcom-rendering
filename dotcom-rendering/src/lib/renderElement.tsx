a0c030d9c5a26acaceb03ff1f4fde08cb27fdf46
Alina Boghiu <alina.boghiu@gmail.com>
2024-03-13 11:35:25 +0000

    extract key takeaway component and integrate with render elements - update schema to include elementid

--- dotcom-rendering/src/lib/renderElement.tsx
+++ dotcom-rendering/src/lib/renderElement.tsx
@@ -27,6 +27,7 @@ import { InteractiveContentsBlockComponent } from '../components/InteractiveCont
 import { InteractiveLayoutAtom } from '../components/InteractiveLayoutAtom';
 import { Island } from '../components/Island';
 import { ItemLinkBlockElement } from '../components/ItemLinkBlockElement';
+import { KeyTakeaways } from '../components/KeyTakeaways';
 import { KnowledgeQuizAtom } from '../components/KnowledgeQuizAtom.importable';
 import { MainMediaEmbedBlockComponent } from '../components/MainMediaEmbedBlockComponent';
 import { MapEmbedBlockComponent } from '../components/MapEmbedBlockComponent.importable';
@@ -136,6 +137,7 @@ export const renderElement = ({
 	switches,
 	isSensitive,
 	isPinnedPost,
+	abTests,
 	editionId,
 }: Props) => {
 	const isBlog =
@@ -416,6 +418,20 @@ export const renderElement = ({
 						/>
 					</Island>
 				</div>
+			);
+		case 'model.dotcomrendering.pageElements.KeyTakeawaysBlockElement':
+			return (
+				<KeyTakeaways
+					keyTakeaways={element.keyTakeaways}
+					format={format}
+					ajaxUrl={ajaxUrl}
+					pageId={pageId}
+					isAdFreeUser={isAdFreeUser}
+					isSensitive={isSensitive}
+					abTests={abTests}
+					switches={switches}
+					editionId={editionId}
+				/>
 			);
 		case 'model.dotcomrendering.pageElements.MapBlockElement':
 			return (
