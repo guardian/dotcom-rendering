fda8eb1cca6976db588e01aa184ccfa80dbe0f0b
Charlotte <charlotte.emms@theguardian.com>
2024-03-18 20:17:10 +0000

    add subheading styles for authoritative clear tone

--- dotcom-rendering/src/components/SubheadingBlockComponent.stories.tsx
+++ dotcom-rendering/src/components/SubheadingBlockComponent.stories.tsx
@@ -1,12 +1,29 @@
 import { css } from '@emotion/react';
+import { ArticleDesign, ArticleDisplay, Pillar } from '@guardian/libs';
 import { SubheadingBlockComponent } from './SubheadingBlockComponent';
+
+const defaultFormat = {
+	design: ArticleDesign.Obituary,
+	display: ArticleDisplay.Standard,
+	theme: Pillar.News,
+};
 
 export default {
 	component: SubheadingBlockComponent,
 	title: 'Components/SubheadingBlockComponent',
 	render: (args: React.ComponentProps<typeof SubheadingBlockComponent>) => (
-		<SubheadingBlockComponent {...args} />
+		<div
+			css={css`
+				margin: 10px;
+			`}
+		>
+			<SubheadingBlockComponent {...args} />
+		</div>
 	),
+	args: {
+		html: '<h2>Subheading</h2>',
+		format: defaultFormat,
+	},
 };
 
 export const Subheading = {
@@ -39,9 +56,21 @@ export const SubheadingSnapshot = () => (
 			}
 		`}
 	>
-		<SubheadingBlockComponent html="<h2>Basic subheading</h2>" />
-		<SubheadingBlockComponent html="<h2>Subheading <a href='/'>with anchor</a></h2>" />
-		<SubheadingBlockComponent html="<h2>Subheading with HTML comment<!-- HTML comment--></h2>" />
-		<SubheadingBlockComponent html="Subheading text only (no HTML)" />
+		<SubheadingBlockComponent
+			format={defaultFormat}
+			html="<h2>Basic subheading</h2>"
+		/>
+		<SubheadingBlockComponent
+			format={defaultFormat}
+			html="<h2>Subheading <a href='/'>with anchor</a></h2>"
+		/>
+		<SubheadingBlockComponent
+			format={defaultFormat}
+			html="<h2>Subheading with HTML comment<!-- HTML comment--></h2>"
+		/>
+		<SubheadingBlockComponent
+			format={defaultFormat}
+			html="Subheading text only (no HTML)"
+		/>
 	</div>
 );
