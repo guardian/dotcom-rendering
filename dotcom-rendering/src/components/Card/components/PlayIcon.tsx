2f8f0d41844a550d0de516b3f77ffd9074519f9d
Anna Beddow <anna.beddow@gmail.com>
2024-04-02 12:30:08 +0100

    Reduce nesting and prefer source theme where available

--- dotcom-rendering/src/components/Card/components/PlayIcon.tsx
+++ dotcom-rendering/src/components/Card/components/PlayIcon.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import { from, palette } from '@guardian/source-foundations';
+import type { ThemeIcon } from '@guardian/source-react-components';
 import { SvgMediaControlsPlay } from '@guardian/source-react-components';
 import type { ImagePositionType, ImageSizeType } from './ImageWrapper';
 
@@ -11,14 +12,6 @@ const sizes = {
 	large: { button: 80, icon: 72 },
 	xlarge: { button: 80, icon: 72 },
 } as const satisfies Record<string, { button: number; icon: number }>;
-
-const iconWrapperStyles = css`
-	display: flex;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-`;
 
 const iconPulseStyles = css`
 	:focus,
@@ -32,6 +25,13 @@ const iconStyles = (
 	size: PlayButtonSize,
 	sizeOnMobile: Extract<PlayButtonSize, 'small' | 'large'>,
 ) => css`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 	background-color: rgba(18, 18, 18, 0.6);
 	border-radius: 50%;
 	width: ${sizes[sizeOnMobile].button}px;
@@ -41,13 +41,7 @@ const iconStyles = (
 		height: ${sizes[size].button}px;
 	}
 
-	display: flex;
-	align-items: center;
-	justify-content: center;
-
 	svg {
-		/* Visual centering */
-		fill: ${palette.neutral[100]};
 		transform: translateX(1px);
 		width: ${sizes[sizeOnMobile].icon}px;
 		height: ${sizes[sizeOnMobile].icon}px;
@@ -57,6 +51,10 @@ const iconStyles = (
 		}
 	}
 `;
+
+const theme = {
+	fill: palette.neutral[100],
+} satisfies Partial<ThemeIcon>;
 
 const getIconSizeOnDesktop = (imageSize: ImageSizeType) => {
 	switch (imageSize) {
@@ -85,17 +83,16 @@ export const PlayIcon = ({
 	isPlayableMediaCard: boolean;
 }) => {
 	return (
-		<div css={[iconWrapperStyles, isPlayableMediaCard && iconPulseStyles]}>
-			<span
-				css={[
-					iconStyles(
-						getIconSizeOnDesktop(imageSize),
-						getIconSizeOnMobile(imagePositionOnMobile),
-					),
-				]}
-			>
-				<SvgMediaControlsPlay />
-			</span>
+		<div
+			css={[
+				iconStyles(
+					getIconSizeOnDesktop(imageSize),
+					getIconSizeOnMobile(imagePositionOnMobile),
+				),
+				isPlayableMediaCard && iconPulseStyles,
+			]}
+		>
+			<SvgMediaControlsPlay theme={theme} />
 		</div>
 	);
 };
