5e37d5f5b0b1cb67cca70c1a55c4946a69c5c877
Dominik Lander <dominik.lander@guardian.co.uk>
2024-04-03 12:53:15 +0100

    Commercial Q4 mega test (#11017)
    Co-authored-by: Dina Hafez <dina.hafez@guardian.co.uk>
    Co-authored-by: Jake Lee Kennedy <jlkennedy127@gmail.com>

--- dotcom-rendering/src/lib/article.ts
+++ dotcom-rendering/src/lib/article.ts
@@ -7,6 +7,7 @@ import { enhanceCommercialProperties } from '../model/enhanceCommercialPropertie
 import { enhanceStandfirst } from '../model/enhanceStandfirst';
 import { enhanceTableOfContents } from '../model/enhanceTableOfContents';
 import { validateAsArticleType } from '../model/validate';
+import type { ServerSideTests } from '../types/config';
 import { type DCRArticle } from '../types/frontend';
 import { type RenderingTarget } from '../types/renderingTarget';
 import { decideFormat } from './decideFormat';
@@ -14,14 +15,17 @@ import { decideFormat } from './decideFormat';
 const enhancePinnedPost = (
 	format: ArticleFormat,
 	renderingTarget: RenderingTarget,
+	abTests: ServerSideTests,
 	block?: Block,
 ) => {
 	if (!block) return;
+
 	return enhanceBlocks([block], format, {
 		renderingTarget,
 		imagesForLightbox: [],
 		promotedNewsletter: undefined,
 		hasAffiliateLinksDisclaimer: false,
+		abTests,
 	})[0];
 };
 
@@ -41,6 +45,7 @@ export const enhanceArticleType = (
 		promotedNewsletter: data.promotedNewsletter,
 		imagesForLightbox,
 		hasAffiliateLinksDisclaimer: !!data.affiliateLinksDisclaimer,
+		abTests: data.config.abTests,
 	});
 
 	const mainMediaElements = enhanceElementsImages(
@@ -52,7 +57,12 @@ export const enhanceArticleType = (
 		...data,
 		mainMediaElements,
 		blocks: enhancedBlocks,
-		pinnedPost: enhancePinnedPost(format, renderingTarget, data.pinnedPost),
+		pinnedPost: enhancePinnedPost(
+			format,
+			renderingTarget,
+			data.config.abTests,
+			data.pinnedPost,
+		),
 		standfirst: enhanceStandfirst(data.standfirst),
 		commercialProperties: enhanceCommercialProperties(
 			data.commercialProperties,
