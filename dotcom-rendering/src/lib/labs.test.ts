b7e996abf5c8d69553fe93f369c43957d51f8fbb
Chris Jones <christopher.jones@guardian.co.uk>
2024-04-03 10:01:23 +0100

    Fix typo in test description
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>

--- dotcom-rendering/src/lib/labs.test.ts
+++ dotcom-rendering/src/lib/labs.test.ts
@@ -2,7 +2,7 @@ import type { Branding } from '../types/branding';
 import { getOphanComponents } from './labs';
 
 describe('getOphanComponents', () => {
-	it('constrcuts the correct data attributes', () => {
+	it('constructs the correct data attributes', () => {
 		const branding = { sponsorName: 'Some Sponsor' } as Branding;
 		expect(getOphanComponents(branding)).toStrictEqual({
 			ophanComponentName: 'labs-logo | article-some-sponsor',
