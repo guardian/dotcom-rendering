b56a4594a15922a042de50fe90fce443dacc54b3
Dina Hafez <dina.hafez@guardian.co.uk>
2024-04-05 10:48:28 +0100

    Replace variable above jsdoc comment

--- dotcom-rendering/src/client/islands/whenIdle.ts
+++ dotcom-rendering/src/client/islands/whenIdle.ts
@@ -1,10 +1,10 @@
+const timeout = 500;
 /**
  * whenIdle executes a callback when the browser is 'idle' or after a short timeout, whichever comes first
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback
  *
  * @param callback the function to execute once the browser is 'idle'
  */
-const timeout = 500;
 export const whenIdle = (callback: () => void): void => {
 	if ('requestIdleCallback' in window) {
 		window.requestIdleCallback(callback, { timeout });
