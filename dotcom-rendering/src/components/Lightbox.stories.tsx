2a41a66fecb877fc958dcfd56cd560e220aacd67
Dominik Lander <dominik.lander@guardian.co.uk>
2024-04-08 13:01:06 +0100

    Ensure cleared localstorage before interaction story

--- dotcom-rendering/src/components/Lightbox.stories.tsx
+++ dotcom-rendering/src/components/Lightbox.stories.tsx
@@ -315,7 +315,10 @@ export const ClickInfo = () => {
 	);
 };
 ClickInfo.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+	storage.local.clear();
 	const canvas = within(canvasElement);
+
+	await expect(canvas.getByText(/ClickInfoTest/)).toBeVisible();
 
 	await userEvent.click(canvas.getByTitle('Toggle caption [i]'));
 	await expect(canvas.getByText(/ClickInfoTest/)).not.toBeVisible();
