13cdf1bc09f008d822fc2812151bc91fd35a519c
Chris Jones <chrislomaxjones96@gmail.com>
2024-04-10 12:30:44 +0100

    Add 0 percent client-side AB test

---
+++ dotcom-rendering/src/experiments/tests/ad-block-ask.ts
@@ -0,0 +1,29 @@
+import type { ABTest } from '@guardian/ab-core';
+
+export const adBlockAsk: ABTest = {
+	id: 'AdBlockAsk',
+	author: '@commercial-dev',
+	start: '2024-04-10',
+	expiry: '2024-05-31',
+	audience: 0 / 100,
+	audienceOffset: 0 / 100,
+	audienceCriteria: '',
+	successMeasure: '',
+	description:
+		'Show new ad block ask component in ad slots when we detect ad blocker usage',
+	variants: [
+		{
+			id: 'control',
+			test: (): void => {
+				/* no-op */
+			},
+		},
+		{
+			id: 'variant',
+			test: (): void => {
+				/* no-op */
+			},
+		},
+	],
+	canRun: () => true,
+};
