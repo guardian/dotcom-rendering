0df4d8a3de9a7af533457f168048e6ec429f4ff8
Charlotte <charlotte.emms@theguardian.com>
2024-04-10 13:05:14 +0100

    add ab tests for header and masthead, set to zero percent audience initially

--- dotcom-rendering/src/experiments/ab-tests.ts
+++ dotcom-rendering/src/experiments/ab-tests.ts
@@ -3,10 +3,12 @@ import { abTestTest } from './tests/ab-test-test';
 import { blockSupporterRevenueMessagingSport } from './tests/block-supporter-revenue-messaging-sport';
 import { consentlessAds } from './tests/consentless-ads';
 import { integrateIma } from './tests/integrate-ima';
+import { mastheadWithHighlights } from './tests/masthead-with-highlights';
 import { mpuWhenNoEpic } from './tests/mpu-when-no-epic';
 import { signInGateAlternativeWording } from './tests/sign-in-gate-alternative-wording';
 import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
+import { updatedHeaderDesign } from './tests/updated-header-design';
 
 // keep in sync with ab-tests in frontend
 // https://github.com/guardian/frontend/tree/main/static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -19,4 +21,6 @@ export const tests: ABTest[] = [
 	integrateIma,
 	mpuWhenNoEpic,
 	blockSupporterRevenueMessagingSport,
+	updatedHeaderDesign,
+	mastheadWithHighlights,
 ];
