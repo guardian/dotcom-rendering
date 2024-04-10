02d2613aea1cebb8a8334dae8a591da681ac9603
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-04-10 14:44:48 +0100

    Add grid decorators
    Some components are written with assumptions about the context in which they will appear. One such assumption is that they will appear in a page that has been laid out using the Guardian's grid.
    
    For example, from the `leftCol` breakpoint thumbnail images will move themselves out of the flow of text and into the left column of the Guardian grid. However, in storybook there is no left column, so these images disappear outside the viewport of the story.
    
    Storybook has a solution for when components need some surrounding context to render correctly: decorators. We already rely on this to set up the palette variables correctly, so that stories have the correct colours in light and dark mode.
    
    This change introduces a set of "grid" decorators, which can be used to lay out components within stories using the Guardian grid. Not all components need to be wrapped in this way, so these are designed to be applied on a per-story or per-component basis, rather than globally as with the colour scheme decorators.
    
    Returning to the example above, using one of these decorators (e.g. `centreColumnDecorator`) allows text and images to be placed on the Guardian grid as they would in a full article. This means the left column is then available within the story for thumbnail images to appear in at appropriate breakpoints.

---
+++ dotcom-rendering/.storybook/decorators/gridDecorators.tsx
@@ -0,0 +1,77 @@
+import { css } from '@emotion/react';
+import type { Decorator } from '@storybook/react';
+import { grid } from './grid';
+import { from } from '@guardian/source-foundations';
+
+/**
+ * Wrap the story in a Guardian grid container. This allows styles either
+ *
+ * 1. Within the component
+ * 2. In the story
+ * 3. In other decorators
+ *
+ * to position elements on the grid.
+ */
+export const gridContainerDecorator: Decorator = (Story) => (
+	<div css={css(grid.container)}>
+		<Story />
+	</div>
+);
+
+/**
+ * Place the story in the centre column of a Guardian grid across all
+ * breakpoints.
+ */
+export const centreColumnDecorator: Decorator = (Story) => (
+	<div css={css(grid.container)}>
+		<div
+			css={css`
+				${grid.column.centre}
+			`}
+		>
+			<Story />
+		</div>
+	</div>
+);
+
+/**
+ * Place the story in the left column of a Guardian grid, when available.
+ * On narrower breakpoints, where the left column isn't available, it will
+ * instead be placed in the centre column (this is a common layout pattern).
+ */
+export const leftColumnDecorator: Decorator = (Story) => (
+	<div css={css(grid.container)}>
+		<div
+			css={css`
+				${grid.column.centre}
+
+				${from.leftCol} {
+					${grid.column.left}
+				}
+			`}
+		>
+			<Story />
+		</div>
+	</div>
+);
+
+/**
+ * Place the story in the right column of a Guardian grid, when available.
+ * On narrower breakpoints, where the right column isn't available, it will
+ * instead be placed in the centre column (this is a common layout pattern).
+ */
+export const rightColumnDecorator: Decorator = (Story) => (
+	<div css={css(grid.container)}>
+		<div
+			css={css`
+				${grid.column.centre}
+
+				${from.desktop} {
+					${grid.column.right}
+				}
+			`}
+		>
+			<Story />
+		</div>
+	</div>
+);
