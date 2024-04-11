53e4ad4ca9efe9f982c686d52248dbe57ff4b4ba
Chris Jones <chrislomaxjones96@gmail.com>
2024-04-11 17:19:25 +0100

    Stud out ad block detect impl

--- dotcom-rendering/src/lib/useAdBlockAsk.ts
+++ dotcom-rendering/src/lib/useAdBlockAsk.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
 import { useAB } from './useAB';
-import { useOnlineStatus } from './useOnlineStatus';
 
 const useIsInAdBlockAskVariant = (): boolean => {
 	const abTestAPI = useAB()?.api;
@@ -10,48 +8,7 @@ const useIsInAdBlockAskVariant = (): boolean => {
 
 export const useAdblockAsk = (): boolean => {
 	const isInVariant = useIsInAdBlockAskVariant();
-	const [adBlockerDetected, setAdBlockerDetected] = useState<boolean>(false);
-	const isOnline = useOnlineStatus();
-
-	useEffect(() => {
-		const makeRequest = async () => {
-			try {
-				// Once we've detected an ad-blocker, we don't care about subsequent detections
-				// Ensure the user is online before performing the detection request
-				if (!adBlockerDetected && isOnline) {
-					const response = await fetch(
-						'https://www3.doubleclick.net',
-						{
-							method: 'HEAD',
-							mode: 'no-cors',
-							cache: 'no-store',
-						},
-					);
-					if (response.redirected) {
-						setAdBlockerDetected(true);
-					}
-				}
-			} catch (err) {
-				setAdBlockerDetected(true);
-			}
-		};
-		void makeRequest();
-	}, [adBlockerDetected, isOnline]);
-
-	/**
-	 * Some ad-blockers will remove slots from the DOM, while others don't
-	 * This clean-up ensures that any space we've reserved for an ad is removed,
-	 * in order to properly layout the ask.
-	 */
-	useEffect(() => {
-		if (adBlockerDetected) {
-			for (const container of document.querySelectorAll(
-				'.ad-slot-container',
-			)) {
-				container.remove();
-			}
-		}
-	}, [adBlockerDetected]);
-
-	return isInVariant && adBlockerDetected;
+	// This is just a stub implementation
+	// TODO(@chrislomaxjones) Replace with proper ad block detection logic in subsequent PR
+	return isInVariant;
 };
