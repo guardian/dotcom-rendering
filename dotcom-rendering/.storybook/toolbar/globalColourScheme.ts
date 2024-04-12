5a88d2d515145824b78c3a931d45a1a9a295453c
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-04-12 15:49:00 +0100

    Adds a storybook toolbar item to set colour scheme
    With the introduction of dark mode we've found it useful to apply colour schemes and split layouts to stories via decorators: `lightDecorator`, `darkDecorator` and `splitTheme`. This change builds on that approach by allowing you to switch any story between one of four colour schemes via the storybook toolbar:
    
    1. Light mode.
    2. Dark mode.
    3. Side-by-side Horizontal: display the story twice, on the left in light mode and on the right in dark mode.
    4. Side-by-side Vertical: display the story twice, on the top in light mode and on the bottom in dark mode.
    
    More details on exactly how this works can be found in the JSDoc comments in `globalColourScheme.ts`.
    
    There are a few advantages to this approach. Firstly, a story no longer needs to be hardcoded to a given colour scheme or split theme layout; any story can be viewed in any one of the four variants. Secondly, and relatedly, we can use Chromatic's "modes" to decide on one or more colour schemes to snapshot a story with, without having to write multiple stories, and tightly couple our stories to the snapshot variations we want. Thirdly, it makes it clearer which format is being applied to a given story. Previously, if a story took `format` as an `arg`, that was always ignored in favour of the `defaultFormat` or a format applied in one of the colour scheme decorators. Now the story's `format` will be used when available, unless you specifically want to display multiple format variants using `parameters.formats`.
    
    This change also updates the `AudioAtom` stories to take advantage of this new approach. In addition, it includes a small refactor to `preview.ts`, to make it clearer and remove the unused `viewports` export.

---
+++ dotcom-rendering/.storybook/toolbar/globalColourScheme.ts
@@ -0,0 +1,129 @@
+import type { Decorator } from '@storybook/react';
+import { lightDecorator, darkDecorator } from '../decorators/themeDecorator';
+import { splitTheme } from '../decorators/splitThemeDecorator';
+import {
+	ArticleDisplay,
+	ArticleDesign,
+	Pillar,
+	type ArticleFormat,
+} from '@guardian/libs';
+
+const defaultFormat: ArticleFormat = {
+	display: ArticleDisplay.Standard,
+	design: ArticleDesign.Standard,
+	theme: Pillar.News,
+};
+
+/**
+ * A global type and toolbar item to control the colour scheme of the story. It
+ * offers four options:
+ *
+ * 1. Light mode.
+ * 2. Dark mode.
+ * 3. Side-by-side Horizontal: display the story twice, on the left in light
+ * mode and on the right in dark mode.
+ * 4. Side-by-side Vertical: display the story twice, on the top in light
+ * mode and on the bottom in dark mode.
+ *
+ * This is added to `globalTypes` in `preview.ts` to include it in the toolbar.
+ * For more information on how this works see:
+ * https://storybook.js.org/docs/essentials/toolbars-and-globals
+ *
+ * It relies on {@linkcode globalColourSchemeDecorator} to read the value that's
+ * been set and apply the appropriate colours and layout.
+ */
+export const globalColourScheme = {
+	description: 'View the story in light and dark mode, or both side-by-side',
+	defaultValue: 'light',
+	toolbar: {
+		title: 'Colour Scheme',
+		icon: 'mirror',
+		items: [
+			{ value: 'light', left: '☀️', title: 'Light' },
+			{ value: 'dark', left: '🌙', title: 'Dark' },
+			{
+				value: 'horizontal',
+				left: '◨',
+				title: 'Side-by-side Horizontal',
+			},
+			{ value: 'vertical', left: '⬓', title: 'Side-by-side Vertical' },
+		],
+		dynamicTitle: true,
+	},
+} as const;
+
+type ColourSchemeParameter =
+	(typeof globalColourScheme.toolbar.items)[number]['value'];
+
+/**
+ * A decorator that reads the {@linkcode globalColourScheme} and applies a
+ * colour palette and layout to the story based on the value. It's designed to
+ * be used globally in `preview.ts` rather than at the story or component level,
+ * as the toolbar item is available on all stories.
+ *
+ * It wraps a given story in a colour palette, which is applied via CSS
+ * variables and defined in `palette.ts`. It makes use of other, existing
+ * decorators to do this in each case: {@linkcode lightDecorator},
+ * {@linkcode darkDecorator} and {@linkcode splitTheme}.
+ *
+ * To make use of a colour palette, an {@linkcode ArticleFormat} must be
+ * available. There are three ways the decorator attempts to retrieve this,
+ * in decreasing priority order (i.e. it will try 1, then 2, then 3):
+ *
+ * 1. If a list of formats is supplied via the story's `formats` parameter, it
+ * will render the component multiple times, once for each format.
+ * 2. If the story in question takes a `format` argument it renders the
+ * component once, using that.
+ * 3. If no format is available, it falls back to {@linkcode defaultFormat},
+ * again rendering the component just once.
+ *
+ * For more information on `parameters` see:
+ * https://storybook.js.org/docs/writing-stories/parameters
+ *
+ * @example
+ * <caption>Using the `formats` parameter</caption>
+ * const myStory = {
+ *   args: {},
+ *   parameters: {
+ *     formats: [standardStandardNews, interviewImmersiveSport],
+ *   },
+ * } satisfies Story;
+ *
+ * @example
+ * <caption>Using the story's `format` arg</caption>
+ * const myStory = {
+ *   args: {
+ *     format: standardStandardNews,
+ *   },
+ * } satisfies Story;
+ *
+ * @example
+ * <caption>Using the fallback `defaultFormat`</caption>
+ * const myStory = {
+ *   args: {},
+ * } satisfies Story;
+ */
+export const globalColourSchemeDecorator: Decorator = (Story, context) => {
+	const colourSchemeParameter: ColourSchemeParameter =
+		context.globals.globalColourScheme;
+	const formats = [
+		context.parameters.formats ?? context.args.format ?? defaultFormat,
+	].flat();
+
+	switch (colourSchemeParameter) {
+		case 'light':
+			return lightDecorator(formats)(Story, context);
+		case 'dark':
+			return darkDecorator(formats)(Story, context);
+		case 'horizontal':
+			return splitTheme(formats, { orientation: 'horizontal' })(
+				Story,
+				context,
+			);
+		case 'vertical':
+			return splitTheme(formats, { orientation: 'vertical' })(
+				Story,
+				context,
+			);
+	}
+};
