236bcf56e68f8b0bb3c6d2792c7163eef6d9912e
Jake Lee Kennedy <jlkennedy127@gmail.com>
2024-04-12 15:45:20 +0100

    Revert "Add alternative propertyHref and propertyId for AMP sourcepoint test …"
    This reverts commit 7648d70fc97ae86b176cc1d1482759db846b20be.

--- dotcom-rendering/src/components/ArticlePage.amp.tsx
+++ dotcom-rendering/src/components/ArticlePage.amp.tsx
@@ -75,7 +75,7 @@ export const AmpArticlePage = ({
 			/>
 			<Analytics key="analytics" analytics={analytics} />
 			<AnalyticsIframe url={config.ampIframeUrl} />
-			<AdConsent abTests={config.abTests} />
+			<AdConsent />
 			{experimentsData && (
 				<AmpExperimentComponent experimentsData={experimentsData} />
 			)}
