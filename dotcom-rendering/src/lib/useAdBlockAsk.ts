52daa287c5ca2e208b4a387750e792bb92921ada
Chris Jones <chrislomaxjones96@gmail.com>
2024-04-15 16:24:11 +0100

    Reinstate ad block detection

--- dotcom-rendering/src/lib/useAdBlockAsk.ts
+++ dotcom-rendering/src/lib/useAdBlockAsk.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useAB } from './useAB';
+import { useOnlineStatus } from './useOnlineStatus';
 
 const useIsInAdBlockAskVariant = (): boolean => {
 	const abTestAPI = useAB()?.api;
@@ -7,15 +8,67 @@ const useIsInAdBlockAskVariant = (): boolean => {
 	return isInVariant;
 };
 
+const makeDetectionRequests = async () => {
+	const requestDoubleclick = async () => {
+		try {
+			const response = await fetch('https://www3.doubleclick.net', {
+				method: 'HEAD',
+				mode: 'no-cors',
+				cache: 'no-store',
+				signal: AbortSignal.timeout(150),
+			});
+
+			if (response.redirected) {
+				return false;
+			}
+
+			return true;
+		} catch (err) {
+			return false;
+		}
+	};
+
+	const requestGuardian = async () => {
+		try {
+			await fetch('https://www.theguardian.com', {
+				method: 'HEAD',
+				mode: 'no-cors',
+				cache: 'no-store',
+				signal: AbortSignal.timeout(150),
+			});
+			return true;
+		} catch (err) {
+			return false;
+		}
+	};
+
+	const [doubleclickSuccess, guardianSuccess] = await Promise.all([
+		requestDoubleclick(),
+		requestGuardian(),
+	]);
+
+	return !doubleclickSuccess && guardianSuccess;
+};
+
 export const useAdblockAsk = (slotId: string): boolean => {
 	const isInVariant = useIsInAdBlockAskVariant();
 	const [adBlockerDetected, setAdBlockerDetected] = useState<boolean>(false);
+	const isOnline = useOnlineStatus();
 
 	useEffect(() => {
-		// This is just a stub implementation
-		// TODO(@chrislomaxjones) Replace with proper ad block detection logic in subsequent PR
-		setAdBlockerDetected(true);
-	}, []);
+		const makeRequest = async () => {
+			// Once we've detected an ad-blocker, we don't care about subsequent detections
+			// Ensure the user is online before performing the detection request
+			if (
+				!adBlockerDetected &&
+				isOnline &&
+				(await makeDetectionRequests())
+			) {
+				setAdBlockerDetected(true);
+			}
+		};
+		void makeRequest();
+	}, [adBlockerDetected, isOnline]);
 
 	/**
 	 * Some ad-blockers will remove slots from the DOM, while others don't
