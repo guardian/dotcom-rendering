638b15c87e91c8d31dc8803d55e6b2de7caecccf
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-04-16 12:00:37 +0100

    Render Timeline Elements (#11050)
    Implements some of what's needed to support Timeline elements. We're opening this PR as-is because it includes most of the core functionality. Remaining tasks are listed elsewhere and can be completed in follow-up PRs. There aren't any Timeline elements in production yet, so getting this merged before everything is complete shouldn't cause a problem.
    
    What's included so far:
    
    1. A `Timeline` component to render both Timeline element variants: flat and sectioned.
    2. Stories for the component, showing different features: flat, sectioned, labels, titles, images etc..
    3. A change in `renderElement` to handle Timeline elements using this new component.
    4. A `NestedArticleElement` wrapper component, to make it easier to render the nested elements that appear in the body of timelines. This could a) perhaps do with a better name, and b) also be used in the existing Key Takeaways and Q And A Explainer components for the same purpose.
    5. A change to the timelines enhancer to drop any main media with a role that's not either `inline`, `showcase` or `thumbnail`. Design do not want other roles to be supported.
    6. A new `Heading` component, to make code that needs to switch between different levels of headings easier to understand.
    7. Drill `isTimeline` context to image and caption
    
    ---------
    
    Co-authored-by: Alina Boghiu <alina.boghiu@gmail.com>
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>

--- dotcom-rendering/src/model/enhanceBlocks.ts
+++ dotcom-rendering/src/model/enhanceBlocks.ts
@@ -15,7 +15,7 @@ import { enhanceInteractiveContentsElements } from './enhance-interactive-conten
 import { enhanceNumberedLists } from './enhance-numbered-lists';
 import { enhanceTweets } from './enhance-tweets';
 import { enhanceLists } from './enhanceLists';
-import { enhanceTimelines } from './enhanceTimelines';
+import { enhanceTimeline } from './enhanceTimeline';
 import { insertPromotedNewsletter } from './insertPromotedNewsletter';
 
 type Options = {
@@ -50,7 +50,7 @@ export const enhanceElements =
 	(elements: FEElement[]): FEElement[] => {
 		return [
 			enhanceLists(enhanceElements(format, blockId, options)),
-			enhanceTimelines(enhanceElements(format, blockId, options)),
+			enhanceTimeline(enhanceElements(format, blockId, options)),
 			enhanceDividers,
 			enhanceH2s,
 			enhanceInteractiveContentsElements,
