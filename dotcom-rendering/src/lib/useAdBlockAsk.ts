6147209b790991b68dc2685ed61c2cfcd7f0a57f
Chris Jones <chrislomaxjones96@gmail.com>
2024-04-17 16:16:32 +0100

    Simplify detectByRequests definition

--- dotcom-rendering/src/lib/useAdBlockAsk.ts
+++ dotcom-rendering/src/lib/useAdBlockAsk.ts
@@ -9,60 +9,61 @@ const useIsInAdBlockAskVariant = (): boolean => {
 };
 
 /**
+ * Make a HEAD request to a URL that is typically blocked by ad-blockers
+ */
+const requestDoubleclick = async (timeoutMs: number) => {
+	try {
+		const response = await fetch('https://www3.doubleclick.net', {
+			method: 'HEAD',
+			mode: 'no-cors',
+			cache: 'no-store',
+			signal: AbortSignal.timeout(timeoutMs),
+		});
+
+		// A redirect is another clue we may be being ad-blocked
+		if (response.redirected) {
+			return false;
+		}
+
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
+
+/**
+ * Make a HEAD request to a URL that should succeed, even when using an
+ * ad-blocker
+ */
+const requestGuardian = async (timeoutMs: number) => {
+	try {
+		await fetch('https://www.theguardian.com', {
+			method: 'HEAD',
+			mode: 'no-cors',
+			cache: 'no-store',
+			signal: AbortSignal.timeout(timeoutMs),
+		});
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
+
+/**
  * Attempt to detect presence of an ad-blocker
  *
  * This implementation of this is likely to be tweaked before launching the test
  * proper
  */
 const detectByRequests = async () => {
-	/**
-	 * Make a HEAD request to a URL that is typically blocked by ad-blockers
-	 */
-	const requestDoubleclick = async () => {
-		try {
-			const response = await fetch('https://www3.doubleclick.net', {
-				method: 'HEAD',
-				mode: 'no-cors',
-				cache: 'no-store',
-				signal: AbortSignal.timeout(1000),
-			});
-
-			// A redirect is another clue we may be being ad-blocked
-			if (response.redirected) {
-				return false;
-			}
-
-			return true;
-		} catch (err) {
-			return false;
-		}
-	};
-
-	/**
-	 * Make a HEAD request to a URL that should succeed, even when using an
-	 * ad-blocker
-	 *
-	 * We set this request with a much smaller timeout than the one we expect to
-	 * be ad-blocked. This should reduce the chance that request fails and this
-	 * one succeeds due to poor network connectivity
-	 */
-	const requestGuardian = async () => {
-		try {
-			await fetch('https://www.theguardian.com', {
-				method: 'HEAD',
-				mode: 'no-cors',
-				cache: 'no-store',
-				signal: AbortSignal.timeout(250),
-			});
-			return true;
-		} catch (err) {
-			return false;
-		}
-	};
-
 	const [doubleclickSuccess, guardianSuccess] = await Promise.all([
-		requestDoubleclick(),
-		requestGuardian(),
+		requestDoubleclick(1000),
+		/**
+		 * We set this request with a much smaller timeout than the one we
+		 * expect to be ad-blocked. This should reduce the chance that request
+		 * fails and this one succeeds due to poor network connectivity
+		 */
+		requestGuardian(250),
 	]);
 
 	return !doubleclickSuccess && guardianSuccess;
