596137e7a757f84273edb6fa23110721d86c3412
Alex Sanders <alex@sndrs.dev>
2024-04-19 10:15:21 +0100

    allow explicit opting in to adaptation

--- dotcom-rendering/src/client/adaptiveSite.ts
+++ dotcom-rendering/src/client/adaptiveSite.ts
@@ -12,6 +12,7 @@ import { recordExperiences } from './ophan/ophan';
  */
 export const shouldAdapt = async (): Promise<boolean> => {
 	if (isServer) return false;
+	if (window.location.hash === '#adapt') return true;
 
 	// only evaluate this code if we want to adapt in response to page performance
 	const { isPerformingPoorly } = await import(
