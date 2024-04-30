662fd9f1bbcd07a5092ab8943cd4ecb71f95bc2d
Alex Sanders <alex@sndrs.dev>
2024-04-30 14:56:38 +0100

    fix some AR lint errors

--- apps-rendering/src/logger/clientProd/index.ts
+++ apps-rendering/src/logger/clientProd/index.ts
@@ -1,16 +1,12 @@
 import type { LoggerFunctions } from '../LoggerFunctions';
 
 class ClientProdLogger implements LoggerFunctions {
-	// eslint-disable-next-line @typescript-eslint/no-empty-function -- To be implemented
 	debug(message: string): void {}
 
-	// eslint-disable-next-line @typescript-eslint/no-empty-function -- To be implemented
 	info(message: string): void {}
 
-	// eslint-disable-next-line @typescript-eslint/no-empty-function -- To be implemented
 	warn(message: string, error?: Error | unknown): void {}
 
-	// eslint-disable-next-line @typescript-eslint/no-empty-function -- To be implemented
 	error(message: string, error?: Error | unknown): void {}
 }
 
