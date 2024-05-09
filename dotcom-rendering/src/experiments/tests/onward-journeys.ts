7468a5c20c8f61320a1b9068618cba9f8a6753e8
Max Duval <max.duval@theguardian.com>
2024-05-09 10:06:36 +0100

    fix: entirety of audience

--- dotcom-rendering/src/experiments/tests/onward-journeys.ts
+++ dotcom-rendering/src/experiments/tests/onward-journeys.ts
@@ -7,7 +7,7 @@ export const onwardJourneys: ABTest = {
 	author: '@web-experience',
 	description:
 		'Show the user one onward journey containers at a time to see which is the most effective',
-	audience: 25 / 100,
+	audience: 100 / 100,
 	audienceOffset: 0,
 	audienceCriteria: 'all users',
 	dataLinkNames: 'OnwardJourneys',
