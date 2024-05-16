0c1a4dd4a2a94a3bc7e282513aaf091204166b44
Ioanna Kokkini <ioannakok@hotmail.com>
2024-05-16 18:14:55 +0100

    Add a story for UKElectionTracker inside a container
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>

--- dotcom-rendering/src/components/UKLocalElectionTracker.stories.tsx
+++ dotcom-rendering/src/components/UKLocalElectionTracker.stories.tsx
@@ -1,5 +1,11 @@
 import { UKLocalElectionTracker as UKLocalElectionTrackerComponent } from './UKLocalElectionTracker';
 import { Meta, StoryObj } from '@storybook/react';
+import { FrontSection } from './FrontSection';
+import { discussionApiUrl } from '../../fixtures/manual/discussionApiUrl';
+import { DynamicPackage } from './DynamicPackage';
+import { DCRFrontCard } from '../types/front';
+import { ArticleDesign, ArticleDisplay, Pillar } from '@guardian/libs';
+import { trails } from '../../fixtures/manual/trails';
 
 const meta = {
 	title: 'Components/UK Local Election Tracker',
@@ -10,4 +16,48 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const snap: DCRFrontCard = {
+	format: {
+		design: ArticleDesign.Standard,
+		theme: Pillar.News,
+		display: ArticleDisplay.Standard,
+	},
+	url: '',
+	headline: '',
+	showQuotedHeadline: false,
+	dataLinkName: '',
+	discussionApiUrl: '',
+	isExternalLink: true,
+	embedUri:
+		'https://content.guardianapis.com/atom/interactive/interactives/2024/04/local-elections-tracker/local-elections-tracker-2024',
+	showLivePlayable: false,
+};
+
 export const UKLocalElectionTracker = {} satisfies Story;
+
+export const DynamoContainer = {
+	render: (args) => (
+		<FrontSection
+			title="Dynamic Package"
+			showTopBorder={true}
+			discussionApiUrl={discussionApiUrl}
+			editionId={'UK'}
+			containerPalette="EventPalette"
+		>
+			<DynamicPackage
+				groupedTrails={{
+					huge: [],
+					veryBig: [],
+					big: [],
+					standard: trails.slice(1, 4),
+					snap: [snap],
+				}}
+				showAge={true}
+				containerPalette="EventPalette"
+				imageLoading="eager"
+			/>
+		</FrontSection>
+	),
+} satisfies Story;
+
+// TODO: Add stories for iOS and Android
