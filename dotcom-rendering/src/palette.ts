3544c3b011e9ea924956fee4f117f51493bd074e
George B <705427+georgeblahblah@users.noreply.github.com>
2024-05-21 15:16:30 +0100

    Fix live/deadblog headline colour

--- dotcom-rendering/src/palette.ts
+++ dotcom-rendering/src/palette.ts
@@ -77,7 +77,7 @@ const headlineTextLight: PaletteFunction = ({ design, display, theme }) => {
 						case ArticleSpecial.SpecialReport:
 							return sourcePalette.specialReport[200];
 						default:
-							return sourcePalette.neutral[97];
+							return sourcePalette.neutral[7];
 					}
 				}
 				case ArticleDesign.Interview:
