55266adbf9a9d51bbf48748435066c2fe27a62d0
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-05-21 12:07:10 +0100

    Remove unecessary typography overrides

--- apps-rendering/src/components/Byline/GalleryByline.tsx
+++ apps-rendering/src/components/Byline/GalleryByline.tsx
@@ -1,11 +1,7 @@
 import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
 import type { ArticleFormat } from '@guardian/libs';
-import {
-	headlineMedium17,
-	headlineMediumItalic17,
-	remSpace,
-} from '@guardian/source-foundations';
+import { headlineMediumItalic17, remSpace } from '@guardian/source-foundations';
 import type { Option } from '../../../vendor/@guardian/types/index';
 import { grid } from 'grid/grid';
 import { border, text } from 'palette';
@@ -25,14 +21,13 @@ const bylineStyles = (format: ArticleFormat): SerializedStyles => css`
 `;
 
 const anchorStyles = (format: ArticleFormat): SerializedStyles => css`
-	${headlineMedium17};
+	${headlineMediumItalic17};
 	/**
 	 * Typography preset styles should not be overridden.
 	 * This has been done because the styles do not directly map to the new presets.
 	 * Please speak to your team's designer and update this to use a more appropriate preset.
 	 */
 	font-weight: 700;
-	font-style: italic;
 	color: ${text.gallery(format)};
 	text-decoration: none;
 	border-bottom: 1px solid ${border.bylineLink(format)};
