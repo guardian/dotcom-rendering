49f37c1ac59e47714025c23940ef4a2503c2d1ee
Tom Wey <tom.wey@theguardian.com>
2024-06-03 12:08:58 +0100

    Refactor

--- dotcom-rendering/src/model/enhanceTags.ts
+++ dotcom-rendering/src/model/enhanceTags.ts
@@ -1,23 +1,24 @@
 import type { FETagType, TagType } from '../types/tag';
 
-export const enhanceTags = (tags: FETagType[]): TagType[] => {
-	return tags.map(({ properties }) => {
-		const {
-			id,
-			tagType,
-			webTitle,
-			twitterHandle,
-			bylineImageUrl,
-			contributorLargeImagePath,
-		} = properties;
+export const enhanceTags = (tags: FETagType[]): TagType[] =>
+	tags.map((tag) => enhanceTag(tag));
 
-		return {
-			id,
-			type: tagType,
-			title: webTitle,
-			twitterHandle,
-			bylineImageUrl,
-			bylineLargeImageUrl: contributorLargeImagePath,
-		};
-	});
+const enhanceTag = ({ properties }: FETagType): TagType => {
+	const {
+		id,
+		tagType,
+		webTitle,
+		twitterHandle,
+		bylineImageUrl,
+		contributorLargeImagePath,
+	} = properties;
+
+	return {
+		id,
+		type: tagType,
+		title: webTitle,
+		twitterHandle,
+		bylineImageUrl,
+		bylineLargeImageUrl: contributorLargeImagePath,
+	};
 };
