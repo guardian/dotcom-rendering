8de1cc1ddb996244fb8bc17aa6891094c2a6024b
Charlotte <charlotte.emms@theguardian.com>
2024-06-03 17:18:23 +0100

    feat: update kicker styles
    - uses sans style font for kickers
    - increases weight of font and decreases size for live kickers (with pulsing dot)
    - adds Kicker Storybook story
    - updates palette logic, including deciding container overrides on fronts
    - updates enhanceCards logic for trails to keep sublink format over inheriting the parent card format

--- dotcom-rendering/src/components/Kicker.tsx
+++ dotcom-rendering/src/components/Kicker.tsx
@@ -1,8 +1,13 @@
 import { css } from '@emotion/react';
+import {
+	space,
+	textSans15,
+	textSansBold14,
+} from '@guardian/source/foundations';
+import { palette } from '../palette';
 import { Island } from './Island';
 import { PulsingDot } from './PulsingDot.importable';
 
-// Defines a prefix to be used with a headline (e.g. 'Live /')
 type Props = {
 	text: string;
 	color: string;
@@ -10,33 +15,63 @@ type Props = {
 	hideLineBreak?: boolean;
 };
 
-const kickerStyles = (colour: string) => css`
-	color: ${colour};
-	font-weight: 700;
-	margin-right: 4px;
+const standardTextStyles = css`
+	${textSans15}
+`;
+const liveTextStyles = css`
+	${textSansBold14}
+	display: flex;
+	flex-direction: row;
+	align-items: baseline;
+	width: fit-content;
+	padding: 0 ${space[1]}px;
+
+	/*
+	This is to keep the same height as the standard kicker
+	which has a slightly larger font
+	*/
+	margin-bottom: 1px;
 `;
 
+const hideLineBreakStyles = css`
+	display: inline-block;
+	margin-right: ${space[1]}px;
+`;
+
+/**
+ * The kicker is a prefix to be used with a headline (e.g. 'Live')
+ */
 export const Kicker = ({
 	text,
 	color,
 	showPulsingDot,
 	hideLineBreak,
 }: Props) => {
+	/** @todo
+	 * Future optimisation is to not have color as a prop, but to infer this through format and CSS vars.
+	 * This would also allow showPulsingDot to be inferred from the format (is LiveBlog)
+	 */
+	const isLiveKicker = !!showPulsingDot;
+
 	return (
 		<div
 			css={[
-				kickerStyles(color),
-				hideLineBreak &&
-					css`
-						display: inline-block;
-					`,
+				isLiveKicker ? liveTextStyles : standardTextStyles,
+				hideLineBreak && hideLineBreakStyles,
 			]}
+			style={{
+				color: isLiveKicker ? palette('--kicker-text-live') : color,
+				backgroundColor: isLiveKicker
+					? palette('--kicker-background-live')
+					: 'transparent',
+			}}
 		>
 			{showPulsingDot && (
 				<Island priority="enhancement" defer={{ until: 'visible' }}>
-					<PulsingDot colour={color} />
+					<PulsingDot colour={palette('--kicker-pulsing-dot-live')} />
 				</Island>
 			)}
+
 			{text}
 		</div>
 	);
