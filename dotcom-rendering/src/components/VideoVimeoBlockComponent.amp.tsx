deed4ec441cffa1d71308dafddabb125fe634457
Max Duval <max.duval@theguardian.com>
2024-06-18 14:20:27 +0100

    fix: handle missing video ID gracefully
    resiliency to malformed data

--- dotcom-rendering/src/components/VideoVimeoBlockComponent.amp.tsx
+++ dotcom-rendering/src/components/VideoVimeoBlockComponent.amp.tsx
@@ -1,6 +1,8 @@
 import { getIdFromUrl } from '../lib/get-video-id.amp';
+import { logger } from '../server/lib/logging';
 import type { VideoVimeoBlockElement } from '../types/content';
 import { Caption } from './Caption.amp';
+import { levels } from 'log4js';
 
 type Props = {
 	element: VideoVimeoBlockElement;
@@ -14,6 +16,9 @@ export const VideoVimeoBlockComponent = ({ element, pillar }: Props) => {
 	// We should remove this once ☝️ is fixed.
 	const url = element.url === '' ? element.originalUrl ?? '' : element.url;
 	const vimeoId = getIdFromUrl(url, true);
+
+	logger.log(levels.ERROR, `Could not get an id from: ${url}`);
+	if (!vimeoId) return null;
 
 	return (
 		<Caption captionText={element.caption} pillar={pillar}>
