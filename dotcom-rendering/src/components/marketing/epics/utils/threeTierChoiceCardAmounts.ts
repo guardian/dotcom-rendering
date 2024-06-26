8e569ef5b01659c1280f96ab3a3b2c5b0c6381df
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-06-26 14:52:17 +0100

    increase tier two prices in new choice card designs

--- dotcom-rendering/src/components/marketing/epics/utils/threeTierChoiceCardAmounts.ts
+++ dotcom-rendering/src/components/marketing/epics/utils/threeTierChoiceCardAmounts.ts
@@ -5,40 +5,41 @@ import {
 
 export type SupportTier = 'support' | 'allAccess' | 'other';
 
+// ToDo: fetch this in a way that isn't hardcoded
 export const threeTierChoiceCardAmounts = {
 	GBPCountries: {
 		support: 4,
-		allAccess: 10,
+		allAccess: 12,
 		other: 0,
 	},
 	UnitedStates: {
 		support: 5,
-		allAccess: 13,
+		allAccess: 15,
 		other: 0,
 	},
 	AUDCountries: {
 		support: 10,
-		allAccess: 17,
+		allAccess: 20,
 		other: 0,
 	},
 	EURCountries: {
 		support: 4,
-		allAccess: 10,
+		allAccess: 12,
 		other: 0,
 	},
 	NZDCountries: {
 		support: 10,
-		allAccess: 17,
+		allAccess: 20,
 		other: 0,
 	},
 	Canada: {
 		support: 5,
-		allAccess: 13,
+		allAccess: 15,
 		other: 0,
 	},
 	International: {
 		support: 3,
-		allAccess: 13,
+		allAccess: 15,
 		other: 0,
 	},
 } as const satisfies Record<CountryGroupId, Record<SupportTier, number>>;
