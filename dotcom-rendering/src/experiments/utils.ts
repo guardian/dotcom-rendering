ccce66c5219bed9065abfa1e8dbd6117065fcc34
Max Duval <max.duval@theguardian.com>
2024-06-27 14:12:29 +0100

    feat: collect web vitals in new pipeline

--- dotcom-rendering/src/experiments/utils.ts
+++ dotcom-rendering/src/experiments/utils.ts
@@ -1,7 +1,9 @@
 import { bypassCommercialMetricsSampling } from '@guardian/commercial';
 import { bypassCoreWebVitalsSampling } from '@guardian/core-web-vitals';
+import { bypassCoreWebVitalsSampling as bypassCoreWebVitalsSamplingAttribution } from '@guardian/core-web-vitals-attribution';
 
 export const bypassMetricsSampling = (): void => {
 	void bypassCommercialMetricsSampling();
 	void bypassCoreWebVitalsSampling();
+	void bypassCoreWebVitalsSamplingAttribution();
 };
