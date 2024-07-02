147869ea5ab1b36b2d34a2e5bbdcb1c93582ebc4
Max Duval <max.duval@theguardian.com>
2024-07-02 17:11:42 +0100

    refactor: record edition appropriately
    INT -> International
    EUR -> Europe

--- dotcom-rendering/src/client/ophan/recordInitialPageEvents.ts
+++ dotcom-rendering/src/client/ophan/recordInitialPageEvents.ts
@@ -1,5 +1,19 @@
+import { type EditionId } from '../../lib/edition';
 import type { RenderingTarget } from '../../types/renderingTarget';
 import { getOphan, recordExperiences, recordPerformance } from './ophan';
+
+const getEditionForOphan = (editionId: EditionId) => {
+	switch (editionId) {
+		case 'UK':
+		case 'US':
+		case 'AU':
+			return editionId;
+		case 'INT':
+			return 'International';
+		case 'EUR':
+			return 'Europe';
+	}
+};
 
 export const recordInitialPageEvents = async (
 	renderingTarget: RenderingTarget,
@@ -7,11 +21,8 @@ export const recordInitialPageEvents = async (
 	const { record } = await getOphan(renderingTarget);
 
 	void recordExperiences(renderingTarget, ['dotcom-rendering']);
-	record({
-		// @ts-expect-error -- Type 'EditionId' is not assignable to type 'TEdition | undefined'.
-		// Type '"INT"' is not assignable to type 'TEdition | undefined'.
-		edition: window.guardian.config.page.edition,
-	});
+	const edition = getEditionForOphan(window.guardian.config.page.edition);
+	record({ edition });
 
 	// We wait for the load event so that we can be sure our assetPerformance is reported as expected.
 	window.addEventListener('load', function load() {
