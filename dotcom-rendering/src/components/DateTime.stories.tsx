8cd79ed791379fbf33481ba4067d588fcdd8258a
Anna Beddow <anna.beddow@gmail.com>
2024-07-03 09:51:52 +0100

    Fix stories

--- dotcom-rendering/src/components/DateTime.stories.tsx
+++ dotcom-rendering/src/components/DateTime.stories.tsx
@@ -1,21 +1,39 @@
 import { css } from '@emotion/react';
 import { space, textSans15 } from '@guardian/source/foundations';
 import type { Meta, StoryObj } from '@storybook/react';
+import type { EditionId } from '../lib/edition';
+import { ConfigProvider } from './ConfigContext';
 import { DateTime } from './DateTime';
 
+interface Parameters {
+	config?: { editionId?: EditionId };
+}
 const meta: Meta<typeof DateTime> = {
 	title: 'Components/DateTime',
 	component: DateTime,
-	decorators: (Story) => (
-		<div
-			css={css`
-				padding: ${space[2]}px;
-				${textSans15}
-			`}
-		>
-			<Story />
-		</div>
-	),
+	decorators: (Story, { parameters }: { parameters: Parameters }) => {
+		const editionId = parameters?.config?.editionId ?? 'UK';
+		return (
+			<div
+				css={css`
+					padding: ${space[2]}px;
+					${textSans15}
+				`}
+			>
+				<ConfigProvider
+					value={{
+						renderingTarget: 'Web',
+						darkModeAvailable: false,
+						updateLogoAdPartnerSwitch: false,
+						assetOrigin: '/',
+						editionId,
+					}}
+				>
+					<Story />
+				</ConfigProvider>
+			</div>
+		);
+	},
 };
 
 type Story = StoryObj<typeof DateTime>;
@@ -24,13 +42,12 @@ type Story = StoryObj<typeof DateTime>;
 const date = new Date('2024-01-14T12:34:00.000Z');
 
 export const UK: Story = {
-	args: { date, editionId: 'UK' },
+	args: { date },
 };
 
 export const TimeOnly: Story = {
 	args: {
 		date,
-		editionId: 'UK',
 		showWeekday: false,
 		showDate: false,
 		showTime: true,
@@ -40,7 +57,6 @@ export const TimeOnly: Story = {
 export const DateOnly: Story = {
 	args: {
 		date,
-		editionId: 'UK',
 		showWeekday: false,
 		showDate: true,
 		showTime: false,
@@ -50,7 +66,6 @@ export const DateOnly: Story = {
 export const WeekdayDateOnly: Story = {
 	args: {
 		date,
-		editionId: 'UK',
 		showWeekday: true,
 		showDate: true,
 		showTime: false,
@@ -58,9 +73,9 @@ export const WeekdayDateOnly: Story = {
 };
 
 export const US: Story = {
+	parameters: { config: { editionId: 'US' } },
 	args: {
 		date,
-		editionId: 'US',
 		showWeekday: true,
 		showDate: true,
 		showTime: true,
@@ -68,9 +83,9 @@ export const US: Story = {
 };
 
 export const AU: Story = {
+	parameters: { config: { editionId: 'AU' } },
 	args: {
 		date,
-		editionId: 'AU',
 		showWeekday: true,
 		showDate: true,
 		showTime: true,
@@ -78,9 +93,9 @@ export const AU: Story = {
 };
 
 export const EUR: Story = {
+	parameters: { config: { editionId: 'EUR' } },
 	args: {
 		date,
-		editionId: 'EUR',
 		showWeekday: true,
 		showDate: true,
 		showTime: true,
@@ -88,9 +103,9 @@ export const EUR: Story = {
 };
 
 export const INT: Story = {
+	parameters: { config: { editionId: 'INT' } },
 	args: {
 		date,
-		editionId: 'INT',
 		showWeekday: true,
 		showDate: true,
 		showTime: true,
