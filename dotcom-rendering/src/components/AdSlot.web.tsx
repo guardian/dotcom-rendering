7752a6f87737c9fdf85e0515df07227385389662
Dina Hafez <dina.hafez@guardian.co.uk>
2024-07-05 16:34:13 +0100

    Replace fixed width size with mobileSticky ad width

--- dotcom-rendering/src/components/AdSlot.web.tsx
+++ dotcom-rendering/src/components/AdSlot.web.tsx
@@ -464,8 +464,8 @@ const mobileStickyAdStylesFullWidth = css`
 	background-color: ${palette.neutral[97]};
 
 	.ad-slot[data-label-show='true']::before {
-		padding-left: calc((100% - 320px) / 2);
-		padding-right: calc((100% - 320px) / 2);
+		padding-left: calc((100% - ${adSizes.mobilesticky.width}px) / 2);
+		padding-right: calc((100% - ${adSizes.mobilesticky.width}px) / 2);
 	}
 `;
 
