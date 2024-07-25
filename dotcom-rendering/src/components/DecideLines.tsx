7d4fe12814b85fdbd89538496b23bd8b92ac18f8
Max Duval <hi@mxdvl.com>
2024-07-25 14:20:29 +0100

    fix: css-only dotted lines

--- dotcom-rendering/src/components/DecideLines.tsx
+++ dotcom-rendering/src/components/DecideLines.tsx
@@ -1,29 +1,47 @@
 import { css } from '@emotion/react';
 import { ArticleDesign, type ArticleFormat, Pillar } from '@guardian/libs';
-import {
-	DottedLines,
-	StraightLines,
-} from '@guardian/source-development-kitchen/react-components';
+import { palette } from '@guardian/source/foundations';
+import { StraightLines } from '@guardian/source-development-kitchen/react-components';
 
 type Props = {
 	format: ArticleFormat;
 	color?: string;
 };
 
+type DottedLinesProps = { color?: string; count: number };
+
+/**
+ * **TODO:** move this up to Source Design Kitchen!
+ * the previous implementation uses SVGs, but creates an invalid
+ * HTML document if you have two on the same page, due to the inclusion
+ * of `id` which are not unique.
+ *
+ * Here we favour a CSS-only approach which works just as well…
+ */
+const DottedLines = ({
+	count,
+	color = palette.neutral[86],
+}: DottedLinesProps) => (
+	<div
+		style={{ height: `${count * 3}px`, color }}
+		css={css`
+			background-size: 3px 3px;
+			background-position: top center;
+			background-image: radial-gradient(
+				currentColor,
+				currentColor 1px,
+				transparent 1px
+			);
+		`}
+	></div>
+);
+
 export const DecideLines = ({ format, color }: Props) => {
 	const count = format.design === ArticleDesign.Comment ? 8 : 4;
 
 	switch (format.theme) {
 		case Pillar.Sport:
-			return (
-				<DottedLines
-					cssOverrides={css`
-						display: block;
-					`}
-					count={count}
-					color={color}
-				/>
-			);
+			return <DottedLines count={count} color={color} />;
 		default:
 			return (
 				<StraightLines
