d47a46ccc271dcd653c8f7c032ac9f132253ea00
Rowanne Kabalan <rowanne.kabalan@guardian.co.uk>
2024-07-29 12:24:00 +0100

    undo ar changes

--- apps-rendering/src/item.ts
+++ apps-rendering/src/item.ts
@@ -11,7 +11,7 @@ import type { Content } from '@guardian/content-api-models/v1/content';
 import type { Element } from '@guardian/content-api-models/v1/element';
 import { ElementType } from '@guardian/content-api-models/v1/elementType';
 import type { Tag } from '@guardian/content-api-models/v1/tag';
-import { type ArticleFormat } from '@guardian/libs';
+import type { ArticleFormat } from '@guardian/libs';
 import {
 	ArticleDesign,
 	ArticleDisplay,
