cd5d2305fd19bda7bdbd4fda0ca65310afe82365
Anna Beddow <anna.beddow@gmail.com>
2024-08-01 12:22:29 +0100

    Add missing source import

--- dotcom-rendering/src/components/HighlightsContainer.importable.tsx
+++ dotcom-rendering/src/components/HighlightsContainer.importable.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { from, space, until } from '@guardian/source/foundations';
+import { between, from, space, until } from '@guardian/source/foundations';
 import {
 	Button,
 	Hide,
