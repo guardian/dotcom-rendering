8a7c10159391722aac7c9d5c89089f0c316a00a7
Ravi <7014230+arelra@users.noreply.github.com>
2024-08-06 00:30:15 +0100

    Add comments describing YouTube loading process

--- dotcom-rendering/src/components/YoutubeAtom/YoutubeAtom.tsx
+++ dotcom-rendering/src/components/YoutubeAtom/YoutubeAtom.tsx
@@ -126,6 +126,21 @@ export const YoutubeAtom = ({
 	 */
 	const compositeEventEmitters = [playerState, ...eventEmitters];
 
+	/**
+	 * The loading sequence of the YoutubeAtom is as follows:
+	 *
+	 * Overlay -> Placeholder -> Player
+	 *
+	 * In detail:
+	 *
+	 * 1. Initially show the overlay if it exists
+	 * 2. When the overlay is clicked
+	 *     2.1 Remove the overlay
+	 *     2.2 Show the placeholder until the player is ready
+	 * 3. When consent and ad targeting is available render the player to initiate loading of the YouTube player
+	 * 4. When the player is ready the placeholder is removed and the YouTube player is shown
+	 */
+
 	const hasOverlay = !!(overrideImage ?? posterImage);
 
 	/**
@@ -184,31 +199,37 @@ export const YoutubeAtom = ({
 			shouldPauseOutOfView={shouldPauseOutOfView}
 		>
 			<MaintainAspectRatio height={height} width={width}>
-				{loadPlayer && consentState && adTargeting && (
-					<YoutubeAtomPlayer
-						videoId={videoId}
-						uniqueId={uniqueId}
-						adTargeting={adTargeting}
-						consentState={consentState}
-						height={height}
-						width={width}
-						title={title}
-						origin={origin}
-						eventEmitters={compositeEventEmitters}
-						/**
-						 * If there is an overlay we want to autoplay
-						 * If there is not an overlay the user will use the YouTube player UI to play
-						 */
-						autoPlay={hasOverlay}
-						onReady={playerReadyCallback}
-						enableIma={imaEnabled}
-						pauseVideo={pauseVideo}
-						deactivateVideo={() => {
-							setIsActive(false);
-						}}
-						abTestParticipations={abTestParticipations}
-					/>
-				)}
+				{
+					/**
+					 * Consent and ad targeting are set by subsequent re-renders
+					 * So we wait until they are set before rendering the player
+					 */
+					loadPlayer && consentState && adTargeting && (
+						<YoutubeAtomPlayer
+							videoId={videoId}
+							uniqueId={uniqueId}
+							adTargeting={adTargeting}
+							consentState={consentState}
+							height={height}
+							width={width}
+							title={title}
+							origin={origin}
+							eventEmitters={compositeEventEmitters}
+							/**
+							 * If there is an overlay we want to autoplay
+							 * If there isn't an overlay the user will use the YouTube player UI to play
+							 */
+							autoPlay={hasOverlay}
+							onReady={playerReadyCallback}
+							enableIma={imaEnabled}
+							pauseVideo={pauseVideo}
+							deactivateVideo={() => {
+								setIsActive(false);
+							}}
+							abTestParticipations={abTestParticipations}
+						/>
+					)
+				}
 				{showOverlay && (
 					<YoutubeAtomOverlay
 						uniqueId={uniqueId}
