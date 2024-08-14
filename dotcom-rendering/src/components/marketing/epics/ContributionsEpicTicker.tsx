be86afd7babb29f48cfdc315c6858d4c142e4a56
Charley_Campbell <charley.campbell@guardian.co.uk>
2024-08-14 15:22:17 +0100

    label correct

--- dotcom-rendering/src/components/marketing/epics/ContributionsEpicTicker.tsx
+++ dotcom-rendering/src/components/marketing/epics/ContributionsEpicTicker.tsx
@@ -1,83 +1,33 @@
 import { css } from '@emotion/react';
-import {
-	space,
-	textSans17,
-	textSansBold17,
-} from '@guardian/source/foundations';
+import { textSans17, textSansBold17 } from '@guardian/source/foundations';
 import type { TickerSettings } from '@guardian/support-dotcom-components/dist/shared/src/types';
 import { useEffect, useState } from 'react';
 import { useIsInView } from '../../../lib/useIsInView';
 import { useTicker } from '../hooks/useTicker';
 import type { ReactComponent } from '../lib/ReactComponent';
 
-const tickerProgressBar = css`
+const rootStyles = css`
 	position: relative;
-	height: ${space[3]}px;
+	height: 65px;
+	margin-bottom: 15px;
+	line-height: 18px;
 `;
 
-const tickerProgressBarBackground = css`
-	width: 100%;
-	height: ${space[3]}px;
-	overflow: hidden;
-	background-color: #5056f5;
-	position: absolute;
-	border-radius: ${space[2]}px;
-`;
-
-const progressBarTransform = (
-	end: number,
-	runningTotal: number,
-	total: number,
-): string => {
-	const haveStartedAnimating = runningTotal > 0;
-
-	if (!haveStartedAnimating) {
-		return 'translateX(-100%)';
-	}
-
-	const percentage = (total / end) * 100 - 100;
-
-	return `translate3d(${percentage >= 0 ? 0 : percentage}%, 0, 0)`;
-};
-
-const tickerProgressBarFill = (
-	end: number,
-	runningTotal: number,
-	total: number,
-) => css`
-	background-color: rgba(80, 86, 245, 0.35);
-	position: absolute;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	transform: ${progressBarTransform(end, total, runningTotal)};
-	transition: transform 3s cubic-bezier(0.25, 0.55, 0.2, 0.85);
-	border-radius: ${space[2]}px;
-`;
-
-const tickerHeadline = css`
-	${textSansBold17};
-	margin-bottom: ${space[2]}px;
-`;
-
-const tickerLabelContainer = css`
-	position: relative;
-	display: flex;
-	margin-top: ${space[1]}px;
-`;
-
-const tickerLabel = css`
+const goalLabelStyles = css`
 	${textSans17}
 `;
 
-const tickerLabelTotal = css`
+const countLabelStyles = css`
 	${textSansBold17};
 	color: #5056f5;
 `;
 
-const containerStyles = css`
-	position: relative;
+const progressBarHeight = 10;
+
+const soFarContainerStyles = css`
+	position: absolute;
+	left: 0;
+	bottom: ${progressBarHeight + 5}px;
 `;
 
 export type Props = {
@@ -105,35 +55,21 @@ export const ContributionsEpicTicker: ReactComponent<Props> = ({
 	}, [hasBeenSeen]);
 
 	const runningTotal = useTicker(total, readyToAnimate);
-	console.log(total, goal, runningTotal);
-	const isGoalReached = total >= goal;
-	const end = total > goal ? total + total * 0.15 : goal;
-	console.log(end);
 
 	const currencySymbol = tickerSettings.currencySymbol;
 	return (
-		<div ref={setNode} css={containerStyles}>
-			<h2 css={tickerHeadline}>
-				{isGoalReached
-					? tickerSettings.copy.goalReachedPrimary
-					: tickerSettings.copy.countLabel}
-			</h2>
-			<div css={tickerProgressBar}>
-				<div css={tickerProgressBarBackground}>
-					<div
-						css={tickerProgressBarFill(end, total, runningTotal)}
-					/>
+		<div ref={setNode} css={rootStyles}>
+			<div>
+				<div css={soFarContainerStyles}>
+					<div css={goalLabelStyles}>
+						<span css={countLabelStyles}>
+							{currencySymbol}
+							{runningTotal.toLocaleString()}
+						</span>{' '}
+						of {currencySymbol}
+						{goal.toLocaleString()} goal
+					</div>
 				</div>
-			</div>
-			<div css={tickerLabelContainer}>
-				<p css={tickerLabel}>
-					<span css={tickerLabelTotal}>
-						{currencySymbol}
-						{total.toLocaleString()}
-					</span>{' '}
-					of {currencySymbol}
-					{goal.toLocaleString()} goal
-				</p>
 			</div>
 		</div>
 	);
