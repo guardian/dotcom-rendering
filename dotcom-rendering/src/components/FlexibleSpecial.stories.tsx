38adcd23d9bb6b7b8beef18f54a3691293a41fa4
Anna Beddow <anna.beddow@gmail.com>
2024-08-15 11:37:36 +0100

    Add story variations

--- dotcom-rendering/src/components/FlexibleSpecial.stories.tsx
+++ dotcom-rendering/src/components/FlexibleSpecial.stories.tsx
@@ -45,12 +45,52 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const One: Story = {
-	name: 'With one standard card',
+	name: 'With one splash card',
 	args: {
 		groupedTrails: {
 			...defaultGroupedTrails,
 			snap: [],
 			standard: trails.slice(0, 1),
+		},
+	},
+};
+export const Two: Story = {
+	name: 'With one splash card and one standard card',
+	args: {
+		groupedTrails: {
+			...defaultGroupedTrails,
+			snap: [],
+			standard: trails.slice(0, 2),
+		},
+	},
+};
+export const Three: Story = {
+	name: 'With one splash card and two standard cards',
+	args: {
+		groupedTrails: {
+			...defaultGroupedTrails,
+			snap: [],
+			standard: trails.slice(0, 3),
+		},
+	},
+};
+export const Four: Story = {
+	name: 'With one splash card and three standard cards',
+	args: {
+		groupedTrails: {
+			...defaultGroupedTrails,
+			snap: [],
+			standard: trails.slice(0, 4),
+		},
+	},
+};
+export const Five: Story = {
+	name: 'With one splash card and four standard cards',
+	args: {
+		groupedTrails: {
+			...defaultGroupedTrails,
+			snap: [],
+			standard: trails.slice(0, 5),
 		},
 	},
 };
