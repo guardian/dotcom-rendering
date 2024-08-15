3582c2282c083d1abafabf9274ebe819b77811ec
Anna Beddow <anna.beddow@gmail.com>
2024-08-15 15:34:19 +0100

    Add imports and use correct container styles

--- dotcom-rendering/src/components/FlexibleSpecial.tsx
+++ dotcom-rendering/src/components/FlexibleSpecial.tsx
@@ -1,3 +1,6 @@
+import { css } from '@emotion/react';
+import { space } from '@guardian/source/foundations';
+import { palette } from '../palette';
 import type {
 	DCRContainerPalette,
 	DCRFrontCard,
@@ -15,6 +18,16 @@ type Props = {
 	showAge?: boolean;
 	absoluteServerTimes: boolean;
 };
+
+const topBarStyles = css`
+	border-top: 1px solid ${palette('--card-border-top')};
+	content: '';
+	z-index: 2;
+	width: 100%;
+	margin: 0 10px;
+	padding-bottom: ${space[2]}px;
+	background-color: unset;
+`;
 
 export const OneCardLayout = ({
 	cards,
@@ -37,7 +50,7 @@ export const OneCardLayout = ({
 				<FrontCard
 					trail={cards[0]}
 					containerPalette={containerPalette}
-					containerType="dynamic/package"
+					containerType="flexible/special"
 					showAge={showAge}
 					absoluteServerTimes={absoluteServerTimes}
 					headlineSize="large"
@@ -74,32 +87,36 @@ const TwoCardOrFourCardLayout = ({
 	//TODO : Rename isTwoCards to a better name
 	const isTwoCards = cards.length <= 2;
 	return (
-		<UL direction="row" padBottom={padBottom}>
-			{cards.map((card, cardIndex) => {
-				return (
-					<LI
-						stretch={false}
-						percentage={isTwoCards ? '50%' : '25%'}
-						key={card.url}
-						padSides={true}
-						showDivider={cardIndex > 0}
-					>
-						<FrontCard
-							trail={card}
-							containerPalette={containerPalette}
-							containerType="dynamic/package"
-							showAge={showAge}
-							absoluteServerTimes={absoluteServerTimes}
-							image={showImage ? card.image : undefined}
-							imageLoading={imageLoading}
-							imagePositionOnDesktop={
-								isTwoCards ? 'left' : 'bottom'
-							}
-						/>
-					</LI>
-				);
-			})}
-		</UL>
+		<>
+			<div css={topBarStyles} />
+			<UL direction="row" padBottom={padBottom}>
+				{cards.map((card, cardIndex) => {
+					return (
+						<LI
+							stretch={false}
+							percentage={isTwoCards ? '50%' : '25%'}
+							key={card.url}
+							padSides={true}
+							showDivider={cardIndex > 0}
+						>
+							<FrontCard
+								trail={card}
+								kickerText={card.kickerText}
+								containerPalette={containerPalette}
+								containerType="flexible/special"
+								showAge={showAge}
+								absoluteServerTimes={absoluteServerTimes}
+								image={showImage ? card.image : undefined}
+								imageLoading={imageLoading}
+								imagePositionOnDesktop={
+									isTwoCards ? 'left' : 'bottom'
+								}
+							/>
+						</LI>
+					);
+				})}
+			</UL>
+		</>
 	);
 };
 
