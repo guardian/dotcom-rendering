f7f6889d898e3e1d4188685e74aefed742204565
Tom Forbes <tom.forbes@theguardian.com>
2024-08-28 16:04:39 +0100

    delete

--- dotcom-rendering/src/components/marketing/banners/designableBanner/stories/DesignableBanner.stories.tsx
+++ dotcom-rendering/src/components/marketing/banners/designableBanner/stories/DesignableBanner.stories.tsx
@@ -54,56 +54,6 @@ export const Default: Story = {
 	name: 'Basic DesignableBanner',
 };
 
-// const contentNoHeading = {
-// 	paragraphs: [
-// 		'Fearless, investigative reporting shapes a fairer world. At the Guardian, our independence allows us to chase the truth wherever it takes us. <strong>We have no shareholders.</strong> No vested interests. Just the determination and passion to bring readers quality reporting, including groundbreaking investigations.',
-// 		'We do not shy away. And we provide all this for free, for everyone.',
-// 	],
-// 	highlightedText:
-// 		'Show your support today from just %%CURRENCY_SYMBOL%%1, or sustain us long term with a little more. Thank you.',
-// 	cta: {
-// 		text: 'Support once',
-// 		baseUrl: 'https://support.theguardian.com/contribute/one-off',
-// 	},
-// 	secondaryCta: {
-// 		type: SecondaryCtaType.Custom,
-// 		cta: {
-// 			text: 'Support monthly',
-// 			baseUrl: 'https://support.theguardian.com/contribute/recurring',
-// 		},
-// 	},
-// };
-//
-// const contentWithHeading = {
-// 	...contentNoHeading,
-// 	heading: 'Show your support for reader-funded journalism',
-// };
-
-// const mobileContentNoHeading = {
-// 	paragraphs: [
-// 		'Fearless, investigative reporting shapes a fairer world. At the Guardian, our independence allows us to chase the truth wherever it takes us. <strong>We have no shareholders.</strong> No vested interests. Just the determination and passion to bring readers quality reporting, including groundbreaking investigations.',
-// 		'We do not shy away. And we provide all this for free, for everyone.',
-// 	],
-// 	highlightedText:
-// 		'Show your support today from just %%CURRENCY_SYMBOL%%1, or sustain us long term with a little more. Thank you.',
-// 	cta: {
-// 		text: 'Support us',
-// 		baseUrl: 'https://support.theguardian.com/contribute/one-off',
-// 	},
-// 	secondaryCta: {
-// 		type: SecondaryCtaType.Custom,
-// 		cta: {
-// 			text: 'Learn more',
-// 			baseUrl: 'https://support.theguardian.com/contribute/recurring',
-// 		},
-// 	},
-// };
-//
-// const mobileContentWithHeading = {
-// 	...mobileContentNoHeading,
-// 	heading: 'Show your support for reader-funded journalism',
-// };
-
 const tickerSettings: TickerSettings = {
 	countType: TickerCountType.money,
 	endType: TickerEndType.hardstop,
