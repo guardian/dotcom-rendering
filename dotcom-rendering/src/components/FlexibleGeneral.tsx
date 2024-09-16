d4fcd21404b1a138a1aa88b9622aeed6241d6f1e
Charlotte <charlotte.emms@theguardian.com>
2024-09-16 13:58:23 +0100

    add splash group to DCRGroupedTrails and add basic support for flexible/general container
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>
    Co-authored-by: Georges Lebreton <georges.lebreton@guardian.co.uk>

---
+++ dotcom-rendering/src/components/FlexibleGeneral.tsx
@@ -0,0 +1,218 @@
+import type { BoostLevel } from '../types/content';
+import type {
+	DCRContainerPalette,
+	DCRFrontCard,
+	DCRGroupedTrails,
+} from '../types/front';
+import type { ImagePositionType } from './Card/components/ImageWrapper';
+import { LI } from './Card/components/LI';
+import { UL } from './Card/components/UL';
+import type { Loading } from './CardPicture';
+import { FrontCard } from './FrontCard';
+import type { Alignment } from './SupportingContent';
+
+type Props = {
+	groupedTrails: DCRGroupedTrails;
+	imageLoading: Loading;
+	containerPalette?: DCRContainerPalette;
+	showAge?: boolean;
+	absoluteServerTimes: boolean;
+};
+
+type boostProperties = {
+	headlineSize: SmallHeadlineSize;
+	headlineSizeOnMobile: SmallHeadlineSize;
+	headlineSizeOnTablet: SmallHeadlineSize;
+	imagePositionOnDesktop: ImagePositionType;
+	imagePositionOnMobile: ImagePositionType;
+	supportingContentAlignment: Alignment;
+};
+
+/**
+ * Boosting a card will affect the layout and style of the card. This function will determine the properties of the card based on the boost level.
+ */
+const determineCardProperties = (
+	boostLevel: BoostLevel = 'default',
+	supportingContentLength: number,
+): boostProperties => {
+	switch (boostLevel) {
+		// The default boost level is equal to no boost. It is the same as the default card layout.
+		case 'default':
+			return {
+				headlineSize: 'medium',
+				headlineSizeOnMobile: 'tiny',
+				headlineSizeOnTablet: 'small',
+				imagePositionOnDesktop: 'right',
+				imagePositionOnMobile: 'top',
+				supportingContentAlignment:
+					supportingContentLength >= 3 ? 'horizontal' : 'vertical',
+			};
+		case 'boost':
+			return {
+				headlineSize: 'large',
+				headlineSizeOnMobile: 'small',
+				headlineSizeOnTablet: 'medium',
+				imagePositionOnDesktop: 'right',
+				imagePositionOnMobile: 'top',
+				supportingContentAlignment:
+					supportingContentLength >= 3 ? 'horizontal' : 'vertical',
+			};
+		case 'megaboost':
+			return {
+				headlineSize: 'large',
+				headlineSizeOnMobile: 'medium',
+				headlineSizeOnTablet: 'medium',
+				imagePositionOnDesktop: 'bottom',
+				imagePositionOnMobile: 'top',
+				supportingContentAlignment: 'horizontal',
+			};
+		case 'gigaboost':
+			return {
+				headlineSize: 'huge',
+				headlineSizeOnMobile: 'large',
+				headlineSizeOnTablet: 'large',
+				imagePositionOnDesktop: 'bottom',
+				imagePositionOnMobile: 'top',
+				supportingContentAlignment: 'horizontal',
+			};
+	}
+};
+export const OneCardLayout = ({
+	cards,
+	containerPalette,
+	showAge,
+	absoluteServerTimes,
+	imageLoading,
+}: {
+	cards: DCRFrontCard[];
+	imageLoading: Loading;
+	containerPalette?: DCRContainerPalette;
+	showAge?: boolean;
+	absoluteServerTimes: boolean;
+}) => {
+	const card = cards[0];
+	if (!card) return null;
+
+	const {
+		headlineSize,
+		headlineSizeOnMobile,
+		headlineSizeOnTablet,
+		imagePositionOnDesktop,
+		imagePositionOnMobile,
+		supportingContentAlignment,
+	} = determineCardProperties(
+		card.boostLevel,
+		card?.supportingContent?.length ?? 0,
+	);
+	return (
+		<UL padBottom={true}>
+			<LI padSides={true}>
+				<FrontCard
+					trail={card}
+					containerPalette={containerPalette}
+					containerType="flexible/general"
+					showAge={showAge}
+					absoluteServerTimes={absoluteServerTimes}
+					headlineSize={headlineSize}
+					headlineSizeOnMobile={headlineSizeOnMobile}
+					headlineSizeOnTablet={headlineSizeOnTablet}
+					imagePositionOnDesktop={imagePositionOnDesktop}
+					imagePositionOnMobile={imagePositionOnMobile}
+					imageSize="jumbo"
+					trailText={card.trailText}
+					supportingContent={card.supportingContent}
+					supportingContentAlignment={supportingContentAlignment}
+					imageLoading={imageLoading}
+					aspectRatio="5:4"
+					kickerText={card.kickerText}
+					showLivePlayable={card.showLivePlayable}
+					boostedFontSizes={true}
+				/>
+			</LI>
+		</UL>
+	);
+};
+
+const TwoCardOrFourCardLayout = ({
+	cards,
+	containerPalette,
+	showAge,
+	absoluteServerTimes,
+	showImage = true,
+	padBottom,
+	imageLoading,
+}: {
+	cards: DCRFrontCard[];
+	imageLoading: Loading;
+	containerPalette?: DCRContainerPalette;
+	showAge?: boolean;
+	absoluteServerTimes: boolean;
+	showImage?: boolean;
+	padBottom?: boolean;
+}) => {
+	const hasTwoOrFewerCards = cards.length <= 2;
+	return (
+		<UL direction="row" padBottom={padBottom}>
+			{cards.map((card, cardIndex) => {
+				return (
+					<LI
+						stretch={false}
+						percentage={hasTwoOrFewerCards ? '50%' : '25%'}
+						key={card.url}
+						padSides={true}
+						showDivider={cardIndex > 0}
+					>
+						<FrontCard
+							trail={card}
+							containerPalette={containerPalette}
+							containerType="flexible/special"
+							showAge={showAge}
+							absoluteServerTimes={absoluteServerTimes}
+							image={showImage ? card.image : undefined}
+							imageLoading={imageLoading}
+							imagePositionOnDesktop={
+								hasTwoOrFewerCards ? 'left' : 'bottom'
+							}
+							supportingContent={undefined} // we don't want to support sublinks on standard cards here so we hard code to undefined.
+							imageSize={'medium'}
+							aspectRatio="5:4"
+							kickerText={card.kickerText}
+							showLivePlayable={false}
+						/>
+					</LI>
+				);
+			})}
+		</UL>
+	);
+};
+
+export const FlexibleGeneral = ({
+	groupedTrails,
+	containerPalette,
+	showAge,
+	absoluteServerTimes,
+	imageLoading,
+}: Props) => {
+	const splash = [...groupedTrails.splash].slice(0, 1);
+	const cards = [...groupedTrails.standard].slice(0, 8); // TODO check maximum number of cards
+
+	return (
+		<>
+			<OneCardLayout
+				cards={splash}
+				containerPalette={containerPalette}
+				showAge={showAge}
+				absoluteServerTimes={absoluteServerTimes}
+				imageLoading={imageLoading}
+			/>
+
+			<TwoCardOrFourCardLayout
+				cards={cards}
+				containerPalette={containerPalette}
+				showAge={showAge}
+				absoluteServerTimes={absoluteServerTimes}
+				imageLoading={imageLoading}
+			/>
+		</>
+	);
+};
