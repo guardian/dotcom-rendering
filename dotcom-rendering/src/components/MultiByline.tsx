818135cadcb328415a27e0f7472f8ead6c75ac0f
Rhys Mills <rhys.mills@guardian.co.uk>
2024-09-25 09:04:01 +0100

    Add foundation for multi-byline element, based on mini-profiles

---
+++ dotcom-rendering/src/components/MultiByline.tsx
@@ -0,0 +1,88 @@
+import { css } from '@emotion/react';
+import type { ArticleFormat } from '@guardian/libs';
+import { palette } from '@guardian/source/foundations';
+import type { EditionId } from '../lib/edition';
+import type { ArticleElementRenderer } from '../lib/renderElement';
+import type { ServerSideTests, Switches } from '../types/config';
+import type { MultiBylineItem, StarRating } from '../types/content';
+import { MultiBylineItem as MultiBylineItemComponent } from './MultiBylineItem';
+
+interface MiniProfilesProps {
+	format: ArticleFormat;
+	ajaxUrl: string;
+	host?: string;
+	pageId: string;
+	isAdFreeUser: boolean;
+	isSensitive: boolean;
+	abTests: ServerSideTests;
+	switches: Switches;
+	editionId: EditionId;
+	hideCaption?: boolean;
+	starRating?: StarRating;
+	multiBylineItems: MultiBylineItem[];
+	RenderArticleElement: ArticleElementRenderer;
+	/**
+	 * Whether this is the last element in the article. If true, no separator will be rendered.
+	 */
+	isLastElement: boolean;
+}
+
+const separatorStyles = css`
+	width: 140px;
+	margin: 8px 0 2px 0;
+	border-top: 1px solid ${palette.neutral[86]};
+`;
+
+export const MultiByline = ({
+	multiBylineItems,
+	format,
+	ajaxUrl,
+	host,
+	pageId,
+	isAdFreeUser,
+	isSensitive,
+	switches,
+	abTests,
+	editionId,
+	hideCaption,
+	starRating,
+	RenderArticleElement,
+	isLastElement,
+}: MiniProfilesProps) => {
+	return (
+		<ol data-ignore="global-ol-styling">
+			{multiBylineItems.map((multiBylineItem, index) => (
+				// eslint-disable-next-line react/no-array-index-key -- Title should usually be identical, but in case it isn't, also use array index
+				<MultiBylineItemComponent
+					multiBylineItem={multiBylineItem}
+					format={format}
+					key={`${multiBylineItem.title}-${index}`}
+				>
+					{multiBylineItem.body.map((element) => (
+						// eslint-disable-next-line react/jsx-key -- The element array should remain consistent as it's derived from the order of elements in CAPI
+						<RenderArticleElement
+							format={format}
+							element={element}
+							ajaxUrl={ajaxUrl}
+							host={host}
+							index={index}
+							isMainMedia={false}
+							pageId={pageId}
+							webTitle={multiBylineItem.title}
+							isAdFreeUser={isAdFreeUser}
+							isSensitive={isSensitive}
+							switches={switches}
+							abTests={abTests}
+							editionId={editionId}
+							hideCaption={hideCaption}
+							starRating={starRating}
+							forceDropCap="off"
+							isListElement={true}
+						/>
+					))}
+				</MultiBylineItemComponent>
+			))}
+			{!isLastElement && <hr css={separatorStyles} />}
+		</ol>
+	);
+};
---
+++ dotcom-rendering/src/components/MultiBylineItem.tsx
@@ -0,0 +1,112 @@
+import { css } from '@emotion/react';
+import { type ArticleFormat } from '@guardian/libs';
+import { neutral, space, textSans14 } from '@guardian/source/foundations';
+import sanitise from 'sanitize-html';
+import { slugify } from '../model/enhance-H2s';
+import { palette } from '../palette';
+import type { MultiBylineItem as MultiBylineItemModel } from '../types/content';
+import { headingLineStyles } from './KeyTakeaway';
+import { subheadingStyles } from './Subheading';
+
+const multiBylineItemStyles = css`
+	padding-top: 8px;
+`;
+
+/** Nesting is necessary in the bio styles because we receive a string of html from the
+ * field. This can contain the following tags:
+ * Blocks: p, ul, li
+ * Inline: strong, em, a
+ */
+const bioStyles = css`
+	${textSans14};
+	padding: ${space[1]}px 0;
+	color: ${palette('--mini-profiles-text-subdued')};
+	p {
+		margin-bottom: ${space[2]}px;
+	}
+	a {
+		color: ${palette('--link-kicker-text')};
+		text-underline-offset: 3px;
+	}
+	a:not(:hover) {
+		text-decoration-color: ${neutral[86]};
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+	ul {
+		list-style: none;
+		margin: 0 0 ${space[2]}px;
+		padding: 0;
+	}
+	ul li {
+		padding-left: ${space[5]}px;
+	}
+	ul li p {
+		display: inline-block;
+		margin-bottom: 0;
+	}
+	ul li:before {
+		display: inline-block;
+		content: '';
+		border-radius: 0.375rem;
+		height: 10px;
+		width: 10px;
+		margin: 0 ${space[2]}px 0 -${space[5]}px;
+		background-color: ${palette('--bullet-fill')};
+	}
+	strong {
+		font-weight: bold;
+	}
+`;
+
+const bottomBorderStyles = css`
+	border-top: 1px solid ${palette('--article-border')};
+	margin-bottom: ${space[2]}px;
+`;
+
+const headingMarginStyle = css`
+	margin-bottom: ${space[2]}px;
+`;
+
+interface MultiBylineItemProps {
+	multiBylineItem: MultiBylineItemModel;
+	format: ArticleFormat;
+	children: React.ReactNode;
+}
+
+export const MultiBylineItem = ({
+	multiBylineItem,
+	format,
+	children,
+}: MultiBylineItemProps) => {
+	return (
+		<>
+			<li css={multiBylineItemStyles} data-spacefinder-role="nested">
+				<hr css={headingLineStyles} />
+				<h3
+					id={slugify(multiBylineItem.title)}
+					css={[subheadingStyles(format), headingMarginStyle]}
+				>
+					{multiBylineItem.title}
+				</h3>
+				<Bio html={multiBylineItem.bio} />
+				{children}
+			</li>
+		</>
+	);
+};
+
+const Bio = ({ html }: { html?: string }) => {
+	if (!html) return null;
+	const sanitizedHtml = sanitise(html, {});
+	return (
+		<>
+			<div
+				css={bioStyles}
+				dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+			/>
+			<div css={bottomBorderStyles} />
+		</>
+	);
+};
