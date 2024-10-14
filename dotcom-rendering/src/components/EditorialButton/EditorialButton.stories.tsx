487a0a63a0275e466be96b2cc4e9957b99de79a2
Simon Adcock <simonadcock2@gmail.com>
2024-10-14 15:52:03 +0100

    remove format from editorial button stories

--- dotcom-rendering/src/components/EditorialButton/EditorialButton.stories.tsx
+++ dotcom-rendering/src/components/EditorialButton/EditorialButton.stories.tsx
@@ -1,31 +1,18 @@
 import { css } from '@emotion/react';
-import { SvgCross } from '@guardian/source/react-components';
 import type { Meta, StoryFn } from '@storybook/react';
-import { Pillar as ArticlePillar, ArticleSpecial } from '../../lib/format';
+import { lightDecorator } from '../../../.storybook/decorators/themeDecorator';
+import {
+	ArticleDesign,
+	ArticleDisplay,
+	ArticleSpecial,
+	Pillar,
+} from '../../lib/format';
 import { EditorialButton } from './EditorialButton';
 import type { EditorialButtonProps } from './EditorialButton';
 
 const meta: Meta<typeof EditorialButton> = {
 	title: 'Components/EditorialButton',
 	component: EditorialButton,
-	argTypes: {
-		icon: {
-			options: ['undefined', 'cross'],
-			mapping: {
-				undefined,
-				cross: <SvgCross />,
-			},
-			control: { type: 'radio' },
-		},
-	},
-	args: {
-		size: 'default',
-		hideLabel: false,
-		icon: undefined,
-		priority: 'primary',
-		iconSide: 'left',
-		nudgeIcon: false,
-	},
 };
 
 export default meta;
@@ -46,69 +33,54 @@ const Template: StoryFn<typeof EditorialButton> = (
 };
 
 const pillars = [
-	ArticlePillar.News,
-	ArticlePillar.Sport,
-	ArticlePillar.Culture,
-	ArticlePillar.Lifestyle,
-	ArticlePillar.Opinion,
+	Pillar.News,
+	Pillar.Sport,
+	Pillar.Culture,
+	Pillar.Lifestyle,
+	Pillar.Opinion,
 	ArticleSpecial.SpecialReport,
 	ArticleSpecial.Labs,
 ];
 
-const RowTemplate: StoryFn<typeof EditorialButton> = (
-	args: Partial<EditorialButtonProps>,
-) => (
-	<div
-		css={css`
-			display: flex;
-			flex-direction: row;
-			justify-content: space-between;
-			width: 800px;
-		`}
-	>
-		{pillars.map((pillar) => (
-			<Template key={pillar} {...args} />
-		))}
-	</div>
-);
+const allThemeStandardVariations = pillars.map((theme) => ({
+	design: ArticleDesign.Standard,
+	display: ArticleDisplay.Standard,
+	theme,
+}));
 
-export const WhenPrimary: StoryFn<typeof EditorialButton> = RowTemplate.bind(
-	{},
-);
+export const WhenPrimary: StoryFn<typeof EditorialButton> = Template.bind({});
 WhenPrimary.args = {
 	priority: 'primary',
 	size: 'small',
 };
+WhenPrimary.decorators = [lightDecorator(allThemeStandardVariations)];
 
 // *****************************************************************************
 
-export const WhenSecondary: StoryFn<typeof EditorialButton> = RowTemplate.bind(
-	{},
-);
+export const WhenSecondary: StoryFn<typeof EditorialButton> = Template.bind({});
 WhenSecondary.args = {
 	priority: 'secondary',
 	size: 'small',
 };
+WhenSecondary.decorators = [lightDecorator(allThemeStandardVariations)];
 
 // *****************************************************************************
 
-export const WhenTertiary: StoryFn<typeof EditorialButton> = RowTemplate.bind(
-	{},
-);
+export const WhenTertiary: StoryFn<typeof EditorialButton> = Template.bind({});
 WhenTertiary.args = {
 	priority: 'tertiary',
 	size: 'small',
 };
+WhenTertiary.decorators = [lightDecorator(allThemeStandardVariations)];
 
 // *****************************************************************************
 
-export const WhenSubdued: StoryFn<typeof EditorialButton> = RowTemplate.bind(
-	{},
-);
+export const WhenSubdued: StoryFn<typeof EditorialButton> = Template.bind({});
 WhenSubdued.args = {
 	priority: 'subdued',
 	size: 'small',
 };
+WhenSubdued.decorators = [lightDecorator(allThemeStandardVariations)];
 
 // *****************************************************************************
 
@@ -118,8 +90,4 @@ WithOverrides.args = {
 		background-color: pink;
 	`,
 };
-
-// *****************************************************************************
-
-export const WithDefaults: StoryFn<typeof EditorialButton> = Template.bind({});
-WithDefaults.args = {};
+WithOverrides.decorators = [lightDecorator(allThemeStandardVariations)];
