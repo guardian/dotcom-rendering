248a7f06360c7043a72f1df368e2665fd47e750e
Anna Beddow <anna.beddow@gmail.com>
2024-10-14 15:18:23 +0100

    remove comment

--- dotcom-rendering/src/components/CardPicture.tsx
+++ dotcom-rendering/src/components/CardPicture.tsx
@@ -80,7 +80,6 @@ const decideImageWidths = (
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#styling_with_css
  *
- * We also add object-fit: cover to ensure the image fills the container and maintains its own aspect ratio.
  */
 const block = css`
 	display: block;
