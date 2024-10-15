f01053a9fa1894cc1fc2d4ef6a475efe9b157fd8
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-10-15 14:55:30 +0100

    Update `CalloutBlockComponent` stories to Component Story Format v3
    This is the default for Storybook 7+, and integrates better with some of its features.
    
    Using `satisfies` gives better type safety for args. Removed the `splitTheme` decorator, as this is now handled by the "global colour scheme" toolbar item and a Chromatic mode. Added the `centreColumnDecorator`, so the stories better reflect the layout commonly used in articles. Refactored the "good request", "bad request" and "unique id" features into decorators.

--- dotcom-rendering/src/components/CalloutBlockComponent.stories.tsx
+++ dotcom-rendering/src/components/CalloutBlockComponent.stories.tsx
@@ -1,21 +1,23 @@
-import type { StoryObj } from '@storybook/react';
+import type { Decorator, Meta, StoryObj } from '@storybook/react';
 import fetchMock from 'fetch-mock';
-import { splitTheme } from '../../.storybook/decorators/splitThemeDecorator';
+import { centreColumnDecorator } from '../../.storybook/decorators/gridDecorators';
 import { calloutCampaign as calloutCampaignV2 } from '../../fixtures/manual/calloutCampaignV2';
-import {
-	ArticleDesign,
-	ArticleDisplay,
-	type ArticleFormat,
-	Pillar,
-} from '../lib/format';
 import { CalloutBlockComponent } from './CalloutBlockComponent.importable';
 
 const tomorrow = new Date().setDate(new Date().getDate() + 1) / 1000;
 const yesterday = new Date().setDate(new Date().getDate() - 1) / 1000;
-const pageId =
-	'world/2023/mar/01/tell-us-have-you-been-affected-by-the-train-crash-in-greece';
 
-const goodRequest = () => {
+const meta = {
+	component: CalloutBlockComponent,
+	title: 'Components/Callout Block Component',
+	decorators: [centreColumnDecorator],
+} satisfies Meta<typeof CalloutBlockComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const goodRequest: Decorator<Story['args']> = (Story) => {
 	fetchMock
 		.restore()
 		.post(
@@ -26,9 +28,11 @@ const goodRequest = () => {
 			},
 		)
 		.spy('end:.hot-update.json');
+
+	return <Story />;
 };
 
-const badRequest = () => {
+const badRequest: Decorator<Story['args']> = (Story) => {
 	fetchMock
 		.restore()
 		.post(
@@ -39,102 +43,95 @@ const badRequest = () => {
 			},
 		)
 		.spy('end:.hot-update.json');
+
+	return <Story />;
 };
 
 /** ensure that multiple form IDs are not present on the same page */
 let counter = 0;
+const uniqueFormId: Decorator<Story['args']> = (Story, context) => (
+	<Story
+		args={{
+			...context.args,
+			callout: {
+				...context.args.callout,
+				formId: context.args.callout.formId + ++counter,
+			},
+		}}
+	/>
+);
 
-const defaultFormat = {
-	display: ArticleDisplay.Standard,
-	design: ArticleDesign.Standard,
-	theme: Pillar.News,
-} satisfies ArticleFormat;
+export const NonCollapsible = {
+	args: {
+		callout: {
+			...calloutCampaignV2,
+			activeUntil: tomorrow,
+		},
+		pageId: 'world/2023/mar/01/tell-us-have-you-been-affected-by-the-train-crash-in-greece',
+	},
+	decorators: [goodRequest],
+} satisfies Story;
 
-export default {
-	component: CalloutBlockComponent,
-	title: 'Components/CalloutBlockComponent',
-};
+export const Collapsible = {
+	args: {
+		...NonCollapsible.args,
+		callout: {
+			...NonCollapsible.args.callout,
+			isNonCollapsible: false,
+		},
+	},
+	decorators: [goodRequest, uniqueFormId],
+} satisfies Story;
 
-export const Collapsible: StoryObj = () => {
-	goodRequest();
-	return (
-		<CalloutBlockComponent
-			callout={{
-				...calloutCampaignV2,
-				formId: calloutCampaignV2.formId + ++counter,
-				isNonCollapsible: false,
-				activeUntil: tomorrow,
-			}}
-			pageId={pageId}
-		/>
-	);
-};
+export const SubmissionFailure = {
+	args: NonCollapsible.args,
+	decorators: [badRequest],
+	play: async ({ canvasElement }) => {
+		await new Promise((resolve) => {
+			// there’s some weirdness that resets the form
+			setTimeout(resolve, 600);
+		});
 
-Collapsible.decorators = [splitTheme([defaultFormat])];
+		const buttons = [
+			...canvasElement.querySelectorAll('button[type=submit]'),
+		];
 
-export const NonCollapsible: StoryObj = () => {
-	goodRequest();
-	return (
-		<CalloutBlockComponent
-			callout={{ ...calloutCampaignV2, activeUntil: tomorrow }}
-			pageId={pageId}
-		/>
-	);
-};
-NonCollapsible.storyName = 'NonCollapsible';
-NonCollapsible.decorators = [splitTheme([defaultFormat])];
+		for (const button of buttons) {
+			if (!(button instanceof HTMLButtonElement)) return;
+			button.click();
+		}
+	},
+} satisfies Story;
 
-export const SubmissionFailure: StoryObj = () => {
-	badRequest();
-	return (
-		<CalloutBlockComponent
-			callout={{ ...calloutCampaignV2, activeUntil: tomorrow }}
-			pageId={pageId}
-		/>
-	);
-};
-SubmissionFailure.decorators = [splitTheme([defaultFormat])];
-SubmissionFailure.play = async ({ canvasElement }) => {
-	await new Promise((resolve) => {
-		// there’s some weirdness that resets the form
-		setTimeout(resolve, 600);
-	});
-	const buttons = [...canvasElement.querySelectorAll('button[type=submit]')];
-	for (const button of buttons) {
-		if (!(button instanceof HTMLButtonElement)) return;
-		button.click();
-	}
-};
+export const Expired = {
+	args: {
+		...NonCollapsible.args,
+		callout: {
+			...NonCollapsible.args.callout,
+			activeUntil: yesterday,
+		},
+	},
+} satisfies Story;
 
-export const Expired: StoryObj = () => {
-	return (
-		<CalloutBlockComponent
-			callout={{ ...calloutCampaignV2, activeUntil: yesterday }}
-			pageId={pageId}
-		/>
-	);
-};
-Expired.decorators = [splitTheme([defaultFormat])];
-
-export const MinimalCallout: StoryObj = () => {
-	return (
-		<>
-			<div css={{ fontWeight: 'bold', paddingBottom: '16px' }}>
-				Prompt, title and description are all optional
-			</div>
-			<CalloutBlockComponent
-				callout={{
-					...calloutCampaignV2,
-					formId: calloutCampaignV2.formId + ++counter,
-					activeUntil: tomorrow,
-					isNonCollapsible: false,
-					title: '',
-					prompt: '',
-					description: '',
-				}}
-				pageId={pageId}
-			/>
-		</>
-	);
-};
-MinimalCallout.decorators = [splitTheme([defaultFormat])];
+export const MinimalCallout = {
+	args: {
+		...Collapsible.args,
+		callout: {
+			...Collapsible.args.callout,
+			title: '',
+			prompt: '',
+			description: '',
+		},
+	},
+	decorators: [
+		uniqueFormId,
+		(Story) => (
+			<>
+				<div css={{ fontWeight: 'bold', paddingBottom: '16px' }}>
+					Prompt, title and description are all optional
+				</div>
+				<Story />
+			</>
+		),
+	],
+} satisfies Story;
