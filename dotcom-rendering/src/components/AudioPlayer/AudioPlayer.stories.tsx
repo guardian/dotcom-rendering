92e09295ff7abed4d1dcf9c28ee752d98ebfe8ed
Alex Sanders <alex@sndrs.dev>
2024-10-16 14:52:08 +0100

    only type this specific file

--- dotcom-rendering/src/components/AudioPlayer/AudioPlayer.stories.tsx
+++ dotcom-rendering/src/components/AudioPlayer/AudioPlayer.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { defaultFormats } from '../../../.storybook/decorators/splitThemeDecorator';
 import { allModes } from '../../../.storybook/modes';
 import { AudioPlayer as Player } from './AudioPlayer';
-import audioFile from './stories/default_audio_test.mp3';
+import localMp3Path from './stories/default_audio_test.mp3';
 
 const meta = {
 	title: 'Components/Audio Player',
@@ -15,7 +15,7 @@ type Story = StoryObj<typeof meta>;
 
 export const AudioPlayer = {
 	args: {
-		src: audioFile,
+		src: localMp3Path,
 		// src: 'https://audio.guim.co.uk/2020/05/05-61553-gnl.fw.200505.jf.ch7DW.mp3',
 		mediaId: 'mediaId',
 	},
--- dotcom-rendering/src/components/AudioPlayer/stories/mp3.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-default-export -- it's how storybook imports them */
-
-declare module '*.mp3' {
-	const src: string;
-	export default src;
-}
