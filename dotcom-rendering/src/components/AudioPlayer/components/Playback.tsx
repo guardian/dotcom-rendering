e41350e58313f98a1a200a399e68b83f4d4884a6
Alex Sanders <alex@sndrs.dev>
2024-10-24 12:13:55 +0100

    formatting

--- dotcom-rendering/src/components/AudioPlayer/components/Playback.tsx
+++ dotcom-rendering/src/components/AudioPlayer/components/Playback.tsx
@@ -60,7 +60,7 @@ Playback.Play = ({
 } & ButtonProps) => {
 	return (
 		<button
-			aria-label={isPlaying ? "Pause" : "Play"}
+			aria-label={isPlaying ? 'Pause' : 'Play'}
 			type="button"
 			css={[
 				buttonBaseCss,
