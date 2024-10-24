d26be10111a77fcf7b6cbb1765106499ed831b5d
Sue Burt <sue.burt@guardian.co.uk>
2024-10-24 11:50:22 +0100

    Swap order of benefits

--- dotcom-rendering/src/components/marketing/epics/ThreeTierChoiceCardData.tsx
+++ dotcom-rendering/src/components/marketing/epics/ThreeTierChoiceCardData.tsx
@@ -38,9 +38,9 @@ export const ChoiceCardTestData_REGULAR: ChoiceInfo[] = [
 		benefitsLabel: 'All-access digital',
 		benefits: () => [
 			'Unlimited access to the Guardian app',
+			'Unlimited access to our new Feast App',
 			'Ad-free reading on all your devices',
 			'Exclusive newsletter for supporters, sent every week from the Guardian newsroom',
-			'Unlimited access to our new Feast App',
 			'Far fewer asks for support',
 		],
 		recommended: true,
@@ -72,9 +72,9 @@ export const ChoiceCardTestData_US: ChoiceInfo[] = [
 		benefitsLabel: 'All-access digital',
 		benefits: () => [
 			'Unlimited access to the Guardian app',
+			'Unlimited access to our new Feast App',
 			'Ad-free reading on all your devices',
 			'Exclusive newsletter for supporters, sent every week from the Guardian newsroom',
-			'Unlimited access to our new Feast App',
 			'Far fewer asks for support',
 		],
 		recommended: true,
