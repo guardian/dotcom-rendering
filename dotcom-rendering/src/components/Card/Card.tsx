d97b6c433240360ad07c6c5b60aecf2bb23a6385
Anna Beddow <anna.beddow@gmail.com>
2024-10-28 09:24:53 +0000

    Reduce gap size if the image is jumbo

--- dotcom-rendering/src/components/Card/Card.tsx
+++ dotcom-rendering/src/components/Card/Card.tsx
@@ -466,7 +466,8 @@ export const Card = ({
 	const getGapSize = (): GapSize => {
 		if (isOnwardContent) return 'none';
 		if (hasBackgroundColour) return 'tiny';
-		if (isFlexSplash) return 'small';
+		if (isFlexSplash || (isFlexibleContainer && imageSize === 'jumbo'))
+			return 'small';
 		if (isSmallCard) return 'medium';
 		if (
 			isFlexibleContainer &&
