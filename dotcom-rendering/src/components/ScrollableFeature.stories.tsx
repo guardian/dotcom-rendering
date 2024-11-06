41d375dcefff16398d1e7e97eefb2d0c7fcb9b9c
Charlotte <charlotte.emms@theguardian.com>
2024-11-06 17:15:15 +0000

    update storybook stories for new containers

--- dotcom-rendering/src/components/ScrollableFeature.stories.tsx
+++ dotcom-rendering/src/components/ScrollableFeature.stories.tsx
@@ -76,6 +76,7 @@ const meta = {
 			discussionApiUrl={discussionApiUrl}
 			editionId="UK"
 			showTopBorder={false}
+			containerLevel="Secondary"
 		>
 			<ScrollableFeature {...args} />
 		</FrontSection>
@@ -106,18 +107,20 @@ const containerPalettes = [
 
 export const WithSpecialPaletteVariations = {
 	parameters: {
+		/** We only want one breakpoint snapshotted for special palette variations */
 		chromatic: { viewports: [breakpoints.desktop] },
 	},
 	render: (args) => (
 		<>
 			{containerPalettes.map((containerPalette) => (
 				<FrontSection
-					title="Scrollable feature"
+					title={containerPalette}
 					discussionApiUrl={discussionApiUrl}
 					editionId="UK"
 					showTopBorder={false}
 					key={containerPalette}
 					containerPalette={containerPalette}
+					containerLevel="Secondary"
 				>
 					<ScrollableFeature
 						{...args}
--- dotcom-rendering/src/components/StaticMediumFour.stories.tsx
+++ dotcom-rendering/src/components/StaticMediumFour.stories.tsx
@@ -2,6 +2,7 @@ import { breakpoints } from '@guardian/source/foundations';
 import type { Meta, StoryObj } from '@storybook/react';
 import { discussionApiUrl } from '../../fixtures/manual/discussionApiUrl';
 import { trails } from '../../fixtures/manual/trails';
+import type { DCRContainerPalette } from '../types/front';
 import { FrontSection } from './FrontSection';
 import { StaticMediumFour } from './StaticMediumFour';
 
@@ -25,9 +26,11 @@ const meta = {
 	},
 	render: (args) => (
 		<FrontSection
+			title="Static medium four"
 			discussionApiUrl={discussionApiUrl}
-			editionId={'UK'}
+			editionId="UK"
 			showTopBorder={true}
+			containerLevel="Primary"
 		>
 			<StaticMediumFour {...args} />
 		</FrontSection>
@@ -38,17 +41,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const One: Story = {
-	name: 'With one card',
+export const Four = {
+	name: 'With four cards',
 	args: {
-		trails: trails.slice(0, 1),
-	},
-};
-
-export const Two: Story = {
-	name: 'With two cards',
-	args: {
-		trails: trails.slice(0, 2),
+		trails: trails.slice(0, 4),
 	},
 };
 
@@ -59,9 +55,59 @@ export const Three: Story = {
 	},
 };
 
-export const Four: Story = {
-	name: 'With four cards',
+export const Two: Story = {
+	name: 'With two cards',
 	args: {
-		trails: trails.slice(0, 4),
+		trails: trails.slice(0, 2),
 	},
 };
+
+export const One: Story = {
+	name: 'With one card',
+	args: {
+		trails: trails.slice(0, 1),
+	},
+};
+
+const containerPalettes = [
+	'InvestigationPalette',
+	'LongRunningPalette',
+	'SombrePalette',
+	'BreakingPalette',
+	'EventPalette',
+	'EventAltPalette',
+	'LongRunningAltPalette',
+	'SombreAltPalette',
+	'SpecialReportAltPalette',
+	'Branded',
+] as const satisfies readonly Omit<
+	DCRContainerPalette,
+	'MediaPalette' | 'PodcastPalette'
+>[];
+
+export const WithSpecialPaletteVariations = {
+	parameters: {
+		/** We only want one breakpoint snapshotted for special palette variations */
+		chromatic: { viewports: [breakpoints.desktop] },
+	},
+	render: (args) => (
+		<>
+			{containerPalettes.map((containerPalette) => (
+				<FrontSection
+					title={containerPalette}
+					discussionApiUrl={discussionApiUrl}
+					editionId="UK"
+					showTopBorder={false}
+					key={containerPalette}
+					containerPalette={containerPalette}
+					containerLevel="Primary"
+				>
+					<StaticMediumFour
+						{...args}
+						containerPalette={containerPalette}
+					/>
+				</FrontSection>
+			))}
+		</>
+	),
+} satisfies Story;
