48fd6e331e139751ed4799dab5b78c8fbef846cb
Sue Burt <sookburt@users.noreply.github.com>
2024-11-13 15:57:49 +0000

    use headline size from model in SDC for mobile banner (#12835)
    Co-authored-by: Sue Burt <“sue.burt@guardian.co.uk”>

--- dotcom-rendering/src/components/marketing/banners/designableBanner/DesignableBanner.tsx
+++ dotcom-rendering/src/components/marketing/banners/designableBanner/DesignableBanner.tsx
@@ -325,6 +325,7 @@ const DesignableBanner: ReactComponent<BannerRenderProps> = ({
 						heading={content.mainContent.heading}
 						mobileHeading={content.mobileContent.heading}
 						headerSettings={templateSettings.headerSettings}
+						headlineSize={design.fonts?.heading.size ?? 'medium'}
 					/>
 				</div>
 				<div css={styles.contentContainer(showReminder)}>
