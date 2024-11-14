932493fb74e56cbe7b2eadbaa4779f2917f9c690
David Blatcher <dv.bltchr@gmail.com>
2024-11-14 11:43:44 +0000

    add story for crude mock up of thrasher layout

---
+++ dotcom-rendering/src/components/FrontNewsletterSignup.stories.tsx
@@ -0,0 +1,27 @@
+import { FrontNewsletterSignup } from './FrontNewsletterSignup';
+
+export default {
+	component: FrontNewsletterSignup,
+	title: 'Components/FrontNewsletterSignup',
+};
+
+export const inThrasher = () => {
+	return (
+		<FrontNewsletterSignup
+			containerType="fixed/thrasher"
+			newsletter={{
+				identityName: 'morning-mail',
+				name: 'Morning Mail',
+				theme: 'news',
+				description:
+					'Our Australian morning briefing breaks down the key stories of the day, telling you what’s happening and why it matters',
+				frequency: 'Every weekday',
+				listId: 4148,
+				group: 'News in depth',
+				successDescription:
+					"We'll send you Guardian Australia's Morning Mail every weekday",
+				regionFocus: 'AU',
+			}}
+		/>
+	);
+};
--- dotcom-rendering/src/components/FrontNewsletterSignup.tsx
+++ dotcom-rendering/src/components/FrontNewsletterSignup.tsx
@@ -1,5 +1,10 @@
+import { css } from '@emotion/react';
+import { palette, space, until } from '@guardian/source/foundations';
+import { SvgGuardianLogo } from '@guardian/source/react-components';
 import type { Newsletter } from '../types/content';
 import type { DCRContainerType } from '../types/front';
+import { NewsletterBadge } from './NewsletterBadge';
+import { NewsletterPrivacyMessage } from './NewsletterPrivacyMessage';
 import { SecureSignup } from './SecureSignup.importable';
 
 interface Props {
@@ -7,7 +12,87 @@ interface Props {
 	containerType?: DCRContainerType;
 }
 
+const cellStyle = css`
+	padding: ${space[2]}px;
+`;
+
+const mainStyle = css`
+	margin-right: 0;
+	flex: 1;
+	display: flex;
+	justify-content: stretch;
+	align-items: 'center';
+`;
+
+const logoContainerStyle = () => css`
+	max-width: 215px;
+	min-width: 215px;
+	padding-left: 0;
+`;
+
+const rightDivStyle = (leftGoesToBottom?: boolean) => css`
+	max-width: 310px;
+	padding-right: 80px;
+	${until.wide} {
+		padding-right: 0;
+		max-width: 220px;
+	}
+	${until.desktop} {
+		max-width: unset;
+		padding-right: ${space[2]}px;
+		padding-top: 0;
+		padding-bottom: ${!leftGoesToBottom ? `${space[3]}px` : '0'};
+	}
+`;
+
 export const FrontNewsletterSignup = ({ newsletter, containerType }: Props) => {
+	if (containerType === 'fixed/thrasher') {
+		return (
+			<section
+				css={{
+					backgroundColor: palette.brand[400],
+					color: palette.neutral[100],
+				}}
+			>
+				<div
+					css={css`
+						display: flex;
+					`}
+				>
+					<div css={[cellStyle, logoContainerStyle()]}>
+						<SvgGuardianLogo
+							textColor={palette.neutral[100]}
+							width={200}
+						/>
+						<NewsletterBadge />
+					</div>
+					<div css={[cellStyle, mainStyle]}>
+						<div
+							css={{
+								backgroundColor: palette.news[400],
+								border: `2px dashed ${palette.neutral[100]}`,
+								padding: space[3],
+								flex: 1,
+							}}
+						>
+							<p>{newsletter.name}</p>
+							<p>{newsletter.description}</p>
+							<SecureSignup
+								newsletterId={newsletter.identityName}
+								successDescription={
+									newsletter.successDescription
+								}
+							/>
+						</div>
+					</div>
+					<div css={[cellStyle, rightDivStyle(false)]}>
+						<NewsletterPrivacyMessage textColor="supporting" />
+					</div>
+				</div>
+			</section>
+		);
+	}
+
 	return (
 		<div>
 			<p>newsletter: {newsletter.name}</p>
