4993eb912145d89a32b5db1e9f702c8327a4205e
Charley_Campbell <charley.campbell@guardian.co.uk>
2024-11-18 17:45:28 +0000

    IFrame handles which URL to render. The stage which we are using needs work as currently it's hard coded to use local There might be a more elegnat solution to pass in the geo location param

---
+++ dotcom-rendering/src/components/marketing/epics/OneTimeContributionIFrame.tsx
@@ -0,0 +1,56 @@
+import { isProd } from '../lib/stage';
+
+interface OneTimeContributionsIFrameProps {
+	countryCode: string;
+	stage: string;
+}
+
+export function OneTimeContributionIFrame({
+	countryCode,
+	stage,
+}: OneTimeContributionsIFrameProps): JSX.Element {
+	const countryCodeParam = getCountryCode(countryCode);
+	// Use the helper function to construct the iframe source URL
+	const iframeSrc = getIframeUrl(isProd(stage), countryCodeParam);
+
+	return (
+		<div style={{ height: '80vh' }}>
+			<iframe
+				src={iframeSrc}
+				width="100%"
+				height="100%"
+				title="One-off contributions embed"
+			/>
+		</div>
+	);
+}
+
+// Helper function to construct iframe URL
+function getIframeUrl(stage: boolean, countryCode: string | undefined): string {
+	const baseUrl = getBaseUrl(stage);
+	return `${baseUrl}/${countryCode}/one-time-checkout-embed`;
+}
+
+// Helper function to determine the base URL
+function getBaseUrl(stage: boolean): string {
+	/*if (stage === 'PROD') {
+		return 'https://support.theguardian.com';
+	} else if (stage === 'CODE') {
+		return 'https://support.code.dev-theguardian.com';
+	}*/
+	return 'https://support.thegulocal.com';
+}
+
+function getCountryCode(countryCode: string): string | undefined {
+	if (countryCode === 'GB') {
+		return 'uk';
+	}
+	if (countryCode === 'US') {
+		return 'us';
+	}
+	if (countryCode === 'AU') {
+		return 'au';
+	} else {
+		return 'int';
+	}
+}
