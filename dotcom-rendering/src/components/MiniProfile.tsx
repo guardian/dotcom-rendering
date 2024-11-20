a580b7808cc2fc6cddc968d5aa4bca91f34e8659
Simon Byford <simon.byford@guardian.co.uk>
2024-11-20 14:31:21 +0000

    Break out `Bio` into its own component

--- dotcom-rendering/src/components/MiniProfile.tsx
+++ dotcom-rendering/src/components/MiniProfile.tsx
@@ -1,10 +1,9 @@
 import { css } from '@emotion/react';
-import { neutral, space, textSans14 } from '@guardian/source/foundations';
-import sanitise from 'sanitize-html';
+import { space } from '@guardian/source/foundations';
 import type { ArticleFormat } from '../lib/articleFormat';
 import { slugify } from '../model/enhance-H2s';
-import { palette } from '../palette';
 import type { MiniProfile as MiniProfileModel } from '../types/content';
+import { Bio } from './Bio';
 import { EndNote } from './EndNote';
 import { Heading } from './Heading';
 import { headingLineStyles } from './KeyTakeaway';
@@ -12,59 +11,6 @@ import { subheadingStyles } from './Subheading';
 
 const miniProfileStyles = css`
 	padding-top: 8px;
-`;
-
-/** Nesting is necessary in the bio styles because we receive a string of html from the
- * field. This can contain the following tags:
- * Blocks: p, ul, li
- * Inline: strong, em, a
- */
-const bioStyles = css`
-	${textSans14};
-	padding: ${space[1]}px 0;
-	color: ${palette('--mini-profiles-text-subdued')};
-	p {
-		margin-bottom: ${space[2]}px;
-	}
-	a {
-		color: ${palette('--link-kicker-text')};
-		text-underline-offset: 3px;
-	}
-	a:not(:hover) {
-		text-decoration-color: ${neutral[86]};
-	}
-	a:hover {
-		text-decoration: underline;
-	}
-	ul {
-		list-style: none;
-		margin: 0 0 ${space[2]}px;
-		padding: 0;
-	}
-	ul li {
-		padding-left: ${space[5]}px;
-	}
-	ul li p {
-		display: inline-block;
-		margin-bottom: 0;
-	}
-	ul li:before {
-		display: inline-block;
-		content: '';
-		border-radius: 0.375rem;
-		height: 10px;
-		width: 10px;
-		margin: 0 ${space[2]}px 0 -${space[5]}px;
-		background-color: ${palette('--bullet-fill')};
-	}
-	strong {
-		font-weight: bold;
-	}
-`;
-
-const bottomBorderStyles = css`
-	border-top: 1px solid ${palette('--article-border')};
-	margin-bottom: ${space[2]}px;
 `;
 
 const headingMarginStyle = css`
@@ -107,28 +53,6 @@ export const MiniProfile = ({
 					<EndNote text={miniProfile.endNote} />
 				) : null}
 			</li>
-		</>
-	);
-};
-
-const containsText = (html: string) => {
-	const htmlWithoutTags = sanitise(html, {
-		allowedTags: [],
-		allowedAttributes: {},
-	});
-	return htmlWithoutTags.length > 0;
-};
-
-const Bio = ({ html }: { html?: string }) => {
-	if (!html || !containsText(html)) return null;
-	const sanitizedHtml = sanitise(html, {});
-	return (
-		<>
-			<div
-				css={bioStyles}
-				dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
-			/>
-			<div css={bottomBorderStyles} />
 		</>
 	);
 };
