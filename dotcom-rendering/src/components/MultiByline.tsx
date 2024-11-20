e2c18e9aa19cb21f47d9a40601aa633281869fa1
Simon Byford <simon.byford@guardian.co.uk>
2024-11-20 14:31:21 +0000

    Break out `Bio` into its own component

--- dotcom-rendering/src/components/MultiByline.tsx
+++ dotcom-rendering/src/components/MultiByline.tsx
@@ -8,7 +8,6 @@ import {
 	headlineMediumItalic28,
 	neutral,
 	space,
-	textSans14,
 	textSansItalic24,
 	textSansItalic28,
 	textSansItalic34,
@@ -24,6 +23,7 @@ import { palette } from '../palette';
 import type { MultiByline as MultiBylineModel } from '../types/content';
 import type { TagType } from '../types/tag';
 import { Avatar } from './Avatar';
+import { Bio } from './Bio';
 import { EndNote } from './EndNote';
 import { subheadingStyles } from './Subheading';
 
@@ -36,59 +36,6 @@ const headingLineStyles = css`
 	margin: 8px 0 2px 0;
 	border: none;
 	border-top: 4px solid ${palette('--heading-line')};
-`;
-
-/** Nesting is necessary in the bio styles because we receive a string of html from the
- * field. This can contain the following tags:
- * Blocks: p, ul, li
- * Inline: strong, em, a
- */
-const bioStyles = css`
-	${textSans14};
-	padding: ${space[1]}px 0;
-	color: ${palette('--mini-profiles-text-subdued')};
-	p {
-		margin-bottom: ${space[2]}px;
-	}
-	a {
-		color: ${palette('--link-kicker-text')};
-		text-underline-offset: 3px;
-	}
-	a:not(:hover) {
-		text-decoration-color: ${neutral[86]};
-	}
-	a:hover {
-		text-decoration: underline;
-	}
-	ul {
-		list-style: none;
-		margin: 0 0 ${space[2]}px;
-		padding: 0;
-	}
-	ul li {
-		padding-left: ${space[5]}px;
-	}
-	ul li p {
-		display: inline-block;
-		margin-bottom: 0;
-	}
-	ul li:before {
-		display: inline-block;
-		content: '';
-		border-radius: 0.375rem;
-		height: 10px;
-		width: 10px;
-		margin: 0 ${space[2]}px 0 -${space[5]}px;
-		background-color: ${palette('--bullet-fill')};
-	}
-	strong {
-		font-weight: bold;
-	}
-`;
-
-const bottomBorderStyles = css`
-	border-top: 1px solid ${palette('--article-border')};
-	margin-bottom: ${space[2]}px;
 `;
 
 const headingMarginStyle = css`
@@ -343,19 +290,5 @@ const Byline = ({
 				</div>
 			)}
 		</div>
-	);
-};
-
-const Bio = ({ html }: { html?: string }) => {
-	if (!html) return null;
-	const sanitizedHtml = sanitise(html, {});
-	return (
-		<>
-			<div
-				css={bioStyles}
-				dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
-			/>
-			<div css={bottomBorderStyles} />
-		</>
 	);
 };
