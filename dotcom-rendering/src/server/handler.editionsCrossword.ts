d4fa808f2094f21678f6955e552534997243b4ee
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2024-11-20 12:40:45 +0000

    Update handler.editionsCrossword.ts

--- dotcom-rendering/src/server/handler.editionsCrossword.ts
+++ dotcom-rendering/src/server/handler.editionsCrossword.ts
@@ -3,6 +3,6 @@ import { validateAsEditionsCrosswordType } from '../model/validate';
 
 export const handleEditionsCrossword: RequestHandler = ({ body }, res) => {
 	const editionsCrosswords = validateAsEditionsCrosswordType(body);
-
+	console.log(editionsCrosswords);
 	res.sendStatus(200);
 };
