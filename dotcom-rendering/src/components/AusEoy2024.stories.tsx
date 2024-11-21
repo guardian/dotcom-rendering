2bce61e9fbbeb8bc17bfb70e60fe772968e25f28
Charley_Campbell <charley.campbell@guardian.co.uk>
2024-11-21 16:29:17 +0000

    lint run

--- dotcom-rendering/src/components/AusEoy2024.stories.tsx
+++ dotcom-rendering/src/components/AusEoy2024.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { AuEoy2024 } from './AusEoy2024Wrapper.importable';
 
 const meta = {
