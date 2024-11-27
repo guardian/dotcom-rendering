29baad0dcd9c7a03fd325458e56fe290189174f8
David Blatcher <dv.bltchr@gmail.com>
2024-11-27 11:36:29 +0000

    use FrontSection

--- dotcom-rendering/src/components/ThrasherSlotNewsletterSignup.stories.tsx
+++ dotcom-rendering/src/components/ThrasherSlotNewsletterSignup.stories.tsx
@@ -5,24 +5,46 @@ export default {
 	title: 'Components/ThrasherSlotNewsletterSignup',
 };
 
+const newsletter = {
+	identityName: 'morning-mail',
+	name: 'Morning Mail',
+	theme: 'news',
+	description:
+		'Our Australian morning briefing breaks down the key stories of the day, telling you what’s happening and why it matters',
+	frequency: 'Every weekday',
+	listId: 4148,
+	group: 'News in depth',
+	successDescription:
+		"We'll send you Guardian Australia's Morning Mail every weekday",
+	regionFocus: 'AU',
+	illustrationCard:
+		'https://uploads.guim.co.uk/2024/11/12/5_3_fight_back.jpg',
+};
+
 export const standard = () => {
+	return <ThrasherSlotNewsletterSignup newsletter={newsletter} />;
+};
+export const withInvestigationPalette = () => {
 	return (
 		<ThrasherSlotNewsletterSignup
-			newsletter={{
-				identityName: 'morning-mail',
-				name: 'Morning Mail',
-				theme: 'news',
-				description:
-					'Our Australian morning briefing breaks down the key stories of the day, telling you what’s happening and why it matters',
-				frequency: 'Every weekday',
-				listId: 4148,
-				group: 'News in depth',
-				successDescription:
-					"We'll send you Guardian Australia's Morning Mail every weekday",
-				regionFocus: 'AU',
-				illustrationCard:
-					'https://uploads.guim.co.uk/2024/11/12/5_3_fight_back.jpg',
-			}}
+			containerPalette={'InvestigationPalette'}
+			newsletter={newsletter}
+		/>
+	);
+};
+export const withSombrePalette = () => {
+	return (
+		<ThrasherSlotNewsletterSignup
+			containerPalette={'SombrePalette'}
+			newsletter={newsletter}
+		/>
+	);
+};
+export const withBreakingPalette = () => {
+	return (
+		<ThrasherSlotNewsletterSignup
+			containerPalette={'BreakingPalette'}
+			newsletter={newsletter}
 		/>
 	);
 };
--- dotcom-rendering/src/components/ThrasherSlotNewsletterSignup.tsx
+++ dotcom-rendering/src/components/ThrasherSlotNewsletterSignup.tsx
@@ -1,6 +1,5 @@
 import { css } from '@emotion/react';
 import {
-	headlineBold24,
 	palette,
 	space,
 	textSans14,
@@ -8,113 +7,82 @@ import {
 } from '@guardian/source/foundations';
 import { SvgGuardianLogo } from '@guardian/source/react-components';
 import type { Newsletter } from '../types/content';
+import type { DCRContainerPalette } from '../types/front';
+import { ContainerOverrides } from './ContainerOverrides';
+import { FrontSection } from './FrontSection';
 import { NewsletterBadge } from './NewsletterBadge';
 import { NewsletterPrivacyMessage } from './NewsletterPrivacyMessage';
 import { SecureSignup } from './SecureSignup.importable';
 
 interface Props {
 	newsletter: Newsletter;
+	containerPalette?: DCRContainerPalette;
 }
 
-const cellStyle = css`
-	padding: ${space[2]}px;
-`;
-
-const mainStyle = css`
-	margin-right: 0;
-	flex: 1;
-	display: flex;
-	justify-content: stretch;
-	align-items: 'center';
-`;
-
 const logoContainerStyle = () => css`
-	max-width: 215px;
-	min-width: 215px;
-	padding-left: 0;
+	max-width: 115px;
+	min-width: 115px;
 `;
 
-const rightDivStyle = (leftGoesToBottom?: boolean) => css`
-	max-width: 310px;
-	padding-right: 80px;
-	${until.wide} {
-		padding-right: 0;
-		max-width: 220px;
-	}
-	${until.desktop} {
-		max-width: unset;
-		padding-right: ${space[2]}px;
-		padding-top: 0;
-		padding-bottom: ${!leftGoesToBottom ? `${space[3]}px` : '0'};
-	}
-`;
-
-export const ThrasherSlotNewsletterSignup = ({ newsletter }: Props) => {
+export const ThrasherSlotNewsletterSignup = ({
+	newsletter,
+	containerPalette,
+}: Props) => {
 	return (
-		<section
-			css={{
-				backgroundColor: palette.brand[400],
-				color: palette.neutral[100],
-				padding: space[2],
-			}}
-		>
-			<div
-				css={css`
-					display: flex;
-				`}
-			>
-				<div css={[cellStyle, logoContainerStyle()]}>
-					<SvgGuardianLogo
-						textColor={palette.neutral[100]}
-						width={100}
-					/>
-					<NewsletterBadge />
-				</div>
-				<div css={[cellStyle, mainStyle]}>
-					<div
-						css={{
-							backgroundColor: palette.news[400],
-							border: `2px dashed ${palette.neutral[100]}`,
-							padding: space[3],
-							flex: 1,
-							display: 'flex',
-							gap: space[3],
-						}}
-					>
-						<img
-							src={newsletter.illustrationCard}
-							alt=""
-							width={250}
-							height={150}
+		<ContainerOverrides containerPalette={containerPalette}>
+			<FrontSection
+				containerPalette={containerPalette}
+				editionId="UK"
+				discussionApiUrl="/"
+				title={newsletter.name}
+				leftContent={
+					<div css={[logoContainerStyle()]}>
+						<SvgGuardianLogo
+							textColor={palette.neutral[0]}
+							width={100}
 						/>
-						<div>
-							<p
-								css={css(`
-									${headlineBold24}
-								`)}
-							>
-								{newsletter.name}
-							</p>
-							<p
-								css={css(`
+						<NewsletterBadge />
+					</div>
+				}
+			>
+				<div
+					css={css`
+						display: flex;
+						gap: ${space[2]}px;
+						${until.desktop} {
+							flex-direction: column;
+						}
+					`}
+				>
+					<div
+						css={css`
+							background-color: ${palette.news[400]};
+							border: 2px dashed ${palette.neutral[100]};
+							padding: ${space[1]}px;
+							flex: 3;
+						`}
+					>
+						<p
+							css={css(`
 									${textSans14}
 								`)}
-							>
-								{newsletter.description}
-							</p>
-							<SecureSignup
-								newsletterId={newsletter.identityName}
-								successDescription={
-									newsletter.successDescription
-								}
-							/>
-						</div>
+						>
+							{newsletter.description}
+						</p>
+						<SecureSignup
+							newsletterId={newsletter.identityName}
+							successDescription={newsletter.successDescription}
+						/>
+					</div>
+					<div
+						css={css`
+							flex: 1;
+						`}
+					>
+						<NewsletterPrivacyMessage textColor="supporting" />
 					</div>
 				</div>
-				<div css={[cellStyle, rightDivStyle(false)]}>
-					<NewsletterPrivacyMessage textColor="supporting" />
-				</div>
-			</div>
-		</section>
+			</FrontSection>
+		</ContainerOverrides>
 	);
 };
