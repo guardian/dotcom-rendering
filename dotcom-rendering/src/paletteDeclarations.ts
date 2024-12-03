39143522499aec9c53424572b24c7e38338ff54c
Simon Byford <simon.byford@guardian.co.uk>
2024-12-03 17:04:32 +0000

    Add new colour `--bio-link-underline`

--- dotcom-rendering/src/paletteDeclarations.ts
+++ dotcom-rendering/src/paletteDeclarations.ts
@@ -5418,6 +5418,8 @@ const lastUpdatedTextDark: PaletteFunction = ({ theme, design }) => {
 	}
 };
 
+const bioLinkUnderline: PaletteFunction = () => sourcePalette.neutral[86];
+
 const bioTextSubduedLight: PaletteFunction = () => sourcePalette.neutral[46];
 const bioTextSubduedDark: PaletteFunction = () => sourcePalette.neutral[86];
 
@@ -5933,6 +5935,10 @@ const paletteColours = {
 	'--avatar-background': {
 		light: avatarLight,
 		dark: avatarDark,
+	},
+	'--bio-link-underline': {
+		light: bioLinkUnderline,
+		dark: bioLinkUnderline,
 	},
 	'--bio-text-subdued': {
 		light: bioTextSubduedLight,
