46c29be389213ba69f29f2f0ba5b2039d1ce0b7e
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2024-12-09 13:45:53 +0000

    Change file name

--- dotcom-rendering/src/server/handler.editionsCrossword.ts
+++ dotcom-rendering/src/server/handler.editionsCrossword.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from 'express';
 import { validateAsEditionsCrosswordType } from '../model/validate';
 import { makePrefetchHeader } from './lib/header';
-import { renderCrosswordHtml } from './render.editionsCrossword.web';
+import { renderCrosswordHtml } from './render.editionsCrossword';
 
 export const handleEditionsCrossword: RequestHandler = ({ body }, res) => {
 	const editionsCrosswords = validateAsEditionsCrosswordType(body);
