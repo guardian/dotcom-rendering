eaaaaf860c93991b3412ab00f196bcc9541bdd7c
Alex Sanders <alex@sndrs.dev>
2024-12-12 10:26:04 +0000

    put comments in the right place
    and use JSdoc for editor assistance

--- dotcom-rendering/src/types/frontend.ts
+++ dotcom-rendering/src/types/frontend.ts
@@ -162,10 +162,15 @@ export interface FEBlocksRequest {
 	keywordIds: string;
 }
 
-// Themes are used for styling
-// RealPillars have pillar palette colours and have a `Pillar` type in Scala
-// FakePillars allow us to make modifications to style based on rules outside of the pillar of an article and have a `Special` type in Scala
-// https://github.com/guardian/content-api-scala-client/blob/master/client/src/main/scala/com.gu.contentapi.client/utils/format/Theme.scala
+/**
+ * Themes are used for styling.
+ *
+ * RealPillars have pillar palette colours and have a `Pillar` type in Scala.
+ *
+ * FakePillars allow us to make modifications to style based on rules outside of the pillar of an article and have a `Special` type in Scala.
+ *
+ * https://github.com/guardian/content-api-scala-client/blob/master/client/src/main/scala/com.gu.contentapi.client/utils/format/Theme.scala
+ */
 type ThemePillar =
 	| 'NewsPillar'
 	| 'OpinionPillar'
@@ -176,9 +181,10 @@ type ThemePillar =
 type ThemeSpecial = 'SpecialReportTheme' | 'Labs' | 'SpecialReportAltTheme';
 export type FETheme = ThemePillar | ThemeSpecial;
 
-// FEDesign is what frontend gives (originating in the capi scala client) us on the Format field
-// https://github.com/guardian/content-api-scala-client/blob/master/client/src/main/scala/com.gu.contentapi.client/utils/format/Design.scala
-
+/**
+ * FEDesign is what frontend gives (originating in the capi scala client) us on the Format field
+ * https://github.com/guardian/content-api-scala-client/blob/master/client/src/main/scala/com.gu.contentapi.client/utils/format/Design.scala
+ */
 export type FEDesign =
 	| 'ArticleDesign'
 	| 'PictureDesign'
@@ -205,29 +211,31 @@ export type FEDesign =
 	| 'NewsletterSignupDesign'
 	| 'TimelineDesign'
 	| 'ProfileDesign'
-	| 'CrosswordDesign'; // FEDisplay is the display information passed through from frontend (originating in the capi scala client) and dictates the displaystyle of the content e.g. Immersive
-// https://github.com/guardian/content-api-scala-client/blob/master/client/src/main/scala/com.gu.contentapi.client/utils/format/Display.scala
+	| 'CrosswordDesign';
 
+/** FEDisplay is the display information passed through from frontend (originating in the capi scala client) and dictates the display style of the content e.g. Immersive
+https://github.com/guardian/content-api-scala-client/blob/master/client/src/main/scala/com.gu.contentapi.client/utils/format/Display.scala */
 export type FEDisplay =
 	| 'StandardDisplay'
 	| 'ImmersiveDisplay'
 	| 'ShowcaseDisplay'
 	| 'NumberedListDisplay';
-// FEFormat is the stringified version of Format passed through from Frontend.
-// It gets converted to the @guardian/libs format on platform
 
+/**
+ * FEFormat is the stringified version of Format passed through from Frontend.
+ * It gets converted to the `@guardian/libs` format on platform
+ */
 export type FEFormat = {
 	design: FEDesign;
 	theme: FETheme;
 	display: FEDisplay;
 };
-// Data types for the API request bodies from clients that require
-// transformation before internal use.
-// Where data types are coming from Frontend we try to use the 'FE' prefix.
-//
-// Prior to this we used 'CAPI' as a prefix which wasn't entirely accurate,
-// and some data structures never received the prefix, meaning some are still missing it.
 
+/**
+ * Data types for the API request bodies from clients that require transformation before internal use.
+ * Where data types are coming from Frontend we try to use the 'FE' prefix.
+ * Prior to this we used 'CAPI' as a prefix which wasn't entirely accurate, and some data structures never received the prefix, meaning some are still missing it.
+ */
 export interface FELinkType {
 	url: string;
 	title: string;
@@ -254,8 +262,9 @@ export interface FENavType {
 }
 
 // Pillars are used for styling
-// RealPillars have pillar palette colours
-// FakePillars allow us to make modifications to style based on rules outside of the pillar of an article
+
+/** `RealPillars` have pillar palette colours */
 type RealPillars = 'news' | 'opinion' | 'sport' | 'culture' | 'lifestyle';
+/** `FakePillars` allow us to make modifications to style based on rules outside of the pillar of an article */
 type FakePillars = 'labs';
 export type LegacyPillar = RealPillars | FakePillars;
