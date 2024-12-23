f713c1bf33a0fe09e8ee5d651f4f18e40f806181
Dominik Lander <dominik.lander@guardian.co.uk>
2024-12-23 11:48:55 +0000

    Convert DocumentBlockComponent to CSF v3

--- dotcom-rendering/src/components/DocumentBlockComponent.stories.tsx
+++ dotcom-rendering/src/components/DocumentBlockComponent.stories.tsx
@@ -1,45 +1,47 @@
 import { css } from '@emotion/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { DocumentBlockComponent } from './DocumentBlockComponent.importable';
 
-export default {
+const meta = {
 	component: DocumentBlockComponent,
-	title: 'Components/DocumentBlockComponent',
+	title: 'Components/Document Block Component',
+	decorators: [
+		(Story) => (
+			<div
+				css={css`
+					max-width: 620px;
+					padding: 20px;
+				`}
+			>
+				<Story />
+			</div>
+		),
+	],
+} satisfies Meta<typeof DocumentBlockComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const ScribdDocument: Story = {
+	args: {
+		embedUrl: 'https://www.scribd.com/embeds/431975393/content',
+		height: 613,
+		width: 460,
+		title: '',
+		isTracking: false,
+		isMainMedia: false,
+	},
 };
 
-const Wrapper = ({ children }: { children: React.ReactNode }) => (
-	<div
-		css={css`
-			max-width: 620px;
-			padding: 20px;
-		`}
-	>
-		{children}
-	</div>
-);
-
-export const documentEmbed = () => {
-	return (
-		<Wrapper>
-			<p>Scribd Document</p>
-			<DocumentBlockComponent
-				embedUrl="https://www.scribd.com/embeds/431975393/content"
-				height={613}
-				width={460}
-				title=""
-				isTracking={false}
-				isMainMedia={false}
-			/>
-			<p>DocumentCloud Document</p>
-			<DocumentBlockComponent
-				embedUrl="https://embed.documentcloud.org/documents/20417938-test-pdf"
-				height={700}
-				width={990}
-				title="TEST PDF (Hosted by DocumentCloud)"
-				source="DocumentCloud"
-				isTracking={false}
-				isMainMedia={false}
-			/>
-		</Wrapper>
-	);
+export const DocumentCloudDocument: Story = {
+	args: {
+		embedUrl: 'https://embed.documentcloud.org/documents/20417938-test-pdf',
+		height: 700,
+		width: 990,
+		title: 'TEST PDF (Hosted by DocumentCloud)',
+		source: 'DocumentCloud',
+		isTracking: false,
+		isMainMedia: false,
+	},
 };
-documentEmbed.storyName = 'document embed';
