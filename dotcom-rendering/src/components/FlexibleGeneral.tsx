1c9dec956346d634c2f76f593f00f82210945a6e
Georges Lebreton <102960844+Georges-GNM@users.noreply.github.com>
2025-01-14 15:05:23 +0000

    bump end value of standard cards slice in flex gen

--- dotcom-rendering/src/components/FlexibleGeneral.tsx
+++ dotcom-rendering/src/components/FlexibleGeneral.tsx
@@ -445,7 +445,7 @@ export const FlexibleGeneral = ({
 	aspectRatio,
 }: Props) => {
 	const splash = [...groupedTrails.splash].slice(0, 1);
-	const cards = [...groupedTrails.standard].slice(0, 8);
+	const cards = [...groupedTrails.standard].slice(0, 19);
 	const groupedCards = decideCardPositions(cards);
 
 	return (
