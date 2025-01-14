218d57777d82d477c94fb85cb6efc9a6846a60aa
Charlotte <charlotte.emms@theguardian.com>
2025-01-14 18:01:51 +0000

    move merch high to above the most viewed container

--- dotcom-rendering/src/lib/getFrontsAdPositions.ts
+++ dotcom-rendering/src/lib/getFrontsAdPositions.ts
@@ -17,8 +17,11 @@ type GroupedCounts = {
 
 type AdCandidate = Pick<DCRCollectionType, 'collectionType'>;
 
-const getMerchHighPosition = (collectionCount: number): number =>
-	collectionCount >= 4 ? 2 : 0;
+/** The Merch high slot is directly before the most viewed container  */
+const getMerchHighPosition = (collections: AdCandidate[]): number => {
+	const mostViewedPosition = collections.findIndex(isMostViewedContainer);
+	return mostViewedPosition - 1;
+};
 
 /**
  * This happens on the recipes front, where the first container is a thrasher
@@ -76,7 +79,7 @@ const isEvenIndex = (_collection: unknown, index: number): boolean =>
  * up to a maximum of `MAX_FRONTS_MOBILE_ADS`
  */
 const getMobileAdPositions = (collections: AdCandidate[]): number[] => {
-	const merchHighPosition = getMerchHighPosition(collections.length);
+	const merchHighPosition = getMerchHighPosition(collections);
 
 	return collections
 		.filter(canInsertMobileAd(merchHighPosition))
