faf38b03e07ee3ec82472ac16648a39b5be87642
Charlotte <charlotte.emms@theguardian.com>
2025-01-15 10:17:43 +0000

    fix tests and annotate with rules

--- dotcom-rendering/src/lib/getFrontsAdPositions.test.ts
+++ dotcom-rendering/src/lib/getFrontsAdPositions.test.ts
@@ -35,183 +35,183 @@ describe('Mobile Ads', () => {
 		expect(mobileAdPositions).not.toContain(3);
 	});
 
-	it('Should not insert ad after a thrasher container', () => {
+	it('Should not insert ad before a thrasher container', () => {
 		const testCollections = [...defaultTestCollections];
 		testCollections.splice(6, 0, { collectionType: 'fixed/thrasher' });
 		testCollections.splice(9, 0, { collectionType: 'fixed/thrasher' });
 
 		const mobileAdPositions = getMobileAdPositions(testCollections);
 
-		expect(mobileAdPositions).not.toContain(7);
-		expect(mobileAdPositions).not.toContain(10);
+		expect(mobileAdPositions).not.toContain(5);
+		expect(mobileAdPositions).not.toContain(8);
 	});
 
 	// We used https://www.theguardian.com/uk/commentisfree as a blueprint
 	it('Non-network front, with more than 4 collections, without thrashers', () => {
 		const testCollections: Pick<DCRCollectionType, 'collectionType'>[] = [
-			{ collectionType: 'fixed/large/slow-XIV' },
+			{ collectionType: 'fixed/large/slow-XIV' }, // Ad position (0)
 			{ collectionType: 'fixed/medium/slow-VI' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (2)
 			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (4)
 			{ collectionType: 'dynamic/slow-mpu' },
-			{ collectionType: 'fixed/small/slow-I' },
+			{ collectionType: 'fixed/small/slow-I' }, // Ad position (6)
 			{ collectionType: 'fixed/medium/slow-VI' },
-			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (8)
 			{ collectionType: 'fixed/small/slow-III' },
-			{ collectionType: 'fixed/medium/fast-XII' },
-			{ collectionType: 'fixed/small/fast-VIII' },
-			{ collectionType: 'news/most-popular' },
+			{ collectionType: 'fixed/medium/fast-XII' }, // Ad position (10)
+			{ collectionType: 'fixed/small/fast-VIII' }, // Ignored - is merch high position
+			{ collectionType: 'news/most-popular' }, // Ignored - is most viewed container
 		];
 
 		const mobileAdPositions = getMobileAdPositions(testCollections);
 
-		expect(mobileAdPositions).toEqual([0, 3, 5, 7, 9, 11]);
+		expect(mobileAdPositions).toEqual([0, 2, 4, 6, 8, 10]);
 	});
 
 	// We used https://www.theguardian.com/uk as a blueprint
 	it('UK Network Front, with more than 4 collections, with thrashers at various places', () => {
 		const testCollections: Pick<DCRCollectionType, 'collectionType'>[] = [
-			{ collectionType: 'dynamic/fast' },
+			{ collectionType: 'dynamic/fast' }, // Ad position (0)
 			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'dynamic/slow' }, // Ad position (2)
 			{ collectionType: 'dynamic/slow' },
-			{ collectionType: 'dynamic/slow' },
-			{ collectionType: 'fixed/small/slow-V-mpu' },
-			{ collectionType: 'dynamic/slow' },
+			{ collectionType: 'fixed/small/slow-V-mpu' }, // Ad position (4)
+			{ collectionType: 'dynamic/slow' }, // Ignored - before thrasher
 			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (8)
 			{ collectionType: 'dynamic/fast' },
-			{ collectionType: 'dynamic/fast' },
-			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'dynamic/fast' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (11)
 			{ collectionType: 'dynamic/slow-mpu' },
-			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (14)
+			{ collectionType: 'dynamic/slow-mpu' }, // Ignored - before thrasher
 			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'fixed/video' }, // Ad position (17)
+			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (19)
 			{ collectionType: 'dynamic/slow-mpu' },
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/video' },
-			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'dynamic/slow-mpu' },
-			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'fixed/medium/slow-VI' },
-			{ collectionType: 'news/most-popular' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (21)
+			{ collectionType: 'fixed/medium/slow-VI' }, // Ignored - is merch high position
+			{ collectionType: 'news/most-popular' }, // Ignored - is most viewed container
 		];
 
 		const mobileAdPositions = getMobileAdPositions(testCollections);
 
-		expect(mobileAdPositions).toEqual([0, 3, 6, 9, 12, 16, 18, 20, 22]);
+		expect(mobileAdPositions).toEqual([0, 2, 4, 8, 11, 14, 17, 19, 21]);
 	});
 
 	// We used https://www.theguardian.com/international as a blueprint
 	it('International Network Front, with more than 4 collections, with thrashers at various places', () => {
 		const testCollections: Pick<DCRCollectionType, 'collectionType'>[] = [
-			{ collectionType: 'dynamic/fast' },
+			{ collectionType: 'dynamic/fast' }, // Ad position (0)
 			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'dynamic/slow' },
+			{ collectionType: 'dynamic/slow' }, // Ad position (2)
 			{ collectionType: 'dynamic/slow-mpu' },
-			{ collectionType: 'dynamic/slow' },
-			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'dynamic/slow' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (5)
 			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'dynamic/fast' },
-			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'dynamic/fast' }, // Ad position (7)
+			{ collectionType: 'fixed/small/slow-IV' }, // Ignored - before thrasher
 			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'dynamic/slow-mpu' },
+			{ collectionType: 'dynamic/slow-mpu' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (11)
+			{ collectionType: 'dynamic/slow-mpu' }, // Ignored - before thrasher
 			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'dynamic/slow-mpu' },
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'dynamic/slow-mpu' },
+			{ collectionType: 'dynamic/slow-mpu' }, // Ad position (14)
 			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (16)
 			{ collectionType: 'fixed/video' },
-			{ collectionType: 'fixed/medium/slow-VI' },
-			{ collectionType: 'news/most-popular' },
+			{ collectionType: 'fixed/medium/slow-VI' }, // Ignored - is merch high position
+			{ collectionType: 'news/most-popular' }, // Ignored - is most viewed container
 		];
 
 		const mobileAdPositions = getMobileAdPositions(testCollections);
 
-		expect(mobileAdPositions).toEqual([0, 3, 6, 9, 13, 15, 17]);
+		expect(mobileAdPositions).toEqual([0, 2, 5, 7, 11, 14, 16]);
 	});
 
 	// We used https://www.theguardian.com/us as a blueprint
 	it('US Network Front, with more than 4 collections, with thrashers at various places', () => {
 		const testCollections: Pick<DCRCollectionType, 'collectionType'>[] = [
-			{ collectionType: 'dynamic/fast' },
+			{ collectionType: 'dynamic/fast' }, // Ad position (0)
 			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (2)
+			{ collectionType: 'dynamic/slow-mpu' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'dynamic/slow' }, // Ad position (5)
+			{ collectionType: 'dynamic/slow' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'fixed/small/slow-III' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (9)
 			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'dynamic/slow-mpu' },
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'dynamic/slow' },
-			{ collectionType: 'dynamic/slow' },
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/small/slow-III' },
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'dynamic/fast' },
-			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'dynamic/fast' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (12)
 			{ collectionType: 'fixed/medium/slow-VI' },
+			{ collectionType: 'dynamic/fast' }, // Ad position (14)
 			{ collectionType: 'dynamic/fast' },
-			{ collectionType: 'dynamic/fast' },
-			{ collectionType: 'fixed/small/slow-V-mpu' },
+			{ collectionType: 'fixed/small/slow-V-mpu' }, // Ad position (16)
 			{ collectionType: 'fixed/video' },
-			{ collectionType: 'fixed/small/slow-III' },
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'news/most-popular' },
+			{ collectionType: 'fixed/small/slow-III' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ignored - is merch high position
+			{ collectionType: 'news/most-popular' }, // Ignored - is most viewed container
 		];
 
 		const mobileAdPositions = getMobileAdPositions(testCollections);
 
-		expect(mobileAdPositions).toEqual([0, 4, 7, 10, 13, 15, 17]);
+		expect(mobileAdPositions).toEqual([0, 2, 5, 9, 12, 14, 16]);
 	});
 
 	// We used https://www.theguardian.com/uk/lifeandstyle as a blueprint
 	it('Lifeandstyle front, with more than 4 collections, with thrashers at various places', () => {
 		const testCollections: Pick<DCRCollectionType, 'collectionType'>[] = [
-			{ collectionType: 'dynamic/slow' },
-			{ collectionType: 'fixed/medium/slow-VI' },
+			{ collectionType: 'dynamic/slow' }, // Ad position (0)
+			{ collectionType: 'fixed/medium/slow-VI' }, // Ignored - before thrasher
 			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/medium/slow-VI' },
+			{ collectionType: 'fixed/medium/slow-VI' }, // Ad position (3)
 			{ collectionType: 'fixed/small/slow-V-third' },
-			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (6)
+			{ collectionType: 'fixed/small/slow-IV' }, // Ignored - before thrasher
 			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/small/slow-IV' },
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/medium/slow-VI' },
+			{ collectionType: 'fixed/medium/slow-VI' }, // Ad position (9)
 			{ collectionType: 'fixed/medium/slow-XII-mpu' },
-			{ collectionType: 'fixed/small/fast-VIII' },
-			{ collectionType: 'fixed/thrasher' },
+			{ collectionType: 'fixed/small/fast-VIII' }, // Ignored - before thrasher
+			{ collectionType: 'fixed/thrasher' }, // Ad position (12)
 			{ collectionType: 'fixed/small/slow-III' },
-			{ collectionType: 'fixed/small/slow-I' },
-			{ collectionType: 'news/most-popular' },
+			{ collectionType: 'fixed/small/slow-I' }, // Ignored - is merch high position
+			{ collectionType: 'news/most-popular' }, // Ignored - is most viewed container
 		];
 
 		const mobileAdPositions = getMobileAdPositions(testCollections);
 
-		expect(mobileAdPositions).toEqual([0, 4, 8, 10, 13]);
+		expect(mobileAdPositions).toEqual([0, 3, 6, 9, 12]);
 	});
 
 	// We used https://www.theguardian.com/tone/recipes as a blueprint
 	it('Recipes front, with more than 4 collections, with thrasher at the first position', () => {
 		const testCollections: Pick<DCRCollectionType, 'collectionType'>[] = [
-			{ collectionType: 'fixed/thrasher' },
-			{ collectionType: 'fixed/medium/slow-VI' },
+			{ collectionType: 'fixed/thrasher' }, // Ignored - is first container and thrasher
+			{ collectionType: 'fixed/medium/slow-VI' }, // Ad position (1)
 			{ collectionType: 'fixed/small/slow-V-third' },
-			{ collectionType: 'fixed/medium/slow-XII-mpu' },
+			{ collectionType: 'fixed/medium/slow-XII-mpu' }, // Ad position (3)
 			{ collectionType: 'fixed/medium/fast-XII' },
-			{ collectionType: 'fixed/small/slow-V-third' },
+			{ collectionType: 'fixed/small/slow-V-third' }, // Ad position (5)
 			{ collectionType: 'fixed/small/fast-VIII' },
-			{ collectionType: 'fixed/medium/fast-XI' },
+			{ collectionType: 'fixed/medium/fast-XI' }, // Ad position (7)
 			{ collectionType: 'fixed/small/slow-III' },
-			{ collectionType: 'fixed/small/slow-IV' },
+			{ collectionType: 'fixed/small/slow-IV' }, // Ad position (9)
 			{ collectionType: 'fixed/small/slow-V-half' },
-			{ collectionType: 'fixed/small/slow-V-third' },
-			{ collectionType: 'fixed/small/fast-VIII' },
-			{ collectionType: 'news/most-popular' },
+			{ collectionType: 'fixed/small/slow-V-third' }, // Ad position (11)
+			{ collectionType: 'fixed/small/fast-VIII' }, // Ignored - is merch high position
+			{ collectionType: 'news/most-popular' }, // Ignored - is most viewed container
 		];
 
 		const mobileAdPositions = getMobileAdPositions(testCollections);
 
-		expect(mobileAdPositions).toEqual([1, 4, 6, 8, 10, 12]);
+		expect(mobileAdPositions).toEqual([1, 3, 5, 7, 9, 11]);
 	});
 });
 
