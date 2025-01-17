4d7318462d25ce8876e4b6e7b852513a61abc5bc
Alex Sanders <alex@sndrs.dev>
2025-01-17 13:46:41 +0000

    Handle crossword articles (#12925)
    * initial addition of crossword player
    
    Co-authored-by: Oliver Abrahams <ollie.abrahams@guardian.co.uk>
    
    ---------
    
    Co-authored-by: oliverabrahams <ollie.abrahams@guardian.co.uk>
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

--- dotcom-rendering/src/types/content.ts
+++ dotcom-rendering/src/types/content.ts
@@ -1,3 +1,4 @@
+import { type CrosswordProps } from '@guardian/react-crossword-next';
 import type { ArticleFormat } from '../lib/articleFormat';
 
 export type StarRating = 0 | 1 | 2 | 3 | 4 | 5;
@@ -756,6 +757,12 @@ interface WitnessTypeBlockElement extends ThirdPartyEmbeddedContent {
 		| WitnessTypeDataVideo
 		| WitnessTypeDataText;
 }
+
+export interface CrosswordElement {
+	_type: 'model.dotcomrendering.pageElements.CrosswordElement';
+	crossword: CrosswordProps['data'];
+}
+
 export type FEElement =
 	| AdPlaceholderBlockElement
 	| AudioAtomBlockElement
@@ -816,7 +823,8 @@ export type FEElement =
 	| VideoYoutubeBlockElement
 	| VineBlockElement
 	| YoutubeBlockElement
-	| WitnessTypeBlockElement;
+	| WitnessTypeBlockElement
+	| CrosswordElement;
 
 // -------------------------------------
 // Misc
