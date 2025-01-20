76f10b2d6d98a6932c7ecf5b7bddbbc7da8d6f3e
oliverabrahams <ollie.abrahams@guardian.co.uk>
2025-01-20 15:57:24 +0000

    add comment

--- dotcom-rendering/src/components/CrosswordComponent.importable.tsx
+++ dotcom-rendering/src/components/CrosswordComponent.importable.tsx
@@ -1,6 +1,10 @@
 import { Crossword as ReactCrossword } from '@guardian/react-crossword-next';
 import type { CrosswordProps } from '@guardian/react-crossword-next';
 
+/*
+	The name of this component is important, when it was "Crossword"
+	webpack was renaming it and the JS wasn't loading in the client.
+ */
 export const CrosswordComponent = ({
 	data,
 }: {
