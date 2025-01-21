ec58bd7735c9c69a9aa9001346d41a8664ce0ee2
Oliver Abrahams <ollie.abrahams@guardian.co.uk>
2025-01-21 15:55:20 +0000

    Change crossword component name to fix renaming issue with build (#13177)
    * Change name to fix renaming issue with build

--- dotcom-rendering/src/components/Crossword.importable.tsx
+++
@@ -1,6 +0,0 @@
-import { Crossword as ReactCrossword } from '@guardian/react-crossword-next';
-import type { CrosswordProps } from '@guardian/react-crossword-next';
-
-export const Crossword = ({ data }: { data: CrosswordProps['data'] }) => (
-	<ReactCrossword data={data} clueMinWidth={150} />
-);
