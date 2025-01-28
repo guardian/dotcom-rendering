a5d40cb6a2b7d119922a574cb55c00e03c765010
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2025-01-28 17:41:12 +0000

    Adjust spacing of crossword instructions

--- dotcom-rendering/src/components/CrosswordInstructions.tsx
+++ dotcom-rendering/src/components/CrosswordInstructions.tsx
@@ -1,12 +1,19 @@
 import { css } from '@emotion/react';
 import {
+	space,
 	textEgyptian17,
 	textEgyptianBold17,
 } from '@guardian/source/foundations';
 import sanitise from 'sanitize-html';
 
+const headerStyles = css`
+	${textEgyptianBold17};
+	margin-bottom: ${space[2]}px;
+`;
+
 const instructionsStyles = css`
 	${textEgyptian17};
+	display: block;
 	white-space: pre-line;
 
 	i {
@@ -18,18 +25,16 @@ const instructionsStyles = css`
 	}
 `;
 
-const headerStyles = css`
-	${textEgyptianBold17};
-	white-space: pre-line;
-`;
-
 export const CrosswordInstructions = ({
 	instructions,
 }: {
 	instructions: string;
 }) => (
-	<div css={instructionsStyles}>
-		<strong css={headerStyles}>Special instructions: </strong>
-		<span dangerouslySetInnerHTML={{ __html: sanitise(instructions) }} />
-	</div>
+	<>
+		<h2 css={headerStyles}>Special instructions:</h2>
+		<p
+			css={instructionsStyles}
+			dangerouslySetInnerHTML={{ __html: sanitise(instructions) }}
+		/>
+	</>
 );
