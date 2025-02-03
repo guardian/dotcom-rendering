8098ad439f514c5d22a40fad75815316ce81f1ac
Anna Beddow <anna.beddow@gmail.com>
2025-02-03 13:05:38 +0000

    Exclude europe beta test from adaption

--- dotcom-rendering/src/client/adaptiveSite.ts
+++ dotcom-rendering/src/client/adaptiveSite.ts
@@ -16,6 +16,13 @@ export const shouldAdapt = async (): Promise<boolean> => {
 	if (!window.guardian.config.switches.adaptiveSite) return false;
 	if (window.location.host !== 'www.theguardian.com') return false;
 
+	/**
+	 * The europe beta front is being served to a 0% audience. This means it's rarely in cache and so it gets adapted more often.
+	 * This is a temporary measure to ensure that the front is not adapted until it is in cache.
+	 */
+	if (window.guardian.config.tests.europeBetaFrontVariant === 'variant')
+		return false;
+
 	// only evaluate this code if we want to adapt in response to page performance
 	const { isPerformingPoorly } = await import(
 		/* webpackMode: "eager" */ './poorPerformanceMonitoring'
