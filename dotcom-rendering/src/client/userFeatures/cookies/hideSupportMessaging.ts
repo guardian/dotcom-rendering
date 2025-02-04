fce7a52b786166c824e20786256743bcb497f530
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-02-04 14:43:07 +0000

    New cookie lifecycle

--- dotcom-rendering/src/client/userFeatures/cookies/hideSupportMessaging.ts
+++ dotcom-rendering/src/client/userFeatures/cookies/hideSupportMessaging.ts
@@ -1,16 +1,10 @@
-import { getCookie, removeCookie, setCookie } from '@guardian/libs';
+import { getCookie } from '@guardian/libs';
+import { cookieIsValid } from './cookieHelpers';
 
-const HIDE_SUPPORT_MESSAGING_COOKIE = 'gu_hide_support_messaging';
+export const HIDE_SUPPORT_MESSAGING_COOKIE = 'gu_hide_support_messaging';
+
+export const hideSupportMessaging = (): boolean =>
+	cookieIsValid(HIDE_SUPPORT_MESSAGING_COOKIE);
 
 export const getHideSupportMessagingCookie = (): string | null =>
 	getCookie({ name: HIDE_SUPPORT_MESSAGING_COOKIE });
-
-export const setHideSupportMessagingCookie = (value: boolean): void => {
-	setCookie({
-		name: HIDE_SUPPORT_MESSAGING_COOKIE,
-		value: String(value),
-	});
-};
-
-export const removeHideSupportMessagingCookie = (): void =>
-	removeCookie({ name: HIDE_SUPPORT_MESSAGING_COOKIE });
