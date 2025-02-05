1651abfa07656724c55584f951666803d1e6dae5
Sue Burt <sue.burt@guardian.co.uk>
2025-02-05 13:50:22 +0000

    Update storybook to receive props

--- dotcom-rendering/src/components/marketing/gutters/StickyLiveblogAsk.stories.tsx
+++ dotcom-rendering/src/components/marketing/gutters/StickyLiveblogAsk.stories.tsx
@@ -1,14 +1,11 @@
 import { css } from '@emotion/react';
 import { space } from '@guardian/source/foundations';
+import type { Meta, StoryObj } from '@storybook/react';
+import lzstring from 'lz-string';
 import { GutterAsk } from './gutterAsk';
 import { props } from './utils/storybook';
 
 const { variant, enrichedUrl, onCtaClick } = props;
-
-export default {
-	component: GutterAsk,
-	title: 'Components/Marketing/StickyLiveblogAsk',
-};
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => (
 	<div
@@ -21,16 +18,33 @@ const Wrapper = ({ children }: { children: React.ReactNode }) => (
 	</div>
 );
 
-export const Default = () => {
-	return (
-		<Wrapper>
-			<GutterAsk
-				variant={variant}
-				enrichedUrl={enrichedUrl}
-				onCtaClick={onCtaClick}
-			/>
-		</Wrapper>
-	);
+type WithJsonProps<T> = T & { json?: string };
+type Props = WithJsonProps<React.ComponentProps<typeof GutterAsk>>;
+
+const meta: Meta<Props> = {
+	component: GutterAsk,
+	title: 'Components/Marketing/StickyLiveblogAsk',
+	args: {
+		variant,
+		enrichedUrl,
+		onCtaClick,
+		json: '',
+	},
+	render: ({ json, ...args }) => {
+		const jsonProps = json
+			? JSON.parse(lzstring.decompressFromEncodedURIComponent(json))
+			: {};
+		return (
+			<Wrapper>
+				<GutterAsk {...args} {...jsonProps} />
+			</Wrapper>
+		);
+	},
 };
 
-Default.storyName = 'Default';
+export default meta;
+
+type Story = StoryObj<Props>;
+export const Default: Story = {
+	name: 'Basic GutterAsk',
+};
