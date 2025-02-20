42ae8ae2a5a44778abc71790c22c21c3148fab96
Anna Beddow <anna.beddow@gmail.com>
2025-02-20 17:12:58 +0000

    Use aspect ratio map in maintain aspect ratio and add mobile aspect ratio support

--- dotcom-rendering/src/components/MaintainAspectRatio.tsx
+++ dotcom-rendering/src/components/MaintainAspectRatio.tsx
@@ -1,10 +1,13 @@
 import { css } from '@emotion/react';
 import type { AspectRatio } from '../types/front';
+import { getAspectRatioPadding } from './CardPicture';
+import { until } from 'node_modules/@guardian/source/dist/foundations';
 
 type Props = {
 	height: number;
 	width: number;
 	aspectRatio?: AspectRatio;
+	mobileAspectRatio?: AspectRatio;
 	children: React.ReactNode;
 };
 
@@ -12,29 +15,39 @@ export const MaintainAspectRatio = ({
 	height,
 	width,
 	aspectRatio,
+	mobileAspectRatio,
 	children,
 }: Props) => {
 	/* https://css-tricks.com/aspect-ratio-boxes/ */
-	const paddingBottom =
-		aspectRatio === '5:4'
-			? `${(4 / 5) * 100}%`
-			: `${(height / width) * 100}%`;
+	const paddingBottom = aspectRatio
+		? getAspectRatioPadding(aspectRatio)
+		: `${(height / width) * 100}%`;
 
 	return (
 		<div
-			css={css`
-				/* position relative to contain the absolutely positioned iframe plus any Overlay image */
-				position: relative;
-				padding-bottom: ${paddingBottom};
-				& > iframe,
-				& > video {
-					width: 100%;
-					height: 100%;
-					position: absolute;
-					top: 0;
-					left: 0;
-				}
-			`}
+			css={[
+				css`
+					/* position relative to contain the absolutely positioned iframe plus any Overlay image */
+					position: relative;
+					padding-bottom: ${paddingBottom};
+					& > iframe,
+					& > video {
+						width: 100%;
+						height: 100%;
+						position: absolute;
+						top: 0;
+						left: 0;
+					}
+				`,
+				mobileAspectRatio &&
+					css`
+						${until.tablet} {
+							padding-bottom: ${getAspectRatioPadding(
+								mobileAspectRatio,
+							)};
+						}
+					`,
+			]}
 		>
 			{children}
 		</div>
