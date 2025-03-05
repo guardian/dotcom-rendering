98f03733926e5b97deef01f67596d374f79f6180
Anna Beddow <anna.beddow@gmail.com>
2025-03-05 15:37:29 +0000

    revert testing

--- dotcom-rendering/src/components/FlexibleGeneral.tsx
+++ dotcom-rendering/src/components/FlexibleGeneral.tsx
@@ -196,7 +196,7 @@ export const SplashCardLayout = ({
 	const card = cards[0];
 	if (!card) return null;
 
-	const isImmersive = true; // TODO: replace with live data from fronts tool - used for testing
+	const isImmersive = false; // TODO: replace with live data from fronts tool - used for testing
 	const shouldShowImmersive = isImmersive && !isMediaCard(card.format);
 
 	if (shouldShowImmersive) {
