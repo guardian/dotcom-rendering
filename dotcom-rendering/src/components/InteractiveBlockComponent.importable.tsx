61fa85ae1fe356c125a03027f06f2b5d26d593b5
frederickobrien <frederick.obrien@guardian.co.uk>
2025-03-19 15:11:12 +0000

    Add `dark=false` param for in-house interactives

--- dotcom-rendering/src/components/InteractiveBlockComponent.importable.tsx
+++ dotcom-rendering/src/components/InteractiveBlockComponent.importable.tsx
@@ -346,23 +346,23 @@ export const InteractiveBlockComponent = ({
 			}
 
 			// Datawrapper-specific fix to suppress scrollbars appearing
-			if (url.includes('datawrapper')) {
-				iframe.scrolling = 'no';
-				// Turn off dark mode for Datawrapper embeds on web
-				// This should be removed if/when dark mode is implements on the website
-				if (
-					!document.querySelector('.ios') &&
-					!document.querySelector('.android')
-				) {
-					const prefersDarkScheme = window.matchMedia(
-						'(prefers-color-scheme: dark)',
-					).matches;
-					const darkMode = darkModeAvailable && prefersDarkScheme;
-					if (!darkMode) {
-						iframe.src +=
-							(iframe.src.includes('?') ? '&' : '?') +
-							'dark=false';
-					}
+			if (url.includes('datawrapper')) iframe.scrolling = 'no';
+
+			// Bespoke dark mode logic for Datawrapper and ai2html embeds on web
+			// This should be removed if/when dark mode is implements on the website
+			if (
+				!document.querySelector('.ios') &&
+				!document.querySelector('.android') &&
+				(url.includes('datawrapper') ||
+					url.includes('interactive.guim.co.uk/uploader/embed/'))
+			) {
+				const prefersDarkScheme = window.matchMedia(
+					'(prefers-color-scheme: dark)',
+				).matches;
+				const darkMode = darkModeAvailable && prefersDarkScheme;
+				if (!darkMode) {
+					iframe.src +=
+						(iframe.src.includes('?') ? '&' : '?') + 'dark=false';
 				}
 			}
 
