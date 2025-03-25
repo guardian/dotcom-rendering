b36ef4c0614491a2bea581f25cc57535ba2cc105
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2025-03-25 14:17:47 +0000

    place components on grid and create background on left

---
+++ dotcom-rendering/src/components/FootballMatchSummary.tsx
@@ -0,0 +1,83 @@
+import { css } from '@emotion/react';
+import { from } from '@guardian/source/foundations';
+import { grid } from '../grid';
+import { palette } from '../palette';
+import type { TeamType } from '../types/sport';
+import { MatchNav } from './MatchNav';
+import { MatchStats } from './MatchStats';
+
+const gridStyles = css`
+	${grid.paddedContainer}
+	position: relative;
+	${from.tablet} {
+		&::before,
+		&::after {
+			content: '';
+			position: absolute;
+			border-left: 1px solid ${palette('--article-border')};
+			top: 0;
+			bottom: 0;
+		}
+
+		&::after {
+			right: 0;
+		}
+	}
+`;
+
+type Props = {
+	homeTeam: TeamType;
+	awayTeam: TeamType;
+	comments?: string;
+	competition: string;
+};
+
+export const FootballMatchSummary = ({
+	homeTeam,
+	awayTeam,
+	comments,
+	competition,
+}: Props) => (
+	<main>
+		<div css={gridStyles}>
+			<div
+				css={css`
+					${grid.column.centre};
+
+					${from.tablet} {
+						:before {
+							content: '';
+							position: absolute;
+							top: 0;
+							bottom: 10px;
+							width: 100vw;
+							left: -50vw;
+							background-color: var(--match-nav-background);
+						}
+					}
+				`}
+			>
+				<MatchNav
+					homeTeam={homeTeam}
+					awayTeam={awayTeam}
+					comments={comments}
+				/>
+			</div>
+		</div>
+		<div css={gridStyles}>
+			<div
+				css={css`
+					${grid.column.centre}
+					/* ToDo: change the background colour without resorting to this */
+					--match-stats-background: initial;
+				`}
+			>
+				<MatchStats
+					home={homeTeam}
+					away={awayTeam}
+					competition={competition}
+				/>
+			</div>
+		</div>
+	</main>
+);
