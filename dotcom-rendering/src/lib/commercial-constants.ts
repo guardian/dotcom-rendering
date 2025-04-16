667fa7b107eb3827503020c4dfce57d879df3edd
Charlotte <charlotte.emms@theguardian.com>
2025-04-16 14:31:10 +0100

    allow more ads on fronts with beta collections

--- dotcom-rendering/src/lib/commercial-constants.ts
+++ dotcom-rendering/src/lib/commercial-constants.ts
@@ -5,6 +5,12 @@
 export const MAX_FRONTS_BANNER_ADS = 6;
 
 /**
+ * The maximum number of fronts-banner ads that can be inserted on fronts with beta collections present.
+ * fronts-banner ads are inserted from the desktop breakpoint.
+ */
+export const MAX_FRONTS_BANNER_ADS_BETA = 8;
+
+/**
  * The maximum number of ads that can be inserted on any mobile front.
  * Mobile ads on fronts are inserted up until the tablet breakpoint.
  */
