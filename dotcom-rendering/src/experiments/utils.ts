4ae62989be191035c16ed15174cdb30f75d929b6
Ravi <7014230+arelra@users.noreply.github.com>
2025-04-17 15:23:37 +0100

    Remove @guardian/core-web-vitals

--- dotcom-rendering/src/experiments/utils.ts
+++ dotcom-rendering/src/experiments/utils.ts
@@ -1,5 +1,5 @@
 import { bypassCommercialMetricsSampling } from '@guardian/commercial';
-import { bypassCoreWebVitalsSampling } from '@guardian/core-web-vitals';
+import { bypassCoreWebVitalsSampling } from '../lib/cwv/cwv';
 
 export const bypassMetricsSampling = (): void => {
 	void bypassCommercialMetricsSampling();
