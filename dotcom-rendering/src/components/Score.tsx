f12dfab32472b0267f016807102c6f45e2307ca8
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2025-04-17 15:09:48 +0100

    Support scores above ten in the match nav

--- dotcom-rendering/src/components/Score.tsx
+++ dotcom-rendering/src/components/Score.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { text } from '@guardian/source/foundations';
+import { palette } from '../palette';
 import { Eight } from './numbers/Eight';
 import { Five } from './numbers/Five';
 import { Four } from './numbers/Four';
@@ -12,103 +12,69 @@ import { Three } from './numbers/Three';
 import { Two } from './numbers/Two';
 import { Zero } from './numbers/Zero';
 
+const ScoreStyles = ({ children }: { children: React.ReactNode }) => (
+	<div
+		css={css`
+			position: relative;
+			width: 3.75rem;
+			height: 3.75rem;
+			border-radius: 1.875rem;
+			border: 0.0625rem solid ${palette('--football-score-border')};
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		`}
+	>
+		{children}
+	</div>
+);
+
 type Props = {
 	score: number;
 };
 
-export const Score = ({ score }: Props) => {
-	const ScoreStyles = ({ children }: { children: React.ReactNode }) => (
-		<div
-			css={css`
-				position: relative;
-				width: 3.75rem;
-				height: 3.75rem;
-				border-radius: 1.875rem;
-				border: 0.0625rem solid ${text.primary};
-
-				svg {
-					position: absolute;
-					top: 0;
-					left: 0;
-					right: 0;
-					bottom: 0;
-					margin: auto;
-				}
-			`}
-		>
-			{children}
-		</div>
-	);
-
-	if (Number.isNaN(score)) return <ScoreStyles> </ScoreStyles>;
+const ScoreNumber = ({ score }: Props) => {
+	if (Number.isNaN(score)) {
+		return null;
+	}
 
 	switch (score) {
 		case 0:
-			return (
-				<ScoreStyles>
-					<Zero />
-				</ScoreStyles>
-			);
+			return <Zero />;
 		case 1:
-			return (
-				<ScoreStyles>
-					<One />
-				</ScoreStyles>
-			);
+			return <One />;
 		case 2:
-			return (
-				<ScoreStyles>
-					<Two />
-				</ScoreStyles>
-			);
+			return <Two />;
 		case 3:
-			return (
-				<ScoreStyles>
-					<Three />
-				</ScoreStyles>
-			);
+			return <Three />;
 		case 4:
-			return (
-				<ScoreStyles>
-					<Four />
-				</ScoreStyles>
-			);
+			return <Four />;
 		case 5:
-			return (
-				<ScoreStyles>
-					<Five />
-				</ScoreStyles>
-			);
+			return <Five />;
 		case 6:
-			return (
-				<ScoreStyles>
-					<Six />
-				</ScoreStyles>
-			);
+			return <Six />;
 		case 7:
-			return (
-				<ScoreStyles>
-					<Seven />
-				</ScoreStyles>
-			);
+			return <Seven />;
 		case 8:
-			return (
-				<ScoreStyles>
-					<Eight />
-				</ScoreStyles>
-			);
+			return <Eight />;
 		case 9:
-			return (
-				<ScoreStyles>
-					<Nine />
-				</ScoreStyles>
-			);
+			return <Nine />;
 		case 10:
+			return <Ten />;
 		default:
 			return (
-				<ScoreStyles>
-					<Ten />
-				</ScoreStyles>
+				<>
+					<ScoreNumber score={Number(score.toString()[0])} />
+					<ScoreNumber score={Number(score.toString().slice(1))} />
+				</>
 			);
 	}
+};
+
+export const Score = ({ score }: Props) => {
+	return (
+		<ScoreStyles>
+			<ScoreNumber score={score} />
+		</ScoreStyles>
+	);
 };
