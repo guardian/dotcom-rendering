00459fe9aa458bf9678293c6d693c1c8d3de29e5
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2025-04-23 11:06:06 +0100

    refactor validity check
    - account for negative
    - isInteger and isNaN is redundant

--- dotcom-rendering/src/components/Score.tsx
+++ dotcom-rendering/src/components/Score.tsx
@@ -34,7 +34,7 @@ type Props = {
 };
 
 const ScoreNumber = ({ score }: Props) => {
-	if (Number.isNaN(score) || !Number.isInteger(score)) {
+	if (!Number.isInteger(score) || score < 0) {
 		return null;
 	}
 
