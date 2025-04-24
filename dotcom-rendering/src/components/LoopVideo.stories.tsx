f02b8f88754e2da4c0a928a0749e6d1ae6fb1377
Dominik Lander <dominik.lander@guardian.co.uk>
2025-04-24 09:35:21 +0100

    Add dimensions to story

--- dotcom-rendering/src/components/LoopVideo.stories.tsx
+++ dotcom-rendering/src/components/LoopVideo.stories.tsx
@@ -21,6 +21,8 @@ export const Default = {
 	args: {
 		src: 'https://uploads.guim.co.uk/2024/10/01/241001HeleneLoop_2.mp4',
 		videoId: 'test-video-1',
+		height: 337.5,
+		width: 600,
 		fallbackImage: (
 			<CardPicture
 				mainImage="https://i.guim.co.uk/img/media/13dd7e5c4ca32a53cd22dfd90ac1845ef5e5d643/91_0_1800_1080/master/1800.jpg?width=465&dpr=1&s=none&crop=5%3A4"
