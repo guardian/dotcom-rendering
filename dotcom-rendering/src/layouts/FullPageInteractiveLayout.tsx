8c31f7130d378e86b4f28daf002b91c0ee2cb6b3
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2025-04-29 16:10:38 +0100

    Update dotcom-rendering/src/layouts/FullPageInteractiveLayout.tsx
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>

--- dotcom-rendering/src/layouts/FullPageInteractiveLayout.tsx
+++ dotcom-rendering/src/layouts/FullPageInteractiveLayout.tsx
@@ -243,18 +243,20 @@ export const FullPageInteractiveLayout = (props: WebProps | AppsProps) => {
 				</>
 			)}
 			{isApps && format.theme === ArticleSpecial.Labs && (
-				<Stuck>
-					<Section
-						fullWidth={true}
-						showTopBorder={false}
-						padSides={true}
-						backgroundColour={sourcePalette.labs[400]}
-						borderColour={sourcePalette.neutral[60]}
-						sectionId="labs-header"
-					>
-						<LabsHeader editionId={editionId} />
-					</Section>
-				</Stuck>
+				<header>
+					<Stuck>
+						<Section
+							fullWidth={true}
+							showTopBorder={false}
+							padSides={true}
+							backgroundColour={sourcePalette.labs[400]}
+							borderColour={sourcePalette.neutral[60]}
+							sectionId="labs-header"
+						>
+							<LabsHeader editionId={editionId} />
+						</Section>
+					</Stuck>
+				</header>
 			)}
 
 			<Section
