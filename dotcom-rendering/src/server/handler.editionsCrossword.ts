714ec22cf8e7d18fb3cdb5f489bb29d2e2699764
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2025-05-02 14:25:20 +0100

    Temporarily remove validation to allow new crossword data model

--- dotcom-rendering/src/server/handler.editionsCrossword.ts
+++ dotcom-rendering/src/server/handler.editionsCrossword.ts
@@ -1,10 +1,9 @@
 import type { RequestHandler } from 'express';
-import { validateAsEditionsCrosswordType } from '../model/validate';
 import { makePrefetchHeader } from './lib/header';
 import { renderCrosswordHtml } from './render.editionsCrossword';
 
 export const handleEditionsCrossword: RequestHandler = ({ body }, res) => {
-	const editionsCrosswords = validateAsEditionsCrosswordType(body);
+	const editionsCrosswords = body;
 	const { html, prefetchScripts } = renderCrosswordHtml({
 		editionsCrosswords,
 	});
