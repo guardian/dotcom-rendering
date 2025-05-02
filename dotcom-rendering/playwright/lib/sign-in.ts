957a34c1ec71b55d2f8cefab2f531b52565022d2
Ravi <7014230+arelra@users.noreply.github.com>
2025-05-02 20:35:39 +0100

    e2e sign in

---
+++ dotcom-rendering/playwright/lib/sign-in.ts
@@ -0,0 +1,112 @@
+import { randomUUID } from 'node:crypto';
+import type { BrowserContext, Page } from '@playwright/test';
+import { disableCMP } from './cmp';
+import { expectToExist } from './locators';
+
+type Networks = 'facebook' | 'apple' | 'google';
+
+type SocialLink = {
+	socialId: number;
+	network: Networks;
+};
+
+type IDAPITestUserOptions = {
+	primaryEmailAddress?: `${string}@theguardian.com`;
+	isUserEmailValidated?: boolean;
+	socialLinks?: SocialLink[];
+	password?: string;
+	deleteAfterMinute?: boolean;
+	isGuestUser?: boolean;
+	doNotSetUsername?: boolean;
+};
+
+type IDAPITestUserResponse = [
+	{
+		key: 'GU_U';
+		value: string;
+	},
+	{
+		key: 'SC_GU_LA';
+		sessionCookie: boolean;
+		value: string;
+	},
+	{
+		key: 'SC_GU_U';
+		value: string;
+	},
+];
+
+type IDAPITestUser = {
+	email: string;
+	cookies: IDAPITestUserResponse;
+	password: string;
+};
+
+const createTestUser = async (): Promise<IDAPITestUser> => {
+	const email = `dcr-e2e-${randomUUID()}@theguardian.com`;
+	const password = randomUUID();
+	try {
+		const response = await fetch(
+			'https://idapi.code.dev-theguardian.com/user/test',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-GU-ID-Client-Access-Token': `Bearer ${process.env.IDAPI_CLIENT_ACCESS_TOKEN}`,
+				},
+				body: JSON.stringify({
+					primaryEmailAddress: email,
+					isUserEmailValidated: true,
+					socialLinks: [],
+					password,
+					deleteAfterMinute: true,
+					isGuestUser: false,
+					doNotSetUsername: false,
+				} as IDAPITestUserOptions),
+			},
+		);
+
+		if (!response.ok) {
+			throw new Error(
+				`Failed to create IDAPI test user: ${response.statusText}`,
+			);
+		}
+
+		const responseBody = (await response.json()) as {
+			values: IDAPITestUserResponse;
+		};
+
+		return {
+			email,
+			cookies: responseBody.values,
+			password,
+		};
+	} catch (error) {
+		throw new Error(`Failed to create IDAPI test user: ${String(error)}`);
+	}
+};
+
+const signIn = async (page: Page, context: BrowserContext): Promise<void> => {
+	const startPage = page.url();
+	await disableCMP(context);
+
+	await page.click('text="Sign in"');
+
+	await page.waitForLoadState('load');
+	await expectToExist(page, 'button:text("Sign in with a password instead")');
+
+	const { email, password } = await createTestUser();
+
+	await page.fill('input[name=email]', email);
+	await page.click('button:text("Sign in with a password instead")');
+
+	await page.waitForLoadState('load');
+	await expectToExist(page, '[data-cy="main-form-submit-button"]');
+
+	await page.fill('input[name=password]', password);
+	await page.click('[data-cy="main-form-submit-button"]');
+
+	await page.waitForURL(startPage);
+};
+
+export { createTestUser, signIn };
