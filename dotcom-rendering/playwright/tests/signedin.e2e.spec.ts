16619d073b9766a6d2eb129a16f2b6cecd5449ea
Ravi <7014230+arelra@users.noreply.github.com>
2025-05-06 10:18:07 +0100

    Signed in e2e test

--- dotcom-rendering/playwright/tests/signedin.e2e.spec.ts
+++ dotcom-rendering/playwright/tests/signedin.e2e.spec.ts
@@ -1,8 +1,9 @@
 import { expect, test } from '@playwright/test';
+import { signIn } from 'playwright/lib/sign-in';
 import { Standard as standardArticle } from '../../fixtures/generated/fe-articles/Standard';
 import { disableCMP } from '../lib/cmp';
 import { waitForIsland } from '../lib/islands';
-import { loadPageWithOverrides } from '../lib/load-page';
+import { loadPage, loadPageWithOverrides } from '../lib/load-page';
 
 test.describe('Signed in readers', () => {
 	test('should not display signed in texts when users are not signed in', async ({
@@ -22,5 +23,17 @@ test.describe('Signed in readers', () => {
 		expect(discussionText).toContain(
 			'Sign in or create your Guardian account to join the discussion',
 		);
+	});
+
+	test.only('should sign in a user and display account details', async ({
+		context,
+		page,
+	}) => {
+		await disableCMP(context);
+		await loadPage({
+			page,
+			path: `/Article/https://www.theguardian.com/politics/2019/oct/29/tories-restore-party-whip-to-10-mps-who-sought-to-block-no-deal-brexit`,
+		});
+		await signIn(page, context);
 	});
 });
