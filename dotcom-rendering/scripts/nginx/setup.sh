232429eb7443c98eaaa4b01d849be00aa150149c
Ravi <7014230+arelra@users.noreply.github.com>
2025-05-11 22:36:34 +0100

    CI secure server setup

--- dotcom-rendering/scripts/nginx/setup.sh
+++ dotcom-rendering/scripts/nginx/setup.sh
@@ -6,6 +6,10 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 brew bundle --file=${DIR}/Brewfile
 
-dev-nginx setup-app ${DIR}/nginx-mappings.yaml
-
-echo "🌎 Successfully installed config. https://r.thegulocal.com is now setup."
+if [ -n "$CI" ]; then
+    dev-nginx setup-app ${DIR}/nginx-mappings-ci.yaml
+    echo "🌎 Successfully installed CI config. https://r.thegulocal.com is now setup."
+else
+    dev-nginx setup-app ${DIR}/nginx-mappings.yaml
+    echo "🌎 Successfully installed local config. https://r.thegulocal.com is now setup."
+fi
