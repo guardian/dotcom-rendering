0a5e18b15e4aef82bc63ae8c07ecd6472ece7ab7
Ravi <7014230+arelra@users.noreply.github.com>
2025-05-11 22:46:09 +0100

    Fix CI nginx setup script

--- dotcom-rendering/scripts/nginx/setup.sh
+++ dotcom-rendering/scripts/nginx/setup.sh
@@ -5,11 +5,5 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 brew bundle --file=${DIR}/Brewfile
-
-if [ -n "$CI" ]; then
-    dev-nginx setup-app ${DIR}/nginx-mappings-ci.yaml
-    echo "🌎 Successfully installed CI config. https://r.thegulocal.com is now setup."
-else
-    dev-nginx setup-app ${DIR}/nginx-mappings.yaml
-    echo "🌎 Successfully installed local config. https://r.thegulocal.com is now setup."
-fi
+dev-nginx setup-app ${DIR}/nginx-mappings.yaml
+echo "🌎 Successfully installed local config. https://r.thegulocal.com is now setup."
