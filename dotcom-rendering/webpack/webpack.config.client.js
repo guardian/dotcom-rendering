e3da6375518dd7ef07d7e9963958b4534c5fafb5
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2025-05-12 11:49:32 +0100

    Revert "Switch back to Preact for server-side rendering (#13901)" (#13931)
    This reverts commit d8c0ae4b03fc06c36efd7735a950a800b8c59dad.

--- dotcom-rendering/webpack/webpack.config.client.js
+++ dotcom-rendering/webpack/webpack.config.client.js
@@ -139,6 +139,13 @@ module.exports = ({ build }) => ({
 			svgr,
 		],
 	},
+	resolve: {
+		alias: {
+			react: 'preact/compat',
+			'react-dom/test-utils': 'preact/test-utils',
+			'react-dom': 'preact/compat',
+		},
+	},
 });
 
 module.exports.transpileExclude = {
--- dotcom-rendering/webpack/webpack.config.js
+++ dotcom-rendering/webpack/webpack.config.js
@@ -30,11 +30,6 @@ const commonConfigs = ({ platform }) => ({
 			? 'source-map'
 			: 'eval-cheap-module-source-map',
 	resolve: {
-		alias: {
-			react: 'preact/compat',
-			'react-dom/test-utils': 'preact/test-utils',
-			'react-dom': 'preact/compat',
-		},
 		extensions: ['.js', '.ts', '.tsx', '.jsx'],
 	},
 	ignoreWarnings: [
