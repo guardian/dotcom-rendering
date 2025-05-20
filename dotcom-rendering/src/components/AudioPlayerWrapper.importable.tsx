aff904f5e94b1c2060dc6fe3a6169566129a601f
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-05-20 11:40:30 +0100

    `ValuesWithChange` Election Trackers Component (#13920)
    Adds a new election trackers component to display a list of values and
    the amount by which they've changed. This could be used, for example, to
    display a list of seats won in an election along with the changes since
    the last election. The included stories show examples of this.
    
    This component is one of several that will allow rendering election
    trackers within DCAR.

--- dotcom-rendering/src/components/AudioPlayerWrapper.importable.tsx
+++ dotcom-rendering/src/components/AudioPlayerWrapper.importable.tsx
@@ -1,13 +1,9 @@
-import { getConsentFor, onConsentChange } from '@guardian/libs';
-import { useEffect, useState } from 'react';
 import { AudioPlayer } from './AudioPlayer/AudioPlayer';
 
 type Props = {
 	mediaId: string;
 	duration?: number;
 	src: string;
-	contentIsNotSensitive: boolean;
-	isAcastEnabled: boolean;
 };
 
 /**
@@ -20,34 +16,6 @@ type Props = {
  *
  * (No visual story exists)
  */
-export const AudioPlayerWrapper = ({
-	duration,
-	src,
-	mediaId,
-	contentIsNotSensitive,
-	isAcastEnabled,
-}: Props) => {
-	const [finalSrc, setFinalSrc] = useState<string>(src);
-
-	useEffect(() => {
-		// this is how frontend checks for whether to show ads or not,
-		// and it's on the window in DCR but it's not clear how...
-		// so this just carries over the existing logic
-		// https://github.com/guardian/frontend/blob/ba57677baaa06f37235e8d7a983cb383d0f5c989/static/src/javascripts/projects/common/modules/audio/index.js#L25-L44
-		const isPodcast = window.guardian.config.page.isPodcast;
-
-		if (contentIsNotSensitive && isAcastEnabled && isPodcast) {
-			onConsentChange((consentState) => {
-				const consentForAcast = getConsentFor('acast', consentState);
-
-				if (consentForAcast) {
-					setFinalSrc(
-						src.replace('https://', 'https://flex.acast.com/'),
-					);
-				}
-			});
-		}
-	}, [src, contentIsNotSensitive, isAcastEnabled]);
-
-	return <AudioPlayer src={finalSrc} mediaId={mediaId} duration={duration} />;
+export const AudioPlayerWrapper = ({ duration, src, mediaId }: Props) => {
+	return <AudioPlayer src={src} mediaId={mediaId} duration={duration} />;
 };
