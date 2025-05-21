03f2e9764bbb525472bf0821d2f5151a5c2abbb7
Imogen Hardy <imogen.hardy@guardian.co.uk>
2025-05-21 17:24:51 +0100

    overhaul svg structure

--- ab-testing/frontend/src/lib/components/AudienceBreakdownSVG.svelte
+++ ab-testing/frontend/src/lib/components/AudienceBreakdownSVG.svelte
@@ -23,18 +23,17 @@
 
 	function textOffset(index: number) {
 		const barStart = index * BAR_HEIGHT;
-		return barStart + (BAR_HEIGHT / 2) + BAR_HEIGHT;
+		return barStart + BAR_HEIGHT / 2 + BAR_HEIGHT;
 	}
 </script>
 
 <svg
-	class="chart"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
 	width="100%"
 	height={tests.length * 50 + 50}
-	aria-labelledby="title"
-	role="img"
+	class="chart"
 >
-	<title id="title">A/B test audience share</title>
 	<g class="legend">
 		<text x="0%" y={BAR_HEIGHT - 9}>0%</text>
 		<text x="25%" y={BAR_HEIGHT - 9}>25%</text>
@@ -42,22 +41,33 @@
 		<text x="75%" y={BAR_HEIGHT - 9}>75%</text>
 	</g>
 	{#each tests as test, index}
-		<g class="bar" width={`${getSize(test)}%`} y={(index * BAR_HEIGHT) + BAR_HEIGHT}>
-			<rect
-				width={`${getSize(test)}%`}
-				height={BAR_HEIGHT}
-				x={`${getOffset(test)}%`}
-				y={(index * BAR_HEIGHT) + BAR_HEIGHT}
-				rx="2"
-			></rect>
-			<text dominant-baseline="center" y={textOffset(index)} x={`${getOffset(test)}%`}>
-				{test.name}
-			</text>
-		</g>
+		<svg
+			x={`${getOffset(test)}%`}
+			width={`${getSize(test)}%`}
+			y={index * BAR_HEIGHT + BAR_HEIGHT}
+		>
+			<g class="bar">
+				<rect
+					height={`${BAR_HEIGHT}px`}
+					width="100%"
+					rx="4"
+				/>
+				<text
+					x="50%"
+					y="12%"
+					>{test.name}</text
+				>
+			</g>
+		</svg>
 	{/each}
 </svg>
 
 <style>
+	.chart {
+		background-color: #f5f5f5;
+		padding: 0 12px;
+	}
+
 	.legend {
 		fill: #000;
 		height: 40px;
@@ -70,15 +80,21 @@
 	}
 
 	.bar {
-		fill: #5bc0de; /* changes the background */
-		height: 40px;
-		transition: fill 0.3s ease;
+		fill: #5bc0de;
+		transition: fill .3s ease;
 		cursor: pointer;
+	}
+
+	.bar:hover,
+	.bar:focus {
+	fill: #02124B;
 	}
 
 	.bar text {
 		fill: #fff;
 		color: #fff;
 		font-size: 12px;
+		dominant-baseline: central;
+		text-anchor:middle;
 	}
 </style>
