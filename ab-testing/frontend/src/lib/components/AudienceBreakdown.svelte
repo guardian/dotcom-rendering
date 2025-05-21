330bbf2c33680e5876612f678bf425ba46e7c0b3
Imogen Hardy <imogen.hardy@guardian.co.uk>
2025-05-21 15:43:22 +0100

    basic audience breakdown as table

--- ab-testing/frontend/src/lib/components/AudienceBreakdown.svelte
+++ ab-testing/frontend/src/lib/components/AudienceBreakdown.svelte
@@ -1,64 +1,101 @@
+<script lang="ts">
+	import type { ABTest } from '../../../../types.ts';
+
+	interface Props {
+		tests: ABTest[];
+	}
+
+	const { tests }: Props = $props();
+
+	function getOffset(test: ABTest) {
+		return test.audienceOffset ?? 0;
+	}
+
+	function getSize(test: ABTest) {
+		return test.audienceSize * 100;
+	}
+
+	function testSegmentEnd(test: ABTest) {
+		return getOffset(test) + getOffset(test);
+	}
+</script>
+
 <table class="table table-bordered">
 	<thead>
 		<tr
-			><th class="audience-breakdown__ruler-division">0%</th>
-			<th class="audience-breakdown__ruler-division">25%</th>
-			<th class="audience-breakdown__ruler-division">50%</th>
-			<th class="audience-breakdown__ruler-division">75%</th>
-		</tr></thead
-	>
+			><th>0%</th>
+			<th>25%</th>
+			<th>50%</th>
+			<th>75%</th>
+		</tr>
+	</thead>
 	<tbody class="audience-breakdown__tests">
-		<tr class="audience-item">
-			<td class="audience-item__container" colspan="4">
-				&nbsp;
-				<div class="audience-item__test" style="width: 55%; left: 35%;">
-					<div class="audience-item__test-label label label-info">
-						SignInGateMainVariant
-					</div>
-					<div class="audience-item__caption">
-						<div class="audience-item__caption-test">
-							SignInGateMainVariant
+		{#each tests as test}
+			<tr class="audience-item">
+				<td class="audience-item__container" colspan="4">
+					&nbsp;
+					<div
+						class="audience-item__test"
+						style={`width: ${getSize(test)}%; left: ${getOffset(
+							test,
+						)}%;`}
+					>
+						<div class="audience-item__test-label label label-info">
+							{test.name}
 						</div>
-						<div class="audience-item__caption-range">
-							35% to 90%
-						</div>
+						<!-- <div class="audience-item__caption">
+							<div class="audience-item__caption-test">
+								{test.name}
+							</div>
+							<div class="audience-item__caption-range">
+								{`${getOffset(test)}% to ${testSegmentEnd(test)}%`}
+							</div>
+						</div> -->
 					</div>
-				</div>
-			</td>
-		</tr><tr class="audience-item">
-			<td class="audience-item__container" colspan="4">
-				&nbsp;
-				<div class="audience-item__test" style="width: 10%; left: 90%;">
-					<div class="audience-item__test-label label label-info">
-						SignInGateMainControl
-					</div>
-					<div class="audience-item__caption">
-						<div class="audience-item__caption-test">
-							SignInGateMainControl
-						</div>
-						<div class="audience-item__caption-range">
-							90% to 100%
-						</div>
-					</div>
-				</div>
-			</td>
-		</tr><tr class="audience-item">
-			<td class="audience-item__container" colspan="4">
-				&nbsp;
-				<div class="audience-item__test" style="width: 35%; left: 0%;">
-					<div class="audience-item__test-label label label-info">
-						AuxiaSignInGate
-					</div>
-					<div class="audience-item__caption">
-						<div class="audience-item__caption-test">
-							AuxiaSignInGate
-						</div>
-						<div class="audience-item__caption-range">
-							0% to 35%
-						</div>
-					</div>
-				</div>
-			</td>
-		</tr></tbody
-	>
+				</td>
+			</tr>
+		{/each}
+	</tbody>
 </table>
+
+<style>
+	table {
+		text-align: left;
+		table-layout: fixed;
+		width: 100%;
+		border-collapse: collapse;
+		border: 1px solid #ddd;
+		margin-bottom: 24px;
+	}
+
+	thead {
+		border: 1px solid #ddd;
+	}
+
+	.audience-item__container {
+		position: relative;
+		padding: 8px;
+		line-height: 1.5;
+		vertical-align: top;
+	}
+
+	.audience-item__test {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		margin: auto 0;
+	}
+
+	.audience-item__test-label {
+		background-color: #5bc0de;
+		border-radius: 4px;
+		color: #fff;
+		font-size: 75%;
+		display: block;
+		text-align: center;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		padding: 8px 3px;
+	}
+</style>
--- ab-testing/frontend/src/routes/+page.svelte
+++ ab-testing/frontend/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { ABTests } from '../../../abTest';
 	import Table from '$lib/components/Table.svelte';
+	import AudienceBreakdown from '$lib/components/AudienceBreakdown.svelte';
 
 	const clientSideTests = ABTests.filter((test) => test.type === 'client');
 	const serverSideTests = ABTests.filter((test) => test.type === 'server');
@@ -10,10 +11,12 @@
 	<h1 class="headline">A/B Tests</h1>
 	<section>
 		<h2 class="sub-headline">Client-side Tests</h2>
+		<AudienceBreakdown tests={clientSideTests} />
 		<Table tests={clientSideTests} />
 	</section>
 	<section>
 		<h2 class="sub-headline">Server-side Tests</h2>
+		<AudienceBreakdown tests={serverSideTests} />
 		<Table tests={serverSideTests} />
 	</section>
 </main>
