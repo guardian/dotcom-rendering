00654fd633429478aa8061f5118a96c9505e2792
Ravi <7014230+arelra@users.noreply.github.com>
2025-05-22 14:43:48 +0100

    MainMediaGallery uses Picture component
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>
    Co-authored-by: Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>

--- dotcom-rendering/src/components/MainMediaGallery.tsx
+++ dotcom-rendering/src/components/MainMediaGallery.tsx
@@ -1,10 +1,10 @@
 import { css } from '@emotion/react';
-import { breakpoints, from } from '@guardian/source/foundations';
+import { from } from '@guardian/source/foundations';
 import { grid } from '../grid';
 import { type ArticleFormat } from '../lib/articleFormat';
 import { getImage } from '../lib/image';
 import { type ImageBlockElement } from '../types/content';
-import { generateSources, Sources } from './Picture';
+import { Picture } from './Picture';
 
 type Props = {
 	mainMedia: ImageBlockElement;
@@ -34,29 +34,18 @@ export const MainMediaGallery = ({ mainMedia, format }: Props) => {
 		return null;
 	}
 
-	const sources = generateSources(asset.url, [
-		{ breakpoint: breakpoints.mobile, width: 480 },
-		{ breakpoint: breakpoints.mobileLandscape, width: 660 },
-		{ breakpoint: breakpoints.phablet, width: 740 },
-		{ breakpoint: breakpoints.tablet, width: 980 },
-		{ breakpoint: breakpoints.desktop, width: 1140 },
-		{ breakpoint: breakpoints.leftCol, width: 1300 },
-		{ breakpoint: breakpoints.wide, width: 1900 },
-	] as const);
-
-	const fallbackSource = sources[0];
-
 	return (
 		<figure css={styles}>
-			<picture>
-				<Sources sources={sources} />
-				<img
-					alt={mainMedia.data.alt}
-					src={fallbackSource?.lowResUrl}
-					width={fallbackSource?.width}
-					height={(fallbackSource?.width ?? 1) * 1.0}
-				/>
-			</picture>
+			<Picture
+				alt={mainMedia.data.alt ?? ''}
+				format={format}
+				role={mainMedia.role}
+				master={asset.url}
+				width={width}
+				height={height}
+				loading="eager"
+				isMainMedia={true}
+			/>
 		</figure>
 	);
 };
