6e85d80f90d7e06ed330fe7ae952fb2ee697ddda
Ravi <7014230+arelra@users.noreply.github.com>
2025-05-27 17:26:11 +0100

    Login via CODE and set GU_U cookie

--- dotcom-rendering/playwright/tests/signedin.e2e.spec.ts
+++ dotcom-rendering/playwright/tests/signedin.e2e.spec.ts
@@ -1,11 +1,8 @@
 import { expect, test } from '@playwright/test';
 import { Standard as standardArticle } from '../../fixtures/generated/fe-articles/Standard';
-import { disableCMP } from '../lib/cmp';
+import { cmpAcceptAll, disableCMP } from '../lib/cmp';
 import { waitForIsland } from '../lib/islands';
-import {
-	fetchAndloadPageWithOverrides,
-	loadPageWithOverrides,
-} from '../lib/load-page';
+import { loadPage, loadPageWithOverrides } from '../lib/load-page';
 import { isSecureServerAvailable } from '../lib/secure';
 import { signIn } from '../lib/sign-in';
 
@@ -36,22 +33,11 @@ test.describe('Signed in readers', () => {
 		page,
 	}) => {
 		const path =
-			'/commentisfree/2025/jan/14/bradford-radical-culture-city-of-culture-bronte';
+			'/Article/https://www.theguardian.com/commentisfree/2025/jan/14/bradford-radical-culture-city-of-culture-bronte';
 		const secureServerAvailable = await isSecureServerAvailable();
 		if (secureServerAvailable) {
-			await disableCMP(context);
-			await fetchAndloadPageWithOverrides(
-				page,
-				`https://www.theguardian.com${path}`,
-				{
-					configOverrides: {
-						idUrl: 'https://profile.thegulocal.com',
-					},
-				},
-				{
-					useSecure: true,
-				},
-			);
+			await loadPage({ page, path, useSecure: true });
+			await cmpAcceptAll(page);
 			await signIn(page, context, path);
 		}
 	});
