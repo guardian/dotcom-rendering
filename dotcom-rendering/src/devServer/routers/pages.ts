70712aae44b250c8cb540dd3a6cc37270b6512db
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-16 13:36:07 +0100

    Add newsletters docs to dev server
    Adds documentation for the all newsletters page, which can be rendered
    on dotcom only.

--- dotcom-rendering/src/devServer/routers/pages.ts
+++ dotcom-rendering/src/devServer/routers/pages.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { Newsletters } from '../docs/newsletters';
 import { Pages } from '../docs/pages';
 import { sendReact } from '../send';
 
 const pages = Router();
 
 pages.get('/', sendReact('Pages', Pages));
+pages.get('/newsletters', sendReact('All Newsletters', Newsletters));
 
 export { pages };
