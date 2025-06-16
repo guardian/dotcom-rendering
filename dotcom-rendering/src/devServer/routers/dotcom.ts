69304613106c386e2ebe12d61d2a4b850d596970
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-16 12:59:38 +0100

    Add fronts docs to dev server
    Adds documentation for fronts, which can be rendered on dotcom only.

--- dotcom-rendering/src/devServer/routers/dotcom.ts
+++ dotcom-rendering/src/devServer/routers/dotcom.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { Dotcom } from '../docs/dotcom';
+import { Front } from '../docs/front';
 import { sendReact } from '../send';
 
 const dotcom = Router();
 
 dotcom.get('/', sendReact('Dotcom', Dotcom));
+dotcom.get('/front', sendReact('Front', Front));
 
 export { dotcom };
