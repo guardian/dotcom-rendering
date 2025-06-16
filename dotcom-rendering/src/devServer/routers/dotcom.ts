0a24ea9540d2e514e0398736dc1e53903768f4b1
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-16 13:54:05 +0100

    Add football docs to dev server
    Adds documentation for the football pages, which can be rendered on
    dotcom only.

--- dotcom-rendering/src/devServer/routers/dotcom.ts
+++ dotcom-rendering/src/devServer/routers/dotcom.ts
@@ -1,9 +1,25 @@
 import { Router } from 'express';
 import { Dotcom } from '../docs/dotcom';
+import { FootballFixtures } from '../docs/footballFixtures';
+import { FootballLive } from '../docs/footballLive';
+import { FootballMatchSummary } from '../docs/footballMatchSummary';
+import { FootballResults } from '../docs/footballResults';
+import { FootballTables } from '../docs/footballTables';
 import { sendReact } from '../send';
 
 const dotcom = Router();
 
 dotcom.get('/', sendReact('Dotcom', Dotcom));
+dotcom.get('/football-live', sendReact('Football Live', FootballLive));
+dotcom.get(
+	'/football-fixtures',
+	sendReact('Football Fixtures', FootballFixtures),
+);
+dotcom.get('/football-results', sendReact('Football Results', FootballResults));
+dotcom.get('/football-tables', sendReact('Football Tables', FootballTables));
+dotcom.get(
+	'/football-match-summary',
+	sendReact('Football Match Summary', FootballMatchSummary),
+);
 
 export { dotcom };
