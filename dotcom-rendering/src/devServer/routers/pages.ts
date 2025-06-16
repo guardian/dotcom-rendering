556715151f34ebd8876c396b4e6fcf1061d9882e
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-16 13:15:51 +0100

    Add front docs to pages router

--- dotcom-rendering/src/devServer/routers/pages.ts
+++ dotcom-rendering/src/devServer/routers/pages.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { Pages } from '../docs/pages';
+import { Front } from '../docs/front';
 import { sendReact } from '../send';
 
 const pages = Router();
 
 pages.get('/', sendReact('Pages', Pages));
+pages.get('/front', sendReact('Front', Front));
 
 export { pages };
