0d76494fe7da2e6e1fa0c3d332b463e38e4bc912
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-24 17:51:22 +0100

    Add tag page docs to dev server (#14092)
    Adds documentation for tag pages, which can be rendered on dotcom only.

--- dotcom-rendering/src/devServer/routers/pages.ts
+++ dotcom-rendering/src/devServer/routers/pages.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { Article } from '../docs/article';
 import { Front } from '../docs/front';
 import { Pages } from '../docs/pages';
+import { TagPage } from '../docs/tagPage';
 import { sendReact } from '../send';
 
 const pages = Router();
@@ -9,5 +10,6 @@ const pages = Router();
 pages.get('/', sendReact('Pages', Pages));
 pages.get('/article', sendReact('Article', Article));
 pages.get('/front', sendReact('Front', Front));
+pages.get('/tag-page', sendReact('Tag Page', TagPage));
 
 export { pages };
