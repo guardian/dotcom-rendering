bf33a63ed077b16eee23560882ae8cf9a16a934c
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-24 16:20:41 +0100

    Add article docs to dev server (#14084)
    Adds documentation for article pages, which can be rendered on dotcom,
    the live apps, and AMP.

--- dotcom-rendering/src/devServer/routers/amp.ts
+++ dotcom-rendering/src/devServer/routers/amp.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { Amp } from '../docs/amp';
+import { Article } from '../docs/article';
 import { sendReact } from '../send';
 
 const amp = express.Router();
 
 amp.get('/', sendReact('AMP', Amp));
+amp.get('/article', sendReact('Article', Article));
 
 export { amp };
