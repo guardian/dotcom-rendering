9e65d5dcf28db3bc35c6d7bc2be04192e1e1ac98
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-26 13:32:44 +0100

    Add interactive docs to dev server (#14093)
    Adds documentation for interactive pages, which can be rendered on
    dotcom, the live apps, and AMP.

--- dotcom-rendering/src/devServer/routers/pages.ts
+++ dotcom-rendering/src/devServer/routers/pages.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { Article } from '../docs/article';
 import { Front } from '../docs/front';
+import { Interactive } from '../docs/interactive';
 import { Pages } from '../docs/pages';
 import { TagPage } from '../docs/tagPage';
 import { sendReact } from '../send';
@@ -11,5 +12,6 @@ pages.get('/', sendReact('Pages', Pages));
 pages.get('/article', sendReact('Article', Article));
 pages.get('/front', sendReact('Front', Front));
 pages.get('/tag-page', sendReact('Tag Page', TagPage));
+pages.get('/interactive', sendReact('Interactive', Interactive));
 
 export { pages };
