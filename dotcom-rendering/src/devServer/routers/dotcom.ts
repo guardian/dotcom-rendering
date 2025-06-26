19bd8e300dfe6262327e062301481911a70d4b8a
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-06-26 14:03:50 +0100

    Fix "resolve conflicts" bug

--- dotcom-rendering/src/devServer/routers/dotcom.ts
+++ dotcom-rendering/src/devServer/routers/dotcom.ts
@@ -1,25 +1,19 @@
 import { Router } from 'express';
 import { Article } from '../docs/article';
 import { Dotcom } from '../docs/dotcom';
-<<<<<<< add-newsletters-docs-to-dev-server
-import { Newsletters } from '../docs/newsletters';
-=======
 import { Front } from '../docs/front';
 import { Interactive } from '../docs/interactive';
+import { Newsletters } from '../docs/newsletters';
 import { TagPage } from '../docs/tagPage';
->>>>>>> main
 import { sendReact } from '../send';
 
 const dotcom = Router();
 
 dotcom.get('/', sendReact('Dotcom', Dotcom));
-<<<<<<< add-newsletters-docs-to-dev-server
-dotcom.get('/newsletters', sendReact('All Newsletters', Newsletters));
-=======
 dotcom.get('/article', sendReact('Article', Article));
 dotcom.get('/front', sendReact('Front', Front));
 dotcom.get('/tag-page', sendReact('Tag Page', TagPage));
 dotcom.get('/interactive', sendReact('Interactive', Interactive));
->>>>>>> main
+dotcom.get('/newsletters', sendReact('All Newsletters', Newsletters));
 
 export { dotcom };
