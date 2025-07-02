be3ca680d3203de6d040fd4566d8c63cf052de02
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-07-02 16:45:23 +0100

    Add a nav for football competitions
    When major competitions are running we'd like to introduce additional
    navigation to the football data pages that relate to that competition:
    fixtures, live matches, results and tables. This will allow users to
    navigate between different pieces of information about said competition.
    
    This change introduces a `FootballCompetitionNav` component to be
    displayed only when the `pageId` matches the given competition. For now
    this is hardcoded to the Women's Euro 2025 competition as a first
    implementation. We will make it generic and configurable for other
    competitions in a later change.

--- dotcom-rendering/src/components/FootballMatchesPage.tsx
+++ dotcom-rendering/src/components/FootballMatchesPage.tsx
@@ -12,6 +12,7 @@ import type { Result } from '../lib/result';
 import { palette } from '../palette';
 import type { FootballMatchListPageKind, Region } from '../sportDataPage';
 import { AdSlot } from './AdSlot.web';
+import { FootballCompetitionNav } from './FootballCompetitionNav';
 import { FootballCompetitionSelect } from './FootballCompetitionSelect';
 import { FootballMatchList } from './FootballMatchList';
 
@@ -57,93 +58,105 @@ export const FootballMatchesPage = ({
 	renderAds,
 	pageId,
 }: Props) => (
-	<main
-		id="maincontent"
-		data-layout="FootballDataPageLayout"
-		css={css`
-			${grid.paddedContainer}
-			position: relative;
-			${from.tablet} {
-				&::before,
-				&::after {
-					content: '';
-					position: absolute;
-					border-left: 1px solid ${palette('--article-border')};
-					top: 0;
-					bottom: 0;
-				}
-
-				&::after {
-					right: 0;
-				}
-			}
-
-			padding-bottom: ${space[9]}px;
-		`}
-	>
-		<h1
+	<>
+		<FootballCompetitionNav
+			selected={kind === 'FootballFixtures' ? 'fixtures' : 'none'}
+			pageId={pageId}
+		/>
+		<main
+			id="maincontent"
+			data-layout="FootballDataPageLayout"
 			css={css`
-				${headlineBold20}
-				padding: ${space[2]}px 0 ${space[3]}px;
-				${grid.column.centre}
-				grid-row: 1;
-				${from.leftCol} {
-					${grid.between('left-column-start', 'centre-column-end')}
-				}
-			`}
-		>
-			{createTitle(kind, edition)}
-		</h1>
-
-		<div
-			css={css`
-				margin-top: ${space[3]}px;
-				margin-bottom: ${space[6]}px;
-				${grid.column.centre}
-				grid-row: 2;
-			`}
-		>
-			<FootballCompetitionSelect
-				regions={regions}
-				kind={kind}
-				pageId={pageId}
-				onChange={goToCompetitionSpecificPage}
-			/>
-		</div>
-
-		<div
-			css={css`
-				${grid.column.centre}
-				grid-row: 3;
-				${from.leftCol} {
-					${grid.between('left-column-start', 'centre-column-end')}
-				}
+				${grid.paddedContainer}
 				position: relative;
+				${from.tablet} {
+					&::before,
+					&::after {
+						content: '';
+						position: absolute;
+						border-left: 1px solid ${palette('--article-border')};
+						top: 0;
+						bottom: 0;
+					}
+
+					&::after {
+						right: 0;
+					}
+				}
+
+				padding-bottom: ${space[9]}px;
 			`}
 		>
-			<FootballMatchList
-				now={now}
-				initialDays={initialDays}
-				edition={edition}
-				getMoreDays={getMoreDays}
-				guardianBaseUrl={guardianBaseUrl}
-				nextPageNoJsUrl={nextPageNoJsUrl}
-			/>
-		</div>
-
-		{renderAds && (
-			<div
+			<h1
 				css={css`
-					${grid.column.right}
-					/** This allows the ad to grow beyond the third row content (up to line 5) */
-					grid-row: 1 / 5;
-					${until.desktop} {
-						display: none;
+					${headlineBold20}
+					padding: ${space[2]}px 0 ${space[3]}px;
+					${grid.column.centre}
+					grid-row: 1;
+					${from.leftCol} {
+						${grid.between(
+							'left-column-start',
+							'centre-column-end',
+						)}
 					}
 				`}
 			>
-				<AdSlot position="football-right" />
+				{createTitle(kind, edition)}
+			</h1>
+
+			<div
+				css={css`
+					margin-top: ${space[3]}px;
+					margin-bottom: ${space[6]}px;
+					${grid.column.centre}
+					grid-row: 2;
+				`}
+			>
+				<FootballCompetitionSelect
+					regions={regions}
+					kind={kind}
+					pageId={pageId}
+					onChange={goToCompetitionSpecificPage}
+				/>
 			</div>
-		)}
-	</main>
+
+			<div
+				css={css`
+					${grid.column.centre}
+					grid-row: 3;
+					${from.leftCol} {
+						${grid.between(
+							'left-column-start',
+							'centre-column-end',
+						)}
+					}
+					position: relative;
+				`}
+			>
+				<FootballMatchList
+					now={now}
+					initialDays={initialDays}
+					edition={edition}
+					getMoreDays={getMoreDays}
+					guardianBaseUrl={guardianBaseUrl}
+					nextPageNoJsUrl={nextPageNoJsUrl}
+				/>
+			</div>
+
+			{renderAds && (
+				<div
+					css={css`
+						${grid.column.right}
+						/** This allows the ad to grow beyond the third row content (up to line 5) */
+						grid-row: 1 / 5;
+						${until.desktop} {
+							display: none;
+						}
+					`}
+				>
+					<AdSlot position="football-right" />
+				</div>
+			)}
+		</main>
+	</>
 );
--- dotcom-rendering/src/components/FootballTablesPage.tsx
+++ dotcom-rendering/src/components/FootballTablesPage.tsx
@@ -10,6 +10,7 @@ import { grid } from '../grid';
 import { palette } from '../palette';
 import type { Region } from '../sportDataPage';
 import { AdSlot } from './AdSlot.web';
+import { FootballCompetitionNav } from './FootballCompetitionNav';
 import { FootballTableList } from './FootballTableList';
 import { FootballTablesCompetitionSelect } from './FootballTablesCompetitionSelect.importable';
 import { Island } from './Island';
@@ -29,87 +30,96 @@ export const FootballTablesPage = ({
 	renderAds,
 	guardianBaseUrl,
 }: Props) => (
-	<main
-		id="maincontent"
-		data-layout="FootballDataPageLayout"
-		css={css`
-			${grid.paddedContainer}
-			position: relative;
-			${from.tablet} {
-				&::before,
-				&::after {
-					content: '';
-					position: absolute;
-					border-left: 1px solid ${palette('--article-border')};
-					top: 0;
-					bottom: 0;
-				}
-
-				&::after {
-					right: 0;
-				}
-			}
-
-			padding-bottom: ${space[9]}px;
-		`}
-	>
-		<h1
+	<>
+		<FootballCompetitionNav selected="tables" pageId={pageId} />
+		<main
+			id="maincontent"
+			data-layout="FootballDataPageLayout"
 			css={css`
-				${headlineBold20}
-				padding: ${space[2]}px 0 ${space[3]}px;
-				${grid.column.centre}
-				grid-row: 1;
-				${from.leftCol} {
-					${grid.between('left-column-start', 'centre-column-end')}
-				}
-			`}
-		>
-			Football tables
-		</h1>
-		<div
-			css={css`
-				margin-top: ${space[3]}px;
-				margin-bottom: ${space[6]}px;
-				${grid.column.centre}
-				grid-row: 2;
-			`}
-		>
-			<Island priority="feature" defer={{ until: 'visible' }}>
-				<FootballTablesCompetitionSelect
-					regions={regions}
-					pageId={pageId}
-					guardianBaseUrl={guardianBaseUrl}
-				/>
-			</Island>
-		</div>
-		<div
-			css={css`
-				${grid.column.centre}
-				grid-row: 3;
-				${from.leftCol} {
-					${grid.between('left-column-start', 'centre-column-end')}
-				}
+				${grid.paddedContainer}
 				position: relative;
+				${from.tablet} {
+					&::before,
+					&::after {
+						content: '';
+						position: absolute;
+						border-left: 1px solid ${palette('--article-border')};
+						top: 0;
+						bottom: 0;
+					}
+
+					&::after {
+						right: 0;
+					}
+				}
+
+				padding-bottom: ${space[9]}px;
 			`}
 		>
-			<FootballTableList
-				competitions={competitions}
-				guardianBaseUrl={guardianBaseUrl}
-			/>
-		</div>
-		{renderAds && (
-			<div
+			<h1
 				css={css`
-					${grid.column.right}
-					/** This allows the ad to grow beyond the third row content (up to line 5) */
-					grid-row: 1 / 4;
-					${until.desktop} {
-						display: none;
+					${headlineBold20}
+					padding: ${space[2]}px 0 ${space[3]}px;
+					${grid.column.centre}
+					grid-row: 1;
+					${from.leftCol} {
+						${grid.between(
+							'left-column-start',
+							'centre-column-end',
+						)}
 					}
 				`}
 			>
-				<AdSlot position="football-right" />
+				Football tables
+			</h1>
+			<div
+				css={css`
+					margin-top: ${space[3]}px;
+					margin-bottom: ${space[6]}px;
+					${grid.column.centre}
+					grid-row: 2;
+				`}
+			>
+				<Island priority="feature" defer={{ until: 'visible' }}>
+					<FootballTablesCompetitionSelect
+						regions={regions}
+						pageId={pageId}
+						guardianBaseUrl={guardianBaseUrl}
+					/>
+				</Island>
 			</div>
-		)}
-	</main>
+			<div
+				css={css`
+					${grid.column.centre}
+					grid-row: 3;
+					${from.leftCol} {
+						${grid.between(
+							'left-column-start',
+							'centre-column-end',
+						)}
+					}
+					position: relative;
+				`}
+			>
+				<FootballTableList
+					competitions={competitions}
+					guardianBaseUrl={guardianBaseUrl}
+				/>
+			</div>
+			{renderAds && (
+				<div
+					css={css`
+						${grid.column.right}
+						/** This allows the ad to grow beyond the third row content (up to line 5) */
+						grid-row: 1 / 4;
+						${until.desktop} {
+							display: none;
+						}
+					`}
+				>
+					<AdSlot position="football-right" />
+				</div>
+			)}
+		</main>
+	</>
 );
