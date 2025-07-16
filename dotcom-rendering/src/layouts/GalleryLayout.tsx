95916a30d42d45a752555bc6cf65685abe0b1934
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2025-07-16 13:25:41 +0100

    add gallery lightbox

--- dotcom-rendering/src/layouts/GalleryLayout.tsx
+++ dotcom-rendering/src/layouts/GalleryLayout.tsx
@@ -200,6 +200,7 @@ export const GalleryLayout = (props: WebProps | AppProps) => {
 						pageId={frontendData.pageId}
 						webTitle={frontendData.webTitle}
 						key={idx}
+						renderingTarget={props.renderingTarget}
 					/>
 				))}
 				<SubMeta
