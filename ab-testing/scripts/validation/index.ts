43bef8e53dd991afd77e12417defff2f0d9953a9
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2025-07-17 15:03:57 +0100

    validate test size and offset

--- ab-testing/scripts/validation/index.ts
+++ ab-testing/scripts/validation/index.ts
@@ -2,6 +2,7 @@ import { ABTests } from '../../abTest.ts';
 import { ABTest } from '../../types.ts';
 import { limitServerSideTests } from './limitServerSide.ts';
 import { allExpirationsValid } from './validExpiration.ts';
+import { validSizeOffset } from './validSizeOffset.ts';
 import { noVariantOverlap } from './variantOverlap.ts';
 
 type ValidationFunction = (tests: ABTest[]) => boolean;
@@ -10,6 +11,7 @@ const rules: ValidationFunction[] = [
 	noVariantOverlap,
 	limitServerSideTests,
 	allExpirationsValid,
+	validSizeOffset,
 ];
 
 function validateTests(testList: ABTest[]) {
