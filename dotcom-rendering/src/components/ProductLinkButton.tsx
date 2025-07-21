204644cbd3d9d141e41a5d6343fce25ef818835f
oliverabrahams <ollie.abrahams@guardian.co.uk>
2025-07-21 15:08:20 +0100

    set max width and make the button text wrap

--- dotcom-rendering/src/components/ProductLinkButton.tsx
+++ dotcom-rendering/src/components/ProductLinkButton.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import {
 	LinkButton,
 	SvgArrowRightStraight,
@@ -7,6 +8,13 @@ type ProductLinkButtonProps = {
 	label: string;
 	url: string;
 };
+
+const linkButtonStyles = css`
+	max-width: 100%;
+	height: fit-content;
+	white-space: normal;
+	overflow-wrap: break-word;
+`;
 
 export const ProductLinkButton = ({ label, url }: ProductLinkButtonProps) => {
 	return (
@@ -20,6 +28,7 @@ export const ProductLinkButton = ({ label, url }: ProductLinkButtonProps) => {
 			data-ignore="global-link-styling"
 			data-link-name="in body link"
 			data-spacefinder-role="inline"
+			cssOverrides={[linkButtonStyles]}
 		>
 			{label}
 		</LinkButton>
