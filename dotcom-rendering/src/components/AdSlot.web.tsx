f2c80c1b0805502f2d6a595bc825a99de969b498
Dina Hafez <dina.hafez@guardian.co.uk>
2025-07-24 16:42:01 +0100

    Add dark ad slot background and ad label which still wip

--- dotcom-rendering/src/components/AdSlot.web.tsx
+++ dotcom-rendering/src/components/AdSlot.web.tsx
@@ -426,10 +426,6 @@ const crosswordBannerMobileAdStyles = css`
 	min-height: ${getMinHeight(adSizes.mobilesticky.height)}px;
 `;
 
-const galleryInlineAdStyles = css`
-	min-height: ${getMinHeight(adSizes.mpu.height)}px;
-`;
-
 const galleryInlineAdContainerStyles = css`
 	${until.tablet} {
 		margin: 0 auto;
@@ -442,6 +438,13 @@ const galleryInlineAdMobileContainerStyles = css`
 		margin: 0 auto;
 		display: none;
 	}
+`;
+
+const galleryInlineAdStyles = css`
+	.ad-slot--dark {
+		background-color: ${schemedPalette('--ad-background-article-inner')};
+	}
+	min-height: ${getMinHeight(adSizes.mpu.height)}px;
 `;
 
 /*** The top-above-nav-mobile and inline slots label should be dark even in light mode.
