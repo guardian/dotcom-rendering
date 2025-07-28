e9c1846c243159ad04e965a5296eef66b2cd52fd
Ravi <7014230+arelra@users.noreply.github.com>
2025-07-28 15:42:57 +0100

    Update imports paths to standard syntax

--- dotcom-rendering/src/components/ArticleTitle.stories.tsx
+++ dotcom-rendering/src/components/ArticleTitle.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react/*';
+import type { Meta, StoryObj } from '@storybook/react';
 import { leftColumnDecorator } from '../../.storybook/decorators/gridDecorators';
 import { defaultFormats } from '../../.storybook/decorators/splitThemeDecorator';
 import { allModes } from '../../.storybook/modes';
