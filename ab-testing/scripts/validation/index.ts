f5d3547daec30345e780c55a5a6bd9b47122a928
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2025-07-28 10:45:43 +0100

    also remove overlap validation

--- ab-testing/scripts/validation/index.ts
+++ ab-testing/scripts/validation/index.ts
@@ -2,15 +2,10 @@ import { ABTests } from '../../abTest.ts';
 import { ABTest } from '../../types.ts';
 import { limitServerSideTests } from './limitServerSide.ts';
 import { allExpirationsValid } from './validExpiration.ts';
-import { noVariantOverlap } from './variantOverlap.ts';
 
 type ValidationFunction = (tests: ABTest[]) => boolean;
 
-const rules: ValidationFunction[] = [
-	noVariantOverlap,
-	limitServerSideTests,
-	allExpirationsValid,
-];
+const rules: ValidationFunction[] = [limitServerSideTests, allExpirationsValid];
 
 function validateTests(testList: ABTest[]) {
 	return rules.every((rule) => rule(testList));
