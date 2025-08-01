9e0dee13fd25e8230cc47f99fb2333c22d50e05d
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2025-08-01 15:10:39 +0100

    swap example tests for sample tests

--- ab-testing/abTest.ts
+++ ab-testing/abTest.ts
@@ -1,8 +1,8 @@
 import { ABTest } from './types.ts';
 
 /**
- * Tests are defined here. They will be assigned mvt ranges based on their index in the array, the
- * size of the test and the number of groups.
+ * Tests are defined here. They will be assigned mvt ranges based on the
+ * size of the test and the number of groups, these ranges may not be contiguous.
  *
  * For 100% tests to run concurrently with other tests, they should be assigned to different
  * test spaces. This means that some users will be in multiple tests at the same time.
@@ -20,32 +20,31 @@ import { ABTest } from './types.ts';
  */
 
 export const ABTests: ABTest[] = [
-	// Example client side AB test definition
+	// Sample tests that will be used for testing the AB testing framework
 	{
-		name: 'commercial-ad-block-ask',
+		name: 'commercial-client-side-test',
 		description:
 			'Show new ad block ask component in ad slots when we detect ad blocker usage',
 		owners: ['commercial.dev@guardian.co.uk'],
 		status: 'ON',
-		expirationDate: new Date('2025-09-02'),
+		expirationDate: '2050-12-30',
 		type: 'client',
 		audienceSize: 10 / 100,
 		groups: ['control', 'variant'],
 	},
 	{
-		name: 'commercial-ad-block-ask-4',
+		name: 'commercial-server-side-test',
 		description:
 			'Show new ad block ask component in ad slots when we detect ad blocker usage',
 		owners: ['commercial.dev@guardian.co.uk'],
 		status: 'ON',
-		expirationDate: new Date('2025-09-02'),
-		type: 'client',
+		expirationDate: '2050-12-30',
+		type: 'server',
 		audienceSize: 10 / 100,
 		groups: ['control', 'variant'],
 	},
-	// Example server side AB test definition
 	{
-		name: 'webex-europe-beta-front',
+		name: 'commercial-large-overlap-test',
 		description:
 			'Allows viewing the beta version of the Europe network front',
 		owners: [
@@ -53,38 +52,10 @@ export const ABTests: ABTest[] = [
 			'dotcom.platform@theguardian.com',
 		],
 		status: 'ON',
-		expirationDate: new Date('2025-09-04'),
+		expirationDate: '2050-12-30',
 		type: 'server',
-		audienceSize: 100 / 100,
+		audienceSize: 50 / 100,
 		groups: ['control', 'variant'],
 		audienceSpace: 'B',
-	},
-	{
-		name: 'commercial-some-test-2',
-		description:
-			'Allows viewing the beta version of the Europe network front',
-		owners: [
-			'project.fairground@theguardian.com',
-			'dotcom.platform@theguardian.com',
-		],
-		status: 'ON',
-		expirationDate: new Date('2025-09-04'),
-		type: 'server',
-		audienceSize: 10 / 100,
-		groups: ['control', 'variant'],
-	},
-	{
-		name: 'commercial-some-test',
-		description:
-			'Allows viewing the beta version of the Europe network front',
-		owners: [
-			'project.fairground@theguardian.com',
-			'dotcom.platform@theguardian.com',
-		],
-		status: 'ON',
-		expirationDate: new Date('2025-09-04'),
-		type: 'server',
-		audienceSize: 10 / 100,
-		groups: ['control', 'variant'],
 	},
 ];
