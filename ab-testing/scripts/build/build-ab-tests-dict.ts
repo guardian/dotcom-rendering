aedfaab63d393a172935cf7aa0c34cf45457ff0e
Imogen Hardy <imogen.hardy@guardian.co.uk>
2025-09-11 10:19:38 +0100

    small code tweak, amend args required by deploy actions

--- ab-testing/scripts/build/build-ab-tests-dict.ts
+++ ab-testing/scripts/build/build-ab-tests-dict.ts
@@ -2,18 +2,14 @@ import { ABTest } from '../../types.ts';
 import { stringifyFastlySubfield } from '../lib/fastly-subfield.ts';
 
 const buildABTestGroupKeyValues = (tests: ABTest[]) =>
-	tests
-		.map((test) =>
-			test.groups.map((group) => ({
-				item_key: `${test.name}:${group}`,
-				item_value: stringifyFastlySubfield({
-					exp: Math.floor(
-						new Date(test.expirationDate).getTime() / 1000,
-					),
-					type: test.type,
-				}),
-			})),
-		)
-		.flat();
+	tests.flatMap((test) =>
+		test.groups.map((group) => ({
+			item_key: `${test.name}:${group}`,
+			item_value: stringifyFastlySubfield({
+				exp: Math.floor(new Date(test.expirationDate).getTime() / 1000),
+				type: test.type,
+			}),
+		})),
+	);
 
 export { buildABTestGroupKeyValues };
