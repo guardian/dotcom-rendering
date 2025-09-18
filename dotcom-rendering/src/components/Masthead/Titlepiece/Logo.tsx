070c22fc3697492445936b8fbdeae3e4d348f808
Dominik Lander <dominik.lander@guardian.co.uk>
2025-09-18 15:00:46 +0100

    Refactor Logo component

--- dotcom-rendering/src/components/Masthead/Titlepiece/Logo.tsx
+++ dotcom-rendering/src/components/Masthead/Titlepiece/Logo.tsx
@@ -1,22 +1,109 @@
-/**
- * @file
- * This file was largely copied from src/components/Logo.tsx
- */
 import { css } from '@emotion/react';
-import { visuallyHidden } from '@guardian/source/foundations';
+import { from, space, visuallyHidden } from '@guardian/source/foundations';
 import { SvgGuardianLogo } from '@guardian/source/react-components';
 import { nestedOphanComponents } from '../../../lib/ophan-helpers';
 import { palette } from '../../../palette';
 
-export const Logo = () => (
-	<a href="/" data-link-name={nestedOphanComponents('header', 'logo')}>
-		<span
-			css={css`
-				${visuallyHidden};
-			`}
-		>
-			The Guardian - Back to home
-		</span>
-		<SvgGuardianLogo textColor={palette('--masthead-nav-link-text')} />
-	</a>
+const gridMainColumn = css`
+	grid-column: main-column-start / main-column-end;
+`;
+
+const veggieBurgerDiameter = 40;
+
+const logoStyles = css`
+	${gridMainColumn}
+	grid-row: 1;
+	position: relative;
+	display: flex;
+	justify-self: end;
+	align-self: end;
+	margin-top: ${space[2]}px;
+	margin-bottom: 6px;
+	right: ${veggieBurgerDiameter + space[3]}px;
+	${from.mobileMedium} {
+		right: 0;
+	}
+	${from.mobileLandscape} {
+		margin-bottom: ${space[2]}px;
+	}
+
+	svg {
+		width: 152px;
+		${from.mobileMedium} {
+			width: 207px;
+		}
+		${from.tablet} {
+			width: 252px;
+		}
+		${from.desktop} {
+			width: 291px;
+		}
+	}
+`;
+
+const logoStylesFromLeftCol = css`
+	svg {
+		${from.leftCol} {
+			width: 324px;
+		}
+	}
+`;
+
+const slimNavLogoOverrides = css`
+	position: relative;
+	margin-top: ${space[2]}px;
+	margin-bottom: ${space[2]}px;
+	right: ${veggieBurgerDiameter + 6}px;
+
+	${from.mobileLandscape} {
+		margin-top: ${space[1]}px;
+		margin-bottom: ${space[2]}px;
+	}
+	${from.tablet} {
+		right: ${space[8]}px;
+	}
+	${from.desktop} {
+		right: ${space[10]}px;
+	}
+	svg {
+		width: 130px;
+		${from.tablet} {
+			width: 86px;
+		}
+		${from.desktop} {
+			width: 130px;
+		}
+		${from.leftCol} {
+			width: 140px;
+		}
+		${from.wide} {
+			width: 145px;
+		}
+	}
+`;
+
+type Props = {
+	hasPageSkin: boolean;
+	showSlimNav: boolean;
+};
+
+export const Logo = ({ hasPageSkin, showSlimNav }: Props) => (
+	<div
+		css={[
+			logoStyles,
+			!hasPageSkin && logoStylesFromLeftCol,
+			showSlimNav && slimNavLogoOverrides,
+		]}
+	>
+		<a href="/" data-link-name={nestedOphanComponents('header', 'logo')}>
+			<span
+				css={css`
+					${visuallyHidden};
+				`}
+			>
+				The Guardian - Back to home
+			</span>
+			<SvgGuardianLogo textColor={palette('--masthead-nav-link-text')} />
+		</a>
+	</div>
 );
--- dotcom-rendering/src/components/Titlepiece.importable.tsx
+++ dotcom-rendering/src/components/Titlepiece.importable.tsx
@@ -35,8 +35,6 @@ interface Props {
 	pageId?: string;
 }
 
-const veggieBurgerDiameter = 40;
-
 const gridContent = css`
 	grid-column: content-start / content-end;
 `;
@@ -57,87 +55,6 @@ const editionSwitcherMenuStyles = css`
 const slimNavEditionSwitcherOverrides = css`
 	${until.tablet} {
 		justify-self: start;
-	}
-`;
-
-const logoStyles = css`
-	${gridMainColumn}
-	grid-row: 1;
-	position: relative;
-	display: flex;
-	justify-self: end;
-	align-self: end;
-	margin-top: ${space[2]}px;
-	margin-bottom: 6px;
-	right: ${veggieBurgerDiameter + space[3]}px;
-	${from.mobileMedium} {
-		right: 0;
-	}
-	${from.mobileLandscape} {
-		margin-bottom: ${space[2]}px;
-	}
-
-	svg {
-		width: 152px;
-		${from.mobileMedium} {
-			width: 207px;
-		}
-		${from.tablet} {
-			width: 252px;
-		}
-		${from.desktop} {
-			width: 291px;
-		}
-	}
-`;
-
-const logoStylesFromLeftCol = css`
-	svg {
-		${from.leftCol} {
-			width: 324px;
-		}
-	}
-`;
-
-const slimNavLogoOverrides = css`
-	position: relative;
-	margin-top: ${space[2]}px;
-	margin-bottom: ${space[2]}px;
-	right: ${veggieBurgerDiameter + 6}px;
-
-	${from.mobile} {
-		right: ${veggieBurgerDiameter + 6}px;
-	}
-	${from.mobileMedium} {
-		right: ${veggieBurgerDiameter + 6}px;
-	}
-	${from.mobileLandscape} {
-		margin-top: ${space[1]}px;
-		margin-bottom: ${space[2]}px;
-	}
-	${from.tablet} {
-		right: ${space[8]}px;
-	}
-	${from.desktop} {
-		right: ${space[10]}px;
-	}
-	svg {
-		width: 130px;
-		${from.mobile} {
-			width: 130px;
-		}
-		${from.tablet} {
-			width: 86px;
-		}
-		${from.desktop} {
-			width: 130px;
-		}
-		${from.leftCol} {
-			width: 140px;
-		}
-		${from.wide} {
-			width: 145px;
-		}
 	}
 `;
 
@@ -502,15 +419,7 @@ export const Titlepiece = ({
 			</div>
 
 			{/* Guardian logo */}
-			<div
-				css={[
-					logoStyles,
-					!hasPageSkin && logoStylesFromLeftCol,
-					showSlimNav && slimNavLogoOverrides,
-				]}
-			>
-				<Logo />
-			</div>
+			<Logo hasPageSkin={!!hasPageSkin} showSlimNav={!!showSlimNav} />
 
 			{/** Expanded menu checkbox */}
 			<input
