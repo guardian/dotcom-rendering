62544dad7c03c924fe037ea77be813619a2c7a66
Charlotte <charlotte.emms@theguardian.com>
2025-09-19 15:26:40 +0100

    Indroduce LabsSectionHeader component for use in FrontSection for labs content, using grid layout

--- dotcom-rendering/src/components/FrontSection.tsx
+++ dotcom-rendering/src/components/FrontSection.tsx
@@ -322,7 +322,6 @@ const sectionHeadlineFromLeftCol = (borderColour: string) => css`
 			display: block;
 			width: 1px;
 			top: 0;
-			height: 1.875rem;
 			right: -10px;
 			position: absolute;
 			background-color: ${borderColour};
