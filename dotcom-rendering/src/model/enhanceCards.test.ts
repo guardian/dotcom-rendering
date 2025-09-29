ad5056119d5a135d4f05b23ed94d734a79e348d5
fweddi <freddiepreece@outlook.com>
2025-09-29 11:57:36 +0100

    Split out tests

--- dotcom-rendering/src/model/enhanceCards.test.ts
+++ dotcom-rendering/src/model/enhanceCards.test.ts
@@ -6,13 +6,6 @@ describe('Enhance Cards', () => {
 		const videoReplace = true;
 		const assets: FEMediaAsset[] = [
 			{
-				id: 'https://guim-example.co.uk/atomID-1.vtt',
-				version: 1,
-				platform: 'Url',
-				mimeType: 'text/vtt',
-				assetType: 'Subtitles',
-			},
-			{
 				id: 'https://guim-example.co.uk/atomID-1.mp4',
 				version: 1,
 				platform: 'Url',
@@ -24,6 +17,66 @@ describe('Enhance Cards', () => {
 				version: 1,
 				platform: 'Url',
 				mimeType: 'application/x-mpegURL',
+				assetType: 'Video',
+			},
+		];
+		const mediaAtom: FEMediaAtom = {
+			id: 'atomID',
+			assets,
+			title: 'Example video',
+			duration: 15,
+			source: '',
+			posterImage: { allImages: [] },
+			trailImage: { allImages: [] },
+			expired: false,
+			activeVersion: 1,
+		};
+		const cardTrailImage = '';
+
+		expect(
+			getActiveMediaAtom(videoReplace, mediaAtom, cardTrailImage),
+		).toEqual({
+			atomId: 'atomID',
+			duration: 15,
+			height: 400,
+			image: '',
+			type: 'LoopVideo',
+			sources: [
+				{
+					mimeType: 'application/x-mpegURL',
+					src: 'https://guim-example.co.uk/atomID-1.m3u8',
+				},
+				{
+					mimeType: 'video/mp4',
+					src: 'https://guim-example.co.uk/atomID-1.mp4',
+				},
+			],
+			width: 500,
+		});
+	});
+
+	it('filters out non-video assets', () => {
+		const videoReplace = true;
+		const assets: FEMediaAsset[] = [
+			{
+				id: 'https://guim-example.co.uk/atomID-1.vtt',
+				version: 1,
+				platform: 'Url',
+				mimeType: 'text/vtt',
+				assetType: 'Subtitles',
+			},
+			{
+				id: 'https://guim-example.co.uk/atomID-1.m3u8',
+				version: 1,
+				platform: 'Url',
+				mimeType: 'application/x-mpegURL',
+				assetType: 'Video',
+			},
+			{
+				id: 'https://guim-example.co.uk/atomID-1.mp4',
+				version: 1,
+				platform: 'Url',
+				mimeType: 'video/mp4',
 				assetType: 'Video',
 			},
 		];
