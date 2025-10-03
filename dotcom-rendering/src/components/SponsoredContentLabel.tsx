64456a1df63ac04aac33fca3873325ab97169d9a
Charlotte <charlotte.emms@theguardian.com>
2025-10-03 14:09:09 +0100

    ensure logo is vertically centered within link area of set height

--- dotcom-rendering/src/components/SponsoredContentLabel.tsx
+++ dotcom-rendering/src/components/SponsoredContentLabel.tsx
@@ -58,6 +58,10 @@ const linkStyles = css`
 	height: 60px;
 	/* See: https://css-tricks.com/nested-links/ */
 	z-index: ${getZIndex('card-nested-link')};
+	/** Vertically center the branding within the link */
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 `;
 
 /**
