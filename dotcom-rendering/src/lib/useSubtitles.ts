45e6bf042d4159fc925a5bfa5293b4eedd1bd802
Anna Beddow <anna.beddow@gmail.com>
2025-10-15 11:53:45 +0100

    Add a useeffect to extra the track from the video element.

--- dotcom-rendering/src/lib/useSubtitles.ts
+++ dotcom-rendering/src/lib/useSubtitles.ts
@@ -1,3 +1,5 @@
+import { text } from 'body-parser';
+import { useEffect, useState } from 'react';
 import type { PlayerStates } from 'src/components/LoopVideoPlayer';
 
 type Props = {
@@ -6,4 +8,21 @@ type Props = {
 	currentTime: number;
 };
 
-export const useSubtitles = ({ video, playerState, currentTime }: Props) => {};
+export const useSubtitles = ({ video, playerState, currentTime }: Props) => {
+	const [activeTrack, setActiveTrack] = useState<TextTrack | null>(null);
+	const [subtitlesAvailable, setSubtitlesAvailable] = useState(false);
+
+	useEffect(() => {
+		if (!video) return;
+
+		const textTracks = video.textTracks;
+		console.log('>>> Text Tracks', textTracks);
+
+		const setTrackFromList = () => {
+			// We currently only support one text track per video, so we are ok to acces [0] here. If we added additional languages, this will need updating.
+			if (!textTracks[0]) return;
+			setActiveTrack(textTracks[0]);
+			console.log(textTracks[0]);
+		};
+	}, [video]);
+};
