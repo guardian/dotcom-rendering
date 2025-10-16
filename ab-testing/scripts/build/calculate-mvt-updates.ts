d10f226765ab9fa90d4cf051c5bc74a43e7bea68
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2025-10-16 12:08:50 +0100

    remove expiry method

--- ab-testing/scripts/build/calculate-mvt-updates.ts
+++ ab-testing/scripts/build/calculate-mvt-updates.ts
@@ -22,9 +22,7 @@ export const calculateSpaceUpdates = (
 					name: test.name,
 					type: test.type,
 					audienceSize: test.audienceSize / test.groups.length,
-					// If the test is OFF we set the expiration to 0 so it is not served by Fastly
-					expirationDate:
-						test.status === 'OFF' ? 0 : test.expirationDate,
+					expirationDate: test.expirationDate,
 					group,
 				},
 			]),
