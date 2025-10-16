40658617ea3cc4b0b6b74274a34044a0ae587ae9
Anna Beddow <anna.beddow@gmail.com>
2025-10-16 10:08:00 +0100

    Track views on cards and demote unengaged cards

--- dotcom-rendering/src/lib/personaliseHighlights.ts
+++ dotcom-rendering/src/lib/personaliseHighlights.ts
@@ -1,19 +1,23 @@
 import { storage } from '@guardian/libs';
 import type { DCRFrontCard } from '../types/front';
 
-export type HighlightsArticleHistory = Array<DCRFrontCard>;
+type HighlightView = {
+	id: string;
+	count: number;
+};
+
+export type HighlightsViewHistory = Array<HighlightView>;
+
+export type OrderedHighlights = Array<DCRFrontCard>;
 
 export const HighlightOrderKey = 'gu.history.highlightsOrder';
+export const ViewedHighlights = 'gu.history.highlightsViewed';
 
-const isValidHistory = (
-	history: unknown,
-): history is HighlightsArticleHistory =>
+const isValidHistory = (history: unknown): history is OrderedHighlights =>
 	Array.isArray(history) && history.every((card) => typeof card === 'object');
 
 // Returns undefined if no highlight articles have been clicked and stored in local storage
-export const getHighlightClickHistory = ():
-	| HighlightsArticleHistory
-	| undefined => {
+export const getHighlightClickHistory = (): OrderedHighlights | undefined => {
 	try {
 		const dailyCount = storage.local.get(HighlightOrderKey);
 
@@ -29,10 +33,8 @@ export const getHighlightClickHistory = ():
 	}
 };
 
-export const setHistoryInLocalStorage = (
-	history: HighlightsArticleHistory,
-): void => {
-	storage.local.set(HighlightOrderKey, history);
+export const setHistoryInLocalStorage = (order: OrderedHighlights): void => {
+	storage.local.set(HighlightOrderKey, order);
 };
 
 export const storeHighlightArticleVisit = (
@@ -56,4 +58,60 @@ export const storeHighlightArticleVisit = (
 	storage.local.set(HighlightOrderKey, newOrder);
 };
 
-//
+const trackViewedCard = (
+	cardId: string,
+	history: Array<{ id: string; count: number }>,
+) => {
+	if (history.find((el) => el.id == cardId)) {
+		return history.map((el) =>
+			el.id == cardId ? { ...el, count: (el.count += 1) } : el,
+		);
+	} else {
+		return history.push({ id: cardId, count: 1 });
+	}
+};
+
+export const manageCardsWithoutViews = (highlights: DCRFrontCard[]): void => {
+	const newViewHistory = getViewsWithoutClick(highlights);
+	const newHighlights = demoteCardsWithoutClicks(newViewHistory, highlights);
+	storage.local.set(ViewedHighlights, newViewHistory);
+	storage.local.set(HighlightOrderKey, newHighlights);
+};
+
+export const getViewsWithoutClick = (
+	highlights: OrderedHighlights,
+): HighlightsViewHistory => {
+	const viewHistory = storage.local.get(ViewedHighlights) as
+		| HighlightsViewHistory
+		| undefined;
+	const unviewedCards = highlights.slice(0, 2);
+	const newViewHistory = viewHistory ? [...viewHistory] : [];
+
+	if (unviewedCards.length === 0) return viewHistory ?? [];
+
+	for (const card of unviewedCards) {
+		trackViewedCard(card.url, newViewHistory);
+	}
+	return newViewHistory;
+};
+
+export const demoteCardsWithoutClicks = (
+	viewHistory: HighlightsViewHistory,
+	highlights: OrderedHighlights,
+) => {
+	const demotedCards = viewHistory
+		.filter((card) => card.count >= 2)
+		.map((card) => card.id);
+	// if no cards have been ignored 2 times or more, dont worry, we don't need to do anything
+	if (demotedCards.length === 0) return;
+	// otherwise, move the demoted cards to the back of the highlights container
+	const newHighlights = highlights.reduce((acc: DCRFrontCard[], card) => {
+		if (demotedCards.includes(card.url)) {
+			acc.push(card);
+		} else {
+			acc.unshift(card);
+		}
+		return acc;
+	}, []);
+	return newHighlights;
+};
