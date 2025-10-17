92b8dfd524f3c573da5d48f669393f415f3ebc8b
Ravi <7014230+arelra@users.noreply.github.com>
2025-10-17 15:35:56 +0100

    Fix linting
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>
    
    Co-authored-by: Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>

--- dotcom-rendering/scripts/perf/schema-validation/validate-parse.ts
+++ dotcom-rendering/scripts/perf/schema-validation/validate-parse.ts
@@ -1,9 +1,8 @@
-import { safeParse } from 'valibot'; // adjust path
-import { FEArticleSchema } from '../../../src/frontend/feArticle';
-import { validateArticle } from '../../../src/model/validate';
-import articleJson from './article.json' with { type: 'json' };
+// import { safeParse } from 'valibot'; // adjust path
+// import { FEArticleSchema } from '../../../src/frontend/feArticle';
+// import { validateArticle } from '../../../src/model/validate';
+// import articleJson from './article.json' with { type: 'json' };
 
-// @ts-expect-error -- ignore
-Deno.bench('Valibot', () => safeParse(FEArticleSchema, articleJson));
-// @ts-expect-error -- ignore
-Deno.bench('AJV', () => validateArticle(articleJson));
+// Deno.bench('Valibot', () => safeParse(FEArticleSchema, articleJson));
+// // @ts-expect-error -- ignore
+// Deno.bench('AJV', () => validateArticle(articleJson));
