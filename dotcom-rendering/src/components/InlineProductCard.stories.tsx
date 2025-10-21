83290f38f8f957e03e8c3fad628c803ad4e7ba86
Charley_Campbell <charley.campbell@guardian.co.uk>
2025-10-21 15:35:17 +0100

    fixing prettier build

--- dotcom-rendering/src/components/InlineProductCard.stories.tsx
+++ dotcom-rendering/src/components/InlineProductCard.stories.tsx
@@ -93,7 +93,7 @@ const sampleProductCard: InlineProductCardProps = {
 	customAttributes: [
 		{ name: 'What we love', value: 'It packs away pretty small' },
 		{
-			name: 'What we don\'t love',
+			name: "What we don't love",
 			value: 'There’s nowhere to stow the remote control',
 		},
 	],
