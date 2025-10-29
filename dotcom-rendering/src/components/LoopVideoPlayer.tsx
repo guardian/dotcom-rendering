f9b4516a8177b0005cb85c005e45d66b43764d12
Anna Beddow <anna.beddow@gmail.com>
2025-10-29 14:15:11 +0000

    Hide cues by default

--- dotcom-rendering/src/components/LoopVideoPlayer.tsx
+++ dotcom-rendering/src/components/LoopVideoPlayer.tsx
@@ -37,6 +37,9 @@ const videoStyles = (
 	object-fit: cover;
 
 	::cue {
+		/* Hide the cue by default as we prefer custom overlay */
+		visibility: hidden;
+
 		color: ${palette('--loop-video-subtitle-text')};
 		${subtitleSize === 'small' && textSans15};
 		${subtitleSize === 'medium' && textSans17};
