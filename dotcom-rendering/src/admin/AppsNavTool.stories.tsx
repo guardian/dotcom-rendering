47b9192ea1b0e5ce63f0757978a1c23b92871d9a
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-11-04 13:06:56 +0000

    Fix storybook 9 import

--- dotcom-rendering/src/admin/AppsNavTool.stories.tsx
+++ dotcom-rendering/src/admin/AppsNavTool.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import { parse } from 'valibot';
 import { ukNav } from '../../fixtures/manual/appsNav/uk';
 import { AppsNavSchema } from './appsNav';
