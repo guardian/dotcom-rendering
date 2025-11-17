8661b688b112efe11bca05b5d704d427f5a42b6d
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2025-11-17 17:04:42 +0000

    fix lint

--- ab-testing/eslint.config.mjs
+++ ab-testing/eslint.config.mjs
@@ -2,7 +2,12 @@ import guardian from "@guardian/eslint-config";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-	globalIgnores(["frontend", "eslint.config.mjs", "index.ts"]),
+	globalIgnores([
+		"frontend",
+		"eslint.config.mjs",
+		"index.ts",
+		"dictionary-deploy-lambda/dist",
+	]),
 	...guardian.configs.recommended,
 	{
 		languageOptions: {
