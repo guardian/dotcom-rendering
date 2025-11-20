2ac5ba65a36ff8076ccd169944474a9529a35a86
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2025-11-20 09:32:39 +0000

    cdk location

--- ab-testing/cdk/lib/deploymentLambda.test.ts
+++ ab-testing/cdk/lib/deploymentLambda.test.ts
@@ -3,7 +3,7 @@ import { snapshot } from "node:test";
 import { basename } from "path";
 import { GuRoot } from "@guardian/cdk/lib/constructs/root.js";
 import { Template } from "aws-cdk-lib/assertions";
-import { DeploymentLambda } from "./deploymentLambda.ts";
+import { AbTestingDeploymentLambda } from "./deploymentLambda.ts";
 
 snapshot.setResolveSnapshotPath(
 	() =>
@@ -15,26 +15,34 @@ snapshot.setResolveSnapshotPath(
 void describe("The ID5 Baton Lambda stack", () => {
 	void it("matches the CODE snapshot", ({ assert }) => {
 		const app = new GuRoot();
-		const stack = new DeploymentLambda(app, "DeploymentLambda", {
-			stack: "frontend",
-			stage: "CODE",
-			env: {
-				region: "eu-west-1",
+		const stack = new AbTestingDeploymentLambda(
+			app,
+			"AbTestingDeploymentLambda",
+			{
+				stack: "frontend",
+				stage: "CODE",
+				env: {
+					region: "eu-west-1",
+				},
 			},
-		});
+		);
 		const template = Template.fromStack(stack);
 		assert.snapshot(template.toJSON());
 	});
 
 	void it("matches the PROD snapshot", ({ assert }) => {
 		const app = new GuRoot();
-		const stack = new DeploymentLambda(app, "DeploymentLambda", {
-			stack: "frontend",
-			stage: "PROD",
-			env: {
-				region: "eu-west-1",
+		const stack = new AbTestingDeploymentLambda(
+			app,
+			"AbTestingDeploymentLambda",
+			{
+				stack: "frontend",
+				stage: "PROD",
+				env: {
+					region: "eu-west-1",
+				},
 			},
-		});
+		);
 		const template = Template.fromStack(stack);
 		assert.snapshot(template.toJSON());
 	});
