f36c850da2612ce787c4888650ce2cc7d4883891
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2025-12-02 15:39:09 +0000

    remove full width text

--- dotcom-rendering/src/components/ProductCardButtons.tsx
+++ dotcom-rendering/src/components/ProductCardButtons.tsx
@@ -20,7 +20,6 @@ export const ProductCardButtons = ({
 					label={label}
 					url={productCta.url}
 					minimisePadding={true}
-					fullWidthText={productCtas.length > 1}
 					priority={index === 0 ? 'primary' : 'tertiary'}
 					fullwidth={true}
 				/>
