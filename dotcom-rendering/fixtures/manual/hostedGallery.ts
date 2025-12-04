2a93eed979c1e691713079f70eff896b670336ac
Imogen Hardy <imogen.hardy@guardian.co.uk>
2025-12-04 15:47:31 +0000

    add manual fixtures for the three hosted content types, add a test for the schema validation

---
+++ dotcom-rendering/fixtures/manual/hostedGallery.ts
@@ -0,0 +1,107 @@
+export const hostedGallery = {
+	id: 'advertiser-content/canary-islands-rugby/the-spanish-rugby-sevens-team',
+	url: '/advertiser-content/canary-islands-rugby/the-spanish-rugby-sevens-team',
+	encodedUrl:
+		'http%3A%2F%2Flocalhost%3A9000%2Fadvertiser-content%2Fcanary-islands-rugby%2Fthe-spanish-rugby-sevens-team',
+	campaign: {
+		id: 'canary-islands-rugby',
+		name: 'Canary Islands Rugby',
+		owner: 'Turismo Islas Canarias',
+		logo: {
+			src: 'https://static.theguardian.com/commercial/sponsor/20/Nov/2025/c1a932a4-5166-4b75-a44a-04e0abe06027-promotur_140.jpg',
+			dimensions: {
+				width: 140,
+				height: 90,
+			},
+			link: 'https://www.hellocanaryislands.com/professional-sports-training/',
+		},
+		fontColour: {
+			hexCode: '#E75412',
+		},
+	},
+	title: 'The Spanish rugby sevens team',
+	mainImageUrl:
+		'https://media.guim.co.uk/7325aed32fadb1e4ae29c4d3193671570a67c392/0_0_1200_800/1200.jpg',
+	thumbnailUrl:
+		'https://i.guim.co.uk/img/media/7325aed32fadb1e4ae29c4d3193671570a67c392/0_0_1200_800/master/1200.jpg?width=300&quality=85&auto=format&fit=max&s=a1bd67c8acede4d4069eed63eb52a453',
+	standfirst:
+		'The Spanish rugby sevens team in training on the island of La Palma, Canary Islands',
+	cta: {
+		url: 'https://www.hellocanaryislands.com/professional-sports-training/',
+		image: 'https://media.guim.co.uk/69d172b14e9df4cee77eb96a1d68728707ca9ed9/0_0_1300_375/1300.jpg',
+		label: 'Professional sports training in the Canary Islands',
+		trackingCode: 'canary-islands-rugby-cta-banner',
+		btnText: 'Discover more',
+	},
+	name: 'Canary Islands Rugby',
+	owner: 'Turismo Islas Canarias',
+	logo: {
+		src: 'https://static.theguardian.com/commercial/sponsor/20/Nov/2025/c1a932a4-5166-4b75-a44a-04e0abe06027-promotur_140.jpg',
+		dimensions: {
+			width: 140,
+			height: 90,
+		},
+		link: 'https://www.hellocanaryislands.com/professional-sports-training/',
+	},
+	fontColour: {
+		hexCode: '#E75412',
+	},
+	images: [
+		{
+			url: 'https://media.guim.co.uk/7325aed32fadb1e4ae29c4d3193671570a67c392/0_0_1200_800/1200.jpg',
+			width: 1200,
+			height: 800,
+			title: 'The Spanish rugby sevens team',
+			caption: 'The Spanish rugby sevens team at La Palma.',
+			credit: 'Photograph: Turismo Islas Canarias',
+		},
+		{
+			url: 'https://media.guim.co.uk/8423b9f1fd3c5bb0dc0e055628edbe60bbdd60b3/0_0_1200_800/1200.jpg',
+			width: 1200,
+			height: 800,
+			title: 'The Spanish rugby sevens team',
+			caption: 'The Spanish rugby sevens team at La Palma.',
+			credit: 'Photograph: Turismo Islas Canarias',
+		},
+		{
+			url: 'https://media.guim.co.uk/eda497e901cfa8d1cca0263d68b1c7ddaa9f4c27/0_0_1200_800/1200.jpg',
+			width: 1200,
+			height: 800,
+			title: 'The Spanish rugby sevens team',
+			caption: 'The Spanish rugby sevens team at La Palma.',
+			credit: 'Photograph: Turismo Islas Canarias',
+		},
+		{
+			url: 'https://media.guim.co.uk/0f59ae7865c2713b5b6ab54d1b3ab5ee4008c58a/0_0_1200_800/1200.jpg',
+			width: 1200,
+			height: 800,
+			title: 'The Spanish rugby sevens team',
+			caption: 'The Spanish rugby sevens team at La Palma.',
+			credit: 'Photograph: Turismo Islas Canarias',
+		},
+		{
+			url: 'https://media.guim.co.uk/0ee979d0cf00b817060cab2051e54c2b2eae36aa/0_0_1200_800/1200.jpg',
+			width: 1200,
+			height: 800,
+			title: 'The Spanish rugby sevens team',
+			caption: 'The Spanish rugby sevens team at La Palma.',
+			credit: 'Photograph: Turismo Islas Canarias',
+		},
+		{
+			url: 'https://media.guim.co.uk/99d74b1dc8edb9c6af3a99cb7f745df5c467029a/0_0_1200_800/1200.jpg',
+			width: 1200,
+			height: 800,
+			title: 'The Spanish rugby sevens team',
+			caption: 'The Spanish rugby sevens team at La Palma.',
+			credit: 'Photograph: Turismo Islas Canarias',
+		},
+		{
+			url: 'https://media.guim.co.uk/6b1be969a4ae09750bbe40faabf80339d288eb54/0_0_1200_800/1200.jpg',
+			width: 1200,
+			height: 800,
+			title: 'The Spanish rugby sevens team',
+			caption: 'The Spanish rugby sevens team at La Palma.',
+			credit: 'Photograph: Turismo Islas Canarias',
+		},
+	],
+};
