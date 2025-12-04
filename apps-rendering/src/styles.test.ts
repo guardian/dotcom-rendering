deac4bbb346091829130b079983b3a5295925417
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-12-04 15:03:12 +0000

    Delete `apps-rendering` directory
    We're turning off this project and deleting the code. Note that this
    change focuses on deleting the directory, and only makes changes to code
    outside it that would otherwise break with the directory gone. This is
    to keep this change as small as possible. Further references will be
    deleted in later changes.

--- apps-rendering/src/styles.test.ts
+++
@@ -1,9 +0,0 @@
-import { darkModeCss } from './styles';
-
-describe('helper functions return correct styles', () => {
-	test('Returns correct dark mode styles', () => {
-		const css = darkModeCss`a { text-decoration: none; color: ${`aliceblue`}; }`;
-		const expectedCss = `@media(prefers-color-scheme:dark){a{text-decoration:none;color:aliceblue;}}`;
-		expect(css.styles.trim().replace(/\s/g, '')).toEqual(expectedCss);
-	});
-});
