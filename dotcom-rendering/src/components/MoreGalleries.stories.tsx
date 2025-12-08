66fd1fa0bdb8678cc94783e5559586229537ab7a
Dominik Lander <dominik.lander@guardian.co.uk>
2025-12-08 14:12:00 +0000

    Add stories with fewer cards

--- dotcom-rendering/src/components/MoreGalleries.stories.tsx
+++ dotcom-rendering/src/components/MoreGalleries.stories.tsx
@@ -12,7 +12,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const MoreGalleries = {
+export const Default = {
 	args: {
 		discussionApiUrl: 'https://discussion.theguardian.com/discussion-api',
 		guardianBaseUrl: 'https://www.theguardian.com',
@@ -22,5 +22,37 @@ export const MoreGalleries = {
 			display: ArticleDisplay.Standard,
 			theme: Pillar.Culture,
 		},
+	},
+} satisfies Story;
+
+export const WithFourCards = {
+	...Default,
+	args: {
+		...Default.args,
+		trails: galleryOnwardsTrails.slice(0, 4),
+	},
+} satisfies Story;
+
+export const WithThreeCards = {
+	...Default,
+	args: {
+		...Default.args,
+		trails: galleryOnwardsTrails.slice(0, 3),
+	},
+} satisfies Story;
+
+export const WithTwoCards = {
+	...Default,
+	args: {
+		...Default.args,
+		trails: galleryOnwardsTrails.slice(0, 2),
+	},
+} satisfies Story;
+
+export const WithOneCard = {
+	...Default,
+	args: {
+		...Default.args,
+		trails: galleryOnwardsTrails.slice(0, 1),
 	},
 } satisfies Story;
