7d23c99c6864d5b5367cb54d4834865573d2756b
Imogen Hardy <imogen.hardy@guardian.co.uk>
2025-12-09 14:56:22 +0000

    basic enhancer function

--- dotcom-rendering/src/types/hostedContent.ts
+++ dotcom-rendering/src/types/hostedContent.ts
@@ -1,5 +1,29 @@
 import { FEHostedContent } from 'src/frontend/feHostedContent';
 
+enum HostedContentType {
+	Article,
+	Video,
+	Gallery,
+}
+
 export type HostedContent = {
 	frontendData: FEHostedContent;
+	type: HostedContentType;
+};
+
+export const enhanceHostedContentType = (
+	data: FEHostedContent,
+): HostedContent => {
+	let type = HostedContentType.Article;
+
+	if (data.video) {
+		type = HostedContentType.Video;
+	} else if (data.images.length) {
+		type = HostedContentType.Gallery;
+	}
+
+	return {
+		frontendData: data,
+		type,
+	};
 };
