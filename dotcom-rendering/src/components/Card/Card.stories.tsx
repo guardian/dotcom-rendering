968a684e7fbdf82486bbab7f47aa4d7f7c9de4e2
Charlotte <charlotte.emms@theguardian.com>
2025-12-15 15:15:08 +0000

    fix stories for labs cards and cards in a labs container

--- dotcom-rendering/src/components/Card/Card.stories.tsx
+++ dotcom-rendering/src/components/Card/Card.stories.tsx
@@ -1301,34 +1301,34 @@ const containerPalettes = [
 	'Branded',
 ] as const satisfies readonly DCRContainerPalette[];
 
-export const WithBranding = () => {
-	const branding = {
-		brandingType: {
-			name: 'sponsored',
+const branding = {
+	brandingType: {
+		name: 'sponsored',
+	},
+	sponsorName: 'theguardian.org',
+	logo: {
+		src: 'https://static.theguardian.com/commercial/sponsor/22/Feb/2024/17ea91fc-659b-4c51-8410-9907241c1710-Guardian.orglogos-for badge.png',
+		dimensions: {
+			width: 280,
+			height: 180,
 		},
-		sponsorName: 'theguardian.org',
-		logo: {
-			src: 'https://static.theguardian.com/commercial/sponsor/22/Feb/2024/17ea91fc-659b-4c51-8410-9907241c1710-Guardian.orglogos-for badge.png',
-			dimensions: {
-				width: 280,
-				height: 180,
-			},
-			link: 'https://theguardian.org/',
-			label: 'Supported by',
+		link: 'https://theguardian.org/',
+		label: 'Supported by',
+	},
+	logoForDarkBackground: {
+		src: 'https://static.theguardian.com/commercial/sponsor/22/Feb/2024/21f5a3a5-30e7-4db7-a09f-031af569454d-guardian.org new logo - white version (3).png',
+		dimensions: {
+			width: 280,
+			height: 180,
 		},
-		logoForDarkBackground: {
-			src: 'https://static.theguardian.com/commercial/sponsor/22/Feb/2024/21f5a3a5-30e7-4db7-a09f-031af569454d-guardian.org new logo - white version (3).png',
-			dimensions: {
-				width: 280,
-				height: 180,
-			},
-			link: 'https://theguardian.org/',
-			label: 'Supported by',
-		},
-		aboutThisLink:
-			'https://www.theguardian.com/environment/2023/jan/06/about-animals-farmed-investigating-modern-farming-around-the-world',
-	} satisfies Branding;
+		link: 'https://theguardian.org/',
+		label: 'Supported by',
+	},
+	aboutThisLink:
+		'https://www.theguardian.com/environment/2023/jan/06/about-animals-farmed-investigating-modern-farming-around-the-world',
+} satisfies Branding;
 
+export const WithBranding = () => {
 	return [undefined, ...containerPalettes].map((containerPalette) => (
 		<ContainerOverrides
 			key={containerPalette}
@@ -1336,7 +1336,7 @@ export const WithBranding = () => {
 		>
 			<Section title={containerPalette ?? 'Standard'}>
 				<UL direction="row" padBottom={true}>
-					<LI percentage={'25%'} padSides={true}>
+					<LI percentage={'33.333%'} padSides={true}>
 						<Card
 							{...basicCardProps}
 							format={{
@@ -1354,7 +1354,7 @@ export const WithBranding = () => {
 							branding={branding}
 						/>
 					</LI>
-					<LI percentage={'25%'} padSides={true}>
+					<LI percentage={'33.333%'} padSides={true}>
 						<Card
 							{...basicCardProps}
 							format={{
@@ -1373,7 +1373,7 @@ export const WithBranding = () => {
 							branding={branding}
 						/>
 					</LI>
-					<LI percentage={'25%'} padSides={true}>
+					<LI percentage={'33.333%'} padSides={true}>
 						<Card
 							{...basicCardProps}
 							format={{
@@ -1382,24 +1382,6 @@ export const WithBranding = () => {
 								theme: Pillar.News,
 							}}
 							headlineText="guardian.org branding does not appear on non Labs articles"
-							kickerText="Kicker"
-							trailText=""
-							mediaPositionOnDesktop="top"
-							mediaPositionOnMobile="left"
-							mediaSize="small"
-							containerPalette={containerPalette}
-							branding={branding}
-						/>
-					</LI>
-					<LI percentage={'25%'} padSides={true}>
-						<Card
-							{...basicCardProps}
-							format={{
-								display: ArticleDisplay.Standard,
-								design: ArticleDesign.Standard,
-								theme: ArticleSpecial.Labs,
-							}}
-							headlineText="Redesigned Labs card"
 							kickerText="Kicker"
 							trailText=""
 							mediaPositionOnDesktop="top"
@@ -1443,6 +1425,7 @@ export const WithSpecialPaletteVariations = () => {
 					containerPalette={containerPalette}
 					discussionId="/p/d8ex5"
 					discussionApiUrl="https://discussion.theguardian.com/discussion-api"
+					branding={branding}
 				/>
 			</LI>
 			<LI percentage={'33.333%'} padSides={true} showDivider={true}>
@@ -1469,6 +1452,7 @@ export const WithSpecialPaletteVariations = () => {
 					mediaSize="medium"
 					mainMedia={mainAudio}
 					containerPalette={containerPalette}
+					branding={branding}
 				/>
 			</LI>
 		</UL>
@@ -1483,6 +1467,17 @@ export const WithSpecialPaletteVariations = () => {
 					containerPalette={containerPalette}
 					key={containerPalette}
 					isLabs={containerPalette === 'Branded'}
+					collectionBranding={
+						containerPalette === 'Branded'
+							? {
+									kind: 'paid-content',
+									branding,
+									isContainerBranding: true,
+									hasMultipleBranding: false,
+									isFrontBranding: false,
+							  }
+							: undefined
+					}
 				>
 					<Cards containerPalette={containerPalette} />
 				</FrontSection>
@@ -1567,6 +1562,17 @@ export const DynamoWithSpecialPaletteVariations = () => {
 					containerPalette={containerPalette}
 					key={containerPalette}
 					isLabs={containerPalette === 'Branded'}
+					collectionBranding={
+						containerPalette === 'Branded'
+							? {
+									kind: 'paid-content',
+									branding,
+									isContainerBranding: true,
+									hasMultipleBranding: false,
+									isFrontBranding: false,
+							  }
+							: undefined
+					}
 				>
 					<DynamoCard containerPalette={containerPalette} />
 				</FrontSection>
