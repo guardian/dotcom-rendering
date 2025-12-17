0b6a1001ae83e55ddcef9f36875979de8fe68708
Marcin Gosz <marcin.gosz@theguardian.com>
2025-12-17 11:43:18 +0000

    removed empty line

--- dotcom-rendering/src/lib/useNewsletterSubscription.ts
+++ dotcom-rendering/src/lib/useNewsletterSubscription.ts
@@ -31,7 +31,6 @@ export const useNewsletterSubscription = (
 
 	useEffect(() => {
 		// Wait for auth to be determined
-
 		if (authStatus.kind === 'Pending') {
 			setIsSubscribed(undefined);
 			return;
