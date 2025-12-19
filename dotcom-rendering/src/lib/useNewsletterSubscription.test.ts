09904750cf6c49c994aa3c726ac792caa7e93f9d
Marcin Gosz <marcin.gosz@theguardian.com>
2025-12-19 10:03:42 +0000

    fixing test

--- dotcom-rendering/src/lib/useNewsletterSubscription.test.ts
+++ dotcom-rendering/src/lib/useNewsletterSubscription.test.ts
@@ -152,7 +152,14 @@ describe('useNewsletterSubscription', () => {
 	});
 
 	it('should return false when API returns non-ok response', async () => {
-		const consoleSpy = jest.spyOn(console, 'error');
+		const sentryReportErrorMock = jest.fn();
+		window.guardian = {
+			modules: {
+				sentry: {
+					reportError: sentryReportErrorMock,
+				},
+			},
+		} as unknown as typeof window.guardian;
 
 		mockUseAuthStatus.mockReturnValue({
 			kind: 'SignedIn',
@@ -173,11 +180,10 @@ describe('useNewsletterSubscription', () => {
 			expect(result.current).toBe(false);
 		});
 
-		expect(consoleSpy).toHaveBeenCalledWith(
-			'Failed to fetch user newsletters',
+		expect(sentryReportErrorMock).toHaveBeenCalledWith(
+			expect.any(Error),
+			'errors-fetching-newsletters',
 		);
-
-		consoleSpy.mockRestore();
 	});
 
 	it('should return false when fetch throws an error', async () => {
