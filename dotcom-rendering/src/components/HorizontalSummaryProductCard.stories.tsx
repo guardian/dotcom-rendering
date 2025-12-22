aacdb8890243456e26f5398a13c2def3e028ee72
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2025-12-22 17:25:54 +0000

    remove chromatic modes use defaults

--- dotcom-rendering/src/components/HorizontalSummaryProductCard.stories.tsx
+++ dotcom-rendering/src/components/HorizontalSummaryProductCard.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import { centreColumnDecorator } from '../../.storybook/decorators/gridDecorators';
-import { allModes } from '../../.storybook/modes';
 import { exampleProduct } from '../../fixtures/manual/productBlockElement';
 import { ArticleDesign, ArticleDisplay, Pillar } from '../lib/articleFormat';
 import { HorizontalSummaryProductCard } from './HorizontalSummaryProductCard';
@@ -8,13 +7,6 @@ import { HorizontalSummaryProductCard } from './HorizontalSummaryProductCard';
 const meta = {
 	title: 'Components/Horizontal Summary Product Card',
 	component: HorizontalSummaryProductCard,
-	parameters: {
-		chromatic: {
-			modes: {
-				'vertical mobileMedium': allModes['vertical mobileMedium'],
-			},
-		},
-	},
 	args: {
 		product: { ...exampleProduct, h2Id: 'example-1' },
 		format: {
