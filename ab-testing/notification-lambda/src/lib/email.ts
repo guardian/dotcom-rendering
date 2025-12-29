8032e8d796fe251d61df367e83ef9bc742d4c2f7
Charlotte <charlotte.emms@theguardian.com>
2025-12-29 17:18:50 +0000

    update email source to include name as well as email

--- ab-testing/notification-lambda/src/lib/email.ts
+++ ab-testing/notification-lambda/src/lib/email.ts
@@ -1,45 +1,31 @@
 import { MessageRejected, SendEmailCommand } from "@aws-sdk/client-ses";
 import { sesClient } from "./sesClient.ts";
 
-const createSendEmailCommand = (
-	recipients: string[],
-	messageHtml: string,
-	messagePlainText?: string,
-) => {
-	return new SendEmailCommand({
-		Destination: {
-			ToAddresses: recipients,
-		},
-		Message: {
-			Body: {
-				Html: {
-					Charset: "UTF-8",
-					Data: messageHtml,
-				},
-				// Only attach plaintext message if provided
-				...(messagePlainText
-					? {
-							Text: {
-								Charset: "UTF-8",
-								Data: messagePlainText,
-							},
-					  }
-					: {}),
-			},
-			Subject: {
-				Charset: "UTF-8",
-				Data: "Expiring AB Tests",
-			},
-		},
-		Source: "dig.dev.web-engineers@theguardian.com",
-	});
-};
-
 export const sendEmail = async (recipients: string[], messageHtml: string) => {
-	const sendEmailCommand = createSendEmailCommand(recipients, messageHtml);
 	console.log("sending email");
+
 	try {
-		return void sesClient.send(sendEmailCommand);
+		return void sesClient.send(
+			new SendEmailCommand({
+				Source: "Dig Dev Web Engineers <dig.dev.web-engineers@theguardian.com>",
+				Destination: {
+					ToAddresses: recipients,
+				},
+				Message: {
+					Subject: {
+						Data: "Expiring AB Tests",
+					},
+					Body: {
+						Html: {
+							Data: messageHtml,
+						},
+						Text: {
+							Data: "Test",
+						},
+					},
+				},
+			}),
+		);
 	} catch (caught) {
 		console.log("error");
 		console.log(caught);
