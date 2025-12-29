3e668eda71a4008413a553817e4c20f2173bec32
Charlotte <charlotte.emms@theguardian.com>
2025-12-29 16:35:58 +0000

    fix linting issue

--- ab-testing/config/index.ts
+++ ab-testing/config/index.ts
@@ -1,4 +1,4 @@
 // this file is covered by the dotcom-rendering tsconfig.json,
 // not the one in this directory (hence no need for .ts extension)
-export { activeABtests, allABTests } from "./abTests.ts";
-export type { ABTest } from "./types.ts";
+export { activeABtests, allABTests } from "./abTests";
+export type { ABTest } from "./types";
