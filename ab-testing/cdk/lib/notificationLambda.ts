bb5a3246798db4a1951d93600b23ed09cba26db2
Charlotte <charlotte.emms@theguardian.com>
2025-12-29 16:20:59 +0000

    separate prod rules from code

--- ab-testing/cdk/lib/notificationLambda.ts
+++ ab-testing/cdk/lib/notificationLambda.ts
@@ -14,18 +14,17 @@ export class AbTestingNotificationLambda extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
+		const runDailyRule = {
+			// 5am daily on weekdays
+			schedule: Schedule.expression("cron(0 5 ? * MON-FRI *)"),
+			description: "Daily expiry checks for AB testing configuration",
+		};
+
 		new GuScheduledLambda(this, "AbTestingNotificationLambda", {
 			app: lambdaFunctionName,
 			fileName: "lambda.zip",
 			handler: "index.handler",
-			rules: [
-				{
-					// 5am daily on weekdays
-					schedule: Schedule.expression("cron(0 5 ? * MON-FRI *)"),
-					description:
-						"Daily expiry checks for AB testing configuration",
-				},
-			],
+			rules: this.stage === "PROD" ? [runDailyRule] : [],
 			monitoringConfiguration: { noMonitoring: true },
 			runtime: Runtime.NODEJS_22_X,
 			initialPolicy: [
