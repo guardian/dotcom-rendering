a74ce2e80ec1556624ab798f5e2fc5ea5bb0754b
Emma Imber <emma-jo.imber@guardian.co.uk>
2026-01-02 16:37:22 +0000

    Only show first three products & add Show all button

--- dotcom-rendering/src/components/StackedProducts.tsx
+++ dotcom-rendering/src/components/StackedProducts.tsx
@@ -3,6 +3,18 @@ import { space } from '@guardian/source/foundations';
 import type { ArticleFormat } from '../lib/articleFormat';
 import type { ProductBlockElement } from '../types/content';
 import { HorizontalSummaryProductCard } from './HorizontalSummaryProductCard';
+import { useState } from 'react';
+import { palette, textSans15 } from '@guardian/source/foundations';
+
+const buttonStyles = css`
+	${textSans15};
+	color: ${palette.lifestyle[400]};
+	font-weight: 700;
+	background-color: transparent;
+	padding: ${space[4]}px 0px 0px 0px;
+	border: none;
+	border-bottom: 1px solid ${palette.neutral[86]};
+`;
 
 export const StackedProducts = ({
 	products,
@@ -11,22 +23,37 @@ export const StackedProducts = ({
 	products: ProductBlockElement[];
 	format: ArticleFormat;
 }) => {
+	const [isExpanded, setIsExpanded] = useState(false);
 	return (
-		<div
-			css={[
-				css`
-					display: grid;
-					gap: ${space[4]}px;
-				`,
-			]}
-		>
-			{products.map((product: ProductBlockElement, index) => (
-				<HorizontalSummaryProductCard
-					key={index}
-					product={product}
-					format={format}
-				/>
-			))}
+		<div>
+			<div
+				css={[
+					css`
+						display: grid;
+						gap: ${space[4]}px;
+					`,
+				]}
+			>
+				{products.map(
+					(product: ProductBlockElement, index) =>
+						(index < 3 || isExpanded) && (
+							<HorizontalSummaryProductCard
+								key={index}
+								product={product}
+								format={format}
+							/>
+						),
+				)}
+			</div>
+
+			{products.length > 3 && !isExpanded && (
+				<button
+					onClick={() => setIsExpanded(!isExpanded)}
+					css={buttonStyles}
+				>
+					Show all
+				</button>
+			)}
 		</div>
 	);
 };
