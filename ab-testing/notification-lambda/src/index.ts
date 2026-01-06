af29cd184b68be51263a368f3435ce772a116b80
Charlotte Emms <43961396+cemms1@users.noreply.github.com>
2026-01-06 16:56:48 +0000

    Improve logging and monitoring of AB testing notification lambda (#15084)
    * improve logging and monitoring of notification lambda
    
    * use human-readable function name for lambda
    
    * specify alarm conditions, improve readability of policy allowing lambda to send emails using SES

--- ab-testing/notification-lambda/src/index.ts
+++ ab-testing/notification-lambda/src/index.ts
@@ -6,6 +6,14 @@ export const handler = async (): Promise<void> => {
 	const expiringAbTestsByOwner =
 		getExpiringAbTestsGroupedByOwner(activeABtests);
 
+	// Check if any test owners have expiring tests
+	// Early return if there are no results
+	if (!Object.keys(expiringAbTestsByOwner).length) {
+		console.log("No owners found with expiring tests");
+		Promise.resolve();
+	}
+
+	// Sending emails to owners with expiring tests
 	await Promise.all(
 		Object.entries(expiringAbTestsByOwner).map(([owner, abTests]) =>
 			sendEmail(owner, abTests),
