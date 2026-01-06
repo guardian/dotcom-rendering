9567109470c442854482d1102141ad39bb4f953c
vanessa <32312712+vlbee@users.noreply.github.com>
2026-01-06 15:54:35 +0000

    Fix byline for tablet landscape views

--- dotcom-rendering/src/components/ArticleMeta.apps.tsx
+++ dotcom-rendering/src/components/ArticleMeta.apps.tsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import { isUndefined } from '@guardian/libs';
-import { from, space, until } from '@guardian/source/foundations';
 import { StraightLines } from '@guardian/source-development-kitchen/react-components';
+import { from, space, until } from '@guardian/source/foundations';
 import type { ReactNode } from 'react';
 import { grid } from '../grid';
 import { interactiveLegacyClasses } from '../layouts/lib/interactiveLegacyStyling';
@@ -98,7 +98,7 @@ const MetaGridByline = ({
 	children: ReactNode;
 	isComment: boolean;
 }) => (
-	// address > div > span > div:first-of-type - spacing for the Follow Tag buttons in follow wrapper,
+	// address > div > div:first-of-type - spacing for the Follow Tag buttons in follow wrapper,
 	// which is contextual of the meta by layout type
 	<div
 		css={css`
@@ -111,7 +111,7 @@ const MetaGridByline = ({
 				padding-bottom: 0px;
 			}
 
-			address > div > span > div:first-of-type {
+			address > div > div:first-of-type {
 				display: block;
 				margin-top: ${space[2]}px;
 				${!isComment && `margin-bottom: ${space[2]}px;`}
