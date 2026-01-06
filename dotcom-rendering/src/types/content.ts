1617400419c9cacdb06601d5cba4ce75d322f167
philmcmahon <philip.mcmahon@theguardian.com>
2026-01-06 12:51:00 +0000

    Add rendering support for reporter callouts

--- dotcom-rendering/src/types/content.ts
+++ dotcom-rendering/src/types/content.ts
@@ -112,6 +112,19 @@ export interface CalloutBlockElementV2 {
 	contacts?: CalloutContactType[];
 }
 
+export interface ReporterCalloutBlockElement {
+	_type: 'model.dotcomrendering.pageElements.ReporterCalloutBlockElement';
+	elementId: string;
+	id: string;
+	activeFrom?: number;
+	activeUntil?: number;
+	displayOnSensitive: boolean;
+	title: string;
+	description: string;
+	role?: RoleType;
+	contacts?: CalloutContactType[];
+}
+
 export interface CartoonBlockElement {
 	_type: 'model.dotcomrendering.pageElements.CartoonBlockElement';
 	elementId: string;
@@ -800,6 +813,7 @@ export type FEElement =
 	| CaptionBlockElement
 	| CalloutBlockElement
 	| CalloutBlockElementV2
+	| ReporterCalloutBlockElement
 	| CartoonBlockElement
 	| ChartAtomBlockElement
 	| CodeBlockElement
