b220388ee42766fead431e02b74a6f8b792bfc82
Anna Beddow <anna.beddow@gmail.com>
2026-01-13 14:03:32 +0000

    track clicks to card

--- dotcom-rendering/src/components/Card/components/CardLink.tsx
+++ dotcom-rendering/src/components/Card/components/CardLink.tsx
@@ -1,6 +1,8 @@
 import { css } from '@emotion/react';
 import { focusHalo } from '@guardian/source/foundations';
 import { getZIndex } from '../../../lib/getZIndex';
+import { ViewHistoryKey } from '../../DynamicMediumFour.importable';
+import { storage } from '@guardian/libs';
 
 const fauxLinkStyles = css`
 	position: absolute;
@@ -27,10 +29,12 @@ const InternalLink = ({
 	linkTo,
 	headlineText,
 	dataLinkName,
+	trackCardClick,
 }: {
 	linkTo: string;
 	headlineText: string;
 	dataLinkName?: string;
+	trackCardClick?: () => void;
 }) => {
 	return (
 		// eslint-disable-next-line jsx-a11y/anchor-has-content -- we have an aria-label attribute describing the content
@@ -39,6 +43,7 @@ const InternalLink = ({
 			css={fauxLinkStyles}
 			data-link-name={dataLinkName}
 			aria-label={headlineText}
+			onClick={trackCardClick}
 		/>
 	);
 };
@@ -68,12 +73,39 @@ const ExternalLink = ({
 	);
 };
 
+const isValidHighlightsState = (history: unknown): history is string[] =>
+	Array.isArray(history) &&
+	history.every((highlight) => typeof highlight === 'string');
+
+export const getViewState = (): string[] | undefined => {
+	try {
+		const highlightHistory = storage.local.get(ViewHistoryKey);
+
+		if (!isValidHighlightsState(highlightHistory)) {
+			throw new Error(`Invalid ${ViewHistoryKey} value`);
+		}
+
+		return highlightHistory;
+	} catch (e) {
+		/* error parsing the string, so remove the key */
+		storage.local.remove(ViewHistoryKey);
+		return undefined;
+	}
+};
+
 export const CardLink = ({
 	linkTo,
 	headlineText,
 	dataLinkName = 'article', //this makes sense if the link is to an article, but should this say something like "external" if it's an external link? are there any other uses/alternatives?
 	isExternalLink,
 }: Props) => {
+	const saveState = (url: string) => {
+		console.log('saveState', url);
+		const viewedCards = getViewState() ?? [];
+		console.log('viewedCards', viewedCards);
+		storage.local.set(ViewHistoryKey, [...viewedCards, url]);
+	};
+
 	return (
 		<>
 			{isExternalLink && (
@@ -81,6 +113,7 @@ export const CardLink = ({
 					linkTo={linkTo}
 					headlineText={headlineText}
 					dataLinkName={dataLinkName}
+					trackCardClick={() => saveState(linkTo)}
 				/>
 			)}
 			{!isExternalLink && (
@@ -88,6 +121,7 @@ export const CardLink = ({
 					linkTo={linkTo}
 					headlineText={headlineText}
 					dataLinkName={dataLinkName}
+					trackCardClick={() => saveState(linkTo)}
 				/>
 			)}
 		</>
