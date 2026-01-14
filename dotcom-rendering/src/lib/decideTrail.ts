bb159347a18c67b23f3333a2dfa0f376c4ae35af
Dominik Lander <dominik.lander@guardian.co.uk>
2026-01-14 12:19:58 +0000

    Render images on video article onwards content

--- dotcom-rendering/src/lib/decideTrail.ts
+++ dotcom-rendering/src/lib/decideTrail.ts
@@ -2,6 +2,7 @@ import type { DCRFrontImage } from '../types/front';
 import type { FETrailType, TrailType } from '../types/trails';
 import { type ArticleFormat, decideFormat } from './articleFormat';
 import { getDataLinkNameCard } from './getDataLinkName';
+import { getSourceImageUrl } from './getSourceImageUrl_temp_fix';
 
 export const dedupeTrail = (trail: FETrailType, webURL: string): boolean => {
 	return trail.url !== webURL;
@@ -43,6 +44,39 @@ export const decideTrailWithMasterImage = (
 	const image: DCRFrontImage | undefined = trail.masterImage
 		? {
 				src: trail.masterImage,
+				altText: '',
+		  }
+		: undefined;
+
+	return {
+		...trail,
+		image,
+		format,
+		dataLinkName: getDataLinkNameCard(format, '0', index),
+	};
+};
+
+/**
+ * When the Onward Journeys AB test is complete, this function can be removed.
+ *
+ * If we move forward with the new design, we shouuld look to a permanent solution for trail
+ * images on onwards content that dont require the deprecated function getSourceImageUrl.
+ * An example page with this type of onward content is video articles:
+ * https://www.theguardian.com/society/video/2025/dec/12/threshold-the-choir-who-sing-to-the-dying-documentary
+ */
+export const decideTrailWithMasterImagePreferred = (
+	trail: FETrailType,
+	index = 0,
+): TrailType => {
+	const format: ArticleFormat = decideFormat(trail.format);
+
+	const masterImageUrl = trail.masterImage;
+	const trailImageUrl = trail.image ? getSourceImageUrl(trail.image) : null;
+
+	const imageUrl = masterImageUrl ?? trailImageUrl;
+	const image: DCRFrontImage | undefined = imageUrl
+		? {
+				src: imageUrl,
 				altText: '',
 		  }
 		: undefined;
