c590c39c20e0a28d08e6cb2dcee4a2b1a2ac62ba
Emma Imber <emma-jo.imber@guardian.co.uk>
2026-01-14 16:30:06 +0000

    Make StackedProducts .importable

--- dotcom-rendering/src/components/StackedProducts.stories.tsx
+++ dotcom-rendering/src/components/StackedProducts.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import { centreColumnDecorator } from '../../.storybook/decorators/gridDecorators';
 import { exampleAtAGlanceProductArray } from '../../fixtures/manual/productBlockElement';
 import { ArticleDesign, ArticleDisplay, Pillar } from '../lib/articleFormat';
-import { StackedProducts } from './StackedProducts';
+import { StackedProducts } from './StackedProducts.importable';
 
 const meta = {
 	title: 'Components/Stacked Horizontal Summary Product Cards',
