ffb24e5c2ed0dfff40dc2a65daaff7d4fa494819
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-01-22 17:13:32 +0000

    Update goal attempt stories to show team colour examples

--- dotcom-rendering/src/components/FootballMatchGoalAttempts.stories.tsx
+++ dotcom-rendering/src/components/FootballMatchGoalAttempts.stories.tsx
@@ -1,18 +1,21 @@
+import { css } from '@emotion/react';
+import { space } from '@guardian/source/foundations';
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
-import { FootballMatchGoalAttempts as FootballMatchGoalAttemptsComponent } from './FootballMatchStat';
+import { footballTeams } from '../../fixtures/manual/footballTeams';
+import { FootballMatchGoalAttempts } from './FootballMatchStat';
 
 const meta = {
 	title: 'Components/Football Match Goal Attempts',
-	component: FootballMatchGoalAttemptsComponent,
+	component: FootballMatchGoalAttempts,
 	parameters: {
 		layout: 'padded',
 	},
-} satisfies Meta<typeof FootballMatchGoalAttemptsComponent>;
+} satisfies Meta<typeof FootballMatchGoalAttempts>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const FootballMatchGoalAttempts = {
+export const Default = {
 	args: {
 		homeTeam: {
 			name: 'Manchester United',
@@ -30,5 +33,35 @@ export const FootballMatchGoalAttempts = {
 			offTarget: 6,
 			onTarget: 2,
 		},
+	},
+} satisfies Story;
+
+export const TeamColours = {
+	render: (args) => (
+		<div
+			css={css`
+				display: flex;
+				flex-direction: column;
+				gap: ${space[2]}px;
+			`}
+		>
+			{footballTeams.map((match, index) => (
+				<FootballMatchGoalAttempts
+					{...args}
+					homeTeam={{
+						name: match.home.name,
+						colour: match.home.colour,
+					}}
+					awayTeam={{
+						name: match.away.name,
+						colour: match.away.colour,
+					}}
+					key={index}
+				/>
+			))}
+		</div>
+	),
+	args: {
+		...Default.args,
 	},
 } satisfies Story;
