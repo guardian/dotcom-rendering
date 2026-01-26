7e31c0fd5954e4a94007e90d5b516722783f15e4
Dina Hafez <dina.hafez@guardian.co.uk>
2026-01-26 10:46:22 +0000

    Testing with an or for location

--- ab-testing/frontend/src/lib/stores/environment.ts
+++ ab-testing/frontend/src/lib/stores/environment.ts
@@ -8,9 +8,8 @@ export const hostname = writable("https://www.theguardian.com");
 if (browser) {
 	console.log("Browser detected, checking URL:", window.location.href);
 	if (
-		window.location.href.includes(
-			"https://frontend.code.dev-gutools.co.uk/",
-		)
+		window.location.href.includes("frontend.code.dev-gutools.co.uk") ||
+		window.location.href.includes("CODE/admin")
 	) {
 		console.log("CODE environment detected, setting CODE hostname");
 		hostname.set("https://m.code.dev-theguardian.com");
