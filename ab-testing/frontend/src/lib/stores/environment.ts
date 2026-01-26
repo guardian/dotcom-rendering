d835ecd34d74ad87b4d8976b912e75b8c2a7ab38
Dina Hafez <dina.hafez@guardian.co.uk>
2026-01-26 13:42:40 +0000

    Tidy code

--- ab-testing/frontend/src/lib/stores/environment.ts
+++ ab-testing/frontend/src/lib/stores/environment.ts
@@ -1,18 +1,8 @@
 import { page } from "$app/state";
 
-export function getHostname(): string {
-	const url = page.url.href;
-
-	console.log("Checking URL:", url);
-	console.log("Checking Hostname:", page.url.hostname);
-
-	// Check for CODE environment
-	if (
-		url.includes("frontend.code.dev-gutools.co.uk") ||
-		page.url.hostname.includes("code.dev-gutools.co.uk")
-	) {
+export const getHostname = (): string => {
+	if (page.url.href.includes("CODE/admin/ab-testing")) {
 		return "https://m.code.dev-theguardian.com";
 	}
-
 	return "https://www.theguardian.com";
-}
+};
