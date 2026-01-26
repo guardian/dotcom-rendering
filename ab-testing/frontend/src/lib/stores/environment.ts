cd0185e34030b1929fca0c9e633fe1401f690709
Dina Hafez <dina.hafez@guardian.co.uk>
2026-01-26 11:04:12 +0000

    Tidy code and remove consoles

--- ab-testing/frontend/src/lib/stores/environment.ts
+++ ab-testing/frontend/src/lib/stores/environment.ts
@@ -6,14 +6,7 @@ export const hostname = writable("https://www.theguardian.com");
 
 // Update on client side only
 if (browser) {
-	console.log("Browser detected, checking URL:", window.location.href);
-	if (
-		window.location.href.includes("frontend.code.dev-gutools.co.uk") ||
-		window.location.href.includes("CODE/admin")
-	) {
-		console.log("CODE environment detected, setting CODE hostname");
+	if (window.location.href.includes("CODE/admin")) {
 		hostname.set("https://m.code.dev-theguardian.com");
-	} else {
-		console.log("Production environment, keeping production hostname");
 	}
 }
