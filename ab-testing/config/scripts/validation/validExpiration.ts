6985e4f4d7965d928f9c316011b0ed24a4c2fed8
Charlotte <charlotte.emms@theguardian.com>
2026-01-30 11:32:16 +0000

    ab testing validExpiration - checks date specified is date as interpreted by javascript

--- ab-testing/config/scripts/validation/validExpiration.ts
+++ ab-testing/config/scripts/validation/validExpiration.ts
@@ -12,6 +12,21 @@ export function allExpirationsValid(tests: ABTest[]): boolean {
 				} has an expiration date in the past: ${expires.toISOString()}, has it expired?`,
 			);
 		}
+
+		/**
+		 * Check if the input date matches the JS interpreted date to check it is valid
+		 * If the input !== output date, we throw an error to highlight the date set is not
+		 * the actual expiration date of the test
+		 */
+		const interpretedExpirationDate = new Date(test.expirationDate)
+			.toISOString()
+			.split("T")[0];
+
+		if (test.expirationDate !== interpretedExpirationDate) {
+			throw new Error(
+				`Invalid expiration date provided on test ${test.name}: ${test.expirationDate}`,
+			);
+		}
 	});
 	return true;
 }
