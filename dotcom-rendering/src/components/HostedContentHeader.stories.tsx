ab9a3c47d8db6ff0f0a536d1de6b3f0785620dba
Dina Hafez <dina.hafez@guardian.co.uk>
2026-01-30 12:50:23 +0000

    Add HostedContentHeader story and wrap the component in Stuck

---
+++ dotcom-rendering/src/components/HostedContentHeader.stories.tsx
@@ -0,0 +1,27 @@
+import { palette as sourcePalette } from '@guardian/source/foundations';
+import { HostedContentHeader } from './HostedContentHeader';
+import { Section } from './Section';
+
+export default {
+	component: HostedContentHeader,
+	title: 'Components/HostedContentHeader',
+};
+
+export const Default = () => {
+	return (
+		<Section
+			fullWidth={true}
+			showSideBorders={false}
+			shouldCenter={false}
+			backgroundColour={sourcePalette.neutral[7]}
+			padSides={false}
+			element="aside"
+		>
+			<HostedContentHeader
+				accentColor={sourcePalette.brand[400]}
+				branding="Branding"
+			/>
+		</Section>
+	);
+};
+Default.storyName = 'default';
