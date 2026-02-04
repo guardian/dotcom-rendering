15396abb2b7c7ee2f12bbc353c11a32b23ce4bd5
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2026-02-04 17:40:24 +0000

    Support slideshow

--- dotcom-rendering/src/components/MultiImageBlockComponent.tsx
+++ dotcom-rendering/src/components/MultiImageBlockComponent.tsx
@@ -5,6 +5,9 @@ import type { ImageBlockElement } from '../types/content';
 import { Caption } from './Caption';
 import { GridItem } from './GridItem';
 import { ImageComponent } from './ImageComponent';
+import { SlideshowCarousel } from './SlideshowCarousel.importable';
+import { getLargest, getMaster } from '../lib/image';
+import { Island } from './Island';
 
 type Props = {
 	images: ImageBlockElement[];
@@ -58,27 +61,27 @@ const SideBySideGrid = ({ children }: { children: React.ReactNode }) => (
 	</div>
 );
 
-const OneAboveTwoGrid = ({ children }: { children: React.ReactNode }) => (
-	<div
-		css={css`
-			${ieFallback}
-			@supports (display: grid) {
-				margin-left: 0;
-				margin-right: ${space[3]}px;
-				display: grid;
-				grid-gap: ${space[3]}px;
-				grid-template-columns:
-					50% /* Left column */
-					50%; /* Right column */
-				grid-template-areas:
-					'first first'
-					'second third';
-			}
-		`}
-	>
-		{children}
-	</div>
-);
+// const OneAboveTwoGrid = ({ children }: { children: React.ReactNode }) => (
+// 	<div
+// 		css={css`
+// 			${ieFallback}
+// 			@supports (display: grid) {
+// 				margin-left: 0;
+// 				margin-right: ${space[3]}px;
+// 				display: grid;
+// 				grid-gap: ${space[3]}px;
+// 				grid-template-columns:
+// 					50% /* Left column */
+// 					50%; /* Right column */
+// 				grid-template-areas:
+// 					'first first'
+// 					'second third';
+// 			}
+// 		`}
+// 	>
+// 		{children}
+// 	</div>
+// );
 
 const GridOfFour = ({ children }: { children: React.ReactNode }) => (
 	<div
@@ -166,51 +169,51 @@ const TwoImage = ({
 	</div>
 );
 
-const ThreeImage = ({
-	images,
-	format,
-	caption,
-}: {
-	images: [ImageBlockElement, ImageBlockElement, ImageBlockElement];
-	format: ArticleFormat;
-	caption?: string;
-}) => (
-	<div css={wrapper}>
-		<OneAboveTwoGrid>
-			<GridItem area="first">
-				<ImageComponent
-					element={images[0]}
-					format={format}
-					hideCaption={true}
-					role={images[0].role}
-				/>
-			</GridItem>
-			<GridItem area="second">
-				<ImageComponent
-					element={images[1]}
-					format={format}
-					hideCaption={true}
-					role={images[1].role}
-				/>
-			</GridItem>
-			<GridItem area="third">
-				<ImageComponent
-					element={images[2]}
-					format={format}
-					hideCaption={true}
-					role={images[2].role}
-				/>
-			</GridItem>
-		</OneAboveTwoGrid>
-		{!!caption && (
-			<Caption
-				captionText={caption}
-				format={format}
-				shouldLimitWidth={false}
-			/>
-		)}
-	</div>
-);
+// const ThreeImage = ({
+// 	images,
+// 	format,
+// 	caption,
+// }: {
+// 	images: [ImageBlockElement, ImageBlockElement, ImageBlockElement];
+// 	format: ArticleFormat;
+// 	caption?: string;
+// }) => (
+// 	<div css={wrapper}>
+// 		<OneAboveTwoGrid>
+// 			<GridItem area="first">
+// 				<ImageComponent
+// 					element={images[0]}
+// 					format={format}
+// 					hideCaption={true}
+// 					role={images[0].role}
+// 				/>
+// 			</GridItem>
+// 			<GridItem area="second">
+// 				<ImageComponent
+// 					element={images[1]}
+// 					format={format}
+// 					hideCaption={true}
+// 					role={images[1].role}
+// 				/>
+// 			</GridItem>
+// 			<GridItem area="third">
+// 				<ImageComponent
+// 					element={images[2]}
+// 					format={format}
+// 					hideCaption={true}
+// 					role={images[2].role}
+// 				/>
+// 			</GridItem>
+// 		</OneAboveTwoGrid>
+// 		{!!caption && (
+// 			<Caption
+// 				captionText={caption}
+// 				format={format}
+// 				shouldLimitWidth={false}
+// 			/>
+// 		)}
+// 	</div>
+// );
 
 const FourImage = ({
 	images,
@@ -271,6 +274,39 @@ const FourImage = ({
 	</div>
 );
 
+const Slideshow = ({
+	images,
+	format,
+	caption,
+}: {
+	images: ImageBlockElement[];
+	format: ArticleFormat;
+	caption?: string;
+}) => {
+	const imagesForSlideshow = images.map((element) => {
+		/** Legacy images do not have a master so we fallback to the largest available */
+		const image =
+			getMaster(element.media.allImages) ??
+			getLargest(element.media.allImages);
+		return {
+			imageSrc: image?.url ?? '',
+			imageCaption: element.data.caption,
+		};
+	});
+	return (
+		<Island priority="feature" defer={{ until: 'visible' }}>
+			<SlideshowCarousel
+				images={imagesForSlideshow}
+				imageSize="medium"
+				hasNavigationBackgroundColour={false}
+				linkTo="https://www.theguardian.com"
+				linkAriaLabel="news | group-3 | card-@1 | media-slideshow"
+				dataLinkName="slideshow-carousel-1"
+			/>
+		</Island>
+	);
+};
+
 export const MultiImageBlockComponent = ({
 	images,
 	format,
@@ -290,7 +326,7 @@ export const MultiImageBlockComponent = ({
 
 	if (one && two && three) {
 		return (
-			<ThreeImage
+			<Slideshow
 				images={[one, two, three]}
 				format={format}
 				caption={caption}
