9e0f1fa86ae4fac0f55c29a41deb71d63c4fa831
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2026-02-19 09:55:56 +0000

    Update return type of buildBrazeBannersSystemConfig to use BrazeBannersSystemMeta

--- dotcom-rendering/src/components/StickyBottomBanner.importable.tsx
+++ dotcom-rendering/src/components/StickyBottomBanner.importable.tsx
@@ -255,7 +255,7 @@ const buildBrazeBannersSystemConfig = (
 	contentType: string,
 	shouldHideReaderRevenue: boolean,
 	tags: TagType[],
-): CandidateConfig<any> => {
+): CandidateConfig<BrazeBannersSystemMeta> => {
 	return {
 		candidate: {
 			id: 'braze-banners-system_StickyBottomBanner',
