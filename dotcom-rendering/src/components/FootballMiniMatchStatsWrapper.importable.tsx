dabd0cfd624648dc2ac85109b2c8e9f629a9dc9e
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-02-23 18:10:02 +0000

    Wrapper padding to align within live blog layout

--- dotcom-rendering/src/components/FootballMiniMatchStatsWrapper.importable.tsx
+++ dotcom-rendering/src/components/FootballMiniMatchStatsWrapper.importable.tsx
@@ -1,15 +1,29 @@
+import { css } from '@emotion/react';
+import { from } from '@guardian/source/foundations';
 import { FootballMiniMatchStats } from './FootballMiniMatchStats';
+
+const wrapperStyles = css`
+	padding: 10px 10px 0;
+	${from.mobileLandscape} {
+		padding: 20px 20px 0;
+	}
+	${from.desktop} {
+		padding: 24px 0 20px 20px;
+	}
+`;
 
 export const FootballMiniMatchStatsWrapper = ({
 	matchStatsUrl,
 }: {
 	matchStatsUrl: string;
 }) => (
-	<FootballMiniMatchStats
-		matchStatsUrl={matchStatsUrl}
-		getMatchStatsData={getMatchStatsData}
-		refreshInterval={16_000}
-	/>
+	<div css={wrapperStyles}>
+		<FootballMiniMatchStats
+			matchStatsUrl={matchStatsUrl}
+			getMatchStatsData={getMatchStatsData}
+			refreshInterval={16_000}
+		/>
+	</div>
 );
 
 const getMatchStatsData = (url: string): Promise<unknown> =>
