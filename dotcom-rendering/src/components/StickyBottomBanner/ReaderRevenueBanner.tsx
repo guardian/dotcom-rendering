99282b11793b3ecd04a09a1706056fd8b37ce237
Juarez Mota <juarez.mota.mindera@guardian.co.uk>
2026-02-25 09:44:08 +0000

    feat: remove banner-version query parameter handling from ReaderRevenueBanner

--- dotcom-rendering/src/components/StickyBottomBanner/ReaderRevenueBanner.tsx
+++ dotcom-rendering/src/components/StickyBottomBanner/ReaderRevenueBanner.tsx
@@ -304,9 +304,6 @@ export const ReaderRevenueBanner = ({
 	const [Banner, setBanner] = useState<React.ElementType | null>(null);
 
 	useEffect(() => {
-		const params = new URLSearchParams(window.location.search);
-		const version = params.get('banner-version');
-
 		(name === 'SignInPromptBanner'
 			? /* webpackChunkName: "sign-in-prompt-banner" */
 			  import(`../marketing/banners/signInPrompt/SignInPromptBanner`)
@@ -314,9 +311,7 @@ export const ReaderRevenueBanner = ({
 			  import(`../marketing/banners/designableBanner/v2/Banner`)
 		)
 			.then((bannerModule: { [key: string]: React.ElementType }) => {
-				// When using banner-version=v2, always use DesignableBanner export
-				const bannerName = version === 'v2' ? 'DesignableBanner' : name;
-				setBanner(() => bannerModule[bannerName] ?? null);
+				setBanner(() => bannerModule[name] ?? null);
 			})
 			.catch((error) => {
 				const msg = `Error importing RR banner: ${String(error)}`;
