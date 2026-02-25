4208ec951b32dab364561feb0a19a5bf8798e368
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-02-25 12:03:50 +0000

    Apply background colour and new stacking context
    These is to ensure the component appears to flow over any ads when it is
    placed in the left column of the live blog layout on desktop

--- dotcom-rendering/src/components/FootballMiniMatchStatsWrapper.importable.tsx
+++ dotcom-rendering/src/components/FootballMiniMatchStatsWrapper.importable.tsx
@@ -1,29 +1,15 @@
-import { css } from '@emotion/react';
-import { from } from '@guardian/source/foundations';
 import { FootballMiniMatchStats } from './FootballMiniMatchStats';
-
-const wrapperStyles = css`
-	padding: 10px 10px 0;
-	${from.mobileLandscape} {
-		padding: 20px 20px 0;
-	}
-	${from.desktop} {
-		padding: 24px 0 20px 20px;
-	}
-`;
 
 export const FootballMiniMatchStatsWrapper = ({
 	matchStatsUrl,
 }: {
 	matchStatsUrl: string;
 }) => (
-	<div css={wrapperStyles}>
-		<FootballMiniMatchStats
-			matchStatsUrl={matchStatsUrl}
-			getMatchStatsData={getMatchStatsData}
-			refreshInterval={16_000}
-		/>
-	</div>
+	<FootballMiniMatchStats
+		matchStatsUrl={matchStatsUrl}
+		getMatchStatsData={getMatchStatsData}
+		refreshInterval={16_000}
+	/>
 );
 
 const getMatchStatsData = (url: string): Promise<unknown> =>
