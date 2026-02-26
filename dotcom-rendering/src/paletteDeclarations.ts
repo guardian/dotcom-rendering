ac8b3fb4e0da7862258702794cfa26dc914a293f
Georges Lebreton <102960844+Georges-GNM@users.noreply.github.com>
2026-02-26 09:49:14 +0000

    Tweak storylines component (like/dislike, section positioning and pagination) (#15227)
    * Tweak storylines component by moving the like/dislike buttons to below the text and ensuring the component and pagination get correctly displayed after the first container even if there's only one container
    
    * lint
    
    * simplify tag page layout logic
    
    * remove unnecessary code
    
    * fix syntax
    
    * Apply suggestions from code review
    
    Co-authored-by: Dominik Lander <dominik.lander@guardian.co.uk>
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>
    
    * Further tweaks, inc: adding a beta label svg, tightening the spacing in the selected title, and removing the pillar colours to instead just use the main brand colour
    
    * tweak pagination logic and side text
    
    * rename storylinesStyle in footer to isStorylines and add colours in storylinesSectionContent to palette
    
    * Implement PR feedback suggestions, removing unused props, add svg title, remove fragment
    
    ---------
    
    Co-authored-by: Dominik Lander <dominik.lander@guardian.co.uk>
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>

--- dotcom-rendering/src/paletteDeclarations.ts
+++ dotcom-rendering/src/paletteDeclarations.ts
@@ -6283,6 +6283,12 @@ const crosswordCluesHeaderBorderBottom: PaletteFunction = () =>
 const crosswordTextLight: PaletteFunction = () => sourcePalette.neutral[7];
 const crosswordTextDark: PaletteFunction = () => sourcePalette.neutral[86];
 
+const storylinesBackground: PaletteFunction = () => sourcePalette.neutral[93];
+const storylinesBorder: PaletteFunction = () => sourcePalette.neutral[86];
+const storylinesTitles: PaletteFunction = () => sourcePalette.brand[400];
+const storylinesActiveTab: PaletteFunction = () => sourcePalette.neutral[60];
+const storylinesInactiveTab: PaletteFunction = () => sourcePalette.neutral[38];
+
 // ----- Palette ----- //
 
 /**
@@ -8076,6 +8082,26 @@ const paletteColours = {
 	'--star-rating-fill': {
 		light: starRatingFillColourLight,
 		dark: starRatingFillColourDark,
+	},
+	'--storylines-active-tab': {
+		light: storylinesActiveTab,
+		dark: storylinesActiveTab,
+	},
+	'--storylines-background': {
+		light: storylinesBackground,
+		dark: storylinesBackground,
+	},
+	'--storylines-border': {
+		light: storylinesBorder,
+		dark: storylinesBorder,
+	},
+	'--storylines-inactive-tab': {
+		light: storylinesInactiveTab,
+		dark: storylinesInactiveTab,
+	},
+	'--storylines-titles': {
+		light: storylinesTitles,
+		dark: storylinesTitles,
 	},
 	'--straight-lines': {
 		light: straightLinesLight,
