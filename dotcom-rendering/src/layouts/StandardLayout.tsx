6d50bcf934990d856ed4a63608ce841deb29be54
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2026-03-11 13:00:05 +0000

    Move all column settings into layout config

--- dotcom-rendering/src/layouts/StandardLayout.tsx
+++ dotcom-rendering/src/layouts/StandardLayout.tsx
@@ -45,7 +45,7 @@ import { Standfirst } from '../components/Standfirst';
 import { StickyBottomBanner } from '../components/StickyBottomBanner.island';
 import { SubMeta } from '../components/SubMeta';
 import { SubNav } from '../components/SubNav.island';
-import { type ColumnPreset, grid } from '../grid';
+import { grid } from '../grid';
 import {
 	ArticleDesign,
 	ArticleDisplay,
@@ -69,6 +69,7 @@ import {
 	type LayoutType,
 } from './lib/articleArrangements';
 import { BannerWrapper, Stuck } from './lib/stickiness';
+import { Grid } from 'src/components/Masthead/Titlepiece/Grid';
 
 const stretchLines = css`
 	${until.phablet} {
