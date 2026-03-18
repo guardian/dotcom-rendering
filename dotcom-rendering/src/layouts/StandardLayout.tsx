37c4f2add8c3a7e377dda6cb38d6de3e80bb4d26
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2026-03-18 16:49:17 +0000

    Rebase tidy

--- dotcom-rendering/src/layouts/StandardLayout.tsx
+++ dotcom-rendering/src/layouts/StandardLayout.tsx
@@ -60,7 +60,6 @@ import type { ArticleDeprecated } from '../types/article';
 import type { RenderingTarget } from '../types/renderingTarget';
 import { type Area, gridCss, type LayoutType } from './lib/furnitureLayouts';
 import { BannerWrapper, Stuck } from './lib/stickiness';
-import { Grid } from 'src/components/Masthead/Titlepiece/Grid';
 
 const stretchLines = css`
 	${until.phablet} {
@@ -168,11 +167,7 @@ export const StandardLayout = (props: WebProps | AppProps) => {
 
 	const renderAds = canRenderAds(article);
 
-	const layoutType: LayoutType = isMatchReport
-		? 'matchReport'
-		: isMedia
-		? 'media'
-		: 'standard';
+	const layoutType: LayoutType = isMedia ? 'media' : 'standard';
 
 	return (
 		<>
@@ -245,12 +240,20 @@ export const StandardLayout = (props: WebProps | AppProps) => {
 					pageId={article.pageId}
 					pageTags={article.tags}
 				/>
-				<article css={css(grid.container)}>
-					<GridItem
-						area="main-media"
-						layoutType={layoutType}
-						element="div"
-					>
+				<article
+					css={css([
+						css`
+							background-color: ${themePalette(
+								'--article-background',
+							)};
+						`,
+						grid.container,
+						grid.verticalRules({
+							centre: isLabs ? false : true,
+						}),
+					])}
+				>
+					<GridItem area="main-media" layoutType={layoutType}>
 						<MainMedia
 							format={format}
 							elements={article.mainMediaElements}
@@ -283,11 +286,7 @@ export const StandardLayout = (props: WebProps | AppProps) => {
 							isMatch={!!footballMatchUrl}
 						/>
 					</GridItem>
-					<GridItem
-						area="headline"
-						layoutType={layoutType}
-						element="div"
-					>
+					<GridItem area="headline" layoutType={layoutType}>
 						<ArticleHeadline
 							format={format}
 							headlineString={article.headline}
@@ -299,11 +298,7 @@ export const StandardLayout = (props: WebProps | AppProps) => {
 							starRating={article.starRating}
 						/>
 					</GridItem>
-					<GridItem
-						area="standfirst"
-						layoutType={layoutType}
-						element="div"
-					>
+					<GridItem area="standfirst" layoutType={layoutType}>
 						<Standfirst
 							format={format}
 							standfirst={article.standfirst}
@@ -402,25 +397,10 @@ export const StandardLayout = (props: WebProps | AppProps) => {
 										article.mainMediaElements
 									}
 								/>
-								<MatchInfoContainer
-									isMatchReport={isMatchReport}
-									footballMatchStatsUrl={
-										footballMatchStatsUrl
-									}
-								/>
-
-								{isApps && (
-									<Island
-										priority="critical"
-										defer={{ until: 'visible' }}
-									>
-										<AppsEpic />
-									</Island>
-								)}
 							</>
 						)}
 					</GridItem>
-					<GridItem area="body" layoutType={layoutType} element="div">
+					<GridItem area="body" layoutType={layoutType}>
 						{/* Only show Listen to Article button on App landscape views */}
 						{isApps && (
 							<Hide until="leftCol">
