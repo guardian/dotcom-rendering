601205e5ce536f747fdb7789fe50f1bb19d6c00f
Dominik Lander <dominik.lander@guardian.co.uk>
2026-03-23 15:26:33 +0000

    Fix test

--- dotcom-rendering/src/lib/video.test.ts
+++ dotcom-rendering/src/lib/video.test.ts
@@ -1,27 +1,44 @@
+import type { VideoAssets } from '../types/content';
+import type { Source } from './video';
 import { convertAssetsToVideoSources } from './video';
 
-const mp4Asset = {
+const mp4Asset: VideoAssets = {
 	url: 'https://guim-example.co.uk/atomID-1.mp4',
 	mimeType: 'video/mp4',
+	dimensions: {
+		height: 720,
+		width: 1280,
+	},
 };
-
-const m3u8Asset = {
+const m3u8Asset: VideoAssets = {
 	url: 'https://guim-example.co.uk/atomID-1.m3u8',
 	mimeType: 'application/x-mpegURL',
+	dimensions: {
+		height: 720,
+		width: 1280,
+	},
 };
-const unsupportedAsset = {
+const unsupportedAsset: VideoAssets = {
 	url: 'https://guim-example.co.uk/atomID-1.mov',
 	mimeType: 'video/quicktime',
+	dimensions: {
+		height: 720,
+		width: 1280,
+	},
 };
 
-const mp4Src = {
+const mp4Src: Source = {
 	src: 'https://guim-example.co.uk/atomID-1.mp4',
 	mimeType: 'video/mp4',
+	height: 720,
+	width: 1280,
 };
 
-const m3u8Src = {
+const m3u8Src: Source = {
 	src: 'https://guim-example.co.uk/atomID-1.m3u8',
 	mimeType: 'application/x-mpegURL',
+	height: 720,
+	width: 1280,
 };
 
 describe('convertAssetsToVideoSources', () => {
