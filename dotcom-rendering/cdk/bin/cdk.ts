1880bbb754fdc586b6e877fc316d51cc9e8e5650
Ravi <7014230+arelra@users.noreply.github.com>
2026-03-26 14:40:51 +0000

    Update article-rendering instances size to 30

--- dotcom-rendering/cdk/bin/cdk.ts
+++ dotcom-rendering/cdk/bin/cdk.ts
@@ -28,8 +28,8 @@ new RenderingCDKStack(cdkApp, 'ArticleRendering-PROD', {
 	stage: 'PROD',
 	domainName: 'article-rendering.guardianapis.com',
 	scaling: {
-		minimumInstances: 42,
-		maximumInstances: 240,
+		minimumInstances: 30,
+		maximumInstances: 300,
 		policies: {
 			step: {
 				cpu: cpuScalingSteps,
