3d71e668c2aed39a1ea01aa30dbba0a1134da89e
Georges Lebreton <georges.lebreton@guardian.co.uk>
2026-04-28 10:33:43 +0100

    remove storylines test

--- ab-testing/config/abTests.ts
+++ ab-testing/config/abTests.ts
@@ -97,19 +97,6 @@ const ABTests: ABTest[] = [
 		shouldForceMetricsCollection: false,
 	},
 	{
-		name: "fronts-and-curation-tag-page-storylines",
-		description:
-			"Testing the AI generated storylines component on tag pages",
-		owners: ["fronts.and.curation@guardian.co.uk"],
-		expirationDate: `2026-04-30`,
-		type: "server",
-		status: "ON",
-		audienceSize: 0 / 100,
-		audienceSpace: "B",
-		groups: ["control", "variant"],
-		shouldForceMetricsCollection: false,
-	},
-	{
 		name: "commercial-hosted-content",
 		description: "Preview the Hosted Content pages using dotcom-rendering",
 		owners: ["commercial.dev@guardian.co.uk"],
