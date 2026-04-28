711e211cc756bb67646f7f3ad3e77ac77cc9924f
Georges Lebreton <georges.lebreton@guardian.co.uk>
2026-04-28 12:16:01 +0100

    remove required code for rendering tag page storylines, including model/component/stories changes

--- dotcom-rendering/src/components/CardHeadline.tsx
+++ dotcom-rendering/src/components/CardHeadline.tsx
@@ -58,7 +58,6 @@ type Props = {
 	kickerColour?: string;
 	quoteColour?: string;
 	kickerImage?: PodcastSeriesImage;
-	isStorylines?: boolean;
 };
 
 const sublinkStyles = css`
@@ -235,7 +234,6 @@ export const CardHeadline = ({
 	kickerColour = palette('--card-kicker-text'),
 	quoteColour = palette('--card-quote-icon'),
 	kickerImage,
-	isStorylines,
 }: Props) => {
 	// The link is only applied directly to the headline if it is a sublink
 	const isSublink = !!linkTo;
@@ -249,7 +247,7 @@ export const CardHeadline = ({
 					isSublink ? 'card-sublink-headline' : 'card-headline'
 				}`}
 				css={[
-					isSublink && !isStorylines
+					isSublink
 						? css`
 								${textSans14}
 						  `
