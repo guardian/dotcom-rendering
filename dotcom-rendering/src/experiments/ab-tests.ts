f677281c71c644a0e08388baa9e4cb1c88df5f99
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2026-05-05 12:46:34 +0100

    Revert file back to original version

--- dotcom-rendering/src/experiments/ab-tests.ts
+++ dotcom-rendering/src/experiments/ab-tests.ts
@@ -2,4 +2,7 @@ import type { ABTest } from '@guardian/ab-core';
 import { abTestTest } from './tests/ab-test-test';
 import { noAuxiaSignInGate } from './tests/no-auxia-sign-in-gate';
 
+// keep in sync with ab-tests in frontend
+// https://github.com/guardian/frontend/tree/main/static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+
 export const tests: ABTest[] = [abTestTest, noAuxiaSignInGate];
