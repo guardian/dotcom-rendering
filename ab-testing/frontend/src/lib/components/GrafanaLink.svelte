85bce3ba01ce7fa451b48b254f60b0016adfd96d
tomaioo <hongsaygio.com@gmail.com>
2026-05-08 05:17:05 -0700

    fix(security): 2 improvements across 2 files
    - Security: Tabnabbing vulnerability in external links
    - Security: Tabnabbing vulnerability in external links
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- ab-testing/frontend/src/lib/components/GrafanaLink.svelte
+++ ab-testing/frontend/src/lib/components/GrafanaLink.svelte
@@ -6,9 +6,10 @@
 	const { testName }: Props = $props();
 </script>
 
-<a
-	href={`https://metrics.gutools.co.uk/d/bfd4abner943ke/page-views?folderUid=efd48ip6ch3i8f&orgId=1&from=now-7d&to=now&var-test_name=${testName}`}
-	target="_blank"
->
+	<a
+		href={`https://metrics.gutools.co.uk/d/bfd4abner943ke/page-views?folderUid=efd48ip6ch3i8f&orgId=1&from=now-7d&to=now&var-test_name=${testName}`}
+		target="_blank"
+		rel="noopener noreferrer"
+	>
 	Grafana
 </a>
