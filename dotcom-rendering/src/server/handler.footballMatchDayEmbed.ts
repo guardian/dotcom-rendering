459b1e008547a6faa901ceb3fb1c88bcc1fa2e99
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-05-14 17:59:35 +0100

    Parse and validate match data

--- dotcom-rendering/src/server/handler.footballMatchDayEmbed.ts
+++ dotcom-rendering/src/server/handler.footballMatchDayEmbed.ts
@@ -1,7 +1,67 @@
 import type { RequestHandler } from 'express';
+import { getParserErrorMessage, parse } from '../footballMatches';
+import type { FEFootballCompetition } from '../frontend/feFootballDataPage';
+import type { FEFootballMatchListPage } from '../frontend/feFootballMatchListPage';
+import { validateAsFootballMatchListPage } from '../model/validate';
+import type { FootballMatchListPage, Region } from '../sportDataPage';
+import { sortRegionsFunction } from './handler.sportDataPage';
 import { renderFootballMatchDayEmbed } from './render.footballMatchDayEmbed';
 
-export const handleFootballMatchDayEmbed: RequestHandler = (_, res) => {
-	const { html } = renderFootballMatchDayEmbed();
+export const handleFootballMatchDayEmbed: RequestHandler = ({ body }, res) => {
+	const footballDataValidated: FEFootballMatchListPage =
+		validateAsFootballMatchListPage(body);
+
+	const parsedFootballData = parseFEFootballMatchList(footballDataValidated);
+
+	const { html } = renderFootballMatchDayEmbed(
+		parsedFootballData.matchesList,
+	);
 	res.status(200).send(html);
+};
+
+const parseFEFootballMatchList = (
+	data: FEFootballMatchListPage,
+): FootballMatchListPage => {
+	const parsedMatchesList = parse(data.matchesList);
+
+	if (!parsedMatchesList.ok) {
+		throw new Error(
+			`Failed to parse matches: ${getParserErrorMessage(
+				parsedMatchesList.error,
+			)}`,
+		);
+	}
+
+	return {
+		matchesList: parsedMatchesList.value,
+		now: new Date().toISOString(),
+		kind: 'FootballLiveScores',
+		nextPage: data.nextPage,
+		nextPageNoJsUrl: undefined,
+		previousPage: data.previousPage,
+		regions: parseFEFootballCompetitionRegions(data.filters),
+		editionId: data.editionId,
+		guardianBaseURL: data.guardianBaseURL,
+		config: data.config,
+		pageFooter: data.pageFooter,
+		isAdFreeUser: data.isAdFreeUser,
+		canonicalUrl: data.canonicalUrl,
+		contributionsServiceUrl: data.contributionsServiceUrl,
+	};
+};
+
+const parseFEFootballCompetitionRegions = (
+	competitionRegions: Record<string, FEFootballCompetition[]>,
+): Region[] => {
+	return Object.entries(competitionRegions)
+		.map(([key, competition]) => {
+			return {
+				name: key,
+				competitions: competition.map((comp) => ({
+					url: comp.url,
+					name: comp.name,
+				})),
+			};
+		})
+		.sort(sortRegionsFunction);
 };
--- dotcom-rendering/src/server/render.footballMatchDayEmbed.tsx
+++ dotcom-rendering/src/server/render.footballMatchDayEmbed.tsx
@@ -1,12 +1,19 @@
+import CleanCSS from 'clean-css';
+import { FootballMatchDay } from '../components/FootballMatchDay';
+import type { FootballMatches } from '../footballMatches';
 import { renderToStringWithEmotion } from '../lib/emotion';
+import { rawFontsCss } from '../lib/fonts-css';
 
 const template = (html: string, css: string): string => {
+	const minifiedFontsCss = new CleanCSS().minify(rawFontsCss).styles;
 	return `<!doctype html>
 		<html lang="en">
 			<head>
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 				<meta name="robots" content="noindex">
+				<style>${minifiedFontsCss}</style>
+				<style>body{margin:0;}</style>
 				${css}
 			</head>
 			<body>
@@ -15,8 +22,16 @@ const template = (html: string, css: string): string => {
 		</html>`;
 };
 
-export const renderFootballMatchDayEmbed = (): { html: string } => {
-	const { html, extractedCss } = renderToStringWithEmotion(<></>);
+export const renderFootballMatchDayEmbed = (
+	matches: FootballMatches,
+): { html: string } => {
+	const { html, extractedCss } = renderToStringWithEmotion(
+		<FootballMatchDay
+			matches={matches}
+			guardianBaseUrl="https://www.theguardian.com"
+			edition="UK"
+		/>,
+	);
 
 	const pageHtml = template(html, extractedCss);
 	return {
