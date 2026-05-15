ba15dc7d0b4fce2c9dee5fd1c73e1ae734fee39e
Harry Fischer <harry.j.fischer@gmail.com>
2026-05-15 16:55:08 +0100

    update style to match new design and add image assets

--- dotcom-rendering/src/components/DirectoryPageNav.tsx
+++ dotcom-rendering/src/components/DirectoryPageNav.tsx
@@ -3,13 +3,10 @@ import {
 	type Breakpoint,
 	breakpoints,
 	from,
-	headlineBold15Object,
-	headlineBold17Object,
 	headlineBold24Object,
 	headlineBold42Object,
-	headlineMedium15Object,
-	headlineMedium17Object,
 	palette,
+	textSans14Object,
 } from '@guardian/source/foundations';
 import { grid } from '../grid';
 import { generateImageURL } from '../lib/image';
@@ -25,6 +22,7 @@ interface DirectoryPageNavConfig {
 	tagIds: string[];
 	textColor: string;
 	backgroundColor: string;
+	titleIconImage: string;
 	title: { label: string; id: string };
 	links: { label: string; id: string }[];
 	backgroundImages?: {
@@ -33,6 +31,7 @@ interface DirectoryPageNavConfig {
 		phablet: string;
 		tablet: string;
 		desktop: string;
+		wide?: string;
 	};
 }
 
@@ -51,6 +50,8 @@ const configs = [
 			label: 'World Cup',
 			id: 'football/world-cup-2026',
 		},
+		titleIconImage:
+			'data:image/svg+xml,%3Csvg%20width%3D%2240%22%20height%3D%2246%22%20viewBox%3D%220%200%2040%2046%22%20fill%3D%22none%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Crect%20width%3D%2212.3697%22%20height%3D%2232.4706%22%20fill%3D%22%23D71921%22/%3E%3Crect%20x%3D%2213.5294%22%20y%3D%2213.5295%22%20width%3D%2212.3697%22%20height%3D%2232.4706%22%20fill%3D%22white%22/%3E%3Crect%20x%3D%2227.059%22%20width%3D%2212.3697%22%20height%3D%2232.4706%22%20fill%3D%22%23007E46%22/%3E%3Ccircle%20cx%3D%2219.7142%22%20cy%3D%226.18487%22%20r%3D%226.18487%22%20fill%3D%22white%22/%3E%3C/svg%3E',
 		links: [
 			{
 				label: 'Match centre',
@@ -73,16 +74,17 @@ const configs = [
 				id: 'football',
 			},
 		],
-		// backgroundImages: {
-		// 	mobile: '',
-		// 	mobileLandscape:
-		// 		'',
-		// 	phablet:
-		// 		'',
-		// 	tablet: '',
-		// 	desktop:
-		// 		'',
-		// },
+		backgroundImages: {
+			mobile: 'https://media.guim.co.uk/4ba0caac6d18c1fe6a5a3267b270d8c21ae6f940/0_0_750_376/750.jpg',
+			mobileLandscape:
+				'https://media.guim.co.uk/4ba0caac6d18c1fe6a5a3267b270d8c21ae6f940/0_0_750_376/750.jpg',
+			phablet:
+				'https://media.guim.co.uk/4ba0caac6d18c1fe6a5a3267b270d8c21ae6f940/0_0_750_376/750.jpg',
+			tablet: 'https://media.guim.co.uk/861646115875f3f246313036f754b2f5f1480b1a/0_0_1480_276/1480.jpg',
+			desktop:
+				'https://media.guim.co.uk/167bec4a208bfc7fdc6b2127186b9bb183932259/0_0_1960_276/1960.jpg',
+			wide: 'https://media.guim.co.uk/4e44f9a88fcc9a3b1b5294f7e581644baa75c904/0_0_2600_276/2600.jpg',
+		},
 	},
 	// Winter Olympics 2026
 	{
@@ -99,6 +101,7 @@ const configs = [
 			label: 'Winter Olympics 2026',
 			id: 'sport/winter-olympics-2026',
 		},
+		titleIconImage: '',
 		links: [
 			{
 				label: 'Schedule',
@@ -142,6 +145,7 @@ const configs = [
 			label: 'Winter Paralympics 2026',
 			id: 'sport/winter-paralympics-2026',
 		},
+		titleIconImage: '',
 		links: [
 			{
 				label: 'Results',
@@ -188,14 +192,20 @@ export const DirectoryPageNav = ({ pageId, pageTags }: Props) => {
 		backgroundColor,
 		'&': css(grid.paddedContainer),
 		alignContent: 'space-between',
+		position: 'relative',
 	});
 
 	const largeLinkStyles = css({
+		position: 'absolute',
+		top: '4px',
+		left: 0,
 		...headlineBold24Object,
 		color: textColor,
 		textDecoration: 'none',
 		'&': css(grid.column.centre),
 		gridRow: 1,
+		display: 'flex',
+		alignItems: 'flex-start',
 		[from.tablet]: headlineBold42Object,
 		[from.leftCol]: css(
 			grid.between('left-column-start', 'right-column-end'),
@@ -204,35 +214,38 @@ export const DirectoryPageNav = ({ pageId, pageTags }: Props) => {
 
 	const list = css({
 		display: 'flex',
-		flexWrap: 'wrap',
 		'&': css(grid.column.all),
-		gridRow: 2,
 		alignSelf: 'end',
 		position: 'relative',
 		'--top-border-gap': '1.55rem',
+		overflowX: 'scroll',
+		scrollbarWidth: 'none',
+		padding: '10px 10px',
+		borderTop: '1px solid',
+		borderColor: palette.brand[600],
+		height: '100%',
 		[from.mobileLandscape]: {
-			paddingLeft: 10,
+			padding: '10px 20px',
 		},
 		[from.tablet]: {
 			'--top-border-gap': '3rem',
 		},
-		backgroundImage: `
-			linear-gradient(
-				${textColor} 0,
-				${textColor} 1px,
-				transparent 1px,
-				transparent var(--top-border-gap),
-				${textColor} var(--top-border-gap),
-				${textColor} calc(var(--top-border-gap) + 1px),
-				transparent 1px,
-				transparent var(--top-border-gap)
-			)
-		`,
+		'&:after': {
+			content: '""',
+			position: 'sticky',
+			right: '-10px',
+			top: '-10px',
+			height: '100%',
+			minWidth: 40,
+			background: `linear-gradient(to left, ${backgroundColor}, transparent)`,
+			[from.mobileLandscape]: {
+				right: '-20px',
+			},
+		},
 	});
 
 	const selectedStyles = {
-		'--selected-height': '4px',
-		'--selected-opacity': '1',
+		fontWeight: 'bold',
 	};
 
 	const listItem = css({
@@ -249,47 +262,38 @@ export const DirectoryPageNav = ({ pageId, pageTags }: Props) => {
 			backgroundColor: textColor,
 			transition: 'height 0.3s ease-in-out, opacity 0.05s 0.1s linear',
 		},
-		'&:hover': selectedStyles,
+		[from.desktop]: {
+			'&:hover a': {
+				textDecoration: 'underline',
+				color: 'var(--masthead-nav-link-text-hover)',
+			},
+		},
 		[from.leftCol]: {
 			flexBasis: 160,
 		},
 	});
 
 	const smallLink = css({
-		...headlineBold15Object,
-		padding: '4px 10px 6px',
+		...textSans14Object,
+		padding: '4px 12px 6px 0',
 		display: 'block',
 		lineHeight: 1,
 		color: textColor,
 		textDecoration: 'none',
-		'&::after': {
-			content: '""',
-			display: 'block',
-			position: 'absolute',
-			top: 0,
-			right: 0,
-			width: 1,
-			height: '1.3rem',
-			backgroundColor: textColor,
-		},
-		[from.tablet]: headlineBold17Object,
+		whiteSpace: 'nowrap',
 		[from.leftCol]: {
 			padding: '9px 10px 10px',
 		},
 	});
 
-	const lastSmallLink = css(smallLink, {
-		...headlineMedium15Object,
-		lineHeight: 1,
-		[from.tablet]: headlineMedium17Object,
-	});
-
 	return (
 		<nav css={[nav, heightStyles]}>
-			<BackgroundImage images={config.backgroundImages} />
 			<a href={`/${config.title.id}`} css={largeLinkStyles}>
+				<TitleIconImage src={config.titleIconImage} />
 				{config.title.label}
 			</a>
+			<BackgroundImage images={config.backgroundImages} />
+
 			<ul css={list}>
 				{config.links.map((link, i) => (
 					<li
@@ -297,14 +301,7 @@ export const DirectoryPageNav = ({ pageId, pageTags }: Props) => {
 						css={listItem}
 						style={pageId === link.id ? selectedStyles : {}}
 					>
-						<a
-							href={`/${link.id}`}
-							css={
-								i === config.links.length - 1
-									? lastSmallLink
-									: smallLink
-							}
-						>
+						<a href={`/${link.id}`} css={smallLink}>
 							{link.label}
 						</a>
 					</li>
@@ -315,14 +312,33 @@ export const DirectoryPageNav = ({ pageId, pageTags }: Props) => {
 };
 
 const heightStyles = css({
-	height: 116,
+	height: '',
 	[from.tablet]: {
-		height: 140,
+		height: '',
 	},
 	[from.desktop]: {
-		height: 150,
+		height: '',
 	},
 });
+
+const TitleIconImage = (props: { src: string }) => {
+	if (!props.src) {
+		return null;
+	}
+
+	return (
+		<img
+			src={props.src}
+			alt=""
+			css={{
+				height: 40,
+				width: 'auto',
+				marginRight: 8,
+				objectFit: 'cover',
+			}}
+		/>
+	);
+};
 
 const BackgroundImage = (props: {
 	images: DirectoryPageNavConfig['backgroundImages'];
@@ -399,8 +415,9 @@ const breakpointToImageSize = (breakpoint: Breakpoint): ImageSize => {
 			return 'tablet';
 		case 'desktop':
 		case 'leftCol':
-		case 'wide':
 			return 'desktop';
+		case 'wide':
+			return 'wide';
 	}
 };
 
