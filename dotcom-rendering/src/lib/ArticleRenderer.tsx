6d95c66df2ebda8827566f83a20f884940403d88
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2026-05-19 18:18:30 +0100

    Stop applying legacy class to article element

--- dotcom-rendering/src/lib/ArticleRenderer.tsx
+++ dotcom-rendering/src/lib/ArticleRenderer.tsx
@@ -13,7 +13,6 @@ import { ArticleDesign, type ArticleFormat } from './articleFormat';
 import type { EditionId } from './edition';
 import { RenderArticleElement } from './renderElement';
 import { withSignInGateSlot } from './withSignInGateSlot';
-import { interactiveLayoutSwitchoverDate } from '../layouts/DecideLayout';
 
 // This is required for spacefinder to work!
 const commercialPosition = css`
@@ -210,7 +209,7 @@ export const ArticleRenderer = ({
 				// Note, this class MUST be on the *direct parent* of the
 				// elements for some legacy interactive styling to work.
 				format.design === ArticleDesign.Interactive &&
-				interactiveLayoutSwitchoverDate > new Date()
+				!isShinyNewInteractiveLayout
 					? interactiveLegacyClasses.contentMainColumn
 					: '',
 			].join(' ')}
