1a6a013831e23cd2d052c4b7abc84a36e253d1ad
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-05-20 11:31:45 +0100

    Reduce template to HTML fragment

--- dotcom-rendering/src/server/render.footballMatchDayEmbed.tsx
+++ dotcom-rendering/src/server/render.footballMatchDayEmbed.tsx
@@ -6,20 +6,7 @@ import type { MatchDayData } from './handler.footballMatchDayEmbed';
 
 const template = (html: string, css: string): string => {
 	const minifiedFontsCss = new CleanCSS().minify(rawFontsCss).styles;
-	return `<!doctype html>
-		<html lang="en">
-			<head>
-				<meta charset="utf-8">
-				<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-				<meta name="robots" content="noindex">
-				<style>${minifiedFontsCss}</style>
-				<style>body{margin:0;}</style>
-				${css}
-			</head>
-			<body>
-				${html}
-			</body>
-		</html>`;
+	return `<style>${minifiedFontsCss}</style>${css}${html}`;
 };
 
 export const renderFootballMatchDayEmbed = (
