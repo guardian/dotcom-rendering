1f31d9cae49e0be7691267912c0b546150e5137a
Charlotte <43961396+cemms1@users.noreply.github.com>
2026-06-01 14:29:46 +0100

    roll out hosted content dcr pages to 50% of the audience (#16013)

--- ab-testing/config/abTests.ts
+++ ab-testing/config/abTests.ts
@@ -77,7 +77,7 @@ const ABTests: ABTest[] = [
 		expirationDate: "2026-07-01",
 		type: "server",
 		status: "ON",
-		audienceSize: 20 / 100,
+		audienceSize: 50 / 100,
 		audienceSpace: "A",
 		groups: ["preview"],
 		shouldForceMetricsCollection: false,
