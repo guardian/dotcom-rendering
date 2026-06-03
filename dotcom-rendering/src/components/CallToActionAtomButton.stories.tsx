f3f38b4ba903bb335d5b842d13bb7bf9dd77ac13
Demetrios <dskamiotis@gmail.com>
2026-06-03 16:34:24 +0100

    update accentColour  for storybook test

--- dotcom-rendering/src/components/CallToActionAtomButton.stories.tsx
+++ dotcom-rendering/src/components/CallToActionAtomButton.stories.tsx
@@ -21,7 +21,7 @@ export const WithAccentColour = () => {
 		<CallToActionButton
 			linkUrl="https://www.trendmicro.com/vinfo/gb/security/research-and-analysis/predictions/the-ai-fication-of-cyberthreats-trend-micro-security-predictions-for-2026?utm_source=guardian&utm_medium=referral&utm_campaign=ent_cyber+risk_aw_e_ukie_int_guardian&utm_content=ghb"
 			buttonText="Explore more"
-			accentColor="#d71920"
+			accentColor="#0077B6"
 		/>
 	);
 };
