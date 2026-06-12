f3b5a1a709c57e03c6d5d721a4934694298c4b89
William Mead <285652121+williammead@users.noreply.github.com>
2026-06-12 14:51:22 +0100

    added explaining documentation

--- dotcom-rendering/webpack/jsdom-patch.js
+++ dotcom-rendering/webpack/jsdom-patch.js
@@ -1,3 +1,12 @@
+/*
+This is a patch found at: https://github.com/jsdom/jsdom/issues/3951
+
+When jsdom upgraded to v27.0.0 they stopped inlining a default-stylesheet.css file
+and instead started loading it from another directory. We do not have access to 
+that directory on the server so this patch replaces the "loading" code with the
+contents of the css file.
+*/
+
 const fs = require('fs');
 const path = require('path');
 
