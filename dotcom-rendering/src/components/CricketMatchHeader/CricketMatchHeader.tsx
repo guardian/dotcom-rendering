f7dd6f01bc46d0ee912a873a815ed80ecb6e022c
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2026-06-12 11:38:00 +0100

    `before` for football header borders (#16145)
    This ensures that parts of the design, such as the central border,
    remain aligned with other parts of the page.

--- dotcom-rendering/src/components/CricketMatchHeader/CricketMatchHeader.tsx
+++ dotcom-rendering/src/components/CricketMatchHeader/CricketMatchHeader.tsx
@@ -246,21 +246,27 @@ const Team = (props: { team: CricketTeam; match: CricketMatch }) => {
 				flex: '1 1 50%',
 				wordBreak: 'break-word',
 				borderLeftStyle: 'solid',
+				borderLeftColor: 'var(--border-left-colour)',
 				'&:last-of-type': {
 					paddingLeft: space[2],
 					borderLeftWidth: 1,
 				},
 				[from.leftCol]: {
-					paddingLeft: space[2],
-					borderLeftWidth: 1,
 					paddingTop: space[3],
-					'&:first-of-type': {
-						marginLeft: `-10px`,
+					position: 'relative',
+					'&:first-of-type::before': {
+						content: '""',
+						top: 0,
+						left: -10,
+						width: 1,
+						backgroundColor: 'var(--border-left-colour)',
+						position: 'absolute',
+						height: '100%',
 					},
 				},
 			}}
 			style={{
-				borderLeftColor: palette(border(props.match.kind)),
+				'--border-left-colour': palette(border(props.match.kind)),
 			}}
 		>
 			<TeamName name={props.team.name} />
