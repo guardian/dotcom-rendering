79f34e598728a4a88c9c4febda41699eb67d53ee
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2026-06-17 15:20:27 +0100

    Add nudgeIndex and idApiUrl to FeastContextualNudge story args

--- dotcom-rendering/src/components/FeastContextualNudge.stories.tsx
+++ dotcom-rendering/src/components/FeastContextualNudge.stories.tsx
@@ -43,6 +43,8 @@ const meta = {
 		recipeArticleTitle: "Meera Sodha's spring onion pancakes",
 		recipe: mockRecipe,
 		isDev: true,
+		nudgeIndex: 1,
+		idApiUrl: undefined,
 	},
 	parameters: {
 		chromatic: { viewports: [375, 740, 980] },
