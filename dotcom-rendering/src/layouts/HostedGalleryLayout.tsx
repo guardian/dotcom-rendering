38be413a300eb80d9b68bdb0f1e3b30823dd94cc
Charlotte <charlotte.emms@theguardian.com>
2026-06-22 11:50:55 +0100

    add hosted override colours to gallery layout

--- dotcom-rendering/src/layouts/HostedGalleryLayout.tsx
+++ dotcom-rendering/src/layouts/HostedGalleryLayout.tsx
@@ -105,6 +105,32 @@ const ctaButtonStyles = css`
 	margin-right: ${space[3]}px;
 `;
 
+/**
+ * Overrides palette declarations in light mode to use the accent color for the hosted content.
+ * @param accentColor - The accentColor to use for the hosted content in light mode.
+ * @returns A CSS string with the overridden palette declarations.
+ */
+export const overridePaletteColours = (accentColor?: string) => {
+	return css`
+		@media (prefers-color-scheme: light) {
+			--article-link-text: ${accentColor ?? 'inherit'};
+			--article-link-text-hover: ${accentColor ?? 'inherit'};
+			--article-link-border-hover: ${accentColor ?? 'inherit'};
+			--accent-colour: ${accentColor ?? `${sourcePalette.neutral[38]}`};
+			--lightbox-divider: ${accentColor ?? 'inherit'};
+		}
+		/* The following styles are to reflect the current accentColor behaviour in storybook as well so we maintain consistency */
+		[data-color-scheme='dark'] & {
+			--article-link-text: inherit;
+			--article-link-text-hover: inherit;
+			--article-link-border-hover: inherit;
+			/* This CSS variable only exists in the scope of hosted content and it isn't defined in the paletteDeclarations.ts */
+			--accent-colour: ${sourcePalette.neutral[86]};
+			--lightbox-divider: ${accentColor ?? 'inherit'};
+		}
+	`;
+};
+
 export const HostedGalleryLayout = (props: WebProps | AppProps) => {
 	const { gallery, renderingTarget, format, serverTime } = props;
 	const { frontendData } = gallery;
