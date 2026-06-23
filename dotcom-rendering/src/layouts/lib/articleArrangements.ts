93c354c15e5f1fdf293f349f9943d6845215f73a
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2026-06-23 16:14:51 +0100

    First, clumsy pass at immersive forking

--- dotcom-rendering/src/layouts/lib/articleArrangements.ts
+++ dotcom-rendering/src/layouts/lib/articleArrangements.ts
@@ -6,8 +6,10 @@ export type LayoutType =
 	| 'standard'
 	| 'showcase'
 	| 'media'
-	| 'immersiveLandscape'
-	| 'immersivePortrait';
+	| 'immersiveLandscapeDefault'
+	| 'immersiveLandscapeFeature'
+	| 'immersivePortraitDefault'
+	| 'immersivePortraitFeature';
 
 export type Area =
 	| 'title'
@@ -155,7 +157,7 @@ const mediaCss: LayoutCssMap = {
 	},
 };
 
-const immersivePortraitCss: LayoutCssMap = {
+const immersivePortraitDefaultCss: LayoutCssMap = {
 	title: {
 		mobile: 'grid-row: 1;',
 		tablet: 'grid-row: 1;',
@@ -198,7 +200,50 @@ const immersivePortraitCss: LayoutCssMap = {
 	},
 };
 
-const immersiveLandscapeCss: LayoutCssMap = {
+const immersivePortraitFeatureCss: LayoutCssMap = {
+	title: {
+		mobile: 'grid-row: 1;',
+		tablet: 'grid-row: 1;',
+		desktop: `grid-row: 1; ${grid.between('centre-column-start', 8)};`,
+		leftCol: `grid-row: 1; ${grid.between('left-column-start', 9)};`,
+	},
+	headline: {
+		mobile: 'grid-row: 2;',
+		tablet: 'grid-row: 2;',
+		desktop: `grid-row: 2; ${grid.between('centre-column-start', 8)};`,
+		leftCol: `grid-row: 2; ${grid.between('left-column-start', 9)};`,
+		wide: `grid-row: 2; ${grid.between('left-column-start', 10)};`,
+	},
+	media: {
+		mobile: 'grid-row: 3;',
+		tablet: 'grid-row: 3;',
+		desktop: `grid-row: 1 / span 4; ${grid.between('centre-column-start', 'right-column-end')};`,
+		leftCol: `grid-row: 1 / span 3; ${grid.between('left-column-start', 'right-column-end')};`,
+		wide: `grid-row: 1 / span 3; ${grid.between('left-column-start', 'right-column-end')};`,
+	},
+	standfirst: {
+		mobile: 'grid-row: 4;',
+		tablet: 'grid-row: 4;',
+		desktop: `grid-row: 3; ${grid.between('centre-column-start', 7)};`,
+		leftCol: `grid-row: 3; ${grid.between('centre-column-start', 8)};`,
+		wide: `grid-row: 3; ${grid.between('centre-column-start', 9)};`,
+	},
+	meta: {
+		mobile: 'grid-row: 5;',
+		tablet: 'grid-row: 5;',
+		desktop: `grid-row: 4; ${grid.between('centre-column-start', 8)};`,
+		leftCol: `grid-row: 3; ${grid.column.left};`,
+	},
+	body: {
+		mobile: 'grid-row: 6;',
+	},
+	'right-column': {
+		desktop: `grid-row: 5; ${grid.column.right};`,
+		leftCol: `grid-row: 4; ${grid.column.right};`,
+	},
+};
+
+const immersiveLandscapeDefaultCss: LayoutCssMap = {
 	title: {
 		mobile: 'grid-row: 1;',
 		tablet: 'grid-row: 1;',
@@ -234,12 +279,38 @@ const immersiveLandscapeCss: LayoutCssMap = {
 	},
 };
 
+const immersiveLandscapeFeatureCss: LayoutCssMap = {
+	title: {
+		desktop: 'grid-row: 2;',
+	},
+	headline: {
+		desktop: 'grid-row: 3 / span 2;',
+	},
+	media: {
+		mobile: `${grid.column.all}`,
+		desktop: `grid-row: 1 / span 3; ${grid.between('centre-column-start', 'right-column-end')};`,
+		leftCol: `grid-row: 1 / span 3; ${grid.between('left-column-start', 'right-column-end')};`,
+	},
+	standfirst: {
+		desktop: 'grid-row: 5;',
+	},
+	meta: {
+		desktop: `grid-row: 6; ${grid.between('centre-column-start', 8)};`,
+		leftCol: `grid-row: 6 / span 2; ${grid.column.left};`,
+	},
+	'right-column': {
+		desktop: `grid-row: 5 / span 3; ${grid.column.right};`,
+	},
+};
+
 const layoutCssMaps: Record<LayoutType, LayoutCssMap> = {
 	standard: standardCss,
 	showcase: showcaseCss,
 	media: mediaCss,
-	immersiveLandscape: immersiveLandscapeCss,
-	immersivePortrait: immersivePortraitCss,
+	immersiveLandscapeDefault: immersiveLandscapeDefaultCss,
+	immersiveLandscapeFeature: immersiveLandscapeFeatureCss,
+	immersivePortraitDefault: immersivePortraitDefaultCss,
+	immersivePortraitFeature: immersivePortraitFeatureCss,
 };
 
 /**
