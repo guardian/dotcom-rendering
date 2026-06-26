3822ddeeaa2fbe549453fc8bfc1b04f0cb86589f
Charlotte <charlotte.emms@theguardian.com>
2026-06-26 11:40:47 +0100

    combine full width styles with general styles for mobile-sticky

--- dotcom-rendering/src/components/AdSlot.web.tsx
+++ dotcom-rendering/src/components/AdSlot.web.tsx
@@ -349,16 +349,19 @@ const liveBlogTopContainerStyles = css`
 `;
 
 const mobileStickyAdStyles = css`
+	z-index: ${getZIndex('mobileSticky')};
 	position: fixed;
 	bottom: 0;
-	width: 320px;
+	width: 100%;
 	margin: 0 auto;
 	right: 0;
 	left: 0;
-	z-index: ${getZIndex('mobileSticky')};
+	text-align: center;
+	background-color: ${schemedPalette('--ad-background')};
 	${from.phablet} {
 		display: none;
 	}
+
 	.ad-slot__close-button {
 		display: none;
 		position: absolute;
@@ -401,15 +404,6 @@ const mobileStickyAdStyles = css`
 		display: block;
 		position: relative;
 		${labelBoxStyles}
-	}
-`;
-
-const mobileStickyAdStylesFullWidth = css`
-	width: 100%;
-	text-align: center;
-	background-color: ${palette.neutral[97]};
-
-	.ad-slot[data-label-show='true']::before {
 		padding-left: calc((100% - ${adSizes.mobilesticky.width}px) / 2);
 		padding-right: calc((100% - ${adSizes.mobilesticky.width}px) / 2);
 	}
@@ -990,8 +984,5 @@ export const AdSlot = ({
 };
 
 export const MobileStickyContainer = () => (
-	<div
-		className="mobilesticky-container"
-		css={[mobileStickyAdStyles, mobileStickyAdStylesFullWidth]}
-	/>
+	<div className="mobilesticky-container" css={mobileStickyAdStyles} />
 );
