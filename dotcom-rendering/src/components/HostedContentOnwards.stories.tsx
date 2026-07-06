7b50c2d243fc3259f97783c05f7768479537c7a4
Charlotte <charlotte.emms@theguardian.com>
2026-07-06 10:42:59 +0100

    fix story background for hosted gallery onwards

--- dotcom-rendering/src/components/HostedContentOnwards.stories.tsx
+++ dotcom-rendering/src/components/HostedContentOnwards.stories.tsx
@@ -1,7 +1,21 @@
+import { css } from '@emotion/react';
 import { hostedPaletteDecorator } from '../../.storybook/decorators/themeDecorator';
 import preview from '../../.storybook/preview';
 import { hostedOnwardsTrails } from '../../fixtures/manual/onwardsTrails';
+import type { ArticleFormat } from '../lib/articleFormat';
+import {
+	ArticleDesign,
+	ArticleDisplay,
+	ArticleSpecial,
+} from '../lib/articleFormat';
+import { palette } from '../palette';
 import { HostedContentOnwards } from './HostedContentOnwards';
+
+const hostedArticleFormat: ArticleFormat = {
+	theme: ArticleSpecial.Labs,
+	display: ArticleDisplay.Standard,
+	design: ArticleDesign.HostedArticle,
+};
 
 const meta = preview.meta({
 	component: HostedContentOnwards,
@@ -11,7 +25,18 @@ const meta = preview.meta({
 		brandName: 'TrendAI',
 		isGalleryPage: false,
 	},
-	render: (args) => <HostedContentOnwards {...args} />,
+	parameters: {
+		formats: [hostedArticleFormat],
+	},
+	render: (args) => (
+		<div
+			css={css`
+				background-color: ${palette('--article-background')};
+			`}
+		>
+			<HostedContentOnwards {...args} />
+		</div>
+	),
 });
 
 export const Default = meta.story({});
@@ -20,9 +45,16 @@ export const WithAccentColour = meta.story({
 	decorators: hostedPaletteDecorator('#d90c1f'),
 });
 
+const hostedGalleryFormat: ArticleFormat = {
+	...hostedArticleFormat,
+	design: ArticleDesign.HostedGallery,
+};
 export const HostedGallery = meta.story({
 	args: {
 		isGalleryPage: true,
 	},
-	decorators: hostedPaletteDecorator('#d90c1f'),
+	parameters: {
+		formats: [hostedGalleryFormat],
+	},
+	decorators: [hostedPaletteDecorator('#d90c1f')],
 });
