3274f3fdfeb9d532cd7e062fae202499958b9579
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2026-07-07 15:34:27 +0100

    Tidy up audio player components

--- dotcom-rendering/src/components/AudioPlayer/AudioPlayer.tsx
+++ dotcom-rendering/src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,3 +1,7 @@
+import { css } from '@emotion/react';
+import { space } from '@guardian/source/foundations';
+import { StraightLines } from '@guardian/source-development-kitchen/react-components';
+import { palette } from '../../palette';
 import { AudioPlayerApps } from '../AudioPlayerApps.island';
 import { AudioPlayerWeb } from '../AudioPlayerWeb.island';
 import { Island } from '../Island';
@@ -19,23 +23,33 @@ export const AudioPlayer = ({
 	isApps: boolean;
 }) => {
 	return (
-		<Island priority="critical" defer={{ until: 'visible' }}>
-			{isApps ? (
-				<AudioPlayerApps
-					audioDuration={
-						typeof audioData.durationSeconds === 'number'
-							? formatAudioDuration(audioData.durationSeconds)
-							: undefined
-					}
-				/>
-			) : (
-				<AudioPlayerWeb
-					contentIsNotSensitive={!isSensitive}
-					isAcastEnabled={isAcastEnabled}
-					src={audioData.audioDownloadUrl}
-					mediaId={audioData.mediaId}
-				/>
-			)}
-		</Island>
+		<>
+			<Island priority="critical" defer={{ until: 'visible' }}>
+				{isApps ? (
+					<AudioPlayerApps
+						audioDuration={
+							typeof audioData.durationSeconds === 'number'
+								? formatAudioDuration(audioData.durationSeconds)
+								: undefined
+						}
+					/>
+				) : (
+					<AudioPlayerWeb
+						contentIsNotSensitive={!isSensitive}
+						isAcastEnabled={isAcastEnabled}
+						src={audioData.audioDownloadUrl}
+						mediaId={audioData.mediaId}
+					/>
+				)}
+			</Island>
+			<StraightLines
+				cssOverrides={css`
+					display: block;
+					margin-bottom: ${space[2]}px;
+				`}
+				count={1}
+				color={palette('--straight-lines')}
+			/>
+		</>
 	);
 };
