dbaabf276566fc3cc7bcf557547a697ca635104a
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2026-07-20 14:36:21 +0100

    Double ECS task CPU/memory to 2 vCPU / 4GB

--- dotcom-rendering/cdk/lib/renderingStack.ts
+++ dotcom-rendering/cdk/lib/renderingStack.ts
@@ -287,8 +287,8 @@ export class RenderingCDKStack extends CDKStack {
 							imageIdentifier,
 
 							// TODO tune these values
-							memoryLimitMiB: 2048,
-							cpu: 1024,
+							memoryLimitMiB: 4096,
+							cpu: 2048,
 							scaling: {
 								minimumTasks: 1,
 								maximumTasks: 2,
