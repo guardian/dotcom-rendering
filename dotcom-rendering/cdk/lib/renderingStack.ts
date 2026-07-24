ba8d9c6faf8d795303898bffae2baf1c62f4c18e
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2026-07-24 12:45:31 +0100

    Add automatic telemetry export

--- dotcom-rendering/cdk/lib/renderingStack.ts
+++ dotcom-rendering/cdk/lib/renderingStack.ts
@@ -17,6 +17,9 @@ import { Metric, Unit } from 'aws-cdk-lib/aws-cloudwatch';
 import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import type { InstanceType } from 'aws-cdk-lib/aws-ec2';
 import { Peer } from 'aws-cdk-lib/aws-ec2';
+import type { CfnService } from 'aws-cdk-lib/aws-ecs';
+import { ContainerImage, LogDrivers } from 'aws-cdk-lib/aws-ecs';
+import { ClusterSettings } from 'aws-cdk-lib/aws-ecs/mixins';
 import { Subscription, SubscriptionProtocol, Topic } from 'aws-cdk-lib/aws-sns';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { getUserData } from './userData';
@@ -238,6 +241,13 @@ export class RenderingCDKStack extends CDKStack {
 					actions: ['cloudwatch:*', 'logs:*'],
 					resources: ['*'],
 				}),
+				new GuAllowPolicy(this, 'AllowPolicyXRay', {
+					actions: [
+						'xray:PutTraceSegments',
+						'xray:PutTelemetryRecords',
+					],
+					resources: ['*'],
+				}),
 				new GuAllowPolicy(this, 'AllowPolicyDescribeDecryptKms', {
 					actions: ['kms:Decrypt', 'kms:DescribeKey'],
 					resources: [
@@ -301,6 +311,8 @@ export class RenderingCDKStack extends CDKStack {
 				GU_STAGE: stage,
 				GU_APP: guApp,
 				GU_STACK: guStack,
+				OTEL_EXPORTER_OTLP_ENDPOINT: 'http://localhost:4318',
+				OTEL_SERVICE_NAME: guApp,
 			};
 
 			for (const [key, value] of Object.entries(ecsEnvVars)) {
@@ -308,6 +320,43 @@ export class RenderingCDKStack extends CDKStack {
 					key,
 					value,
 				);
+			}
+			// ADOT (AWS Distro for OpenTelemetry) sidecar to receive OTLP
+			// traces from the app and forward them to AWS X-Ray.
+			app.ecsService?.taskDefinition.addContainer('adot-collector', {
+				image: ContainerImage.fromRegistry(
+					'public.ecr.aws/aws-observability/aws-otel-collector:v0.49.0',
+				),
+				command: ['--config=/etc/ecs/ecs-default-config.yaml'],
+				logging: LogDrivers.awsLogs({
+					streamPrefix: `${guApp}-adot`,
+				}),
+				portMappings: [{ containerPort: 4318 }],
+				cpu: 256,
+				memoryReservationMiB: 256,
+			});
+
+			// TODO make these changes at the pattern level in GuCDK
+			if (app.ecsService) {
+				app.ecsService.cluster.with(
+					new ClusterSettings([
+						{ name: 'containerInsights', value: 'enhanced' },
+					]),
+				);
+
+				const cfnService = app.ecsService.node
+					.defaultChild as CfnService;
+				cfnService.addPropertyOverride('Monitoring', {
+					MetricConfigurations: [
+						{
+							MetricNames: [
+								'CPUUtilization',
+								'MemoryUtilization',
+							],
+							ResolutionSeconds: 20,
+						},
+					],
+				});
 			}
 		}
 
