958c322d2f49092b5d28eb65e8231f67e63d39f9
Demetrios Skamiotis <49187886+dskamiotis@users.noreply.github.com>
2026-07-29 11:13:29 +0100

    Remove the outstream ozone AB test (#16458) (#16457)
    Co-authored-by: Dominik Lander <dominik.lander@guardian.co.uk>

--- ab-testing/config/abTests.ts
+++ ab-testing/config/abTests.ts
@@ -70,19 +70,6 @@ const ABTests: ABTest[] = [
 		shouldForceMetricsCollection: true,
 	},
 	{
-		name: "commercial-mobile-sticky-liveblog-us",
-		description:
-			"Holdback test, where variant is the 'holdback' group, to measure uplift in adding the mobile-sticky slot for Liveblogs articles in the US.",
-		owners: ["commercial.dev@guardian.co.uk"],
-		expirationDate: "2026-07-22",
-		type: "client",
-		status: "ON",
-		audienceSize: 5 / 100,
-		audienceSpace: "A",
-		groups: ["control", "variant"],
-		shouldForceMetricsCollection: true,
-	},
-	{
 		name: "newsletters-in-article-signup-preview",
 		description:
 			"Test in-article newsletter signup with illustrated preview CTA vs without preview CTA",
