bbdc97c1d4439924ab7584cf339e94bf8ae95b47
Anna Beddow <anna.beddow@gmail.com>
2026-07-30 17:29:39 +0100

    Add editorialAbTest as an optional field on Front model

--- dotcom-rendering/src/types/front.ts
+++ dotcom-rendering/src/types/front.ts
@@ -39,6 +39,7 @@ export interface Front {
 	webURL: string;
 	guardianBaseURL: string;
 	serverTime?: number;
+	editorialAbTests?: EditorialAbTest[];
 }
 
 interface PressedPage {
@@ -191,4 +192,11 @@ export type Test = {
 	hasManuallyEndedOnThisTrail: boolean;
 	manuallyEndedOnThisTrailByName?: string;
 	manuallyEndedOnThisTrailByEmail?: string;
+};
+
+export type EditorialAbTest = {
+	testUuid: string;
+	expiryDate?: number;
+	frontsThisTestCanRunOn: string[];
+	hasManuallyEndedOnThisTrail: boolean;
 };
