f2af0370a9a69e07b3b5f5836d94839bcccf13a0
Jake <1731150+Jakeii@users.noreply.github.com>
2026-08-25 14:01:45 +0000

    Add 2 more AB Test audience spaces (#16588)
    * Add D and E audience spaces
    
    * add a test test
    
    * update unit test
    
    * unify space definition
    
    * remove test test
    
    * AUDIENCE_SPACES -> AudienceSpaces
    
    * lint
    
    * add comment

--- ab-testing/config/lib/constants.ts
+++ ab-testing/config/lib/constants.ts
@@ -13,10 +13,4 @@ const MVT_COUNT = 1000;
  */
 const MAX_SERVER_SIDE_TESTS = 20;
 
-/**
- * The spaces used for test groups, each space covers the entire mvt space allowing for concurrent overlapping tests where necessary.
- * If this is increased, the fastly VCL configuration will need to be updated to match.
- */
-const AUDIENCE_SPACES = ["A", "B", "C"];
-
-export { MVT_COUNT, MAX_SERVER_SIDE_TESTS, AUDIENCE_SPACES };
+export { MVT_COUNT, MAX_SERVER_SIDE_TESTS };
