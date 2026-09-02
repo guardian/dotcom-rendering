bf6be331b81613953eb022c58028e6ab288feac4
Dominik Lander <dominik.lander@guardian.co.uk>
2026-09-02 12:58:15 +0000

    Set Prebid failsafe timeout AB test to 1% (#16662)
    * Set prebid failsafe timeout to 1%
    
    * Update description of test

--- ab-testing/config/abTests.ts
+++ ab-testing/config/abTests.ts
@@ -209,16 +209,14 @@ const ABTests: ABTest[] = [
 		groups: ["a", "b"],
 		shouldForceMetricsCollection: false,
 	},
-
 	{
 		name: "commercial-prebid-failsafe-timeout",
-		description:
-			"Test to measure the impact of the Prebid failsafe timeout",
+		description: "Gradually roll out the Prebid failsafe timeout feature",
 		owners: ["commercial.dev@guardian.co.uk"],
 		expirationDate: "2026-09-30",
 		type: "client",
 		status: "ON",
-		audienceSize: 0 / 100,
+		audienceSize: 1 / 100,
 		audienceSpace: "B",
 		groups: ["control", "variant"],
 		shouldForceMetricsCollection: true,
