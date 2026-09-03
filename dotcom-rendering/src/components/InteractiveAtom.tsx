766d50865976d1959103d16dd15089d739b0f44d
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2026-09-03 10:23:18 +0000

    Make `customData` available to interactive atoms (#16612)
    * Make `customData` available to interactive atoms
    
    * Add tests
    
    * Add web test case

--- dotcom-rendering/src/components/InteractiveAtom.tsx
+++ dotcom-rendering/src/components/InteractiveAtom.tsx
@@ -34,6 +34,7 @@ type InteractiveAtomType = {
 	elementCss?: string;
 	isMainMedia?: boolean;
 	title: string;
+	customData?: string;
 };
 
 export const InteractiveAtom = ({
@@ -43,6 +44,7 @@ export const InteractiveAtom = ({
 	elementCss,
 	isMainMedia,
 	title,
+	customData,
 }: InteractiveAtomType) => {
 	const { renderingTarget } = useConfig();
 
@@ -51,6 +53,7 @@ export const InteractiveAtom = ({
 			css={[containerStyles, !!isMainMedia && fullHeightStyles]}
 			data-atom-id={id}
 			data-atom-type="interactive"
+			{...(customData ? { 'data-atom-custom-data': customData } : {})}
 		>
 			<Island
 				priority="feature"
---
+++ dotcom-rendering/src/lib/interactiveAtoms.test.tsx
@@ -0,0 +1,64 @@
+import { render } from '@testing-library/react';
+import { ConfigProvider } from '../components/ConfigContext';
+import { InteractiveAtom } from '../components/InteractiveAtom';
+import { InteractiveLayoutAtom } from '../components/InteractiveLayoutAtom';
+
+const mockCustomData = JSON.stringify({ key: 'value' });
+
+describe('Interactive atom custom data attributes', () => {
+	it('should add custom data attribute to InteractiveAtom on web', () => {
+		const { container } = render(
+			<ConfigProvider
+				value={{
+					renderingTarget: 'Web',
+					darkModeAvailable: true,
+					assetOrigin: '/',
+					editionId: 'UK',
+				}}
+			>
+				<InteractiveAtom
+					id="test-id"
+					title="Test Title"
+					customData={mockCustomData}
+				/>
+			</ConfigProvider>,
+		);
+		const el = container.querySelector(
+			`[data-atom-custom-data='${mockCustomData}']`,
+		);
+		expect(el).toBeInTheDocument();
+	});
+
+	it('should add custom data attribute to InteractiveAtom on apps', () => {
+		const { container } = render(
+			<ConfigProvider
+				value={{
+					renderingTarget: 'Apps',
+					darkModeAvailable: true,
+					assetOrigin: '/',
+					editionId: 'UK',
+				}}
+			>
+				<InteractiveAtom
+					id="test-id"
+					title="Test Title"
+					customData={mockCustomData}
+				/>
+			</ConfigProvider>,
+		);
+		const el = container.querySelector(
+			`[data-atom-custom-data='${mockCustomData}']`,
+		);
+		expect(el).toBeInTheDocument();
+	});
+
+	it('should add custom data attribute to InteractiveLayoutAtom', () => {
+		const { container } = render(
+			<InteractiveLayoutAtom id="test-id" customData={mockCustomData} />,
+		);
+		const el = container.querySelector(
+			`[data-atom-custom-data='${mockCustomData}']`,
+		);
+		expect(el).toBeInTheDocument();
+	});
+});
