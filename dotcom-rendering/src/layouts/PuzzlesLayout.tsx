8ee58680faf67d1a54295fe2973aaed50fadc362
Gustavo Olmedo <gustavo.olmedo.formosa@gmail.com>
2026-09-10 13:31:55 +0000

    Puzzles/align hub blueprint with target composition (#16706)
    * Update types after updating blueprint format to include ads
    
    * Update model validation after updating blueprint for ads
    
    * Update to refactor experiment utility
    
    * Update to pass the abtesting obj instead of the whole config obj
    
    * Update to remove darkmode and paid content, we will work on it later
    
    * Update to add archive button island
    
    * Add puzzles directory component
    
    * Add tests for puzzles directory
    
    * Add puzzles directory stories
    
    * Update layout to add initial rendering WIP
    
    * Add simple unit tests for the layout
    
    * Update after puzzles page contract
    
    * Update to work on the responsive puzzles directory
    
    * Fix puzzles page island markup
    
    * Update featured pluzze card layout
    
    * Update puzzles hub URL fixture
    
    * Update missing changes related to puzzles routes & update archive links
    
    * Update to run chromatic
    
    * Fix typo in Puzzles and games texts
    
    * Update to run chromatic

--- dotcom-rendering/src/layouts/PuzzlesLayout.tsx
+++ dotcom-rendering/src/layouts/PuzzlesLayout.tsx
@@ -1,79 +1,119 @@
 import { css } from '@emotion/react';
-import { headlineBold34, palette, space } from '@guardian/source/foundations';
+import {
+	from,
+	headlineBold50,
+	palette,
+	space,
+} from '@guardian/source/foundations';
 import { Footer } from '../components/Footer';
 import { HeaderAdSlot } from '../components/HeaderAdSlot';
 import { Masthead } from '../components/Masthead/Masthead';
+import { PuzzlesDirectory } from '../components/PuzzlesDirectory';
 import { Section } from '../components/Section';
 import type { NavType } from '../model/extract-nav';
 import type { FEPuzzlesPageType } from '../types/puzzlesPage';
 import { Stuck } from './lib/stickiness';
 
-type Props = {
-	puzzlesPage: FEPuzzlesPageType;
-	NAV: NavType;
-};
-
-const heading = css`
-	margin: 0;
-	padding: ${space[6]}px 0 ${space[12]}px;
-	${headlineBold34};
+const mainStyles = css`
+	padding-bottom: ${space[12]}px;
+	background: ${palette.neutral[100]};
+	color: ${palette.neutral[7]};
+	color-scheme: light;
 `;
 
-/**
- * The base puzzles page shell. Blueprint-driven composition is intentionally
- * deferred to the next task.
- */
-export const PuzzlesLayout = ({ puzzlesPage, NAV }: Props) => (
-	<>
-		<div data-print-layout="hide" id="bannerandheader">
-			{!puzzlesPage.isAdFreeUser && (
-				<Stuck>
-					<Section
-						fullWidth={true}
-						showTopBorder={false}
-						showSideBorders={false}
-						padSides={false}
-						shouldCenter={false}
-					>
-						<HeaderAdSlot />
-					</Section>
-				</Stuck>
-			)}
-			<Masthead
-				nav={NAV}
-				editionId={puzzlesPage.editionId}
-				idUrl={puzzlesPage.config.idUrl}
-				mmaUrl={puzzlesPage.config.mmaUrl}
-				discussionApiUrl={puzzlesPage.config.discussionApiUrl}
-				idApiUrl={puzzlesPage.config.idApiUrl}
-				contributionsServiceUrl={puzzlesPage.contributionsServiceUrl}
-				showSubNav={true}
-				showSlimNav={false}
-				hasPageSkin={false}
-				hasPageSkinContentSelfConstrain={false}
-			/>
-		</div>
+const brandStyles = css`
+	max-width: 1300px;
+	margin: 0 auto;
+	padding: ${space[6]}px ${space[3]}px ${space[8]}px;
+	border-right: 1px solid ${palette.neutral[86]};
+	border-left: 1px solid ${palette.neutral[86]};
+	background: ${palette.neutral[97]};
+`;
 
-		<main data-layout="PuzzlesPageLayout" id="maincontent">
-			<Section fullWidth={true} showTopBorder={false}>
-				<h1 css={heading}>{puzzlesPage.webTitle}</h1>
+const titleStyles = css`
+	max-width: 10ch;
+	margin: 0;
+	${headlineBold50};
+	font-size: 48px;
+	line-height: 0.9;
+
+	${from.tablet} {
+		font-size: 64px;
+	}
+`;
+
+export const PuzzlesLayout = ({
+	puzzlesPage,
+	NAV,
+}: {
+	puzzlesPage: FEPuzzlesPageType;
+	NAV: NavType;
+}) => {
+	const renderAds = !puzzlesPage.isAdFreeUser;
+	return (
+		<>
+			<div data-print-layout="hide" id="bannerandheader">
+				{renderAds && (
+					<Stuck>
+						<Section
+							fullWidth={true}
+							showTopBorder={false}
+							showSideBorders={false}
+							padSides={false}
+							shouldCenter={false}
+						>
+							<HeaderAdSlot />
+						</Section>
+					</Stuck>
+				)}
+				<Masthead
+					nav={NAV}
+					editionId={puzzlesPage.editionId}
+					idUrl={puzzlesPage.config.idUrl}
+					mmaUrl={puzzlesPage.config.mmaUrl}
+					discussionApiUrl={puzzlesPage.config.discussionApiUrl}
+					idApiUrl={puzzlesPage.config.idApiUrl}
+					contributionsServiceUrl={
+						puzzlesPage.contributionsServiceUrl
+					}
+					showSubNav={true}
+					showSlimNav={false}
+					hasPageSkin={false}
+					hasPageSkinContentSelfConstrain={false}
+				/>
+			</div>
+			<main
+				css={mainStyles}
+				data-layout="PuzzlesPageLayout"
+				id="maincontent"
+			>
+				<header css={brandStyles}>
+					<h1 css={titleStyles}>
+						Puzzles
+						<br />
+						&amp; Games
+					</h1>
+				</header>
+				<PuzzlesDirectory
+					layout={puzzlesPage.layout}
+					renderAds={renderAds}
+				/>
+			</main>
+			<Section
+				fullWidth={true}
+				padSides={false}
+				backgroundColour={palette.brand[400]}
+				borderColour={palette.brand[600]}
+				showSideBorders={false}
+				element="footer"
+			>
+				<Footer
+					pageFooter={puzzlesPage.pageFooter}
+					pillars={NAV.pillars}
+					urls={NAV.readerRevenueLinks.footer}
+					editionId={puzzlesPage.editionId}
+				/>
 			</Section>
-		</main>
-
-		<Section
-			fullWidth={true}
-			padSides={false}
-			backgroundColour={palette.brand[400]}
-			borderColour={palette.brand[600]}
-			showSideBorders={false}
-			element="footer"
-		>
-			<Footer
-				pageFooter={puzzlesPage.pageFooter}
-				pillars={NAV.pillars}
-				urls={NAV.readerRevenueLinks.footer}
-				editionId={puzzlesPage.editionId}
-			/>
-		</Section>
-	</>
-);
+		</>
+	);
+};
