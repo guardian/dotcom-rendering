e0377513425eb53a1c0a573702b8c0a52d2c1bac
Ravi <7014230+arelra@users.noreply.github.com>
2026-09-11 18:02:35 +0100

    \

--- dotcom-rendering/eslint.config.mjs
+++ dotcom-rendering/eslint.config.mjs
@@ -297,7 +297,7 @@ export default defineConfig([
 		},
 	},
 	{
-		files: ['**/**.stories.tsx', '**/**.stories.jsx'],
+		files: ['**/**.stories.tsx'],
 
 		rules: {
 			'import/prefer-default-export': 'off',
