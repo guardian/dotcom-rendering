11400a4ad6436e4dd0395faeda80047169f660be
Akinsola Lawanson <akinsola.lawanson@guardian.co.uk>
2026-09-11 10:37:57 +0100

    Comment out all other US states apart from South Carolina

--- dotcom-rendering/src/components/marketing/lib/consentBannerTest.ts
+++ dotcom-rendering/src/components/marketing/lib/consentBannerTest.ts
@@ -2,18 +2,18 @@ import { getCookie } from '@guardian/libs';
 
 const AB_TEST_GEO_REGION_COOKIE = 'GU_geo_country_region';
 const AB_TEST_US_STATES = [
-	'WA', // Washington
-	'NC', // North Carolina
-	'OH', // Ohio
 	'SC', // South Carolina
-	'MI', // Michigan
-	'AZ', // Arizona
-	'MO', // Missouri
-	'WI', // Wisconsin
-	'DC', // District of Columbia
-	'KS', // Kansas
-	'NM', // New Mexico
-	'ME', // Maine
+	// 'WA', // Washington
+	// 'NC', // North Carolina
+	// 'OH', // Ohio
+	// 'MI', // Michigan
+	// 'AZ', // Arizona
+	// 'MO', // Missouri
+	// 'WI', // Wisconsin
+	// 'DC', // District of Columbia
+	// 'KS', // Kansas
+	// 'NM', // New Mexico
+	// 'ME', // Maine
 ];
 
 export const isInUsStateForAbTest = (): boolean => {
