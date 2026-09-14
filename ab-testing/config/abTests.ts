a1ec92dfac833ffd1c55fb14eeaf7bcdfbec5715
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2026-09-14 13:51:34 +0100

    adding new variant variant2 which does not show the RR banner

--- ab-testing/config/abTests.ts
+++ ab-testing/config/abTests.ts
@@ -215,7 +215,7 @@ const ABTests: ABTest[] = [
 		status: "ON",
 		audienceSize: 0 / 100,
 		audienceSpace: "B",
-		groups: ["control", "variant"],
+		groups: ["control", "variant", "variant2"],
 		shouldForceMetricsCollection: true,
 	},
 	{
