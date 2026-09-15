1b1a5761c70e01bb5aace36e9258087583fa0e7c
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2026-09-15 15:07:16 +0100

    Remove font weight override

--- dotcom-rendering/src/components/Standfirst.tsx
+++ dotcom-rendering/src/components/Standfirst.tsx
@@ -417,15 +417,6 @@ export const Standfirst = ({ format, standfirst, layoutType }: Props) => {
 					layoutType?.startsWith('immersive')
 						? immersiveGridOverrides
 						: undefined,
-					layoutType?.startsWith('immersive') === true &&
-						format.theme === ArticleSpecial.Labs &&
-						css`
-							font-weight: 500;
-
-							${from.tablet} {
-								font-weight: 500;
-							}
-						`,
 				]}
 				className={
 					format.design === ArticleDesign.Interactive
