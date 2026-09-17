b509f261c992e05f853f055b98b20cdd031cf0e2
Dina Hafez <dina.hafez@guardian.co.uk>
2026-09-17 10:10:07 +0000

    Open up `article-end` in US for Header Bidding (#16739)
    * Change additionalSizes for article-end header bidding integration and add 0% test
    
    * Add condition for variant 0% test
    
    * Update unit tests based on the changes

--- ab-testing/config/abTests.ts
+++ ab-testing/config/abTests.ts
@@ -333,6 +333,19 @@ const ABTests: ABTest[] = [
 		],
 		shouldForceMetricsCollection: true,
 	},
+	{
+		name: "commercial-article-end-header-bidding",
+		description:
+			"Test opening up the article-end ad slot in the US region for HeaderBidding",
+		owners: ["commercial.dev@guardian.co.uk"],
+		expirationDate: "2026-10-01",
+		type: "client",
+		status: "ON",
+		audienceSize: 0 / 100,
+		audienceSpace: "B",
+		groups: ["control", "variant"],
+		shouldForceMetricsCollection: true,
+	},
 ];
 
 const activeABtests = ABTests.filter((test) => test.status === "ON");
--- dotcom-rendering/src/components/SlotBodyEnd.island.tsx
+++ dotcom-rendering/src/components/SlotBodyEnd.island.tsx
@@ -56,6 +56,7 @@ type Props = {
 
 const slotStyles = css`
 	color: ${palette.neutral[7]};
+	margin: 12px auto;
 `;
 
 const buildReaderRevenueEpicConfig = (
@@ -130,10 +131,18 @@ export const SlotBodyEnd = ({
 	const [asyncArticleCount, setAsyncArticleCount] =
 		useState<Promise<WeeklyArticleHistory | undefined>>();
 
-	const showPublicGood = countryCode === 'US';
+	const isInUS = countryCode === 'US';
+	const isInArticleEndHeaderBiddingTest = abTests?.isUserInTestGroup(
+		'commercial-article-end-header-bidding',
+		'variant',
+	);
 
 	const showArticleEndSlot =
-		renderAds && !isLabs && showPublicGood && articleEndSlot;
+		renderAds &&
+		!isLabs &&
+		isInUS &&
+		articleEndSlot &&
+		isInArticleEndHeaderBiddingTest;
 
 	useEffect(() => {
 		setAsyncArticleCount(
@@ -238,7 +247,9 @@ export const SlotBodyEnd = ({
 				new CustomEvent('gu.commercial.slot.fill', {
 					detail: {
 						slotId: 'dfp-ad--article-end',
-						additionalSizes: { mobile: [adSizes.fluid] }, // Public Good additional ad slot sizes
+						additionalSizes: {
+							mobile: [adSizes.mpu],
+						},
 					},
 				}),
 			);
