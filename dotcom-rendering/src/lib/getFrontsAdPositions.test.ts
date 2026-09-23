78831511ec54d114fe5b9935b8ab595422984e2d
Dina Hafez <dina.hafez@guardian.co.uk>
2026-09-23 13:02:10 +0100

    Add maximum ad limit on mobile test

--- dotcom-rendering/src/lib/getFrontsAdPositions.test.ts
+++ dotcom-rendering/src/lib/getFrontsAdPositions.test.ts
@@ -364,6 +364,16 @@ describe('Mobile Ads', () => {
 
 		expect(mobileAdPositions).toEqual([4, 6, 8, 13, 18]);
 	});
+
+	it('inserts a maximum of 20 ads for fronts', () => {
+		const adPositions = getMobileAdPositions(
+			// 22x number of test collections in fixture to reach maximum level
+			new Array<DCRCollectionType[]>(22).fill(testCollectionsUk).flat(),
+			'europe',
+		);
+
+		expect(adPositions.length).toEqual(20);
+	});
 });
 
 describe('Desktop Ads', () => {
